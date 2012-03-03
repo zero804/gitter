7c07b0ee872aec5ef1280256710dc100d9e8cc51
Michael Bartlett <mike@hannibal.(none)>
2012-03-03 12:34:21 +0100

    Got the haraka plugin to deliver email to Mongo

--- haraka/plugins/troupe.js
+++ haraka/plugins/troupe.js
@@ -7,26 +7,51 @@
 
 var persistence = require("./../../server/services/persistence-service.js")
 
+var who; //who is the email TO mapped as a troupe URI
+var from;
+
+// let's look for the sender first
+exports.hook_mail = function(next,connection,params) {
+
+  var tmp = params[0].toString();
+  var loc = tmp.indexOf(">");
+  from = tmp.substring(1,loc);
+  
+  
+  // for now we'll just look up to see if the SENDER exists in our user table.
+  // we actually want to match them to the Troupe to make sure they are in the Troupe and can send.
+  
+    persistence.User.findOne({
+		email : from
+	  }, function(err, user) {
+		if (err) {
+		  callbackFunction(err, null);
+		}
+	
+		if (user == null) {
+		  connection.logdebug("SENDER CHECK: Unknown sender - bounce the message");
+		  return next(DENY, "Sorry, I don't know you.");
+		  
+		} else {
+		  connection.logdebug("SENDER CHECK: Valid sender (" + from + ") - so far so good");
+		  next();
+		  return true;
+		  
+		}
+	  });
+    
+}
+
+// now let's look for the recipient and match that to a Troupe
 exports.hook_rcpt = function(next, connection, params) {
   var rcpt = params[0];
-  var who;
   var tmp;
+ 
 
   tmp = rcpt.toString();
-
   var loc = tmp.indexOf("@");
-
-  // we start at 1 because the recipient always starts with a "<" (i think!)
   who = tmp.substring(1, loc);
 
-  this.loginfo("Trying to send to : " + who);
-
-  // Yeah, bad bad bad.
-  // Haraka has some weird hybrid plugin system and I cannot for the life of my
-  // figure out where the relative path to this is, I've tried everything - so
-  // maybe we need to somehow extract this service so it can be put into
-  // package.json, which is what i did with mongoose. (Also BAD BAD BAD, is I
-  // just copied node_modules from troupe's web server into the haraka dir).
 
   // I don't entirely understand the node queue system, but I guess this is NOT
   // the way to do it as I've seen the output of the Mongo query happen after
@@ -42,11 +67,44 @@ exports.hook_rcpt = function(next, connection, params) {
     }
 
     if (whatTroupe == null) {
-      connection.loginfo("Unknown Troupe (" + who + ") - bounce the message");
+      connection.logdebug("RECIPIENT TROUPE CHECK: Unknown Troupe (" + who + ") - bounce the message");
+	  return next(DENY, "Nope, that Troupe doesn't exist.");
+	  
     } else {
-      connection.logdebug("Valid Troupe (" + who + ") - deliver the message");
+      connection.logdebug("RECIPIENT TROUPE CHECK: Valid Troupe (" + who + ") - deliver the message");
+	  
+	  
+	  next();
+	  return true;
+	  
     }
   });
 
-  next();
+  
 }
+
+
+// now lets move on and deliver the mail to the database
+
+exports.hook_queue = function(next, connection) {
+    var lines = connection.transaction.data_lines;
+    if (lines.length === 0) {
+        return next(DENY);
+    }
+    
+	
+	 var storeMail = new persistence.Email();
+	  storeMail.from = from.toString();
+	  storeMail.troupeURI = who.toString();
+	  storeMail.mail = lines.join('');
+	  
+	
+	  storeMail.save(function(err) {
+		if(err == null) {
+		 
+		  connection.logdebug("Stored the email.");
+		  //callback(null);
+		  return next(OK);
+		}
+	  });
+};
