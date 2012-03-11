7638bbb1a0c7744d9f6b0975dfc655c5b28b5cc4
Michael Bartlett <mike@hannibal.(none)>
2012-03-11 13:36:42 +0100

    Added preview field when storing email

--- haraka/plugins/troupe.js
+++ haraka/plugins/troupe.js
@@ -4,8 +4,14 @@ var persistence = require("./../../server/services/persistence-service.js")
  
 var troupeService = require("./../../server/services/troupe-service.js");
 
+
+exports.hook_data = function (next, connection) {
+    // enable mail body parsing
+    connection.transaction.parse_body = 1;
+    next();
+}
+
 exports.hook_queue = function(next, connection) {
-	
 	// Get some stuff from the header to store later
 	var subject = connection.transaction.header.get("Subject");
 	var date = connection.transaction.header.get("Date");
@@ -19,7 +25,7 @@ exports.hook_queue = function(next, connection) {
 	fromName = fromName.replace(/\n/g,"");
 	subject = subject.replace(/\n/g,"");
 	
-	
+	// do some string parsing for email formats such as Name <email>
 	
 	if (fromName.indexOf("<") > 0)  {
 	  var fromEmail = fromName.substring(fromName.indexOf("<") + 1, fromName.indexOf(">"));
@@ -28,28 +34,36 @@ exports.hook_queue = function(next, connection) {
 	else { 
 	   var fromEmail = fromName;
 	 }
+	 
+	// We're going to extract a message preview to store in mongo so we don't have to parse the email every time someone views it
+	// First we check to see if the message is single part or multipart and then we extract the bodytext or plaintext bodytext and then we chop it down to 255 chars
+	 
+	if (connection.transaction.body.children.length==0) {
+		//connection.logdebug("Single part message");
+		var preview = connection.transaction.body.bodytext;
+	}
+	else {
+		//connection.logdebug("Multipart message");
+		var preview = connection.transaction.body.children[0].bodytext;
+	}
 	
+	if (preview.length>255) preview=preview.substring(0,255);
 	
-	connection.logdebug("To: " + JSON.stringify(toName));
-	connection.logdebug("From: " + fromName);
-	connection.logdebug("Email: " + fromEmail);
+	//connection.logdebug("Body: " + JSON.stringify(connection.transaction.body.bodytext));
+    //connection.logdebug("Children: " + JSON.stringify(connection.transaction.body.children.length));
+	//connection.logdebug("Child: " + connection.transaction.body.children[0].bodytext);
+	//connection.logdebug("To: " + JSON.stringify(toName));
+	//connection.logdebug("From: " + fromName);
+	//connection.logdebug("Email: " + fromEmail);
+	//connection.logdebug("Preview: " + preview);
+	//connection.logdebug("Mail Body : "+ lines.join(''));
 	
 	troupeService.validateTroupeEmail({ to: toName, from: fromEmail}, function(err, troupe) {
 	  if (err) return next(DENY, "Sorry, either we don't know you, or we don't know the recipient. You'll never know which.");
 	  if (!troupe) return next (DENY, "Sorry, either we don't know you, or we don't know the recipient. You'll never know which.")  
 	  
-	  var storeMail = new persistence.Email();
-	  storeMail.from = fromEmail;
-	  storeMail.troupeId = troupe.id;
-	  storeMail.subject = subject;
-	  storeMail.date = date;
-	  storeMail.fromName = fromName;
-	  storeMail.mail = lines.join('');
-	  storeMail.delivered = false;
-	  
-	
-	  storeMail.save(function(err) {
-		if (err) return next(DENY);
+	  troupeService.storeEmail({ fromEmail: fromEmail, troupeID: troupe.ID, subject: subject, date: date, fromName: fromName, preview: preview, mailBody: lines.join('')}, function(err) {
+	   if (err) return next(DENY, "Failed to store the email");
 		 
 		connection.logdebug("Stored the email.");
 
@@ -61,9 +75,5 @@ exports.hook_queue = function(next, connection) {
 																		  
 	})
 	
-	// Make the FromName field look better. Sometimes From can only be an email address, other times it has a name with the email address in <>
-	//if (fromName.indexOf("<") > 0)  {
-//		fromName = fromName.substring(0, fromName.indexOf("<")-1);					 
-//	 }
-
+	
 };
