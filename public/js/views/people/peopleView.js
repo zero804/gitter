3c26c74ba79ae7c9688213428242508a04e18ed0
Andrew Newdigate <andrewn@datatribe.net>
2012-03-12 22:58:15 +0000

    Re-added redis for session mgmt Added people page Fixed Mike's hack for the user's name in the toolbar. Added /troupe/:id:/users/ REST resource to API (work in progress)

--- public/js/views/people/peopleView.js
+++ public/js/views/people/peopleView.js
@@ -4,13 +4,64 @@ define([
   'underscore',
   'backbone',
   'mustache',
-  'text!templates/people/people.mustache'
-], function($, _, Backbone, Mustache, template){
+  'text!templates/people/people.mustache',
+  'text!templates/people/row.mustache'
+], function($, _, Backbone, Mustache, template, rowTemplate){
   var PeopleView = Backbone.View.extend({    
+    
+    initialize: function() {
+      var self = this;
+      $.ajax({
+        url: "/troupes/" + window.troupeContext.troupe.id + "/users",
+        contentType: "application/json",
+        dataType: "json",
+        type: "GET",
+        success: function(data) {
+          self.renderUsers(data);
+        }
+      });
+    },
+    
+    events: {
+      "click .button-makeAdmin": "makeAdmin",
+      "click .button-remove": "removeUser"
+    },
+    
     render: function() {
       var compiledTemplate = Mustache.render(template, { });
       $(this.el).html(compiledTemplate);
       return this;
+    },
+    
+    renderUsers: function(users) {
+      $(".frame-people", this.el).empty();
+      while(users.length > 0) {
+        var p1 = users.shift();
+        var p2 = users.shift();
+        
+        var rowHtml = Mustache.render(rowTemplate, { 
+          person1Name: p1.displayName,
+          person2Name: p2 ? p2.displayName : null,
+          person2: p2,
+          more: users.length > 0,
+          person1MakeAdmin: true,
+          person2MakeAdmin: true,
+          person1Remove: true,
+          person2Remove: true
+        });
+        
+        $(".frame-people", this.el).append(rowHtml);
+      }
+    },
+    
+    makeAdmin: function() {
+      window.alert("Make Admin");
+      return false;
+    },
+    
+    removeUser: function() {
+      window.alert("Remove User");
+      return false;
     }
     
   });
---
+++ server/resources/users.js
@@ -0,0 +1,43 @@
+var troupeService = require("../services/troupe-service"),
+    userService = require("../services/user-service");
+
+module.exports = {
+    index: function(req, res, next) {
+      userService.findByIds(req.troupe.users, function(err, users) {
+        if(err) return next(err);
+        
+        res.send(users);
+      });
+    },
+
+    new: function(req, res){
+      res.send(500);
+    },
+
+    create: function(req, res) {
+      res.send(500);
+    },
+
+    show: function(req, res){
+      res.send(500);
+    },
+
+    edit: function(req, res){
+      res.send(500);
+    },
+
+    update:  function(req, res){
+      res.send(500);
+    },
+
+    destroy: function(req, res){
+      res.send(500);
+    },
+
+    load: function(id, fn){
+      process.nextTick(function(){
+        fn(null, { id: id, title: 'SHARE' });
+      });
+    }
+
+};
