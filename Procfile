82898d162be2a548f0088a6bc3a45b791162cef3
Andrew Newdigate <andrewn@datatribe.net>
2012-04-09 00:51:35 +0100

    Continued work on server readiness

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: node web.js
 mail: node node_modules/Haraka/bin/haraka -c ./haraka
+websockets: node websockets.js
--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #/bin/sh
 ./node_modules/less/bin/lessc -x public/bootstrap/less/troupe.less  > public/bootstrap/css/troupe.css 
+./node_modules/less/bin/lessc -x public/bootstrap/less/trp.less  > public/bootstrap/css/trp.css 
