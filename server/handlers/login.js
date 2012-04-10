f886906c375ffdfb4530c353400a6775074d5a55
Andrew Newdigate <andrewn@datatribe.net>
2012-04-10 18:53:28 +0100

    Fixed nginx login bug

--- server/handlers/login.js
+++ server/handlers/login.js
@@ -3,26 +3,29 @@
 "use strict";
 
 var passport = require("passport");
+var nconf = require('./server/utils/config').configure();
 
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
-  res.relativeRedirect('/loginfail')
+  res.relativeRedirect('/loginfail');
 }
 
 module.exports = {
     install: function(app) {
+      var basepath = nconf.get("web:basepath");
+
       app.post('/login',
-          passport.authenticate('local', { successRedirect: '/loginsuccess',
-                                           failureRedirect: '/loginfail' })
+          passport.authenticate('local', { successRedirect: basepath + '/loginsuccess',
+                                           failureRedirect: basepath + '/loginfail' })
         );
 
       /* Cheap trick for testing */
       app.get('/login', function(req, res) {
         res.render("login/login");
       });
-      
+
       app.get('/loginsuccess', ensureAuthenticated, function(req, res) {
-        res.send({     
+        res.send({
           failed: false,
           user: req.user
         });
