e24fda2d89caeb743b94b19a40842df56e7a89b9
Mike Bartlett <mike.bartlett@gmail.com>
2012-04-17 16:54:52 +0100

    Validation on front page, megahack. Yuk.

--- public/js/signup.js
+++ public/js/signup.js
@@ -6,24 +6,71 @@ require.config({
   priority : [ 'jquery' ]
 });
 
+// Two weird things I can't figure out here.
+// Firstly, the first time you click Get Started if you haven't touched any of the form elements, no tooltip shows
+// even though the tooltip show call looks like it happens - I tested to make sure it was being executed by putting an alert before it
+// I thought it maybe had something to do with the form field automatically focusing, so I turned focusInvalid on the validater off
+// and that didn't do anything. 
+//
+// AH! I figured it out. The tooltip element hasn't been created yet or something like that. If I pre-create the tooltip in the HTML (see the moustache file)
+// then it's all fine. But this is a filthy hack.
+
+// The second bit I don't get is that the tooltips continue to show afterwards, even if I try to set options.trigger to manual, they still still
+// on hover and focus. Annoying.
+//
+// Anyhoo, it's a bit of a mess at the moment, but at least it won't submit with bad shit in it.
+
 require(
     [ 'jquery', 'jquery_validate' ],
     function($, v) {
+      var validationErrors = {};
+      function attachTooltipHandlerToItem(index, el) {
+        var jel = $(el);
+          jel.tooltip({title: function() {
+            var v = validationErrors[el.name];
+            return v ? v:"";
+          }});
+      }
+      $('#signupForm .validateable').each(attachTooltipHandlerToItem);
       $("#signupForm").validate({
+        debug: true,
+        focusInvalid: false,
+        submitHandler: function(form) {
+          form.submit();
+        },
+        showErrors: function(errorMap, errorList){
+          $('#signupForm .validateable').each(function(index, el) {
+            if (errorMap[el.name]) {
+              $(el).data('tooltip').options.placement = 'bottom';
+              $(el).data('tooltip').options.trigger = 'focus';
+              $(el).tooltip('show');
+            } else {
+              $(el).tooltip('hide');
+            }
+
+          });
+          validationErrors = errorMap;
+        },
         errorClass : "help-inline",
         rules : {
           troupeName : {
             maxlength : 32,
-            required : true,
-            remote: {
-              url: "troupenameavailable",
-              type: "get"
-            }
+            minlength: 4,
+            required : true
           },
+          email : {
+            required: true,
+            email: true
+          }
         },
         messages: {
           troupeName: {
-            remote: "Sorry, this name is taken, please choose another." 
+            minlength: "Please choose a longer name for your Troupe. It needs to be at least 4 letters.",
+            required: "Please choose a name for your Troupe."
+          },
+          email : {
+            required: "Please type your email address here.",
+            email: "Hmmm, that doesn't look like an email address."
           }
         }
   
