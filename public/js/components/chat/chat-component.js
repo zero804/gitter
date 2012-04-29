f63a7c4a9df9bd0bb72a6125370ec43ebefb72cd
Andrew Newdigate <andrewn@datatribe.net>
2012-04-29 12:41:02 +0100

    Presence service

--- public/js/components/chat/chat-component.js
+++ public/js/components/chat/chat-component.js
@@ -3,35 +3,36 @@ define([
   'underscore',
   'now'
 ], function($, _, Backbone, nowStub){
-  
+
   "use strict";
-  
+
   var connected = false;
   var subscribeOnConnect = true;
-  
+
   var module = {
     connect: function() {
 
       now.onTroupeChatMessage = function(message) {
         $(document).trigger('chat', message);
       };
-      
+
       now.ready(function() {
         connected = true;
+        now.subscribeToTroupe(window.troupeContext.troupe.id);
         if(subscribeOnConnect) {
           module.subscribeTroupeChatMessages();
         }
       });
 
     },
-    
+
     send: function(message) {
       now.newChatMessageToTroupe({
         troupeId: window.troupeContext.troupe.id,
         text: message
       });
     },
-    
+
     subscribeTroupeChatMessages: function() {
       if(connected) {
         now.subscribeToTroupeChat(window.troupeContext.troupe.id);
@@ -39,7 +40,7 @@ define([
         subscribeOnConnect = true;
       }
     },
-    
+
     unsubscribeTroupeChatMessages: function() {
      if (connected) {
         now.unsubscribeToTroupeChat(window.troupeContext.troupe.id);
@@ -47,7 +48,6 @@ define([
       subscribeOnConnect = false;
     }
   };
-  
-  
+
   return module;
 });
---
+++ server/services/presence-service.js
@@ -0,0 +1,232 @@
+/*jshint globalstrict:true, trailing:false */
+/*global console:false, require: true, module: true */
+"use strict";
+
+var redis = require("redis"),
+    winston = require('winston'),
+    redisClient;
+
+function resetClientState() {
+  function clearState(collection, item, name) {
+    redisClient.smembers(collection, function(err, members) {
+      if(err) return winston.error("Redis error: " + err);
+
+      if(members.length) {
+        var keysToDelete = members.map(function(userId) { return item + ":" + userId });
+        keysToDelete.push(collection);
+
+        redisClient.del(keysToDelete, function(err, count) {
+          winston.info("Removed " + (count - 1) + " stale " + name + "(s).");
+        });
+      }
+    });
+  }
+
+  clearState("presence:activeusers", "user_sockets", "user");
+  clearState("presence:activetroupes", "troupe_users", "troupe");
+  clearState("presence:activesockets", "socket_troupe", "socket");
+}
+
+
+console.log("Presence service establishing redis client");
+/* TODO: shutdown client at end of session */
+redisClient = redis.createClient();
+
+function defaultRedisCallback(err) {
+  if(err) winston.error("Redis error: " + err);
+}
+
+function addUserToActiveUsers(userId, callback) {
+  if(!callback) callback = defaultRedisCallback;
+  redisClient.sadd("presence:activeusers", userId, function(err, count) {
+    if(err) return callback(err);
+    callback(null, count == 1);
+  });
+}
+
+function addTroupeToActiveTroupes(troupeId, callback) {
+  if(!callback) callback = defaultRedisCallback;
+  redisClient.sadd("presence:activetroupes", troupeId, function(err, count) {
+    if(err) return callback(err);
+    callback(null, count == 1);
+  });
+}
+
+
+function addSocketToUserSockets(userId, socketId, callback) {
+  if(!callback) callback = defaultRedisCallback;
+  redisClient.rpush("user_sockets:" + userId, socketId, callback);
+}
+
+function addSocketToActiveSockets(socketId, callback) {
+  if(!callback) callback = defaultRedisCallback;
+  redisClient.sadd("presence:activesockets", socketId, callback);
+}
+
+function addUserToTroupe(userId, troupeId, callback) {
+  if(!callback) callback = defaultRedisCallback;
+
+  redisClient.lrange("troupe_users:" + troupeId, 0, -1, function(err, presentUsers) {
+    if(err) return callback(err);
+
+    var initialJoin = presentUsers.indexOf(userId) == -1;
+    redisClient.rpush("troupe_users:" + troupeId, userId, function(err, newLength) {
+      if(err) return callback(err);
+      callback(null, initialJoin);
+    });
+
+  });
+}
+
+function associateSocketToTroupe(socketId, troupeId, callback) {
+  if(!callback) callback = defaultRedisCallback;
+
+  redisClient.set("socket_troupe:" + socketId, troupeId, callback);
+}
+
+function removeSocketFromUserSockets(socketId, userId, callback) {
+  if(!callback) callback = defaultRedisCallback;
+
+  var key = "user_sockets:" + userId;
+
+  /* Manage user presence */
+  redisClient.lrem(key, 0, socketId, function(err, count) {
+    if(err) return callback(err);
+
+    redisClient.llen(key, function(err, count) {
+      if(err) return callback(err);
+      var lastDisconnect = count == 0;
+
+      callback(null, lastDisconnect);
+    });
+  });
+}
+
+function removeUserFromActiveUsers(userId, callback) {
+  if(!callback) callback = defaultRedisCallback;
+  redisClient.srem("presence:activeusers", userId, callback);
+}
+
+function disassociateSocketFromTroupe(socketId, callback) {
+  if(!callback) callback = defaultRedisCallback;
+  redisClient.del("socket_troupe:" + socketId);
+}
+
+function getTroupeAssociatedToSocket(socketId, callback) {
+  if(!callback) callback = defaultRedisCallback;
+  redisClient.get("socket_troupe:" + socketId, callback);
+}
+
+function removeUserFromTroupe(userId, troupeId, callback) {
+  if(!callback) callback = defaultRedisCallback;
+
+  /* Remove one copy of this user from the troupe active users list */
+  redisClient.lrem("troupe_users:" + troupeId, 1, userId, function(err, count) {
+    if(err) return callback(err);
+    if(count != 1) {
+      winston.warn("Inconsistent state: userId " + userId + " was removed from troupe " + troupeId + " " + count + " times. Expecting once.");
+    }
+
+    /* TODO: make the troupe_users keys into SORTED SETS */
+    redisClient.lrange("troupe_users:" + troupeId, 0, -1, function(err, presentUsers) {
+      if(err) return callback(err);
+      var lastConnectionForUser = presentUsers.indexOf(userId) == -1;
+      callback(null, lastConnectionForUser);
+    });
+  });
+}
+
+function getNumberOfUsersInTroupe(troupeId, callback) {
+  if(!callback) callback = defaultRedisCallback;
+
+  redisClient.llen("troupe_users:" + troupeId, callback);
+}
+
+function removeTroupe(troupeId, callback) {
+  if(!callback) callback = defaultRedisCallback;
+
+  redisClient.del("troupe_users:" + troupeId, function(err) {
+    if(err) return winston.error("Redis error: " + err);
+  });
+
+  redisClient.srem("presence:activetroupes", troupeId, function(err, count) {
+    if(err) return winston.error("Redis error: " + err);
+  });
+
+}
+
+function removeActiveSocket(socketId, callback) {
+  if(!callback) callback = defaultRedisCallback;
+  redisClient.srem("presence:activesockets", socketId, callback);
+}
+
+
+resetClientState();
+
+module.exports = {
+  userSocketConnected: function(userId, socketId) {
+    addUserToActiveUsers(userId, function(err, initialConnection) {
+      if(initialConnection) {
+        winston.info("User " + userId + " connected.");
+      }
+    });
+
+    addSocketToUserSockets(userId, socketId);
+    addSocketToActiveSockets(socketId);
+  },
+
+  userSubscribedToTroupe: function(userId, troupeId, socketId) {
+    addTroupeToActiveTroupes(troupeId, function(err, initialConnection) {
+      if(initialConnection) {
+        winston.info("Troupe " + troupeId + " activated");
+      }
+    });
+
+    addUserToTroupe(userId, troupeId, function(err, initialJoin) {
+      if(initialJoin) {
+        /* User joining this troupe for the first time.... */
+        winston.info("User " + userId + " has just joined " + troupeId);
+      }
+    });
+
+    associateSocketToTroupe(socketId, troupeId);
+  },
+
+  userSocketDisconnected: function(userId, socketId) {
+    removeSocketFromUserSockets(socketId, userId, function(err, lastDisconnect) {
+      if(lastDisconnect) {
+        removeUserFromActiveUsers(userId);
+        winston.error("User " + userId + " is now offline");
+      }
+    });
+
+    getTroupeAssociatedToSocket(socketId, function(err, troupeId) {
+      disassociateSocketFromTroupe(socketId);
+
+      if(err) return winston.error("Redis error: " + err);
+      if(!troupeId) return; /* No associated with a troupe. Fuggitaboutit */
+
+      removeUserFromTroupe(userId, troupeId, function(err, lastConnectionForUserInTroupe) {
+        if(lastConnectionForUserInTroupe) {
+          winston.info("User " + userId + " is gone from " + troupeId);
+
+          getNumberOfUsersInTroupe(troupeId, function(err, count) {
+            if(err) return winston.error("Redis error: " + err);
+
+            if(count == 0) {
+              winston.info("The last user has disconnected from troupe " + troupeId);
+              removeTroupe(troupeId);
+            }
+
+          });
+        }
+      });
+    });
+
+    removeActiveSocket(socketId);
+    /* Manage user in troupe presence */
+    // Find out what troupe (if any) this socket is associated with
+  }
+
+
+};
