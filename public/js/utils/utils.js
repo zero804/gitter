1cadd7fc4343d07be975fc5c6e0892a6537d0deb
Andrew Newdigate <andrewn@datatribe.net>
2012-05-21 23:26:49 +0100

    Grouping of emails

---
+++ public/js/utils/utils.js
@@ -0,0 +1,29 @@
+define([
+  'underscore'
+], function(_) {
+
+  return {
+    /* Index an array into a hash */
+    index: function(array, transformer) {
+      var result = {};
+      for(var i = 0; i < array.length; i++) {
+        var item = array[i];
+        var key = transformer(item);
+        if(result[key]) {
+          result[key].push(item);
+        } else {
+          result[key] = [ item ];
+        }
+      }
+
+      return result;
+    },
+
+    /* Returns a date with no time part */
+    extractDateFromDateTime: function(dateTime) {
+      if(!dateTime) return null;
+      return new Date(dateTime.getFullYear(), dateTime.getMonth(), dateTime.getDate());
+    }
+  };
+
+});
