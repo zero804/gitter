e1a13b1fe20607cc9d4d35fb50c7f8b5141644df
Mike Bartlett <mike.bartlett@gmail.com>
2012-05-22 12:19:06 +0100

    mail item - not working, can't figure out the routing following similar pattern to the web router

--- public/js/m/views/mailItemView.js
+++ public/js/m/views/mailItemView.js
@@ -1,10 +1,34 @@
 define([
        'underscore',
-       'backbone'
+       'backbone',
+       'mustache',
+       'text!templates/mail/item.mustache'
 ], function(_, Backbone){
 
   var MailView = Backbone.View.extend({
     el: '#mailitem',
+
+    initialize: function(options) {
+      this.router = options.router;
+      var self = this;
+      $.ajax({
+        url: "/troupes/" + window.troupeContext.troupe.id + "/mails/" + this.id,
+        contentType: "application/json",
+        dataType: "json",
+        type: "GET",
+        success: function(data) {
+          self.mail = data;
+          self.renderMailItem();
+        }
+      });
+    },
+
+    renderMailItem: function() {
+      $('.label-subject',this.el).text(this.mail.subject);
+      $('.label-body',this.el).html(this.mail.mail);
+
+      this.generateAttachmentMenu();
+    },
 
     render: function() {
       return this;
