95107cf32edbaa0d61c5d4424da3a2c9e75c67c8
Mike Bartlett <mike.bartlett@gmail.com>
2012-06-25 15:45:53 +0100

    Added new CSS to build script

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 mkdir -p public/bootstrap/css/
 ./node_modules/less/bin/lessc -x public/bootstrap/less/troupe.less  > public/bootstrap/css/troupe.css 
 ./node_modules/less/bin/lessc -x public/bootstrap/less/trp.less  > public/bootstrap/css/trp.css 
+./node_modules/less/bin/lessc -x public/bootstrap/less/trp2.less  > public/bootstrap/css/trp2.css 
