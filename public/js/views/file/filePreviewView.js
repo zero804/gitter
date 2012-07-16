c85f5f6ec090cd05d077b8674ee263b975f94cf8
Andrew Newdigate <andrewn@datatribe.net>
2012-07-16 22:51:03 +0100

    Preview

---
+++ public/js/views/file/filePreviewView.js
@@ -0,0 +1,77 @@
+// Filename: views/home/main
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'views/base',
+  'hbs!./filePreviewView'
+], function($, _, Backbone, TroupeViews, template){
+  return TroupeViews.Base.extend({
+    template: template,
+
+    events: {
+      //"click .trpFileActionMenuButton": "showFileActionMenu",
+    },
+
+    initialize: function(options) {
+      //_.bindAll(this, 'onPreviewLinkClick', 'showFileActionMenu', 'hideFileActionMenu');
+    },
+
+    getRenderData: function() {
+      var item = this.model;
+      var previewMimeType = item.get('previewMimeType');
+      var mimeType = item.get('previewMimeType');
+
+      if(/^image\//.test(previewMimeType)) {
+        return {
+          href: item.get('embeddedUrl') + '?embedded=1',
+          photo: true
+        };
+      }
+
+      if(previewMimeType == 'application/pdf') {
+        return {
+          href: '/pdfjs/web/viewer.html?file=' + item.get('embeddedUrl'),
+          iframe: true,
+          width: "80%",
+          height: "80%"
+        };
+      }
+
+      if(/^image\//.test(mimeType)) {
+        return {
+          href: item.get('url') + '?embedded=1',
+          photo: true
+        };
+      }
+
+      if(mimeType == 'application/pdf') {
+        return {
+          width: "80%",
+          height: "80%",
+          href:  '/pdfjs/web/viewer.html?file=' + item.get('url') + "?embedded=1",
+          iframe: true
+        };
+      }
+
+      return {
+        noPreviewAvailable: true
+      };
+    },
+
+    render: function() {
+      var data = this.getRenderData();
+      var el;
+      if(data.photo) {
+        el = this.make("img", {"src": data.href} );
+      } else if(data.iframe) {
+        el = this.make("iframe", {"src": data.href, width: "500px", height: "500px"} );
+      }
+
+      this.$el.append(el);
+      return this;
+    }
+
+  });
+
+});
