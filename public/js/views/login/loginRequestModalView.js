6cc42e9dde6a5c9a5da4d14262d6c4da263c7b45
Mike Bartlett <mike.bartlett@gmail.com>
2012-07-18 16:31:17 +0100

    Request access - needs to be wired up and stuff

---
+++ public/js/views/login/loginRequestModalView.js
@@ -0,0 +1,29 @@
+// Filename: views/home/main
+define([
+  'jquery',
+  'underscore',
+  'views/base',
+  'hbs!./loginRequestModalView'
+], function($, _, TroupeViews, template) {
+  return TroupeViews.Base.extend({
+    template: template,
+
+    initialize: function(options) {
+      _.bindAll(this, 'onFormSubmit');
+    },
+
+    events: {
+      "submit form": "onFormSubmit",
+      "click #existing-user" : "showLoginForm"
+    },
+
+    showLoginForm: function(e) {
+      alert("Transition to login form");
+    },
+
+    onFormSubmit: function(e) {
+     
+    }
+  });
+
+});
