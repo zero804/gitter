7e6024fe30a62d6dc1b9a1b1b8815096a9e770d7
Andrew Newdigate <andrewn@datatribe.net>
2012-07-20 12:14:42 +0100

    CDN deployment

--- build.sh
+++ build.sh
@@ -2,13 +2,6 @@
 OLDTAG=`cat /var/log/troupe/cdn-version`
 NEWTAG=`git log -n 1 --pretty=format:%h`
 
-cd public
-s3cmd --config /etc/s3cfg cp --acl-public --recursive s3://troupe-cdn/$OLDTAG/ s3://troupe-cdn/$NEWTAG/
-sudo chown troupe:troupe /var/log/troupe/cdn-version
-sudo -u troupe sh -c 'git log -n 1 --pretty=format:%h > /var/log/troupe/cdn-version'
-s3cmd --config /etc/s3cfg sync --delete-removed --acl-public /opt/troupe/app/public/ s3://troupe-cdn/$NEWTAG/ --exclude "*.git*"
-cd ..
-
 mkdir -p public/bootstrap/css/
 ./node_modules/less/bin/lessc -x public/bootstrap/less/troupe.less  > public/bootstrap/css/troupe.css 
 ./node_modules/less/bin/lessc -x public/bootstrap/less/trp.less  > public/bootstrap/css/trp.css 
