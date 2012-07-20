337ab58a49ced6166a3002874edac63b8bbfe07d
Andrew Newdigate <andrewn@datatribe.net>
2012-07-20 12:32:11 +0100

    Remove old files from S3

--- deploy-cdn.sh
+++ deploy-cdn.sh
@@ -3,6 +3,12 @@ OLDTAG=`cat /var/log/troupe/cdn-version`
 NEWTAG=`git log -n 1 --pretty=format:%h`
 
 if [ "XX$OLDTAG" != "XX$NEWTAG" ]; then
+  if [ "XX$OLDTAG" != "XX$" ]; then
+    for i in `s3cmd ls s3://troupe-cdn|awk '{print $2}'|grep -v "$OLDTAG"`; do 
+      s3cmd rm $i
+    done
+  fi
+
   cd public
   s3cmd --config /etc/s3cfg cp --acl-public --recursive s3://troupe-cdn/$OLDTAG/ s3://troupe-cdn/$NEWTAG/
   echo $NEWTAG > /var/log/troupe/cdn-version
