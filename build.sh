0330150ef5568fa81e30edf5272d696aa7c0606a
Mike Bartlett <mike.bartlett@gmail.com>
2012-08-14 10:46:53 +0100

    Turned off uglify for debugging

--- build.sh
+++ build.sh
@@ -14,10 +14,10 @@ mkdir -p public-processed/bootstrap/css/
 ./node_modules/less/bin/lessc -x public-processed/bootstrap/less/trp.less  > public-processed/bootstrap/css/trp.css
 ./node_modules/less/bin/lessc -x public-processed/bootstrap/less/trp2.less  > public-processed/bootstrap/css/trp2.css
 
-while IFS= read -r -d $'\0' file; do 
-  echo Uglifying $file
-  ./node_modules/.bin/uglifyjs --overwrite -nc --lift-vars $file 
-done < <(find public-processed -name "*.js" ! -iname "*min* " -print0)
+# while IFS= read -r -d $'\0' file; do 
+#   echo Uglifying $file
+#   ./node_modules/.bin/uglifyjs --overwrite -nc --lift-vars $file 
+# done < <(find public-processed -name "*.js" ! -iname "*min* " -print0)
 
 while IFS= read -r -d $'\0' file; do 
   cp -p "$file" "${file}.gztmp"
