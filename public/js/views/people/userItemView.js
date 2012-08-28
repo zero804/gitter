e8493da0a8d05902245debd98ca0aa0dc5cc810f
Mike Bartlett <mike.bartlett@gmail.com>
2012-08-28 15:11:13 +0100

    Attempted to remove element when accepting a Request, without success - but left the code there for you to see.

--- public/js/views/people/userItemView.js
+++ public/js/views/people/userItemView.js
@@ -35,12 +35,16 @@ define([
 
     onAcceptClicked: function() {
       var that = this;
+      var thisPerson = this;
       $.ajax({
         url: "/troupes/" + window.troupeContext.troupe.id + "/requests/" + this.model.get('id'),
         dataType: "json",
         data: "",
         type: "PUT",
         success: function(data) {
+          // NOTHING HERE SEEMS TO GET CALLED?!
+          alert("Great Success!");
+          thisPerson.$el.remove();
           that.trigger('accept.complete', { userId: this.model.get('id') } );
         }
       });
