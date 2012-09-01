5fd1d32567a040f93eaa2ca3d26362e280528ebc
Andrew Newdigate <andrewn@datatribe.net>
2012-09-01 15:12:39 +0100

    https://app.asana.com/0/1089303904118/1089328393617 Sort Files

--- public/js/views/file/fileView.js
+++ public/js/views/file/fileView.js
@@ -11,6 +11,31 @@ define([
 ], function($, _, Backbone, TroupeViews, template, FileItemView, fileUploaderStub, fileModels) {
   "use strict";
 
+  function makeSort(sortField) {
+    return function(e) {
+      e.preventDefault();
+      this.sort(sortField);
+    };
+  }
+
+  function reverseComparatorFunction(comparatorFunction) {
+    return function(left, right) {
+      return -1 * comparatorFunction(left, right);
+    };
+  }
+
+  function sortByComparator(sortByFunction) {
+    return function(left, right) {
+      var l = sortByFunction(left);
+      var r = sortByFunction(right);
+
+      if (l === void 0) return 1;
+      if (r === void 0) return -1;
+
+      return l < r ? -1 : l > r ? 1 : 0;
+    };
+  }
+
   return TroupeViews.Base.extend({
     template: template,
     initialize: function(options) {
@@ -35,6 +60,48 @@ define([
       return {};
     },
 
+    events: {
+      "click .link-sort-filename": makeSort('fileName'),
+      "click .link-sort-mtime": makeSort('mtime'),
+      "click .link-sort-filetype": makeSort('mimeType'),
+      "click .link-sort-groupby": makeSort('mimeType') // TODO: do something else
+    },
+
+    sort: function(sortField) {
+      var reverse;
+      if(sortField == this.currentSortField) {
+        reverse = true;
+        this.currentSortField = "-" + sortField;
+      } else {
+        reverse = false;
+        this.currentSortField = sortField;
+      }
+
+      var comparator = sortByComparator(function(file) {
+        switch(sortField) {
+          case 'mtime':
+            var versions = file.get('versions');
+            if(!versions || !versions.length) return null;
+            var version = versions.at(versions.length - 1);
+            return version.createdDate;
+
+          case 'fileName':
+            var fileName = file.get('fileName');
+            return fileName ? fileName.toLowerCase() : '';
+
+          default:
+            return file.get(sortField);
+        }
+      });
+
+      if(reverse) {
+        comparator = reverseComparatorFunction(comparator);
+      }
+
+      this.collection.comparator = comparator;
+      this.collection.sort();
+    },
+
     afterRender: function() {
       this.itemView = new TroupeViews.Collection({
         itemView: FileItemView,
@@ -45,14 +112,12 @@ define([
 
     onCollectionReset: function() {
       // Probably not the best way to do this, want to show/hide frame-help if there are no files
-      console.log("Reset collection");
       $("#frame-help").show();
       $(".frame-files", this.el).empty();
       this.collection.each(this.onCollectionAdd);
     },
 
     onCollectionAdd: function(item) {
-      console.log("Collection add");
       $("#frame-help").hide();
       $(".frame-files", this.el).append(new FileItemView({ model: item }).render().el);
     },
