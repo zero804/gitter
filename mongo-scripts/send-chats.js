6254c7d4b7ad71a9b30f00d9709cee3ceeeffefd
Mike Bartlett <mike.bartlett@gmail.com>
2012-09-05 16:12:16 +0100

    some basic stats sent to ducks board

---
+++ mongo-scripts/send-chats.js
@@ -0,0 +1,39 @@
+var options = {
+	api_key : 'GAnM1FPIZjemnmUxIWEgNbjN8pQOHVvGCkXrBuGOUfltil3B9S'
+};
+
+//create ducksnode object
+var ducksnode = require ('../node_modules/ducksnode').create(options);
+var mongoose = require ('../node_modules/mongoose'),
+	Schema = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost/troupe');
+
+var ts = Math.round((new Date()).getTime() / 1000);
+
+var ChatMessageSchema = new Schema({
+  text: String,
+  sent: { type: Date, "default": Date.now }
+});
+
+var Chats = mongoose.model('chatmessages', ChatMessageSchema);
+
+mongoose.connection.on("open", function(){
+  console.log("Mongoose connected");
+  Chats.count({}, function( err, count){
+    ducksnode.push ('74815', {value: count, timestamp: ts}, function(err, response_status){
+      if (err){
+        console.error(err);
+      }
+      else{
+        console.log('OK! Sent number: ' + count + ' at ' + ts);
+        process.exit(code=0)
+      }
+    });
+  });
+});
+
+
+
+
+
---
+++ mongo-scripts/send-users.js
@@ -0,0 +1,44 @@
+var options = {
+	api_key : 'GAnM1FPIZjemnmUxIWEgNbjN8pQOHVvGCkXrBuGOUfltil3B9S'
+};
+
+//create ducksnode object
+var ducksnode = require ('../node_modules/ducksnode').create(options);
+var mongoose = require ('../node_modules/mongoose'),
+	Schema = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost/troupe');
+
+var ts = Math.round((new Date()).getTime() / 1000);
+
+var UserSchema = new Schema({
+  displayName: { type: String }, 
+  email: { type: String },
+  confirmationCode: {type: String },
+  status: { type: String, "enum": ['UNCONFIRMED', 'PROFILE_NOT_COMPLETED', 'ACTIVE'], "default": 'UNCONFIRMED'},
+  passwordHash: { type: String },
+  avatarUrlSmall: String,
+  avatarUrlMedium: String
+});
+
+var Users = mongoose.model('users', UserSchema);
+
+mongoose.connection.on("open", function(){
+  console.log("Mongoose connected");
+  Users.count({}, function( err, count){
+    ducksnode.push ('74811', {value: count, timestamp: ts}, function(err, response_status){
+      if (err){
+        console.error(err);
+      }
+      else{
+        console.log('OK! Sent number: ' + count + ' at ' + ts);
+        process.exit(code=0)
+      }
+    });
+  });
+});
+
+
+
+
+
