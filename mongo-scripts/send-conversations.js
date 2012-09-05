3a8deaa7737faa64afc65b27172db7d49e31f484
Mike Bartlett <mike.bartlett@gmail.com>
2012-09-05 16:12:16 +0100

    some basic stats sent to ducks board

---
+++ mongo-scripts/send-conversations.js
@@ -0,0 +1,38 @@
+var options = {
+	api_key : 'GAnM1FPIZjemnmUxIWEgNbjN8pQOHVvGCkXrBuGOUfltil3B9S'
+};
+
+//create ducksnode object
+var ducksnode = require ('../node_modules/ducksnode').create(options);
+var mongoose = require ('../node_modules/mongoose'),
+	Schema = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost/troupe');
+
+var ts = Math.round((new Date()).getTime() / 1000);
+
+var ConversationSchema = new Schema({
+  subject: { type: String }
+});
+
+var Conversations = mongoose.model('conversations', ConversationSchema);
+
+mongoose.connection.on("open", function(){
+  console.log("Mongoose connected");
+  Conversations.count({}, function( err, count){
+    ducksnode.push ('74816', {value: count, timestamp: ts}, function(err, response_status){
+      if (err){
+        console.error(err);
+      }
+      else{
+        console.log('OK! Sent number: ' + count + ' at ' + ts);
+        process.exit(code=0)
+      }
+    });
+  });
+});
+
+
+
+
+
---
+++ mongo-scripts/send-troupes.js
@@ -0,0 +1,40 @@
+var options = {
+	api_key : 'GAnM1FPIZjemnmUxIWEgNbjN8pQOHVvGCkXrBuGOUfltil3B9S'
+};
+
+//create ducksnode object
+var ducksnode = require ('../node_modules/ducksnode').create(options);
+var mongoose = require ('../node_modules/mongoose'),
+	Schema = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost/troupe');
+
+var ts = Math.round((new Date()).getTime() / 1000);
+
+var TroupeSchema = new Schema({
+  name: { type: String },
+  uri: { type: String },
+  status: { type: String, "enum": ['INACTIVE', 'ACTIVE'], "default": 'INACTIVE'}
+});
+
+var Troupes = mongoose.model('troupes', TroupeSchema);
+
+mongoose.connection.on("open", function(){
+  console.log("Mongoose connected");
+  Troupes.count({}, function( err, count){
+    ducksnode.push ('74814', {value: count, timestamp: ts}, function(err, response_status){
+      if (err){
+        console.error(err);
+      }
+      else{
+        console.log('OK! Sent number: ' + count + ' at ' + ts);
+        process.exit(code=0)
+      }
+    });
+  });
+});
+
+
+
+
+
