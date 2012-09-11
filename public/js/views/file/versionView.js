d2c0fde5a8bfe87e3139cb56f8a09b3bbc7ff47e
Mike Bartlett <mike.bartlett@gmail.com>
2012-09-11 12:23:16 +0100

    file versions styling - TODO need url per version

--- public/js/views/file/versionView.js
+++ public/js/views/file/versionView.js
@@ -4,8 +4,9 @@ define([
   'underscore',
   'backbone',
   'views/base',
+  'dateFormat',
   'hbs!./versionView'
-], function($, _, Backbone, TroupeViews, template){
+], function($, _, Backbone, TroupeViews, dateFormat, template){
   return TroupeViews.Base.extend({
     template: template,
 
@@ -19,6 +20,23 @@ define([
     },
 
     beforeClose: function() {
+    },
+
+    getRenderData: function() {
+      var data = this.model.toJSON();
+
+      // TODO Date.parse doesn't work in loads of browsers
+      data.createdDate = Date.parse(data.createdDate);
+      var d = new Date(data.createdDate);
+      data.createdDate = d.toUTCString();
+      var now = new Date();
+      if (now.getDate() === d.getDate() && now.getMonth() === d.getMonth() && now.getFullYear() === d.getFullYear()) {
+        data.createdDate = d.format('h:MM TT');
+      }
+      else {
+        data.createdDate = d.format('mmmm d');
+      }
+      return data;
     }
 
   });
