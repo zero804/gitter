b52b54483762be92e082b9277ef2cd831ba5c4f8
Mike Bartlett <mike.bartlett@gmail.com>
2012-10-04 14:19:43 +0100

    confirm modal when removing a user

---
+++ public/js/views/people/confirmRemoveModalView.js
@@ -0,0 +1,44 @@
+// Filename: views/home/main
+// TODO: Confirmation after invite sent
+
+define([
+  'jquery',
+  'underscore',
+  'views/base',
+  'hbs!./confirmRemoveModalView'
+], function($, _, TroupeViews, template) {
+
+    return TroupeViews.Base.extend({
+    template: template,
+
+    initialize: function(options) {
+      var thisModel = options.model;
+      this.displayName = thisModel.get('displayName');
+    },
+
+    getRenderData: function() {
+      return {
+        displayName: this.displayName
+      };
+    },
+
+    events: {
+      "click #button-yes": "yesClicked",
+      "click #button-no" : "noClicked"
+    },
+
+    yesClicked: function() {
+      this.trigger('confirm.yes');
+    },
+
+    noClicked: function() {
+      this.trigger('confirm.no');
+    },
+
+    afterRender: function(e) {
+
+    }
+
+  });
+
+});
