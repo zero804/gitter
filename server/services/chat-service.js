8ac4ca7e2cb13381848c878e7c5067e73e3f3984
Andrew Newdigate <andrewn@datatribe.net>
2012-11-06 10:28:05 +0000

    Fixed chat bug

--- server/services/chat-service.js
+++ server/services/chat-service.js
@@ -4,7 +4,7 @@
 
 var persistence = require("./persistence-service"),
     troupeService = require("./troupe-service"),
-    mongoose = require("mongoose"),
+    restSerializer = require("../serializers/rest-serializer"),
     appEvents = require("../app-events"); // TODO: decouple service from socket
 
 module.exports = {
@@ -12,19 +12,23 @@ module.exports = {
     troupeService.findById(troupeId, function(err, troupe) {
       if(err) return callback(err);
       if(!troupe) return callback();
-      
+
       if(!troupeService.userHasAccessToTroupe(user, troupe)) return callback("Access denied");
-   
+
       var chatMessage = new persistence.ChatMessage();
       chatMessage.fromUserId = user.id;
       chatMessage.toTroupeId = troupe.id;
       chatMessage.text = text;
-      
       chatMessage.save(function (err) {
         if(err) return callback(err);
-        
-        appEvents.troupeChat(troupe.id, chatMessage.narrow(user, troupe));
-        
+
+        var strategy = new restSerializer.ChatStrategy({ user: user, troupeId: troupe.id });
+        restSerializer.serialize(chatMessage, strategy, function(err, serialized) {
+          if(err) return callback(err);
+
+          appEvents.troupeChat(troupe.id, serialized);
+        });
+
         return callback(null, chatMessage);
       });
       
