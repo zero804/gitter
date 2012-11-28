97996c74c580f2978fb627fa26a1ca4df26cf9d8
Mike Bartlett <mike.bartlett@gmail.com>
2012-11-28 14:43:34 +0000

    build script for more less files

--- build.sh
+++ build.sh
@@ -15,7 +15,10 @@ mkdir -p public-processed/bootstrap/css/
 ./node_modules/less/bin/lessc -x public-processed/bootstrap/less/trp2.less  > public-processed/bootstrap/css/trp2.css
 ./node_modules/less/bin/lessc -x public-processed/bootstrap/less/mtrp.less  > public-processed/bootstrap/css/mtrp.css
 ./node_modules/less/bin/lessc -x public-processed/bootstrap/less/trpHomePage.less  > public-processed/bootstrap/css/trpHomePage.css
-
+./node_modules/less/bin/lessc -x public-processed/bootstrap/less/trpChat.less  > public-processed/bootstrap/css/trpChat.css
+./node_modules/less/bin/lessc -x public-processed/bootstrap/less/trpFiles.less  > public-processed/bootstrap/css/trpFiles.css
+./node_modules/less/bin/lessc -x public-processed/bootstrap/less/trpMails.less  > public-processed/bootstrap/css/trpMails.css
+./node_modules/less/bin/lessc -x public-processed/bootstrap/less/trpPeople.less  > public-processed/bootstrap/css/trpPeople.css
 # while IFS= read -r -d $'\0' file; do 
 #   echo Uglifying $file
 #   ./node_modules/.bin/uglifyjs --overwrite -nc --lift-vars $file 
