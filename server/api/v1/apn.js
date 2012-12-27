625a269a71643ad5bee3601cb2c7978862d14610
Andrew Newdigate <andrewn@datatribe.net>
2012-12-27 08:55:57 +0000

    Additional debugging information

--- server/api/v1/apn.js
+++ server/api/v1/apn.js
@@ -6,10 +6,9 @@ var pushNotificationService = require('../../services/push-notification-service'
 
 module.exports = {
   create: function(req, res, next) {
-    winston.info("APN post");
-    console.dir(req.body);
+    winston.info("APN device registration", { deviceId: req.body.deviceId, deviceName: req.body.deviceName });
 
-    pushNotificationService.registerAppleDevice(req.body.deviceId, new Buffer(req.body.deviceToken, 'base64'), function(err) {
+    pushNotificationService.registerAppleDevice(req.body.deviceId, req.body.deviceName, new Buffer(req.body.deviceToken, 'base64'), function(err) {
       if(err) return next(err);
       res.send({ success: true });
     });
