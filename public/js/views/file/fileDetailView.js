1ce2f9e119b31516ef08e795e536494c02a9bd8b
Andrew Newdigate <andrewn@datatribe.net>
2013-01-04 11:26:55 +0000

    File detail

--- public/js/views/file/fileDetailView.js
+++ public/js/views/file/fileDetailView.js
@@ -4,7 +4,7 @@ define([
   'underscore',
   'backbone',
   'views/base',
-  'hbs!./fileVersionsView',
+  'hbs!./fileDetailView',
   './versionView'
 ], function($, _, Backbone, TroupeViews, template, VersionView){
   return TroupeViews.Base.extend({
@@ -16,18 +16,11 @@ define([
     initialize: function(options) {
     },
 
-    afterRender: function() {
-      new TroupeViews.Collection({
-         collection: this.model.get('versions'),
-         itemView: VersionView,
-         el: this.$el.find('.frame-versions')
-      }).render();
-    },
-
     getRenderData: function () {
       var d = this.model.toJSON();
       var latestVersion = this.model.get('versions').length - 1;
       d.fileIcon = '/troupes/' + window.troupeContext.troupe.id + '/thumbnails/' + d.fileName + "?version=" + latestVersion;
+      d.previewUrl = '#file/preview/' + d.id;
       return d;
     }
 
