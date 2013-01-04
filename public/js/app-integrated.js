ba0f0c93a61cd990ecc123e969a921186a03d0d6
Andrew Newdigate <andrewn@datatribe.net>
2013-01-04 11:26:55 +0000

    File detail

--- public/js/app-integrated.js
+++ public/js/app-integrated.js
@@ -3,14 +3,17 @@ require([
   'underscore',
   'backbone',
   'marionette',
+  'views/base',
   'views/app/appIntegratedView',
   'views/chat/chatView',
   'views/file/fileView',
   'views/conversation/conversationView',
   'utils/vent',
   'collections/files',
-  'views/file/fileDetailView'
-], function($, _, Backbone, Marionette, AppIntegratedView, ChatView, FileView, ConversationView, vent, fileModels, FileDetailView) {
+  'views/file/fileDetailView',
+  'views/file/filePreviewView',
+  'views/file/fileVersionsView'
+], function($, _, Backbone, Marionette, TroupeViews, AppIntegratedView, ChatView, FileView, ConversationView, vent, fileModels, FileDetailView, FilePreviewView, FileVersionsView) {
   /*jslint browser: true*/
   /*global require console */
   "use strict";
@@ -34,20 +37,73 @@ require([
     },
 
     showFileDetail: function(id) {
-      function showDetail() {
-        var view = new FileDetailView({
-          model: fileCollection.get(id)
-        });
-        app.rightPanelRegion.show(view);
-        vent.trigger("detailView:show");
+      // Do we need to wait for file collection to load?
+      if(fileCollection.length === 0) {
+        fileCollection.on('reset', this.showFileDetail, this);
+        return;
+      } else {
+        // TODO: use 'once' instead of on and off when we upgrade
+        fileCollection.off('reset', this.showFileDetail, this);
       }
 
-      if(fileCollection.length > 0) {
-        showDetail();
+      var view = new FileDetailView({
+        model: fileCollection.get(id)
+      });
+      app.rightPanelRegion.show(view);
+      vent.trigger("detailView:show");
+    },
+
+    showFilePreview: function(id) {
+      // Do we need to wait for file collection to load?
+      if(fileCollection.length === 0) {
+        fileCollection.on('reset', this.showFilePreview, this);
+        return;
       } else {
-        var callback = function() { fileCollection.off('reset', callback, this); showDetail(); };
-        fileCollection.on('reset', callback, this);
+        // TODO: use 'once' instead of on and off when we upgrade
+        fileCollection.off('reset', this.showFilePreview, this);
       }
+
+      var self = this;
+      var currentModel = fileCollection.get(id);
+
+      var navigationController = {
+        hasNext: function() {
+          var i = fileCollection.indexOf(currentModel);
+          return i < fileCollection.length - 1;
+        },
+
+        getNext: function() {
+          var i = fileCollection.indexOf(currentModel);
+          if(i < fileCollection.length - 1) {
+            currentModel = fileCollection.at(i + 1);
+            return currentModel;
+          }
+          return null;
+        },
+
+        hasPrevious: function() {
+          var i = fileCollection.indexOf(currentModel);
+          return i > 0;
+        },
+
+        getPrevious: function() {
+          var i = fileCollection.indexOf(currentModel);
+          if(i > 0) {
+            currentModel = fileCollection.at(i - 1);
+            return currentModel;
+          }
+          return null;
+        }
+      };
+
+      var view = new FilePreviewView({ model: currentModel, navigationController: navigationController });
+      var modal = new TroupeViews.Modal({ view: view, className: 'modal trpFilePreview', menuItems: [
+      {
+        id: "download",
+        text: "Download"
+      }
+      ]});
+      modal.show();
     }
   });
 
@@ -55,7 +111,8 @@ require([
 
   var Router = Marionette.AppRouter.extend({
     appRoutes: {
-      "file/:id": "showFileDetail"
+      "file/:id":         "showFileDetail",
+      "file/preview/:id": "showFilePreview"
     }
   });
 
