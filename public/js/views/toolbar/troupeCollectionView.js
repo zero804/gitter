a5797947ec7016f300e1d2367cfbd05797f18d0f
Andrew Newdigate <andrewn@datatribe.net>
2013-01-10 13:30:12 +0000

    Troupe list in

---
+++ public/js/views/toolbar/troupeCollectionView.js
@@ -0,0 +1,23 @@
+// Filename: views/home/main
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'marionette',
+  'views/base',
+  'hbs!./troupeListItem'
+], function($, _, Backbone, Marionette, TroupeViews, troupeListItemTemplate) {
+  /*jslint browser: true*/
+  /*global require */
+  "use strict";
+
+  var TroupeItemView = Backbone.Marionette.ItemView.extend({
+    tagName: 'li',
+    template: troupeListItemTemplate
+  });
+
+  return Backbone.Marionette.CollectionView.extend({
+    itemView: TroupeItemView
+  });
+
+});
