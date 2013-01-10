6401638f18986d19b31600182fb0d6e5bdb0cc40
Mike Bartlett <mike.bartlett@gmail.com>
2013-01-10 14:18:36 +0000

    improvements to the way the UI slides around

---
+++ public/js/views/app/UIVars.js
@@ -0,0 +1,10 @@
+define([], function() {
+	return {
+		panelWidthValue: '280px', 
+		menuSlideValue: '320px',
+		hidePanelValue: '-280px',
+		shiftedPanelValue: '-200px',
+		shiftedMarginValue: '200px'
+	};
+}
+);
