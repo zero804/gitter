451205276a78a98a5b934de9bc461f31107830ab
Tim Lind <tim@troupe.co>
2013-02-15 12:07:33 +0200

    Create troupe view uses the troupe collection to save the new troupe (when available).
    Fixes so that the create view works from within app integrated and from outside of it (when someone has no troupes they get sent to the signup page which will only then use this view).
    
    Included validation for the invite fields.
    
    References #71 References #81

--- public/js/views/signup/createTroupeView.js
+++ public/js/views/signup/createTroupeView.js
@@ -16,15 +16,13 @@ define([
       _.bindAll(this, 'onFormSubmit');
       if (!options) return;
       this.existingUser = options.existingUser;
+      this.shareTableView = new ShareTableView();
 
-      /*var invitations = null, troupe = window.troupeContext.troupe;
-      if (window.troupeContext.troupe.oneToOne) {
-        invitations = [
-          { displayName: troupe.user.displayName, inviteEmail: troupe.user.email },
-          {} // an empty input row
-        ];
-      }*/
-      this.shareTableView = new ShareTableView(/*{invitations: invitations}*/);
+      if (window.troupeContext) {
+        this.isOneToOne = window.troupeContext.troupe.oneToOne;
+      } else {
+        this.isOneToOne = false;
+      }
     },
 
     events: {
@@ -32,24 +30,22 @@ define([
     },
 
     getRenderData: function() {
-      var isOneToOne = window.troupeContext.troupe.oneToOne;
+      var data = {
+        existingUser: this.existingUser,
+        isOneToOne: this.isOneToOne
+      };
+
       if (window.troupeContext || window.userId) {
-        userId = (window.userId) ? window.userId : window.troupeContext.user.id;
-        return {
-          existingUser: this.existingUser,
-          userId: userId,
-          isOneToOne: isOneToOne
-        };
-      } else {
-        return {
-          existingUser: this.existingUser,
-          isOneToOne: isOneToOne
-        };
-     }
+        data.userId = window.userId;
+      }
+
+      return data;
     },
 
-    afterRender : function() {
-      if (window.troupeContext.troupe.oneToOne) {
+    afterRender: function() {
+      // only show the invites fields if we are on a 1-1 troupe (TODO show it always, but first fix the way the add button works)
+      var tx = window.troupeContext;
+      if (tx && tx.troupe.oneToOne) {
         this.$el.find('#invites-for-create').append(this.shareTableView.el);
       }
 
@@ -59,56 +55,53 @@ define([
     },
 
     validateForm : function () {
-      var validateEl = this.$el.find('#signup-form');
-      validateEl.validate({
-        debug: true,
-        showErrors: function(errorMap, errorList) {
-          if (errorList.length > 0) $('.signup-failure').show();
-          var errors = "";
-          $.each(errorList, function () { errors += this.message + "<br>"; });
-          $('#failure-text').html(errors);
-        },
-        messages: {
-          troupeName: {
-            minlength: "Please choose a longer name for your Troupe, it needs to be at least 4 letters.",
-            required: "Please choose a name for your Troupe. "
-          },
-        email : {
-          required: "We need to know your email address",
-          email: "Hmmm, that doesn't look like your email address."
-          }
-        }
-      });
+      var validationConfig = _.extend(this.shareTableView.getValidationConfig());
+
+      validationConfig.showErrors = function(errorMap, errorList) {
+        if (errorList.length > 0) $('.signup-failure').show();
+        var errors = "";
+        $.each(errorList, function () { errors += this.message + "<br>"; });
+        $('#failure-text').html(errors);
+      };
+
+      validationConfig.messages.troupeName = {
+        minlength: "Please choose a longer name for your Troupe, it needs to be at least 4 letters.",
+        required: "Please choose a name for your Troupe. "
+      };
+
+      validationConfig.messages.email = {
+        required: "We need to know your email address",
+        email: "Hmmm, that doesn't look like your email address."
+      };
+
+      this.$el.find('#signup-form').validate(validationConfig);
     },
 
     onFormSubmit: function(e) {
       if(e) e.preventDefault();
       var that = this, form = this.$el.find('form'), serializedForm;
 
-      if (window.troupeContext.troupe.oneToOne) {
+      // we are sometimes executing from the signup page which excludes all the app integrated goodness
+      if (this.collection || window.troupeContext) {
         serializedForm = {
-          name: this.$el.find('form input[name=name]').val(),
-          oneToOneTroupeId: window.troupeContext.troupe.id,
+          name: form.find('form input[name=troupeName]').val(),
+          userId: form.find('form input[name=userId]').val(),
           invites: this.shareTableView.serialize()
         };
 
-        $.ajax({
-          url: "/troupes",
-          type: "PUT",
-          contentType: "json",
-          dataType: "json",
-          data: serializedForm,
-          success: function(troupe) {
-            console.log('response from upgrading one to one troupe', troupe);
-            if (troupe.uri) {
-              window.location.href = "/" + troupe.uri + "#|shareTroupe";
-            } else {
-              that.trigger('signup.complete', troupe);
-            }
+        if (window.troupe.oneToOne) {
+          serializedForm.oneToOneTroupeId = window.troupeContext.troupe.id;
+        }
 
+        that.collection.create(serializedForm, {
+          wait: true,
+          success: function(troupe /*, resp, options*/) {
+            console.log('response from upgrading one to one troupe', troupe);
+            window.location.href = "/" + troupe.get('uri') + "#|shareTroupe";
           }
         });
       }
+      // we are operating from the signup page, without app integrated, so we don't have a collection
       else {
         serializedForm = form.serialize();
         console.log("Serialized form: " + serializedForm);
@@ -120,12 +113,8 @@ define([
           type: "POST",
           success: function(data) {
             if (data.redirectTo) {
-              console.log(JSON.stringify(data));
               window.location.href = "/" + data.redirectTo + "#|shareTroupe";
             }
-            else {
-               that.trigger('signup.complete', data);
-             }
           }
         });
       }
@@ -134,9 +123,9 @@ define([
   });
 
 var Modal = TroupeViews.Modal.extend({
-    initialize: function(/* options */) {
+    initialize: function(options) {
       TroupeViews.Modal.prototype.initialize.apply(this, arguments);
-      this.view = new View({ });
+      this.view = new View(options);
     }
   });
 
