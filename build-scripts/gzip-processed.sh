b2fe2bf79a5bc7a42a9df749ca279ae1a2f08d2e
Andrew Newdigate <andrew@troupe.co>
2013-02-20 14:44:35 +0200

    Productionisation. nginx gzip encoded content should not be working a treat. Also uses a custom build of nginx.

--- build-scripts/gzip-processed.sh
+++ build-scripts/gzip-processed.sh
@@ -4,4 +4,4 @@ while IFS= read -r -d $'\0' file; do
   cp -p "$file" "${file}.gztmp"
   gzip --best "$file"
   mv "${file}.gztmp" "$file"
-done < <(find public-processed -type f ! -iname ".*" ! -iname "*.gz" -print0)
+done < <(find public-processed -type f ! -iname ".*" ! -iname "*.gz" ! -iname "*.png" ! -iname "*.jpg" ! -iname "*.gif"  -print0)
