5cace7a9882e3ec33b9a66695d2891f57ea0599f
Tim Lind <tim@troupe.co>
2013-02-25 12:49:47 +0200

    included the home url nconf in the mac-app page rendering. closes #200

--- server/handlers/mac-app.js
+++ server/handlers/mac-app.js
@@ -1,13 +1,14 @@
 /*jshint globalstrict:true, trailing:false unused:true node:true*/
 /*global console:false, require: true, module: true */
 "use strict";
+var nconf = require('nconf');
 
 module.exports = {
     install: function(app) {
       app.get(
         '/mac-app',
         function(req, res) {
-          res.render('mac-app');
+          res.render('mac-app', { homeUrl: nconf.get('web:homeurl') });
         }
       );
     }
