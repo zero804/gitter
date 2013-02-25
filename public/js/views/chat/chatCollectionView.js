43178403fc4e975069f3126b0714e1056a1fced5
Mike Bartlett <mike.bartlett@gmail.com>
2013-02-25 15:38:32 +0200

    fix for making sure the document goes to the bottom of the chat

--- public/js/views/chat/chatCollectionView.js
+++ public/js/views/chat/chatCollectionView.js
@@ -76,9 +76,9 @@ define([
       // console.log("scrollOf scroll: " + $(this.scrollOf).scrollTop() + " container height: " + $(this.container).height());
       // this is an ugly hack to deal with some weird timing issues
       var self = this;
-      setTimeout(function() {
+      $(function() {
       $(self.scrollOf).scrollTop($(self.container).height());
-      }, 150);
+      });
     },
 
     onAfterItemAdded: function() {
