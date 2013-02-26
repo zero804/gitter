9d684a92d9acf2c168239069ae584e9de45537cb
Andrew Newdigate <andrew@troupe.co>
2013-02-26 12:38:46 +0200

    Shutdown handlers

--- server/utils/redis.js
+++ server/utils/redis.js
@@ -1,12 +1,40 @@
-/*jshint node:true */
+/*jshint node:true unused:true */
 "use strict";
 
+var Q = require('q');
 var nconf = require("./config");
 var redis = require("redis");
-
+var shutdown = require('./shutdown');
 var host = nconf.get("redis:host");
 var port = nconf.get("redis:port");
+var clients = [];
+
+shutdown.addHandler('redis', 1, function(callback) {
+  var promises = [];
+
+  clients.forEach(function(client, index) {
+    var d = Q.defer();
+    promises.push(d.promise);
+
+    console.log("Calling quit on " + index);
+    client.quit(function() {
+      console.log("Completed quit on " + index);
+      d.resolve();
+    });
+  });
+
+  Q.all(promises).then(function() {
+    console.log("Clients have all quit");
+    callback();
+  }).fail(function(err) {
+    console.log("Quit failed", err);
+    callback(err);
+  });
+});
+
 
 exports.createClient = function createClient() {
-  return redis.createClient(port, host);
+  var client = redis.createClient(port, host);
+  clients.push(client);
+  return client;
 };
