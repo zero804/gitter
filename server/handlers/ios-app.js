d171baaca76bf28cd59e184727650727ad6fea69
Mike Bartlett <mike.bartlett@gmail.com>
2013-02-26 12:12:45 +0200

    coming soon on mobile troupe front page. Closes #203

--- server/handlers/ios-app.js
+++ server/handlers/ios-app.js
@@ -1,13 +1,14 @@
 /*jshint globalstrict:true, trailing:false unused:true node:true*/
 /*global console:false, require: true, module: true */
 "use strict";
+var nconf = require('nconf');
 
 module.exports = {
     install: function(app) {
       app.get(
         '/ios-app',
         function(req, res) {
-          res.render('ios-app');
+          res.render('ios-app', { homeUrl: nconf.get('web:homeurl') });
         }
       );
     }
