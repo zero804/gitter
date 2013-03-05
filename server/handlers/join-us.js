ce9634318c90a3e5f97572b0cf65911d1b621265
Mike Bartlett <mike.bartlett@gmail.com>
2013-03-05 17:59:27 +0200

    join us page. WIP.

---
+++ server/handlers/join-us.js
@@ -0,0 +1,15 @@
+/*jshint globalstrict: true, trailing: false, unused: true, node: true */
+"use strict";
+
+var nconf = require('nconf');
+
+module.exports = {
+    install: function(app) {
+      app.get(
+        '/join-us',
+        function(req, res) {
+          res.render('join-us', { homeUrl: nconf.get('web:homeurl') });
+        }
+      );
+    }
+};
