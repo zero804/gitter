32285bdb552da76b2396da828990174399e190a1
Andrew Newdigate <andrew@troupe.co>
2013-03-07 21:09:20 +0200

    Closes #265

--- server/handlers/health-check.js
+++ server/handlers/health-check.js
@@ -1,12 +1,15 @@
 /*jshint globalstrict: true, trailing: false, unused: true, node: true */
 "use strict";
 
+var os = require("os");
+var nconf = require('../utils/config');
+
 //TODO: remove
 module.exports = {
     install: function(app) {
       app.get('/health-check',
         function(req, res) {
-          res.send("OK");
+          res.send("OK from " + os.hostname() + ":" + nconf.get('PORT'));
         });
     }
 };
