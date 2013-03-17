8324d8e88750f6c3fca317d1b33b5453244437c6
Andrew Newdigate <andrew@troupe.co>
2013-03-17 22:36:34 +0000

    CasperJS tests: continued work

---
+++ test/end-to-end/casper/inc.js
@@ -0,0 +1,17 @@
+var baseUrl = casper.cli.get('url') || "http://localhost:5000/";
+casper.baseUrl = baseUrl;
+
+casper.onError = function() {
+  test.fail('Error');
+};
+
+casper.on('error', function(e) {
+    casper.echo('Fail!' + e);
+});
+
+
+
+casper.on('http.status.500', function(resource) {
+  casper.test.fail('500 on ' + resource.url);
+});
+
