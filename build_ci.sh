dcbbc097084b4656a89586c2df5bb4b564809d7d
Andrew Newdigate <andrew@troupe.co>
2013-03-22 13:09:33 +0000

    Using make more

--- build_ci.sh
+++ build_ci.sh
@@ -2,22 +2,6 @@
 set -e
 shopt -s extglob
 
-echo build.sh executing as user `whoami`
-npm prune
-npm install
+echo In future just run `make continuous-integration`
 
-grunt -no-color process
-
-echo $GIT_COMMIT > GIT_COMMIT
-echo $GIT_BRANCH > VERSION
-
-./scripts/upgrade-data.sh
-
-make test
-
-if [ -d output ]; then rm -r output; fi
-
-mkdir -p output
-
-tar -cv !(@(node_modules|output|assets|mongo-backup-*))|gzip -9 - > output/troupe.tgz
-
+make continuous-integration
