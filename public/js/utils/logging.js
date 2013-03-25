589f78058ff8e30240c9a2456237d5266d853eac
Andrew Newdigate <andrew@troupe.co>
2013-03-25 13:28:42 +0000

    console.log for IE. Closes #290 and #330

--- public/js/utils/logging.js
+++ public/js/utils/logging.js
@@ -1,32 +1,15 @@
 /*jshint unused:true, browser:true*/
 define([
-  'underscore'
-], function(_) {
+], function() {
   "use strict";
-
-  if(console && console.error && (typeof console.error == "function")) {
-    return {
-      getLogger: function() {
-        return {
-          error: _.bind(console.error, console),
-          warn: _.bind(console.warn, console),
-          info: _.bind(console.info, console),
-          debug: _.bind(console.debug, console)
-        };
-      }
-    };
-  }
-
-  // No logger exists
-  function noOp() {}
 
   return {
     getLogger: function() {
       return {
-        error: noOp,
-        warn: noOp,
-        info: noOp,
-        debug: noOp
+        error: console.error,
+        warn: console.warn,
+        info: console.info,
+        debug: console.debug
       };
     }
   };
