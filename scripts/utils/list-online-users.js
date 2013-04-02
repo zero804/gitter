cdd038f41c24070d324e987a87f7a5afdd054fd7
Andrew Newdigate <andrew@troupe.co>
2013-04-02 14:37:03 +0200

    More presence GC. References #377

--- scripts/utils/list-online-users.js
+++ scripts/utils/list-online-users.js
@@ -7,32 +7,31 @@ var userService = require('../../server/services/user-service');
 var shutdown = require('../../server/utils/shutdown');
 
 var winston = require('../../server/utils/winston');
-/*
+
 var opts = require("nomnom")
-   .option('user', {
-      abbr: 'u',
-      list: true,
-      required: true,
-      help: 'Send message to userId'
-   })
-   .option('message', {
-      abbr: 'm',
-      required: true,
-      help: 'Message to send'
-   })
-   .option('sound', {
-      abbr: 's',
-      help: 'Sound to send'
+   .option('name', {
+      abbr: 'n',
+      flag: true,
+      help: 'Display names'
    })
    .parse();
-*/
 
 presenceService.listOnlineUsers(function(err, userIds) {
-   userService.findByIds(userIds, function(err, users) {
-      users.forEach(function(user) {
-         console.log(user.displayName);
+   if(opts.name) {
+      userService.findByIds(userIds, function(err, users) {
+         users.forEach(function(user) {
+            console.log(user.displayName);
+         });
+
+         shutdown.shutdownGracefully();
       });
 
-      shutdown.shutdownGracefully();
+      return;
+   }
+
+   userIds.forEach(function(userId) {
+      console.log(userId);
    });
+   shutdown.shutdownGracefully();
+
 });
