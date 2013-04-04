60c141f91dd4cc3775870881036e4a2c66772f90
Andrew Newdigate <andrew@troupe.co>
2013-04-04 22:11:43 +0200

    Push notifications for production and dev concurrently

--- server/api/v1/apn.js
+++ server/api/v1/apn.js
@@ -8,10 +8,16 @@ module.exports = {
   create: function(req, res, next) {
     var deviceId = req.body.deviceId;
     var deviceName = req.body.deviceName;
+    var deviceType = req.body.deviceType;
     var deviceToken = new Buffer(req.body.deviceToken, 'base64');
 
+    // Backwards compatiblity, remove
+    if(!deviceType) {
+      deviceType = 'APPLE-DEV';
+    }
+
     winston.info("APN device registration", { deviceId: deviceId, deviceName: deviceName });
-    pushNotificationService.registerDevice(deviceId, 'APPLE', deviceToken, deviceName, function(err) {
+    pushNotificationService.registerDevice(deviceId, deviceType, deviceToken, deviceName, function(err) {
       if(err) return next(err);
       res.send({ success: true });
     });
