d8cccccc5fafd2b7bd54826ec42c298da42a20f6
Andrew Newdigate <andrew@troupe.co>
2013-04-09 18:27:13 +0200

    Better tests for signup

---
+++ test/integration/services/signup-service-test.js
@@ -0,0 +1,114 @@
+/*jslint node:true, unused:true*/
+/*global describe:true, it:true*/
+"use strict";
+
+var testRequire = require('../test-require');
+
+var assert = require("assert");
+var persistence = testRequire("./services/persistence-service");
+var sinon = require("sinon");
+
+describe('signup-service', function() {
+
+  describe('#newSignup()', function() {
+
+    it('should create a new user and new troupe, allow the user to confirm', function(done) {
+      var nonExistingEmail = 'testuser' + Date.now() + '@troupetest.local';
+      var troupeName = 'Test Troupe ' + new Date();
+
+      var emailNotificationService = testRequire('./services/email-notification-service');
+
+      var emailNotificationServiceMock = sinon.mock(emailNotificationService);
+      var signupService = testRequire.withProxies("./services/signup-service", {
+        './services/email-notification-service': emailNotificationService
+      });
+
+      emailNotificationServiceMock.expects('sendConfirmationForNewUser').once();
+
+      signupService.newSignup({
+        email: nonExistingEmail,
+        troupeName: troupeName
+      }, function(err, troupeId) {
+        if(err) return done(err);
+
+        emailNotificationServiceMock.verify();
+
+        persistence.User.findOne({ email: nonExistingEmail }, function(err, user) {
+          if(err) return done(err);
+          assert(user, 'Expected new user to be created');
+          assert(user.confirmationCode, 'Expected a confirmation code for a new user');
+          assert(user.status === 'UNCONFIRMED', 'Expected the user to be unconfirmed');
+
+          persistence.Troupe.findById(troupeId, function(err, troupe) {
+            if(err) return done(err);
+
+            assert(troupe, 'Expected a new troupe to have been created');
+            assert(troupe.users.length == 1, 'Expected the new troupe to have a single user');
+            assert(troupe.users[0].userId == user.id, 'Expected the new troupe to have a reference to the new user');
+
+            signupService.confirm(user, function(err, user, troupe) {
+              assert(user, 'Expected user to be created');
+              assert(user.confirmationCode, 'Expected the user to still have a confirmation code');
+              assert(user.status === 'PROFILE_NOT_COMPLETED', 'Expected the user to be PROFILE_NOT_COMPLETED');
+
+              assert(troupe, 'Expected a troupe');
+
+              emailNotificationServiceMock.verify();
+
+              done();
+
+            });
+
+          });
+        });
+      });
+    });
+
+    it('should create a new user and new troupe, and allow a user to resend the confirmation email', function(done) {
+      var nonExistingEmail = 'testuser' + Date.now() + '@troupetest.local';
+      var troupeName = 'Test Troupe ' + new Date();
+
+      var emailNotificationService = testRequire('./services/email-notification-service');
+
+      var emailNotificationServiceMock = sinon.mock(emailNotificationService);
+
+      var signupService = testRequire.withProxies("./services/signup-service", {
+        './services/email-notification-service': emailNotificationService
+      });
+
+      signupService.newSignup({
+        email: nonExistingEmail,
+        troupeName: troupeName
+      }, function(err, troupeId) {
+        if(err) return done(err);
+
+        emailNotificationServiceMock.expects('sendConfirmationForNewUser').once();
+
+        signupService.resendConfirmation({ email: nonExistingEmail }, function(err, troupeId2) {
+          if(err) return done(err);
+          assert(troupeId === troupeId2, 'TroupeID should match');
+
+          emailNotificationServiceMock.verify();
+
+          emailNotificationServiceMock = sinon.mock(emailNotificationService);
+
+          emailNotificationServiceMock.expects('sendConfirmationForNewUser').once();
+
+          signupService.resendConfirmation({ troupeId: troupeId }, function(err, troupeId3) {
+            if(err) return done(err);
+            assert(troupeId === troupeId3, 'TroupeIDs should match');
+
+            emailNotificationServiceMock.verify();
+
+            done();
+          });
+
+        });
+      });
+    });
+
+
+
+  });
+
+});
--- test/integration/test-require.js
+++ test/integration/test-require.js
@@ -2,8 +2,22 @@
 /*global describe:true, it: true, beforeEach:true, afterEach:true */
 "use strict";
 
+var proxyquireNoCallThru = require("proxyquire").noCallThru();
+
+function resolveModuleName(module) {
+  if(module.indexOf('.') !== 0 && module.indexOf('/') !== 0) {
+    return module;
+  }
+
+  if(process.env['TROUPE_COVERAGE']) {
+    return __dirname + '/../../coverage/' + module;
+  }
+
+  return __dirname + '/../../server/' + module;
+
+}
 // when using this require function, the module path should be relative to the server directory
-module.exports = function(module) {
+var testRequire = module.exports = function(module) {
   if(module.indexOf('.') !== 0 && module.indexOf('/') !== 0) {
     return require(module);
   }
@@ -12,6 +26,15 @@ module.exports = function(module) {
     return require(__dirname + '/../../coverage/' + module);
   }
 
-  return require(__dirname + '/../../server/' + module);
+  return require(resolveModuleName(module));
 
+};
+
+testRequire.withProxies = function(module, proxies) {
+  var p = {};
+  Object.keys(proxies).forEach(function(key) {
+    p[resolveModuleName(key)] = proxies[key];
+  });
+
+  return proxyquireNoCallThru(resolveModuleName(module), p);
 };
