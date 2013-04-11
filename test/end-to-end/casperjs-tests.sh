2961699bafba5dd4705b5597106e9de9b8083426
Andrew Newdigate <andrew@troupe.co>
2013-04-11 12:52:45 +0200

    Moved casper files about

--- test/end-to-end/casperjs-tests.sh
+++ test/end-to-end/casperjs-tests.sh
@@ -4,9 +4,9 @@ set -e
 
 export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-mkdir -p $SCRIPT_DIR/../../output/
+mkdir -p $SCRIPT_DIR/../../output/test-reports/
 
-$SCRIPT_DIR/casperjs/bin/casperjs test --xunit=$SCRIPT_DIR/../../output/casper.xml \
+$SCRIPT_DIR/casperjs/bin/casperjs test --xunit=$SCRIPT_DIR/../../output/test-reports/casper.xml \
                     --includes=$SCRIPT_DIR/casper/inc.js \
                     $SCRIPT_DIR/casper/tests/ \
                     $@
