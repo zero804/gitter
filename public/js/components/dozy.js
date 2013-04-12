4451cca53135b07593c9ec4c700822857d38d12a
Andrew Newdigate <andrew@troupe.co>
2013-04-12 15:44:29 +0200

    Initial work. References #292

---
+++ public/js/components/dozy.js
@@ -0,0 +1,27 @@
+/*jshint unused:true, browser:true*/
+define([
+  'jquery',
+  'utils/log'
+], function($, log) {
+  "use strict";
+
+  // 10 second timeout, 20 seconds to respond
+  var TIMEOUT = 10000;
+  var THRESHOLD = TIMEOUT * 2;
+
+  var last = Date.now();
+  window.setInterval(function() {
+    var now = Date.now();
+
+    var time = now - last;
+    var sleepDetected = time > THRESHOLD;
+    last = now;
+
+    if(sleepDetected) {
+      log('Sleep detected!');
+      $(document).trigger('reawaken', time);
+    }
+
+  }, TIMEOUT);
+
+});
