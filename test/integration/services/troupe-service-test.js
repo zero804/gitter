c3e5c51c696d37ac746b814d065d0371f829f127
Andrew Newdigate <andrew@troupe.co>
2013-04-15 10:30:05 +0200

    Null confirmation code fixes. References #369

--- test/integration/services/troupe-service-test.js
+++ test/integration/services/troupe-service-test.js
@@ -15,44 +15,68 @@ var once = times(1);
 var twice = times(2);
 var thrice = times(3);
 
-describe('troupe-service', function() {
+function testInviteAcceptance(email, userStatus, emailNotificationConfirmationMethod, done) {
+  var emailNotificationServiceMock = mockito.spy(testRequire('./services/email-notification-service'));
+  var troupeService = testRequire.withProxies("./services/troupe-service", {
+    './email-notification-service': emailNotificationServiceMock
+  });
 
-  describe('#acceptRequest()', function() {
-    it('should allow an UNCONFIRMED user (without a confirmation code) request to be accepted', function(done) {
-      var nonExistingEmail = 'testuser' + Date.now() + '@troupetest.local';
+  persistence.Troupe.findOne({ uri: 'testtroupe1' }, function(err, troupe) {
+    if(err) return done(err);
 
-      var emailNotificationServiceMock = mockito.spy(testRequire('./services/email-notification-service'));
-      var troupeService = testRequire.withProxies("./services/troupe-service", {
-        './email-notification-service': emailNotificationServiceMock
-      });
-
-
-      persistence.Troupe.findOne({ uri: 'testtroupe1' }, function(err, troupe) {
+    persistence.User.create({
+      email: email,
+      displayName: 'Test User ' + new Date(),
+      confirmationCode: null,  // IMPORTANT. This is the point of the test!!!
+      status: userStatus }, function(err, user) {
         if(err) return done(err);
 
-        persistence.User.create({
-          email: nonExistingEmail,
-          displayName: 'Test User ' + new Date(),
-          confirmationCode: null,  // IMPORTANT. This is the point of the test!!!
-          status: 'UNCONFIRMED' }, function(err, user) {
+        troupeService.addRequest(troupe.id, user.id, function(err, request) {
+          if(err) return done(err);
+
+          troupeService.acceptRequest(request, function(err) {
             if(err) return done(err);
 
-            troupeService.addRequest(troupe.id, user.id, function(err, request) {
+            mockito.verify(emailNotificationServiceMock, once)[emailNotificationConfirmationMethod]();
+
+            persistence.Troupe.findOne({ uri: 'testtroupe1' }, function(err, troupe2) {
               if(err) return done(err);
 
-              troupeService.acceptRequest(request, function(err) {
-                if(err) return done(err);
+              assert(troupeService.userHasAccessToTroupe(user, troupe2), 'User has not been granted access to the troupe');
+              assert(troupeService.userIdHasAccessToTroupe(user.id, troupe2), 'User has not been granted access to the troupe');
 
-                mockito.verify(emailNotificationServiceMock, once).sendConfirmationForNewUserRequest();
-
-                done();
-              });
+              done();
             });
 
           });
         });
 
       });
+  });
+}
 
+describe('troupe-service', function() {
+
+  describe('#acceptRequest()', function() {
+    it('should allow an ACTIVE user (without a confirmation code) request to be accepted', function(done) {
+
+      var nonExistingEmail = 'testuser' + Date.now() + '@troupetest.local';
+
+      testInviteAcceptance(nonExistingEmail, 'ACTIVE', 'sendRequestAcceptanceToUser', done);
     });
+
+    it('should allow an UNCONFIRMED user (without a confirmation code) request to be accepted', function(done) {
+      var nonExistingEmail = 'testuser' + Date.now() + '@troupetest.local';
+
+      testInviteAcceptance(nonExistingEmail, 'UNCONFIRMED', 'sendConfirmationForNewUserRequest', done);
+    });
+
+
+    it('should allow an PROFILE_NOT_COMPLETED user (without a confirmation code) request to be accepted', function(done) {
+      var nonExistingEmail = 'testuser' + Date.now() + '@troupetest.local';
+
+      testInviteAcceptance(nonExistingEmail, 'PROFILE_NOT_COMPLETED', 'sendRequestAcceptanceToUser', done);
+    });
+
+  });
 });
