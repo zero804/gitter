8acb5e60d287182c5e24b57c2117e96d0724fb43
Tim Lind <tim@troupe.co>
2013-04-29 18:00:31 +0200

    Front end code for auto complete invite is working correctly and sending invites. Needs styling, and email address validation. references #448

--- public/js/views/shareSearch/shareSearchView.js
+++ public/js/views/shareSearch/shareSearchView.js
@@ -15,19 +15,56 @@ define([
   var View = TroupeViews.Base.extend({
     template: template,
 
+    events: {
+      'click .removeInvite': 'deselectPerson',
+      'click button[type=submit]': 'sendInvites'
+    },
+
     initialize: function() {
+      // [ { userId: }, or { email: } ]
+      this.invites = [];
     },
 
     afterRender: function() {
+      this.createTypeahead();
+    },
+
+    createTypeahead: function() {
+      var self = this;
+      var sources = {}, source;
+
       this.$el.find('input[name=inviteSearch]').typeahead({
         source: function(query, process) {
-          $.ajax({ url: '/user?q=' + query, success: function(data) {
-            var names = _.map(data.results, function(u) {
-              u.toString = function() {return u.displayName;};
-              return u.displayName;
-            });
+          if(sources[query]) {
+            source = sources[query];
+            // process(source); not needed
+            return sources[query];
+          }
 
-            process(data.results);
+          var emptyPreviously = _.some(sources, function(v,k) {
+            return query.toLowerCase().indexOf(k.toLowerCase()) === 0 && v.length <= 1;
+          });
+
+          if(emptyPreviously) {
+            // a previous search with a shorter matching query has returned no results.
+            // don't fetch.
+            source = sources[query] = [];
+            addEmailOption(source, query);
+            installToString(source);
+            process(source);
+            return;
+          }
+
+          // fetch from server
+          $.ajax({ url: '/user?q=' + query, success:
+            function(data) {
+
+              source = data.results;
+              sources[query] = source;
+
+              addEmailOption(source, query);
+              installToString(source);
+              process(source);
             }
           });
         },
@@ -35,28 +72,86 @@ define([
           return _.sortBy(items, 'displayName');
         },
         matcher: function(item) {
-          var query = this.query.trim(), fullname = item.displayName;
-          return _.some(fullname.split(' '), function (name) {
-            return name.indexOf(query) === 0;
+          var query = this.query.trim(), queryWords = query.split(' '), fullname = item.displayName;
+          return _.any(fullname.split(' '), function (name) {
+            return _.any(queryWords, function(q) {
+              return name.toLowerCase().indexOf(q.toLowerCase()) === 0;
+            });
           });
         },
         highlighter: function(item) {
+          // TODO (maybe) include the avatar, but that would not be recommended for email address invites as the avatar url changes at each keystroke
           return $.fn.typeahead.Constructor.prototype.highlighter.call(this, item.displayName);
         },
-        updater: function(item) {
-          return item;
-        }
-        /*name: 'users',
-        remote: '/user?q=%QUERY',
-        cache: false,
-        filter: function(data) {
-          _.each(data.results, function(user) {
-            user.value = user.displayName;
-            user.tokens = user.displayName.split(' ');
+        updater: function(value) {
+          var item = _.find(source, function(u) {
+            return u.id === value || u.email === value;
           });
 
-          return data.results;
-        }*/
+          // TODO VALIDATE EMAIL ADDRESS FIELD!
+
+          self.selectPerson(item);
+
+          return ''; // empty the search box
+        },
+        minLength: 2
+      });
+
+      function addEmailOption(source, query) {
+        // add the query as an email address option
+        source.push({ email: query, displayName: query, avatarUrlSmall: '/gravatar/'+query }); // note:  this will provide a diff avatar each key stroke, don't show it in the autocomplete!
+      }
+
+      function installToString(source) {
+        // install a toString function on each data item so it stores the id in the data-value element attribute
+        _.each(source, function(item) {
+          item.toString = function() {
+            return this.id || this.email;
+          };
+        });
+      }
+
+    },
+
+    selectPerson: function(user) {
+      // TODO don't allow adding the same person more than once (alt: don't show them in the autocomplete results)
+      var invite = {};
+      if (user.id)
+        invite.userId = user.id;
+      if (user.email)
+        invite.email = user.email;
+
+      this.invites.push(invite);
+
+      this.$el.find("#invites").append(rowTemplate({ user: user, value: user.toString() }));
+    },
+
+    deselectPerson: function(e) {
+      var value = $(e.target).data('value');
+      this.$el.find('#invites .invite[data-value="'+value+'"]').remove();
+      this.invites = _.filter(this.invites, function(invite) {
+        if (invite.userId)
+          return invite.userId !== value;
+        else
+          return invite.email !== value;
+      });
+    },
+
+    sendInvites: function() {
+      $.ajax({
+        url: "/troupes/" + window.troupeContext.troupe.id + "/invites",
+        contentType: "application/json",
+        dataType: "json",
+        data: JSON.stringify(this.invites),
+        type: "POST",
+        success: function(data) {
+           if(data.failed) {
+            return;
+          }
+          $('.modal-content').hide();
+          $('.modal-success').show();
+          // self.trigger('share.complete', data);
+        }
       });
     }
 
