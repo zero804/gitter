98dd28e212a2e67b246d8d956710b43f56195198
Tim Lind <tim@troupe.co>
2013-05-02 16:23:03 +0200

    Deleted old shareView code. References #448

--- public/js/app-integrated.js
+++ public/js/app-integrated.js
@@ -35,7 +35,6 @@ require([
   'views/shareSearch/shareSearchView',
   'views/signup/createTroupeView',
   'hbs!./views/app/tmpl/appHeader',
-  'views/share/shareView',
   'views/app/troupeSettingsView',
   'components/webNotifications',
   'components/unread-items-client',
@@ -45,7 +44,7 @@ require([
 ], function($, _, Backbone, _backboneKeys, Marionette, _Helpers, TroupeViews, realtime, eyeballs, dozy, AppIntegratedView, ChatInputView, ChatCollectionView, FileView, ConversationView, RequestView,
             collections, troupeModels, fileModels, conversationModels, userModels, chatModels, requestModels, FileDetailView, filePreviewView, fileVersionsView,
             RequestDetailView, PersonDetailView, conversationDetailView, TroupeCollectionView, PeopleCollectionView, profileView, shareView,
-            createTroupeView, headerViewTemplate, shareTroupeView,
+            createTroupeView, headerViewTemplate,
             troupeSettingsView, webNotifications, unreadItemsClient, log /*, errorReporter , FilteredCollection */) {
   "use strict";
 
@@ -146,7 +145,6 @@ require([
         { name: "profile",        re: /^profile$/,                viewType: profileView.Modal },
         { name: "share",          re: /^share$/,                  viewType: shareView.Modal },
         { name: "create",         re: /^create$/,                 viewType: createTroupeView.Modal,       collection: collections.troupes,   skipModelLoad: true },
-        { name: "shareTroupe",    re: /^shareTroupe/,             viewType: shareTroupeView.Modal },
         { name: "troupeSettings", re: /^troupeSettings/,          viewType: troupeSettingsView }
 
       ];
