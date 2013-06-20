8526f6e2cf51978234bcdd20a03616c3292524b6
Andrew Newdigate <andrew@troupe.co>
2013-06-20 22:57:21 +0100

    Moving code around to remove duplication and refactor for usernames

--- public/js/router-homepage.js
+++ public/js/router-homepage.js
@@ -14,40 +14,17 @@ require([
   'views/toolbar/troupeCollectionView',
   'views/profile/profileView',
   'views/signup/createTroupeView',
-  'hbs!./views/invite/tmpl/invitesItemTemplate',
+  'views/app/invitesView',
   'hbs!./views/app/tmpl/appHeader',
   'components/webNotifications',
+  'utils/router',
   'components/errorReporter',
   'filtered-collection'
 ], function($, _, Backbone, _backboneKeys, Marionette, _Helpers, TroupeViews, realtime, dozy, AppIntegratedView,
             troupeCollections, TroupeCollectionView, profileView,
-            createTroupeView, invitesItemTemplate, headerViewTemplate,
-            webNotifications /*, errorReporter , FilteredCollection */) {
+            createTroupeView, InvitesView, headerViewTemplate,
+            webNotifications, Router /*, errorReporter , FilteredCollection */) {
   "use strict";
-
-  $(document).on("click", "a", function(event) {
-    if(this.href) {
-      var href = $(this).attr('href');
-      if(href.substring(0, 2) === "#|") {
-        event.preventDefault();
-
-        href = href.substring(2);
-
-        var currentFragment;
-        var hash = window.location.hash;
-
-        if(!hash) {
-          currentFragment = '#';
-        } else {
-          currentFragment = hash.split('|', 1)[0];
-        }
-
-        window.location = currentFragment + "|" + href;
-      }
-    }
-
-    return true;
-  });
 
   var app = new Marionette.Application();
   app.collections = {};
@@ -84,131 +61,7 @@ require([
   };
 
   var router;
-  var appView = new AppIntegratedView({ app: app });
-
-  function track(name) {
-    $(document).trigger('track', name);
-  }
-
-  var Router = Backbone.Router.extend({
-    initialize: function(/*options*/) {
-      this.regionsFragments = {};
-      this.route(/^(.*?)$/, "handle");
-    },
-
-    regionFragmentMapping: [
-      'rightPanelRegion',
-      'dialogRegion'
-    ],
-
-    getViewDetails: function(fragment) {
-
-      var routes = [
-
-        { name: "profile",        re: /^profile$/,                viewType: profileView.Modal },
-        { name: "create",         re: /^create$/,                 viewType: createTroupeView.Modal, collection: troupeCollections.troupes,   skipModelLoad: true },
-        { name: "troupeSettings", re: /^troupeSettings/,          viewType: troupeSettingsView }
-
-      ];
-
-      var match = null;
-      _.any(routes, function(route) {
-        if(route.re.test(fragment)) {
-          match = route;
-          return true;
-        }
-      });
-
-      if(!match) return null;
-
-      var result = match.re.exec(fragment);
-
-      return {
-        viewType: match.viewType,
-        collection: match.collection,
-        viewOptions: match.viewOptions,
-        skipModelLoad: match.skipModelLoad ? match.skipModelLoad : /* If there is no collection, skipModelLoad=true */ !match.collection,
-        name: match.name,
-        id: result[1]
-      };
-    },
-
-    handle: function(path) {
-      var h = window.location.hash;
-      if (h.indexOf('#%7C') === 0) {
-        window.location.hash = h.replace(/%7C/, '|');
-        path = path.replace(/%7C/, '|');
-      }
-      var parts = path ? path.split("|") : [];
-
-      this.regionFragmentMapping.forEach(function(regionName, index) {
-        var fragment = parts[index] ? parts[index] : "";
-
-        if(fragment.substring(0, 1) === '#') {
-          fragment = fragment.substring(1);
-        }
-
-        var region, viewDetails;
-
-        function loadItemIntoView() {
-          var model = viewDetails.skipModelLoad ? null : viewDetails.collection.get(viewDetails.id);
-          var cv = region.currentView;
-
-          if(viewDetails.collection) {
-            if(cv instanceof viewDetails.viewType &&
-              cv.supportsModelReplacement &&
-              cv.supportsModelReplacement()) {
-              cv.replaceModel(model);
-              $(document).trigger('appNavigation');
-
-              return;
-            }
-          }
-
-          /* Default case: load the view from scratch */
-          var viewOptions = _.extend({ model: model, collection: viewDetails.collection }, viewDetails.viewOptions);
-          var view = new viewDetails.viewType(viewOptions);
-          region.show(view);
-          $(document).trigger('appNavigation');
-        }
-
-        if(this.regionsFragments[regionName] !== fragment) {
-          this.regionsFragments[regionName] = fragment;
-
-          region = app[regionName];
-
-          if(fragment) {
-            // lookup handler:
-            viewDetails = this.getViewDetails(fragment);
-
-            if(viewDetails) {
-              track(viewDetails.name);
-
-              // If we have a collection and we need to load a model item,
-              // ensure that the collection has already been populated. If it
-              // hasn't, wait until it has
-              if(!viewDetails.skipModelLoad) {
-                if(viewDetails.collection.length === 0) {
-                  viewDetails.collection.once('reset', loadItemIntoView, this);
-                  return;
-                }
-              }
-
-              loadItemIntoView();
-              return;
-            } else {
-              track('unknown');
-            }
-          }
-
-          region.close();
-        } else {
-          // This hasn't changed....
-        }
-      }, this);
-    }
-
-  });
+  new AppIntegratedView({ app: app });
 
   // Troupe Collections
   var filteredTroupeCollection = troupeCollections.normalTroupes;
@@ -234,51 +87,34 @@ require([
     // add the troupe views to the left menu
 
     // recent troupe view
-    var recentTroupeCollectionView = new TroupeCollectionView({
-      collection: recentTroupeCollection
-    });
-    app.leftMenuRecent.show(recentTroupeCollectionView);
+    app.leftMenuRecent.show(new TroupeCollectionView({ collection: recentTroupeCollection }));
 
     // normal troupe view
-    var troupeCollectionView = new TroupeCollectionView({
-      collection: filteredTroupeCollection
-    });
-    app.leftMenuTroupes.show(troupeCollectionView);
+    app.leftMenuTroupes.show(new TroupeCollectionView({collection: filteredTroupeCollection }));
 
     // one to one troupe view
-    var oneToOneTroupeCollectionView = new TroupeCollectionView({
-      collection: peopleOnlyTroupeCollection
-    });
-    app.leftMenuPeople.show(oneToOneTroupeCollectionView);
+    app.leftMenuPeople.show(new TroupeCollectionView({collection: peopleOnlyTroupeCollection }));
 
     // unread troupe view
-    var unreadTroupeCollectionView = new TroupeCollectionView({
-      collection: unreadTroupeCollection
-    });
-    app.leftMenuUnread.show(unreadTroupeCollectionView);
+    app.leftMenuUnread.show(new TroupeCollectionView({collection: unreadTroupeCollection }));
 
     // favourite troupe view
-    var favouriteTroupeCollectionView = new TroupeCollectionView({
-      collection: favouriteTroupesCollection
-    });
-    app.leftMenuFavourites.show(favouriteTroupeCollectionView);
+    app.leftMenuFavourites.show(new TroupeCollectionView({ collection: favouriteTroupesCollection }));
 
     // incoming invites collection view
-    var invitesCollectionView = new Marionette.CollectionView({
-      collection: incomingInvitesCollection,
-      itemView: TroupeViews.Base.extend({
-        tagName: 'li',
-        template: invitesItemTemplate
-      })
-    });
-    app.leftMenuInvites.show(invitesCollectionView);
+    app.leftMenuInvites.show(new InvitesView({ collection: incomingInvitesCollection }));
 
   });
 
   app.on("initialize:after", function(){
-    router = new Router({});
+    router = new Router({
+      routes: [
+        { name: "profile",        re: /^profile$/,                viewType: profileView.Modal },
+        { name: "create",         re: /^create$/,                 viewType: createTroupeView.Modal, collection: troupeCollections.troupes,   skipModelLoad: true }
+      ],
+      app: app
+    });
 
-    router.initialize();
     Backbone.history.start();
   });
 
@@ -294,5 +130,6 @@ require([
   window._troupeDebug = {
     app: app
   };
+
   return app;
 });
