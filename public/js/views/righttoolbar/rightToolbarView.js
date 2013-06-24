46c3b66f15bb495f5dd09bec66f737068eab50c3
Tim Lind <tim@troupe.co>
2013-06-24 13:53:42 +0200

    Removed right hand bar from app integrated hbs. Beginnings of the rightToolbarView. References #595

---
+++ public/js/views/righttoolbar/rightToolbarView.js
@@ -0,0 +1,64 @@
+/*jshint unused:true, browser:true */
+define([
+  'marionette',
+  'hbs!./tmpl/rightToolbar',
+  'collections/instances/integrated-items',
+  'collections/instances/troupes',
+  'views/request/requestView',
+  'views/file/fileView',
+  'views/conversation/conversationView',
+  'views/people/peopleCollectionView'
+], function(Marionette, rightToolbarTemplate, itemCollections, troupeCollections, RequestView, FileView, ConversationView, PeopleCollectionView) {
+  "use strict";
+
+  return Backbone.Marionette.Layout.extend({
+    template: rightToolbarTemplate,
+
+    regions: {
+      requests: "#request-roster",
+      people: "#people-roster",
+      files: "#file-list",
+      conversations: "#frame-conversations",
+      sidebar: "#right-panel"
+    },
+
+    afterRender: function() {
+      var self = this;
+
+      //this.requests.show();
+      //this.people.show();
+      //this.files.show();
+      //this.conversations.show();
+      //this.sidebar.show();
+
+      // reference collections
+      var requestCollection = itemCollections.requests;
+      var fileCollection = itemCollections.files;
+      var conversationCollection = itemCollections.conversations;
+      var userCollection = itemCollections.users;
+
+      // Request View
+      this.requests.show(new RequestView({ collection: requestCollection }));
+
+      // File View
+      this.files.show(new FileView({ collection: fileCollection }));
+
+      // Conversation View
+      if (!window.troupeContext.troupe.oneToOne) {
+        var conversationView = new ConversationView({
+          collection: conversationCollection
+        });
+        self.conversations.show(conversationView);
+      }
+      else {
+        $('#mail-list').hide();
+      }
+
+      // People View
+      this.people.show(new PeopleCollectionView({ collection: userCollection }));
+
+    }
+  });
+
+
+});
