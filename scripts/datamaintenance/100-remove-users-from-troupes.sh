bd68a732c80d1408302ec99f70184b1222123270
Andrew Newdigate <andrew@troupe.co>
2013-06-27 10:21:15 +0100

    Scripts to maintain your data

---
+++ scripts/datamaintenance/100-remove-users-from-troupes.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+MONGO_URL=$1
+if [ -z "$MONGO_URL" ]; then MONGO_URL=troupe; fi
+
+mongo --verbose $MONGO_URL <<DELIM
+
+db.troupes.find({}).forEach(function(f) {
+  print('Updating' + f);
+
+  f.users.forEach(function(user) {
+    if(!db.users.findOne({ _id: user.userId})) {
+      print('Removing user ' + user.userId);
+      db.troupes.update( { _id: { $exists: true }, { $pull: { users: { userId: user.userId } } });
+    }
+  });
+});
+
+DELIM
