c7cfc5f2784df031d48c786de1745c1134c1e0a6
Tim Lind <tim@troupe.co>
2013-07-05 16:20:25 +0200

    Moved the unread bade code to the user header view so it works on the user home page as well. closes #628

--- public/js/views/app/troupeHeaderView.js
+++ public/js/views/app/troupeHeaderView.js
@@ -3,9 +3,8 @@ define([
   'jquery',
   'utils/context',
   'views/app/userHeaderView',
-  'components/unread-items-client',
   'collections/instances/troupes'
-  ], function($, context, UserHeaderView, unreadItemsClient, collections)  {
+  ], function($, context, UserHeaderView, collections)  {
   "use strict";
 
   return UserHeaderView.extend({
@@ -15,34 +14,7 @@ define([
     },
 
     initialize: function() {
-      var self = this;
-
-      $(document).on('troupeUpdate', function(e, message) {
-        // header title
-        $('.trpHeaderTitle').html(message.model.name);
-        // window / title bar
-        self.updateTitlebar(unreadItemsClient.getCounts());
-      });
-
-      function onTroupeUnreadTotalChange(event, values) {
-        self.updateTitlebar(values);
-
-        function updateBadge(selector, count) {
-          var badge = $(selector);
-          badge.text(count);
-          if(count > 0) {
-            badge.show();
-          } else {
-            badge.hide();
-          }
-        }
-
-        // overall count
-        updateBadge('#unread-badge', values.overall);
-      }
-
-      $(document).on('troupeUnreadTotalChange', onTroupeUnreadTotalChange);
-      onTroupeUnreadTotalChange(null, unreadItemsClient.getCounts());
+      UserHeaderView.prototype.initialize.apply(this, arguments);
     },
 
     getRenderData: function() {
@@ -76,37 +48,6 @@ define([
       window.troupeContext.troupe.favourite = isFavourite;
       var troupe = collections.troupes.get(window.troupeContext.troupe.id);
       troupe.set('favourite', isFavourite);
-    },
-
-    updateTitlebar: function(values) {
-      $('title').html(this.getTitlebar(values));
-    },
-
-    getTitlebar: function(counts) {
-      var mainTitle;
-      if (window.troupeContext.title) {
-        mainTitle = window.troupeContext.title + " - Troupe";
-      } else
-      {
-        mainTitle = "Troupe";
-      }
-
-      // TODO this isn't working properly when updating the troupe name, need to be able to poll unreadItems count not just accept the event
-      var overall = counts.overall;
-      var current = counts.current;
-      if(overall <= 0) {
-        return mainTitle;
-      }
-
-      if(overall <= 10) {
-        if(current > 0) {
-          return String.fromCharCode(0x2789 + overall) + ' ' + mainTitle;
-        }
-
-        return String.fromCharCode(0x277F + overall) + ' ' + mainTitle;
-      }
-
-      return '[' + overall + '] ' + window.troupeContext.title + " - Troupe";
     }
 
   });
