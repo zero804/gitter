f38182874c3d73dd658234461baf3cc54970a58c
Mike Bartlett <mike.bartlett@gmail.com>
2013-07-11 13:17:27 +0100

    little script to output test users

---
+++ scripts/utils/get-test-users.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+/*jshint globalstrict:true, trailing:false, unused:true, node:true */
+"use strict";
+
+var nconf       = require('../../server/utils/config');
+var winston     = require('../../server/utils/winston');
+var persistence = require("../../server/services/persistence-service");
+var shutdown    = require('../../server/utils/shutdown');
+
+
+function getAllUsers(callback) {
+  persistence.User.find({email:/local/}, function (err, users) {
+    if (err) console.log(err);
+    callback("",users);
+  });
+}
+
+getAllUsers(function(err, users) {
+  users.forEach(function(user) {
+    console.log(user._id);
+  });
+
+  shutdown.shutdownGracefully();
+
+});
+
