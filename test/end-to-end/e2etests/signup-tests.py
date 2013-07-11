6115335f8d7d31404b87f5bf01a2392898992c1c
Andy Trevorah <andy@troupe.co>
2013-07-11 10:29:07 +0100

    added asserts to the user signup tests

--- test/end-to-end/e2etests/signup-tests.py
+++ test/end-to-end/e2etests/signup-tests.py
@@ -2,6 +2,9 @@ import utils
 import time
 import urllib2
 from nose.plugins.attrib import attr
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = None
 
@@ -51,73 +54,76 @@ def testSignupFromHomePage():
 def testUsernameSelectionUnavailable():
     driver.get(utils.baseUrl("/signout"))
     driver.get(utils.baseUrl("/x"))
-    emailAddress = 'testuser.' + time.strftime("%Y%m%d%H%M%S", time.gmtime()) + '@troupetest.local'
+    emailAddress = generateUniqueEmailAddress()
     driver.find_element_by_css_selector('#button-signup').click()
     form = driver.find_element_by_css_selector('#signup-form')
     form.find_element_by_name('email').send_keys(emailAddress)
     form.find_element_by_name('submit').click()
     driver.find_element_by_css_selector('.label-signupsuccess')
 
-    queryurl = utils.baseUrl("/testdata/confirmationCodeForEmail?email=" + emailAddress)
-    response = urllib2.urlopen(queryurl)
-    confirmCode = response.read()
-
-    # visit confirm link
-    driver.get(utils.baseUrl('/confirm/'+confirmCode))
+    confirmEmailAddress(emailAddress, driver)
 
     # choose a username
     username = 'testuser1'
     inputUser = driver.find_element_by_css_selector('input[name=username]')
     inputUser.send_keys(username)
-    driver.find_element_by_css_selector('.not-valid-message').is_displayed()
+    errorMessage = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, 'not-valid-message')))
+
+    assert errorMessage.is_displayed()
 
 
 def testUsernameSelectionAvailable():
     driver.get(utils.baseUrl("/signout"))
     driver.get(utils.baseUrl("/x"))
-    emailAddress = 'testuser.' + time.strftime("%Y%m%d%H%M%S", time.gmtime()) + '@troupetest.local'
+    emailAddress = generateUniqueEmailAddress()
     driver.find_element_by_css_selector('#button-signup').click()
     form = driver.find_element_by_css_selector('#signup-form')
     form.find_element_by_name('email').send_keys(emailAddress)
     form.find_element_by_name('submit').click()
     driver.find_element_by_css_selector('.label-signupsuccess')
 
-    queryurl = utils.baseUrl("/testdata/confirmationCodeForEmail?email=" + emailAddress)
-    response = urllib2.urlopen(queryurl)
-    confirmCode = response.read()
-
-    # visit confirm link
-    driver.get(utils.baseUrl('/confirm/'+confirmCode))
+    confirmEmailAddress(emailAddress, driver)
 
     # choose a username
     username = 'testuser' + time.strftime("%Y%m%d%H%M%S", time.gmtime())
     inputUser = driver.find_element_by_css_selector('input[name=username]')
     inputUser.send_keys(username)
-    driver.find_element_by_css_selector('.valid-message').is_displayed()
+    validMessage = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, 'valid-message')))
+
+    assert validMessage.is_displayed()
 
 
 def testUsernameSelectionSuggestions():
     driver.get(utils.baseUrl("/signout"))
     driver.get(utils.baseUrl("/x"))
-    emailAddress = 'testuser.' + time.strftime("%Y%m%d%H%M%S", time.gmtime()) + '@troupetest.local'
+    emailAddress = generateUniqueEmailAddress()
     driver.find_element_by_css_selector('#button-signup').click()
     form = driver.find_element_by_css_selector('#signup-form')
     form.find_element_by_name('email').send_keys(emailAddress)
     form.find_element_by_name('submit').click()
     driver.find_element_by_css_selector('.label-signupsuccess')
 
-    queryurl = utils.baseUrl("/testdata/confirmationCodeForEmail?email=" + emailAddress)
-    response = urllib2.urlopen(queryurl)
-    confirmCode = response.read()
-
-    # visit confirm link
-    driver.get(utils.baseUrl('/confirm/'+confirmCode))
+    confirmEmailAddress(emailAddress, driver)
 
     # choose a username
     username = 'testuser1'
     inputUser = driver.find_element_by_css_selector('input[name=username]')
     inputUser.send_keys(username)
-    driver.find_element_by_css_selector('[data-username="testuser11"]')
+    suggestion = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '[data-username="testuser11"]')))
+
+    assert suggestion.is_displayed()
+
+
+def generateUniqueEmailAddress():
+    return 'testuser.' + time.strftime("%Y%m%d%H%M%S", time.gmtime()) + '@troupetest.local'
+
+
+def confirmEmailAddress(emailAddress, driver):
+    queryurl = utils.baseUrl("/testdata/confirmationCodeForEmail?email=" + emailAddress)
+    response = urllib2.urlopen(queryurl)
+    confirmCode = response.read()
+    # visit confirm link
+    driver.get(utils.baseUrl('/confirm/'+confirmCode))
 
 
 def teardown_module():
