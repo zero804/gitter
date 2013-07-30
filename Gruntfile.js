f9d5f9668299e8021765e9975a1f7a5964281e6b
Andy Trevorah <andy@troupe.co>
2013-07-30 11:54:34 +0100

    mobile and desktop now have separate homepages

--- Gruntfile.js
+++ Gruntfile.js
@@ -247,6 +247,7 @@ module.exports = function( grunt ) {
           "public/bootstrap/css/mtrp.css": "public/bootstrap/less/mtrp.less",
           "public/bootstrap/css/signup.css": "public/bootstrap/less/signup.less",
           "public/bootstrap/css/trpHomePage.css": "public/bootstrap/less/trpHomePage.less",
+          "public/bootstrap/css/homepage-mobile.css": "public/bootstrap/less/homepage-mobile.less",
           "public/bootstrap/css/trpChat.css": "public/bootstrap/less/trpChat.less",
           "public/bootstrap/css/trpFiles.css": "public/bootstrap/less/trpFiles.less",
           "public/bootstrap/css/trpMails.css": "public/bootstrap/less/trpMails.less",
