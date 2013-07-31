8b9a9b599dc1d10e2abdd4b4065c06ad75db3a30
Andy Trevorah <andy@troupe.co>
2013-07-31 15:43:14 +0100

    all mobile routers now extend one router

--- public/js/routers/mobile/native/chat-router.js
+++ public/js/routers/mobile/native/chat-router.js
@@ -1,10 +1,35 @@
 /*jshint strict:true, undef:true, unused:strict, browser:true *//* global require:false */
-require(['routers/mobile/mobile-router'], function(MobileRouter) {
+require([
+  'jquery',
+  'routers/mobile/mobile-router',
+  'collections/chat',
+  'views/chat/chatCollectionView',
+  'views/chat/chatInputView',
+  'utils/mobile-resizer'
+  ], function($, MobileRouter, chatModels, ChatCollectionView, chatInputView, mobileResizer) {
   "use strict";
 
   var NativeChatRouter = MobileRouter.extend({
     initialize: function() {
       this.constructor.__super__.initialize.apply(this);
+
+      var chatCollection = new chatModels.ChatCollection();
+      chatCollection.listen();
+
+      var chatCollectionView = new ChatCollectionView({
+        el: $('#frame-chat'),
+        collection: chatCollection
+      });
+
+      chatCollectionView.render();
+
+      new chatInputView.ChatInputView({
+        el: $('#chat-input'),
+        collection: chatCollection,
+        scrollDelegate: chatCollectionView.scrollDelegate
+      }).render();
+
+      mobileResizer.reset();
 
       // Prevent Header & Footer From Showing Browser Chrome
       document.addEventListener('touchmove', function(event) {
--- public/js/routers/mobile/web/mobile-app-router.js
+++ public/js/routers/mobile/web/mobile-app-router.js
@@ -2,16 +2,35 @@
 require([
   'jquery',
   'retina',
+  'collections/chat',
+  'views/chat/chatCollectionView',
+  'views/chat/chatInputView',
   'utils/mobile-resizer',
   'routers/mobile/mobile-router',
   'views/toolbar/troupeMenu',
   'views/app/mobileAppView'
-  ], function($, retina, mobileResizer, MobileRouter, TroupeMenu, MobileAppView) {
+  ], function($, retina, chatModels, ChatCollectionView, chatInputView, mobileResizer, MobileRouter, TroupeMenu, MobileAppView) {
   "use strict";
 
   var MobileAppRouter = MobileRouter.extend({
     initialize: function() {
       this.constructor.__super__.initialize.apply(this);
+
+      var chatCollection = new chatModels.ChatCollection();
+      chatCollection.listen();
+
+      var chatCollectionView = new ChatCollectionView({
+        el: $('#frame-chat'),
+        collection: chatCollection
+      });
+
+      chatCollectionView.render();
+
+      new chatInputView.ChatInputView({
+        el: $('#chat-input'),
+        collection: chatCollection,
+        scrollDelegate: chatCollectionView.scrollDelegate
+      }).render();
 
       new TroupeMenu({
         el: $('#troupeList')
@@ -20,6 +39,8 @@ require([
       new MobileAppView({
         el: $('#pageContainer')
       });
+
+      mobileResizer.reset();
 
       var fakeWindow = {};
       retina.init(fakeWindow);
