081417424fd665697c6c41a9eadbb9c4eae17f5d
Andy Trevorah <andy@troupe.co>
2013-07-31 17:49:48 +0100

    mobile homepage now slides to signup screen

--- public/js/signup-mobile.js
+++ public/js/signup-mobile.js
@@ -1,8 +1,33 @@
 require([
-  'jquery',
+  'jquery-hammer',
+  'backbone',
   'views/signup/signupModalView',
   'views/signup/signupModalConfirmView'
-  ], function($, SignupModalView, SignupModalConfirmView) {
+  ], function($, BackBone, SignupModalView, SignupModalConfirmView) {
+
+    var Router = Backbone.Router.extend({
+      routes: {
+        'signup': 'showSignup',
+        '*path':  'defaultRoute'
+      },
+
+      showSignup: function() {
+        $('#panelList').addClass('showingSecondPanel');
+      },
+
+      defaultRoute: function() {
+        $('#panelList').removeClass('showingSecondPanel');
+      }
+    });
+
+    var app = new Router();
+
+    $('#signup').hammer().on('tap', function(event) {
+      app.navigate('signup', {trigger: true});
+      event.stopPropagation();
+    });
+
+    Backbone.history.start();
 
     var view = new SignupModalView({el: $('#signup-form')});
     view.once('signup.complete', function(data) {
