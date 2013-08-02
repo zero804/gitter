e19f2c19e7e486ab71b1d4fd04c0bd9420d4c3de
Andrew Newdigate <andrew@troupe.co>
2013-08-02 11:24:59 +0100

    Cleaning up a few things in the test in the hope that it doesn't fail

--- test/integration/services/contact-service-test.js
+++ test/integration/services/contact-service-test.js
@@ -4,9 +4,6 @@
 
 var testRequire = require('./../test-require');
 var fixtureLoader = require('../test-fixtures');
-var fixture = {
-  user1: {}
-};
 
 var contactService = testRequire('./services/contact-service');
 var persistence = testRequire('./services/persistence-service');
@@ -16,7 +13,7 @@ var assert = require('assert');
 var googleJSON = {
   version: '1.0',
   encoding: 'UTF-8',
-  feed: { 
+  feed: {
     xmlns: 'http://www.w3.org/2005/Atom',
     'xmlns$openSearch': 'http://a9.com/-/spec/opensearchrss/1.0/',
     'xmlns$gContact': 'http://schemas.google.com/contact/2008',
@@ -66,32 +63,46 @@ var googleJSON = {
 describe('Contact Service', function() {
 
   describe('Data ingestion', function() {
-    before(fixtureLoader(fixture, {user1: {}}));
+    var fixture = {};
+
+    before(fixtureLoader(fixture, { user1: { } }));
 
     it('should process Google JSON properly', function(done) {
       var user = fixture.user1;
       contactService.ingestGoogleContacts(user, googleJSON, function() {
         persistence.Contact.find({'userId': user.id}).exec(function(err, contacts) {
+          if(err) return done(err);
           assert(contacts[0].emails[0] == 'hackers.are.rockstars@gmail.com', 'Failed to import.');
           done();
         });
       });
     });
+
+    after(function() {
+      fixture.cleanup();
+    });
+
   });
 
 
   describe('Contact search', function() {
+    var fixture = {};
 
-    before(fixtureLoader(fixture, {contact1: { 
-      user: 'user1',
-      name: 'Mauro Pompilio',
-      emails: ['hackers.are.rockstars@gmail.com'],
-      source: 'google'
-    }, user1: {}}));
+    before(fixtureLoader(fixture, {
+      contact1: {
+        user: 'user1',
+        name: 'Mauro Pompilio',
+        emails: ['hackers.are.rockstars@gmail.com'],
+        source: 'google'
+      },
+      user1: {}
+    }));
 
     it('should find a user by name', function(done) {
       var user = fixture.user1;
       contactService.find(user, 'mau', function(err, matches) {
+        if(err) return done(err);
+
         assert(matches[0].displayName == fixture.contact1.name, "Can't match by name");
         done();
       });
@@ -100,13 +111,15 @@ describe('Contact Service', function() {
     it('should find a user by email', function(done) {
       var user = fixture.user1;
       contactService.find(user, 'hack', function(err, matches) {
+        if(err) return done(err);
+
         assert(matches[0].displayName == fixture.contact1.name, "Can't match by email");
         done();
       });
     });
 
     after(function() {
-      persistence.Contact.remove({});
+      fixture.cleanup();
     });
 
   });
