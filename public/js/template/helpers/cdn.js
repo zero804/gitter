81e27fc782cd9f675d25b427b238089b8f1a2215
Andrew Newdigate <andrew@troupe.co>
2013-08-09 14:52:53 +0100

    Full client-side CDN implementation

--- public/js/template/helpers/cdn.js
+++ public/js/template/helpers/cdn.js
@@ -1,15 +1,56 @@
 /*jshint strict:true, undef:true, unused:strict, browser:true *//* global define:false */
 define([
+  'utils/context',
   'handlebars'
-], function ( Handlebars ) {
+], function (context, Handlebars ) {
   "use strict";
 
-  // TODO: write a complete CDN implementation
-  function cdn (url) {
+  var hosts = context.env('cdns');
+  var hostLength = hosts && hosts.length;
+  var cdnPrefix = context.env('appVersion') ? "/s/" + context.env('appVersion') : '';
+
+  function cdnPassthrough(url) {
     return "/" + url;
   }
 
-  Handlebars.registerHelper( 'cdn', cdn );
-  return cdn;
+  function cdnSingle(url, options) {
+    var nonrelative = options && options.nonrelative;
+
+    var prefix = nonrelative ? "https://" : "//";
+    if(options && options.notStatic === true) {
+      return prefix + hosts[0] + "/" + url;
+    }
+
+    return prefix + hosts[0] + cdnPrefix + "/" + url;
+  }
+
+  function cdnMulti(url, options) {
+    var x = 0;
+    for(var i = 0; i < url.length; i = i + 3) {
+      x = x + url.charCodeAt(i);
+    }
+
+    var host = hosts[x % hostLength];
+
+    var nonrelative = options && options.nonrelative;
+    var prefix = nonrelative ? "https://" : "//";
+
+    if(options && options.notStatic === true) {
+      return prefix + host + "/" + url;
+    }
+
+    return prefix + host + cdnPrefix + "/" + url;
+  }
+
+  if(!hostLength) {
+    Handlebars.registerHelper( 'cdn', cdnPassthrough);
+    return cdnPassthrough;
+  } else if(hostLength == 1) {
+    Handlebars.registerHelper( 'cdn', cdnSingle);
+    return cdnSingle;
+  } else {
+    Handlebars.registerHelper( 'cdn', cdnMulti);
+    return cdnMulti;
+  }
 
 });
