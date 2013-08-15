11ca5de02834766ee3af9648d613095a67ae5d36
Andy Trevorah <andy@troupe.co>
2013-08-15 15:46:43 +0100

    share modal now appears on mobile web chat app

--- public/js/mobile-app.js
+++ public/js/mobile-app.js
@@ -4,8 +4,10 @@ require([
   'mobile-app-container',
   'collections/chat',
   'views/chat/chatCollectionView',
-  'views/chat/chatInputView'
-  ], function($, app, chatModels, ChatCollectionView, chatInputView) {
+  'views/chat/chatInputView',
+  'utils/router',
+  'views/shareSearch/shareSearchView'
+  ], function($, app, chatModels, ChatCollectionView, chatInputView, Router, shareSearchView) {
   "use strict";
 
   var chatCollection = new chatModels.ChatCollection();
@@ -20,6 +22,34 @@ require([
     collection: chatCollection,
     scrollDelegate: chatCollectionView.scrollDelegate
   }).render();
+
+  app.addInitializer(function() {
+    var dialogRegion = {
+      currentView: null,
+      show: function(view) {
+        if(this.currentView) {
+          this.currentView.fade = false;
+          this.currentView.hideInternal();
+        }
+        this.currentView = view;
+        view.navigable = true;
+        view.show();
+      },
+      close: function() {
+        if(this.currentView) {
+          this.currentView.navigationalHide();
+          this.currentView = null;
+        }
+      }
+    };
+
+    new Router({
+      routes: [
+        { name: "share",            re: /^share$/,                  viewType: shareSearchView.Modal },
+      ],
+      regions: [null, dialogRegion]
+    });
+  });
 
   app.content.show(chatCollectionView);
   app.start();
