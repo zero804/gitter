98d3222b54a38e110f179906fef66c45e0bbf8f1
Andrew Newdigate <andrew@troupe.co>
2013-09-03 12:06:15 +0100

    Connecting contacts on user import

--- server/services/contact-service.js
+++ server/services/contact-service.js
@@ -2,10 +2,30 @@
 "use strict";
 
 var persistence   = require("./persistence-service");
+var userService   = require("./user-service");
 var statsService  = require("./stats-service");
 var _             = require('underscore');
 var winston       = require('winston');
 var Q             = require('Q');
+
+function indexImportsByEmail(contacts) {
+  var result = {};
+  contacts.forEach(function(contact) {
+    contact.emails.forEach(function(email) {
+      email = email.toLowerCase();
+
+      var a = result[email];
+      if(!a) {
+        a = [];
+        result[email] = a;
+      }
+
+      a.push(contact);
+    });
+  });
+
+  return result;
+}
 
 exports.ingestGoogleContacts = function(user, data, cb) {
   var contacts = [];
@@ -27,28 +47,56 @@ exports.ingestGoogleContacts = function(user, data, cb) {
 
   winston.verbose('[contact] Importing contacts: ', contacts.length);
 
-  var imported = _.inject(contacts, function(accum, contact) {
-    var newContact = {
-      userId  : user.id,
-      source  : 'google',
-      sourceId: contact.id,
-      name    : contact.name,
-      emails  : contact.emails
-    };
+  var importsByEmail = indexImportsByEmail(contacts);
+  var emails = Object.keys(importsByEmail);
 
-    persistence.Contact.findOneAndUpdate({sourceId: newContact.sourceId}, {$set: newContact}, {upsert: true}, function(err, contact) {});
+  // Try find any existing users with these email addresses
+  return userService.findByEmailsIndexed(emails)
+    .then(function(usersIndexed) {
 
-    accum.push(newContact);
-    return accum;
-  }, []);
+      // Map contactUserId for any existing contacts
+      emails.forEach(function(importedEmail) {
+        var existingUser = usersIndexed[importedEmail];
+        if(!existingUser) return;
 
-  if (imported.length > 0) {
-    statsService.userUpdate(user, {'import_contacts': 'Google'});
-    statsService.event('import_contacts', {'userId': user.id});
-  }
+        var contacts = importsByEmail[importedEmail];
+        contacts.forEach(function(contact) {
+          contact.contactUserId = existingUser._id;
+        });
+      });
 
+      return contacts;
+    })
+    .then(function(contacts) {
+      // Save the contacts to the DB
+      return Q.all(contacts.map(function(contact) {
+        var newContact = {
+          userId        : user._id,
+          source        : 'google',
+          sourceId      : contact.id,
+          name          : contact.name,
+          emails        : contact.emails
+        };
 
-  cb(null, imported);
+        if(contact.contactUserId) {
+          newContact.contactUserId = contact.contactUserId;
+        }
+
+        return persistence.Contact.findOneAndUpdateQ(
+            { source: newContact.source, sourceId: newContact.sourceId },
+            { $set: newContact },
+            { upsert: true })
+          .thenResolve(newContact);
+      }));
+    })
+    .then(function(imported) {
+      if (imported.length > 0) {
+        statsService.userUpdate(user, {'import_contacts': 'Google'});
+        statsService.event('import_contacts', {'userId': user.id});
+      }
+      return imported;
+    })
+    .nodeify(cb);
 };
 
 exports.find = function(user, pattern, cb) {
