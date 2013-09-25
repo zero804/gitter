8f92065a4dfc0ab2302bb3fc4f0173373c65a2bc
Andy Trevorah <andy@troupe.co>
2013-09-25 17:13:17 +0100

    we no longer escape urls twice (twitter-text expected plaintext urls)

--- public/js/template/helpers/linkify.js
+++ public/js/template/helpers/linkify.js
@@ -1,12 +1,17 @@
 /*jshint strict:true, undef:true, unused:strict, browser:true *//* global define:false */
 define([
+  'underscore',
   'handlebars',
   'twitter-text'
-], function ( Handlebars, TwitterText ) {
+], function ( _, Handlebars, TwitterText ) {
   "use strict";
 
   function linkify(message, urls) {
     if (urls && urls.length) {
+      urls.forEach(function(urlData) {
+        urlData.url = _.unescape(urlData.url);
+      });
+
       message = TwitterText.txt.autoLinkEntities(message, urls, {targetBlank: true, urlClass: 'link'});
     }
 
