ff51336ef6e1b5240f35fbb3dacd2662708a0b45
Andrew Newdigate <andrew@troupe.co>
2013-09-26 12:03:19 +0100

    Native chat app caching chats

---
+++ public/js/components/cache-sync.js
@@ -0,0 +1,73 @@
+/*jshint strict:true, undef:true, unused:strict, browser:true *//* global define:false */
+define([
+  'log!cache-sync',
+  'utils/context'
+], function(log, context) {
+  "use strict";
+  var PLUGIN = "ChatSnapshot";
+
+  function cdvError(callback) {
+    return function(error) {
+      log("Error: " + error, error);
+      return callback && callback(error);
+    };
+  }
+
+  function cdvSuccess(callback) {
+    return function(result) {
+      log('CDV:' + result, result);
+      return callback && callback(null, result);
+    };
+  }
+
+  var slice = Function.prototype.call.bind(Array.prototype.slice);
+
+  function cdvCall(name) {
+    return function() {
+      var callback = arguments[arguments.length - 1];
+      window.cordova.exec(cdvSuccess(callback), cdvError(callback), PLUGIN, name, slice(arguments, 0, -1));
+    };
+  }
+
+  var getChatsForTroupe = cdvCall('getChatsForTroupe');
+  var updateChat = cdvCall('updateChat');
+  var removeChat = cdvCall('removeChat');
+
+  function ChatCacheSync(collection) {
+    var troupeId = context.getTroupeId();
+
+    getChatsForTroupe(troupeId, function(err, result) {
+      if(err || !result) return;
+      log('Cache contains ' + result.length + ' items');
+
+      collection.reset(result, { parse: true });
+    });
+
+    this.onAddChange = function(model) {
+      if(model.id) {
+        updateChat(troupeId, model.id, JSON.stringify(model.toJSON()), null);
+      }
+    };
+
+    this.onRemove = function(model) {
+      if(model.id) {
+        removeChat(troupeId, model.id, null);
+      }
+    };
+
+    collection.on('add change', this.onAddChange);
+    collection.on('remove', this.onRemove);
+  }
+
+  return {
+    install: function(collection) {
+      var cordova = window.cordova;
+      if(cordova) {
+        document.addEventListener("deviceready", function() {
+          new ChatCacheSync(collection);
+        });
+      }
+    }
+  };
+
+});
