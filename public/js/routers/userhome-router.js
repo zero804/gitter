0a525f09b0c7defc80b92cf88abb3f3c88dff692
Andrew Newdigate <andrew@troupe.co>
2013-09-26 17:12:49 +0100

    Fixing merge failure

--- public/js/routers/userhome-router.js
+++ public/js/routers/userhome-router.js
@@ -2,6 +2,7 @@
 define([
   'utils/router',
   'utils/context',
+  'views/base',
   'views/profile/profileView',
   'views/profile/profileEmailView',
   'views/profile/profileAddEmailView',
@@ -9,32 +10,87 @@ define([
   'collections/instances/troupes',
   'collections/useremails',
   'views/shareSearch/shareSearchView',
-  'views/invite/reinviteModal'
-  ], function(Router, context, profileView, profileEmailView, profileAddEmailView, createTroupeView, troupeCollections, UserEmailCollection, shareSearchView, InviteModal) {
+  'views/invite/reinviteModal',
+  'hbs!views/connect/tmpl/connectUserTemplate',
+  'hbs!views/login/tmpl/loginRequestModalView'
+  ], function(Router, context, TroupeViews, profileView, profileEmailView, profileAddEmailView, createTroupeView,
+    troupeCollections, UserEmailCollection, shareSearchView, InviteModal, connectUserTemplate,
+    loginRequestTemplate) {
   "use strict";
 
   // instantiate user email collection
   var userEmailCollection = new UserEmailCollection.UserEmailCollection();
 
+  var InviteSuccessView = TroupeViews.Base.extend({
+    template: connectUserTemplate,
+    getRenderData: function() {
+      var data = JSON.parse(window.localStorage.pendingConnectConfirmation);
+      delete window.localStorage.pendingConnectConfirmation;
+      return data;
+    },
+    afterRender: function() {
+      this.$el.find('.modal-content').hide();
+      this.$el.find('.modal-success').show();
+      this.$el.find('#learn-more-btn').hide();
+    },
+    events: {
+      'click #cancel-button': function(e) {
+        this.remove();
+        if (this.dialog) this.dialog.hide();
+        e.preventDefault();
+      }
+    }
+  });
+
+  var InviteSuccessViewModal = TroupeViews.Modal.extend({
+    view: new InviteSuccessView()
+  });
+
+  var RequestSuccessView = TroupeViews.Base.extend({
+    template: loginRequestTemplate,
+    getRenderData: function() {
+      var data = window.localStorage.pendingRequestConfirmation;
+      delete window.localStorage.pendingRequestConfirmation;
+      return data;
+    },
+    afterRender: function() {
+      this.$el.find('.modal-content').hide();
+      this.$el.find('.modal-success').show();
+    },
+    events: {
+      'click #cancel-button': function(e) {
+        this.remove();
+        if (this.dialog) this.dialog.hide();
+        e.preventDefault();
+      }
+    }
+  });
+
+  var RequestSuccessViewModal = TroupeViews.Modal.extend({
+    view: new RequestSuccessView()
+  });
+
+
+
   var router = Router.extend({
     routes: [
-      { name: "profile",  re: /^profile$/,         viewType: profileView.Modal },
-      { name: "profileEmails",    re: /^profile\/emails$/,        viewType: profileEmailView.Modal, collection: userEmailCollection, skipModelLoad: true },
-      { name: "profileEmailsAdd", re: /^profile\/emails\/add$/,   viewType: profileAddEmailView.Modal, collection: userEmailCollection, skipModelLoad: true },
-      { name: "create",   re: /^create$/,          viewType: createTroupeView.Modal, collection: troupeCollections.troupes,   skipModelLoad: true },
-      { name: "share",    re: /^share$/,           viewType: shareSearchView.Modal },
-      { name: "reinvite", re: /^reinvite\/(\w+)$/, viewType: InviteModal,                  collection: troupeCollections.outgoingConnectionInvites, viewOptions: { overrideContext: true, inviteToConnect: true } },
-      { name: "connect",  re: /^connect$/,         viewType: shareSearchView.Modal, viewOptions: { overrideContext: true, inviteToConnect: true } },
-      { name: "inviteSent", re: /^invitesent$/, viewType: InviteSuccessViewModal, skipModelLoad: true },
-      { name: "joinTroupeRequestSent", re: /^joinrequestsent$/, viewType: RequestSuccessViewModal, skipModelLoad: true },
+    { name: "profile",               re: /^profile$/,                viewType: profileView.Modal },
+    { name: "profileEmails",         re: /^profile\/emails$/,        viewType: profileEmailView.Modal,    collection: userEmailCollection, skipModelLoad: true },
+    { name: "profileEmailsAdd",      re: /^profile\/emails\/add$/,   viewType: profileAddEmailView.Modal, collection: userEmailCollection, skipModelLoad: true },
+    { name: "create",                re: /^create$/,                 viewType: createTroupeView.Modal,    collection: troupeCollections.troupes,   skipModelLoad: true },
+    { name: "share",                 re: /^share$/,                  viewType: shareSearchView.Modal },
+    { name: "reinvite",              re: /^reinvite\/(\w+)$/,        viewType: InviteModal,               collection: troupeCollections.outgoingConnectionInvites, viewOptions: { overrideContext: true, inviteToConnect: true } },
+    { name: "connect",               re: /^connect$/,                viewType: shareSearchView.Modal,     viewOptions: { overrideContext: true, inviteToConnect: true } },
+    { name: "inviteSent",            re: /^invitesent$/,             viewType: InviteSuccessViewModal,    skipModelLoad: true },
+    { name: "joinTroupeRequestSent", re: /^joinrequestsent$/,        viewType: RequestSuccessViewModal,   skipModelLoad: true },
     ],
     regions: []
   });
 
-  if(context.popEvent('password_reset')) {
-    new profileView.Modal({ disableClose: true }).show();
-  }
+if(context.popEvent('password_reset')) {
+  new profileView.Modal({ disableClose: true }).show();
+}
 
-  return router;
+return router;
 
 });
