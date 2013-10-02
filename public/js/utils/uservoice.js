efd7100fc553ec77f5ad5dbcecb89abace15d4d3
Mike Bartlett <mike.bartlett@gmail.com>
2013-10-02 11:34:30 +0100

    user voice installed properly

---
+++ public/js/utils/uservoice.js
@@ -0,0 +1,30 @@
+/*jshint strict:true, undef:true, unused:strict, browser:true *//* global define:false */
+define([
+  'utils/context'
+], function(context) {
+  "use strict";
+
+
+  return {
+    install: function(element, user) {
+      require(['uservoice'],function (UserVoice) {
+        UserVoice.push(['set', {
+          accent_color: '#6aba2e',
+          trigger_color: 'white',
+          target: '#help-icon',
+          smartvote_enabled: false,
+          trigger_background_color: 'rgba(46, 49, 51, 0.6)'
+        }]);
+        // Autoprompt for Satisfaction and SmartVote (only displayed under certain conditions)
+        UserVoice.push(['autoprompt', {}]);
+        UserVoice.push(['addTrigger', element[0], { }]);
+        UserVoice.push(['identify', {
+          email: user.email,
+          name: user.displayName,
+          id: user.id
+        }]);
+      });;
+      
+    }
+  }
+});
