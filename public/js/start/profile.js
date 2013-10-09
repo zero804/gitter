95782665abadb189c8df96d0a5e03f58014c2442
Andrew Newdigate <andrew@troupe.co>
2013-10-09 16:09:15 +0100

    Javascript added

---
+++ public/js/start/profile.js
@@ -0,0 +1,11 @@
+/*jshint strict:true, undef:true, unused:strict, browser:true *//* global define:false */
+define([
+  'jquery'
+], function() {
+  "use strict";
+
+  return function() {
+
+  };
+
+});
--- server/handlers/start.js
+++ server/handlers/start.js
@@ -1,41 +1,44 @@
 /*jshint globalstrict:true, trailing:false, unused:true, node:true */
 "use strict";
 
+var contextGenerator = require('../web/context-generator');
+
+function startPage(template, options) {
+  return function(req, res, next) {
+    contextGenerator.generateMiniContext(req, function(err, troupeContext) {
+      if(err) return next(err);
+
+      options.layout = 'start-template';
+      options.troupeContext = troupeContext;
+      options.page = template.split('/')[1];
+
+      res.render(template, options);
+    });
+  };
+}
 
 module.exports = {
 
     install: function(app) {
       app.get('/start/profile',
-        function(req, res, next) {
-          res.render('start/profile', {
-            title: 'Profile',
-            layout: 'start-template'
-          });
-        });
+        startPage('start/profile', {
+            title: 'Profile'
+          }));
 
       app.get('/start/create',
-        function(req, res, next) {
-          res.render('start/create', {
-            title: 'Create',
-            layout: 'start-template'
-          });
-        });
+        startPage('start/create', {
+            title: 'Create'
+          }));
 
       app.get('/start/invite',
-        function(req, res, next) {
-          res.render('start/invite', {
-            title: 'Invite',
-            layout: 'start-template'
-          });
-        });
+        startPage('start/invite', {
+            title: 'Invite'
+          }));
 
       app.get('/start/finish',
-        function(req, res, next) {
-          res.render('start/finish', {
-            title: 'Finished',
-            layout: 'start-template'
-          });
-        });
+        startPage('start/finish', {
+            title: 'Finished'
+          }));
 
     }
 };
