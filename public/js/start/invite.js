44b28943628c4b7323a8c528a0e483c6f8e4cc70
Andy Trevorah <andy@troupe.co>
2013-10-10 16:23:49 +0100

    /start/invite now invites people to your troupe

--- public/js/start/invite.js
+++ public/js/start/invite.js
@@ -1,10 +1,27 @@
 /*jshint strict:true, undef:true, unused:strict, browser:true *//* global define:false */
 define([
   'jquery'
-], function() {
+], function($) {
   "use strict";
 
   return function() {
+    var hasInvitedSomeone = false;
+    var troupeId = window.location.hash.replace('#', '');
+
+    $('.trpStartInviteButton').on('click', function() {
+      var data = {
+        email: $('input').val()
+      };
+      $.post('/troupes/'+troupeId+'/invites', data, function() {
+        hasInvitedSomeone = true;
+      });
+    });
+
+    $('#next-button').on('click', function() {
+      if(hasInvitedSomeone) {
+        window.location.href = 'finish';
+      }
+    });
 
   };
 });
