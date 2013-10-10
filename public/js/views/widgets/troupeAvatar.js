b5118c478685f44c32cfed6311b8d31c2802294f
Mike Bartlett <mike.bartlett@gmail.com>
2013-10-10 11:41:50 +0100

    new avatar styling. references #969

--- public/js/views/widgets/troupeAvatar.js
+++ public/js/views/widgets/troupeAvatar.js
@@ -18,6 +18,44 @@ define([
       this.listenTo(this.model, 'change:unreadItems', this.unreadItemsChanged);
     },
 
+    hash:function(troupeId) {
+      if(!troupeId) return 0;
+      var result = 0;
+      for(var i = 0; i < troupeId.length; i++) {
+        result += troupeId.charCodeAt(i);
+      }
+
+      return result;
+    },
+
+
+    getRenderData:function () {
+      var that = this;
+      if (!this.model.attributes.oneToOne) {
+        var initials = this.model.attributes.name
+                            .split(/[^\w]/)
+                            .filter(function(f) { return f; })
+                            .map(function(s) { return s.charAt(0); })
+                            .slice(0,2)
+                            .join('')
+                            .toUpperCase();
+        var colours = ['#1abc9c', '#2ecc71', '#9b59b6', '#3498db', '#e67e22', '#e74c3c', '#f1c40f',
+                  '#9b59b6', '#1abc9c'];
+        console.log("Using colour: " + that.hash(this.model.attributes.id) % colours.length);
+        var colour = colours[that.hash(this.model.attributes.id) % colours.length];
+        return {
+          oneToOne: this.model.attributes.oneToOne,
+          initials: initials,
+          colour: colour
+        };
+      } else {
+        return {
+          avatarUrl: this.model.attributes.avatarUrl || "",
+          oneToOne: this.model.attributes.oneToOne
+        };
+      }
+    },
+
     unreadItemsChanged: function() {
       var newUnread = this.model.get('unreadItems') > 0;
       if(this.hasUnread !== newUnread) {
