51cfa98eaf25a9eeda1bc266e60bf19e570965d7
Mike Bartlett <mike.bartlett@gmail.com>
2013-10-11 14:50:24 +0100

    added errors on to profile form

--- public/js/start/profile.js
+++ public/js/start/profile.js
@@ -6,6 +6,40 @@ define([
 ], function($, profile, UsernameView) {
   "use strict";
 
+  function showPasswordError() {
+    $('#password-help').hide();
+    $('#password-error').show();
+    $('#password-area .help').css({ 'opacity' : 1 });
+  }
+
+  function hidePasswordError() {
+    $('#password-error').hide();
+    $('#password-help').show();
+    $('#password-area .help').css({ 'opacity' : 0.8 });
+  }
+
+  function showNameError() {
+    $('#name-error').show();
+    $('#displayname-area .help').css({ 'opacity' : 1 });
+  }
+
+  function hideNameError() {
+    $('#name-error').hide();
+    $('#displayname-area .help').css({ 'opacity' : 0.8 });
+  }
+
+  function showUsernameError() {
+    $('.tip-message, .not-valid-message').hide();
+    $('#username-error').show();
+    $('#username-area .help').css({ 'opacity' : 1 });
+  }
+
+  function hideUsernameError() {
+    $('.tip-message').show();
+    $('#username-error').hide();
+    $('#username-area .help').css({ 'opacity' : 0.8 });
+  }
+
   return function() {
     var profileView = new profile.View({
       el: $('.trpStartContent')
@@ -20,7 +54,22 @@ define([
     });
 
     $('#next-button').on('click', function() {
+      showPasswordError();
+      showNameError();
+      showUsernameError();
       profileView.onFormSubmit();
+    });
+
+    $('#password').on('focus' , function() {
+      hidePasswordError();
+    });
+
+    $('#displayName').on('focus' , function() {
+      hideNameError();
+    });
+
+    $('#username').on('focus' , function() {
+      hideUsernameError();
     });
 
     $('#username').on('focus', function() {
