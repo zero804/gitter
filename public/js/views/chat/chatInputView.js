a630628b513c7b978cb872a45c1034fdfb5b3a3e
Andrew Newdigate <andrew@troupe.co>
2013-10-14 15:09:32 +0100

    Resizing on mobile

--- public/js/views/chat/chatInputView.js
+++ public/js/views/chat/chatInputView.js
@@ -14,7 +14,10 @@ define([
   "use strict";
 
   /** @const */
-  var maxChatHeight = 150;
+  var MAX_CHAT_HEIGHT = 145;
+
+  /** @const */
+  var EXTRA_PADDING = 10;
 
   var ChatInputView = TroupeViews.Base.extend({
     template: template,
@@ -63,47 +66,86 @@ define([
     }
   });
 
-  var ChatInputBoxView = TroupeViews.Base.extend({
+  var ChatCollectionResizer = function(options) {
+    var compact = options.compactView;
+    var rollers = options.rollers;
+    var editMode = options.editMode;
 
+    var el = options.el;
+    var $el = $(el);
+
+    var frameChat = $(compact ? '#content': '#content-wrapper').first();
+
+    this.resetInput = function() {
+      $el.css({ height: '', 'overflow-y': '' });
+
+      var css = {};
+      css[compact ? 'padding-bottom' : 'margin-bottom'] = '';
+      frameChat.css(css);
+      rollers.adjustScroll();
+    };
+
+    this.resizeInput = function() {
+      var scrollHeight = el.scrollHeight;
+      var height = scrollHeight > MAX_CHAT_HEIGHT ? MAX_CHAT_HEIGHT : scrollHeight;
+      var offsetHeight = el.offsetHeight;
+      if(offsetHeight == height) {
+        return;
+      }
+
+      var overflow = height < scrollHeight ? 'scroll' : '';
+      $el.css({ height: height, 'overflow-y': overflow });
+
+      if (!editMode) {
+        var css = {};
+
+        if(compact) {
+          frameChat.css({ 'padding-bottom': (height + EXTRA_PADDING) + 'px'});
+        } else {
+          frameChat.css({ 'margin-bottom': height + 'px'});
+
+        }
+        frameChat.css(css);
+      }
+
+      rollers.adjustScroll();
+      window.setTimeout(function() {
+        rollers.adjustScroll();
+      }, 100);
+    };
+
+  };
+
+  var ChatInputBoxView = TroupeViews.Base.extend({
     events: {
-      "keyup": "detectNewLine",
-      "keydown":  "detectReturn",
+      "keydown":  "onKeyDown",
       "focusout": "onFocusOut"
     },
 
     // pass in the textarea as el for ChatInputBoxView
     // pass in a scroll delegate
     initialize: function(options) {
-      this.rollers = options.rollers;
       this.$el.placeholder();
+
       if(hasScrollBars()) {
         this.$el.addClass("scroller");
       }
-      this.resizeInput();
+
+      this.chatResizer = new ChatCollectionResizer({
+        compactView: this.compactView,
+        el: this.el,
+        editMode: this.options.editMode,
+        rollers: options.rollers
+      });
+
+      this.chatResizer.resetInput();
     },
 
     onFocusOut: function() {
       if (this.compactView) this.send();
     },
 
-    resetInput: function() {
-      this.$el.css({ height: '', 'overflow-y': '' });
-    },
-
-    resizeInput: function() {
-      var scrollHeight = this.el.scrollHeight;
-      var height = scrollHeight > maxChatHeight ? maxChatHeight : scrollHeight;
-      var offsetHeight = this.el.offsetHeight;
-      if(offsetHeight == height) {
-        return;
-      }
-
-      var overflow = height < scrollHeight ? 'scroll' : '';
-      this.$el.css({ height: height, 'overflow-y': overflow });
-      return;
-    },
-
-    detectReturn: function(e) {
+    onKeyDown: function(e) {
       if(e.keyCode == 13 && (!e.ctrlKey && !e.shiftKey) && (!this.$el.val().match(/^\s+$/))) {
         e.stopPropagation();
         e.preventDefault();
@@ -111,15 +153,14 @@ define([
         this.send();
         return;
       }
-
-      this.resizeInput();
+      this.chatResizer.resizeInput();
     },
 
     send: function() {
       this.trigger('save', safeHtml(this.$el.val()));
       $('#chatInputForm').trigger('reset');
       this.$el.val('');
-      this.resetInput();
+      this.chatResizer.resetInput();
     }
   });
 
