148d319e660251125f623d0d7f64d3d3b518b79e
Mike Bartlett <mike.bartlett@gmail.com>
2013-10-14 17:01:51 +0100

    fixed problem with goSquared tracking

--- public/js/utils/tracking.js
+++ public/js/utils/tracking.js
@@ -42,17 +42,12 @@ define([
     var GoSquared = {};
     GoSquared.acct = goSquaredTrackingId;
     (function(w){
-      function gs(){
         w._gstc_lt = +new Date;
         var d = document, g = d.createElement("script");
         g.type = "text/javascript";
         g.src = "//d1l6p2sc9645hc.cloudfront.net/tracker.js";
         var s = d.getElementsByTagName("script")[0];
         s.parentNode.insertBefore(g, s);
-      }
-      w.addEventListener ?
-        w.addEventListener("load", gs, false) :
-        w.attachEvent("onload", gs);
     })(window);
   }
 
