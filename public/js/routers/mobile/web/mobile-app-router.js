87a3313eb5d85e8cfa09cc9418b0f21f89537506
Andrew Newdigate <andrew@troupe.co>
2013-10-14 16:07:38 +0100

    JS errors in mobile router fixed

--- public/js/routers/mobile/web/mobile-app-router.js
+++ public/js/routers/mobile/web/mobile-app-router.js
@@ -2,15 +2,20 @@
 define([
   'jquery',
   'retina',
+  'collections/chat',
+  'views/chat/chatInputView',
   'utils/mobile-resizer',
   'routers/mobile/mobile-router',
   'views/toolbar/troupeMenu'
-  ], function($, retina, mobileResizer, MobileRouter, TroupeMenu) {
+  ], function($, retina, chatModels, chatInputView, mobileResizer, MobileRouter, TroupeMenu) {
   "use strict";
 
   var MobileAppRouter = MobileRouter.extend({
     initialize: function() {
       this.constructor.__super__.initialize.apply(this);
+
+      var chatCollection = new chatModels.ChatCollection();
+      chatCollection.listen();
 
       new chatInputView.ChatInputView({
         el: $('#chat-input'),
