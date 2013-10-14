962912771153dc88fc7a8d3650ca2f8f7503429e
Mike Bartlett <mike.bartlett@gmail.com>
2013-10-14 16:32:12 +0100

    another attempt at goSquared tracking

--- public/js/utils/tracking.js
+++ public/js/utils/tracking.js
@@ -37,24 +37,24 @@ define([
 
   }
 
-  var goSquaredTrackingId = context.env('goSquaredTrackingId');
-  if(goSquaredTrackingId) {
-    var GoSquared = {};
-    GoSquared.acct = goSquaredTrackingId;
-    (function(w){
-      function gs(){
-        w._gstc_lt = +new Date;
-        var d = document, g = d.createElement("script");
-        g.type = "text/javascript";
-        g.src = "//d1l6p2sc9645hc.cloudfront.net/tracker.js";
-        var s = d.getElementsByTagName("script")[0];
-        s.parentNode.insertBefore(g, s);
-      }
-      w.addEventListener ?
-        w.addEventListener("load", gs, false) :
-        w.attachEvent("onload", gs);
-    })(window);
-  }
+  // var goSquaredTrackingId = context.env('goSquaredTrackingId');
+  // if(goSquaredTrackingId) {
+  //   var GoSquared = {};
+  //   GoSquared.acct = goSquaredTrackingId;
+  //   (function(w){
+  //     function gs(){
+  //       w._gstc_lt = +new Date;
+  //       var d = document, g = d.createElement("script");
+  //       g.type = "text/javascript";
+  //       g.src = "//d1l6p2sc9645hc.cloudfront.net/tracker.js";
+  //       var s = d.getElementsByTagName("script")[0];
+  //       s.parentNode.insertBefore(g, s);
+  //     }
+  //     w.addEventListener ?
+  //       w.addEventListener("load", gs, false) :
+  //       w.attachEvent("onload", gs);
+  //   })(window);
+  // }
 
   function trackPageView(routeName) {
     if(window.mixpanel) {
