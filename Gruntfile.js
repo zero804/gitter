a3ab3a23d7e96438baba9b57f1b1d580ccc83f51
Andrew Newdigate <andrew@troupe.co>
2013-10-15 12:21:32 +0100

    jQuery 2.0.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -455,15 +455,6 @@ module.exports = function( grunt ) {
         modules: {},
         exports: 'expect'
       },
-      'jquery-migrate': {
-        files : {
-          'output/client-libs/jquery/jquery-migrate-amd.js': ['output/client-libs/jquery/jquery-migrate' + (min ? '.min' : '') + '.js']
-        },
-        modules: {
-          'jquery': 'jQuery'
-        },
-        exports: 'jQuery'
-      },
       'jquery.validation':  {
         files : {
           'output/client-libs/jquery.validation/jquery.validate-amd.js': ['output/client-libs/jquery.validation/jquery.validate.js']
@@ -501,26 +492,6 @@ module.exports = function( grunt ) {
         exports: 'ScrollFix'
       },
 
-      'bootstrap_tooltip': {
-        files : {
-          'output/client-libs/bootstrap/bootstrap-tooltip.js': ['output/client-libs/bootstrap/js/bootstrap-tooltip.js']
-        },
-        modules: {
-          'jquery': 'jQuery'
-        },
-        exports: 'jQuery'
-      },
-
-      'typeahead': {
-        files : {
-          'output/client-libs/typeahead.js/typeahead.js': ['output/client-libs/typeahead.js/dist/typeahead' + (min ? '.min' : '') + '.js']
-        },
-        modules: {
-          'jquery': 'jQuery'
-        },
-        exports: 'jQuery'
-      },
-
       'bootstrap-typeahead': {
         files : {
           'output/client-libs/bootstrap/bootstrap-typeahead.js': ['output/client-libs/bootstrap/js/bootstrap-typeahead.js']
@@ -529,16 +500,6 @@ module.exports = function( grunt ) {
           'jquery': 'jQuery'
         },
         exports: 'jQuery'
-      },
-
-      'zepto': {
-        files : {
-          'output/client-libs/zepto/zepto-amd.js': ['output/client-libs/zepto/zepto.min.js']
-        },
-        modules: {
-
-        },
-        exports: 'Zepto'
       },
 
       'zeroclipboard': {
