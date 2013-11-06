2044164f77bdaba35d1b695a8eefb365150fc4fb
Andrew Newdigate <andrew@troupe.co>
2013-11-06 17:26:47 +0000

    Invite acceptance bug

--- server/resources/user/connectioninvites.js
+++ server/resources/user/connectioninvites.js
@@ -6,36 +6,37 @@ var restSerializer = require("../../serializers/rest-serializer");
 var winston = require('winston');
 
 module.exports = {
-    // Outgoing connection invites.
-    index: function(req, res, next) {
-      inviteService.findAllUnusedConnectionInvitesFromUserId(req.user.id, function(err, invites) {
+  id: 'userConnectionInvite',
+  // Outgoing connection invites.
+  index: function(req, res, next) {
+    inviteService.findAllUnusedConnectionInvitesFromUserId(req.user.id, function(err, invites) {
+      if(err) return next(err);
+
+      var strategy = new restSerializer.InviteStrategy({});
+
+      restSerializer.serialize(invites, strategy, function(err, serialized) {
         if(err) return next(err);
 
-        var strategy = new restSerializer.InviteStrategy({});
-
-        restSerializer.serialize(invites, strategy, function(err, serialized) {
-          if(err) return next(err);
-
-          res.send(serialized);
-        });
-
+        res.send(serialized);
       });
-    },
 
-    destroy: function(req, res, next) {
-      // Delete an invite created by this user
-      req.connectioninvite.remove(function(err) {
-        if(err) {
-          winston.error("Unable to remove connection invite", { exception: err });
-          return next(err);
-        }
+    });
+  },
 
-        res.send({ success: true });
-      });
-    },
+  destroy: function(req, res, next) {
+    // Delete an invite created by this user
+    req.userConnectionInvite.remove(function(err) {
+      if(err) {
+        winston.error("Unable to remove connection invite", { exception: err });
+        return next(err);
+      }
 
-    load: function(req, id, callback) {
-      inviteService.findInviteForUserById(req.user.id, id, callback);
-    }
+      res.send({ success: true });
+    });
+  },
+
+  load: function(req, id, callback) {
+    inviteService.findInviteForUserById(req.resourceUser.id, id, callback);
+  }
 
 };
