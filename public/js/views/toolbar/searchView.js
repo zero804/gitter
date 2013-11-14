8e8198b89df859c573b52cd95fe812c88ebce927
Mike Bartlett <mike.bartlett@gmail.com>
2013-11-14 11:44:03 +0000

    fully wired up search

--- public/js/views/toolbar/searchView.js
+++ public/js/views/toolbar/searchView.js
@@ -22,6 +22,7 @@ define([
       // listen for keypresses on the input
       var self = this;
       this.$input.on('keyup', function(e) {
+        $(window).trigger('showSearch');
         return self.keyPress(e);
       });
     },
@@ -31,6 +32,8 @@ define([
 
       query = query.toLowerCase().trim();
 
+      if (query === '') 
+        $(window).trigger('hideSearch');
       // don't do anything if the search term hasn't changed
       if (query === this.query)
         return;
