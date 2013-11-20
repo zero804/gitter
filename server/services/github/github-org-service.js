df6741c368afe4d48d632ff611bdb621dd6f7be3
Andrew Newdigate <andrew@troupe.co>
2013-11-20 13:40:56 +0000

    github org service

---
+++ server/services/github/github-org-service.js
@@ -0,0 +1,34 @@
+/*jshint globalstrict:true, trailing:false, unused:true, node:true */
+"use strict";
+
+var github = require('octonode');
+var publicClient = github.client();
+var Q = require('q');
+
+function GitHubOrgService(user) {
+  this.user = user;
+  this.client = user ? github.client(user.githubAccessToken) : publicClient;
+}
+
+/**
+ * Returns the list of users with publisized membership to an organisation
+ */
+GitHubOrgService.prototype.members = function(org) {
+  var ghorg  = this.client.org(org);
+  var d = Q.defer();
+  ghorg.members(d.makeNodeResolver());
+  return d.promise;
+};
+
+GitHubOrgService.prototype.member = function(org, username) {
+  var ghorg  = this.client.org(org);
+  var d = Q.defer();
+  ghorg.member(username, d.makeNodeResolver());
+  return d.promise
+    .fail(function(err) {
+      if(err.statusCode === 404) return false;
+      throw err;
+    });
+};
+
+module.exports = GitHubOrgService;
