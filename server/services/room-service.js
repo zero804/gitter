1acf1869ed667f0f20d01ce95de9520ff2b6e8ac
Andrew Newdigate <andrew@troupe.co>
2013-11-20 15:10:13 +0000

    Room service sir?

--- server/services/room-service.js
+++ server/services/room-service.js
@@ -3,6 +3,7 @@
 
 var persistence = require('./persistence-service');
 var assert = require("assert");
+var ObjectID = require('mongodb').ObjectID;
 
 function findOrCreateRoom(options) {
   assert(options);
@@ -11,17 +12,30 @@ function findOrCreateRoom(options) {
 
   var uri = options.uri;
   var githubType = options.githubType;
+  var user = options.user;
+
+
+  var users = user ? [{ _id: new ObjectID(), userId: user.id }] : [];
 
   return persistence.Troupe.findOneAndUpdateQ(
     { uri: uri, githubType: githubType },
     {
       $setOnInsert: {
         uri: uri,
-        githubType: githubType
+        githubType: githubType,
+        users: users
       }
     },
     {
       upsert: true
+    })
+    .then(function(troupe) {
+      if(!user) return troupe;
+
+      if(troupe.containsUserId(user.id)) return troupe;
+
+      troupe.addUserById(user.id);
+      return troupe.saveQ().thenResolve(troupe);
     });
 
 }
