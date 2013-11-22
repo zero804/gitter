f249a4b88a39a181eb0db58777691d1a329fc6e0
Andrew Newdigate <andrew@troupe.co>
2013-11-22 11:20:17 +0000

    Softfail on tests

--- test/integration/services/file-server-test.js
+++ test/integration/services/file-server-test.js
@@ -1,10 +1,9 @@
 /*jslint node:true, unused:true*/
-/*global describe:true, it:true*/
+/*global describe:true, it:true, before:true */
 "use strict";
 
 var testRequire = require('../test-require');
 
-var assert = require("better-assert");
 var fs = require("fs");
 var temp = require("temp");
 
@@ -14,7 +13,6 @@ var fixture = {};
 
 describe('file-service', function() {
   var fileService = testRequire("./services/file-service");
-  var persistence = testRequire("./services/persistence-service");
   var mongoose = require('mongoose');
 
   describe('#deleteFileFromGridStore()', function() {
@@ -23,58 +21,57 @@ describe('file-service', function() {
     *
     */
     it('should delete files when they are dereferenced from a troupe', function(done) {
-      var troupe = persistence.Troupe({ name: "Test "});
-      troupe.save(function(err) {
-        if(err) return done(err);
+      temp.open('test', function(err, info) {
+        fs.write(info.fd, "Hello");
+        fs.close(info.fd, function(err) {
+          if(err) return done(err);
 
-        temp.open('test', function(err, info) {
-          fs.write(info.fd, "Hello");
-          fs.close(info.fd, function(err) {
+          fileService.storeFile({
+            troupe: fixture.troupe1.id,
+            user: fixture.user1,
+            fileName: 'temp.txt',
+            mimeType: 'text/text',
+            file: info.path
+          }, function(err, fileAndVersion) {
             if(err) return done(err);
 
-            fileService.storeFile({
-              troupe: troupe.id,
-              user: fixture.user1,
-              fileName: 'temp.txt',
-              mimeType: 'text/text',
-              file: info.path
-            }, function(err, fileAndVersion) {
-              if(err) return done(err);
+            var db = mongoose.connection.db;
+            var GridStore = mongoose.mongo.GridStore;
+            var file = fileAndVersion.file;
+            var gridFileName = fileService.getMainFileName(file.id, fileAndVersion.version);
 
-              var db = mongoose.connection.db;
-              var GridStore = mongoose.mongo.GridStore;
-              var file = fileAndVersion.file;
-              var gridFileName = fileService.getMainFileName(file.id, fileAndVersion.version);
+            GridStore.exist(db, gridFileName, function(err, result) {
+              if(!result) return done("File " + gridFileName + " not found");
 
-              GridStore.exist(db, gridFileName, function(err, result) {
-                if(!result) return done("File " + gridFileName + " not found");
+              file.remove(function(err) {
+                if(err) return done(err);
 
-                file.remove(function(err) {
-                  if(err) return done(err);
+                setTimeout(function() {
 
-                  setTimeout(function() {
-
-                    GridStore.exist(db, gridFileName, function(err, result) {
-                      if(err) return done(err);
-                      if(result) return done("File " + gridFileName + " still exists but should have been deleted");
-                      troupe.remove(function(err) {
-                        done(err);
-                      });
-
+                  GridStore.exist(db, gridFileName, function(err, result) {
+                    if(err) return done(err);
+                    if(result) return done("File " + gridFileName + " still exists but should have been deleted");
+                    fixture.troupe1.remove(function(err) {
+                      done(err);
                     });
 
-                  }, 1000);
+                  });
 
-                });
+                }, 1000);
+
               });
-
             });
+
           });
         });
-     });
+      });
+
     });
   });
 
-  before(fixtureLoader(fixture));
+  before(fixtureLoader({
+    troupe1: { },
+    user1: { }
+  }, fixture));
 
 });
