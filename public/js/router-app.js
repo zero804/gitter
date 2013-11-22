49cca8a58a09460df8a40225f76e6b0a9fe17614
Andrew Newdigate <andrew@troupe.co>
2013-11-22 21:04:29 +0000

    Removing bits from the app router

--- public/js/router-app.js
+++ public/js/router-app.js
@@ -8,25 +8,25 @@ require([
   'views/chat/chatCollectionView',
   'collections/instances/integrated-items',
   'collections/instances/troupes',
-  'collections/useremails',
+  // 'collections/useremails',
   'views/righttoolbar/rightToolbarView',
   'views/file/filePreviewView',
   'views/file/fileVersionsView',
-  'views/request/requestDetailView',
+  // 'views/request/requestDetailView',
   'views/invite/inviteDetailView',
   'views/people/personDetailView',
   'views/conversation/conversationDetailView',
-  'views/profile/profileView',
-  'views/profile/profileEmailView',
-  'views/profile/profileAddEmailView',
-  'views/modals/completeYourProfileModal',
+  // 'views/profile/profileView',
+  // 'views/profile/profileEmailView',
+  // 'views/profile/profileAddEmailView',
+  // 'views/modals/completeYourProfileModal',
   'views/shareSearch/shareSearchView',
   'views/signup/createTroupeView',
-  'views/signup/usernameView',
+  // 'views/signup/usernameView',
   'views/app/troupeSettingsView',
   'views/app/integrationSettingsModal',
   'views/toolbar/troupeMenu',
-  'views/invite/reinviteModal',
+  // 'views/invite/reinviteModal',
   'utils/router',
   'components/unread-items-client',
 
@@ -44,13 +44,15 @@ require([
   'template/helpers/all', // No ref
   'components/eyeballs', // No ref
   'bootstrap-dropdown' // No ref
-], function($, Backbone, context, AppIntegratedView, chatInputView, ChatCollectionView,
-            itemCollections, troupeCollections, UserEmailCollection, RightToolbarView,
-            filePreviewView, fileVersionsView, RequestDetailView, InviteDetailView, PersonDetailView,
-            conversationDetailView, profileView, profileEmailView, profileAddEmailView, completeYourProfileModal, shareSearchView,
-            createTroupeView, UsernameView, troupeSettingsView, IntegrationSettingsModal, TroupeMenuView, ReinviteModal, Router,
-            unreadItemsClient, FileDecorator, webhookDecorator, userDecorator,
-            embedDecorator /*, errorReporter , FilteredCollection */) {
+], function($, Backbone, context, AppIntegratedView, chatInputView,
+    ChatCollectionView, itemCollections, troupeCollections, /*UserEmailCollection,*/
+    RightToolbarView, filePreviewView, fileVersionsView, /* RequestDetailView,*/
+    InviteDetailView, PersonDetailView, conversationDetailView, /*profileView,
+    profileEmailView, profileAddEmailView,*/ /* completeYourProfileModal,*/
+    shareSearchView, createTroupeView, /*UsernameView,*/ troupeSettingsView,
+    IntegrationSettingsModal, TroupeMenuView, /* ReinviteModal, */ Router,
+    unreadItemsClient, FileDecorator, webhookDecorator, userDecorator,
+    embedDecorator /*, errorReporter , FilteredCollection */) {
   "use strict";
 
   // Make drop down menus drop down
@@ -74,7 +76,7 @@ require([
   appView.rightToolbarRegion.show(new RightToolbarView());
 
   // instantiate user email collection
-  var userEmailCollection = new UserEmailCollection.UserEmailCollection();
+  // var userEmailCollection = new UserEmailCollection.UserEmailCollection();
 
   // Setup the ChatView
 
@@ -94,27 +96,27 @@ require([
     rollers: chatCollectionView.rollers
   }).render();
 
-  var profileModal = context.getUser().username ? profileView.Modal : completeYourProfileModal;
+  // var profileModal = context.getUser().username ? profileView.Modal : completeYourProfileModal;
 
   new Router({
     routes: [
       { name: "file",             re: /^file\/(\w+)$/,            viewType: filePreviewView.Modal,               collection: itemCollections.files },
-      { name: "request",          re: /^request\/(\w+)$/,         viewType: RequestDetailView.Modal,            collection: itemCollections.requests },
+      // { name: "request",          re: /^request\/(\w+)$/,         viewType: RequestDetailView.Modal,            collection: itemCollections.requests },
       { name: "invite",           re: /^invite\/(\w+)$/,          viewType: InviteDetailView.Modal,             collection: itemCollections.invites },
       { name: "filePreview",      re: /^file\/preview\/(\w+)$/,   viewType: filePreviewView.Modal,        collection: itemCollections.files },
       { name: "fileVersions",     re: /^file\/versions\/(\w+)$/,  viewType: fileVersionsView.Modal,       collection: itemCollections.files },
       { name: "mail",             re: /^mail\/(\w+)$/,            viewType: conversationDetailView.Modal, collection: itemCollections.conversations },
       { name: "person",           re: /^person\/(\w+)$/,          viewType: PersonDetailView.Modal,             collection: itemCollections.users },
 
-      { name: "profile",          re: /^profile$/,                viewType: profileModal },
-      { name: "profileEmails",    re: /^profile\/emails$/,        viewType: profileEmailView.Modal,       collection: userEmailCollection, skipModelLoad: true },
-      { name: "profileEmailsAdd", re: /^profile\/emails\/add$/,   viewType: profileAddEmailView.Modal,    collection: userEmailCollection, skipModelLoad: true },
+      // { name: "profile",          re: /^profile$/,                viewType: profileModal },
+      // { name: "profileEmails",    re: /^profile\/emails$/,        viewType: profileEmailView.Modal,       collection: userEmailCollection, skipModelLoad: true },
+      // { name: "profileEmailsAdd", re: /^profile\/emails\/add$/,   viewType: profileAddEmailView.Modal,    collection: userEmailCollection, skipModelLoad: true },
       { name: "share",            re: /^share$/,                  viewType: shareSearchView.Modal },
       { name: "connect",          re: /^connect$/,                viewType: shareSearchView.Modal,        viewOptions: { overrideContext: true, inviteToConnect: true } },
       { name: "create",           re: /^create$/,                 viewType: createTroupeView.Modal,       collection: troupeCollections.troupes,   skipModelLoad: true },
-      { name: "upgradeOneToOne",  re: /^upgradeOneToOne$/,        viewType: createTroupeView.Modal,       collection: troupeCollections.troupes,   skipModelLoad: true, viewOptions: { upgradeOneToOne: true } } ,
-      { name: "chooseUsername",   re: /^chooseUsername/,          viewType: UsernameView.Modal },
-      { name: "reinvite",         re: /^reinvite\/(\w+)$/,        viewType: ReinviteModal,                collection: troupeCollections.outgoingConnectionInvites, viewOptions: { overrideContext: true, inviteToConnect: true } },
+      // { name: "upgradeOneToOne",  re: /^upgradeOneToOne$/,        viewType: createTroupeView.Modal,       collection: troupeCollections.troupes,   skipModelLoad: true, viewOptions: { upgradeOneToOne: true } } ,
+      // { name: "chooseUsername",   re: /^chooseUsername/,          viewType: UsernameView.Modal },
+      // { name: "reinvite",         re: /^reinvite\/(\w+)$/,        viewType: ReinviteModal,                collection: troupeCollections.outgoingConnectionInvites, viewOptions: { overrideContext: true, inviteToConnect: true } },
       { name: "troupeSettings",   re: /^troupeSettings/,          viewType: troupeSettingsView },
       { name: "integrations",     re: /^integrations/,            viewType: IntegrationSettingsModal }
     ],
