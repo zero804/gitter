36352f71be561d293b5b2bb172c8194f5fc01190
Andrew Newdigate <andrew@troupe.co>
2013-11-22 18:40:25 +0000

    Moved the integrations around

--- server/handlers/app/index.js
+++ server/handlers/app/index.js
@@ -27,7 +27,7 @@ module.exports = {
 
       // require('./native-redirects').install(app);
       // require('./invites').install(app);
-      // require('./integrations').install(app);
+      require('./integrations').install(app);
 
     }
 };
