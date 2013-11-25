101fa39c63722e91725057287cccecdf9a39194d
Andy Trevorah <andy@troupe.co>
2013-11-25 15:08:27 +0000

    issues are now more like the github api

--- server/resources/troupes/issues.js
+++ server/resources/troupes/issues.js
@@ -5,21 +5,21 @@ module.exports = {
   index: function(req, res) {
     var term = req.query.q || '';
     var matches = [
-      {id:'1111', description:'PC LOAD LETTER'},
-      {id:'1234', description:'No keyboard detected. Press F1 to resume.'},
-      {id:'12', description:'<script>alert();</script>'},
-      {id:'5678', description:'You don’t exist. Go away.'},
-      {id:'223', description:'Not a Typewriter'},
-      {id:'121', description:'I’m Sorry Dave, I’m afraid I can’t do that.'},
-      {id:'122', description:'Really long descriptions are a fantastic way to test how the autocomplete behaves'},
-      {id:'123', description:'EVERYTHING IS BROKEN'},
-      {id:'124', description:'EVERYTHING IS BROKEN'},
-      {id:'125', description:'EVERYTHING IS BROKEN'},
-      {id:'126', description:'EVERYTHING IS BROKEN'},
-      {id:'127', description:'EVERYTHING IS BROKEN'},
-      {id:'128', description:'EVERYTHING IS BROKEN'}
+      {number:12, title:'<script>alert();</script>'},
+      {number:111, title:'PC LOAD LETTER'},
+      {number:113, title:'No keyboard detected. Press F1 to resume.'},
+      {number:121, title:'I’m Sorry Dave, I’m afraid I can’t do that.'},
+      {number:122, title:'Really long descriptions are a fantastic way to test how the autocomplete behaves'},
+      {number:123, title:'EVERYTHING IS BROKEN'},
+      {number:124, title:'EVERYTHING IS BROKEN'},
+      {number:125, title:'EVERYTHING IS BROKEN'},
+      {number:126, title:'EVERYTHING IS BROKEN'},
+      {number:127, title:'EVERYTHING IS BROKEN'},
+      {number:128, title:'EVERYTHING IS BROKEN'},
+      {number:223, title:'Not a Typewriter'},
+      {number:567, title:'You don’t exist. Go away.'}
     ].filter(function(issue) {
-      return issue.id.indexOf(term) === 0;
+      return (''+issue.number).indexOf(term) === 0;
     });
     res.send(matches);
   }
