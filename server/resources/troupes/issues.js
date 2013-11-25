71a9a09db0a8d806be367b5b9957f74434f1e795
Andy Trevorah <andy@troupe.co>
2013-11-25 13:53:03 +0000

    added more issues for testing

--- server/resources/troupes/issues.js
+++ server/resources/troupes/issues.js
@@ -5,14 +5,19 @@ module.exports = {
   index: function(req, res) {
     var term = req.query.q || '';
     var matches = [
-      {
-        id: '1234',
-        description: 'PC LOAD LETTER'
-      },
-      {
-        id: '1235',
-        description: 'No keyboard detected. Press F1 to resume.'
-      }
+      {id:'1111', description:'PC LOAD LETTER'},
+      {id:'1234', description:'No keyboard detected. Press F1 to resume.'},
+      {id:'12', description:'<script>alert();</script>'},
+      {id:'5678', description:'You don’t exist. Go away.'},
+      {id:'223', description:'Not a Typewriter'},
+      {id:'121', description:'I’m Sorry Dave, I’m afraid I can’t do that.'},
+      {id:'122', description:'Really long descriptions are a fantastic way to test how the autocomplete behaves'},
+      {id:'123', description:'EVERYTHING IS BROKEN'},
+      {id:'124', description:'EVERYTHING IS BROKEN'},
+      {id:'125', description:'EVERYTHING IS BROKEN'},
+      {id:'126', description:'EVERYTHING IS BROKEN'},
+      {id:'127', description:'EVERYTHING IS BROKEN'},
+      {id:'128', description:'EVERYTHING IS BROKEN'}
     ].filter(function(issue) {
       return issue.id.indexOf(term) === 0;
     });
