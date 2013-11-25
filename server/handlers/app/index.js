de48f96d680756a1797f06a5402ea86127d2c779
Andy Trevorah <andy@troupe.co>
2013-11-25 12:27:20 +0000

    issue typeahead working with hardcoded data on server side

--- server/handlers/app/index.js
+++ server/handlers/app/index.js
@@ -25,6 +25,23 @@ module.exports = {
           // }
         });
 
+      app.get('/:userOrOrg/issues', function(req, res) {
+        var term = req.query.q || '';
+        var matches = [
+          {
+            id: '1234',
+            description: 'PC LOAD LETTER'
+          },
+          {
+            id: '1235',
+            description: 'No keyboard detected. Press F1 to resume.'
+          }
+        ].filter(function(issue) {
+          return issue.id.indexOf(term) === 0;
+        });
+        res.send(matches);
+      });
+
       // require('./native-redirects').install(app);
       // require('./invites').install(app);
       require('./integrations').install(app);
