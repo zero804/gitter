0c6c40b6ad3ceed1a3a166692e9b6c7ad06b2b8d
Andy Trevorah <andy@troupe.co>
2013-11-26 14:48:50 +0000

    issues now include all open issues

--- server/services/github/github-issue-service.js
+++ server/services/github/github-issue-service.js
@@ -5,6 +5,8 @@ var github = require('octonode');
 var publicClient = github.client();
 var Q = require('q');
 var assert = require('assert');
+var parser = require('parse-links');
+var url = require('url');
 
 function GitHubIssueService(user) {
   assert(!user || user.githubToken, 'User must have a githubToken');
@@ -13,14 +15,52 @@ function GitHubIssueService(user) {
   this.client = user ? github.client(user.githubToken) : publicClient;
 }
 
+function getFirstPage(repo) {
+  return getIssuePage(repo, 1);
+}
+
+function getIssuePage(repo, number) {
+  var d = Q.defer();
+  repo.issues(number, 100, d.makeNodeResolver());
+  return d.promise.then(function(page) {
+    return {
+      issues: page[0],
+      header: page[1]
+    };
+  });
+}
+
+function getLastPageNumber(header) {
+  var links = parser(header.link);
+  var last = url.parse(links.last, true);
+  return last.query.page;
+}
+
 /**
- * Returns a promise of the first 30 issues for a repo
+ * Returns a promise of the open issues for a repo
  */
 GitHubIssueService.prototype.getIssues = function() {
-  var ghrepo = this.client.repo('twbs/bootstrap');
-  var d = Q.defer();
-  ghrepo.issues(d.makeNodeResolver());
-  return d.promise;
+  var repo = this.client.repo('twbs/bootstrap');
+  return getFirstPage(repo)
+    .then(function(page) {
+      var lastPageNumber = getLastPageNumber(page.header);
+      var promises = [page];
+      for (var i = 2; i <= lastPageNumber; i++) {
+        promises.push(getIssuePage(repo, i));
+      }
+      return promises;
+    })
+    .then(function(promises) {
+      return Q.all(promises);
+    })
+    .then(function(pages) {
+      var issues = pages.map(function(page) {
+        return page.issues;
+      }).reduce(function(previousIssues, currentIssues) {
+        return previousIssues.concat(currentIssues);
+      }, []);
+      return issues;
+    });
 };
 
 module.exports = GitHubIssueService;
