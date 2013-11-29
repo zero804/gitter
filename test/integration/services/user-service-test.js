7641c19d2aeb6f412a224fc7e8d37c2b21ca8c4e
Andrew Newdigate <andrew@troupe.co>
2013-11-29 14:40:00 +0000

    User ETL. Some fixes

--- test/integration/services/user-service-test.js
+++ test/integration/services/user-service-test.js
@@ -58,57 +58,6 @@ describe("User Service", function() {
     });
   });
 
-  describe('#findByLogin', function() {
-
-    it('should find testuser by username', function(done) {
-
-      var userService = testRequire("./services/user-service");
-
-      var count = 0;
-      var username = 'testuser1';
-      userService.findByLogin(username, assertions);
-      userService.findByLogin(username.toUpperCase(), assertions);
-
-
-      function assertions(err, user) {
-        assert(user, "A user should have been found");
-        assert(user.username === username,  "Incorrect user found");
-        count += 1;
-        if (count == 2)
-          done();
-      }
-
-    });
-
-
-    it('should find testuser@troupetest.local by email', function(done) {
-      var userService = testRequire("./services/user-service");
-
-      var email = 'testuser@troupetest.local';
-      userService.findByLogin(email, function(err, user) {
-        assert(user, "A user should have been found");
-        assert(user.email === email, "Incorrect user found");
-        done();
-      });
-
-    });
-  });
-
-  //
-  describe('#findUsernameForUserId', function() {
-    it('should findUsernameForUserId correctly', function(done) {
-      var userService = testRequire("./services/user-service");
-      userService.findUsernameForUserId(fixture2.user1.id)
-        .then(function(username) {
-          assert.equal(username, fixture2.user1.username);
-        })
-        .nodeify(done);
-    });
-
-  });
-
-
-
 
   it('should allow two users with the same githubId to be created at the same moment, but only create a single account', function(done) {
     var userService = testRequire("./services/user-service");
