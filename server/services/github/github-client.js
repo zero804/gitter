ea156974c01e049a20f064fb9594401fa5d57089
Andy Trevorah <andy@troupe.co>
2013-12-04 16:23:31 +0000

    overriding the octonode requests

--- server/services/github/github-client.js
+++ server/services/github/github-client.js
@@ -4,11 +4,17 @@
 var github = require('troupe-octonode');
 var assert = require('assert');
 var winston = require('winston');
+var request = require('request');
+
+function requestWrapper(options, callback) {
+  winston.info('github request made', options);
+  request(options, callback);
+}
 
 function createClient(user) {
   assert(user && user.githubToken, 'User must have a githubToken');
 
-  var client = github.client(user.githubToken);
+  var client = github.client(user.githubToken, requestWrapper);
   // client.middlewares.push(function(err, res, body, next) {
   //   if(res.statusCode >= 400 && res.statusCode != 404) {
   //     winston.error("Github failure", res.headers);
