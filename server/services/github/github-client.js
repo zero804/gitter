6d17eae772a696088a40b4ef0ae0d278dc92c982
Andrew Newdigate <andrew@troupe.co>
2013-12-05 15:42:06 +0000

    Hotfix logging failures

--- server/services/github/github-client.js
+++ server/services/github/github-client.js
@@ -5,11 +5,14 @@ var github = require('troupe-octonode');
 var assert = require('assert');
 var request = require('request');
 var fetchAllPages = require('./fetch-all-pages');
+var logFailingRequest = require('./log-failing-request');
+
 
 function createClient(user) {
   assert(user && user.githubToken, 'User must have a githubToken');
 
-  var client = github.client(user.githubToken, fetchAllPages(request));
+  // var client = github.client(user.githubToken, fetchAllPages(request));
+  var client = github.client(user.githubToken, logFailingRequest(request));
   return client;
 }
 
