76efd604df69fe7bd3ff9ddc747d6fb87cac0b37
Andy Trevorah <andy@troupe.co>
2014-01-08 17:32:07 +0000

    basic browser notifications config

--- public/js/components/desktopNotifications.js
+++ public/js/components/desktopNotifications.js
@@ -1,95 +1,22 @@
 /*jshint strict:true, undef:true, unused:strict, browser:true *//* global define:false */
 define([
   'jquery',
-  'utils/appevents',
   'utils/context',
-  './eyeballs',
   './webNotifications',
-  'hbs!./tmpl/request'
-], function($, appEvents, context, eyeballs, webNotifications, template){
+  'hbs!./tmpl/request',
+  './notifications'
+], function($, context, webNotifications, template, notifications) {
   "use strict";
 
-  if(!context().desktopNotifications) {
-    return;
-  }
-
-  var webkitNotifications = window.webkitNotifications;
-  var Notification = window.Notification;
-
-  function listen() {
-    appEvents.on('user_notification', function(message) {
-      // No notifications while eyeballs on
-      if(eyeballs.getEyeBalls()) return;
-
-      var link = message.link;
-      var title = message.title;
-      var text = message.text;
-      var notification;
-
-      function handleClick() {
-        window.focus();
-        if(link) {
-          if(link == window.location.pathname) return;
-
-          if(link.split('#')[0] == window.location.pathname) {
-            window.location.replace('#' + link.split('#',2)[1]);
-            return;
-          }
-
-          window.location.replace(link);
-
-        }
-      }
-
-      if(Notification) {
-        notification = new Notification(title, { body: text, tag: link, icon: '/images/2/gitter/logo-mark-blue-64.png' });
-        notification.onshow = function() { setTimeout(function() { notification.close(); }, 10000); };
-        notification.onclick = handleClick;
-      } else {
-        notification = webkitNotifications.createNotification('/images/2/gitter/logo-mark-blue-64.png', title, text);
-        notification.onclick = handleClick;
-        notification.show();
-      }
-
-    });
-  }
-
-  function checkPermission() {
-
-    if(Notification && Notification.permission) {
-      return Notification.permission;
-    }
-
-    if(webkitNotifications) {
-      switch(webkitNotifications.checkPermission()) {
-        case 0: return "default";
-        case 1: return "denied";
-        case 2: return "granted";
-      }
-    }
-
-    return;
-  }
-
-
-  function request() {
-    try {
-      window.localStorage.notificationsPrompted = true;
-    } catch(e) {
-    }
-
-    if(webkitNotifications && webkitNotifications.requestPermission) {
-      webkitNotifications.requestPermission(listen);
-    } else if(Notification && Notification.requestPermission) {
-      Notification.requestPermission(listen);
-    }
-  }
+  // if(!context().desktopNotifications) {
+  //   return;
+  // }
 
   function prompt() {
     var e = $(template());
 
     e.find('#enable').on('click', function() {
-      request();
+      notifications.enable();
       webNotifications.notify({
         id: 'request-notifications',
         action: 'hide'
@@ -103,16 +30,10 @@ define([
     });
   }
 
-  switch(checkPermission()) {
-    case "granted": return listen();
-    case "denied":
-      if(!window.localStorage.notificationsPrompted) {
-        prompt();
-      }
-      return;
-
-    case "default": return listen();
+  if(notifications.hasNotBeenSetup()) {
+    prompt();
+  } else if(notifications.hasBeenGranted()) {
+    notifications.enable();
   }
-
 
 });
