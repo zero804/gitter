ed5bf18766c9a6fe4ab4536f248d3b46a17bccdf
Andy Trevorah <andy@troupe.co>
2014-01-13 12:07:36 +0000

    roster now shows number of remaining users

--- public/js/views/people/peopleCollectionView.js
+++ public/js/views/people/peopleCollectionView.js
@@ -1,11 +1,11 @@
 /*jshint strict:true, undef:true, unused:strict, browser:true *//* global define:false */
 define([
   'marionette',
-  'utils/context',
+  'backbone',
   'views/base',
   'hbs!./tmpl/peopleItemView',
   'hbs!./tmpl/peopleCollectionView'
-], function(Marionette, context, TroupeViews, peopleItemViewTemplate, peopleCollectionViewTemplate) {
+], function(Marionette, backbone, TroupeViews, peopleItemViewTemplate, peopleCollectionViewTemplate) {
   "use strict";
 
   var PeopleItemView = TroupeViews.Base.extend({
@@ -17,20 +17,39 @@ define([
     }
   });
 
+  var RemainingView = backbone.View.extend({
+    initialize: function(options) {
+      this.roster = options.roster;
+      this.users = options.users;
+      this.listenTo(this.roster, 'add remove', this.render);
+      this.listenTo(this.users, 'add remove', this.render);
+    },
+    render: function() {
+      var remainingCount = this.users.length - this.roster.length;
+      this.$el.html('and '+remainingCount+' more');
+      return this;
+    }
+  });
+
   return TroupeViews.Base.extend({
     template: peopleCollectionViewTemplate,
 
-    initialize: function(/*options*/) {
-      this.data = { isOneToOne: window.troupeContext.troupe.oneToOne };
-      this.collectionView = new Marionette.CollectionView({
+    initialize: function(options) {
+      this.rosterView = new Marionette.CollectionView({
         tagName: "span",
-        collection: this.collection,
+        collection: options.rosterCollection,
         itemView: PeopleItemView
+      });
+      this.remainingView = new RemainingView({
+        roster: options.rosterCollection,
+        users: options.userCollection
       });
     },
 
     afterRender: function() {
-      this.$el.find('.frame-people').append(this.collectionView.render().el);
+      this.$el.find('.frame-people')
+        .append(this.rosterView.render().el)
+        .append(this.remainingView.render().el);
     }
 
   });
