abe5941bb6f4c16168f5e7eb4075811e8c3accef
Andy Trevorah <andy@troupe.co>
2014-01-15 17:39:40 +0000

    pulled out DoubleHash

---
+++ public/js/utils/double-hash.js
@@ -0,0 +1,85 @@
+/*jshint strict:true, undef:true, unused:strict, browser:true *//* global define:false */
+define([
+  'underscore'
+], function(_) {
+  "use strict";
+
+  // -----------------------------------------------------
+  // Stores value pairs
+  // -----------------------------------------------------
+
+  var DoubleHash = function() {
+    this._data = {};
+  };
+
+  DoubleHash.prototype = {
+    // Add an item, return true if it did not exist before
+    _add: function(itemType, itemId) {
+      var substore = this._data[itemType];
+      if(!substore) {
+        substore = this._data[itemType] = {};
+      }
+
+      var exists = substore[itemId];
+      if(exists) return false;
+      substore[itemId] = true;
+
+      if(this._onItemAdded) this._onItemAdded(itemType, itemId);
+
+      return true;
+    },
+
+    // Add an item, return true iff it exists
+    _remove: function(itemType, itemId) {
+       var substore = this._data[itemType];
+      if(!substore) return false;
+      var exists = substore[itemId];
+      if(!exists) return false;
+
+      delete substore[itemId];
+
+      if(_.keys(substore).length === 0) {
+        delete this._data[itemType];
+      }
+
+      if(this._onItemRemoved) this._onItemRemoved(itemType, itemId);
+
+      return true;
+    },
+
+    _contains: function(itemType, itemId) {
+      var substore = this._data[itemType];
+      if(!substore) return false;
+      return !!substore[itemId];
+    },
+
+    _count: function() {
+      var types = _.keys(this._data);
+
+      return _.reduce(types, function(memo, itemType) {
+        var ids = _.keys(this._data[itemType]);
+        return memo + ids.length;
+      }, 0, this);
+    },
+
+    _marshall: function() {
+      var self = this;
+      var r = {};
+      var types = _.keys(self._data);
+      _.each(types, function(itemType) {
+        var array = [];
+
+        _.each(_.keys(self._data[itemType]), function(itemId) {
+          array.push(itemId);
+        });
+
+        r[itemType] = array;
+      });
+
+      return r;
+    }
+  };
+
+  return DoubleHash;
+
+});
