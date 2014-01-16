97a054f145ac609bda5dc42560540958ac030fae
Mike Bartlett <mike.bartlett@gmail.com>
2014-01-16 11:12:12 +0000

    mike first pass, needs a bit more

--- public/js/views/toolbar/troupeCollectionView.js
+++ public/js/views/toolbar/troupeCollectionView.js
@@ -1,5 +1,6 @@
 /*jshint strict:true, undef:true, unused:strict, browser:true *//* global define:false */
 define([
+  'jquery',
   'utils/context',
   'marionette',
   'hbs!./tmpl/troupeListItem',
@@ -8,7 +9,7 @@ define([
   'views/base',
   'cocktail',
   'log!troupeCollView'
-], function(context, Marionette, troupeListItemTemplate, appEvents, moment,  TroupeViews, cocktail, log) {
+], function($, context, Marionette, troupeListItemTemplate, appEvents, moment,  TroupeViews, cocktail, log) {
   "use strict";
 
   var createRoom = context.getUser().createRoom;
@@ -26,6 +27,36 @@ define([
       var data = this.model.toJSON();
       data.createRoom = createRoom;
       return data;
+    },
+    setupSortableListGroups: function() {
+      var cancelDrop = false;
+      $("ul.list-favourites").sortable({
+        group: 'mega-list',
+        pullPlaceholder: false,
+        onDrop: function (item, container, _super) {
+          if (!cancelDrop) {
+            if ($(container.el).attr('id') == 'list-favs') {
+              // do whatever else needs to be done to add to favourites and store positions
+              item.addClass("item-fav");
+            }
+            cancelDrop = false;
+          }
+          _super(item, container);
+        },
+        onCancel: function(item, container, _super) {      
+          if ($(container.el).attr('id') == 'list-favs') {
+            // do whatever else needs to be done to remove from favourites and store positions
+            // TODO: at the moment if you remove all items, the UL takes up space and that makes no sense!
+            item.remove();
+            cancelDrop = true;
+          }
+        }
+      });
+      $("ul.list-recents").sortable({
+        group: 'mega-list',
+        pullPlaceholder: false,
+        drop: false,
+      });
     },
     clicked: function() {
       var model = this.model;
