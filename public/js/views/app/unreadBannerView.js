a7fe18f1f8e1b968d2da1badaa472f9971b77c92
Andy Trevorah <andy@troupe.co>
2014-01-17 14:07:49 +0000

    plural banner grammar

--- public/js/views/app/unreadBannerView.js
+++ public/js/views/app/unreadBannerView.js
@@ -12,8 +12,12 @@ define(['backbone'], function(backbone)  {
     },
     render: function() {
       var unreadCount = this.collection.where({unread: true}).length;
+      var message = unreadCount+' unread message';
+      if(unreadCount > 1) {
+        message = message+'s';
+      }
       this.$el.toggleClass('hide', unreadCount === 0);
-      this.$el.html(unreadCount+' unread messages');
+      this.$el.text(message);
     },
     scrollToFirstUnread: function() {
       this.chatCollectionView.scrollToFirstUnread();
