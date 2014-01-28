78483edddf1c2f1614c0bda7b0a5cb76ab5b9fd0
Andrew Newdigate <andrew@troupe.co>
2014-01-28 15:20:55 +0000

    Removing unused notify stuff

--- server/services/room-service.js
+++ server/services/room-service.js
@@ -126,8 +126,7 @@ function findOrCreateNonOneToOneRoom(user, troupe, uri) {
                 topic: topic || "",
                 users:  user ? [{
                   _id: new ObjectID(),
-                  userId: user._id,
-                  notify: 1 /* The creator of the room always has notify on */
+                  userId: user._id
                 }] : []
               }
             },
@@ -188,21 +187,8 @@ function ensureAccessControl(user, troupe, access) {
       /* In troupe? */
       if(troupe.containsUserId(user.id)) return Q.resolve(troupe);
 
-      return Q.fcall(function() {
-          switch(troupe.githubType) {
-            case 'ORG':
-              return 1;
-            case 'REPO':
-              return determineDefaultNotifyForRoom(user, troupe);
-
-            default:
-              throw new Error('Unknown room type: ' + troupe.githubType);
-          }
-        }).then(function(notify) {
-          troupe.addUserById(user.id, { notify: notify });
-          return troupe.saveQ().thenResolve(troupe);
-        });
-
+      troupe.addUserById(user.id);
+      return troupe.saveQ().thenResolve(troupe);
 
     } else {
       /* No access */
