20eb56b450d429b23e29b3b5000cf1fd020ef3ef
Andy Trevorah <andy@troupe.co>
2014-01-28 18:08:11 +0000

    /api/v1/user/:id/emails -> /api/private/user_email

--- server/api/private/user-email.js
+++ server/api/private/user-email.js
@@ -1,18 +1,18 @@
-/*jshint globalstrict:true, trailing:false, unused:true, node:true */
+/*jshint globalstrict: true, trailing: false, unused: true, node: true */
 "use strict";
 
 var GithubMe  = require("../../services/github/github-me-service");
 
-module.exports = {
-  index: function(req, res, next) {
-    if(!req.user) {
+module.exports = function(req, res, next) {
+  if(!req.user) {
       return next(403);
     }
 
     var user = new GithubMe(req.user);
 
     user.getEmail().then(function(email) {
-      res.send(email ? [email] : []);
+      res.send({
+        email: email
+      });
     }).fail(next);
-  }
 };
