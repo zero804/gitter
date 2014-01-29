22f48effe7a156741e21e25ec616684e0f055591
Mike Bartlett <mike.bartlett@gmail.com>
2014-01-29 14:48:15 +0000

    Revert "broken slideshow"
    This reverts commit 77de6f5c683001c1373ea66c85048053c13736ca.

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,23 +1,15 @@
 /*jshint strict:true, undef:true, unused:strict, browser:true *//*global require:false */
 require([
   'jquery',
-  'jquery-slideshow',
   'retina' //noref
  ],
-  function($,jquerySlideshow) {
+  function($) {
     "use strict";
 
     require([
       'utils/tracking'
     ], function() {
       // No need to do anything here
-    });
-
-    $(document).ready(function(){
-        $('#slideshow').fadeSlideShow({
-          width: 920,
-          height: 611
-        });
     });
 
     $('.scroll-link').click(function(){
