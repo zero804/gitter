d4ac9ff77935564d972455c8bc4bf81e0ec9db46
Andrew Newdigate <andrew@troupe.co>
2014-01-30 11:30:54 +0000

    Remove unread items

--- redis-lua/unread/unread-remove-item.lua
+++ redis-lua/unread/unread-remove-item.lua
@@ -5,13 +5,14 @@ local item_id = ARGV[2];
 
 local key_count = #KEYS/2
 
-local key_type = redis.call("TYPE", key)["ok"]
 
 local updated_badge_count_positions = {}
 
 for i = 1,key_count do
 	local user_troupe_key = KEYS[i]
 	local user_badge_key = KEYS[i + key_count]
+
+	local key_type = redis.call("TYPE", user_troupe_key)["ok"]
 
 	local removed;
 
