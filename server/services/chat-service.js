2df8db3843483794f380339851f02d3be6efc59e
Mike Bartlett <mike.bartlett@gmail.com>
2014-02-03 14:35:54 +0000

    increased chat limit size

--- server/services/chat-service.js
+++ server/services/chat-service.js
@@ -14,7 +14,7 @@ var processChat   = require('../utils/process-chat');
  */
 var VERSION_INITIAL; /* = undefined; All previous versions are null due to a bug */
 var VERSION_SWITCH_TO_SERVER_SIDE_RENDERING = 5;
-var MAX_CHAT_MESSAGE_LENGTH = 2048;
+var MAX_CHAT_MESSAGE_LENGTH = 4096;
 
 var CURRENT_META_DATA_VERSION = VERSION_SWITCH_TO_SERVER_SIDE_RENDERING;
 
