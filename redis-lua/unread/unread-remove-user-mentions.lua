fd1de18292ad8c074b45e9ffc1d5b0c810c1ebdc
Andrew Newdigate <andrew@troupe.co>
2014-02-03 13:49:46 +0000

    Mentions work

--- redis-lua/unread/unread-remove-user-mentions.lua
+++ redis-lua/unread/unread-remove-user-mentions.lua
@@ -6,7 +6,8 @@ local user_mentions_key = KEYS[2]
 local troupe_id = table.remove(ARGV, 1)
 local count = -1;
 
-if redis.call("SREM", user_troupe_mention_key, unpack(ARGV)) > 0 then
+--if redis.call("SREM", user_troupe_mention_key, unpack(ARGV)) > 0 then
+if redis.call("SREM", user_troupe_mention_key, ARGV[1]) > 0 then
   count = redis.call("SCARD", user_troupe_mention_key);
   if count == 0 then
     -- If count is zero, then this user no longer has any mentions in this troupe
