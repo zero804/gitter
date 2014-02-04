0e510993f3e58094e9bf0e8a45a8d077fca5904e
Andy Trevorah <andy@troupe.co>
2014-02-04 16:13:57 +0000

    titlebar logic refactor

--- public/js/components/titlebar.js
+++ public/js/components/titlebar.js
@@ -5,73 +5,68 @@ define([
 ], function($, appEvents) {
   "use strict";
 
-    function TitlebarUpdater () {
-        var self = this;
+  function updateLeftMenuBadge(unreadCount) {
+    $('.unread-count').text(unreadCount);
+  }
 
-        function onTroupeUnreadTotalChange(values) {
-          self._counts = values;
-          self.updateTitlebar(null, values);
+  function updateFavicon(unreadCount) {
+    var image = (unreadCount > 0) ? '/images/2/gitter/favicon5-unread.png' : '/images/2/gitter/favicon5.png';
+    $('#favicon').attr('href', image);
+  }
 
-          function updateBadge(selector, count) {
-            var badge = $(selector);
-            badge.text(count);
-            if(count > 0) {
-              // badge.show();
-              $("#favicon").attr("href","/images/2/gitter/favicon5-unread.png");
-            } else {
-              // badge.hide();
-              $("#favicon").attr("href","/images/2/gitter/favicon5.png");
-            }
-          }
+  function TitlebarUpdater() {
+    var self = this;
 
-          // overall count
-          updateBadge('.unread-count', values.overall);
-        }
+    appEvents.on('troupeUnreadTotalChange', function(values) {
+      self._counts = values;
+      self.updateTitlebar(null, values);
 
-        appEvents.on('troupeUnreadTotalChange', onTroupeUnreadTotalChange);
+      var unreadCount = values.overall;
+      updateLeftMenuBadge(unreadCount);
+      updateFavicon(unreadCount);
+    });
+  }
 
+  TitlebarUpdater.prototype.updateTitlebar = function(name, values) {
+    if(!name) {
+      name = this.name;
+    } else {
+      this.name = name;
     }
 
-    TitlebarUpdater.prototype.updateTitlebar = function(name, values) {
-      if(!name) {
-        name = this.name;
-      } else {
-        this.name = name;
+    if(!values) {
+      values = this._counts;
+    }
+
+    var title = this.getTitlebar(name, values);
+    document.title = title;
+  };
+
+  TitlebarUpdater.prototype.getTitlebar = function(name, counts) {
+    var mainTitle;
+    if (name) {
+      mainTitle = name + " - Gitter";
+    } else {
+      mainTitle = "Gitter";
+    }
+
+    // TODO this isn't working properly when updating the troupe name, need to be able to poll unreadItems count not just accept the event
+    var overall = counts.overall;
+    var current = counts.current;
+    if(overall <= 0) {
+      return mainTitle;
+    }
+
+    if(overall <= 10) {
+      if(current > 0) {
+        return String.fromCharCode(0x2789 + overall) + ' ' + mainTitle;
       }
 
-      if(!values) {
-        values = this._counts;
-      }
+      return String.fromCharCode(0x277F + overall) + ' ' + mainTitle;
+    }
 
-      var title = this.getTitlebar(name, values);
-      document.title = title;
-    };
+    return '[' + overall + '] ' + mainTitle;
+  };
 
-    TitlebarUpdater.prototype.getTitlebar = function(name, counts) {
-      var mainTitle;
-      if (name) {
-        mainTitle = name + " - Gitter";
-      } else {
-        mainTitle = "Gitter";
-      }
-
-      // TODO this isn't working properly when updating the troupe name, need to be able to poll unreadItems count not just accept the event
-      var overall = counts.overall;
-      var current = counts.current;
-      if(overall <= 0) {
-        return mainTitle;
-      }
-
-      if(overall <= 10) {
-        if(current > 0) {
-          return String.fromCharCode(0x2789 + overall) + ' ' + mainTitle;
-        }
-
-        return String.fromCharCode(0x277F + overall) + ' ' + mainTitle;
-      }
-
-      return '[' + overall + '] ' + mainTitle;
-    };
-
-    return TitlebarUpdater;
+  return TitlebarUpdater;
 });
