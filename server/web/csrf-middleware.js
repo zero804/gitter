883aa4b1d3b965bddbee37a2294c30da589c2b3a
Andy Trevorah <andy@troupe.co>
2014-02-06 12:00:21 +0000

    access token is now stored on the session

--- server/web/csrf-middleware.js
+++ server/web/csrf-middleware.js
@@ -1,24 +1,46 @@
 /*jshint globalstrict:true, trailing:false, unused:true, node:true */
 'use strict';
 
+var Q = require('q');
+var winston = require('winston');
+var assert = require('assert');
 var oauthService = require('../services/oauth-service');
 
 module.exports = function(req, res, next){
+  // ignore these methods
+  if ('GET' == req.method || 'HEAD' == req.method || 'OPTIONS' == req.method) return next();
+
   var clientToken = getClientToken(req);
-  
-  oauthService.findOrGenerateWebToken(req.user.id)
+
+  getServerTokenQ(req)
     .then(function(serverToken) {
+      if(!clientToken || clientToken !== serverToken) {
+        return next(403);
+      }
 
-    // ignore these methods
-    if ('GET' == req.method || 'HEAD' == req.method || 'OPTIONS' == req.method) return next();
-
-    if(!clientToken || clientToken !== serverToken) {
-      return next(403);
-    }
-
-    return next();
-  });
+      return next();
+    })
+    .fail(function(err) {
+      winston.err('couldnt get server auth token', err);
+      next(403);
+    });
 };
+
+function getServerTokenQ(req) {
+  return Q.fcall(function() {
+    assert(req.session, 'session is required');
+
+    if(req.session.accessToken) return req.session.accessToken;
+
+    assert(req.user, 'user is required');
+
+    return oauthService.findOrGenerateWebToken(req.user.id)
+      .then(function(serverToken) {
+        req.session.accessToken = serverToken;
+        return req.session.accessToken;
+      });
+  });
+}
 
 function getClientToken(req) {
   return (req.body && req.body.accessToken) ||
