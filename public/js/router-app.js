dd071f78a6cf4b4456d79cec62fdbea9aa71a8ef
Andrew Newdigate <andrew@troupe.co>
2014-02-11 14:43:49 +0000

    Added squash

--- public/js/router-app.js
+++ public/js/router-app.js
@@ -7,11 +7,11 @@ require([
   'collections/instances/troupes',
   'components/titlebar',
   'components/realtime',
-  'views/widgets/preload', // No ref
-  'components/webNotifications', // No ref
-  'components/desktopNotifications', // No ref
-  'components/errorReporter',  // No ref
-  'template/helpers/all', // No ref
+  'views/widgets/preload',            // No ref
+  'components/webNotifications',      // No ref
+  'components/desktopNotifications',  // No ref
+  'template/helpers/all',             // No ref
+  'components/bug-reporting'          // No ref
 ], function(appEvents, context, AppIntegratedView, TroupeMenuView, troupeCollections, TitlebarUpdater, realtime) {
   "use strict";
 
