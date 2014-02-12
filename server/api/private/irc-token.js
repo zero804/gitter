c79e36e08559edc5e4521646b959434ec8d4bcb7
Mauro Pompilio <hackers.are.rockstars@gmail.com>
2014-02-12 18:28:19 +0000

    Add new oauth irc tokens

--- server/api/private/irc-token.js
+++ server/api/private/irc-token.js
@@ -1,13 +1,20 @@
 /*jshint globalstrict:true, trailing:false, unused:true, node:true */
 "use strict";
 
-var oauthService = require('../../services/oauth-service');
+var oauthService    = require('../../services/oauth-service');
+var restSerializer  = require("../../serializers/rest-serializer");
 
 module.exports =  function(req, res, next) {
+  var strategy = new restSerializer.UserStrategy();
 
-  oauthService.findOrGenerateIRCToken(req.user.id, function(err, token) {
-    res.send({token: token});
+  restSerializer.serialize(req.user, strategy, function(err, serialized) {
+    if(err) return next(err);
+
+    oauthService.findOrGenerateIRCToken(req.user.id, function(err, token) {
+      res.send({token: token, user: serialized});
+    });
   });
+
 
 };
 
