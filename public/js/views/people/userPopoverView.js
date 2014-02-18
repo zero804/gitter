942d70f9f8f345115b7f05d71ab7c5dc7c21e459
Andrew Newdigate <andrew@troupe.co>
2014-02-18 14:13:37 +0000

    Continued work

--- public/js/views/people/userPopoverView.js
+++ public/js/views/people/userPopoverView.js
@@ -1,25 +1,38 @@
 /*jshint strict:true, undef:true, unused:strict, browser:true *//* global define:false */
 define([
   'marionette',
+  'backbone',
   'views/popover',
-  'hbs!./tmpl/userPopoverView'
-], function(Marionette, Popover, template) {
+  'hbs!./tmpl/userPopoverView',
+  'utils/momentWrapper'
+], function(Marionette, Backbone, Popover, template, moment) {
   "use strict";
 
   var UserView = Marionette.ItemView.extend({
     template: template,
+    modelEvents: {
+        'change': 'render'
+    },
     serializeData: function() {
       var data = this.model.toJSON();
-      data.avatarUrl = data.avatarUrlMedium + "&s=200";
+      data.avatarUrl = data.avatar_url && data.avatar_url + "&s=200";
+      data.joined = data.created_at && moment(data.created_at).format('LL');
       return data;
     }
   });
 
   var UserPopoverView = Popover.extend({
-
     initialize: function(options) {
+      var ghModel = new Backbone.Model({
+        login: this.model.get('username'),
+        name: this.model.get('displayName')
+      });
+      ghModel.url = '/api/private/gh/users/' + this.model.get('username');
+
+      ghModel.fetch();
+
       Popover.prototype.initialize.apply(this, arguments);
-      this.view = new UserView({ model: this.model, userCollection: options.userCollection });
+      this.view = new UserView({ model: ghModel, userCollection: options.userCollection });
     }
   });
 
