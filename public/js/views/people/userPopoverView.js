ef5f1d49ff2eb94ed68133d502d69ea5ca854e0c
Andrew Newdigate <andrew@troupe.co>
2014-02-18 15:11:52 +0000

    User popover everywhere

--- public/js/views/people/userPopoverView.js
+++ public/js/views/people/userPopoverView.js
@@ -4,30 +4,58 @@ define([
   'backbone',
   'views/popover',
   'hbs!./tmpl/userPopoverView',
+  'utils/appevents',
+  'utils/context',
   'utils/momentWrapper'
-], function(Marionette, Backbone, Popover, template, moment) {
+], function(Marionette, Backbone, Popover, template, appEvents, context, moment) {
   "use strict";
 
   var UserView = Marionette.ItemView.extend({
     template: template,
     modelEvents: {
-        'change': 'render'
+        'change': 'render',
+    },
+    events: {
+      'click #button-onetoone': function() {
+        var username = this.model.get('login');
+        appEvents.trigger('navigation', '/' + username, 'chat', username, this.model.id);
+      }
     },
     serializeData: function() {
       var data = this.model.toJSON();
+      var chatPrivately = data.has_gitter_login && data.login !== context.user().get('username');
+
+      // Special case
+      if(context.inOneToOneTroupeContext()) {
+        if(context.troupe().get('user').username === data.login) {
+          chatPrivately = false;
+        }
+      }
+
       data.avatarUrl = data.avatar_url && data.avatar_url + "&s=200";
       data.joined = data.created_at && moment(data.created_at).format('LL');
+      data.chatPrivately = chatPrivately;
+
       return data;
     }
   });
 
   var UserPopoverView = Popover.extend({
     initialize: function(options) {
+      var username, displayName;
+      if(this.model) {
+        username = this.model.get('username');
+        displayName = this.model.get('displayName');
+      } else {
+        username = options.username;
+        displayName = options.displayName;
+      }
+
       var ghModel = new Backbone.Model({
-        login: this.model.get('username'),
-        name: this.model.get('displayName')
+        login: username,
+        name: displayName
       });
-      ghModel.url = '/api/private/gh/users/' + this.model.get('username');
+      ghModel.url = '/api/private/gh/users/' + username;
 
       ghModel.fetch();
 
