9f27e353ab6597146d63bc93d8e744570482cf80
Andrew Newdigate <andrew@troupe.co>
2014-02-21 11:52:52 +0000

    More XSS safety

--- public/js/views/app/headerView.js
+++ public/js/views/app/headerView.js
@@ -4,7 +4,7 @@ define([
   'utils/context',
   'marionette',
   'hbs!./tmpl/headerViewTemplate',
-  'utils/autolink',
+  'autolink',
   'underscore',
   'components/notifications'
 ], function($, context, Marionette, headerViewTemplate, autolink, _, notifications)  {
@@ -61,11 +61,6 @@ define([
 
     serializeData: function() {
       var troupe = this.model.toJSON();
-      var topic = troupe.topic;
-      if (topic) {
-        var safeTopic = _.escape(topic);
-        topic = autolink(safeTopic);
-      }
 
       return {
         isNativeDesktopApp: context().isNativeDesktopApp,
@@ -73,10 +68,14 @@ define([
         permissions: context().permissions,
         oneToOne: troupe.oneToOne,
         troupeName: troupe.name,
-        troupeTopic: topic,
+        troupeTopic:  troupe.topic,
         troupeUri : troupe.url,
         troupeFavourite: troupe.favourite
       };
+    },
+
+    onRender: function() {
+      autolink(this.el);
     }
   });
 
