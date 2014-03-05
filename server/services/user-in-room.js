c0441a97ba743fd21bb9510f346509817eb77913
Andrew Newdigate <andrew@troupe.co>
2014-03-05 15:40:32 +0000

    Permissions tests

---
+++ server/services/user-in-room.js
@@ -0,0 +1,19 @@
+/*jshint globalstrict:true, trailing:false, unused:true, node:true */
+"use strict";
+
+var persistence = require('./persistence-service');
+
+function userIsInRoom(uri, user) {
+  var lcUri = uri.toLowerCase();
+  return persistence.Troupe.findOneQ({ lcUri: lcUri }, 'users.userId', { lean: true })
+    .then(function(troupe) {
+      if(!troupe) return;
+
+      return troupe.users.some(function(troupeUser) {
+        return troupeUser.userId === user._id;
+      });
+
+    });
+}
+
+module.exports = userIsInRoom;
