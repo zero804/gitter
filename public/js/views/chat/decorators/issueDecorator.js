19b4802a92d2c800ae04c4ab61616b17f365c64d
Andy Trevorah <andy@troupe.co>
2014-03-07 14:33:02 +0000

    issue decorator now has mention and open in github buttons

--- public/js/views/chat/decorators/issueDecorator.js
+++ public/js/views/chat/decorators/issueDecorator.js
@@ -4,24 +4,42 @@ define([
   'jquery',
   'backbone',
   'utils/context',
+  'utils/appevents',
   'views/popover',
   'hbs!./tmpl/issuePopover',
   'hbs!./tmpl/issuePopoverTitle',
-], function($, Backbone, context, Popover, issuePopoverTemplate, issuePopoverTitleTemplate) {
+  'hbs!./tmpl/commitPopoverFooter'
+], function($, Backbone, context, appEvents, Popover, bodyTemplate, titleTemplate, footerTemplate) {
   "use strict";
 
   var BodyView = Backbone.View.extend({
     className: 'issue-popover-body',
     render: function() {
-      this.$el.html(issuePopoverTemplate(this.model.attributes));
+      this.$el.html(bodyTemplate(this.model.attributes));
       return this;
     }
   });
 
   var TitleView = Backbone.View.extend({
     render: function() {
-      this.$el.html(issuePopoverTitleTemplate(this.model.attributes));
+      this.$el.html(titleTemplate(this.model.attributes));
       return this;
+    }
+  });
+
+  var FooterView = Backbone.View.extend({
+    className: 'commit-popover-footer',
+    events: {
+      'click button.mention': 'onMentionClick'
+    },
+    render: function() {
+      this.$el.html(footerTemplate(this.model.attributes));
+      return this;
+    },
+    onMentionClick: function() {
+      var mentionText = this.model.get('repo')+'#'+this.model.get('number');
+      appEvents.trigger('input.append', mentionText);
+      this.parentPopover.hide();
     }
   });
 
@@ -42,6 +60,7 @@ define([
     return new Popover({
       titleView: new TitleView({model: model}),
       view: new BodyView({model: model}),
+      footerView: new FooterView({model: model}),
       targetElement: targetElement,
       placement: 'horizontal'
     });
@@ -94,6 +113,7 @@ define([
 
           var model = new Backbone.Model(issue);
           model.set('date', moment(issue.created_at).format("LLL"));
+          model.set('repo', repo);
 
           $issue.on('click', showPopover);
           $issue.on('mouseover', showPopoverLater);
