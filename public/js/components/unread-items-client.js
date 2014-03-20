61610e6b6f6defe0443a68abb5ec6f7dd0b726fb
Andrew Newdigate <andrew@troupe.co>
2014-03-20 16:04:30 +0000

    Prevent embeds and other third party source failures from generating errors in raven.

--- public/js/components/unread-items-client.js
+++ public/js/components/unread-items-client.js
@@ -6,9 +6,10 @@ define([
   './realtime',
   'log!unread-items-client',
   'backbone',
+  './csrf',
   'utils/appevents',
   'utils/double-hash'
-], function($, _, context, realtime, log, Backbone, appEvents, DoubleHash) {
+], function($, _, context, realtime, log, Backbone, csrf, appEvents, DoubleHash) {
   "use strict";
 
   function limit(fn, context, timeout) {
@@ -205,6 +206,7 @@ define([
         contentType: "application/json",
         data: JSON.stringify(queue),
         async: async,
+        beforeSend: csrf,
         type: "POST",
         global: false,
         success: function() {
