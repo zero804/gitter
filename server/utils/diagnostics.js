6040fcd429867869820a75a0a2de9ddf46bb4e29
Andrew Newdigate <andrew@troupe.co>
2014-03-20 11:05:17 +0000

    Response times to statsd

--- server/utils/diagnostics.js
+++ server/utils/diagnostics.js
@@ -5,15 +5,13 @@ var nconf = require('./config');
 var winston = require('./winston');
 
 if(nconf.get('diagnostics:heapdump')) {
-  process.env.NODE_HEAPDUMP_OPTIONS = 'nosignal';
-
-  var heapdump = require('heapdump');
-
-  process.on('SIGUSR2', function() {
-    var filename = '/tmp/heap.' + Date.now() + '.heapsnapshot';
-    winston.warn('Writing heapsnapshot: ' + filename);
-    heapdump.writeSnapshot(filename);
-  });
+  // process.env.NODE_HEAPDUMP_OPTIONS = 'nosignal';
+  // var heapdump = require('heapdump');
+  // process.on('SIGUSR2', function() {
+  //   var filename = '/tmp/heap.' + Date.now() + '.heapsnapshot';
+  //   winston.warn('Writing heapsnapshot: ' + filename);
+  //   heapdump.writeSnapshot(filename);
+  // });
 
   var memwatch = require('memwatch');
   memwatch.on('leak', function(info) {
