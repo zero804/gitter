fb3939035497ae870727bec016afde315f5893ee
Andrew Newdigate <andrew@troupe.co>
2014-03-20 14:04:58 +0000

    Transformaiton of cache buster urls

--- public/js/components/bug-reporting.js
+++ public/js/components/bug-reporting.js
@@ -11,6 +11,18 @@ define([
       Raven.config(ravenUrl, {
           // # we highly recommend restricting exceptions to a domain in order to filter out clutter
           // whitelistUrls: ['example.com/scripts/']
+          dataCallback: function(data) {
+            try {
+              data.stacktrace.frames.forEach(function(frame) {
+                if(frame.filename) {
+                  frame.filename = frame.filename.replace(/\/_s\/\w+/, '/_s/l/');
+                }
+              });
+            } catch(e) {
+            }
+
+            return data;
+          }
       }).install();
 
       var user = context.user();
