ffbbf211f3111aae50b03aca1ecbf630988fea0c
Andy Trevorah <andy@troupe.co>
2014-03-24 12:44:29 +0000

    added height limit only for gist embeds

--- public/js/views/chat/decorators/embedDecorator.js
+++ public/js/views/chat/decorators/embedDecorator.js
@@ -12,7 +12,15 @@ define([
       if(el.childElementCount === 0 && el.innerText === el.href) {
         oEmbed.parse(el.href, function(embed) {
           if (embed && embed.html) {
-            $(el).after('<div class="embed">' + embed.html + '</div>');
+            var $embed = $(document.createElement('div'));
+            $embed.addClass('embed');
+
+            if(embed.site === 'GitHub Gists') {
+              $embed.addClass('embed-limited');
+            }
+            $embed.html(embed.html);
+
+            $(el).after($embed);
 
             // any iframely iframes will resize once content loads
             $.iframely.registerIframesIn(chatItemView.$el);
