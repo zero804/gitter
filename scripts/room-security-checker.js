b2934daf9171e57eeaddffc2cd25cffec87663ab
Andrew Newdigate <andrew@troupe.co>
2014-04-08 10:52:16 +0100

    Private meta

--- scripts/room-security-checker.js
+++ scripts/room-security-checker.js
@@ -4,18 +4,11 @@
 "use strict";
 
 var persistence = require('../server/services/persistence-service');
-var GithubRepoService = require('../server/services/github/github-repo-service');
-var roomService = require('../server/services/room-service');
+var checkRepoPrivacy = require('../server/services/check-repo-privacy');
 
 var Q = require('q');
 
 var opts = require("nomnom")
-  .option('token', {
-    abbr: 't',
-    default: 'ac5b214ab993ba4901ec023b3c57e4add2dea06b',
-    required: false,
-    help: 'Github token'
-  })
   .option('max', {
     abbr: 'm',
     default: '50',
@@ -31,23 +24,6 @@ function die(error) {
   process.exit(1);
 }
 
-
-function checkRepo(uri) {
-  var repoService = new GithubRepoService({ githubToken: opts.token });
-  return repoService.getRepo(uri)
-    .then(function(repo) {
-      console.log('WHATS UP?', repo && repo.name);
-      if(!repo || repo.private) {
-        return roomService.ensureRepoRoomSecurity(uri, 'PRIVATE');
-      }
-
-      return roomService.ensureRepoRoomSecurity(uri, 'PUBLIC');
-    })
-    .fail(function(err) {
-      throw err;
-    });
-}
-
 persistence.Troupe
   .where('githubType', 'REPO')
   .sort({ dateLastSecurityCheck: 1 })
@@ -55,7 +31,7 @@ persistence.Troupe
   .execQ()
   .then(function(repos) {
     return Q.all(repos.map(function(repo) {
-      return checkRepo(repo.uri);
+      return checkRepoPrivacy(repo.uri);
     }));
   })
   .then(function() {
