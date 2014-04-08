ec1b4e35b6d3e4faeb775b58ae48ea162cae72be
Andrew Newdigate <andrew@troupe.co>
2014-04-08 10:52:16 +0100

    Private meta

--- server/api/private/hooks.js
+++ server/api/private/hooks.js
@@ -5,6 +5,8 @@ var crypto        = require('crypto');
 var winston       = require('../../utils/winston');
 var eventService  = require('../../services/event-service');
 var troupeService = require('../../services/troupe-service');
+var checkRepoPrivacy = require('../../services/check-repo-privacy');
+var winston       = require('../../utils/winston');
 
 var passphrase = 'wyElt0ian8waunt8';
 
@@ -28,6 +30,22 @@ module.exports = function(req, res) {
       if (err) winston.error('Error creating Event: ' + err);
     });
   });
+
+  // This is a bit of a hack, but it's somewhat useful:
+  // check to see whether a repo has been made public
+  if(meta) {
+    var service = meta.service;
+    var event = meta.event;
+    var repo = meta.repo;
+
+    if(service === 'github' && event === 'public' && repo) {
+      /* Do this asynchronously */
+      checkRepoPrivacy(repo)
+        .fail(function(err) {
+          winston.error('Repo privacy check failed: ' + err, { exception: err });
+        });
+    }
+  }
 
   res.send('OK');
 };
