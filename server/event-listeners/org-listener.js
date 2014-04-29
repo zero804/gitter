8f563572929f95fda01376aac4039459f6b4a26b
Mauro Pompilio <hackers.are.rockstars@gmail.com>
2014-04-29 19:06:56 +0100

    Styling

--- server/event-listeners/org-listener.js
+++ server/event-listeners/org-listener.js
@@ -1,22 +1,17 @@
 /*jshint globalstrict:true, trailing:false, unused:true, node:true */
 "use strict";
 
-var appEvents   = require('../app-events');
-var persistenceService = require('../services/persistence-service');
+var appEvents           = require('../app-events');
+var persistenceService  = require('../services/persistence-service');
+
+// This listeners keeps a set of users per org so we can bill them properly.
+// Receives events from the permission-model.
 
 exports.install = function() {
   appEvents.localOnly.onTrackOrgMembership(function(data) {
-    console.log('TRACKING');
-
     return persistenceService.Org.findOneAndUpdateQ(
       { uri: data.uri },
-      {
-        $addToSet: {
-          members: data.userId
-        }
-      },
-      {
-        upsert: true
-      });
+      { $addToSet: { members: data.userId }},
+      { upsert: true });
   });
 };
