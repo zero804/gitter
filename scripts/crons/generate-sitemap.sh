1e0c01c5b351b29b1ce00d9cc0be0e84353e677a
Andrew Newdigate <andrew@troupe.co>
2014-05-01 14:08:15 +0100

    Sitemap changes

--- scripts/crons/generate-sitemap.sh
+++ scripts/crons/generate-sitemap.sh
@@ -5,7 +5,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SITEMAP_FILE=$TMPDIR/gitter-sitemap.xml
 rm -f $SITEMAP_FILE $SITEMAP_FILE.gz
 
-
 $SCRIPT_DIR/../generate-sitemap.js --sitemap $SITEMAP_FILE
 
 gzip -9 $SITEMAP_FILE
