2b992cf9b2f5baa938247b36d5c90ef80e4ce6f2
Andy Trevorah <andy@troupe.co>
2014-05-16 11:57:34 +0100

    removed middlewares that mobile no longer needs

--- server/handlers/app/mobile.js
+++ server/handlers/app/mobile.js
@@ -1,29 +1,15 @@
 /*jshint globalstrict: true, trailing: false, unused: true, node: true */
 "use strict";
 
-var middleware        = require('../../web/middleware');
-var appRender         = require('./render');
-var appMiddleware     = require('./middleware');
-
-var mainFrameMiddlewarePipeline = [
-  appMiddleware.isPhoneMiddleware,
-  middleware.grantAccessForRememberMeTokenMiddleware,
-  function(req, res, next) {
-    appRender.renderMobileAppcacheChat(req, res, next);
-  }
-];
-
-var userhomeMiddlewarePipeline = [
-  appMiddleware.isPhoneMiddleware,
-  middleware.grantAccessForRememberMeTokenMiddleware,
-  function(req, res, next) {
-    appRender.renderMobileUserHomeApp(req, res, next);
-  }
-];
+var appRender = require('./render');
 
 module.exports = {
     install: function(app) {
-      app.get('/mobile/chat', mainFrameMiddlewarePipeline);
-      app.get('/mobile/home', userhomeMiddlewarePipeline);
+      app.get('/mobile/chat', function(req, res, next) {
+        appRender.renderMobileAppcacheChat(req, res, next);
+      });
+      app.get('/mobile/home', function(req, res, next) {
+        appRender.renderMobileUserHomeApp(req, res, next);
+      });
     }
 };
