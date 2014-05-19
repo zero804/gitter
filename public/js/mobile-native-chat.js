52aa28634bb0681cceff12cfa5a9c74a2739228b
Andy Trevorah <andy@troupe.co>
2014-05-19 13:44:04 +0100

    fixed native chat asynchronously fetching non minified source

--- public/js/mobile-native-chat.js
+++ public/js/mobile-native-chat.js
@@ -1,49 +1,33 @@
 /*jshint strict:true, undef:true, unused:strict, browser:true *//* global require:false */
-require(['utils/context'], function(context) {
+require([
+  'jquery',
+  'collections/chat',
+  'views/chat/chatCollectionView',
+  'views/chat/chatInputView',
+  'components/unread-items-client',
+  'views/chat/decorators/emojiDecorator',
+  'components/csrf'                             // No ref
+  ], function($, chatModels, ChatCollectionView, chatInputView, unreadItemsClient, emojiDecorator) {
+
   "use strict";
 
-  // The room id is taken from the hash instead of the url.
-  // This means that the we can use the same url for all rooms, and so
-  // cache one page in the user's browser.
-  var roomId = window.location.hash.split('#')[1] || window.localStorage.lastTroupeId;
+  var chatCollection = new chatModels.ChatCollection();
+  chatCollection.listen();
 
-  if(!roomId) {
-    window.location.href = '/mobile/home';
-  }
+  var chatCollectionView = new ChatCollectionView({
+    el: $('#frame-chat'),
+    collection: chatCollection,
+    decorators: [emojiDecorator]
+  }).render();
 
-  window.localStorage.lastTroupeId = roomId;
-  context.setTroupeId(roomId);
+  unreadItemsClient.monitorViewForUnreadItems($('#content-frame'));
 
-  require([
-    'jquery',
-    'collections/chat',
-    'views/chat/chatCollectionView',
-    'views/chat/chatInputView',
-    'components/unread-items-client',
-    'views/chat/decorators/emojiDecorator',
-    'components/csrf'                             // No ref
-    ], function($, chatModels, ChatCollectionView, chatInputView,
-      unreadItemsClient, emojiDecorator) {
+  new chatInputView.ChatInputView({
+    el: $('#chat-input'),
+    collection: chatCollection,
+    rollers: chatCollectionView.rollers
+  }).render();
 
-    var chatCollection = new chatModels.ChatCollection();
-    chatCollection.listen();
-
-    var chatCollectionView = new ChatCollectionView({
-      el: $('#frame-chat'),
-      collection: chatCollection,
-      decorators: [emojiDecorator]
-    }).render();
-
-    unreadItemsClient.monitorViewForUnreadItems($('#content-frame'));
-
-    new chatInputView.ChatInputView({
-      el: $('#chat-input'),
-      collection: chatCollection,
-      rollers: chatCollectionView.rollers
-    }).render();
-
-    $('html').removeClass('loading');
-
-  });
+  $('html').removeClass('loading');
 
 });
