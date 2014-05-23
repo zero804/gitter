7e502298fb5ec647f789818544fc37d25d3a0c91
Andrew Newdigate <andrew@troupe.co>
2014-05-23 10:35:51 +0100

    Revert "Testing CORS"
    This reverts commit 445254700b0d46a31085f739982fac51f07770f1.

--- server/web/express.js
+++ server/web/express.js
@@ -10,14 +10,12 @@ var expressHbs     = require('express-hbs');
 var path           = require('path');
 var rememberMe     = require('./middlewares/rememberme-middleware');
 var I18n           = require('i18n-2');
-var cors           = require('cors');
 
 // Naughty naughty naught, install some extra methods on the express prototype
 require('./http');
 
-var staticContentDir = path.join(__dirname, '..', '..', config.get('web:staticContent'));
 
-var corsMiddleware = cors({ origin: config.get('web:corsOrigin')});
+var staticContentDir = path.join(__dirname, '..', '..', config.get('web:staticContent'));
 
 module.exports = {
   /**
@@ -60,7 +58,9 @@ module.exports = {
       maxAge: config.get('web:staticContentExpiryDays') * 86400 * 1000
     }));
 
-    app.use(corsMiddleware);
+    if(!config.get("logging:logStaticAccess")) {
+    }
+
     app.use(express.cookieParser());
     app.use(express.urlencoded());
     app.use(express.json());
@@ -127,7 +127,6 @@ module.exports = {
     app.set('trust proxy', true);
 
     app.use(env.middlewares.accessLogger);
-    app.use(corsMiddleware);
 
     app.use(express.urlencoded());
     app.use(express.json());
@@ -146,8 +145,6 @@ module.exports = {
     app.disable('x-powered-by');
     app.set('trust proxy', true);
     app.use(env.middlewares.accessLogger);
-    app.use(cors);
-
     app.use(express.cookieParser());
     app.use(express.urlencoded());
     app.use(express.json());
