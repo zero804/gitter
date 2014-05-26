f9c8b4a7236c251f71f585a7d92714e658300948
Andrew Newdigate <andrew@troupe.co>
2014-05-26 16:54:19 +0100

    Extra output

--- scripts/utils/query-bayeux.js
+++ scripts/utils/query-bayeux.js
@@ -3,80 +3,118 @@
 "use strict";
 
 var shutdown = require('shutdown');
-var opts = require('nomnom').parse();
+var presenceService = require('../../server/services/presence-service');
+var bayeux = require('../../server/web/bayeux');
 var async = require('async');
+var env = require('../../server/utils/env');
 
-var CliOutput = require('./cli-output');
+env.installUncaughtExceptionHandler();
 
-var cliOutput = new CliOutput({
-  socketId: { width: 32 },
-  bayeux: { width: 8 },
-  userId: { width: 24 },
-  troupeId: { width: 24 },
-  eyeballs: { width: 8 },
-  mobile: { width: 8 },
-  createdTime: { width: 25 },
-  clientType: { width: 10 }
-});
+// env.domainWrap(function() {
+  var CliOutput = require('./cli-output');
 
-require('../../server/utils/event-listeners').installLocalEventListeners();
+  var cliOutput = new CliOutput({
+    socketId: { width: 32 },
+    bayeux: { width: 8 },
+    userId: { width: 24 },
+    troupeId: { width: 24 },
+    eyeballs: { width: 8 },
+    mobile: { width: 8 },
+    createdTime: { width: 25 },
+    clientType: { width: 10 }
+  }, {
+    all: { flag: true }
+  });
 
+  var opts = cliOutput.opts;
 
-if(opts._.length === 0) {
-  shutdown.shutdownGracefully();
-} else {
-  var presenceService = require('../../server/services/presence-service');
-  var bayeux = require('../../server/web/bayeux');
-
-  async.parallel([
-      function(cb) {
-        async.parallel(opts._.map(function(socketId) {
-          return function(cb) {
-            bayeux.engine.clientExists(socketId, function(exists) {
-              return cb(null, exists);
-            });
-          };
-        }), function(err, existence) {
-          if(err) return cb(err);
-
-          var result = opts._.reduce(function(result, socketId, index) {
-            result[socketId] = existence[index];
-            return result;
-          }, {});
-
-          return cb(null, result);
+  function checkEngineExistence(socketIds, callback) {
+    async.parallel(socketIds.map(function(socketId) {
+      return function(cb) {
+        bayeux.engine.clientExists(socketId, function(exists) {
+          return cb(null, exists);
         });
-      },
-      function(cb) { presenceService.getSockets(opts._, cb); },
-    ], function(err, results) {
-      if(err) {
-        console.error(err);
-        return shutdown.shutdownGracefully(1);
-      }
-      cliOutput.headers();
+      };
+    }), function(err, existence) {
+      if(err) return callback(err);
 
-      var clientExists = results[0];
-      var sockets = results[1];
+      var result = socketIds.reduce(function(result, socketId, index) {
+        result[socketId] = existence[index];
+        return result;
+      }, {});
 
-      opts._.forEach(function(socketId) {
-        var socketInfo = sockets[socketId];
+      return callback(null, result);
+    });
+  }
 
-        cliOutput.row({
-          socketId: socketId,
-          bayeux: clientExists[socketId] ? 1 : 0,
-          userId: socketInfo && socketInfo.userId,
-          troupeId: socketInfo && socketInfo.troupeId,
-          eyeballs: socketInfo && (socketInfo.eyeballs ? 1 : 0),
-          mobile: socketInfo && (socketInfo.mobile ? 1 : 0),
-          createdTime: socketInfo && socketInfo.createdTime.toISOString(),
-          clientType: socketInfo && socketInfo.clientType
+  function die(err) {
+    console.error(err);
+    shutdown.shutdownGracefully(1);
+  }
+  cliOutput.headers();
+
+  if(opts.all) {
+    presenceService.listActiveSockets(function(err, sockets) {
+      if(err) return die(err);
+
+      var socketIds = sockets.map(function(socketInfo) { return socketInfo.id; });
+      checkEngineExistence(socketIds, function(err, clientExists) {
+        if(err) return die(err);
+        sockets.forEach(function(socket) {
+          cliOutput.row({
+            socketId: socket.id,
+            bayeux: clientExists[socket.id] ? 1 : 0,
+            userId: socket.userId,
+            troupeId: socket.troupeId,
+            eyeballs: socket.eyeballs ? 1 : 0,
+            mobile: socket.mobile ? 1 : 0,
+            createdTime: socket.createdTime && new Date(socket.createdTime).toISOString(),
+            clientType: socket.clientType
+          });
+
         });
+
+        shutdown.shutdownGracefully();
+
+      });
+    });
+
+  } else if(opts._.length === 0) {
+    shutdown.shutdownGracefully();
+  } else {
+    async.parallel([
+        function(cb) {
+          checkEngineExistence(opts._, cb);
+        },
+        function(cb) { presenceService.getSockets(opts._, cb); },
+      ], function(err, results) {
+        if(err) return die(err);
+
+        var clientExists = results[0];
+        var sockets = results[1];
+
+        opts._.forEach(function(socketId) {
+          var socketInfo = sockets[socketId];
+
+          cliOutput.row({
+            socketId: socketId,
+            bayeux: clientExists[socketId] ? 1 : 0,
+            userId: socketInfo && socketInfo.userId,
+            troupeId: socketInfo && socketInfo.troupeId,
+            eyeballs: socketInfo && (socketInfo.eyeballs ? 1 : 0),
+            mobile: socketInfo && (socketInfo.mobile ? 1 : 0),
+            createdTime: socketInfo && socketInfo.createdTime.toISOString(),
+            clientType: socketInfo && socketInfo.clientType
+          });
+
+        });
+
+        return shutdown.shutdownGracefully();
 
       });
 
-      return shutdown.shutdownGracefully();
+  }
 
-    });
+// });
 
-}
 
