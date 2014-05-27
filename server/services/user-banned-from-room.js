cd2c1d8a43e9b819b9f6ffb4776abaeb15737fc8
Andrew Newdigate <andrew@troupe.co>
2014-05-27 11:56:54 +0100

    Enforce ban

---
+++ server/services/user-banned-from-room.js
@@ -0,0 +1,21 @@
+/*jshint globalstrict:true, trailing:false, unused:true, node:true */
+"use strict";
+
+var persistence = require('./persistence-service');
+
+function userIsBannedFromRoom(uri, user) {
+  var lcUri = uri.toLowerCase();
+  return persistence.Troupe.findOneQ({ lcUri: lcUri }, 'bans.userId', { lean: true })
+    .then(function(troupe) {
+      if(!troupe) return;
+
+      var isBannedFromRoom = troupe.bans.some(function(troupeBan) {
+        return "" + troupeBan.userId === "" + user._id;
+      });
+
+      return isBannedFromRoom;
+
+    });
+}
+
+module.exports = userIsBannedFromRoom;
