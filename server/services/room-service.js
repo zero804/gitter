0e5a11fb46d11493322f6721aa0ebb1eafdf1185
Andrew Newdigate <andrew@troupe.co>
2014-05-27 12:06:56 +0100

    Doh

--- server/services/room-service.js
+++ server/services/room-service.js
@@ -819,7 +819,7 @@ function banUserFromRoom(room, username, requestingUser, callback) {
   if(!room) return Q.reject(new StatusError(400, 'Room required')).nodeify(callback);
   if(!username) return Q.reject(new StatusError(400, 'Username required')).nodeify(callback);
   if(!requestingUser) return Q.reject(new StatusError(401, 'Not authenicated')).nodeify(callback);
-  if(requestingUser.username === requestingUser.username) return Q.reject(new StatusError(400, 'Cannot ban yourself')).nodeify(callback);
+  if(requestingUser.username === username) return Q.reject(new StatusError(400, 'Cannot ban yourself')).nodeify(callback);
   if(!canBanInRoom(room)) return Q.reject(new StatusError(404, 'This room does not support bans')).nodeify(callback);
 
   /* Does the requesting user have admin rights to this room? */
