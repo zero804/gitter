fb2b4f9ade49701039ffd2c57524a744e133a8eb
Andrew Newdigate <andrew@troupe.co>
2014-05-29 17:11:45 +0100

    Unhandled error handling

--- public/js/components/bug-reporting.js
+++ public/js/components/bug-reporting.js
@@ -2,7 +2,8 @@
 define([
   'require',
   'utils/context',
-  'utils/appevents'
+  'utils/appevents',
+  'log!errors'
 ], function(require, context, appEvents) {
   "use strict";
 
@@ -10,6 +11,26 @@ define([
 
   function normalise(s) {
     return s.replace(/\/_s\/\w+\//, '/_s/l/');
+  }
+
+  function logUnhandledError(message, filename, lineno, colno, error) {
+    var c = window['con' + 'sole'];
+    if(c) {
+      var m = message || error && error.message || 'Unhandler error';
+      var errorInfo = {
+        message: message,
+        filename: filename,
+        lineno: lineno,
+        colno:colno,
+        error: error
+      };
+
+      if(c.error) {
+        c.error(m, errorInfo);
+      } else if(c.log) {
+        c.log(m, errorInfo);
+      }
+    }
   }
 
   if(ravenUrl) {
@@ -40,6 +61,22 @@ define([
         username: user && user.get('username')
       });
 
+      window.onerror = function (message, filename, lineno, colno, error) {
+        try {
+          logUnhandledError(message, filename, lineno, colno, error);
+          appEvents.trigger('stats.event', 'error');
+
+          if(error instanceof Error) {
+            Raven.captureException(error, { message: message, filename: filename, lineno: lineno, colno: colno });
+          } else {
+            Raven.captureMessage(message, { message: message, filename: filename, lineno: lineno, colno: colno });
+          }
+        } catch(e) {
+          window.onerror = null;
+          throw e;
+        }
+      };
+
       appEvents.on('bugreport', function(description, data) {
         if(description instanceof Error) {
           appEvents.trigger('stats.event', 'error');
@@ -53,6 +90,15 @@ define([
     });
 
   } else {
+    window.onerror = function (message, filename, lineno, colno, error) {
+      try {
+        logUnhandledError(message, filename, lineno, colno, error);
+        appEvents.trigger('stats.event', 'error');
+      } catch(e) {
+        window.onerror = null;
+        throw e;
+      }
+    };
 
     /* No raven here please */
     appEvents.on('bugreport', function(description) {
