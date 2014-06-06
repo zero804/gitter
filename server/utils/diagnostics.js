db57aec937e35878e7f4901535a5c1b4e027f2f7
Andrew Newdigate <andrew@troupe.co>
2014-06-06 10:51:13 +0100

    Fixes nodemon heapdumps

--- server/utils/diagnostics.js
+++ server/utils/diagnostics.js
@@ -4,8 +4,18 @@
 var nconf = require('./config');
 var winston = require('./winston');
 
-/* Heapdump is now always on */
-require('heapdump');
+/* Heapdump and nodemon don't play nicely together */
+
+if(!process.env.NODEMON) {
+  /* Heapdump is now always on */
+  process.env.NODE_HEAPDUMP_OPTIONS = 'nosignal';
+  var heapdump = require('heapdump');
+  process.on('SIGUSR2', function() {
+    var filename = 'heap.' + Date.now() + '.heapsnapshot';
+    winston.warn('Writing heapsnapshot: ' + filename);
+    heapdump.writeSnapshot(filename);
+  });
+}
 
 if(nconf.get('diagnostics:heapdump')) {
   var memwatch = require('memwatch');
