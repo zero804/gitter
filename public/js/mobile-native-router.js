3fed86aedf68b6b8f1c1a6fea75a41b9ed0026f3
Andy Trevorah <andy@troupe.co>
2014-06-06 16:37:18 +0100

    fixup: added file missing from commit

---
+++ public/js/mobile-native-router.js
@@ -0,0 +1,7 @@
+require([], function() {
+  /*
+   * DELETE THIS FILE WHEN MIKE AND RUSSEL HAVE CLEANED OUT THEIR APPCACHES
+   */
+  window.applicationCache.update();
+  window.location.reload();
+});
