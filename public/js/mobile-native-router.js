c561b0cc4b5934dd7feb6d9a7547b6e52392efe4
Andrew Newdigate <andrew@troupe.co>
2014-06-06 17:15:55 +0100

    Safer force appcache

--- public/js/mobile-native-router.js
+++ public/js/mobile-native-router.js
@@ -3,5 +3,10 @@ require([], function() {
    * DELETE THIS FILE WHEN MIKE AND RUSSEL HAVE CLEANED OUT THEIR APPCACHES
    */
   window.applicationCache.update();
-  window.location.reload();
+  window.applicationCache.addEventListener('updateready', function() {
+    applicationCache.swapCache();
+    setTimeout(function() {
+      window.location.reload();
+    }, 10000);
+  }, false);
 });
