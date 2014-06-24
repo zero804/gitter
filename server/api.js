16e47f769fb7bed7240f8fa32a2239cdb3d24c64
Andrew Newdigate <andrew@troupe.co>
2014-06-24 23:14:25 +0100

    Readded the full health check

--- server/api.js
+++ server/api.js
@@ -35,6 +35,8 @@ require('./services/kue-workers').startWorkers();
 // APIS
 require('./api/').install(app, '', require('./web/middlewares/auth-api'));
 
+app.get('/api/private/health_check', require('./api/private/health-check'));
+app.get('/api/private/health_check/full', require('./api/private/health-check-full'));
 app.get('/', function(req, res) {
   res.redirect('https://developer.gitter.im');
 });
