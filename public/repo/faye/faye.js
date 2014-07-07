9d5c6c12d24041329007c2e5a79e61165b7ef721
Andrew Newdigate <andrew@troupe.co>
2014-07-07 16:55:31 +0100

    Debugging of received messages on websockets

--- public/repo/faye/faye.js
+++ public/repo/faye/faye.js
@@ -2344,6 +2344,7 @@ Faye.Transport.WebSocket = Faye.extend(Faye.Class(Faye.Transport, {
     };
 
     socket.onmessage = function(event) {
+      self.debug('Websocket message received');
       var replies = JSON.parse(event.data);
       if (!replies) return;
 
