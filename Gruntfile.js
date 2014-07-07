f3e51c73610fb00af98c2ad5adf5e3f8858dbf29
Andrew Newdigate <andrew@troupe.co>
2014-07-07 22:51:53 +0100

    Fast build option

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,196 @@
+var MODULES = [
+  {
+    name: "core-libraries",
+    include: [
+      "../repo/requirejs/requirejs"
+    ]
+  },
+  {
+      name: "signup",
+      exclude: ["core-libraries"]
+  },
+  {
+      name: "router-app",
+      include: [
+        "utils/tracking",
+        "views/widgets/avatar",
+        "views/widgets/timeago"
+      ],
+      exclude: ["core-libraries"]
+  },
+  {
+      name: "router-nli-app",
+      include: [
+        "utils/tracking",
+        // XXX: why are these commented out?
+        // "views/widgets/avatar",
+        // "views/widgets/timeago"
+      ],
+      exclude: ["core-libraries"]
+  },
+  {
+      name: "router-chat",
+      include: [
+        "views/widgets/avatar",
+        "views/widgets/timeago"
+      ],
+      exclude: ["core-libraries"]
+  },
+  {
+      name: "router-nli-chat",
+      include: [
+        "views/widgets/avatar",
+        "views/widgets/timeago"
+      ],
+      exclude: ["core-libraries"]
+  },
+  {
+      name: "router-archive-home",
+      include: [
+        "views/widgets/avatar",
+        "views/widgets/timeago"
+      ],
+      exclude: ["core-libraries"]
+  },
+  {
+      name: "router-archive-chat",
+      include: [
+        "views/widgets/avatar",
+        "views/widgets/timeago"
+      ],
+      exclude: ["core-libraries"]
+  },
+  {
+      name: "userhome",
+      include: [
+        "utils/tracking",
+        "views/widgets/avatar",
+        "views/widgets/timeago"
+      ],
+      exclude: ["core-libraries"]
+  },
+  {
+      name: "mobile-native-userhome",
+      include: [
+        "utils/tracking",
+        "views/widgets/avatar",
+        "views/widgets/timeago"
+      ],
+      exclude: ["core-libraries"]
+  },
+  {
+      name: "mobile-native-router",
+      include: [
+        "utils/tracking",
+        "views/widgets/avatar",
+        "views/widgets/timeago"
+      ],
+      exclude: ["core-libraries"]
+  },
+  {
+      name: "mobile-native-chat",
+      include: [
+        "utils/tracking",
+        "views/widgets/avatar",
+        "views/widgets/timeago"
+      ],
+      exclude: ["core-libraries"]
+  },
+  {
+      name: "mobile-app",
+      include: [
+        "utils/tracking",
+        "views/widgets/avatar",
+        "views/widgets/timeago"
+      ],
+      exclude: ["core-libraries"]
+  },
+  {
+      name: "mobile-nli-app",
+      include: [
+        "utils/tracking",
+        "views/widgets/avatar",
+        "views/widgets/timeago"
+      ],
+      exclude: ["core-libraries"]
+  },
+  {
+      name: "mobile-userhome",
+      include: [
+        "utils/tracking",
+        "views/widgets/avatar",
+        "views/widgets/timeago"
+      ],
+      exclude: ["core-libraries"]
+  }
+];
+
+var CLOSURE_PATH = 'build-scripts/closure-v20130722';
 
 module.exports = function( grunt ) {
   'use strict';
 
   var min = !grunt.option('disableMinifiedSource');
 
-  function createClosureConfig(name) {
-    return {
-      js: 'public-processed/js/' + name + '.js',
-      jsOutputFile: 'public-processed/js/' + name + '.min.js',
-      closurePath: 'build-scripts/closure-v20130722',
-      reportFile: 'output/' + name.replace(/\//g, '-') + '.report.txt',
-      options: {
-        //compilation_level: 'ADVANCED_OPTIMIZATIONS',
-        //compilation_level: 'WHITESPACE_ONLY',
-        compilation_level: 'SIMPLE_OPTIMIZATIONS',
-        language_in: 'ECMASCRIPT5_STRICT',
-        create_source_map: 'public-processed/js/' + name + '.min.js.map',
-        // define: [
-        // '"DEBUG=false"',
-        // '"UI_DELAY=500"'
-        // ],
-      }
-    };
+  function createClosureConfig(modules) {
+    var config = {};
+
+    var closureModule = grunt.option('closureModule');
+
+    modules.forEach(function(module) {
+      var name = module.name;
+
+      if(closureModule && closureModule !== name + '.js') return;
+
+      config[module.name] = {
+        js: 'public-processed/js/' + name + '.js',
+        jsOutputFile: 'public-processed/js/' + name + '.min.js',
+        closurePath: CLOSURE_PATH,
+        reportFile: 'output/' + name.replace(/\//g, '-') + '.report.txt',
+        options: {
+          compilation_level: 'SIMPLE_OPTIMIZATIONS',
+          language_in: 'ECMASCRIPT5_STRICT',
+          create_source_map: 'public-processed/js/' + name + '.min.js.map',
+        }
+      };
+
+    });
+
+    return config;
+  }
+
+  function compileRequireModules(modules) {
+    var requireConfig = {};
+
+    modules.forEach(function(module) {
+
+      requireConfig[module.name] = {
+        options: {
+          optimize: 'none',
+          generateSourceMaps: true,
+          preserveLicenseComments: false,
+          baseUrl: "public/js",
+          name: module.name,
+          include: module.include,
+          exclude: module.exclude,
+          mainConfigFile: 'public/templates/partials/require_config.hbs',
+          optimizeCss: "none",
+          inlineText: true,
+          out: 'public-processed/js/' + module.name + '.js',
+          pragmasOnSave: {
+            //removes Handlebars.Parser code (used to compile template strings) set
+            //it to `false` if you need to parse template strings even after build
+            //excludeHbsParser : true,
+            // kills the entire plugin set once it's built.
+            //excludeHbs: true,
+            // removes i18n precompiler, handlebars and json2
+            //excludeAfterBuild: true
+          }
+        },
+      };
+    });
+
+    return requireConfig;
   }
 
   //
@@ -35,288 +204,8 @@ module.exports = function( grunt ) {
       all: ["public/**/*.hbs"]
     },
 
-  requirejs: {
-      compile: {
-        options: {
-
-          //optimize: 'uglify2',
-          optimize: 'none',
-          //generateSourceMaps: true,
-          preserveLicenseComments: true,
-          //useSourceUrl: true,
-
-          appDir: "public/",
-          baseUrl: "js",
-          dir: "public-processed/",
-          mainConfigFile: 'public/templates/partials/require_config.hbs',
-          optimizeCss: "none",
-          inlineText: true,
-          pragmasOnSave: {
-              //removes Handlebars.Parser code (used to compile template strings) set
-              //it to `false` if you need to parse template strings even after build
-              //excludeHbsParser : true,
-              // kills the entire plugin set once it's built.
-              //excludeHbs: true,
-              // removes i18n precompiler, handlebars and json2
-              //excludeAfterBuild: true
-          },
-          modules: [
-              {
-                name: "core-libraries",
-                include: [
-                  "../repo/requirejs/requirejs"
-                ]
-              },
-              {
-                  name: "signup",
-                  exclude: ["core-libraries"]
-              },
-              {
-                  name: "router-app",
-                  include: [
-                    "utils/tracking",
-                    "views/widgets/avatar",
-                    "views/widgets/timeago"
-                  ],
-                  exclude: ["core-libraries"]
-              },
-              {
-                  name: "router-nli-app",
-                  include: [
-                    "utils/tracking",
-                    // "views/widgets/avatar",
-                    // "views/widgets/timeago"
-                  ],
-                  exclude: ["core-libraries"]
-              },
-              {
-                  name: "router-chat",
-                  include: [
-                    "views/widgets/avatar",
-                    "views/widgets/timeago"
-                  ],
-                  exclude: ["core-libraries"]
-              },
-              {
-                  name: "router-nli-chat",
-                  include: [
-                    "views/widgets/avatar",
-                    "views/widgets/timeago"
-                  ],
-                  exclude: ["core-libraries"]
-              },
-              {
-                  name: "router-archive-home",
-                  include: [
-                    "views/widgets/avatar",
-                    "views/widgets/timeago"
-                  ],
-                  exclude: ["core-libraries"]
-              },
-              {
-                  name: "router-archive-chat",
-                  include: [
-                    "views/widgets/avatar",
-                    "views/widgets/timeago"
-                  ],
-                  exclude: ["core-libraries"]
-              },
-              {
-                  name: "userhome",
-                  include: [
-                    "utils/tracking",
-                    "views/widgets/avatar",
-                    "views/widgets/timeago"
-                  ],
-                  exclude: ["core-libraries"]
-              },
-              {
-                  name: "mobile-native-userhome",
-                  include: [
-                    "utils/tracking",
-                    "views/widgets/avatar",
-                    "views/widgets/timeago"
-                  ],
-                  exclude: ["core-libraries"]
-              },
-              {
-                  name: "mobile-native-router",
-                  include: [
-                    "utils/tracking",
-                    "views/widgets/avatar",
-                    "views/widgets/timeago"
-                  ],
-                  exclude: ["core-libraries"]
-              },
-              {
-                  name: "mobile-native-chat",
-                  include: [
-                    "utils/tracking",
-                    "views/widgets/avatar",
-                    "views/widgets/timeago"
-                  ],
-                  exclude: ["core-libraries"]
-              },
-              {
-                  name: "mobile-app",
-                  include: [
-                    "utils/tracking",
-                    "views/widgets/avatar",
-                    "views/widgets/timeago"
-                  ],
-                  exclude: ["core-libraries"]
-              },
-              {
-                  name: "mobile-nli-app",
-                  include: [
-                    "utils/tracking",
-                    "views/widgets/avatar",
-                    "views/widgets/timeago"
-                  ],
-                  exclude: ["core-libraries"]
-              },
-              {
-                  name: "mobile-userhome",
-                  include: [
-                    "utils/tracking",
-                    "views/widgets/avatar",
-                    "views/widgets/timeago"
-                  ],
-                  exclude: ["core-libraries"]
-              },/*
-              {
-                  name: "routers/mobile/native/files-router",
-                  include: [
-                    "utils/tracking",
-                    "views/widgets/avatar",
-                    "views/widgets/timeago"
-                  ],
-                  exclude: ["core-libraries"]
-              },
-              {
-                  name: "routers/mobile/native/conversations-router",
-                  include: [
-                    "utils/tracking",
-                    "views/widgets/avatar",
-                    "views/widgets/timeago"
-                  ],
-                  exclude: ["core-libraries"]
-              },
-              {
-                  name: "routers/mobile/native/people-router",
-                  include: [
-                    "utils/tracking",
-                    "views/widgets/avatar",
-                    "views/widgets/timeago"
-                  ],
-                  exclude: ["core-libraries"]
-              },
-              {
-                  name: "routers/mobile/native/accept-router",
-                  include: [
-                    "utils/tracking",
-                    "views/widgets/avatar",
-                    "views/widgets/timeago"
-                  ],
-                  exclude: ["core-libraries"]
-              },
-              {
-                  name: "routers/mobile/native/connect-router",
-                  include: [
-                    "utils/tracking",
-                    "views/widgets/avatar",
-                    "views/widgets/timeago"
-                  ],
-                  exclude: ["core-libraries"]
-              },
-              {
-                  name: "routers/mobile/native/create-troupe-router",
-                  include: [
-                    "utils/tracking",
-                    "views/widgets/avatar",
-                    "views/widgets/timeago"
-                  ],
-                  exclude: ["core-libraries"]
-              },
-              {
-                  name: "complete-profile",
-                  include: [
-                    "utils/tracking",
-                    "views/widgets/avatar"
-                  ],
-                  exclude: ["core-libraries"]
-              },
-              {
-                  name: "login",
-                  exclude: ["core-libraries"]
-              },
-              {
-                  name: "tours/tour-controller",
-                  include: [
-                  ],
-                  exclude: [
-                    "core-libraries",
-                    "router-app"
-                  ]
-              },
-              {
-                  name: "start-app",
-                  include: [
-                    "utils/tracking",
-                    "views/widgets/avatar"
-                  ],
-                  exclude: ["core-libraries"]
-              },*/
-
-          ]
-
-        }
-      }
-    },
-
-    'closure-compiler': {
-      'core-libraries': {
-            js: 'public-processed/js/core-libraries.js',
-            jsOutputFile: 'public-processed/js/core-libraries.min.js',
-            closurePath: 'build-scripts/closure-v20130722',
-            reportFile: 'output/core-libraries.report.txt',
-            options: {
-              //compilation_level: 'WHITESPACE_ONLY',
-              compilation_level: 'SIMPLE_OPTIMIZATIONS',
-              language_in: 'ECMASCRIPT3',
-              create_source_map: 'public-processed/js/core-libraries.min.js.map',
-            }
-          },
-      'signup': createClosureConfig('signup'),
-      'router-app': createClosureConfig('router-app'),
-      'router-nli-app': createClosureConfig('router-nli-app'),
-      'router-chat': createClosureConfig('router-chat'),
-      'router-nli-chat': createClosureConfig('router-nli-chat'),
-      'router-archive-home': createClosureConfig('router-archive-home'),
-      'router-archive-chat': createClosureConfig('router-archive-chat'),
-      'userhome': createClosureConfig('userhome'),
-      "mobile-native-userhome" : createClosureConfig('mobile-native-userhome'),
-      "mobile-native-router": createClosureConfig('mobile-native-router'),
-      "mobile-native-chat": createClosureConfig('mobile-native-chat'),
-      "mobile-app": createClosureConfig('mobile-app'),
-      "mobile-nli-app": createClosureConfig('mobile-nli-app'),
-      "mobile-userhome": createClosureConfig('mobile-userhome'),
-//       "native-files-router": createClosureConfig('routers/mobile/native/files-router'),
-//       "native-conversations-router": createClosureConfig('routers/mobile/native/conversations-router'),
-//       "native-people-router": createClosureConfig('routers/mobile/native/people-router'),
-//       "native-accept-router": createClosureConfig('routers/mobile/native/accept-router'),
-//       "native-connect-router": createClosureConfig('routers/mobile/native/connect-router'),
-//       "native-create-troupe-router": createClosureConfig('routers/mobile/native/create-troupe-router'),
-//       "complete-profile": createClosureConfig('complete-profile'),
-//       "start-app": createClosureConfig('start-app'),
-//       "tour-controller": createClosureConfig('tours/tour-controller'),
-//       "login": createClosureConfig('login')
-    },
-
-    // headless testing through PhantomJS
-    mocha: {
-      all: ['test/**/*.html']
-    },
+    requirejs: compileRequireModules(MODULES),
+    "closure-compiler": createClosureConfig(MODULES),
 
     // default watch configuration
     watch: {
@@ -532,14 +421,6 @@ module.exports = function( grunt ) {
 
   /* using matchdep to load all grunt related modules */
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-  // grunt-contrib-requirejs
-  // grunt-contrib-less
-  // grunt-contrib-watch
-  // grunt-retire
-  // grunt-bower-require-wrapper
-  // grunt-wrap
-  // grunt-closure-compiler
-  // grunt-nsp-shrinkwrap
 
   grunt.registerTask('process', ['less', 'requirejs', 'closure-compiler']);
   grunt.registerTask('process-no-min', ['less', 'requirejs']);
---
+++ build-scripts/selective-js-compile.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+
+generate_md5() {
+  if [[ -x $(which md5) ]]; then
+    md5 -q $1
+  else
+    md5sum $1 |cut -d\  -f1
+  fi
+}
+
+for i in $(find $ROOT_DIR/public-processed/js -name '*.js' ! -name '*.min.js'); do 
+  min_file=${i%.js}.min.js
+  md5_file=$i.md5  
+  md5_checksum=$(generate_md5 $i)
+    
+  if [[ ! -f $md5_file ]] || [[ ! -f $min_file ]] || [[ $md5_checksum != $(cat $md5_file) ]]; then
+    echo $i needs updating >&2
+    
+    rm -f $min_file ${min_file}.gz ${min_file}.map ${min_file}.map.gz
+    grunt closure-compiler --closureModule $(basename $i) &
+    echo $md5_checksum > $md5_file 
+  fi
+done
+
+wait
+
+for i in $(find $ROOT_DIR/public-processed/js -name '*.js' ! -name '*.min.js'); do 
+  min_file=${i%.js}.min.js
+  if [[ ! -f $min_file ]]; then
+    echo Missing $min_file >&2
+    exit 1
+  fi
+done
