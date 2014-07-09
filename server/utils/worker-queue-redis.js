53dd654e9a7bfa2be5a81402d45c77f24c2125e1
Andy Trevorah <andy@troupe.co>
2014-07-09 16:25:51 +0100

    Revert "Merge branch 'feature/badge-batcher-problems' into develop"
    This reverts commit c03668e5a7d1f86cf12fb918ca3bb35726f4d9a4, reversing
    changes made to 570c5b018948cc20be7cb9703784a5d66304b83d.

--- server/utils/worker-queue-redis.js
+++ server/utils/worker-queue-redis.js
@@ -13,10 +13,6 @@ var scheduler = new resque.scheduler({connection: connectionDetails}, function()
   scheduler.start();
 });
 
-scheduler.on('error', function(err){
-  winston.error('scheduler failed: ' + err, { exception: err });
-});
-
 var jobs = {
   echo: {
     perform: function(data, callback) {
@@ -31,51 +27,23 @@ var Queue = function(name, options, loaderFn) {
 
   var self = this;
 
-  this.worker = new resque.worker({connection: connectionDetails, queues: [name]}, jobs, function(){
-    self.worker.workerCleanup();
-    self.worker.start();
+  var worker = new resque.worker({connection: connectionDetails, queues: [name]}, jobs, function(){
+    worker.workerCleanup();
+    worker.start();
   });
 
   this.internalQueue = new resque.queue({connection: connectionDetails}, jobs, function(){
     // ready to add to queue
   });
 
-  this.worker.on('start', function() {
-    winston.silly('worker started');
-  });
-
-  this.worker.on('end', function(){
-    winston.silly("worker ended");
-  });
-
-  this.worker.on('cleaning_worker', function(worker) {
-    winston.silly("cleaning old worker " + worker);
-  });
-
-  this.worker.on('poll',            function(queue){
-    winston.silly("worker polling " + queue);
-  });
-
-  this.worker.on('job', function(queue, job){
-    winston.silly("working job " + queue + " " + JSON.stringify(job));
-  });
-
-  this.worker.on('reEnqueue', function(queue, job, plugin){
-    winston.silly("reEnqueue job (" + plugin + ") " + queue + " " + JSON.stringify(job));
-  });
-
-  this.worker.on('pause', function(){
-    winston.silly("worker paused");
-  });
-
-  this.worker.on('success', function(queue, job, result){
+  worker.on('success', function(queue, job, result){
     self.fn(result, function(err) {
-      if(err) return winston.error('worker callback failed: ' + err, { exception: err });
+      if(err) return winston.error('worker calback failed:' + err, { exception: err });
     });
   });
 
-  this.worker.on('error', function(queue, job, err) {
-    winston.error('queue worker failed: ' + err, { queue: queue, job: job, exception: err });
+  worker.on('error', function(queue, job, err) {
+    winston.error('queue worker failed:' + err, { queue: queue, job: job, exception: err });
   });
 };
 
@@ -88,10 +56,6 @@ Queue.prototype.invoke = function(data, options, callback) {
   var delay = options && options.delay || 0;
 
   this.internalQueue.enqueueIn(delay, this.name, 'echo', data);
-};
-
-Queue.prototype.destroy = function() {
-  this.worker.end();
 };
 
 module.exports = {
