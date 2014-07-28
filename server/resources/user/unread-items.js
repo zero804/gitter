e97e06144ed6f1c508e5372261adf95d1ae66d3e
Andrew Newdigate <andrew@troupe.co>
2014-07-28 13:40:17 +0100

    Removing more naked errors

--- server/resources/user/unread-items.js
+++ server/resources/user/unread-items.js
@@ -2,6 +2,7 @@
 "use strict";
 
 var unreadItemService = require("../../services/unread-item-service");
+var StatusError = require('statuserror');
 
 module.exports = {
   id: 'unreadItem',
@@ -17,9 +18,9 @@ module.exports = {
 
   create: function(req, res, next) {
     var unreadItems = req.body;
-    if(!unreadItems) return next(400);
+    if(!unreadItems) return next(new StatusError(400, 'No body'));
 
-    if(!unreadItems.mention && !unreadItems.chat) return next(400); /* You comin at me bro? */
+    if(!unreadItems.mention && !unreadItems.chat) return next(new StatusError(400, 'No chat or mention items')); /* You comin at me bro? */
 
     return unreadItemService.markItemsRead(req.resourceUser.id, req.userTroupe.id, unreadItems.chat, unreadItems.mention)
       .then(function() {
@@ -40,7 +41,7 @@ module.exports = {
   },
 
   destroy: function(req, res, next) {
-    if(req.params.unreadItem.toLowerCase() !== 'all') return next(404);
+    if(req.params.unreadItem.toLowerCase() !== 'all') return next(new StatusError(404, 'Not found'));
 
     return unreadItemService.markAllChatsRead(req.resourceUser.id, req.userTroupe.id, { member: true })
       .then(function() {
