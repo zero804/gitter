c29ba444d2270bfd0e895e14683e2b79041eef51
Andrew Newdigate <andrew@troupe.co>
2014-08-01 14:15:32 +0100

    never-ending-story: Dynamic magic numbers based on clientHeight instead of constants.

--- public/js/utils/never-ending-story.js
+++ public/js/utils/never-ending-story.js
@@ -1,10 +1,6 @@
 define(['underscore', 'backbone', 'log!nes'], function(_, Backbone, log) {
   "use strict";
 
-  /* @const */
-  var ABSOLUTE_MARGIN = 50;
-  var IGNORE_MARGIN = 500;
-
   /* Put your scrolling panels on rollers */
   function NeverEndingStory(target, options) {
     this._target = target;
@@ -12,44 +8,88 @@ define(['underscore', 'backbone', 'log!nes'], function(_, Backbone, log) {
     this._prevScrollTop = 0;
     this._prevScrollTime = Date.now();
     this._scrollHandler = this.scroll.bind(this);
+    this.scrollRateLimited = _.throttle(this.scrollRate.bind(this), 100, { leading: false });
     this.enable();
   }
   _.extend(NeverEndingStory.prototype, Backbone.Events, {
     scroll: function() {
       var target = this._target;
 
-      var now = Date.now();
       var scrollTop = target.scrollTop;
       var scrollBottom = target.scrollHeight - target.scrollTop - target.clientHeight;
 
-      // var st = this._reverse ? target.scrollTop : target.scrollHeight - target.scrollTop - target.clientHeight;
-      var prevScrollTime = this._prevScrollTime;
       var prevScrollTop = this._prevScrollTop;
       var prevScrollBottom = this._prevScrollBottom;
 
       this._prevScrollTop = scrollTop;
       this._prevScrollBottom = scrollBottom;
-      this._prevScrollTime = now;
+
+      var deltaTop = prevScrollTop - scrollTop;
+      var deltaBottom = prevScrollBottom - scrollBottom;
+
+      if(deltaTop > 0) {
+        /* We're scrolling towards the top */
+        if(scrollTop < target.clientHeight/2) {
+          // log('approaching.top (margin)');
+          this.trigger('approaching.top');
+        }
+      } else if(deltaBottom > 0) {
+        /* We're scrolling towards the bottom */
+        if(scrollBottom < target.clientHeight/2) {
+          // log('approaching.bottom (margin)');
+          this.trigger('approaching.bottom');
+        }
+      }
+
+      this.scrollRateLimited();
+    },
+
+    scrollRate: function() {
+      var target = this._target;
+
+      var now = Date.now();
+      var scrollTop = target.scrollTop;
+      var scrollBottom = target.scrollHeight - target.scrollTop - target.clientHeight;
+
+      var prevScrollTime = this._prevScrollTimeRate;
+      var prevScrollTop = this._prevScrollTopRate;
+      var prevScrollBottom = this._prevScrollBottomRate;
+
+      this._prevScrollTopRate = scrollTop;
+      this._prevScrollBottomRate = scrollBottom;
+      this._prevScrollTimeRate = now;
 
       if(!prevScrollTime || this.loading) return;
 
       var deltaTop = prevScrollTop - scrollTop;
       var deltaBottom = prevScrollBottom - scrollBottom;
       var timeDelta = now - prevScrollTime;
+      var speed, timeToLimit;
 
       if(deltaTop > 0) {
-        /* We're scrolling towards the top */
-        if(scrollTop < ABSOLUTE_MARGIN || (scrollTop < IGNORE_MARGIN && (scrollTop * timeDelta/deltaTop < 300))) {
-          log('approaching.top');
+        if(scrollTop > target.clientHeight) return;
+
+        speed = deltaTop / timeDelta;
+        timeToLimit = scrollTop / speed;
+        if(timeToLimit < 600) {
+          // log('approaching.top (rate)');
           this.trigger('approaching.top');
         }
-      } else if(deltaBottom > 0) {
-        /* We're scrolling towards the bottom */
-        if(scrollBottom < ABSOLUTE_MARGIN || (scrollBottom < IGNORE_MARGIN && (scrollBottom * timeDelta/deltaBottom < 300))) {
-          log('approaching.bottom');
+        return;
+      }
+
+      if(deltaBottom > 0) {
+        if(scrollBottom > target.clientHeight) return;
+
+        speed = deltaBottom / timeDelta;
+        timeToLimit = scrollBottom / speed;
+
+        if(timeToLimit < 600) {
+          // log('approaching.bottom (rate)');
           this.trigger('approaching.bottom');
         }
       }
+
     },
 
     loadComplete: function() {
