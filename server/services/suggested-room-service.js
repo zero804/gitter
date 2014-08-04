49ca22daca8c206699483aba9f385b9b3716a794
Andrew Newdigate <andrew@troupe.co>
2014-08-04 10:23:36 +0100

    Suggested repos now uses a series of categories, each ranks as a percent

--- server/services/suggested-room-service.js
+++ server/services/suggested-room-service.js
@@ -5,62 +5,152 @@ var repoService     = require('./repo-service');
 var GithubRepo      = require("./github/github-repo-service");
 var persistence     = require("./persistence-service");
 var Q               = require('q');
-var lazy            = require('lazy.js');
-var moment          = require('moment');
 var winston         = require('../utils/winston');
 var _               = require('underscore');
 
-var SCORE_STARRED = 50;
-var SCORE_WATCHED = 100;
-var SCORE_OWN = 500;
-var SCORE_PUSH = 1500;
-var SCORE_ADMIN = 3000;
-var SCORE_RECENT = 5000;
-var SCORE_EXISTING = 5000;
-var SCORE_ROOM_PER_USER = 300;
+var HILIGHTED_ROOMS = [{
+  uri: 'gitterHQ/gitter',
+  lang: 'en'
+}, {
+  uri: 'marionettejs/backbone.marionette',
+  language: 'JavaScript',
+  lang: 'en'
+},{
+  uri: 'LaravelRUS/chat',
+  channel: true,
+  language: 'PHP',
+  lang: 'ru'
+}, {
+  uri: 'gitterHQ/nodejs',
+  chanel: true,
+  language: 'JavaScript',
+  lang: 'en'
+}, {
+  uri: 'lotus/chat',
+  chanel: true,
+  language: 'Ruby',
+  lang: 'en'
+},{
+  uri: 'rom-rb/chat',
+  chanel: true,
+  language: 'Ruby',
+  lang: 'en'
+}, {
+  uri: 'webpack/webpack',
+  language: 'JavaScript',
+  lang: 'en'
+}, {
+  uri: 'ruby-vietnam/chat',
+  chanel: true,
+  language: 'Ruby',
+  lang: 'vi'
+}, {
+  uri: 'require-lx/group',
+  language: 'JavaScript',
+  lang: 'en'
+}, {
+  uri: 'angular-ui/ng-grid',
+  language: 'JavaScript',
+  lang: 'en'
+}, {
+  uri: 'opscode/supermarket',
+  language: 'Ruby',
+  lang: 'en'
+}, {
+  uri: 'MahApps/MahApps.Metro',
+  language: 'PowerShell',
+  lang: 'en'
+}, {
+  uri: 'sympy/sympy',
+  language: 'Python',
+  lang: 'en'
+}, {
+  uri: 'rogeriopvl/erebot',
+  language: 'JavaScript',
+  lang: 'es'
+}];
 
-var WATCHER_COEFFICIENT = 0.1;
-var FORK_COEFFICIENT = 0.3;
-var STAR_GAZER_COEFFICIENT = 0.05;
-
-var BILLY_NO_MATES_ROOMS = ['gitterHQ/gitter', 'marionettejs/backbone.marionette'];
-
-function calculateScore(item) {
-  // force billy no mates rooms to only show if there is nothing else
-  if(item.billy_no_mates) return 0;
-
-  var score = 0;
-  var repo = item.repo;
-  score += repo.watchers_count * WATCHER_COEFFICIENT;
-  if(!repo.fork) {
-    score += repo.forks_count * FORK_COEFFICIENT;
+var CATEGORIES = {
+  pushTime: function(item) {
+    var repo = item.repo;
+    if(!repo) return;
+    if(!repo.pushed_at) return;
+    return new Date(repo.pushed_at).valueOf();
+  },
+  forks: function(item) {
+    var repo = item.repo;
+    if(!repo) return;
+    return repo.forks_count;
+  },
+  watchers: function(item) {
+    var repo = item.repo;
+    if(!repo) return;
+    return repo.watchers_count;
+  },
+  starGazers: function(item) {
+    var repo = item.repo;
+    if(!repo) return;
+    return repo.stargazers_count;
+  },
+  userCount: function(item) {
+    var room = item.room;
+    if(!room) return 0;
+    return room.users.length;
+  },
+  isWatchedByUser: function(item) {
+    return item.is_watched_by_user ? 1 : 0;
+  },
+  isStarredByUser: function(item) {
+    return item.is_starred_by_user ? 1 : 0;
+  },
+  programmingLanguage: function(item, context) {
+    var language = item.repo && item.repo.language || item.language;
+    if(!language) return;
+    return context.languageScores[language] || 0;
+  },
+  userLanguage: function(item, context) {
+    if(!context.lang || !item.lang) return;
+    return item.lang === context.lang ? 1 : 0;
+  },
+  highlighted: function(item) {
+    return item.highlighted ? 1 : 0;
   }
-  score += repo.stargazers_count * STAR_GAZER_COEFFICIENT;
+};
 
-  if(repo.pushed_at) {
-    var duration = moment.duration(moment().diff(repo.pushed_at));
-    var minsSincePush = duration.asMinutes();
-    // 10080 = minutes in a week
-    // therefore, repos with no commits with one week will get half the score
-    score += SCORE_RECENT * 10080 / (10080 + minsSincePush);
-  }
+var CATEGORY_COEFFICIENTS = {
+  userLanguage: 2
+};
 
-  if(repo.permissions) {
-    if(repo.permissions.admin) score += SCORE_ADMIN;
-    if(repo.permissions.push) score += SCORE_PUSH;
-  }
+function processCategory(name, items, context) {
+  var valueFunc = CATEGORIES[name];
+  var coefficient = CATEGORY_COEFFICIENTS[name];
 
-  if(repo.is_watched_by_user) score += SCORE_WATCHED;
+  var values = items.map(function(f) {
+    return valueFunc(f, context);
+  }).filter(function(f) {
+    return f !== undefined && f !== null;
+  });
+  if(!values.length) return;
+  var max = _.max(values);
+  var min = _.min(values);
 
-  if(repo.is_owned_by_user) score += SCORE_OWN;
+  if(max === min) return; // All the values are the same, useless for ranking
 
-  if(item.room) {
-    score += SCORE_EXISTING;
-    score += item.room.users.length * SCORE_ROOM_PER_USER;
-  }
+  items.forEach(function(item) {
+    var score = valueFunc(item, context);
+    if(score === undefined || score === null) return;
+    var rank = ((score - min) / (max - min)) * 100;
+    if(coefficient > 0) {
+      rank = rank * coefficient;
+    }
 
-  return score;
+    if(!item.scores) item.scores = {};
+    item.scores[name] = rank;
+    item.score = item.score ? item.score + rank : rank;
+    item.categories = item.categories ? item.categories + 1 : 1;
+  });
 }
+
 
 /* This seems to be a very badly performing query! */
 function findReposWithRooms(repoList) {
@@ -73,7 +163,7 @@ function findReposWithRooms(repoList) {
   winston.info("Querying reposWithRooms for " + uris.length + " repositories");
 
   return persistence.Troupe.findQ({
-      githubType: 'REPO',
+      // githubType: 'REPO',
       lcUri: { $in: uris }
     }, { uri: 1, users: 1 });
 }
@@ -85,6 +175,8 @@ function filterSuggestions(suggestions, user) {
       return !suggestion.room.containsUserId(user.id);
     }
 
+    if(!suggestion.repo) return true; // No repo? It's a suggestion
+
     if(suggestion.repo.permissions && suggestion.repo.permissions.admin) return true;
 
     return false;
@@ -92,54 +184,79 @@ function filterSuggestions(suggestions, user) {
   });
 }
 
-function getSuggestions(user) {
+function getLanguageScores(suggestions) {
+  var result = {};
+  suggestions.forEach(function(item) {
+    var repo = item.repo;
+    if(!repo || !repo.language) return;
+    result[repo.language] = result[repo.language] ? result[repo.language] + 1 : 1;
+  });
+
+  return result;
+}
+
+
+function getSuggestions(user, lang) {
 
   var ghRepo  = new GithubRepo(user);
   return Q.all([
-      // ghRepo.getStarredRepos().then(map(SCORE_STARRED)),
+      ghRepo.getStarredRepos(),
       ghRepo.getWatchedRepos(),
       repoService.getReposForUser(user)
     ])
-    .spread(function(watchedRepos, ownedRepos) {
-      watchedRepos.forEach(function(repo) {
-        repo.is_watched_by_user = true;
+    .spread(function(starredRepos, watchedRepos, ownedRepos) {
+      var suggestions = {};
+
+      function addSuggestion(flag) {
+        return function(repo) {
+          if(!suggestions[repo.full_name]) {
+            suggestions[repo.full_name] = { uri: repo.full_name, repo: repo };
+          }
+          suggestions[repo.full_name][flag] = true;
+        };
+      }
+
+      starredRepos.forEach(addSuggestion('is_starred_by_user'));
+      watchedRepos.forEach(addSuggestion('is_watched_by_user'));
+      ownedRepos.forEach(addSuggestion('is_owned_by_user'));
+
+      HILIGHTED_ROOMS.forEach(function(room) {
+        if(!suggestions[room.uri]) {
+          suggestions[room.uri] = { uri: room.uri, language: room.language };
+        }
+        suggestions[room.uri].lang = room.lang;
+        suggestions[room.uri].highlighted = true;
       });
 
-      ownedRepos.forEach(function(repo) {
-        repo.is_owned_by_user = true;
+      // Find suggestions that a) are not channels but b) don't have repos
+      var missingUris = _.values(suggestions).filter(function(item) {
+        return !item.channel && !item.repo;
+      }).map(function(item) {
+        return item.uri;
       });
 
-      return watchedRepos.concat(ownedRepos);
-    })
-    .then(function(repos) {
-      return repos.map(function(repo) {
-        return { uri: repo.full_name, repo: repo, score: 0 };
-      });
+      return Q.all(missingUris.map(function(uri) {
+          return ghRepo.getRepo(uri)
+            .then(function(result) {
+              if(result) {
+                if(suggestions[uri]) {
+                  suggestions[uri].repo = result;
+                }
+              }
+            });
+        }))
+        .thenResolve(suggestions);
     })
     .then(function(suggestions) {
-
-      var uriMap = {};
-      var uris = [];
-
-      suggestions.forEach(function(suggestion) {
-        uriMap[suggestion.uri] = suggestion;
-        uris.push(suggestion.uri);
-      });
-
-      BILLY_NO_MATES_ROOMS.forEach(function(roomname) {
-        if(uris.indexOf(roomname) < 0) {
-          uriMap[roomname] = { uri: roomname, score: 0, billy_no_mates: true };
-          uris.push(roomname);
-        }
-      });
+      var uris = Object.keys(suggestions);
 
       return findReposWithRooms(uris)
         .then(function(rooms) {
           rooms.forEach(function(room) {
-            uriMap[room.uri].room = room;
+            suggestions[room.uri].room = room;
           });
 
-          return _.values(uriMap);
+          return _.values(suggestions);
         });
 
     })
@@ -147,19 +264,28 @@ function getSuggestions(user) {
       return filterSuggestions(suggestions, user);
     })
     .then(function(suggestions) {
-      suggestions.forEach(function(suggestion) {
-        suggestion.score = calculateScore(suggestion);
+      var languageScores = getLanguageScores(suggestions);
+
+      Object.keys(CATEGORIES).forEach(function(name) {
+        processCategory(name, suggestions, {
+          languageScores: languageScores,
+          lang: lang
+        });
       });
 
-      return suggestions;
-    })
-    .then(function(suggestions) {
-      return suggestions.sort(function(a, b) {
+      suggestions = suggestions.filter(function(item) {
+        return item.categories >= 1;
+      });
+
+      suggestions.forEach(function(item) {
+        item.score = item.score / item.categories;
+      });
+
+      suggestions.sort(function(a, b) {
         return b.score - a.score;
       });
-    })
-    .then(function(suggestions) {
-      return suggestions.slice(0, 6);
+
+      return suggestions.slice(0, 10);
     });
 }
 
