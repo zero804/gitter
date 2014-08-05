3393fb46b232cd18fe460f9933c332a6672502b2
Mike Bartlett <mike.bartlett@gmail.com>
2014-08-05 11:43:08 +0100

    content adjustments

--- public/js/signup.js
+++ public/js/signup.js
@@ -25,87 +25,87 @@ require([
        *   Cache the "this" object for when context changes
        */
       var me = this;
-      
+
       /**
        *   Where do we append our elements?
        */
       this.mapEl = $('.map');
       this.bg = $('#intro-panel > .bg');
       this.apps = $('#apps-panel');
-      
+
       this.speeds = {
         default: 350,
         fast: 150,
         slow: 800
       };
-      
+
       /**
        *   What happens when we start up?
        */
       this.init = function() {
         //  Wait until all images have loaded
         $(window).load(this.onLoad);
-        
+
         //  Start the map conversation thing
         this.map();
-        
+
         //  Cycle blockquotes
-        this.cycle($('#testimonials-panel blockquote'));
+        this.cycle($('#testimonials-panel blockquote'), 7000);
         this.cycle($('.loves li'), 2500);
-        
+
         $('.team a').attr('target', '_blank');
-        
+
         $('.tooltip-container').on('mouseout', function() {
           var me = $(this);
-          
+
           me.addClass('out');
           setTimeout(function() { me.removeClass('out'); }, 400);
         }).on('mouseover', function() {
           $(this).removeClass('out');
         });
       };
-      
+
       this.cycle = function(el, speed) {
         el.first().addClass('visible');
-        
+
         el.parent().css('height', el.outerHeight());
-        
+
         setInterval(function() {
           var active = el.filter('.visible').removeClass('visible').addClass('going');
           var target = active.next();
-          
+
           if(!target.length) {
             target = el.first();
           }
-          
+
           target.removeClass('going').addClass('visible');
         }, speed || 4000);
       };
-      
+
       this.onLoad = function() {
         //  Fade in the background image
         me.bg.fadeIn(me.speeds.slow);//.resizeToParent();
-        
+
         //  @TODO
         //  Initiate the chat boxes
         //  @TODO
-        
+
         //  Make the apps panel start on scroll
         me.appsPanel();
       };
-      
+
       this.appsPanel = function() {
         var pos = me.apps.position().top;
         var win = $(window);
         var offset = 150;
-        
+
         win.scroll(function() {
           if(win.scrollTop() + offset >= pos) {
             me.apps.addClass('visible');
           }
         });
       };
-      
+
       /**
        *   Generate pseudo-random conversations
        */
@@ -116,11 +116,11 @@ require([
           [340, 95], [490, 141], [531, 206], [579, 268], [345, 104],
           [532, 21], [218, 48], [384, 226], [153, 226], [420, 157]
         ];
-        
+
         //  And have some random messages
         //var messages = ['Hey guys, how’s it going?', 'Hey everyone!', 'Anyone looking at <b>#146</b>?',
         //        'Did anyone see that ludicrous display last night?', '<code>sudo rm -rf /</code>'];
-                
+
         var generate = function(chatMessage, pos) {
           var msg = $('<div class="msg" />');
 
@@ -171,7 +171,7 @@ require([
           if (messages.length < 5) messages.push(msg);
         }, 2500);
       };
-      
+
       //  And go!
       this.init();
     };
