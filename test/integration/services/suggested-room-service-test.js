7475632e3232fedb9684cfe39c3d6afbcf89b441
Andy Trevorah <andy@troupe.co>
2014-08-06 10:57:16 +0100

    replaced suggested-room-service-test with something more useful

--- test/integration/services/suggested-room-service-test.js
+++ test/integration/services/suggested-room-service-test.js
@@ -1,5 +1,5 @@
 /*jslint node:true, unused:true*/
-/*global describe:true, it:true, before:true, after: true */
+/*global describe:true, it:true */
 "use strict";
 
 var testRequire = require('../test-require');
@@ -9,10 +9,10 @@ var FAKE_USER = { username: 'gittertestbot', githubToken: '8e8ec7658e8a3bc645cfb
 var suggestionService = testRequire("./services/suggested-room-service");
 
 describe('suggested-room-service', function() {
-  it('should fetch suggestedReposForUser', function(done) {
-    return suggestionService.suggestedReposForUser(FAKE_USER)
-      .then(function(repos) {
-        assert(repos);
+  it('should get 6 suggestions', function(done) {
+    return suggestionService.getSuggestions(FAKE_USER)
+      .then(function(suggestions) {
+        assert(suggestions.length, 6);
       })
       .nodeify(done);
   });
