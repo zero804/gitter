f998556ee60b77e16812252b74fffb4c18a30601
Andy Trevorah <andy@troupe.co>
2014-08-08 17:18:57 +0100

    removed jquery selectors that would never hit

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,7 +1,4 @@
-require([
-  'jquery'
- ],
-  function($) {
+require([], function() {
     "use strict";
 
     require([
@@ -10,26 +7,4 @@ require([
       // No need to do anything here
     });
 
-    $('.scroll-link').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
-    return false;
-    });
-
-
-    $('#arrow-1').click(function() {
-        if (window.mixpanel) {
-            window.mixpanel.track('arrow-click', { arrow: 'arrow-1' });
-        }
-    });
-    $('#arrow-2').click(function() {
-        if (window.mixpanel) {
-            window.mixpanel.track('arrow-click', { arrow: 'arrow-2' });
-        }
-    });
-
 });
-
-
-
