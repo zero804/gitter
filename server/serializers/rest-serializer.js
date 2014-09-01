b1bf263ab153e8dc1c3a6900ae62ecb479a18bed
Andrew Newdigate <andrew@troupe.co>
2014-09-01 14:22:33 +0100

    Revert "Notification serialiser now the same as the rest serializer"
    This reverts commit be5a104346964db9da6db42f69057051f2b46b80.

--- server/serializers/rest-serializer.js
+++ server/serializers/rest-serializer.js
@@ -1,73 +1,170 @@
 /*jshint globalstrict:true, trailing:false, unused:true, node:true */
 "use strict";
 
-var Q                = require("q");
-var createSerializer = require('./create-serializer');
+var Q       = require("q");
+var winston = require('../utils/winston');
+var fs      = require('fs');
+var path    = require('path');
+
+/* This method should move */
+function serialize(items, strat, callback) {
+  var d = Q.defer();
+
+  if(!items) {
+    return Q.resolve().nodeify(callback);
+  }
+
+  var single = !Array.isArray(items);
+  if(single) {
+    items = [ items ];
+  }
+
+  function pkg(i) {
+    return single ? i[0] : i;
+  }
+
+  strat.preload(items, function(err) {
+
+    if(err) {
+      winston.error("Error during preload", { exception: err });
+      return d.reject(err);
+    }
+
+    var serialized = items.map(strat.map)
+      .filter(function(f) {
+        return f !== undefined;
+      });
+
+    if(strat.post) {
+      serialized = strat.post(serialized);
+    }
+
+    d.resolve(pkg(serialized));
+  });
+
+  return d.promise.nodeify(callback);
+
+}
+
+function serializeExcludeNulls(items, strat, callback) {
+  var single = !Array.isArray(items);
+
+  return serialize(items, strat)
+    .then(function(results) {
+      if(single) return results;
+      return results.filter(function(f) { return !!f; });
+    })
+    .nodeify(callback);
+}
+
+// XXX: deprecated
+function serializeQ(items, strat) {
+  return serialize(items, strat);
+}
+
 
 // TODO: deprecate this....
 function getStrategy(modelName) {
   switch(modelName) {
     case 'chat':
-      return serializer.ChatStrategy;
+      return restSerializer.ChatStrategy;
     case 'chatId':
-      return serializer.ChatIdStrategy;
+      return restSerializer.ChatIdStrategy;
     case 'troupe':
-      return serializer.TroupeStrategy;
+      return restSerializer.TroupeStrategy;
     case 'troupeId':
-      return serializer.TroupeIdStrategy;
+      return restSerializer.TroupeIdStrategy;
     case 'user':
-      return serializer.UserStrategy;
+      return restSerializer.UserStrategy;
   }
 }
 
 function serializeModel(model, callback) {
-  return Q.fcall(function() {
-      if(model === null) return;
+  if(model === null) return Q.resolve().nodeify(callback);
 
-      var schema = model.schema;
-      if(!schema) throw new Error("Model does not have a schema");
-      if(!schema.schemaTypeName) throw new Error("Schema does not have a schema name");
+  var schema = model.schema;
+  if(!schema) return Q.reject(new Error("Model does not have a schema")).nodeify(callback);
+  if(!schema.schemaTypeName) return Q.reject(new Error("Schema does not have a schema name")).nodeify(callback);
 
-      var strategy;
+  var strategy;
 
-      switch(schema.schemaTypeName) {
-        case 'UserSchema':
-          strategy = new serializer.UserStrategy();
-          break;
+  switch(schema.schemaTypeName) {
+    case 'UserSchema':
+      strategy = new restSerializer.UserStrategy();
+      break;
 
-        case 'TroupeSchema':
-          strategy = new serializer.TroupeStrategy();
-          break;
+    case 'TroupeSchema':
+      strategy = new restSerializer.TroupeStrategy();
+      break;
 
-        case 'TroupeUserSchema':
-          strategy = new serializer.TroupeUserStrategy();
-          break;
+    case 'TroupeUserSchema':
+      strategy = new restSerializer.TroupeUserStrategy();
+      break;
 
-        case 'TroupeBanStrategy':
-          strategy = new serializer.TroupeBanStrategy();
-          break;
+    case 'TroupeBanStrategy':
+      strategy = new restSerializer.TroupeBanStrategy();
+      break;
 
-        case 'ChatMessageSchema':
-          strategy = new serializer.ChatStrategy();
-          break;
+    case 'ChatMessageSchema':
+      strategy = new restSerializer.ChatStrategy();
+      break;
 
-        case 'EventSchema':
-          strategy = new serializer.EventStrategy();
-          break;
+    case 'EventSchema':
+      strategy = new restSerializer.EventStrategy();
+      break;
 
-      }
+  }
 
-      if(!strategy) throw new Error("No strategy for " + schema.schemaTypeName);
+  if(!strategy) return Q.reject(new Error("No strategy for " + schema.schemaTypeName)).nodeify(callback);
 
-      return serializer.serialize(model, strategy);
-    })
-    .nodeify(callback);
+  return serialize(model, strategy, callback);
 }
 
-var serializer = createSerializer('rest', {
+function eagerLoadStrategies() {
+  Object.keys(restSerializer).forEach(function(key) {
+    restSerializer[key]; // NB! Force the lazy-loading of strategies
+  });
+}
+
+var restSerializer = {
   getStrategy: getStrategy,
+  serialize: serialize,
+  serializeExcludeNulls: serializeExcludeNulls,
+  serializeQ: serializeQ,
   serializeModel: serializeModel,
+  testOnly: {
+    eagerLoadStrategies: eagerLoadStrategies
+  }
+};
+
+
+fs.readdirSync(__dirname + '/rest').forEach(function(fileName) {
+  if(!/\.js$/.test(fileName)) return;
+
+  var baseName = path.basename(fileName, '.js');
+
+  var strategyName = baseName.replace(/\-./g, function(match) {
+    return match[1].toUpperCase();
+  }).replace(/^./, function(match) {
+    return match.toUpperCase();
+  });
+
+  Object.defineProperty(restSerializer, strategyName, {
+    enumerable: true,
+    configurable: true,
+    get: function() {
+      var strategy = require('./rest/' + baseName);
+
+      Object.defineProperty(restSerializer, strategyName, {
+        enumerable: true,
+        configurable: false,
+        writable: false,
+        value: strategy
+      });
+
+      return strategy;
+    }
+  });
 });
 
-
-module.exports = serializer;
+module.exports = restSerializer;
