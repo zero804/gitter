7b9dde81574d82055444c4b75c4ebd64a46f4f6f
Walter Carvalho <walter@gitter.im>
2014-09-05 16:06:40 +0100

    more dynamic generation of markdown for badge

--- server/api/private/create-badge-pr.js
+++ server/api/private/create-badge-pr.js
@@ -1,17 +1,19 @@
 /*jshint globalstrict: true, trailing: false, unused: true, node: true */
 "use strict";
-
+var conf = require('../../utils/config');
 var badgerService = require('../../services/badger-service');
 
-module.exports = function(req, res, next) {
-  var content = '[![Gitter](https://badges.gitter.im/' + req.body.uri + '.png)](https://gitter.im/' + req.body.uri + ')';
 
-  return badgerService.updateFileAndCreatePullRequest(req.body.uri, req.user.username, 'master', 'gitter-badge', content)
-  .then(function(pr) {
-    res.send(pr);
-  })
-  .fail(function(err) {
-    console.error(err);
-    next(err);
-  });
+module.exports = function (req, res, next) {
+  var content = '[![Gitter]('+ conf.get('web:badgeBaseUrl') + '/' + req.body.uri + '.svg)](' + conf.get('web:basepath') + req.body.uri + ')';
+
+  return badgerService
+    .updateFileAndCreatePullRequest(req.body.uri, req.user.username, 'master', 'gitter-badge', content)
+    .then(function (pr) {
+      res.send(pr);
+    })
+    .fail(function (err) {
+      console.error(err);
+      next(err);
+    });
 };
