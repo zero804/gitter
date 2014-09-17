8f45283a80036d20c214aa6c9dd72b7a2963129a
Andy Trevorah <andy@troupe.co>
2014-09-17 16:18:36 +0100

    added basic module-cache-wrapper

---
+++ test/integration/utils/module-cache-wrapper.js
@@ -0,0 +1,58 @@
+"use strict";
+
+var assert = require('assert');
+var Q = require('q');
+var testRequire = require('../test-require');
+
+
+function getWrapper(lookupFunc) {
+  var MockSnappyCache = function() {};
+  MockSnappyCache.prototype.lookup = lookupFunc;
+  return testRequire.withProxies('./utils/module-cache-wrapper', {
+    'snappy-cache': MockSnappyCache
+  });
+}
+
+describe('module-cache-wrapper', function() {
+
+  describe('single function modules', function() {
+
+    var module = function(name) {
+      return Q.resolve('hello ' + name);
+    };
+    
+    it('looks up correct key', function(done) {
+      var wrapper = getWrapper(function(key) {
+        assert.equal(key, 'my-module:::world');
+        done();
+      });
+
+      var wrapped = wrapper('my-module', module);
+      wrapped('world');
+    });
+
+    it('encodes colons (eww, gross!) in the key', function(done) {
+      var wrapper = getWrapper(function(key) {
+        assert.equal(key, 'my-module:::look%3Aat%3Amy%3Acolons');
+        done();
+      });
+
+      var wrapped = wrapper('my-module', module);
+      wrapped('look:at:my:colons');
+    });
+
+    it('calls function correctly on cache miss', function(done) {
+      var wrapper = getWrapper(function(key, cachedFunc, cb) {
+        cachedFunc(cb);
+      });
+
+      var wrapped = wrapper('my-module', module);
+      wrapped('world').then(function(result) {
+        assert.equal(result, 'hello world');
+        done();
+      });
+    });
+
+  });
+
+});
