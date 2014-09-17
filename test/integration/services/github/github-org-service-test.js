e2a44e16bc52a7a2ab14c3cf3d354c2bf33577eb
Andy Trevorah <andy@troupe.co>
2014-09-17 11:52:54 +0100

    fixed the auth tokens used in tests just like 7baf2db and 5c7d405

--- test/integration/services/github/github-org-service-test.js
+++ test/integration/services/github/github-org-service-test.js
@@ -5,124 +5,72 @@
 var testRequire    = require('../../test-require');
 var assert         = require("assert");
 var GithubOrgService = testRequire('./services/github/github-org-service');
-var FAKE_USER = { username: 'gittertestbot', githubToken: '64c1d90a8c60d2ee75fc5b3d3f7881d94559fec8'};
+var GITTER_TEST_BOT = { username: 'gittertestbot', githubToken: '***REMOVED***'};
 
 describe('github-org-service', function() {
-  it('members should fetch members', function(done) {
-    var gh = new GithubOrgService(FAKE_USER);
 
-    gh.members('gitterHQ')
-      .then(function(members) {
-        assert(members.length >= 1);
+  describe('members', function() {
 
-        gh.members('gitterHQ')
-          .then(function(members) {
-            assert(members.length >= 1);
-          });
-      })
-      .nodeify(done);
+    it('should fetch members', function(done) {
+      var gh = new GithubOrgService(GITTER_TEST_BOT);
+
+      gh.members('gitterHQ')
+        .then(function(members) {
+          assert(members.length >= 1);
+        })
+        .nodeify(done);
+    });
+
+    it('should return true if a user checks that it is in an org', function(done) {
+      var gh = new GithubOrgService(GITTER_TEST_BOT);
+
+      gh.member('gitterHQ', 'gittertestbot')
+        .then(function(isMember) {
+          assert(isMember);
+        })
+        .nodeify(done);
+    });
+
+    it('should return true if a user checks that another member is in an org', function(done) {
+      var gh = new GithubOrgService(GITTER_TEST_BOT);
+
+      gh.member('gitterHQ', 'mydigitalself')
+        .then(function(isMember) {
+          assert(isMember);
+        })
+        .nodeify(done);
+    });
+
+    it('should return a list of users in the owners team in an org', function(done) {
+      var gh = new GithubOrgService(GITTER_TEST_BOT);
+
+      gh.getOwners('gitterHQ')
+        .then(function(members) {
+          assert(members.some(function(member) { return member.login === 'suprememoocow'; }));
+        })
+        .nodeify(done);
+    });
+
+    it('should return false if a user checks that it is in an org that it is not a member of', function(done) {
+      var gh = new GithubOrgService(GITTER_TEST_BOT);
+
+      gh.member('adobe', 'gittertestbot')
+        .then(function(isMember) {
+          assert(!isMember);
+        })
+        .nodeify(done);
+    });
+
+    it('should return false if a user checks that a stranger is in the users org', function(done) {
+      var gh = new GithubOrgService(GITTER_TEST_BOT);
+
+      gh.member('gitterHQ', 'indexzero')
+        .then(function(isMember) {
+          assert(!isMember);
+        })
+        .nodeify(done);
+    });
+
   });
-
-  it('member should return true if a user is in an org', function(done) {
-    var gh = new GithubOrgService(FAKE_USER);
-
-    gh.member('gitterHQ','gittertestbot')
-      .then(function(isMember) {
-        assert(isMember);
-      })
-      .nodeify(done);
-  });
-
-
-  it('member should return true if a user is in an org', function(done) {
-    var gh = new GithubOrgService(FAKE_USER);
-
-    gh.member('gitterHQ','mydigitalself')
-      .then(function(isMember) {
-        assert(isMember);
-      })
-      .nodeify(done);
-  });
-
-  it('member should return a list of users in the owners team in an org', function(done) {
-    var gh = new GithubOrgService(FAKE_USER);
-
-    gh.getOwners('gitterHQ')
-      .then(function(members) {
-        assert(members.some(function(member) { return member.login === 'suprememoocow'; }));
-      })
-      .nodeify(done);
-  });
-
-
-
-  xit('member should return false if a user is not in an org GITTERHQ', function(done) {
-    var gh = new GithubOrgService({ username: 'mbtesting', githubToken: 'e00b7680cd3665d50ebd6f0fe0ba3e49e2600c67'});
-
-    gh.member('gitterHQ','mbtesting')
-      .then(function(isMember) {
-        assert(!isMember);
-      })
-      .nodeify(done);
-  });
-
-
-  xit('member should return false if a user is not in an org TROUPE', function(done) {
-    var gh = new GithubOrgService({ username: 'mbtesting', githubToken: 'e00b7680cd3665d50ebd6f0fe0ba3e49e2600c67'});
-
-    gh.member('troupe','mbtesting')
-      .then(function(isMember) {
-        assert(!isMember);
-      })
-      .nodeify(done);
-  });
-
-
-  xit('member should return mtbtesting is not a member of ADOBE', function(done) {
-    var gh = new GithubOrgService({ username: 'mbtesting', githubToken: 'e00b7680cd3665d50ebd6f0fe0ba3e49e2600c67'});
-
-    gh.member('adobe','mbtesting')
-      .then(function(isMember) {
-        assert(!isMember);
-      })
-      .nodeify(done);
-  });
-
-
-  it('member should that suprememoocow is a member of gitterTest', function(done) {
-    var gh = new GithubOrgService({ username: 'suprememoocow', githubToken: '23cc53bcfa09e8ff22a03cf8a73e1115f987b0e6'});
-
-    gh.member('gitterTest','suprememoocow')
-      .then(function(isMember) {
-        assert(!isMember);
-      })
-      .nodeify(done);
-  });
-
-
-  it('member should that suprememoocow is not a member of gitterTest', function(done) {
-    var gh = new GithubOrgService({ username: 'suprememoocow', githubToken: '23cc53bcfa09e8ff22a03cf8a73e1115f987b0e6'});
-
-    gh.member('gitterTest','suprememoocow')
-      .then(function(isMember) {
-        assert(!isMember);
-      })
-      .nodeify(done);
-  });
-
-
-
-  it('member should return true if a user is in an org', function(done) {
-    var gh = new GithubOrgService(FAKE_USER);
-
-    gh.member('gitterHQ','indexzero')
-      .then(function(isMember) {
-        assert(!isMember);
-      })
-      .nodeify(done);
-  });
-
-
-
 
 });
