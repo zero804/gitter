1107d05312741b9fb98b91d62c0bc9839e6c852b
Walter Carvalho <walter@gitter.im>
2014-09-18 15:13:49 +0100

    pluralizing, formatting numbers and latest for explore

--- server/handlers/explore.js
+++ server/handlers/explore.js
@@ -6,7 +6,12 @@ var repoDescription = require('../services/github/github-fast-repo-description')
 var getRoughMessageCount = require('../services/chat-service').getRoughMessageCount;
 var Q = require('q');
 
-var DEFAULT_TAGS = ['javascript', 'ruby', 'php'];
+// @const
+var DEFAULT_TAGS = ['javascript', 'ruby', 'php'].sort();
+
+function trim(str) {
+  return str.trim();
+}
 
 function getRoomRenderData(room) {
   return Q.all([
@@ -24,37 +29,34 @@ function getRoomRenderData(room) {
     });
 }
 
-function processTagResult(tag, rooms) {
-  return Q.all(rooms.map(getRoomRenderData))
-    .then(function(roomsWithExtraData) {
-      return {
-        tag: tag,
-        rooms: roomsWithExtraData
-      };
-    });
+function processTagResult(rooms) {
+  return Q.all(rooms.map(getRoomRenderData));
 }
 
-function getRenderDataForTag(tag) {
-  return suggestedService.getTaggedRooms(tag)
-    .then(function(rooms) {
-      return processTagResult(tag, rooms);
-    });
+function createResponseData(tags, rooms) {
+  return {
+    tags: tags.join(', '),
+    rooms: rooms
+  };
 }
 
 module.exports = {
-  install: function(app) {
-    // app.get('/explore', function (req, res, next) {
-    //   return Q.all(DEFAULT_TAGS.map(getRenderDataForTag))
-    //     .then(function(tagRenderDataList) {
-    //         res.render('explore', { tags: tagRenderDataList });
-    //       })
-    //       .fail(next);
-    // });
+  install: function (app) {
 
-    app.get('/explore/tags/:tag', function (req, res, next) {
-      return getRenderDataForTag(req.params.tag, req.user)
-        .then(function(tagRenderData) {
-          res.render('explore', tagRenderData );
+    // route to handle search only
+    app.get('/explore/tags', function (req, res) {
+      var search = req.query.search;
+      var tags = (search) ? search.split(/[ ,]+/).map(trim).sort().join(',') : DEFAULT_TAGS.join(',');
+      res.redirect('/explore/tags/' + tags);
+    });
+
+    app.get('/explore/tags/:tags', function (req, res, next) {
+      var tags = req.params.tags.split(',');
+      suggestedService.fetchByTags(tags)
+        .then(processTagResult)
+        .then(createResponseData.bind(null, tags))
+        .then(function (data) {
+          res.render('explore', data);
         })
         .fail(next);
     });
