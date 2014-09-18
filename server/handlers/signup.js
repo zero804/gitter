cc9aa741ff36f91144082771e8827f8ace5c6ee5
Andrew Newdigate <andrew@troupe.co>
2014-09-18 10:52:04 +0100

    Language selection

--- server/handlers/signup.js
+++ server/handlers/signup.js
@@ -3,19 +3,31 @@
 
 var loginUtils = require('../web/login-utils');
 var nconf      = require('../utils/config');
+var locale     = require("locale");
+var supported  = new locale.Locales(["en", "ru", "ja"]);
 
 module.exports = {
     install: function(app) {
       app.get(nconf.get('web:homeurl'),
         require('../web/middlewares/unawesome-browser'),
         function(req, res, next) {
+          var locales = new locale.Locales(req.headers["accept-language"]);
+
           if(req.user && req.query.redirect !== 'no') {
             loginUtils.redirectUserToDefaultTroupe(req, res, next);
             return;
           }
 
+          var lang = locales.best(supported);
+          var template;
+          if(lang.code === 'en') {
+            template = 'homepage';
+          } else {
+            template = 'homepage-' + lang.code;
+          }
+
           // when the viewer is not logged in:
-          res.render('homepage-ru', {
+          res.render(template, {
             billingBaseUrl: nconf.get('web:billingBaseUrl')
           });
         });
