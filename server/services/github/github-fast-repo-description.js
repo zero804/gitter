80c269ea9afce790cd64f06681ad027f9e4dc41b
Andy Trevorah <andy@troupe.co>
2014-09-18 12:02:55 +0100

    updated github-fast-repo-description to use a cache

--- server/services/github/github-fast-repo-description.js
+++ server/services/github/github-fast-repo-description.js
@@ -1,28 +1,14 @@
 /*jshint globalstrict:true, trailing:false, unused:true, node:true */
 "use strict";
 
-var createClient = require('./github-client');
+var RepoService = require('./github-repo-service');
 var wrapper = require('../../utils/module-cache-wrapper');
-var badCredentialsCheck = require('./bad-credentials-check');
-var Q = require('q');
-
-function getRepoInfo(repoName) {
-  var client = createClient.full();
-  var ghrepo = client.repo(repoName);
-  var d = Q.defer();
-  ghrepo.info(createClient.makeResolver(d));
-  return d.promise
-    .fail(badCredentialsCheck)
-    .fail(function(err) {
-      if(err.statusCode == 404) return;
-      throw err;
-    });
-}
 
 function getDescription(repoName) {
-  return getRepoInfo(repoName).then(function(repoInfo) {
-    return repoInfo.description;
+  var repoService = new RepoService();
+  return repoService.getRepo(repoName).then(function(repo) {
+    return repo.description;
   });
 }
 
-module.exports = wrapper('github-fast-repo-description', getDescription);
+module.exports = wrapper('github-fast-repo-description', getDescription, { ttl: 20 });
