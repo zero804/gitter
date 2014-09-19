3264d97902043ef43f6619ee107228c21c9dcfe9
Walter Carvalho <walter@gitter.im>
2014-09-19 17:27:34 +0100

    stats on steroids

--- public/js/explore.js
+++ public/js/explore.js
@@ -5,8 +5,35 @@ require([
 ], function($, appEvents, tracking) {
   "use strict";
 
-  $('.room-item').on('click', function (e) {
+  var ui = {
+    tile: $('.js-room-item'),
+    form: $('.js-search-form'),
+    pills: $('.js-pills'),
+    createNew: $('.js-create-new'),
+    findOut: $('.js-find-out')
+  };
+
+  $(ui.form).on('submit', function (e) {
+    appEvents.trigger('track-event', 'explore_searched', {
+      searchTerm: e.target.search.value
+    });
+  });
+
+  $(ui.pills).on('click', function (e) {if (prevent) e.preventDefault();
+    appEvents.trigger('track-event', 'explore_pills_click', {
+      tag: e.target.innerText.toLowerCase()
+    });
+  });
+
+  $(ui.tile).on('click', function (e) {if (prevent) e.preventDefault();
     appEvents.trigger('track-event', 'explore_room_click');
   });
 
+  $(ui.createNew).on('click', function (e) {if (prevent) e.preventDefault();
+    appEvents.trigger('track-event', 'explore_room_createNew');
+  });
+
+  $(ui.findOut).on('click', function (e) {if (prevent) e.preventDefault();
+    appEvents.trigger('track-event', 'explore_room_about');
+  });
 });
