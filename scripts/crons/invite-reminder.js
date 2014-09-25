54d365285ef4d50115c11da881332adc859ceec7
Walter Carvalho <walter@gitter.im>
2014-09-25 12:48:14 +0100

    adding Q back, we need Q.all

--- scripts/crons/invite-reminder.js
+++ scripts/crons/invite-reminder.js
@@ -7,6 +7,7 @@ var persistenceService = require('../../server/services/persistence-service');
 var userService = require('../../server/services/user-service');
 var troupeService = require('../../server/services/troupe-service');
 var emailNotificationService = require('../../server/services/email-notification-service');
+var Q = require('q');
 
 var logger = require('../../server/utils/env').logger;
 
@@ -31,11 +32,11 @@ function mapInviteesAndRooms(users) {
 
 // fetches the invitees and rooms from database
 function populate(users, invitees, rooms) {
-  return [
+  return Q.all([
     users,
     userService.findByIds(invitees),
     troupeService.findByIds(rooms)
-  ];
+  ]);
 }
 
 function markAsReminded(user) {
@@ -49,25 +50,27 @@ persistenceService.User
   .then(mapInviteesAndRooms)
   .spread(populate)
   .spread(function (users, invitees, rooms) {
-    logger.info('attempting to sent reminder to', users, 'user(s)');
+    logger.info('attempting to sent reminder to ' + users.length + ' user(s)');
     invitees = collections.indexById(invitees);
     rooms = collections.indexById(rooms);
 
-    return users.map(function (user) {
+    return Q.all(users.map(function (user) {
       var fromUser = invitees[user.invitedByUser];
       var room = rooms[user.invitedToRoom];
       return emailNotificationService.sendInvitation(fromUser, user, room, true)
+        .then(function(x) {
+          return x;
+        })
         .then(markAsReminded.bind(null, user))
         .catch(function () {
-          logger.error('Couldn\'t notify user:', user.displayName, 'id ->', user._id);
+          logger.error('Couldn\'t notify user: ', user.displayName, ' id ->', user._id);
         });
-    });
+    }));
   })
   .then(function (users) {
-    logger.info('invitation reminder sent to', users.length, 'user(s)');
+    console.log(users);
+    logger.info('invitation reminder sent to ' + users.length + ' user(s)');
     logger.info('exiting...');
     process.exit();
   })
-  .catch(function (err) {
-    logger.error(err);
-  });
+  .catch(die);
