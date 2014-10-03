ccae6e9008129bb268e9d72ff7d584d172f3831c
Andy Trevorah <andy@troupe.co>
2014-10-03 15:16:09 +0100

    embedded app can now be loaded

--- server/web/hbs-helpers.js
+++ server/web/hbs-helpers.js
@@ -34,7 +34,7 @@ var troupeEnv = {
   logging: nconf.get("web:consoleLogging"),
   ravenUrl: nconf.get("errorReporting:clientRavenUrl"),
   websockets: {
-    fayeUrl: nconf.get('ws:fayeUrl') || "/faye",
+    fayeUrl: nconf.get('ws:fayeUrl'),
     options: {
       timeout: nconf.get('ws:fayeTimeout'),
       retry: nconf.get('ws:fayeRetry'),
@@ -55,7 +55,15 @@ exports.cdn = function(url, parameters) {
 exports.bootScript = function(url, parameters) {
   var options = parameters.hash;
 
-  var cdnFunc  = (options.skipCdn) ? function(a) { return '/' + a; } : cdn;
+  var cdnFunc;
+  if(options.skipCdn) {
+    cdnFunc = function(a) { return '/' + a; };
+  } else if(options.root) {
+    cdnFunc = function(a) { return options.root + a; };
+  } else {
+    cdnFunc = cdn;
+  }
+
   var skipCore = options.skipCore;
 
   var async    = 'async' in options ? options.async : true;
