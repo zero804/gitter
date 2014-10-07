42703e3cfb51b37f755d320e4b63bfe96aa5ae45
Andrew Newdigate <andrew@troupe.co>
2014-10-07 11:11:34 +0100

    issues list uses apiclient

--- public/js/components/apiClient.js
+++ public/js/components/apiClient.js
@@ -6,15 +6,23 @@ define([
   /* @const */
   var DEFAULT_TIMEOUT = 15 * 1000;
 
+  // If future this should direct clients to api.gitter.im
+  function makeFullUrl(url) {
+    // XXX: we need to remove /api/ from all calls to apiClient
+    if(url.indexOf('/api')) return url;
+
+    return '/api' + url;
+  }
+
   // TODO return a proper promise instead of a $.Deferred
   function operation(method, url, data, options) {
     if(!options) options = {};
 
-    var dataSerialized = JSON.stringify(data);
+    var dataSerialized = method === 'get' ? data : JSON.stringify(data);
 
     return $.ajax({
-      url: url,
-      contentType: "application/json",
+      url: makeFullUrl(url),
+      contentType: method === 'get' ? undefined : "application/json",
       dataType: "json",
       type: method,
       global: options.global === undefined ? true : options.global,
