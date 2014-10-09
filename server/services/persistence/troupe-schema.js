7e33897fd8949851f3d90b5bdf8fd53ec4060f35
Walter Carvalho <walter@gitter.im>
2014-10-09 11:46:30 +0100

    added owner to Troupe schema

--- server/services/persistence/troupe-schema.js
+++ server/services/persistence/troupe-schema.js
@@ -72,6 +72,7 @@ module.exports = {
       tags: [String],
       lcUri: { type: String, 'default': function() { return this.uri ? this.uri.toLowerCase() : null; }  },
       githubType: { type: String, 'enum': ['REPO', 'ORG', 'ONETOONE', 'REPO_CHANNEL', 'ORG_CHANNEL', 'USER_CHANNEL'], required: true },
+      owner: { type: String },
       status: { type: String, "enum": ['ACTIVE', 'DELETED'], "default": 'ACTIVE'},
       oneToOne: { type: Boolean, "default": false },
       users: [TroupeUserSchema],
