4f2a1ce533f86d43cad7ddf900493cf4623405dc
Walter Carvalho <walter@gitter.im>
2014-11-10 17:12:45 +0000

    disabling sync for uglify

--- build-scripts/selective-js-compile.sh
+++ build-scripts/selective-js-compile.sh
@@ -12,17 +12,17 @@ generate_md5() {
   fi
 }
 
-for i in $(find $ROOT_DIR/public-processed/js -maxdepth 1 -name '*.js' ! -name '*.min.js'); do 
+for i in $(find $ROOT_DIR/public-processed/js -maxdepth 1 -name '*.js' ! -name '*.min.js'); do
   min_file=${i%.js}.min.js
-  md5_file=$i.md5  
+  md5_file=$i.md5
   md5_checksum=$(generate_md5 $i)
-  
+
   #
   # A little bit of debugging
   #
   if [[ ! -f $md5_file ]]; then
     echo "$i: $md5_file does not exist" >&2
-  else 
+  else
     if [[ ! -f $min_file ]]; then
         echo "$i: $min_file does not exist" >&2
     else
@@ -31,21 +31,21 @@ for i in $(find $ROOT_DIR/public-processed/js -maxdepth 1 -name '*.js' ! -name '
       fi
     fi
   fi
-    
+
   if [[ ! -f $md5_file ]] || [[ ! -f $min_file ]] || [[ $md5_checksum != $(cat $md5_file) ]]; then
     echo $i needs updating >&2
-    
+
     rm -f $min_file ${min_file}.gz ${min_file}.map ${min_file}.map.gz
-    nice grunt manglejs --module $(basename $i) &
-    echo $md5_checksum > $md5_file 
+    nice grunt manglejs --module $(basename $i) # & disabling async
+    echo $md5_checksum > $md5_file
   else
     echo "$i is compiled" >&2
   fi
 done
 
-wait
+# wait
 
-for i in $(find $ROOT_DIR/public-processed/js -name '*.js' ! -name '*.min.js'); do 
+for i in $(find $ROOT_DIR/public-processed/js -name '*.js' ! -name '*.min.js'); do
   min_file=${i%.js}.min.js
   if [[ ! -f $min_file ]]; then
     echo Missing $min_file >&2
