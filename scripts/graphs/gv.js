4a1e362c79b735db9b3e68a021f74505f48e1f62
Andrew Newdigate <andrew@troupe.co>
2014-11-24 12:19:51 +0000

    Graph generation

--- scripts/graphs/gv.js
+++ scripts/graphs/gv.js
@@ -7,7 +7,15 @@ var persistenceService = require('../../server/services/persistence-service');
 // Create digraph G
 var g = graphviz.digraph("G");
 
-persistenceService.Troupe.findQ({}, 'oneToOne users.userId')
+var ONE_TO_ONE_EDGE_COLOR =  "#0000ff";
+var ORG_EDGE_COLOR =  "#00ff00";
+var OTHER_EDGE_COLOR =  "#ff0000";
+
+var COLOR1 = "#00d1a0";
+var COLOR2 = "#fe0063";
+var COLOR3 = "#ff8d3e";
+
+persistenceService.Troupe.findQ({}, 'oneToOne users.userId githubType security uri')
   .then(function(rooms) {
     rooms.forEach(function(room) {
       if(room.oneToOne) {
@@ -16,27 +24,38 @@ persistenceService.Troupe.findQ({}, 'oneToOne users.userId')
           var userId1 = room.users[0].userId;
           var userId2 = room.users[1].userId;
           if(userId1 && userId2) {
-            g.addNode("" + userId1, { shape: "point" } );
-            g.addNode("" + userId2, { shape: "point" } );
+            g.addNode("" + userId1, { shape: "point", color: COLOR1 } );
+            g.addNode("" + userId2, { shape: "point", color: COLOR1 } );
 
-            g.addEdge("" + userId1, "" + userId2);
-            g.addEdge("" + userId2, "" + userId1);
+            g.addEdge("" + userId1, "" + userId2, { color: COLOR1,  arrowhead: "none"  });
+            // g.addEdge("" + userId2, "" + userId1);
           }
         }
       } else {
         if(!room.users || !room.users.length) return;
-        g.addNode("" + room.id, { shape: "point" });
+        var color;
+        if(room.githubType === 'ORG' || room.security === 'PRIVATE' || room.security === 'INHERITED') {
+          color = COLOR2;
+        } else {
+          color = COLOR3;
+        }
+
+        // if(room.users.length > 100) {
+        //   g.addNode("" + room.id, { shape: "point", label: room.uri });
+        // } else {
+          g.addNode("" + room.id, { shape: "point", color: color });
+        // }
 
         room.users.forEach(function(roomUser) {
           if(roomUser.userId) {
-            g.addNode("" + roomUser.userId, { shape: "point" } );
-            g.addEdge("" + roomUser.userId, "" + room.id, { arrowhead: "halfopen" });
+            g.addNode("" + roomUser.userId, { shape: "point", color: COLOR1 } );
+            g.addEdge("" + roomUser.userId, "" + room.id, { color: color,  arrowhead: "none" });
           }
         });
       }
     });
 
-    fs.writeFileSync('d.dot',  g.to_dot());
+    fs.writeFileSync('prod2.dot',  g.to_dot());
     // g.output( "png", "test01.png" );
     process.exit(0);
   })
@@ -44,5 +63,3 @@ persistenceService.Troupe.findQ({}, 'oneToOne users.userId')
     console.error(err);
     process.exit(1);
   });
-
-
