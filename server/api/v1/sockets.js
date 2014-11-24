b6cb3a2c821b4986cf4665d5e250201e9dc22f17
Andrew Newdigate <andrew@troupe.co>
2014-11-24 16:59:44 +0000

    DELETE endpoint for socket fixed

--- server/api/v1/sockets.js
+++ server/api/v1/sockets.js
@@ -3,25 +3,19 @@
 
 var presenceService = require('../../services/presence-service');
 
-module.exports = {
-  destroy: function(req, res, next) {
-    var socketId = req.socket;
+module.exports = function(req, res, next) {
+  var socketId = String(req.params.socketId);
+  var userId = req.user && req.user.id;
 
-    presenceService.socketDisconnectionRequested(req.user.id, socketId, function(err) {
-      if(err) {
-        if(err.invalidSocketId) {
-          return res.send(400);
-        }
-
-        return next(err);
+  presenceService.socketDisconnectionRequested(userId, socketId, function(err) {
+    if(err) {
+      if(err.invalidSocketId) {
+        return res.send(400);
       }
 
-      res.send('OK');
-    });
-  },
+      return next(err);
+    }
 
-  load: function(id, done) {
-    return done(null, id);
-  }
-
+    res.send('OK');
+  });
 };
