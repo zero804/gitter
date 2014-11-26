5820b02776f430601d4eccf2fc92a3ec6703b5e9
Andy Trevorah <andy@troupe.co>
2014-11-26 11:58:27 +0000

    replaced winston with logger

--- server/api/v1/gcm.js
+++ server/api/v1/gcm.js
@@ -1,7 +1,7 @@
 /*jshint globalstrict:true, trailing:false, unused:true, node:true */
 "use strict";
 
-var winston = require('../../utils/winston');
+var logger = require('../../utils/env').logger;
 var pushNotificationService = require('../../services/push-notification-service');
 
 module.exports = function(req, res, next) {
@@ -11,7 +11,7 @@ module.exports = function(req, res, next) {
   var appVersion = req.body.version;
   var userId = req.user.id;
 
-  winston.info("GCM device registration", { deviceId: deviceId, deviceName: deviceName });
+  logger.info("GCM device registration", { deviceId: deviceId, deviceName: deviceName });
   pushNotificationService.registerAndroidDevice(deviceId, deviceName, registrationId, appVersion, userId, function(err) {
     if(err) return next(err);
 
