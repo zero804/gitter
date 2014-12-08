19d6dc9ed81f9df67f048d40fda62770eaca5845
Andrew Newdigate <andrew@troupe.co>
2014-12-08 18:46:07 +0000

    Fix for broken org member counting in billing application

--- server/api/private/org-members.js
+++ server/api/private/org-members.js
@@ -5,23 +5,36 @@ var GitHubOrgService   = require('../../services/github/github-org-service');
 var restSerializer     = require("../../serializers/rest-serializer");
 var userService        = require('../../services/user-service');
 var StatusError        = require('statuserror');
+var Q                  = require('q');
 
 /* Only org owners get to call this service */
 module.exports = function(req, res, next) {
   var uri = req.params.orgUri;
   var user = req.user;
 
-  if(!user) return next(401);
+  return Q.fcall(function() {
+      if(!user) throw new StatusError(401);
 
-  var ghOrg = new GitHubOrgService(req.user);
-  return ghOrg.getOwners(uri)
-    .then(function(owners) {
-      // If the user requesting the members list happens to be one of the owners  we'll return a
-      // list of the org members that are also Gitter users, just that intersection.
-      if (!owners.some(function(owner) { return owner.login === user.username; }))
-        throw new StatusError(403);
+      if(req.query.on_behalf_of) {
+        return userService.findById(req.query.on_behalf_of)
+          .then(function(user) {
+            if(!user) throw new StatusError(404);
 
-      return ghOrg.members(uri);
+            var ghOrg = new GitHubOrgService(user);
+            return ghOrg.members(uri);
+          });
+      }
+
+      var ghOrg = new GitHubOrgService(req.user);
+      return ghOrg.getOwners(uri)
+        .then(function(owners) {
+          // If the user requesting the members list happens to be one of the owners  we'll return a
+          // list of the org members that are also Gitter users, just that intersection.
+          if (!owners.some(function(owner) { return owner.login === user.username; }))
+            throw new StatusError(403);
+
+          return ghOrg.members(uri);
+        });
     })
     .then(function(orgMembers) {
       var usernames = orgMembers.map(function(user) { return user.login; });
@@ -30,13 +43,15 @@ module.exports = function(req, res, next) {
     .then(function(users) {
       return users.filter(function(user) { return !user.state; });
     })
-   .then(function(users) {
-     return restSerializer.serializeQ(users, new restSerializer.UserStrategy());
-    })
-    .then(function(serializeUsers) {
-      res.send(serializeUsers);
-    })
+    .then(function(users) {
+      if(req.query.count_only)
+        return res.send({ count: users.length });
 
-
+      /* Return the actual users */
+      return restSerializer.serializeQ(users, new restSerializer.UserStrategy())
+        .then(function(serializeUsers) {
+          res.send(serializeUsers);
+        });
+    })
     .fail(next);
 };
