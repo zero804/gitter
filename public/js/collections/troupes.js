13d240babc357ace53488fcbbc64c5e60a4519c5
Andy Trevorah <andy@troupe.co>
2014-12-12 15:23:47 +0000

    added more tests to room sort

--- public/js/collections/troupes.js
+++ public/js/collections/troupes.js
@@ -1,5 +1,4 @@
 "use strict";
-var context = require('utils/context');
 var apiClient = require('components/apiClient');
 var Backbone = require('backbone');
 var TroupeCollections = require('./base');
