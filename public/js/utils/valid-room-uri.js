04a00bd70bb08f2881030224b58fe7f0adb6ba85
Walter Carvalho <walter@gitter.im>
2014-12-19 18:56:23 +0000

    first stab at regexp

--- public/js/utils/valid-room-uri.js
+++ public/js/utils/valid-room-uri.js
@@ -3,7 +3,8 @@
 var RESERVED_RE = new RegExp('^\/(' + require('./vanity-keywords').join('|') + ')');
 
 module.exports = function (name) {
+  if (!name) return false;
   if (RESERVED_RE.test(name)) { return false; }
   if (/\/archives\/(all|\d{4}\/\d{2}\/\d{2})/.test(name)) { return false; }
-  return true;
+  return /\/[^]+/.test(name);
 };
