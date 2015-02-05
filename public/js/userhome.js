fef55d9c1f3734776c905927825f78ee32448cb9
Andrew Newdigate <andrew@troupe.co>
2015-02-05 14:37:31 +0000

    Switch contexts from userhome

--- public/js/userhome.js
+++ public/js/userhome.js
@@ -5,7 +5,7 @@ var Backbone = require('backbone');
 var confirmRepoRoomView = require('views/createRoom/confirmRepoRoomView');
 var modalRegion = require('components/modal-region');
 var onready = require('./utils/onready');
-
+var frameUtils = require('./utils/frame-utils');
 require('utils/tracking');
 
 // Preload widgets
@@ -16,10 +16,15 @@ onready(function() {
   new UserHomeView({ el: '#userhome' }).render();
 
   require('components/link-handler').installLinkHandler();
-  appEvents.on('navigation', function(url) {
-    // No pushState here. Open links within the parent...
-    // Remember that (window.parent === window) when there is no parent frame
-    window.parent.location.href = url;
+
+  appEvents.on('navigation', function(url, type, title) {
+    if(frameUtils.hasParentFrameSameOrigin()) {
+      frameUtils.postMessage({ type: "navigation", url: url, urlType: type, title: title});
+    } else {
+      // No pushState here. Open the link directly
+      // Remember that (window.parent === window) when there is no parent frame
+      window.parent.location.href = url;
+    }
   });
 
 
