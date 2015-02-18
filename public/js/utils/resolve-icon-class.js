1475839f4d9dfc8008124e9092de64cd7177a518
Walter Carvalho <walter@gitter.im>
2015-02-18 13:25:07 +0000

    now we have a custom set plus octicons

--- public/js/utils/resolve-icon-class.js
+++ public/js/utils/resolve-icon-class.js
@@ -5,21 +5,26 @@ module.exports = function (room) {
   room = ensurePojo(room);
   var iconName = room.githubType && room.githubType.toLowerCase();
 
+
   // repo_channel, user_channel etc
-  if (iconName && iconName.indexOf('channel') >= 0) {
-    iconName = 'hash';
+  if (iconName && iconName.indexOf('channel') > -1) {
+    iconName = 'icon-hash';
   }
 
-  if (iconName && iconName.indexOf('onetoone') >= 0) {
-    iconName = 'mention';
+  if (iconName && iconName.indexOf('onetoone') > -1) {
+    iconName = 'icon-mention';
   }
 
-  if (iconName === 'org') {
-    iconName = 'organization';
+  if (iconName && iconName.indexOf('repo') > -1) {
+    iconName = 'octicon octicon-repo';
+  }
+
+  if (iconName && iconName.indexOf('org') > -1) {
+    iconName = 'octicon octicon-organization';
   }
 
   if (room.favourite) {
-    iconName = 'star-1';
+    iconName = 'icon-star-1';
   }
 
   return iconName;
