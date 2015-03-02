2481bf4c89ccddf6c3323825e1ee9231a60fbd0e
Andrew Newdigate <andrew@troupe.co>
2015-03-02 11:38:43 +0000

    Aggressive caching

--- server/services/github/request-wrapper.js
+++ server/services/github/request-wrapper.js
@@ -5,23 +5,27 @@ var request = require('request');
 var fetchAllPages = require('./fetch-all-pages');
 var logFailingRequest = require('./log-failing-request');
 var logRateLimit = require('./log-rate-limit');
+var httpCache = require('./http-cache');
 var requestWithRetry = require('./request-with-retry');
 var publicTokenPool = require('./public-token-pool');
 
 module.exports = publicTokenPool(
                     fetchAllPages(
                       logFailingRequest(
+                        httpCache(
                         requestWithRetry({ maxRetries: 3 },
                           logRateLimit(
-                            request)))));
+                            request))))));
 
 module.exports.fastRequest = publicTokenPool(
                                 logFailingRequest(
-                                  logRateLimit(
-                                    request)));
+                                  httpCache(
+                                    logRateLimit(
+                                      request))));
 
 module.exports.firstPageOnlyRequest = publicTokenPool(
                                         logFailingRequest(
                                           requestWithRetry({ maxRetries: 3 },
                                             logRateLimit(
-                                              request))));
+                                              httpCache(
+                                              request)))));
