159a5ad565cb17f2a37fe40ea48c9ff4d6fc864c
Andrew Newdigate <andrew@troupe.co>
2015-03-09 16:03:34 +0200

    Moved nested request calls to request-extensible

--- server/services/github/fetch-all-pages.js
+++ server/services/github/fetch-all-pages.js
@@ -29,7 +29,7 @@ function updatePerPage(options) {
   return options;
 }
 
-module.exports = exports = function(request) {
+module.exports = exports = function(options, callback, request) {
   function subrequest(options) {
     var d = Q.defer();
 
@@ -51,70 +51,67 @@ module.exports = exports = function(request) {
     return d.promise;
   }
 
+  /* Allow clients to disable this */
+  if (options.firstPageOnly) {
+    return request(options, callback);
+  }
 
-  return function requestWrapper(options, callback) {
-    /* Allow clients to disable this */
-    if (options.firstPageOnly) {
-      return request(options, callback);
+  /* Always fetch 100 items per page */
+  options = updatePerPage(options);
+
+  request(options, function (error, response, body) {
+    if(error) return callback(error, response, body);
+    if(response.statusCode >= 400) return callback(error, response, body);
+
+    var remaining;
+
+    if(!response.headers.link) {
+      return callback(error, response, body);
     }
 
-    /* Always fetch 100 items per page */
-    options = updatePerPage(options);
+    var firstPage;
+    try {
+      firstPage = JSON.parse(body);
+    } catch(e) {
+      return callback(e, response, body);
+    }
 
-    request(options, function (error, response, body) {
-      if(error) return callback(error, response, body);
-      if(response.statusCode >= 400) return callback(error, response, body);
+    var links = parseLinks(response.headers.link);
+    if(links.next && links.last) {
+      var lastParsed = url.parse(links.last, true);
+      var lastPage = parseInt(lastParsed.query.page, 10);
 
-      var remaining;
+      if(lastPage > 1) {
+        remaining = lastPage - 1;
 
-      if(!response.headers.link) {
-        return callback(error, response, body);
+        winston.info('Fetching another ' + (lastPage - 1)  + ' pages of results');
+
+        var promises = lazy.range(1, lastPage + 1).map(function(i) {
+            if(i == 1) return firstPage;
+
+            var uri = url.parse(links.last, true);
+            delete uri.search;
+            uri.query.page = i;
+
+            var pageUrl = url.format(uri);
+
+            var clonedOptions = lazy(options).assign({ uri: pageUrl }).toObject();
+
+            return subrequest(clonedOptions);
+          }).toArray();
+
+        return Q.all(promises)
+          .then(function(results) {
+            var all = lazy(results).flatten().toArray();
+
+            callback(null, response, all);
+          })
+          .catch(function(error) {
+            callback(error);
+          });
       }
+    }
 
-      var firstPage;
-      try {
-        firstPage = JSON.parse(body);
-      } catch(e) {
-        return callback(e, response, body);
-      }
-
-      var links = parseLinks(response.headers.link);
-      if(links.next && links.last) {
-        var lastParsed = url.parse(links.last, true);
-        var lastPage = parseInt(lastParsed.query.page, 10);
-
-        if(lastPage > 1) {
-          remaining = lastPage - 1;
-
-          winston.info('Fetching another ' + (lastPage - 1)  + ' pages of results');
-
-          var promises = lazy.range(1, lastPage + 1).map(function(i) {
-              if(i == 1) return firstPage;
-
-              var uri = url.parse(links.last, true);
-              delete uri.search;
-              uri.query.page = i;
-
-              var pageUrl = url.format(uri);
-
-              var clonedOptions = lazy(options).assign({ uri: pageUrl }).toObject();
-
-              return subrequest(clonedOptions);
-            }).toArray();
-
-          return Q.all(promises)
-            .then(function(results) {
-              var all = lazy(results).flatten().toArray();
-
-              callback(null, response, all);
-            })
-            .catch(function(error) {
-              callback(error);
-            });
-        }
-      }
-
-      return callback(error, response, body);
-    });
-  };
+    return callback(error, response, body);
+  });
 };
