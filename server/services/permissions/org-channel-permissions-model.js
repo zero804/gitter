48530b547b3ddd000f6e91e674c495f93cc272f5
Mauro Pompilio <hackers.are.rockstars@gmail.com>
2015-03-11 18:13:45 +0000

    Unlimited repo rooms

--- server/services/permissions/org-channel-permissions-model.js
+++ server/services/permissions/org-channel-permissions-model.js
@@ -2,13 +2,10 @@
 "use strict";
 
 var userIsInRoom           = require('../user-in-room');
-var premiumOrThrow         = require('./premium-or-throw');
-var ownerIsEarlyAdopter    = require('../owner-is-early-adopter');
-
 var commonPermissionsModel = require('./common-org-repo-channel-permissions-model');
 var orgPermissionsModel    = require('./org-permissions-model');
 
 /**
  * ORG_CHANNEL permissions model
  */
-module.exports = commonPermissionsModel(orgPermissionsModel, userIsInRoom, premiumOrThrow, ownerIsEarlyAdopter);
+module.exports = commonPermissionsModel(orgPermissionsModel, userIsInRoom);
--- server/services/permissions/premium-or-throw.js
+++ server/services/permissions/premium-or-throw.js
@@ -1,14 +1,13 @@
 /*jshint globalstrict:true, trailing:false, unused:true, node:true */
 "use strict";
 
-var env          = require('../../utils/env');
 var uriIsPremium = require('../uri-is-premium');
 var StatusError  = require('statuserror');
 
 module.exports = function premiumOrThrow(uri) {
   return uriIsPremium(uri)
     .then(function(isPremium) {
-      if(isPremium || env.config.get('premium:disabled')) return true;
+      if (isPremium) return true;
 
       var err = new StatusError(402 /* Payment required */, 'Payment required');
       err.uri = uri;
