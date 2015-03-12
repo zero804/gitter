d8fdb25df95c14573d90a8edcc3a01aca570091e
Andy Trevorah <andy@troupe.co>
2015-03-12 11:32:48 +0000

    changed 402 page to mention user limits

--- gulpfile.js
+++ gulpfile.js
@@ -300,7 +300,7 @@ gulp.task('css-web', function () {
   return gulp.src([
     'public/less/signup.less',
     'public/less/trpAppsPage.less',
-    'public/less/oauth-error-page.less',
+    'public/less/error-page.less',
     'public/less/generic-layout.less',
     'public/less/trpHooks.less',
     'public/less/login.less',
