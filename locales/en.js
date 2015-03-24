c8717ad38bf6804353cd264efec9e7869e1164e0
Mike Bartlett <mike.bartlett@gmail.com>
2015-03-24 14:22:06 +0000

    styled payment required page

--- locales/en.js
+++ locales/en.js
@@ -15,5 +15,17 @@
 	"Unread messages from %%s, %%s and one other": {
 		"one": "Unread messages from %%s, %%s and one other",
 		"other": "Unread messages from %%s, %%s and %d others"
-	}
+	},
+	"Organisation pricing": "Organisation pricing",
+	"Free": "Free",
+	"per user/month": "per user/month",
+	"<b>Unlimited</b> chat history": "<b>Unlimited</b> chat history",
+	"<b>Unlimited</b> integrations": "<b>Unlimited</b> integrations",
+	"<b>Unlimited</b> public rooms": "<b>Unlimited</b> public rooms",
+	"<b>Unlimited</b> private rooms": "<b>Unlimited</b> private rooms",
+	"Limited to 25 users per private room": "Limited to 25 users per private room",
+	"Pro": "Pro",
+	"<b>Unlimited</b> users per private room": "<b>Unlimited</b> users per private room",
+	"<b>Purchase</b> now": "<b>Purchase</b> now",
+	"Prices may be subject to VAT. By signing up you agree to our <a %s>Terms and Conditions</a>.": "Prices may be subject to VAT. By signing up you agree to our <a %s>Terms and Conditions</a>."
 }
