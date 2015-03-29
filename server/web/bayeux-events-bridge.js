5f64a9855d2b7d5b7c3633d9ca1b478facb44c68
Andy Trevorah <andy@troupe.co>
2015-03-29 23:47:20 +0100

    Revert "Merge branch 'hotfix/9.8.3'"
    9.8.3 was causing self ddosing
    This reverts commit 79033075648d0dfe2a118977fde5efaedc1166a6, reversing
    changes made to 2e5fdcc98e0e8e9c7ab82984721261fde3f36d27.

--- server/web/bayeux-events-bridge.js
+++ server/web/bayeux-events-bridge.js
@@ -3,7 +3,6 @@
 
 var winston           = require('../utils/winston');
 var appEvents         = require("../app-events");
-var bayeuxTransition  = require('./bayeux-transition');
 var bayeux            = require('./bayeux');
 var ent               = require('ent');
 var presenceService   = require("../services/presence-service");
@@ -24,17 +23,14 @@ function findFailbackChannel(channel) {
 
 exports.install = function() {
   var bayeuxClient = bayeux.client;
-  var bayeuxTransitionClient = bayeuxTransition.client;
 
   function publish(channel, message) {
     bayeuxClient.publish(channel, message);
-    bayeuxTransitionClient.publish(channel, message);
 
     var failbackChannel = findFailbackChannel(channel);
 
     if(failbackChannel) {
       bayeuxClient.publish(failbackChannel, message);
-      bayeuxTransitionClient.publish(failbackChannel, message);
     }
   }
 
@@ -254,3 +250,4 @@ exports.install = function() {
 
   });
 };
+
