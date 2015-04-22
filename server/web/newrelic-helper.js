cd57848385c0e22b1dbe2774ca4b6c562f979ecb
Mauro Pompilio <hackers.are.rockstars@gmail.com>
2015-04-22 15:37:58 +0100

    NewRelic browser trackign per environment

--- server/web/newrelic-helper.js
+++ server/web/newrelic-helper.js
@@ -4,5 +4,9 @@ var env = require('../utils/env');
 var template = require('./compile-web-template')('/js/views/tmpl/newrelic');
 
 module.exports = function () {
-  return (env.config.get('newrelic:enabled')) ? template() : '';
+  if (env.config.get('newrelic:enabled')) {
+    var key = env.config.get('newrelic:browserkey');
+    var id  = env.config.get('newrelic:browserid');
+    return template({key: key, id: id});
+  }
 };
