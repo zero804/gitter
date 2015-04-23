b6294ca11f1ab259738a73bfb4a4141a62311d9d
Andy Trevorah <andy@troupe.co>
2015-04-23 18:25:57 +0100

    debouncing user search on typeahead

--- public/js/views/chat/typeaheads/user-typeahead.js
+++ public/js/views/chat/typeaheads/user-typeahead.js
@@ -25,6 +25,58 @@ function isNotCurrentUser(user) {
   return user.username.toLowerCase() !== lcUsername;
 }
 
+var lastTerm;
+var lastCallback;
+var debounceCallback;
+
+// the textcomplete library requires *all* async
+// functions to call their callback.
+//
+// So debouncing has to be custom.
+function userSearchDebounced(term, callback) {
+  if (lastCallback) {
+    // kill the old callback
+    lastCallback([]);
+  }
+
+  // long live the new callback!
+  lastTerm = term;
+  lastCallback = callback;
+
+  if (!debounceCallback) {
+    debounceCallback = setTimeout(function() {
+      // allow new search to be debounced
+      debounceCallback = null;
+
+      // callback and term now belong to userSearch()
+      var requestTerm = lastTerm;
+      var requestCallback = lastCallback;
+      lastTerm = null;
+      lastCallback = null;
+
+      userSearch(requestTerm, requestCallback);
+    }, 500);    
+  }
+}
+
+function userSearch(term, callback) {
+  apiClient.room.get('/users', { q: term, limit: MAX_TYPEAHEAD_SUGGESTIONS })
+    .then(function(users) {
+      var lowerTerm = term.toLowerCase();
+
+      users = users.filter(isNotCurrentUser);
+
+      if (context().permissions.admin && '/all'.indexOf(lowerTerm) === 0) {
+        users.unshift({ username: '/all', displayName: 'Group' });
+      }
+
+      callback(users);
+    })
+    .fail(function() {
+      callback([]);
+    });
+}
+
 module.exports = {
   match: /(^|\s)@(\/?[a-zA-Z0-9_\-]*)$/,
   maxCount: MAX_TYPEAHEAD_SUGGESTIONS,
@@ -44,21 +96,7 @@ module.exports = {
       return callback(users);
     }
 
-    apiClient.room.get('/users', { q: term, limit: MAX_TYPEAHEAD_SUGGESTIONS })
-      .then(function(users) {
-        var lowerTerm = term.toLowerCase();
-
-        users = users.filter(isNotCurrentUser);
-
-        if (context().permissions.admin && '/all'.indexOf(lowerTerm) === 0) {
-          users.unshift({ username: '/all', displayName: 'Group' });
-        }
-
-        callback(users);
-      })
-      .fail(function() {
-        callback([]);
-      });
+    userSearchDebounced(term, callback);
   },
   template: function(user) {
     return template({
