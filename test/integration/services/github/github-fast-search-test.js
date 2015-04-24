f8b6defa01e2ab09bb948bfee95312340f3c4f01
Andrew Newdigate <andrew@troupe.co>
2015-04-24 12:52:54 +0100

    Extra test for ensuring that only one page is ever returned

--- test/integration/services/github/github-fast-search-test.js
+++ test/integration/services/github/github-fast-search-test.js
@@ -20,4 +20,16 @@ describe('github-fast-search #slow', function() {
       .nodeify(done);
   });
 
+  it('should not find more than one page of results', function(done) {
+    var search = new Search(FAKE_USER);
+
+    search.findUsers('andrew')
+      .then(function(results) {
+        assert(Array.isArray(results));
+        assert.strictEqual(results.length, 30);
+        assert(results[0]);
+      })
+      .nodeify(done);
+  });
+
 });
