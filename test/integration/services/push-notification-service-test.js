43e017ad1e2020baa266ca063a1ec97e53a89045
Andrew Newdigate <andrew@troupe.co>
2015-04-27 15:18:48 +0100

    Additional test for push notification service

--- test/integration/services/push-notification-service-test.js
+++ test/integration/services/push-notification-service-test.js
@@ -6,10 +6,40 @@ var testRequire = require('../test-require');
 
 var pushNotificationService = testRequire('./services/push-notification-service');
 var persistenceService = testRequire('./services/persistence-service');
+var mongoUtils = testRequire('./utils/mongo-utils');
 
 var assert = require("assert");
 
 describe('push-notification-service', function() {
+  describe('findUsersWithDevices', function() {
+    it('should find a user with a registered device', function(done) {
+      var userId = mongoUtils.getNewObjectIdString();
+
+      pushNotificationService.testOnly.expireCachedUsersWithDevices();
+
+      return pushNotificationService.registerUser('DEVICE1', userId)
+        .then(function() {
+          return pushNotificationService.findUsersWithDevices([userId]);
+        })
+        .then(function(userIds) {
+          assert.strictEqual(userIds.length, 1);
+          assert.equal(userIds[0], userId);
+        })
+        .nodeify(done);
+    });
+
+    it('should not find user without a registered device', function(done) {
+      var userId = mongoUtils.getNewObjectIdString();
+
+      return pushNotificationService.findUsersWithDevices([userId])
+        .then(function(userIds) {
+          assert.strictEqual(userIds.length, 0);
+        })
+        .nodeify(done);
+    });
+
+  });
+
   describe('device registration', function() {
     it('should prune unused old devices', function(done) {
       var token = 'TESTTOKEN';
