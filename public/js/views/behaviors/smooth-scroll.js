5858a93796cdae7e0252f843e1d85271310f0f5f
Mauro Pompilio <hackers.are.rockstars@gmail.com>
2015-04-29 18:20:37 +0100

    Unread banners working with the smooth scroller

--- public/js/views/behaviors/smooth-scroll.js
+++ public/js/views/behaviors/smooth-scroll.js
@@ -2,6 +2,9 @@
 var Marionette = require('marionette');
 var behaviourLookup = require('./lookup');
 var _ = require('underscore');
+var unreadBannerModel = require('../app/unreadBannerModel');
+var context = require('utils/context');
+var realtime = require('components/realtime');
 
 var Behavior = Marionette.Behavior.extend({
   defaults: {
@@ -19,27 +22,40 @@ var Behavior = Marionette.Behavior.extend({
     this.lazyDecorator      = _.debounce(this.decorateIfVisible.bind(this), 500);
     this.lazyTracker        = _.debounce(this.trackViewport.bind(this), 500);
     this.lazyPointerEvents  = _.debounce(this.enablePointerEvents.bind(this), 250);
+    this.lazyUnreadItems    = _.throttle(this.updateUnreadBanners.bind(this), 250);
 
     this.scrollHandler = this.smoothScroll.bind(this);
     this.scrollElement.addEventListener('scroll', this.scrollHandler, false);
 
     setTimeout(this.decorateIfVisible.bind(this), 100);
+    setTimeout(this.updateUnreadBanners.bind(this), 100);
+
+    var subscription = '/v1/user/' + context.getUserId() + '/rooms/' + context.getTroupeId() + '/unreadItems';
+    realtime.subscribe(subscription, this.updateUnreadBanners.bind(this));
   },
 
   decorateIfVisible: function() {
     this.view.children.each(function(child) {
-      if (this.isElementInViewport(child.el)) child.trigger('decorate');
+      if (this.viewportStatus(child.el) === 'visible') child.trigger('messageInViewport');
     }.bind(this));
   },
 
-  isElementInViewport: function(el) {
+  updateUnreadBanners: function() {
+    var items = {'visible': 0, 'above': 0, 'below': 0};
+    this.view.children.each(function(child) {
+      var pos = this.viewportStatus(child.el);
+      if (child.model.get('unread')) items[pos] += 1;
+    }.bind(this));
+
+    unreadBannerModel.set('unreadAbove', items.above);
+    unreadBannerModel.set('unreadBelow', items.below);
+  },
+
+  viewportStatus: function(el) {
     var rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+    if (rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)) return 'visible';
+    if (rect.top >= 0 && rect.bottom >= (window.innerHeight || document.documentElement.clientHeight)) return 'below';
+    if (rect.top <= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)) return 'above';
   },
 
   trackViewport: function() {
@@ -61,6 +77,7 @@ var Behavior = Marionette.Behavior.extend({
     this.lazyDecorator();
     this.lazyTracker();
     this.lazyPointerEvents();
+    this.lazyUnreadItems();
   },
 
   onClose: function() {
