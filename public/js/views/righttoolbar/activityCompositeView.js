11bae194334a1f047a5e5db2faa643ae6bc994f8
Andrew Newdigate <andrew@troupe.co>
2015-05-14 14:50:18 +0100

    Prerendered events back on again

--- public/js/views/righttoolbar/activityCompositeView.js
+++ public/js/views/righttoolbar/activityCompositeView.js
@@ -22,12 +22,11 @@ var jenkinsTemplate = require('./tmpl/jenkins.hbs');
 var travisTemplate = require('./tmpl/travis.hbs');
 var sprintlyTemplate = require('./tmpl/sprintly.hbs');
 var trelloTemplate = require('./tmpl/trello.hbs');
-var prerenderedTemplate = require('./tmpl/prerendered.hbs');
+var prerenderedTemplate = require('./tmpl/activity-item-prerendered.hbs');
 var activityTemplate = require('./tmpl/activity-composite.hbs');
 var activityEmptyTemplate = require('./tmpl/activity-empty.hbs');
+var activityDecorators = require('shared/activity/activity-decorators');
 var context = require('utils/context');
-var cocktail = require('cocktail');
-var LoadingCollectionMixin = require('views/loading-mixin');
 
 require('views/widgets/timeago');
 require('views/behaviors/widgets');
@@ -56,109 +55,6 @@ module.exports = (function() {
     watch:          githubWatchTemplate
   };
 
-  var decorators = {
-    trello: function(meta, payload) { // jshint unused:true
-      var trello_actions = {
-        updateCard:   'updated',
-        createCard:   'created',
-        commentCard:  'commented',
-        voteOnCard:   'voted',
-      };
-      return {trello_action: trello_actions[payload.action.type]};
-    },
-    sprintly: function(meta, payload) { // jshint unused:true
-      var extra = {};
-
-      if (payload.model == "Item") {
-        extra.sprintly_action = "created";
-      } else if (payload.model == "Comment") {
-        extra.sprintly_action = "commented on";
-      }
-      return extra;
-    },
-    bitbucket: function(meta) {
-      // meta.commits is undefined for empty arrays in old hooks.
-      // this is because of issue #233 (form encoding was removing empty arrays)
-      var commits = meta.commits || [];
-      return {
-        first_commit: commits[0],
-        is_single_commit: commits.length === 1,
-        commits_count: commits.length
-      };
-    },
-    github: function(meta, payload) {
-      var extra = {};
-
-      if (meta.event == 'push') {
-        // if you push a tag, then commits is undefined
-        var commits = payload.commits || [];
-        var commitCount = commits.length;
-
-        extra.commits = commits;
-        extra.commits.forEach(function(commit){
-          commit.short_sha = commit.id.substring(0,7);
-          var message = commit.message;
-          commit.short_message = (message.length > 32) ? message.substr(0,31) + '…' : message;
-        });
-
-        if(commitCount > 3) {
-          extra.commits = extra.commits.slice(0,3);
-          extra.hidden_commit_count = commitCount - 3;
-        }
-
-        extra.commits_count = commitCount;
-
-      } else if (meta.event == 'gollum') {
-        extra.wiki_url = payload.pages[0].html_url;
-        extra.wiki_page = payload.pages[0].page_name;
-      } else if (meta.event === 'commit_comment') {
-        // fall back to payload data for old hooks
-        extra.commit = meta.commit || {
-          id: payload.comment.commit_id,
-          short_sha: payload.comment.commit_id.substring(0,7),
-        };
-      }
-      return extra;
-    },
-    jenkins: function(meta, payload) { // jshint unused:true
-      var status = payload.build.status ? payload.build.status.toLowerCase() : payload.build.phase.toLowerCase();
-      return { build_status: status };
-    },
-    travis: function(meta, payload) { // jshint unused:true
-      var extra = {};
-      var status = payload.status_message ? payload.status_message.toLowerCase() : '';
-      extra.build_status = (status === 'still failing') ? 'failing' : status;
-      return extra;
-    },
-    huboard: function(meta) {
-      var extra = {};
-      var column = meta.column;
-      var previousColumn = meta.previousColumn;
-
-      if(meta.milestone) {
-        extra.context = 'to '+meta.milestone;
-      } else if(meta.status) {
-        extra.context = 'to '+meta.status;
-      } else if(column) {
-        extra.context = previousColumn ? 'from '+previousColumn+' to '+column : 'in '+column;
-      }
-      return extra;
-    }
-  };
-
-  function getExtraRenderData(meta, payload) {
-    var decorator = decorators[meta.service];
-    var extra = decorator ? decorator(meta, payload) : {};
-
-    // Support branch names with slashes, ie: develop/feature/123-foo
-    if (payload && payload.ref) {
-      var refs = payload.ref.split('/');
-      extra.branch_name = refs[3] ? refs[3] : refs[2];
-      extra.repo = payload.repository.owner.name + '/' + payload.repository.name;
-    }
-
-    return extra;
-  }
 
   var ActivityItemView = Marionette.ItemView.extend({
     tagName: 'li',
@@ -198,7 +94,7 @@ module.exports = (function() {
           html: html
         };
 
-        var extra = meta.prerendered ? {} : getExtraRenderData(meta, payload);
+        var extra = meta.prerendered ? {} : activityDecorators(meta, payload);
         return _.extend(core, extra);
       } catch (e) {
         var modelData = this.model && this.model.attributes;
@@ -234,6 +130,13 @@ module.exports = (function() {
     template: activityTemplate,
     childViewContainer: '#activity-list',
     childView: ActivityItemView,
+    childViewOptions: function(item) {
+      if(item && item.id) {
+        // This allows the chat collection view to bind to an existing element...
+        var e = this.$el.find('.model-id-' + item.id)[0];
+        if(e) return { el: e };
+      }
+    },
     getEmptyView: function() {
       // Admins see "Configure your integrations" empty
       if (context().permissions.admin) return ActivityEmptyItemView;
@@ -252,9 +155,28 @@ module.exports = (function() {
       // so that they get to
       var headerVisible = !!(context().permissions.admin || this.collection.length);
       this.ui.header.toggle(headerVisible);
+    },
+    _renderTemplate: function() {
+      var data = {};
+      data = this.serializeData();
+      data = this.mixinTemplateHelpers(data);
+
+      this.triggerMethod('before:render:template');
+
+      var template = this.getTemplate();
+      if (template !== false) {
+        var html = Marionette.Renderer.render(template, data, this);
+        this.attachElContent(html);
+      }
+
+      // the ui bindings is done here and not at the end of render since they
+      // will not be available until after the model is rendered, but should be
+      // available before the collection is rendered.
+      this.bindUIElements();
+      this.triggerMethod('render:template');
     }
   });
-  // cocktail.mixin(ActivityView, LoadingCollectionMixin);
+
 
   return ActivityView;
 
