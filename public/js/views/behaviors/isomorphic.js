5e2997cbd2cfc432a27ffc7c6f5c3d078431bbe1
Andrew Newdigate <andrew@troupe.co>
2015-05-19 12:14:45 +0100

    WIP IsoMorphic2

--- public/js/views/behaviors/isomorphic.js
+++ public/js/views/behaviors/isomorphic.js
@@ -72,4 +72,60 @@ var Behavior = Marionette.Behavior.extend({
 
 behaviourLookup.register('Isomorphic', Behavior);
 
+var Behavior2 = Marionette.Behavior.extend({
+  onRender: function() {
+    this.setupRegions();
+  },
+
+  onBeforeShow: function() {
+    this.setupRegions();
+  },
+
+  setupRegions: function() {
+    /* Only perform setup once */
+    if (this.setup) return;
+    this.setup = 1;
+
+    var view = this.view;
+    var isoRegions = this.options;
+
+    Object.keys(isoRegions).forEach(function(regionName) {
+      var isoRegionDefn = isoRegions[regionName];
+      var el = isoRegionDefn.el;
+      var region = view.addRegion(regionName, el);
+      var initMethod = isoRegionDefn.init;
+
+      if(!initMethod) return;
+
+      function optionsForRegion(options) {
+        var regionEl = region.$el[0];
+        if (!regionEl)
+          throw new Error('Region ' + regionName + ' does not exist.');
+
+        var regionElChildLen = regionEl.children.length;
+
+        var baseOptions;
+        if (regionElChildLen === 0) {
+          baseOptions = {};
+        } else if (regionElChildLen === 1) {
+          baseOptions = { template: false, el: regionEl.children[0] };
+        } else {
+          throw new Error('Region can have zero or one elements, but not more. Region ' + regionName + " has " + regionElChildLen + ". Are you sure you wrapped the region with a parent?");
+        }
+
+        if(!options) return baseOptions;
+        return _.extend(baseOptions, options);
+      }
+
+      var childView = initMethod.call(view, optionsForRegion);
+      if(childView) {
+        view.showChildView(regionName, childView);
+      }
+    });
+
+  }
+});
+
+behaviourLookup.register('Isomorphic2', Behavior2);
+
 module.exports = Behavior;
