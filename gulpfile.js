ef8feff75e6c104c76c7df32a7956b9c5ece1b4a
Andy Trevorah <andy@troupe.co>
2015-05-19 17:20:03 +0100

    fixed modal buttons by removing cssnano

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,6 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer-core');
 var mqpacker = require('css-mqpacker');
 var csswring = require('csswring');
-var cssnano = require('gulp-cssnano');
 var mkdirp = require('mkdirp');
 var gulpif = require('gulp-if');
 var sourcemaps = require('gulp-sourcemaps');
@@ -273,9 +272,8 @@ gulp.task('css-ios', function () {
         cascade: false
       }),
       mqpacker,
-      // csswring
+      csswring
     ]))
-    .pipe(cssnano())
     .pipe(gulpif(DEV_MODE, sourcemaps.write('output/assets/styles')))
     .pipe(gulp.dest('output/assets/styles'));
 });
@@ -304,9 +302,8 @@ gulp.task('css-mobile', function () {
         cascade: false
       }),
       mqpacker,
-      // csswring
+      csswring
     ]))
-    .pipe(cssnano())
     .pipe(gulpif(DEV_MODE, sourcemaps.write('output/assets/styles')))
     .pipe(gulp.dest('output/assets/styles'));
 });
@@ -353,9 +350,8 @@ gulp.task('css-web', function () {
         cascade: false
       }),
       mqpacker,
-      // csswring
+      csswring
     ]))
-    .pipe(cssnano())
     .pipe(gulpif(DEV_MODE, sourcemaps.write('output/assets/styles')))
     .pipe(gulp.dest('output/assets/styles'));
 });
