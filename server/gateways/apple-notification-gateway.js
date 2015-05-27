584f920b393416197d5fe40aea0315432b247af1
Andy Trevorah <andy@troupe.co>
2015-05-27 15:30:18 +0100

    moved ios push notifications into its own module

---
+++ server/gateways/apple-notification-gateway.js
@@ -0,0 +1,144 @@
+/*jshint globalstrict:true, trailing:false, unused:true, node:true */
+"use strict";
+
+
+var apn = require('apn');
+var env = require('../utils/env');
+var logger = env.logger;
+var config = env.config;
+var rootDirname = __dirname+'/../..';
+
+var ERROR_DESCRIPTIONS = {
+  0: 'No errors encountered',
+  1: 'Processing error',
+  2: 'Missing device token',
+  3: 'Missing topic',
+  4: 'Missing payload',
+  5: 'Invalid token size',
+  6: 'Invalid topic size',
+  7: 'Invalid payload size',
+  8: 'Invalid token',
+  255: 'None (unknown)'
+};
+
+logger.info('Starting APN');
+var connections = {
+  'APPLE': createConnection('Prod'),
+  'APPLE-DEV': createConnection('Dev'),
+  'APPLE-BETA': createConnection('Beta'),
+  'APPLE-BETA-DEV': createConnection('BetaDev')
+};
+
+// setup feedback listeners
+createFeedbackListener('Prod');
+createFeedbackListener('Dev');
+createFeedbackListener('Beta');
+createFeedbackListener('BetaDev');
+
+var sendNotificationToDevice = function(notification, badge, device, callback) {
+  var appleNotification = createAppleNotification(notification, badge);
+
+  var deviceToken = new apn.Device(device.appleToken);
+
+  var connection = connections[device.deviceType];
+
+  if (!connection) return callback(new Error('unknown device type: ' + device.deviceType));
+
+  connection.pushNotification(appleNotification, deviceToken);
+
+  return callback();
+};
+
+function createConnection(suffix) {
+  var connection = new apn.Connection({
+    cert: rootDirname + '/' + config.get('apn:cert' + suffix),
+    key: rootDirname + '/' + config.get('apn:key' + suffix),
+    gateway: config.get('apn:gateway' + suffix),
+    enhanced: true,
+    errorCallback: function(errCode, notification) {
+      try {
+        var errorDescription = ERROR_DESCRIPTIONS[errCode];
+
+        if(errCode === 8 && notification.pushDevice) {
+          logger.error('Removing invalid device ', {
+            deviceName: notification.pushDevice.deviceName,
+            deviceId: notification.pushDevice.deviceId
+          });
+
+          notification.pushDevice.remove();
+          return;
+        }
+
+        logger.error('APN error', {
+          exception: errCode,
+          errorDescription: errorDescription,
+          notification: notification ? notification.payload : null
+        });
+      } catch (e) {
+        logger.error('Error while handling APN error:' + e, {exception: e});
+      }
+    },
+    connectionTimeout: 60000
+  });
+
+  connection.on('error', function(err) {
+    logger.error('APN service (' + suffix + ') experienced an error', { error: err.message });
+  });
+
+  return connection;
+}
+
+function createFeedbackListener(suffix) {
+  try {
+
+    var feedback = new apn.Feedback({
+        cert: rootDirname+'/'+config.get('apn:cert' + suffix),
+        key: rootDirname+'/'+config.get('apn:key' + suffix),
+        gateway: config.get('apn:feedback' + suffix),
+        interval: config.get('apn:feedbackInterval' + suffix),
+        batchFeedback: true
+    });
+
+    feedback.on('feedback', function(devices) {
+      if(devices.length) {
+        logger.error('Failed delivery. Need to remove the following devices', { deviceCount: devices.length });
+      }
+    });
+
+    feedback.on('error', function(err) {
+      logger.error('Feedback service experienced an error', { error: err.message });
+    });
+
+  } catch(e) {
+    logger.error('Unable to start feedback service ', { exception: e });
+  }
+}
+
+function createAppleNotification(notification, badge) {
+  var note = new apn.Notification();
+  var message = notification && notification.message;
+  var sound = notification && notification.sound;
+  var link = notification && notification.link;
+
+  if(badge >= 0) {
+    note.badge = badge;
+  }
+
+  if(message) {
+    note.setAlertText(message);
+  }
+
+  if(sound) {
+    note.sound = sound;
+  }
+
+  if(link) {
+    /* For IOS7 push notifications */
+    note.contentAvailable = true;
+
+    note.payload = { 'l': link };
+  }
+  return note;
+}
+
+module.exports.sendNotificationToDevice = sendNotificationToDevice;
--- server/gateways/push-notification-gateway.js
+++ server/gateways/push-notification-gateway.js
@@ -3,211 +3,44 @@
 
 var env = require('../utils/env');
 var logger = env.logger;
-var config = env.config;
 var stats = env.stats;
 
 var pushNotificationService = require("../services/push-notification-service");
 var unreadItemService = require("../services/unread-item-service");
-var apns = require('apn');
 var workerQueue = require('../utils/worker-queue');
 var androidGateway = require('./android-notification-gateway');
-
-var errorDescriptions = {
-  0: 'No errors encountered',
-  1: 'Processing error',
-  2: 'Missing device token',
-  3: 'Missing topic',
-  4: 'Missing payload',
-  5: 'Invalid token size',
-  6: 'Invalid topic size',
-  7: 'Invalid payload size',
-  8: 'Invalid token',
-  255: 'None (unknown)'
-};
-
-var rootDirname = __dirname+'/../..';
-
-function errorEventOccurred(err, notification) {
-  try {
-    var errorDescription = errorDescriptions[err];
-
-    if(err === 8 && notification.pushDevice) {
-      logger.error("Removing invalid device ", { deviceName: notification.pushDevice.deviceName, deviceId: notification.pushDevice.deviceId });
-      notification.pushDevice.remove();
-      return;
-    }
-
-    logger.error("APN error", {
-      exception: err,
-      errorDescription: errorDescription,
-      notification: notification ? notification.payload : null
-    });
-  } catch (e) {
-    logger.error("Error while handling APN error:" + e, {exception: e});
-  }
-}
-
-logger.info("Starting APN");
-var apnsConnectionDev = new apns.Connection({
-    cert: rootDirname+'/'+config.get('apn:certDev'),
-    key: rootDirname+'/'+config.get('apn:keyDev'),
-    gateway: config.get('apn:gatewayDev'),
-    enhanced: true,
-    errorCallback: errorEventOccurred,
-    connectionTimeout: 60000
-});
-
-apnsConnectionDev.on("error", function(err) {
-    logger.error("APN service (dev) experienced an error", { error: err.message });
-});
-
-var apnsConnectionBetaDev = new apns.Connection({
-    cert: rootDirname+'/'+config.get('apn:certBetaDev'),
-    key: rootDirname+'/'+config.get('apn:keyBetaDev'),
-    gateway: config.get('apn:gatewayBetaDev'),
-    enhanced: true,
-    errorCallback: errorEventOccurred,
-    connectionTimeout: 60000
-});
-
-apnsConnectionBetaDev.on("error", function(err) {
-    logger.error("APN service (beta dev) experienced an error", { error: err.message });
-});
-
-var apnsConnectionBeta = new apns.Connection({
-    cert: rootDirname+'/'+config.get('apn:certBeta'),
-    key: rootDirname+'/'+config.get('apn:keyBeta'),
-    gateway: config.get('apn:gatewayBeta'),
-    enhanced: true,
-    errorCallback: errorEventOccurred,
-    connectionTimeout: 60000
-});
-
-apnsConnectionBeta.on("error", function(err) {
-    logger.error("APN service (beta) experienced an error", { error: err.message });
-});
-
-var apnsConnection = new apns.Connection({
-    cert: rootDirname+'/'+config.get('apn:certProd'),
-    key: rootDirname+'/'+config.get('apn:keyProd'),
-    gateway: config.get('apn:gatewayProd'),
-    enhanced: true,
-    errorCallback: errorEventOccurred,
-    connectionTimeout: 60000
-});
-
-apnsConnection.on("error", function(err) {
-    logger.error("APN service (prod) experienced an error", { error: err.message });
-});
-
-
-['Dev', 'BetaDev', 'Beta', 'Prod'].forEach(function(suffix) {
-  try {
-
-    var feedback = new apns.Feedback({
-        cert: rootDirname+'/'+config.get('apn:cert' + suffix),
-        key: rootDirname+'/'+config.get('apn:key' + suffix),
-        gateway: config.get('apn:feedback' + suffix),
-        interval: config.get('apn:feedbackInterval' + suffix),
-        batchFeedback: true
-    });
-
-    feedback.on("feedback", function(devices) {
-      if(devices.length) {
-        logger.error("Failed delivery. Need to remove the following devices", { deviceCount: devices.length });
-      }
-    });
-
-    feedback.on("error", function(err) {
-        logger.error("Feedback service experienced an error", { error: err.message });
-    });
-
-  } catch(e) {
-    logger.error('Unable to start feedback service ', { exception: e });
-  }
-});
-
+var appleGateway = require('./apple-notification-gateway');
 
 function sendNotificationToDevice(notification, badge, device) {
-  var sent = false;
-  var message = notification && notification.message;
-  var sound = notification && notification.sound;
-  var link = notification && notification.link;
 
   if((device.deviceType === 'APPLE' ||
       device.deviceType === 'APPLE-DEV' ||
       device.deviceType === 'APPLE-BETA' ||
       device.deviceType === 'APPLE-BETA-DEV') && device.appleToken) {
     logger.info("push-gateway: Sending apple push notification", { userId: device.userId, notification: notification, badge: badge });
-    var note = new apns.Notification();
 
-    if(badge >= 0) {
-      note.badge = badge;
-    }
+    appleGateway.sendNotificationToDevice(notification, badge, device, function(err) {
+      if(err) return logger.error('apple push notification failed', { err: err });
 
-    if(message) {
-      note.setAlertText(message);
-    }
-
-    if(sound) {
-      note.sound = sound;
-    }
-
-    if(link) {
-      /* For IOS7 push notifications */
-      note.contentAvailable = true;
-
-      note.payload = { 'l': link };
-    }
-
-    //note.alert = message;
-    var deviceToken = new apns.Device(device.appleToken);
-
-    note.pushDevice = device;
-
-    switch(device.deviceType) {
-      case 'APPLE':
-        apnsConnection.pushNotification(note, deviceToken);
-        break;
-
-      case 'APPLE-BETA-DEV':
-        apnsConnectionBetaDev.pushNotification(note, deviceToken);
-        break;
-
-      case 'APPLE-BETA':
-        apnsConnectionBeta.pushNotification(note, deviceToken);
-        break;
-
-      case 'APPLE-DEV':
-        apnsConnectionDev.pushNotification(note, deviceToken);
-        break;
-      default:
-        logger.warn('Unknown device type: ' + device.deviceType);
-    }
-
-    sent = true;
+      stats.event("push_notification", {
+        userId: device.userId,
+        deviceType: device.deviceType
+      });
+    });
   } else if(device.deviceType === 'ANDROID') {
     androidGateway.sendNotificationToDevice(notification, badge, device, function(err, data) {
       if(err) return logger.error('android push notification failed', { err: err });
 
-      sent = true;
+      stats.event("push_notification", {
+        userId: device.userId,
+        deviceType: device.deviceType
+      });
 
       if(data) return logger.info('gcm push results', data);
     });
   } else {
     logger.warn('Unknown device type: ' + device.deviceType);
   }
-
-  // Blackberry push goes here
-
-  // Finally, send statistics out
-  if(sent) {
-    stats.event("push_notification", {
-      userId: device.userId,
-      deviceType: device.deviceType
-    });
-  }
-
 }
 
 var queue = workerQueue.queue('push-notification', {}, function() {
