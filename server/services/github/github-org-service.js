399262b00737b5a508e20a53967ea6ef641d8905
Andrew Newdigate <andrew@troupe.co>
2015-06-01 17:51:43 +0100

    Org service back to using Moondragon

--- server/services/github/github-org-service.js
+++ server/services/github/github-org-service.js
@@ -3,6 +3,7 @@
 var wrap = require('./github-cache-wrapper');
 var tentacles = require('./tentacles-client');
 var userTokenSelector = require('./user-token-selector').full;
+var githubMediaTypes = require('./github-media-types');
 
 function GitHubOrgService(user) {
   this.user = user;
@@ -13,26 +14,41 @@ function GitHubOrgService(user) {
  * Returns the details of an org
  */
 GitHubOrgService.prototype.getOrg = function(org) {
-  return tentacles.org.get(org, { accessToken: this.accessToken });
+  return tentacles.org.get(org, {
+    accessToken: this.accessToken,
+    headers: { Accept: githubMediaTypes.MOONDRAGON }
+  });
 };
 
 /**
  * Returns the list of users with publisized membership to an organisation
  */
 GitHubOrgService.prototype.members = function(org) {
-  return tentacles.orgMember.listMembers(org, { accessToken: this.accessToken });
+  return tentacles.orgMember.listMembers(org, {
+    accessToken: this.accessToken,
+    headers: { Accept: githubMediaTypes.MOONDRAGON }
+  });
 };
 
 GitHubOrgService.prototype.member = function(org, username) {
-  return tentacles.orgMember.checkMembershipForUser(org, username, { accessToken: this.accessToken });
+  return tentacles.orgMember.checkMembershipForUser(org, username, {
+    accessToken: this.accessToken,
+    headers: { Accept: githubMediaTypes.MOONDRAGON }
+  });
 };
 
 GitHubOrgService.prototype.getRepos = function(org) {
-  return tentacles.repo.listForOrg(org, { accessToken: this.accessToken });
+  return tentacles.repo.listForOrg(org, {
+    accessToken: this.accessToken,
+    headers: { Accept: githubMediaTypes.MOONDRAGON }
+  });
 };
 
 GitHubOrgService.prototype.getMembership = function(org, username) {
-  return tentacles.orgMember.getMembershipForUser(org, username, { accessToken: this.accessToken });
+  return tentacles.orgMember.getMembershipForUser(org, username, {
+    accessToken: this.accessToken,
+    headers: { Accept: githubMediaTypes.MOONDRAGON }
+  });
 };
 
 module.exports = wrap(GitHubOrgService, function() {
