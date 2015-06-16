1a16966224c7607b7b159200691b418e7a3d3146
Andrew Newdigate <andrew@troupe.co>
2015-06-16 12:07:21 -0700

    Some tests

--- shared/time/time-format.js
+++ shared/time/time-format.js
@@ -1,18 +1,25 @@
 'use strict';
 
 var moment = require('moment');
-var ONE_DAY = 86400 * 1000;
+var ONE_DAY = 86400000;
 
-function pastThrehold(date) {
-  return Date.now() - date > ONE_DAY;
+/**
+ * For now, this simply looks for dates older than one day,
+ * but it should really switch over when the date has changed
+ * (in your local timezone)
+ */
+function pastThrehold(date, now) {
+  if (!now) now = Date.now();
+  return (now - date) > ONE_DAY;
 }
 
 module.exports = function timeFormat(time, options) {
-  var lang, tzOffset, compact;
+  var lang, tzOffset, compact, now;
   if (options) {
     lang = options.lang;
     tzOffset = options.tzOffset;
     compact = options.compact;
+    now = options.now; // Makes testing easier
   }
 
   if (!time) return '';
@@ -22,11 +29,11 @@ module.exports = function timeFormat(time, options) {
     time.locale(lang);
   }
 
-  if (tzOffset) {
+  if (tzOffset !== undefined) {
     time.utcOffset(-tzOffset);
   }
 
-  if (pastThrehold(time.valueOf())) {
+  if (pastThrehold(time.valueOf(), now)) {
     return compact ? time.format("MMM DD") : time.format("MMM DD HH:mm");
   }
 
