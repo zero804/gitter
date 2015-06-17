2618e6249be7585d83c827b2e80ee4133ead67e6
Andrew Newdigate <andrew@troupe.co>
2015-06-16 17:08:50 -0700

    Don't pre-render dates in the main application if we don't know timezone ... but allow the chat archives page to render the dates in UTC as the client does not re-render them in this environment

--- server/web/widget-prerenderers/timeago.js
+++ server/web/widget-prerenderers/timeago.js
@@ -5,11 +5,33 @@ var timeFormat = require('../../../shared/time/time-format');
 
 module.exports = exports = function() {
   return function timeagoWidgetHandler(params) {
-    var options = params.hash || {};
+    var options = params.hash;
+    var root = params.data.root;
 
-    var time    = options.time;
-    var lang    = options.lang;
+    var time, lang, compact, alwaysShow;
 
-    return timeFormat(time, { lang: lang, tzOffset: params.data.root.tzOffset, compact: options.compact });
+    if (options) {
+      time = options.time;
+      lang = options.lang;
+      compact = options.compact;
+    }
+
+    var tzOffset = root.tzOffset;
+    var tz = root.tz;
+    var alwaysShow = root.showDatesWithoutTimezone;
+
+    /* In the chat environment, we don't want to prerender the time
+     * when we don't know what timezone the user is in: we'll just wait
+     * until the javascript kicks in and render it then.
+     *
+     * However, in the archive environment, we don't want to do this
+     * for SEO and also because the chats are not re-rendered, so no
+     * times will be shown.
+     */
+    if (!tz && !alwaysShow) {
+      return '';
+    }
+
+    return timeFormat(time, { lang: lang, tzOffset: tzOffset, compact: compact });
   };
 };
