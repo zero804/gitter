9d4de721b26e75765908e06d6e66344accf25509
Andrew Newdigate <andrew@troupe.co>
2015-06-25 12:40:22 +0100

    Tests for auto-lurk

--- scripts/find-old-user-troupes.js
+++ scripts/find-old-user-troupes.js
@@ -4,13 +4,13 @@
 "use strict";
 
 var persistence = require('../server/services/persistence-service');
-var roomService = require('../server/services/room-service');
+var userService = require('../server/services/user-service');
+var autoLurkerService = require('../server/services/auto-lurker-service');
 var troupeService = require('../server/services/troupe-service');
-var userTroupeSettingsService = require('../server/services/user-troupe-settings-service');
 var collections = require('../server/utils/collections');
 var shutdown = require('shutdown');
 
-require('../server/utils/event-listeners').installLocalEventListeners();
+// require('../server/utils/event-listeners').installLocalEventListeners();
 
 var opts = require("nomnom")
    .option('room', {
@@ -31,94 +31,33 @@ var opts = require("nomnom")
 
 var minTimeInDays = parseInt(opts.min, 10);
 
-function getLastAccessTimesForUsersInTroupe(troupeId, userIds) {
-  return persistence.UserTroupeLastAccess.findQ(
-     { userId: { $in: userIds } },
-     'userId troupes.' + troupeId)
-    .then(function(results) {
-      return results.reduce(function(memo, result) {
-        memo[result.userId] = result.troupes && result.troupes[troupeId];
-        return memo;
-      }, {});
-    });
-}
 
 troupeService.findByUri(opts.room)
   .then(function(troupe) {
-    var userIds = troupe.getUserIds();
-
-    var lurkStatus = troupe.users.reduce(function(memo, troupeUser) {
-      memo[troupeUser.userId] = troupeUser.lurk;
-      return memo;
-    }, {});
-
-    return [troupe.id, userIds, lurkStatus, getLastAccessTimesForUsersInTroupe(troupe.id, userIds)];
-  })
-  .spread(function(troupeId, userIds, lurkStatus, results) {
-    var cutoff = Date.now() - minTimeInDays * 86400000;
-
-    var oldUserIds = Object.keys(results).map(function(userId) {
-        var lastAccess = results[userId];
-
-        if (lastAccess && lastAccess < cutoff) {
-          return userId;
-        }
-      }).filter(function(f) {
-        return !!f;
-      });
-
-    return [
-      troupeId,
-      oldUserIds,
-      lurkStatus,
-      opts.dryRun && persistence.User.findQ({ _id: { $in: oldUserIds }}, { username: 1 }),
-      userTroupeSettingsService.getUserTroupeSettingsForUsersInTroupe(troupeId, 'notifications', oldUserIds),
-      results
-    ];
-  })
-  .spread(function(troupeId, oldUsersIds, lurkStatus, oldUsers, settings, lastAccessDates) {
-    var lurkUserIds = oldUsersIds.filter(function(userId) {
-      return !lurkStatus[userId];
-    });
-
-    var settingsUserIds = oldUsersIds.filter(function(userId) {
-      var notificationSettings = settings[userId];
-      var push = notificationSettings && notificationSettings.push;
-
-      return push !== 'mute' && push !== 'mention';
-    });
-
     if (opts.dryRun) {
-      var oldUsersIndexed = collections.indexById(oldUsers);
-      var mapUsername = function (userId) {
-        var user = oldUsersIndexed[userId];
-        return user && user.username;
-      };
+      return autoLurkerService.findLurkCandidates(troupe, { minTimeInDays: minTimeInDays })
+        .then(function(candidates) {
+          var userIds = candidates.map(function(c) { return c.userId; });
+          return [candidates, userService.findByIds(userIds)];
+        })
+        .spread(function(candidates, users)  {
+          var usersHash = collections.indexById(users);
 
-      console.log('old users', oldUsersIds.map(function(userId) {
-        var user = oldUsersIndexed[userId];
-        return (user && user.username) + ' - ' + new Date(lastAccessDates[userId]).toISOString();
-      }).join('\n'));
+          candidates.forEach(function(c) {
+            var user = usersHash[c.userId];
+            if (!user) return;
 
-      console.log('lurk users', lurkUserIds.map(mapUsername).join(', '), '(' + lurkUserIds.length + ')');
-      console.log('settings users', settingsUserIds.map(mapUsername).join(', '), '(' + settingsUserIds.length + ')');
-
-      process.exit(0);
+            console.log({
+              username: user.username,
+              notificationSettings: c.notificationSettings,
+              lurk: c.lurk,
+              lastAccessTime: c.lastAccessTime && c.lastAccessTime.toISOString()
+            });
+          });
+        });
     } else {
-      console.log('lurk users', lurkUserIds.length);
-      console.log('settingsUserIds', settingsUserIds.length);
+      return autoLurkerService.autoLurkInactiveUsers(troupe, { minTimeInDays: minTimeInDays });
     }
-
-    return roomService.bulkLurkUsers(troupeId, lurkUserIds)
-      .then(function() {
-        console.log('Bulk complete');
-
-        return userTroupeSettingsService.setUserSettingsForUsersInTroupe(troupeId, settingsUserIds, 'notifications', { push: 'mention' });
-      })
-      .then(function() {
-        console.log('Done');
-      });
-
   })
   .delay(1000)
   .then(function() {
--- server/services/auto-lurker-service.js
+++ server/services/auto-lurker-service.js
@@ -1,7 +1,11 @@
 "use strict";
 
-var recentRoomService = require('./recent-room-service');
+var recentRoomService         = require('./recent-room-service');
 var userTroupeSettingsService = require('./user-troupe-settings-service');
+var persistence               = require('./persistence-service');
+var unreadItemService         = require('./unread-item-service');
+var Q                         = require('q');
+var qlimit                    = require('qlimit');
 
 /**
  * Returns a list of users who could be lurked
@@ -61,3 +65,75 @@ function findLurkCandidates(troupe, options) {
 }
 
 exports.findLurkCandidates = findLurkCandidates;
+
+
+var bulkUnreadItemLimit = qlimit(5);
+/**
+ * Bulk lurk users without putting undue strain on mongodb
+ */
+function bulkLurkUsers(troupeId, userIds) {
+  var userHash = userIds.reduce(function(memo, userId) {
+    memo[userId] = true;
+    return memo;
+  }, {});
+
+  return persistence.Troupe.findByIdQ(troupeId)
+    .then(function(troupe) {
+      troupe.users.forEach(function(troupeUser) {
+        if (userHash[troupeUser.userId]) {
+          troupeUser.lurk = true;
+        }
+      });
+      troupe._skipTroupeMiddleware = true; // Don't send out an update
+      return troupe.saveQ();
+    })
+    .then(function() {
+      return Q.all(userIds.map(bulkUnreadItemLimit(function(userId) {
+        return unreadItemService.ensureAllItemsRead(userId, troupeId);
+      })));
+    });
+
+    // Odd, user not found
+    // if(!count) return;
+
+    // Don't send updates for now
+    //appEvents.userTroupeLurkModeChange({ userId: userId, troupeId: troupeId, lurk: lurk });
+    // TODO: in future get rid of this but this collection is used by the native clients
+    //appEvents.dataChange2('/user/' + userId + '/rooms', 'patch', { id: troupeId, lurk: lurk });
+
+    // Delete all the chats in Redis for this person too
+}
+exports.bulkLurkUsers = bulkLurkUsers;
+
+
+/**
+ * Auto lurk users in a room
+ */
+function autoLurkInactiveUsers(troupe, options) {
+  return findLurkCandidates(troupe, options)
+    .then(function(candidates) {
+      if (!candidates.length) return;
+
+      var usersToLurk = candidates
+        .filter(function(candidate) {
+          return !candidate.lurk;
+        })
+        .map(function(candidate) {
+          return candidate.userId;
+        });
+
+      var usersToChangeSettings = candidates
+        .filter(function(candidate) {
+          return candidate.notificationSettings !== 'mute' && candidate.notificationSettings !== 'mention';
+        })
+        .map(function(candidate) {
+          return candidate.userId;
+        });
+        
+      return Q.all([
+        usersToChangeSettings.length && userTroupeSettingsService.setUserSettingsForUsersInTroupe(troupe.id, usersToChangeSettings, 'notifications', { push: 'mention' }),
+        usersToLurk.length && bulkLurkUsers(troupe.id, usersToLurk)
+      ]);
+    });
+}
+exports.autoLurkInactiveUsers = autoLurkInactiveUsers;
