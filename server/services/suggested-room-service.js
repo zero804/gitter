d0357f6d6c98c72ccbf43a784535e84d4c892ed3
Andrew Newdigate <andrew@troupe.co>
2015-06-26 17:18:00 +0100

    Removed explore stuff from suggested rooms

--- server/services/suggested-room-service.js
+++ server/services/suggested-room-service.js
@@ -215,23 +215,3 @@ function getSuggestions(user, localeLanguage) {
 }
 
 exports.getSuggestions = getSuggestions;
-
-/**
- * fetchByTags() retrives rooms that match a given set of tags
- *
- * tags     Array the querying tags
- * @return  Promise promise of matching rooms
- */
-function fetchByTags(tags) {
-  // limit by 8 tags to avoid mega queries
-  tags = tags.slice(0, 8);
-
-  return persistence.Troupe
-    .where('security').equals('PUBLIC')
-    .where('tags').in(tags)
-    .sort({ userCount: -1 })
-    .limit(50)
-    .execQ();
-}
-
-exports.fetchByTags = fetchByTags;
