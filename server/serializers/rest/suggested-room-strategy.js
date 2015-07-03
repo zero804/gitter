cabb3681bdb3cd5939ac88f634f49ca5b4ad5f43
Andrew Newdigate <andrew@troupe.co>
2015-07-03 22:48:29 +0100

    A/B testing of suggestions

--- server/serializers/rest/suggested-room-strategy.js
+++ server/serializers/rest/suggested-room-strategy.js
@@ -5,32 +5,44 @@ var persistence = require('../../services/persistence-service');
 var collections = require('../../utils/collections');
 var mongoUtils = require('../../utils/mongo-utils');
 
-function getOwnerAvatarUrl(room) {
-  if (!room.uri) return;
+function getOwnerAvatarUrl(roomUri) {
+  if (!roomUri) return;
 
-  return 'https://avatars.githubusercontent.com/' + room.uri.split('/')[0];
+  return 'https://avatars.githubusercontent.com/' + roomUri.split('/')[0];
 }
 
 function SuggestedRoomStrategy() {
   var roomHash;
 
-  this.preload = function(suggestedRoomsIds, callback) {
-    persistence.Troupe.findQ({ _id: { $in: mongoUtils.asObjectIDs(suggestedRoomsIds) }, security: 'PUBLIC' }, { uri: 1, githubType: 1, userCount: 1, topic: 1 })
+  this.preload = function(suggestedRooms, callback) {
+    var suggestedRoomIds = suggestedRooms
+      .filter(function(f) { return !!f.roomId; })
+      .map(function(f) { return mongoUtils.asObjectID(f.roomId); });
+
+    if (!suggestedRoomIds.length) {
+      roomHash = {};
+      callback();
+      return;
+    }
+
+    persistence.Troupe.findQ({ _id: { $in: suggestedRoomIds }, security: 'PUBLIC' }, { uri: 1, githubType: 1, userCount: 1, topic: 1 })
       .then(function(rooms) {
         roomHash = collections.indexById(rooms);
       })
       .nodeify(callback);
   };
 
-  this.map = function(suggestedRoomId) {
-    var room = roomHash[suggestedRoomId];
+  this.map = function(suggestedRoom) {
+    var room = roomHash[suggestedRoom.roomId] || suggestedRoom.room;
+    var uri = room && room.uri || suggestedRoom.uri;
+    if (!uri) return;
 
     return {
-      id: room.id,
-      uri: room.uri,
-      avatarUrl: getOwnerAvatarUrl(room),
-      userCount: room && room.userCount,
-      description: room && room.topic,
+      id: room && room.id,
+      uri: uri,
+      avatarUrl: getOwnerAvatarUrl(uri),
+      userCount: room && room.userCount || suggestedRoom.userCount,
+      description: room && room.topic || suggestedRoom.topic,
       exists: !!room
     };
   };
