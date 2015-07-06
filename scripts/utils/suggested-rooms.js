d40857a0860d07bdbb23712641ebcac8cebf9a72
Andrew Newdigate <andrew@troupe.co>
2015-07-06 15:35:57 +0100

    Fixed suggested rooms scripts

--- scripts/utils/suggested-rooms.js
+++ scripts/utils/suggested-rooms.js
@@ -3,6 +3,7 @@
 "use strict";
 
 var userService = require('../../server/services/user-service');
+var troupeService = require('../../server/services/troupe-service');
 var restSerializer = require('../../server/serializers/rest-serializer');
 var shutdown = require('shutdown');
 
@@ -14,6 +15,9 @@ var opts = require("nomnom")
     required: true,
     help: "username to look up e.g trevorah"
   })
+  .option('uri', {
+    help: "room in which to do the lookup"
+  })
   .option('language', {
     default: 'en',
     help: "human language"
@@ -22,6 +26,13 @@ var opts = require("nomnom")
 
 userService.findByUsername(opts.username)
   .then(function(user) {
+    return [user, opts.uri && troupeService.findByUri(opts.uri)];
+  })
+  .spread(function(user, room) {
+    if (room) {
+      return graphRecommendations.getSuggestionsForRoom(room, user, opts.language);
+    }
+    
     return graphRecommendations.getSuggestionsForUser(user, opts.language);
   })
   .then(function(suggestions) {
