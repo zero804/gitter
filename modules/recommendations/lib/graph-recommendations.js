302caed07d3f9e11f7bb7d7ea3c15074a3a0ba3a
Andrew Newdigate <andrew@troupe.co>
2015-07-06 17:11:07 +0100

    Added affirmative action weighting

--- modules/recommendations/lib/graph-recommendations.js
+++ modules/recommendations/lib/graph-recommendations.js
@@ -1,21 +1,32 @@
 'use strict';
 
 var env   = require('gitter-web-env');
+var stats = env.stats;
 var cypher = require("cypher-promise");
 var neo4jClient = cypher(env.config.get('neo4j:endpoint'));
 var debug = require('debug')('gitter:graph-recommendations');
 
 function query(text, params) {
   debug("neo4j query: %s %j", text, params);
-  return neo4jClient.query(text, params);
-}
+  var start = Date.now();
+
+  return neo4jClient.query(text, params)
+    .then(function(response) {
+      stats.responseTime('suggestions.graph.query', Date.now() - start);
+      return response;
+    })
+    .catch(function(err) {
+      stats.event('chat.search.error');
+      throw err;
+    });
+  }
 
 function queryRoomRecommedations(roomId, userId) {
   if (userId) {
     /* Given a user .... */
     return query("MATCH (r:Room)-[:MEMBER]-(:User)-[:MEMBER]-(r2:Room), (u:User) " +
                  "WHERE u.userId = {userId} AND r.roomId = {roomId} AND NOT(u-[:MEMBER]-r2) AND r2.security = 'PUBLIC'" +
-                 "RETURN r2.roomId, count(*) as occurrence " +
+                 "RETURN r2.roomId, count(*) * r2.weight as occurrence " +
                  "ORDER BY occurrence DESC " +
                  "LIMIT 6",
     {
@@ -27,7 +38,7 @@ function queryRoomRecommedations(roomId, userId) {
   /* Anonymous query */
   return query("MATCH (r:Room)-[:MEMBER]-(:User)-[:MEMBER]-(r2:Room)" +
                "WHERE r.roomId = {roomId} AND r2.security = 'PUBLIC'" +
-               "RETURN r2.roomId, count(*) as occurrence " +
+               "RETURN r2.roomId, count(*) * r2.weight as occurrence " +
                "ORDER BY occurrence DESC " +
                "LIMIT 6",
   {
@@ -51,7 +62,7 @@ exports.getSuggestionsForRoom = getSuggestionsForRoom;
 function getSuggestionsForUser(user /*, locale */) {
   return query("MATCH (u:User)-[:MEMBER]->(:Room)-[:MEMBER]-(:User)-[:MEMBER]-(r:Room) " +
                "WHERE u.userId = {userId} AND NOT(u-[:MEMBER]-r) AND r.security = 'PUBLIC'" +
-               "RETURN r.roomId, count(*) as occurrence " +
+               "RETURN r.roomId, count(*) * r.weight as occurrence " +
                "ORDER BY occurrence DESC " +
                "LIMIT 6",
           {
