51e8e5bf22d4e8852cf3776f595121252d26cb9d
Andy Trevorah <andy@troupe.co>
2015-07-07 14:36:36 +0100

    inlined if statements

--- modules/split-tests/lib/server/split-tests-server.js
+++ modules/split-tests/lib/server/split-tests-server.js
@@ -27,10 +27,7 @@ function configure(req, res, testName, disableSet) {
   // If cookies are not enabled in the current environment use the set value
   // defaulting to control.
   // CORS with Cookies is more trouble that it's worth
-  if (!req.cookies) {
-    if (!forcedParam) return 'control';
-    return forcedParam;
-  }
+  if (!req.cookies) return forcedParam || 'control';
 
   // From here on, the cookies middleware has been installed
   var value = req.cookies[cookieName];
