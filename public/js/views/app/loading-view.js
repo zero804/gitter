ad3193c33aa4a476ec9cf3d0205e586095c59e73
Andy Trevorah <andy@troupe.co>
2015-07-07 16:44:27 +0100

    fixed loading spinner taking too long in picture filled rooms

--- public/js/views/app/loading-view.js
+++ public/js/views/app/loading-view.js
@@ -9,20 +9,37 @@ var LoadingView = function(iframe, loadingFrame) {
     self.hide();
 
     // now that we have a new content window,
-    // we have to attach a new unload listener
+    // we have to attach new unload listeners
     // as the old content window has been destroyed
     self.iframe.contentWindow.addEventListener('beforeunload', function() {
+      // iframe is about to be destroyed, but another listener could abort this process
+      // showing the spinner on unload however, feels too slow.
       self.show();
+    });
+
+    self.iframe.contentWindow.addEventListener('unload', function() {
+      // the exiting contentDocument is about to be destroyed, but the
+      // iframe's new contentDocument will be instantiated in the next event loop.
+      setTimeout(function() {
+        self.iframe.contentDocument.addEventListener('DOMContentLoaded', onIframeLoad);
+      }, 0)
     });
   }
 
-  if (this.iframe.contentDocument.readyState === 'complete') {
+  var readyState = this.iframe.contentDocument.readyState;
+
+  if (readyState === 'interactive' || readyState === 'complete') {
     // iframe has already loaded.
     // such a speedy iframe!
     onIframeLoad();
   }
 
-  // prepare for all future iframe loads
+  // this will fire before the iframe's full window load,
+  // but the contentDocument will get destroyed on navigation
+  this.iframe.contentDocument.addEventListener('DOMContentLoaded', onIframeLoad);
+
+  // listen on the iframe window, just to be sure
+  // this is slow as it waits for images to load, even if the readyState is "interactive"
   this.iframe.addEventListener('load', onIframeLoad);
 };
 
