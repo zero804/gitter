2fa2467699c8cd4c1d4ccc313e392b2d8810a6cb
Andy Trevorah <andy@troupe.co>
2015-07-08 13:32:31 +0100

    fix for desktop client loading indicator never hiding

--- public/js/views/app/loading-view.js
+++ public/js/views/app/loading-view.js
@@ -1,9 +1,26 @@
 "use strict";
 
+var appEvents = require('utils/appevents');
+
 var LoadingView = function(iframe, loadingFrame) {
   var self = this;
   this.iframe = iframe;
   this.loadingFrame = loadingFrame;
+
+  function onbeforeunload() {
+    // iframe is about to be destroyed, but another listener could abort this process
+    // showing the spinner on unload however, feels too slow.
+    self.show();
+  }
+
+  function onunload() {
+    // the exiting contentDocument is about to be destroyed, but the
+    // iframe's new contentDocument will be instantiated in the next event loop.
+    setTimeout(function() {
+      self.iframe.contentDocument.removeEventListener('DOMContentLoaded', onIframeLoad);
+      self.iframe.contentDocument.addEventListener('DOMContentLoaded', onIframeLoad);
+    }, 0);
+  }
 
   function onIframeLoad() {
     self.hide();
@@ -11,19 +28,11 @@ var LoadingView = function(iframe, loadingFrame) {
     // now that we have a new content window,
     // we have to attach new unload listeners
     // as the old content window has been destroyed
-    self.iframe.contentWindow.addEventListener('beforeunload', function() {
-      // iframe is about to be destroyed, but another listener could abort this process
-      // showing the spinner on unload however, feels too slow.
-      self.show();
-    });
+    self.iframe.contentWindow.removeEventListener('beforeunload', onbeforeunload);
+    self.iframe.contentWindow.addEventListener('beforeunload', onbeforeunload);
 
-    self.iframe.contentWindow.addEventListener('unload', function() {
-      // the exiting contentDocument is about to be destroyed, but the
-      // iframe's new contentDocument will be instantiated in the next event loop.
-      setTimeout(function() {
-        self.iframe.contentDocument.addEventListener('DOMContentLoaded', onIframeLoad);
-      }, 0)
-    });
+    self.iframe.contentWindow.removeEventListener('unload', onunload);
+    self.iframe.contentWindow.addEventListener('unload', onunload);
   }
 
   var readyState = this.iframe.contentDocument.readyState;
@@ -34,13 +43,17 @@ var LoadingView = function(iframe, loadingFrame) {
     onIframeLoad();
   }
 
-  // this will fire before the iframe's full window load,
-  // but the contentDocument will get destroyed on navigation
+  // this will fire when readyState is "interactive", but before the iframe's full window load.
+  // However, the contentDocument will get destroyed on navigation
   this.iframe.contentDocument.addEventListener('DOMContentLoaded', onIframeLoad);
 
-  // listen on the iframe window, just to be sure
-  // this is slow as it waits for images to load, even if the readyState is "interactive"
+  // this will fire when readyState is "complete", after the 'DOMContentLoaded' event.
+  // this is slow as it waits for images to load, but the iframe wont be destroyed on navigation
   this.iframe.addEventListener('load', onIframeLoad);
+
+  // listen to our own load event as node-webkit 0.11.6 loses track
+  // of the child iframe event listeners when the iframe.src changes (!)
+  appEvents.on('childframe:loaded', onIframeLoad);
 };
 
 LoadingView.prototype.show = function() {
