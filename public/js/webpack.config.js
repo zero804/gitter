9df6f899b5b93f88b656d26f6e8a0e40acc716ab
Andy Trevorah <andy@troupe.co>
2015-07-14 14:10:46 +0100

    Revert "added oauth upgrade link to new userhome"
    This reverts commit 5d1c3ec1840efae2dd5f7524023de9cecec59731.

--- public/js/webpack.config.js
+++ public/js/webpack.config.js
@@ -26,7 +26,6 @@ var webpackConfig = {
     "mobile-native-embedded-chat": path.resolve(path.join(__dirname, "./mobile-native-embedded-chat")),
     "mobile-native-userhome": path.resolve(path.join(__dirname, "./mobile-native-userhome")),
     "userhome": path.resolve(path.join(__dirname, "./userhome")),
-    "userhome_treatment": path.resolve(path.join(__dirname, "./userhome_treatment")),
     "router-archive-chat": path.resolve(path.join(__dirname, "./router-archive-chat")),
     "router-archive-home": path.resolve(path.join(__dirname, "./router-archive-home")),
     "router-embed-chat": path.resolve(path.join(__dirname, "./router-embed-chat")),
