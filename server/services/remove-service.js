e922e0a6adf394f99550fcee863ab73817f26705
Andrew Newdigate <andrew@troupe.co>
2015-07-14 18:02:54 +0100

    Remove service tests passing

--- server/services/remove-service.js
+++ server/services/remove-service.js
@@ -7,6 +7,7 @@ var appEvents          = require('gitter-web-appevents');
 var recentRoomService  = require('./recent-room-service');
 var roomPermissionsModel = require('./room-permissions-model');
 var unreadItemService = require('./unread-item-service');
+var roomMembershipService = require('./room-membership-service');
 
 // Check parameters
 
@@ -23,8 +24,7 @@ function removeFavourite(room, userId) {
 }
 
 function removeFromRoom(room, userId) {
-  room.removeUserById(userId);
-  return room.saveQ();
+  return roomMembershipService.removeRoomMember(room._id, userId);
 }
 
 function removeUnreadCounts(room, userId) {
@@ -33,12 +33,12 @@ function removeUnreadCounts(room, userId) {
 
 function removeFromRoomAndFavouriteAndUnread(room, userId) {
   return removeFromRoom(room, userId)
-  .then(function() {
-    return removeUnreadCounts(room, userId);
-  })
-  .then(function() {
-    return removeFavourite(room, userId);
-  });
+    .then(function() {
+      return removeUnreadCounts(room, userId);
+    })
+    .then(function() {
+      return removeFavourite(room, userId);
+    });
 }
 
 // Exported functions
@@ -53,20 +53,24 @@ function userLeaveRoom(room, user) {
 function removeRecentRoomForUser(room, userId) {
   return checkParameters(room, userId)
   .then(function() {
+    return removeFavourite(room, userId);
+  })
+  .then(function() {
+    return roomMembershipService.getMemberLurkStatus(room._id, userId)
+  })
+  .then(function(userLurkStatus) {
     var roomId = room.id;
-    var user = room.findTroupeUser(userId);
 
-    return removeFavourite(room, userId)
-    .then(function() {
-      if (user) {
-        if (user.lurk) return removeFromRoom(room, userId);
-
-        // TODO: in future get rid of this but this collection is used by the native clients
-        appEvents.dataChange2('/user/' + userId + '/rooms', 'patch', { id: roomId, favourite: null, lastAccessTime: null, mentions: 0, unreadItems: 0 });
-        return;
-      }
+    if (userLurkStatus === null) {
+      // User does not appear to be in the room...
       appEvents.dataChange2('/user/' + userId + '/rooms', 'remove', { id: roomId });
-    });
+      return;
+    }
+
+    if (userLurkStatus) return removeFromRoom(room, userId);
+
+    // TODO: in future get rid of this but this collection is used by the native clients
+    appEvents.dataChange2('/user/' + userId + '/rooms', 'patch', { id: roomId, favourite: null, lastAccessTime: null, mentions: 0, unreadItems: 0 });
   });
 }
 
