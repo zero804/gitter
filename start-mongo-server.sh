75d24b3104e284d2ddd5d876e81c00c225011803
Andrew Newdigate <andrew@troupe.co>
2015-07-15 22:42:49 +0100

    Updated to wiredTiger storage. Please update your dev mongo instances @mydigitalself @trevorah @malditogeek @ricardobaeta

--- start-mongo-server.sh
+++ start-mongo-server.sh
@@ -6,5 +6,5 @@ SERVER_NUMBER=$1
 
 PORT=$((27017 + $SERVER_NUMBER))
 
-mkdir -p /usr/local/var/mongodb/r${SERVER_NUMBER}
-mongod run -replSet troupeSet --journal --port $PORT --dbpath /usr/local/var/mongodb/r${SERVER_NUMBER} --rest --profile=1 --slowms=15
+mkdir -p "/usr/local/var/mongodb/r${SERVER_NUMBER}"
+mongod run -replSet troupeSet --journal --port $PORT --dbpath "/usr/local/var/mongodb/r${SERVER_NUMBER}" --rest --profile=1 --slowms=15 --notablescan
