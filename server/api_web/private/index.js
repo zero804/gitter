6c93cd4262f8aee6f9300a1fc7ca8a9e0d5216e5
Andrew Newdigate <andrew@troupe.co>
2015-07-23 13:02:59 +0100

    Added health-checks to /api_web

--- server/api_web/private/index.js
+++ server/api_web/private/index.js
@@ -6,7 +6,15 @@ module.exports = {
 
     app.get('/api_web/private/ping',
         authMiddleware,
-        require('./ping.js'));
+        require('./ping'));
+
+    app.get('/api_web/private/health_check',
+        // No auth
+        require('../../api/private/health-check'));
+
+    app.get('/api_web/private/health_check/full',
+        // No auth
+        require('../../api/private/health-check-full'));
 
   }
 };
