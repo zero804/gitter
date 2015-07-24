986df0b69942a6be6caf58d9ca05c031a6754165
Andrew Newdigate <andrew@troupe.co>
2015-07-24 17:16:04 +0100

    Remove unused files and stuff

--- start-elasticsearch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -ex
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-export CONF_DIR=${SCRIPT_DIR}/config/elasticsearch-dev/
-elasticsearch -Des.path.conf=${SCRIPT_DIR}/config/elasticsearch-dev/
- #-Des.config=${SCRIPT_DIR}/config/elasticsearch-dev/elasticsearch.yml
--- start-neo4j.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-export DOCKER_HOST=tcp://192.168.99.100:2376
-export DOCKER_CERT_PATH=~/.docker/machine/machines/dev
-export DOCKER_TLS_VERIFY=1
-
-docker run -d -p 7474:7474 --name graphdb kbastani/docker-neo4j
