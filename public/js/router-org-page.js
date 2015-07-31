e557a85bb579c3944a9bcdf060be923c066e5f37
Jon Parsons <jon@ctrlxctrlv.co.uk>
2015-07-31 16:33:45 +0100

    added button styles to modal

---
+++ public/js/router-org-page.js
@@ -0,0 +1,40 @@
+/*global require, console, window, document */
+"use strict";
+
+var onReady = require('./utils/onready');
+var Backbone= require('backbone');
+var appEvents = require('utils/appevents');
+var frameUtils = require('./utils/frame-utils');
+var modalRegion = require('components/modal-region');
+
+onReady(function(){
+
+  require('components/link-handler').installLinkHandler();
+
+  appEvents.on('navigation', function(url, type, title) {
+    if(frameUtils.hasParentFrameSameOrigin()) {
+      frameUtils.postMessage({ type: "navigation", url: url, urlType: type, title: title});
+    } else {
+      // No pushState here. Open the link directly
+      // Remember that (window.parent === window) when there is no parent frame
+      window.parent.location.href = url;
+    }
+  });
+
+  var Router = Backbone.Router.extend({
+
+    routes: {
+      ':org/tags': 'onNavigateTags'
+    },
+
+    onNavigateTags: function(org){
+      require.ensure(['views/app/editTagsView'], function(require) {
+        var EditTagsView = require('views/app/editTagsView');
+        modalRegion.show(new EditTagsView());
+      });
+    }
+  });
+
+  new Router();
+  Backbone.history.start();
+});
