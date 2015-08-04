658d3cb8e719a215492d2700a1e185dfb5c6f4fb
Jon Parsons <jon@ctrlxctrlv.co.uk>
2015-08-04 10:47:18 +0100

    moved tag input view into its own set of files

---
+++ public/js/views/app/tags/tag-input-view.js
@@ -0,0 +1,78 @@
+/* jshint node:true  */
+"use strict";
+
+var Marionette = require('backbone.marionette');
+var TagModel = require('../../../collections/tag-collection.js').TagModel;
+
+var tagInputTemplate = require('./tmpl/tag-input-template.hbs');
+
+var ENTER_KEY_CODE = 13;
+var BACKSPACE_KEY_CODE = 8;
+var DELETE_KEY_CODE = 46;
+
+var TagInputView = Marionette.ItemView.extend({
+
+  template: tagInputTemplate,
+
+  events: {
+    'submit': 'onTagSubmit',
+    'input': 'onTagInput',
+    'keypress': 'onKeyPressed'
+  },
+
+  initialize: function(){
+    this.model = new TagModel();
+    this.listenTo(this.model, 'invalid', this.onModelInvalid);
+    this.listenTo(this.model, 'change', this.onModelChange);
+  },
+
+  onTagSubmit: function(e){
+    if(e) e.preventDefault();
+    //TODO --> what happens if the model is invalid??
+    //jp 3/9/15
+    if(this.model.isValid()){
+      this.collection.addModel(this.model);
+      this.model = new TagModel();
+      this.$el.find('input').val('');
+    }
+  },
+
+  onTagInput: function(e){
+    if(e) e.preventDefault();
+    //guard against manual invocation of this function
+    var val =  e ? e.target.value : this.$el.find('input').val();
+    this.model.set('value', val, {validate: true});
+  },
+
+  onKeyPressed: function(e){
+    switch(e.keyCode) {
+
+      //submit tag by pressing enter
+      case ENTER_KEY_CODE :
+        //manually trigger tag submission
+        this.onTagInput();
+        this.onTagSubmit();
+        break;
+
+      //if a user presses backspace/delete
+      //and the input is empty
+      //remove the last tag
+      case BACKSPACE_KEY_CODE :
+      case DELETE_KEY_CODE :
+        var val =  this.$el.find('input').val();
+        if(val === '') this.collection.pop();
+        break;
+    }
+  },
+
+  onModelChange: function(){
+    this.$el.find('input').removeClass('invalid');
+  },
+
+  onModelInvalid: function(){
+    this.$el.find('input').addClass('invalid');
+  }
+
+});
+
+module.exports = TagInputView;
