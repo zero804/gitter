1b5eea2c42d0507fd9ab2ea7b4119fd4bc4654f6
Jon Parsons <jon@ctrlxctrlv.co.uk>
2015-08-04 11:00:55 +0100

    split out the rest of the tag view components

---
+++ public/js/views/app/tags/tag-list-view.js
@@ -0,0 +1,18 @@
+/* jshint node:true */
+
+"use strict";
+
+var Marionette = require('backbone.marionette');
+var TagView = require('./tag-view');
+
+var TagListView = Marionette.CollectionView.extend({
+  childView: TagView,
+  childEvents: {
+    'remove:tag': 'onRemoveTag'
+  },
+  onRemoveTag: function(view, model){
+    this.collection.remove(model);
+  }
+});
+
+module.exports = TagListView;
