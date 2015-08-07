db84865c6372c602e5205e7016e5c6d7fe91f81a
Andrew Newdigate <andrew@troupe.co>
2015-08-07 17:42:11 +0100

    No truncating of individual messages

--- server/services/notifications/notification-message-generator.js
+++ server/services/notifications/notification-message-generator.js
@@ -25,15 +25,15 @@ Mike: blah?
 Mike: ?
 */
 
-var MAX_LINE_LENGTH = 50;
 var MAX_NOTIFICATION_TEXT = 1024;
+var LAST_LINE_MIN_LENGTH = 30;
 var MAX_LINES = 3;
 
 var ent = require('ent');
 
 function truncate(line, maxLineLength) {
   if(line.length > maxLineLength) {
-    line = line.substring(0, maxLineLength - 2).trim() + '…';
+    line = line.substring(0, maxLineLength - 1).trim() + '…';
   }
   return line;
 }
@@ -42,53 +42,56 @@ function getHeaderLine(troupe) {
   return troupe.name || troupe.uri;
 }
 
-function getChatLine(troupe, chat) {
+function getText(username, chat) {
   if (!chat.text) return;
-  var encodedText = ent.decode(chat.text);
-  if (troupe.oneToOne) {
+  var encodedText = ent.decode(chat.text); // Do we need to do this?
+  if (username) {
+    return username + ': ' + encodedText;
+  } else {
     return encodedText;
   }
-
-  var fromLabel = getShortFromUserName(chat.fromUser);
-  if (fromLabel) return fromLabel  + ': ' + encodedText;
-
-  return encodedText;
 }
 
 function getShortFromUserName(user) {
+  if (!user) return;
   if (user.username) return user.username;
+
   var displayName = user.displayName;
   if (!displayName) return '';
+
   return displayName && displayName.split(/\s/,1)[0];
 }
 
 function summarizeChatsInRoom(troupe, chats, options) {
   var appendText = options && options.appendText;
-  var maxLineLength = options && options.maxLineLength || MAX_LINE_LENGTH;
   var maxMessageLength = options && options.maxMessageLength || MAX_NOTIFICATION_TEXT;
-  var maxNumberOfLines = options && options.maxLines || MAX_LINES;
-
   var maxLength = maxMessageLength - (appendText ? appendText.length : 0);
 
   //
   // Generate notification text
   //
-  var notificationText = getHeaderLine(troupe);
-  var lineCount = 1;
-  for(var i = 0; i < chats.length && lineCount <= maxNumberOfLines; i++) {
-    var nextLine = getChatLine(troupe, chats[i]);
-    if (!nextLine) continue;
+  var notificationText = getHeaderLine(troupe) || '';
+  var lastUsername = null;
+  for(var i = 0; i < chats.length && notificationText.length <= (maxLength - LAST_LINE_MIN_LENGTH); i++) {
+    var chat = chats[i];
 
-    nextLine = truncate(nextLine, maxLineLength);
-
-    // We add extra spaces so that when they're removed on an iphone the notificationText still makes sense
-    var lineWithNext = notificationText ? notificationText + '  \n' + nextLine : nextLine;
-    if(lineWithNext.length <= maxLength) {
-      notificationText = lineWithNext;
-      lineCount++;
+    // Only group chats prefix the username
+    var username = troupe.oneToOne ? null : getShortFromUserName(chat.fromUser);
+    if (username === lastUsername) {
+      username = null; // Don't prefix the username if it's the same as the last message
     } else {
-      break;
+      lastUsername = username;
     }
+
+    var text = getText(username, chat);
+    if (!text) continue;
+
+    if ((notificationText.length + text.length) > maxLength) {
+      text = truncate(text, maxLength - notificationText.length - 3 /* newline etc */);
+      // We add extra spaces so that when they're removed on an iphone the notificationText still makes sense
+    }
+
+    notificationText = notificationText ? notificationText + '  \n' + text : text;
   }
 
   if (appendText) {
