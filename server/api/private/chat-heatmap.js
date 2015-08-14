3a7f68e4e99436937f121df84870a5c4aed43a7a
Andy Trevorah <andy@troupe.co>
2015-08-14 16:39:26 +0100

    simplified heatmap to remove timezone adjustment

--- server/api/private/chat-heatmap.js
+++ server/api/private/chat-heatmap.js
@@ -8,46 +8,21 @@ var moment = require('moment');
 module.exports = function(req, res, next) {
   var roomId = req.params.roomId;
 
-  var endDate = req.query.end ? moment(req.query.end) : moment.utc().endOf('day');
-  var startDate = req.query.start ? moment(req.query.start) : moment(endDate).subtract('years', 1).startOf('day');
+  var endMoment = req.query.end ? moment(req.query.end) : moment.utc().endOf('day');
+  var startMoment = req.query.start ? moment(req.query.start) : moment(endDate).subtract(1, 'years').startOf('day');
 
+  var endDate = endMoment.toDate();
+  var startDate = startMoment.toDate();
+
+  // simple auth check, throws on failure
   roomService.findByIdForReadOnlyAccess(req.user, roomId)
     .then(function(room) {
       var roomId = room.id;
 
-      return chatService.findDailyChatActivityForRoom(roomId, startDate.toDate(), endDate.toDate())
-        .then(function(r) {
-          var adjusted = {};
-          var rSorted = Object.keys(r).map(function(key) {
-            var value = moment.utc(key, 'YYYYMMDD').valueOf();
-            return { timestamp: value, value: r[key] };
-          });
-
-          rSorted.sort(function(a,b) {
-            return a.timestamp - b.timestamp;
-          });
-
-          var d = moment(startDate);
-          while(rSorted.length && (d.isBefore(endDate) || d.isSame(endDate))) {
-            var rangeStart = d.valueOf();
-            var rangeEnd = moment(d).add('days', 1).valueOf();
-
-            if(rSorted[0].timestamp < rangeStart) {
-              rSorted.shift();
-            }
-
-            if(!rSorted.length) break;
-
-            if(rSorted[0].timestamp < rangeEnd) {
-              var item = rSorted.shift();
-              adjusted[d.valueOf() / 1000] = item.value;
-            }
-
-            d.add('days', 1);
-          }
-
-          res.send(adjusted);
-        });
+      return chatService.findDailyChatActivityForRoom(roomId, startDate, endDate);
+    })
+    .then(function(chatActivity) {
+      res.send(chatActivity);
     })
     .fail(next);
 };
