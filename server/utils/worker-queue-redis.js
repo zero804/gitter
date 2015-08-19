8075971b23f804bf1e0ac9b294e1e468cb3915d0
Andrew Newdigate <andrew@gitter.im>
2015-08-19 17:19:26 +0100

    Additional comments as per @trevorah's request

--- server/utils/worker-queue-redis.js
+++ server/utils/worker-queue-redis.js
@@ -159,6 +159,10 @@ Queue.prototype.createWorker = function() {
 
   var jobs = {
     // TODO: remove 'echo' this by 1 September 2015
+    // Reasoning: Because until PR#747, the application servers were 
+    // submitting echo jobs and we need to gracefully roll over to the 
+    // more sane name of invoke. If we dropped it immediately, all the old 
+    // jobs during the deployment period would be dropped.
     echo: {
       perform: function(data, callback) {
         debug('Invoking echo');
