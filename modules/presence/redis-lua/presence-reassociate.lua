7e0db56f3963d200539cde86641305bd39ffe5a6
Andrew Newdigate <andrew@troupe.co>
2015-08-24 17:18:16 +0100

    Presence service: Reassociate

--- modules/presence/redis-lua/presence-reassociate.lua
+++ modules/presence/redis-lua/presence-reassociate.lua
@@ -1,40 +1,56 @@
 local key_socket_user = KEYS[1];
-local key_active_users = KEYS[2];
-local key_mobile_users = KEYS[3];
-local key_active_sockets = KEYS[4];
-local key_user_lock = KEYS[5];
-local key_user_sockets = KEYS[6];
+local key_user_lock = KEYS[2];
+local key_troupe_users = KEYS[3];
+local key_prev_troupe_users = KEYS[4];
 
 local user_id = ARGV[1];
 local socket_id = ARGV[2];
-local create_time = ARGV[3];
-local mobile_connection = tonumber(ARGV[4]);
-local client = ARGV[5];
-local troupe_id = ARGV[6];
+local troupe_id = ARGV[3];
+local known_prev_troupe_id = ARGV[4];
+local is_eyeballs_on = ARGV[5];
 
-if redis.call("EXISTS", key_socket_user) == 1 then
+if redis.call("EXISTS", key_socket_user) ~= 1 then
+	-- socket does not exist
 	return { 0 }
 end
 
 redis.call("INCR", key_user_lock);
 redis.call("EXPIRE", key_user_lock, 10);
 
-redis.call("HSET", key_socket_user, "uid", user_id)
-redis.call("HSET", key_socket_user, "ctime", create_time)
-redis.call("HSET", key_socket_user, "ct", client)
-redis.call("HSET", key_socket_user, "tid", troupe_id)
+local prev_troupe_id = redis.call("HGET", key_socket_user, "tid")
+local was_eyeballs_on = redis.call("HGET", key_socket_user, "eb") == "1"
 
-local user_socket_count = -1
+local user_in_troupe_count = -1
+local user_in_prev_troupe_count = -1
 
--- For mobile users, add them to the mobile users collection
-if mobile_connection == 1 then
-	redis.call("HSET", key_socket_user, "mob", 1)
-	redis.call("ZINCRBY", key_mobile_users, 1, user_id)
-else
-	user_socket_count = redis.call("ZINCRBY", key_active_users, 1, user_id)
+-- Theres a very small chance the troupe has changed this the app server
+-- queried for the existing value. Double check and don't process
+-- this request if that's the case
+if known_prev_troupe_id ~= prev_troupe_id then
+	return { 0 }
 end
 
-redis.call("SADD", key_user_sockets, socket_id)
-local socket_add_result = redis.call("SADD", key_active_sockets, socket_id)
+-- Eyeballs off the previous troupe
+if was_eyeballs_on and prev_troupe_id then
+	user_in_prev_troupe_count = redis.call("ZINCRBY", key_prev_troupe_users, -1, user_id)
+	redis.call("ZREMRANGEBYSCORE", key_prev_troupe_users, '-inf', '0')
+end
 
-return { 1, user_socket_count, socket_add_result }
+if troupe_id and troupe_id ~= "" then
+	-- If we are reassociating with a new troupe
+	redis.call("HSET", key_socket_user, "tid", troupe_id)
+
+	if is_eyeballs_on and user_id and user_id ~= "" then
+		-- Set eyeballs status for non-anonymous users
+		user_in_troupe_count = redis.call("ZINCRBY", key_troupe_users, 1, user_id)
+		redis.call("HSET", key_socket_user, "eb", 1)
+	else
+		-- Clear eyeballs if off or anonymous
+		redis.call("HDEL", key_socket_user, "eb")
+	end
+else
+	-- No longer associated with a troupe
+	redis.call("HDEL", key_socket_user, "tid", "eb")
+end
+
+return { 1, user_in_troupe_count, user_in_prev_troupe_count  }
