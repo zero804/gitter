35ad2b2025c6182df789377102a4278ff7dd173a
Mauro Pompilio <hackers.are.rockstars@gmail.com>
2015-08-25 18:14:14 +0100

    Allow logged in users to chat in the embed

---
+++ public/js/router-nli-embed-chat.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var ChatLayout = require('views/layouts/chat');
+var chatModels = require('collections/chat');
+var onready = require('./utils/onready');
+
+/* Set the timezone cookie */
+require('components/timezone-cookie');
+
+require('components/statsc');
+require('views/widgets/preload');
+require('components/dozy');
+require('template/helpers/all');
+require('components/bug-reporting');
+require('components/ping');
+
+// Preload widgets
+require('views/widgets/avatar');
+require('views/widgets/timeago');
+
+var chatCollection = new chatModels.ChatCollection(null, { listen: true });
+chatCollection.on('add', function (item) {
+  setTimeout(item.set.bind(item, 'unread', false), 500);
+});
+
+onready(function() {
+  var appView = new ChatLayout({ template: false, el: 'body', chatCollection: chatCollection });
+  appView.render();
+});
