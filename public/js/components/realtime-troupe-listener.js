f4afa56f457575b2e14b24fb361ddb591e4ed70a
Andrew Newdigate <andrew@troupe.co>
2015-09-02 15:45:40 +0100

    Switching more stuff when the context changes: realtime-troupe-listener, live-context. Started work on reassociation. Subscribe no longer takes options

--- public/js/components/realtime-troupe-listener.js
+++ public/js/components/realtime-troupe-listener.js
@@ -1,40 +1,33 @@
 "use strict";
+
 var context = require('utils/context');
 var realtime = require('./realtime');
 var appEvents = require('utils/appevents');
-var apiClient = require('components/apiClient');
 
-module.exports = (function() {
+var subscribeCount = 0;
 
-
-  var subscription;
-
-  context.troupe().watch('change:id', function(troupe) {
-    if(subscription) {
-      subscription.cancel();
-      subscription = null;
+realtime.getClient().subscribeTemplate({
+  urlTemplate: '/v1/rooms/:troupeId',
+  contextModel: context.contextModel(),
+  onMessage: function(message) {
+    if(message.notification === 'presence') {
+      if(message.status === 'in') {
+        appEvents.trigger('userLoggedIntoTroupe', message);
+      } else if(message.status === 'out') {
+        appEvents.trigger('userLoggedOutOfTroupe', message);
+      }
     }
+  },
 
-    if(troupe.id) {
-      // raw faye client
-      var client = realtime.getClient();
+  getSubscribeOptions: function() {
+    subscribeCount++;
+    // No need to reassociate the connection on the first subscription
+    if (subscribeCount <= 1) return;
 
-      var url = apiClient.room.channel();
-
-      subscription = client.subscribe(url, function(message) {
-        if(message.notification === 'presence') {
-          if(message.status === 'in') {
-            appEvents.trigger('userLoggedIntoTroupe', message);
-          } else if(message.status === 'out') {
-            appEvents.trigger('userLoggedOutOfTroupe', message);
-          }
-        }
-      });
-
-    }
-
-  });
-
-
-
-})();
+    return {
+      reassociate: {
+        eyeballs: 1 /* TODO: use real eyeballs state */
+      }
+    };
+  }
+});
