08307db5c54277b39fda697ba361fb4939a5670a
Mauro Pompilio <hackers.are.rockstars@gmail.com>
2015-09-14 14:50:29 +0100

    Add tests for room-context-service

---
+++ test/integration/services/room-context-service-test.js
@@ -0,0 +1,98 @@
+"use strict";
+
+var testRequire = require('../test-require');
+var assert = require('assert');
+var mockito = require('jsmockito').JsMockito;
+var Q = require('q');
+
+var permissionsModelMock = mockito.mockFunction();
+var roomPermissionsModelMock = mockito.mockFunction();
+
+mockito.when(permissionsModelMock)().thenReturn(Q.resolve(true));
+mockito.when(roomPermissionsModelMock)().thenReturn(Q.resolve(true));
+
+var roomContextService = testRequire.withProxies("./services/room-context-service", {
+  './permissions-model': permissionsModelMock,
+  './room-permissions-model': roomPermissionsModelMock
+});
+
+var fixtureLoader = require('../test-fixtures');
+var fixture = {};
+
+describe('room-context-service', function() {
+
+  before(fixtureLoader(fixture, {
+    user1: {},
+    user2: {},
+    troupe1: {
+      users: ['user1', 'user2']
+    },
+    troupe2: {}
+  }));
+
+  it('should generate context for non-members', function(done) {
+    return roomContextService.findContextForUri(fixture.user1, fixture.troupe2.uri, {})
+    .then(function(roomContext) {
+      assert(!roomContext.roomMember);
+    })
+    .nodeify(done);
+  });
+
+  it('should generate context for members', function(done) {
+    return roomContextService.findContextForUri(fixture.user1, fixture.troupe1.uri, {})
+    .then(function(roomContext) {
+      assert(roomContext.roomMember);
+    })
+    .nodeify(done);
+  });
+
+  it('should throw for users without access to the room', function(done) {
+    mockito.when(roomPermissionsModelMock)().thenReturn(Q.resolve(false));
+
+    return roomContextService.findContextForUri(fixture.user1, fixture.troupe2.uri, {})
+    .then(function(roomContext) {
+    })
+    .catch(function(err) {
+      assert(err.status === 404);
+    })
+    .nodeify(done);
+  });
+
+  it('should generate context for 1:1', function(done) {
+    mockito.when(roomPermissionsModelMock)().thenReturn(Q.resolve(false));
+
+    return roomContextService.findContextForUri(fixture.user1, fixture.user2.username, {})
+    .then(function(roomContext) {
+      assert(roomContext.roomMember);
+    })
+    .catch(function(err) {
+    })
+    .nodeify(done);
+  });
+
+  it('should throw a redirect for 1:1 same user', function(done) {
+    return roomContextService.findContextForUri(fixture.user1, fixture.user1.username, {})
+    .then(function(roomContext) {
+    })
+    .catch(function(err) {
+      assert(err.status === 301);
+      assert(err.path === '/home');
+    })
+    .nodeify(done);
+  });
+
+  it('should be logged in to see a 1:1', function(done) {
+    return roomContextService.findContextForUri(null, fixture.user1.username, {})
+    .then(function(roomContext) {
+    })
+    .catch(function(err) {
+      assert(err.status === 401);
+    })
+    .nodeify(done);
+  });
+
+  after(function() {
+    fixture.cleanup();
+  });
+
+});
