f347c04a2d5900d916c86da0e50641f6916e5335
Andy Trevorah <andy@troupe.co>
2015-09-14 12:02:03 +0100

    replaced old edit resize logic with chat-input-box-view's logic

--- public/js/views/chat/chat-edit-view.js
+++ public/js/views/chat/chat-edit-view.js
@@ -1,84 +1,59 @@
 "use strict";
 var Marionette = require('backbone.marionette');
-var $ = require('jquery');
 var appEvents = require('utils/appevents');
 var hasScrollBars = require('utils/scrollbar-detect');
-
-function calculateMaxHeight() {
-  // TODO: this sucks. normalise everything to HEADER
-  var header = $("#header-wrapper");
-  if(!header.length) header = $("header");
-
-  var headerHeight = header.height();
-
-  return $(document).height() - headerHeight - 140;
-}
-
-var ChatCollectionResizer = function(options) {
-  var el = options.el;
-  var $el = $(el);
-
-  this.resetInput = function(initial) {
-    $el.css({ height: '', 'overflow-y': '' });
-
-    adjustScroll(initial);
-  };
-
-  this.resizeInput = function() {
-    var maxHeight = calculateMaxHeight();
-    var scrollHeight = el.scrollHeight;
-    var height = scrollHeight > maxHeight ? maxHeight : scrollHeight;
-    var offsetHeight = el.offsetHeight;
-
-    if(offsetHeight == height) {
-      return;
-    }
-
-    $el.height(height);
-
-    adjustScroll();
-  };
-
-  function adjustScroll(initial) {
-    /* Tell the chatCollectionView that the viewport will resize
-     * the argument is whether the resize is animated */
-    appEvents.trigger('chatCollectionView:viewportResize', !initial);
-  }
-};
 
 var ChatEditView = Marionette.ItemView.extend({
 
   events: {
-    "keyup": "onKeyUp"
+    'input': 'onTextChange',
+    'blur': 'resetTextareaSize',
   },
 
   initialize: function() {
     if(hasScrollBars()) {
-      this.$el.addClass("scroller");
+      this.$el.addClass('scroller');
     }
 
-    var chatResizer = new ChatCollectionResizer({
-      el: this.el
-    });
-
-    this.chatResizer = chatResizer;
-
-    this.listenTo(this, 'change', function() {
-      chatResizer.resizeInput();
-    });
-
-    chatResizer.resetInput(true);
-
-    this.chatResizer.resizeInput();
-  },
-
-  onKeyUp: function() {
-    this.chatResizer.resizeInput();
+    this.onTextChange();
   },
 
   processInput: function() {
     this.trigger('save', this.$el.val());
-  }
+  },
+
+  onTextChange: function() {
+    if (this.$el.val()) {
+      this.expandTextareaIfNeeded();
+    } else {
+      this.shrinkTextarea();
+    }
+  },
+
+  resetTextareaSize: function() {
+    this.shrinkTextarea();
+    this.expandTextareaIfNeeded();
+  },
+
+  shrinkTextarea: function() {
+    this.$el.css('height', '');
+    appEvents.trigger('chatCollectionView:viewportResize', false);
+  },
+
+  expandTextareaIfNeeded: function() {
+    var $textarea = this.$el;
+    var textarea = $textarea[0];
+    var currentHeight = textarea.offsetHeight;
+    var scrollHeight = textarea.scrollHeight;
+    var maxHeight = window.innerHeight / 2;
+
+    var newHeight = Math.min(scrollHeight, maxHeight);
+
+    if (newHeight > currentHeight) {
+      $textarea.css('height', newHeight);
+      appEvents.trigger('chatCollectionView:viewportResize', true);
+    }
+  },
 
 });
 
