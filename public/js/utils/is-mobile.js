942f771c898cd1d7148e2be187ffc85896d47653
Andy Trevorah <andy@troupe.co>
2015-09-14 11:35:11 +0100

    cleaned up is-mobile

--- public/js/utils/is-mobile.js
+++ public/js/utils/is-mobile.js
@@ -1,16 +1,11 @@
 "use strict";
 
+var memo;
 
-module.exports = (function() {
-  var memo;
+module.exports = function() {
+  if (typeof memo === 'undefined') {
+    memo = document.body.classList.contains('mobile');
+  }
 
-  return function() {
-    if (typeof memo === 'undefined') {
-      memo = document.body.classList.contains('mobile');
-    }
-
-    return memo;
-  };
-
-})();
-
+  return memo;
+};
