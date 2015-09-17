b3777dae4868b7010a748bf8420aa8e8b7efb29a
Andrew Newdigate <andrew@troupe.co>
2015-09-17 15:20:33 +0100

    Live roomMember updates in live-collections

--- public/js/components/live-context.js
+++ public/js/components/live-context.js
@@ -3,6 +3,7 @@ var context = require('utils/context');
 var troupeModels = require('collections/troupes');
 var realtime = require('./realtime');
 var log = require('utils/log');
+var _ = require('underscore');
 
 module.exports = {
   syncRoom: function() {
@@ -21,6 +22,12 @@ module.exports = {
 
         switch(operation) {
           case 'create':
+            var attributes = _.extend({}, parsed.attributes, { roomMember: true });
+            // There can be existing documents for create events if the doc was created on this
+            // client and lazy-inserted into the collection
+            context.troupe().set(attributes);
+            break;
+
           case 'patch':
           case 'update':
             // There can be existing documents for create events if the doc was created on this
@@ -29,13 +36,14 @@ module.exports = {
             break;
 
           case 'remove':
-            // TODO: send out an event that we've been removed from the collection
+            context.troupe().set({ roomMember: false });
             break;
 
           default:
             log.info("Unknown operation " + operation + ", ignoring");
         }
       },
+      
       getSnapshotState: function() {
         /* We don't want snapshots. Just the live stream */
         return false;
