8ed22d72cb01cc55adac0dad41a4d7c3ff8e7911
Andrew Newdigate <andrew@troupe.co>
2015-09-18 18:18:03 +0100

    More debug logging

--- public/js/components/unread-items-frame-client.js
+++ public/js/components/unread-items-frame-client.js
@@ -1,82 +1,78 @@
 "use strict";
+
 var _ = require('underscore');
 var context = require('utils/context');
-var log = require('utils/log');
 var appEvents = require('utils/appevents');
+var debug = require('debug-proxy')('app:unread-items-frame');
 
-module.exports = (function() {
 
+function limit(fn, context, timeout) {
+  return _.throttle(_.bind(fn, context), timeout || 30, { leading: false });
+}
 
-  function limit(fn, context, timeout) {
-    return _.throttle(_.bind(fn, context), timeout || 30, { leading: false });
+// -----------------------------------------------------
+// Counts all the unread items in a troupe collection and
+// publishes notifications on changes
+// -----------------------------------------------------
+
+var TroupeUnreadNotifier = function(troupeCollection) {
+  this._collection = troupeCollection;
+
+  this._currentStoreValueChanged = _.bind(this._currentStoreValueChanged, this);
+  context.troupe().on('change:id', _.bind(this._recount, this));
+
+  this._recountLimited = limit(this._recount, this, 50);
+  this._collection.on('change:unreadItems change:mentions reset sync add remove destroy', this._recountLimited, this);
+
+  this._recountLimited();
+};
+
+TroupeUnreadNotifier.prototype = {
+  _currentStoreValueChanged: function() {
+    this._recountLimited();
+  },
+
+  _recount: function() {
+    function count(memo, troupe) {
+      var c = troupe.get('unreadItems') + troupe.get('mentions');
+      return memo + (c > 0 ? 1 : 0);
+    }
+
+    var c = this._collection;
+
+    var newTroupeUnreadTotal = c.reduce(count, 0);
+
+    var currentUnreadTotal;
+    var currentTroupeId = context.getTroupeId();
+    if(currentTroupeId) {
+      currentUnreadTotal = c.filter(function(trp) { return trp.id === currentTroupeId; }).reduce(count, 0);
+    } else {
+      currentUnreadTotal = 0;
+    }
+
+    var counts = {
+      overall: newTroupeUnreadTotal,
+      current: currentUnreadTotal
+    };
+
+    debug('troupeUnreadTotalChange: overall=%s, current=$s', newTroupeUnreadTotal, currentUnreadTotal);
+
+    appEvents.trigger('troupeUnreadTotalChange', counts);
+
   }
 
-  // -----------------------------------------------------
-  // Counts all the unread items in a troupe collection and
-  // publishes notifications on changes
-  // -----------------------------------------------------
+};
 
-  var TroupeUnreadNotifier = function(troupeCollection) {
-    this._collection = troupeCollection;
+var unreadItemsClient = {
 
-    this._currentStoreValueChanged = _.bind(this._currentStoreValueChanged, this);
-    context.troupe().on('change:id', _.bind(this._recount, this));
+  installTroupeListener: function(troupeCollection) {
+    /* Store can be optional below */
+    new TroupeUnreadNotifier(troupeCollection);
+  }
 
-    this._recountLimited = limit(this._recount, this, 50);
-    this._collection.on('change:unreadItems change:mentions reset sync add remove destroy', this._recountLimited, this);
+};
 
-    this._recountLimited();
-  };
+// Mainly useful for testing
+unreadItemsClient.TroupeUnreadNotifier = TroupeUnreadNotifier;
 
-  TroupeUnreadNotifier.prototype = {
-    _currentStoreValueChanged: function() {
-      this._recountLimited();
-    },
-
-    _recount: function() {
-      function count(memo, troupe) {
-        var c = troupe.get('unreadItems') + troupe.get('mentions');
-        return memo + (c > 0 ? 1 : 0);
-      }
-
-      var c = this._collection;
-
-      var newTroupeUnreadTotal = c.reduce(count, 0);
-
-      var currentUnreadTotal;
-      var currentTroupeId = context.getTroupeId();
-      if(currentTroupeId) {
-        currentUnreadTotal = c.filter(function(trp) { return trp.id === currentTroupeId; }).reduce(count, 0);
-      } else {
-        currentUnreadTotal = 0;
-      }
-
-      var counts = {
-        overall: newTroupeUnreadTotal,
-        current: currentUnreadTotal
-      };
-
-      log.info('uf: troupeUnreadTotalChange', counts);
-
-      appEvents.trigger('troupeUnreadTotalChange', counts);
-
-    }
-
-  };
-
-  var unreadItemsClient = {
-
-    installTroupeListener: function(troupeCollection) {
-      /* Store can be optional below */
-      new TroupeUnreadNotifier(troupeCollection);
-    }
-
-  };
-
-  // Mainly useful for testing
-  unreadItemsClient.TroupeUnreadNotifier = TroupeUnreadNotifier;
-
-  return unreadItemsClient;
-
-})();
-
+module.exports = unreadItemsClient;
