11b1ed1e7c3b57051e1a1fe159939d0e4bb00706
Jon Parsons <jon@ctrlxctrlv.co.uk>
2015-10-05 13:50:57 +0100

    vagrant fixes

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -5,7 +5,26 @@ curl -sL https://deb.nodesource.com/setup_0.10 | sudo bash -
 
 # Install packages
 apt-get update
-apt-get install -y build-essential nodejs python-pip #linux-image-generic-lts-trusty
+apt-get install -y build-essential python-pip git-core #linux-image-generic-lts-trusty
+
+
+# Install nvm and correct node version
+git clone https://github.com/creationix/nvm.git /home/vagrant/.nvm
+pushd /home/vagrant/.nvm
+git checkout `git describe --abbrev=0 --tags`
+
+# Activate NVM
+source nvm.sh
+
+# Install nodejs
+nvm install 0.12
+nvm use 0.12
+
+#Make sure vagrant can use it
+echo "source ~/.nvm/nvm.sh" >> /home/vagrant/.bashrc
+echo "nvm use 0.12" >> /home/vagrant/.bashrc
+popd
+
 
 # Install Docker
 curl -sSL https://get.docker.com/ | sh
@@ -23,5 +42,14 @@ restart docker
 # Set env var for docker-compose
 echo "export DOCKER_HOST=127.0.0.1:2375" >> /home/vagrant/.profile
 
+
+#Install npm 2 because npm3.x.x breaks everything
+# TODO this doesn't update the binary for the vagrant user
+# figure out how to do that in a clean manor
+npm i -g npm@2
+
 # Update npm and install nodemon
 npm install -g npm nodemon
+
+# Make vagrant the owner of all the things (node)
+chown -R  vagrant:vagrant /home/vagrant/.nvm
