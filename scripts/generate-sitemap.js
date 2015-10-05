3400c3308a20c3370e2815c84e2fd403703b4326
Le Roux Bodenstein <leroux@gitter.im>
2015-10-05 17:19:14 +0200

    Archive index in sitemap, split into index.
    This puts 50k rooms per sitemap file and then lists all the sitemap files into
    a sitemap index. So sitemap.xml actually becomes the sitemap index file.
    
    Next step is to link to all the days from the index page. It could even be done
    in a <noscript> block if we don't want to turn this into a design exercise.

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -3,17 +3,33 @@
 /*jshint globalstrict:true, trailing:false, unused:true, node:true */
 "use strict";
 
+var _ = require('underscore');
 var persistence = require('../server/services/persistence-service');
-var sm = require('sitemap');
+//var sm = require('sitemap');
 var moment = require('moment');
 var nconf = require('../server/utils/config');
 var fs = require('fs');
 
+/*
 var opts = require("nomnom")
   .option('sitemap', {
     abbr: 's',
     required: true,
     help: 'Where to write the sitemap file to'
+  })
+  .parse();
+*/
+
+var opts = require("nomnom")
+  .option('tempdir', {
+    abbr: 't',
+    required: true,
+    help: 'Where to write the sitemap files to'
+  })
+  .option('name', {
+    abbr: 'name',
+    required: true,
+    help: 'What to call the sitemap (ie. the prefix)'
   })
   .parse();
 
@@ -24,6 +40,84 @@ function die(error) {
   process.exit(1);
 }
 
+function createSitemap(urls) {
+  var xml = [];
+  xml.push('<?xml version="1.0" encoding="UTF-8"?>');
+  xml.push('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">');
+  urls.forEach(function(url) {
+    xml.push('<url>');
+    xml.push('<loc>'+url+'</loc>');
+    xml.push('<changefreq>monthly</changefreq>');
+    xml.push('</url>');
+  });
+  xml.push('</urlset>');
+  return xml.join('\n')
+}
+
+function createSitemapIndex(urls) {
+  var xml = [];
+
+  xml.push('<?xml version="1.0" encoding="UTF-8"?>');
+  xml.push('<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" ' +
+      'xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" ' +
+      'xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">');
+
+  urls.forEach(function(url, index) {
+    xml.push('<sitemap>');
+    xml.push('<loc>' + url + '</loc>');
+    xml.push('</sitemap>');
+  });
+
+  xml.push('</sitemapindex>');
+  return xml.join('\n')
+};
+
+var query = {
+  security: 'PUBLIC',
+  '$or': [
+    {'noindex': {'$exists': false}},
+    {'noindex': false}
+  ]
+};
+var projection = { _id: 1, uri: 1 };
+persistence.Troupe
+  .find(query, projection)
+  .sort({_id: 1})
+  .exec()
+  .then(function(rooms) {
+    // batch them into groups of 50000 rooms
+    var lists = _.chain(rooms).groupBy(function(item, index) {
+      return Math.floor(index/50000);
+    }).toArray().value();
+
+    var basePath = nconf.get('web:basepath');
+    var sitemapLocation = nconf.get('sitemap:location');
+
+    var sitemapURLs = [];
+    lists.forEach(function(list, index) {
+      var sitemapURL = sitemapLocation.replace('.xml', '-'+index+'.xml');
+      sitemapURLs.push(sitemapURL);
+
+      var urls = [];
+      list.forEach(function(room) {
+        var url = basePath + '/' + room.uri + '/archives/all';
+        urls.push(url);
+      });
+      var sitemapData = createSitemap(urls);
+      //console.log(data);
+      fs.writeFileSync(opts.tempdir+'/'+opts.name+'-'+index+'.xml', sitemapData);
+    });
+
+    var indexData = createSitemapIndex(sitemapURLs);
+    //console.log(data);
+    fs.writeFileSync(opts.tempdir+'/'+opts.name+'.xml', indexData);
+
+    process.exit(0);
+    return;
+  })
+  .catch(die);
+
+/*
 persistence.Troupe.find({ security: 'PUBLIC', '$or': [{'noindex': {'$exists': false}}, {'noindex': false}]}, { _id: 1, uri: 1 })
   .exec()
   .then(function(troupes) {
@@ -95,3 +189,4 @@ persistence.Troupe.find({ security: 'PUBLIC', '$or': [{'noindex': {'$exists': fa
     });
   })
   .catch(die);
+*/
