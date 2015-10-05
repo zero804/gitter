259d5fb9cb1c9ffb4f07b2575b94cbb1df95d99d
Le Roux Bodenstein <leroux@gitter.im>
2015-10-05 17:19:14 +0200

    Archive index in sitemap, split into index.
    This puts 50k rooms per sitemap file and then lists all the sitemap files into
    a sitemap index. So sitemap.xml actually becomes the sitemap index file.
    
    Next step is to link to all the days from the index page. It could even be done
    in a <noscript> block if we don't want to turn this into a design exercise.

--- scripts/crons/generate-sitemap.sh
+++ scripts/crons/generate-sitemap.sh
@@ -3,15 +3,48 @@ set -e
 set -x
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SITEMAP_FILE=${TMPDIR-/tmp}/gitter-sitemap.xml
-rm -f $SITEMAP_FILE $SITEMAP_FILE.gz
+TEMP_DIR=${TMPDIR-/tmp}
+#SITEMAP_FILE=$TEMP_DIR/gitter-sitemap.xml
+#rm -f $SITEMAP_FILE $SITEMAP_FILE.gz
+rm -f $TEMP_DIR/sitemap*
 
-$SCRIPT_DIR/../generate-sitemap.js --sitemap $SITEMAP_FILE
+#$SCRIPT_DIR/../generate-sitemap.js --sitemap $SITEMAP_FILE
+$SCRIPT_DIR/../generate-sitemap.js --tempdir $TEMP_DIR --name sitemap
+# will write:
+# * sitemap.xml
+# * sitemap-0.xml
+# * sitemap-1.xml
+# * sitemap-2.xml
+# * ...
 
-gzip -9 $SITEMAP_FILE
+#gzip -9 $SITEMAP_FILE
+gzip -9 $TEMP_DIR/sitemap*
+# should end up with
+# * sitemap.xml.gz
+# * sitemap-0.xml.gz
+# * sitemap-1.xml.gz
+# * sitemap-2.xml.gz
 
-/usr/local/bin/aws s3 cp $SITEMAP_FILE.gz s3://gitter-sitemap/$NODE_ENV/sitemap.xml --content-encoding gzip --acl public-read
+# * ...
 
-rm -f $SITEMAP_FILE $SITEMAP_FILE.gz
+# I'm worried about this. I mean sure it is a gzipped file, but what tells the
+# browser that once you unzip it it is an xml file other than the file
+# extension? If that's OK then why bother with content-encoding gzip in the
+# first place? We could just link straight to sitemap-*.xml.gz
+#/usr/local/bin/aws s3 cp $SITEMAP_FILE.gz s3://gitter-sitemap/$NODE_ENV/sitemap.xml --content-encoding gzip --acl public-read
+for f in $TEMP_DIR/sitemap*.xml.gz
+do
+  NAME=$(basename $f .gz)
+  /usr/local/bin/aws s3 cp $f s3://gitter-sitemap/$NODE_ENV/$NAME --content-encoding gzip --acl public-read
+done
+# should copy
+# /tmp/sitemap.xml.gz to sitemap.xml
+# /tmp/sitemap-0.xml.gz to sitemap-0.xml
+# /tmp/sitemap-1.xml.gz to sitemap-1.xml
+# /tmp/sitemap-2.xml.gz to sitemap-2.xml
+
+
+#rm -f $SITEMAP_FILE $SITEMAP_FILE.gz
+rm -f $TEMP_DIR/sitemap*
 
 
