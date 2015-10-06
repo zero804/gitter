dfcc97286d63335242d46f29ea45bffaf9fc6810
Le Roux Bodenstein <leroux@gitter.im>
2015-10-06 18:16:48 +0200

    Remove commented out code.

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -10,16 +10,6 @@ var moment = require('moment');
 var nconf = require('../server/utils/config');
 var fs = require('fs');
 
-/*
-var opts = require("nomnom")
-  .option('sitemap', {
-    abbr: 's',
-    required: true,
-    help: 'Where to write the sitemap file to'
-  })
-  .parse();
-*/
-
 var opts = require("nomnom")
   .option('tempdir', {
     abbr: 't',
@@ -104,12 +94,10 @@ persistence.Troupe
         urls.push(url);
       });
       var sitemapData = createSitemap(urls);
-      //console.log(data);
       fs.writeFileSync(opts.tempdir+'/'+opts.name+'-'+index+'.xml', sitemapData);
     });
 
     var indexData = createSitemapIndex(sitemapURLs);
-    //console.log(data);
     fs.writeFileSync(opts.tempdir+'/'+opts.name+'.xml', indexData);
 
     process.exit(0);
@@ -117,76 +105,3 @@ persistence.Troupe
   })
   .catch(die);
 
-/*
-persistence.Troupe.find({ security: 'PUBLIC', '$or': [{'noindex': {'$exists': false}}, {'noindex': false}]}, { _id: 1, uri: 1 })
-  .exec()
-  .then(function(troupes) {
-    var uris = troupes.reduce(function(memo,value) { memo[value._id] = value.uri; return memo; }, {});
-    var ids = troupes.map(function(value) { return value._id; });
-
-    return persistence.ChatMessage.aggregate([
-        { $match: {
-            toTroupeId: { $in: ids }
-          }
-        },
-        { $project: {
-            _id: 0,
-            toTroupeId: 1,
-            sent: 1
-          }
-        },
-        { $group: {
-            _id: "$toTroupeId",
-            dates: {
-              $addToSet: {
-                $add: [
-                  { $multiply: [{ $year: '$sent' }, 10000] },
-                  { $multiply: [{ $month: '$sent' }, 100] },
-                  { $dayOfMonth: '$sent' }
-                ]
-              }
-            }
-          }
-        }
-      ])
-      .exec()
-      .then(function(result) {
-        var sitemap = sm.createSitemap ({
-          hostname: nconf.get('web:basepath')
-        });
-
-        var today = moment.utc().startOf('day');
-
-        result.forEach(function(m) {
-          var uri = uris[m._id];
-
-          m.dates.forEach(function(d) {
-            var date = moment.utc("" + d,  "YYYYMMDD");
-
-            var url = '/' + uri + '/archives/' + date.format('YYYY') + '/' + date.format('MM') + '/' + date.format('DD');
-
-            var changeFreq = 'yearly';
-
-            if(!date.isBefore(today)) {
-              changeFreq = 'hourly';
-            }
-
-            sitemap.add({
-              url: encodeURI(url),
-              changefreq: changeFreq
-            });
-          });
-
-        });
-
-        return sitemap;
-      });
-  })
-  .then(function(sitemap) {
-    fs.writeFile(opts.sitemap, sitemap.toString(), { mode: parseInt('0644', 8) }, function(err) {
-      if(err) return die(err);
-      process.exit(0);
-    });
-  })
-  .catch(die);
-*/
