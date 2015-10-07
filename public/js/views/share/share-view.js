957f78019ca81679421364def7c866ba24c7daf0
Jon Parsons <jon@ctrlxctrlv.co.uk>
2015-10-07 14:52:23 +0100

    udateed share modal

--- public/js/views/share/share-view.js
+++ public/js/views/share/share-view.js
@@ -1,137 +1,131 @@
 "use strict";
 
-var $ = require('jquery');
-var Marionette = require('backbone.marionette');
-var context = require('utils/context');
-var apiClient = require('components/apiClient');
-var social = require('utils/social');
-var ModalView = require('views/modal');
-var cdn = require('utils/cdn');
-var template = require('./tmpl/share.hbs');
+var $             = require('jquery');
+var Marionette    = require('backbone.marionette');
+var context       = require('utils/context');
+var apiClient     = require('components/apiClient');
+var social        = require('utils/social');
+var ModalView     = require('views/modal');
+var cdn           = require('utils/cdn');
+var template      = require('./tmpl/share.hbs');
 var ZeroClipboard = require('zeroclipboard');
 
-module.exports = (function() {
+ZeroClipboard.config({ swfPath: cdn('repo/zeroclipboard/ZeroClipboard.swf') });
 
+var View = Marionette.ItemView.extend({
+  template: template,
+  className: 'share-view',
 
-  ZeroClipboard.config({ swfPath: cdn('repo/zeroclipboard/ZeroClipboard.swf') });
+  initialize: function() {
+    this.listenTo(this, 'menuItemClicked', this.menuItemClicked);
+  },
 
-  var View = Marionette.ItemView.extend({
-    template: template,
-    className: 'share-view',
+  events: {
+    'click .js-badge': 'sendBadgePullRequest'
+  },
 
-    initialize: function() {
-      this.listenTo(this, 'menuItemClicked', this.menuItemClicked);
-    },
+  menuItemClicked: function(button) {
+    switch(button) {
+      case 'add':
+        this.dialog.hide();
+        window.location.hash = "#add";
+        break;
 
-    events: {
-      'click .js-badge': 'sendBadgePullRequest'
-    },
-
-    menuItemClicked: function(button) {
-      switch(button) {
-        case 'add':
-          this.dialog.hide();
-          window.location.hash = "#add";
-          break;
-
-        case 'cancel':
-          this.dialog.hide();
-          break;
-      }
-    },
-
-    getShareUrl: function() {
-      return context.env('basePath') + '/' + context.getTroupe().uri + '?utm_source=share-link&utm_medium=link&utm_campaign=share-link';
-    },
-
-    getBadgeUrl: function() {
-      return context.env('badgeBaseUrl') + '/Join%20Chat.svg';
-    },
-
-    getBadgeMD: function() {
-      var linkUrl = context.env('basePath') + '/' + context.getTroupe().uri + '?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge';
-      return '[![Gitter](' + this.getBadgeUrl() + ')](' + linkUrl + ')';
-    },
-
-    detectFlash: function() {
-      if (navigator.plugins && navigator.plugins["Shockwave Flash"]) {
-        return true;
-      }
-
-      if(~navigator.appVersion.indexOf("MSIE") && !~navigator.userAgent.indexOf("Opera")){
-        try{
-          new window.ActiveXObject("ShockwaveFlash.ShockwaveFlash");
-          return true;
-        } catch(e) {
-          // Ignore the failure
-        }
-      }
-
-      return false;
-    },
-
-    serializeData: function() {
-      var room = context.getTroupe();
-      var isAdmin = context.isTroupeAdmin();
-
-      return {
-        isRepo: room.githubType === 'REPO',
-        isPublic: room.security === 'PUBLIC',
-        allowBadgePR: room.security === 'PUBLIC' && isAdmin,
-        hasFlash: this.detectFlash(),
-        url: this.getShareUrl(),
-        badgeUrl: this.getBadgeUrl(),
-        badgeMD: this.getBadgeMD(),
-        twitterUrl: social.generateTwitterShareUrl(),
-        facebookUrl: social.generateFacebookShareUrl()
-      };
-    },
-
-    onRender: function() {
-      // ZeroClipboard instances are left hanging around
-      // even after this view is closed.
-      // 500pts will be awarded if you can fix this.
-      var clipboard = new ZeroClipboard(this.$el.find('.js-copy'));
-      clipboard.on('aftercopy', function(e) {
-        $(e.target).text('Copied!');
-      });
-    },
-
-    sendBadgePullRequest: function(e) {
-      var btn = e.target;
-      var $btn = $(btn);
-      $btn.text('Sending...');
-      btn.disabled = true;
-
-      apiClient.priv.post('/create-badge', { uri: context.troupe().get('uri') }, { global: false })
-        .then(function() {
-          $btn.text('Pull Request sent!');
-        })
-        .fail(function() {
-          $btn.text('Failed. Try again?');
-          btn.disabled = false;
-        });
+      case 'cancel':
+        this.dialog.hide();
+        break;
     }
-  });
+  },
 
-  var Modal = ModalView.extend({
-    initialize: function(options) {
-      options = options || {};
-      options.title = options.title || "Share this chat room";
+  getShareUrl: function() {
+    return context.env('basePath') + '/' + context.getTroupe().uri + '?utm_source=share-link&utm_medium=link&utm_campaign=share-link';
+  },
 
-      ModalView.prototype.initialize.call(this, options);
-      this.view = new View(options);
-    },
-    menuItems: [
-      { action: "cancel", text: "Close", className: "trpBtnLightGrey" },
-      { action: "add", text: "Add people", className: "trpBtnBlue trpBtnRight"}
-    ]
-  });
+  getBadgeUrl: function() {
+    return context.env('badgeBaseUrl') + '/Join%20Chat.svg';
+  },
 
-  return {
-    View: View,
-    Modal: Modal
-  };
+  getBadgeMD: function() {
+    var linkUrl = context.env('basePath') + '/' + context.getTroupe().uri + '?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge';
+    return '[![Gitter](' + this.getBadgeUrl() + ')](' + linkUrl + ')';
+  },
 
+  detectFlash: function() {
+    if (navigator.plugins && navigator.plugins["Shockwave Flash"]) {
+      return true;
+    }
 
-})();
+    if(~navigator.appVersion.indexOf("MSIE") && !~navigator.userAgent.indexOf("Opera")){
+      try{
+        new window.ActiveXObject("ShockwaveFlash.ShockwaveFlash");
+        return true;
+      } catch(e) {
+        // Ignore the failure
+      }
+    }
+
+    return false;
+  },
+
+  serializeData: function() {
+    var room = context.getTroupe();
+    var isAdmin = context.isTroupeAdmin();
+
+    return {
+      isRepo: room.githubType === 'REPO',
+      isPublic: room.security === 'PUBLIC',
+      allowBadgePR: room.security === 'PUBLIC' && isAdmin,
+      hasFlash: this.detectFlash(),
+      url: this.getShareUrl(),
+      badgeUrl: this.getBadgeUrl(),
+      badgeMD: this.getBadgeMD(),
+      twitterUrl: social.generateTwitterShareUrl(),
+      facebookUrl: social.generateFacebookShareUrl()
+    };
+  },
+
+  onRender: function() {
+    // ZeroClipboard instances are left hanging around
+    // even after this view is closed.
+    // 500pts will be awarded if you can fix this.
+    var clipboard = new ZeroClipboard(this.$el.find('.js-copy'));
+    clipboard.on('aftercopy', function(e) {
+      $(e.target).text('Copied!');
+    });
+  },
+
+  sendBadgePullRequest: function(e) {
+    var btn = e.target;
+    var $btn = $(btn);
+    $btn.text('Sending...');
+    btn.disabled = true;
+
+    apiClient.priv.post('/create-badge', { uri: context.troupe().get('uri') }, { global: false })
+      .then(function() {
+        $btn.text('Pull Request sent!');
+      })
+      .fail(function() {
+        $btn.text('Failed. Try again?');
+        btn.disabled = false;
+      });
+  }
+});
+
+var Modal = ModalView.extend({
+  initialize: function(options) {
+    options = options || {};
+    options.title = options.title || "Share this chat room";
+
+    ModalView.prototype.initialize.call(this, options);
+    this.view = new View(options);
+  },
+  menuItems: [
+    { action: "cancel", text: "Close",      className: "modal--default__footer__btn--negative" },
+    { action: "add",    text: "Add people", className: "modal--default__footer__btn"}
+  ]
+});
+
+module.exports = {
+  View: View,
+  Modal: Modal
+};
