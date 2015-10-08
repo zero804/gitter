73ef4493fb773a2263759818a7f72daf26c47525
Le Roux Bodenstein <leroux@gitter.im>
2015-10-08 19:22:22 +0200

    Streamify the sitemap generation.
    Just because.

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -9,6 +9,7 @@ var persistence = require('../server/services/persistence-service');
 var moment = require('moment');
 var nconf = require('../server/utils/config');
 var fs = require('fs');
+var BatchStream = require('batch-stream');
 
 var opts = require("nomnom")
   .option('tempdir', {
@@ -17,7 +18,7 @@ var opts = require("nomnom")
     help: 'Where to write the sitemap files to'
   })
   .option('name', {
-    abbr: 'name',
+    abbr: 'n',
     required: true,
     help: 'What to call the sitemap (ie. the prefix)'
   })
@@ -62,6 +63,33 @@ function createSitemapIndex(urls) {
   return xml.join('\n')
 };
 
+var batch = new BatchStream({size: 50000});
+var pageNum = 0;
+var sitemapURLs = [];
+var basePath = nconf.get('web:basepath');
+var sitemapLocation = nconf.get('sitemap:location');
+
+batch.on('data', function(rooms) {
+  pageNum++;
+
+  var sitemapURL = sitemapLocation.replace('.xml', '-'+pageNum+'.xml');
+  sitemapURLs.push(sitemapURL);
+
+  var urls = [];
+  rooms.forEach(function(room) {
+    var url = basePath + '/' + room.uri + '/archives';
+    urls.push(url);
+  });
+  var sitemapData = createSitemap(urls);
+  fs.writeFileSync(opts.tempdir+'/'+opts.name+'-'+pageNum+'.xml', sitemapData);
+});
+
+batch.on('end', function() {
+  var indexData = createSitemapIndex(sitemapURLs);
+  fs.writeFileSync(opts.tempdir+'/'+opts.name+'.xml', indexData);
+  process.exit(0);
+});
+
 var query = {
   security: 'PUBLIC',
   '$or': [
@@ -70,38 +98,9 @@ var query = {
   ]
 };
 var projection = { _id: 1, uri: 1 };
-persistence.Troupe
+var stream = persistence.Troupe
   .find(query, projection)
   .sort({_id: 1})
-  .exec()
-  .then(function(rooms) {
-    // batch them into groups of 50000 rooms
-    var lists = _.chain(rooms).groupBy(function(item, index) {
-      return Math.floor(index/50000);
-    }).toArray().value();
+  .stream();
 
-    var basePath = nconf.get('web:basepath');
-    var sitemapLocation = nconf.get('sitemap:location');
-
-    var sitemapURLs = [];
-    lists.forEach(function(list, index) {
-      var sitemapURL = sitemapLocation.replace('.xml', '-'+index+'.xml');
-      sitemapURLs.push(sitemapURL);
-
-      var urls = [];
-      list.forEach(function(room) {
-        var url = basePath + '/' + room.uri + '/archives';
-        urls.push(url);
-      });
-      var sitemapData = createSitemap(urls);
-      fs.writeFileSync(opts.tempdir+'/'+opts.name+'-'+index+'.xml', sitemapData);
-    });
-
-    var indexData = createSitemapIndex(sitemapURLs);
-    fs.writeFileSync(opts.tempdir+'/'+opts.name+'.xml', indexData);
-
-    process.exit(0);
-    return;
-  })
-  .catch(die);
-
+stream.pipe(batch);
