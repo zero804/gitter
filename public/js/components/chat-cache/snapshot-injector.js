c38feb3f707b3ff293704c1c10091add92ae161c
Jon Parsons <jon@ctrlxctrlv.co.uk>
2015-10-22 12:22:19 +0100

    WIP refactor

--- public/js/components/chat-cache/snapshot-injector.js
+++ public/js/components/chat-cache/snapshot-injector.js
@@ -1,21 +1,10 @@
 'use strict';
 
-var promiseDB      = require('promise-db');
 var context        = require('utils/context');
 var chatCollection = require('collections/instances/integrated-items').chats;
 
 context.troupe().on('change:id', onRoomChange);
 
 function onRoomChange() {
-  promiseDB
-    .createDB(context.getIndexedDBConfig())
-    .then(function(db) {
-      return promiseDB.getCollection(db, 'chats', 'roomId', context.troupe().get('id'));
-    })
-    .then(function(collection) {
-      console.log(collection);
-      chatCollection.handleSnapshot(collection);
-    })
-    .done();
 
 }
