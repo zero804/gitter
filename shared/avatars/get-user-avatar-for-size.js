3fa56ed7afe05417e96cf40f2f1395a17024c103
Le Roux Bodenstein <leroux@gitter.im>
2015-10-22 17:03:08 +0200

    Change to resolver fallback method as discussed.

--- shared/avatars/get-user-avatar-for-size.js
+++ shared/avatars/get-user-avatar-for-size.js
@@ -5,14 +5,6 @@ var urlParser = require('../urls/url-parser');
 var resolveAvatarUrl = require('./resolve-avatar-url');
 var _ = require('underscore');
 
-// So when we prerender we get 1, if not then the real thing? Won't that
-// potentially be weird? Rather just return 2x, 3x or 4x always? - 1x screens
-// are increasingly rare and these are tiny tiny thumbnails anyway..
-// Also: putting it in the file duplicates it..
-var getPixelDensity = targetEnv.isBrowser ?
-  function() { return window.devicePixelRatio || 1; } :
-  function() { return 1; };
-
 function getAliasForSizeFromHostname(hostname) {
   // This might have to get more granular than just per-hostname in future if
   // they go through different versioning schemes. It also might be a path
@@ -28,62 +20,47 @@ function getAliasForSizeFromHostname(hostname) {
 }
 
 function sizeUrlForAvatarUrl(url, size) {
+  // NOTE: This will only happen server-side because the client will just use
+  // avatarUrlSmall without going through this again.
   var parsed = urlParser.parse(url, true);
   parsed.query[getAliasForSizeFromHostname(parsed.hostname)] = size;
   return urlParser.format(parsed);
 }
 
-function safeGet(obj, key) {
-  // normal object
-  if (obj[key] !== undefined) {
-    return obj[key];
-  }
-  // backbone model
-  if (obj.get) {
-    return obj.get(key);
-  }
-}
-
-
-var AVATAR_KEYS = [
-  'gravatarImageUrl', // what's in the db, also the ideal
-  'avatar_url', // returned by github, some strategies
-  'avatarUrl', // some backbone models or marionette serializeData()
-  'avatarUrlSmall', // some strategies (deprecated?)
-  'avatarUrlMedium', // some strategies (deprecated?)
-];
-
 module.exports = function getUserAvatarForSize(user, size) {
   // NOTE: user could just be a serialised partial bit of json or a backbone model too
 
-  size = (size || 60) * getPixelDensity();
+  size = size || 60;
 
   if (user) {
-    var key = _.find(AVATAR_KEYS, function(key) {
-      return safeGet(user, key) !== undefined;
-    });
+    var username = user.username;
 
-    if (key) {
-      // prefer a full avatar URL if present
-      return sizeUrlForAvatarUrl(safeGet(user, key), size);
-
-    } else {
-      var username = safeGet(user, 'username');
-      if (username) {
-        // fall back to the github optimised method
-        var spec = {username: username, size: size};
-
-        var gravatarVersion = safeGet(user, 'gravatarVersion');
-        var gv = safeGet(user, 'gv');
-        if (gravatarVersion) {
-          spec.version = gravatarVersion;
-
-        } else if (gv) {
-          spec.version = gv;
-        }
-
-        return resolveAvatarUrl(spec);
+    if (user.avatarUrlSmall) {
+      // don't recalculate it if we already have it (already serialized)
+      if (size == 128 && user.avatarUrlMedium) {
+        return user.avatarUrlMedium;
+      } else {
+        return user.avatarUrlSmall;
       }
+
+    } else if (user.gravatarImageUrl) {
+      // straight outta the db, so figure out what parameter to add
+      return sizeUrlForAvatarUrl(user.gravatarImageUrl, size);
+
+    } else if (username) {
+      // fall back to the username method
+      var spec = {username: username, size: size};
+
+      var gravatarVersion = user.gravatarVersion;
+      var gv = user.gv;
+      if (gravatarVersion) {
+        spec.version = gravatarVersion;
+
+      } else if (gv) {
+        spec.version = gv;
+      }
+
+      return resolveAvatarUrl(spec);
     }
   }
 
