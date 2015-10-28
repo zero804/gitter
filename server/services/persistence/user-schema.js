350ed347948d18de7213b8eaa54123f9f367ffac
Le Roux Bodenstein <leroux@gitter.im>
2015-10-28 17:39:00 +0200

    Revert "Don't mix promise libs in the same file."
    This reverts commit 1733f1663a51944abdc57eb9f9bad93375c35b00.

--- server/services/persistence/user-schema.js
+++ server/services/persistence/user-schema.js
@@ -23,7 +23,6 @@ var UserSchema = new Schema({
   githubUserToken: { type: String }, // The scope for this token will always be 'user'
   githubId: {type: Number },
   googleId: {type: Number },
-  noGitHubIdentity: { type: Boolean, 'default': false },
   permissions: {
     createRoom: { type: Boolean, 'default': true }
   },
@@ -36,7 +35,8 @@ var UserSchema = new Schema({
     abbr: String,   // Abbreviation, like PDT (note that these are NOT globally unique)
     iana: String    // Timezone IANA description, eg `Europe/London` or `America/Los_Angeles`
   },
-  _tv: { type: 'MongooseNumber', 'default': 0 }
+  _tv: { type: 'MongooseNumber', 'default': 0 },
+  noGitHubIdentity: { type: Boolean, 'default': false }
 });
 
 UserSchema.index({ githubId: 1 }, { unique: true, sparse: true }); // TODO: does this still need to be sparse?
