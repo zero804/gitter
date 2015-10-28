6224200fbe82027c58d7468696941d319b65bc3e
Le Roux Bodenstein <leroux@gitter.im>
2015-10-28 11:27:31 +0200

    Refactor passport callback (progress)

--- server/web/strategies/github-upgrade.js
+++ server/web/strategies/github-upgrade.js
@@ -2,10 +2,38 @@
 
 var env = require('gitter-web-env');
 var config = env.config;
+var logger = env.logger;
 
 var GitHubStrategy = require('gitter-passport-github').Strategy;
 var TokenStateProvider = require('gitter-passport-oauth2').TokenStateProvider;
-var githubOauthCallback = require('./github-oauth-callback');
+var githubUserCallback = require('./github-user-callback');
+
+
+function githubUpgradeCallback(req, accessToken, refreshToken, params, _profile, done) {
+  if (req.user && req.session.githubScopeUpgrade) {
+    var requestedScopes = params.scope.split(/,/);
+    var scopeHash = requestedScopes.reduce(function(memo, v) {
+      memo[v] = true;
+      return memo;
+    }, {});
+
+    req.user.githubToken = accessToken;
+    req.user.githubScopes = scopeHash;
+
+    req.user.save(function(err) {
+      if (err) {
+        logger.error('passport: user save failed: ' + err, { exception: err });
+        return done(err);
+      }
+
+      logger.info('passport: User updated with token');
+      return done(null, req.user);
+    });
+
+  } else {
+    githubUserCallback(req, accessToken, refreshToken, params, _profile, done);
+  }
+}
 
 var githubUpgradeStrategy = new GitHubStrategy({
     clientID: config.get('github:client_id'),
@@ -14,7 +42,7 @@ var githubUpgradeStrategy = new GitHubStrategy({
     stateProvider: new TokenStateProvider({ passphrase: config.get('github:statePassphrase') }),
     skipUserProfile: true,
     passReqToCallback: true
-  }, githubOauthCallback);
+  }, githubUpgradeCallback);
 
 githubUpgradeStrategy.name = 'github_upgrade';
 
