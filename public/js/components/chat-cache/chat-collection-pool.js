f5b10a52e9cc96a62dd6abf989cfee127fd70df0
Jon Parsons <jon@ctrlxctrlv.co.uk>
2015-11-06 13:59:34 +0000

    added cache pool flushing

--- public/js/components/chat-cache/chat-collection-pool.js
+++ public/js/components/chat-cache/chat-collection-pool.js
@@ -5,29 +5,57 @@ var ChatCollection   = require('collections/chat').ChatCollection;
 var chatCollection   = require('collections/instances/integrated-items').chats;
 var context          = require('utils/context');
 
-var pool     = {};
-var poolSize = 10;
+var pool        = {};
+var poolSize    = 10;
+var maxPoolSize = 20;
 var lastRoom;
+var userRooms;
 
 module.exports = function chatCollectionPool(roomList) {
-  //sort by last access time
-  roomList = roomList.sort(function(roomA, roomB) {
-    return roomA.lastAccess > roomB.lastAccess ? -1 : 1;
-  });
 
-  //only grab the top 10 rooms
-  roomList.slice(0, poolSize).forEach(function(roomData) {
-    var name = roomData.name;
-    pool[name] =  generateCollection(roomData.id);
-  });
+  userRooms = roomList;
+  generatePool();
 
   //save the last selected room
   lastRoom = context.troupe().get('name');
 
-
   //listen to room changes
   context.troupe().on('change:id', onRoomChange);
 };
+
+function generatePool() {
+
+  //sort by last access time
+  var _roomList = userRooms.sort(function(roomA, roomB) {
+    return roomA.lastAccess > roomB.lastAccess ? -1 : 1;
+  });
+
+  //only grab the top 10 rooms
+  var rooms = _roomList.slice(0, poolSize);
+
+  rooms.forEach(function(roomData) {
+    var name = roomData.name;
+
+    //if we don't already have a collection create one
+    if(!pool[name]) pool[name] =  generateCollection(roomData.id);
+  });
+
+  //flush
+  Object.keys(pool).forEach(function(key){
+
+    //if we have keys that don't exist in the room list remove
+    var shouldKeep = rooms.reduce(function(prev, current){
+      return (prev || current.name === key);
+    }, false);
+
+    if(!shouldKeep) {
+      pool[key] = null;
+      delete pool[key];
+    }
+
+  });
+
+}
 
 function generateCollection(roomId) {
 
@@ -70,4 +98,8 @@ function onRoomChange(model) {
 
   //switch collections
   chatCollection.switchCollection(collection);
+
+  if(Object.keys(pool).length >= maxPoolSize){
+    generatePool();
+  }
 }
