28d60c83029db73c4f766dc795b157b6afaa97fb
Andrew Newdigate <andrew@troupe.co>
2015-11-11 11:23:39 +0000

    Fixes https://github.com/troupe/gitter-webapp/issues/683

--- test/public-js/utils/valid-room-uri-test.js
+++ test/public-js/utils/valid-room-uri-test.js
@@ -50,4 +50,11 @@ describe('valid-room-uri', function () {
     test('/gitterHQ/gitter?test=true');
     test('/gitterHQ/gitter/channel?test=true');
   });
+
+  /**
+   * https://github.com/troupe/gitter-webapp/issues/683
+   */
+  it('should detect orgs with dashes in their names', function() {
+    test('/orgs/dev-ua/rooms', false);
+  });
 });
