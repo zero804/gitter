8aab33004792c0e798e829d998cd4a0c8be509a2
Andrew Newdigate <andrew@troupe.co>
2015-11-16 14:43:02 +0000

    Feature toggles are now set in mongodb

--- server/web/middlewares/feature-toggles.js
+++ server/web/middlewares/feature-toggles.js
@@ -2,33 +2,51 @@
 
 var fflip = require('fflip');
 var mongoUtils = require('../../utils/mongo-utils');
+var persistence = require('../../services/persistence-service');
 
+/**
+ * List of criteria functions to be used by feature toggles
+ */
 var Criteria = {
+  /* Allow a certain percentage of users */
   percentageOfUsers: function(user, percent) {
+    if (!user) return false;
     var timestamp = Math.round(mongoUtils.getTimestampFromObjectId(user._id) / 1000) || 0;
-
     return (timestamp % 100 < percent);
   },
 
+  /* Allow a hash of usernames */
   allowUsernames: function(user, usernameHash) {
+    if (!user) return false;
     if (!usernameHash) return false;
-    return usernameHash[user.username];
+    return !!usernameHash[user.username];
+  },
+
+  /* Enabled criteria */
+  enabled: function(/*user*/) {
+    return true;
   }
 };
 
-// TODO: replace this with a dynamic lookup
-var StaticFeatures = {
-  halley: {
-    criteria: {
-      allowUsernames: { suprememoocow: 1 }
-    }
-  }
-};
+function getFeatures(callback) {
+  persistence.FeatureToggle.find({})
+    .lean()
+    .exec()
+    .then(function(togglesList) {
+      var featureToggles = togglesList.reduce(function(memo, toggle) {
+        memo[toggle.name] = { criteria: toggle.criteria };
+        return memo;
+      }, {});
+
+      // NOTE: this callback doesn't have an err.
+      callback(featureToggles);
+    });
+}
 
 fflip.config({
   criteria: Criteria,
-  features: StaticFeatures,
-  reload: 60
+  features: getFeatures,
+  reload: 60 // Reload features every 60 seconds
 });
 
 
