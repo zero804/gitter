6462ff5c3523a61b2d8dbeab9f2201f38ef660e9
Le Roux Bodenstein <leroux@gitter.im>
2015-12-07 15:33:46 +0200

    Move code from route to restful for easier testing.

--- server/api/v1/user-profile.js
+++ server/api/v1/user-profile.js
@@ -1,31 +1,12 @@
 "use strict";
 
-var userService = require("../../services/user-service");
-var restSerializer = require("../../serializers/rest-serializer");
-var userScopes = require("../../utils/models/user-scopes");
 var StatusError = require('statuserror');
-var gitHubProfileService = require('gitter-web-github-backend/lib/github-profile-service');
+var restful = require("../../services/restful");
 
 module.exports = function getUserProfile(req, res, next) {
   if (!req.params || !req.params.username) return next(new StatusError(404));
 
-  var username = req.params.username;
-  userService.findByUsername(username)
-    .then(function(user) {
-      if (user) {
-        var strategy = new restSerializer.UserProfileStrategy();
-        return restSerializer.serialize(user, strategy);
-
-      } else {
-        var gitHubUser = {username: username};
-
-        if (!userScopes.isGitHubUser(gitHubUser)) {
-          return next(new StatusError(404));
-        }
-
-        return gitHubProfileService(gitHubUser, true);
-      }
-    })
+  return restful.serializeProfileForUsername(req.params.username)
     .then(function(response) {
       res.send(response);
     })
