54f1ff9739479c4d956e741aad25fe6aabc791e2
Jon Parsons <jon@ctrlxctrlv.co.uk>
2015-12-08 10:57:14 +0000

    added orgList generator

--- shared/orgs/suggested-orgs-from-room-list.js
+++ shared/orgs/suggested-orgs-from-room-list.js
@@ -3,7 +3,7 @@ var _ = require('lodash');
 
 module.exports = function suggestedOrgsFromRoomList(roomList){
   return roomList.reduce(function(memo, room){
-    if(room.githubType === 'ONETOONE') return memo;
+    if(room.githubType !== 'ORG') return memo;
     var orgName = room.name.split('/')[0];
     if(!!_.where(memo, { name: orgName }).length) return memo;
     memo.push({ name: orgName });
--- test/in-browser/specs/shared/suggested-orgs-from-room-list-test.js
+++ test/in-browser/specs/shared/suggested-orgs-from-room-list-test.js
@@ -5,7 +5,7 @@ var assert                    = require('assert');
 var _                         = require('underscore');
 var suggestedOrgsFromRoomList = require('../../../../shared/orgs/suggested-orgs-from-room-list');
 
-describe.only('suggestedOrgsFromRoomList', function() {
+describe('suggestedOrgsFromRoomList', function() {
 
   var rooms = [
     { name: 'gitterHQ', githubType: 'ORG', favourite: 1 },
@@ -19,6 +19,7 @@ describe.only('suggestedOrgsFromRoomList', function() {
     { name: 'someusername', githubType: 'ONETOONE' },
     { name: 'someotherusername', githubType: 'ONETOONE', favourite: 4 },
     { name: 'username', githubType: 'ONETOONE' },
+    { name: 'someorg/somerepo', githubType: 'REPO' },
   ];
 
   it('should contain no one-to-one rooms', function(){
@@ -26,6 +27,11 @@ describe.only('suggestedOrgsFromRoomList', function() {
     assert.equal(0, _.where(orgs, {name: 'someuser'}));
     assert.equal(0, _.where(orgs, {name: 'someotherusername'}));
     assert.equal(0, _.where(orgs, {name: 'username'}));
+  });
+
+  it('should contain no repo rooms', function(){
+    var orgs = suggestedOrgsFromRoomList(rooms);
+    assert.equal(0, _.where(orgs, {name: 'someorg'}));
   });
 
   it('should contain no entries that have a / in the name', function(){
