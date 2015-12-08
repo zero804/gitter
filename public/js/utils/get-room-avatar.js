caa6c6870a58bc6e985c60fcbe4e3c286228e36e
Jon Parsons <jon@ctrlxctrlv.co.uk>
2015-12-08 12:07:37 +0000

    fixed orgList

--- public/js/utils/get-room-avatar.js
+++ public/js/utils/get-room-avatar.js
@@ -1,7 +1,11 @@
 'use strict';
 
+/*
+ *  TODO MOve this into shared
+ * */
+
 module.exports = function getRoomAvatar(roomName) {
   if(!roomName) throw new Error('A valid room name must be passed to getRoomAvatar');
   var name = /\//.test(roomName) ? roomName.split('/')[0] : roomName;
   return 'https://avatars1.githubusercontent.com/' + name;
-}
+};
--- shared/orgs/suggested-orgs-from-room-list.js
+++ shared/orgs/suggested-orgs-from-room-list.js
@@ -1,12 +1,13 @@
 'use strict';
 var _ = require('lodash');
+var getRoomAvatar = require('../../public/js/utils/get-room-avatar');
 
 module.exports = function suggestedOrgsFromRoomList(roomList){
   return roomList.reduce(function(memo, room){
     if(room.githubType !== 'ORG') return memo;
     var orgName = room.name.split('/')[0];
     if(!!_.where(memo, { name: orgName }).length) return memo;
-    memo.push({ name: orgName });
+    memo.push({ name: orgName, imgUrl: getRoomAvatar(orgName) });
     return memo;
   }, []);
 };
