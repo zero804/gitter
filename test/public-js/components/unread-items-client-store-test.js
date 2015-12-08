4df5c511146bfbb28128ed4bf73c04e2b943396b
Andrew Newdigate <andrew@troupe.co>
2015-12-08 11:02:02 +0000

    Better unread items handling with chat cache

--- test/public-js/components/unread-items-client-store-test.js
+++ test/public-js/components/unread-items-client-store-test.js
@@ -1,80 +1,132 @@
 /*jslint node:true, unused:true*/
-/*global describe:true, it:true */
+/*global describe:true, it:true, beforeEach:true */
 
 var UnreadItemsStore = require('../../../public/js/components/unread-items-client-store');
 var assert = require('assert');
+var sinon = require('sinon');
 
 describe('unread-items-client-store', function() {
+  beforeEach(function() {
+    this.store = new UnreadItemsStore();
+    this.onUnreadItemRemoved = sinon.spy();
+    this.onChangeStatus = sinon.spy();
+    this.onItemMarkedRead = sinon.spy();
+    this.onAdd = sinon.spy();
+
+    this.store.on('unreadItemRemoved', this.onUnreadItemRemoved);
+    this.store.on('change:status', this.onChangeStatus);
+    this.store.on('itemMarkedRead', this.onItemMarkedRead);
+    this.store.on('add', this.onAdd);
+  });
+
   it('should add items', function() {
-    var store = new UnreadItemsStore();
-    store._unreadItemAdded("1");
+    var store = this.store;
+    store._unreadItemAdded("1", false);
     assert.strictEqual(store.length, 1);
     assert.deepEqual(store.getItems(), ["1"]);
+
+    assert.strictEqual(1, this.onAdd.callCount);
+    assert(this.onAdd.calledWith("1", false));
   });
 
   it('should add multiple items', function() {
-    var store = new UnreadItemsStore();
-    store._unreadItemAdded("1");
-    store._unreadItemAdded("2");
+    var store = this.store;
+    store._unreadItemAdded("1", false);
+    store._unreadItemAdded("2", false);
     assert.strictEqual(store.length, 2);
     assert.deepEqual(store.getItems(), ["1", "2"]);
     assert.strictEqual(store.getFirstItem(), "1");
+
+    assert.strictEqual(2, this.onAdd.callCount);
+    assert(this.onAdd.calledWith("1", false));
+    assert(this.onAdd.calledWith("2", false));
   });
 
   it('should add mentions', function() {
-    var store = new UnreadItemsStore();
+    var store = this.store;
     store._unreadItemAdded("1", true);
     assert.strictEqual(store.length, 1);
     assert.deepEqual(store.getItems(), ["1"]);
     assert.deepEqual(store.getMentions(), ["1"]);
+
+    assert.strictEqual(1, this.onAdd.callCount);
+    assert(this.onAdd.calledWith("1", true));
   });
 
   it('should add unread items which become mentions', function() {
-    var store = new UnreadItemsStore();
+    var store = this.store;
     store._unreadItemAdded("1", false);
 
     assert.strictEqual(store.length, 1);
     assert.deepEqual(store.getItems(), ["1"]);
     assert.deepEqual(store.getMentions(), []);
 
+    assert.strictEqual(1, this.onAdd.callCount);
+    assert(this.onAdd.calledWith("1", false));
+
     store._unreadItemAdded("1", true);
 
     assert.strictEqual(store.length, 1);
     assert.deepEqual(store.getItems(), ["1"]);
     assert.deepEqual(store.getMentions(), ["1"]);
+
+    assert.strictEqual(1, this.onAdd.callCount);
+    assert.strictEqual(1, this.onChangeStatus.callCount);
+
+    assert(this.onChangeStatus.calledWith("1", true));
+
   });
 
   it('should add unread items which become unmentions', function() {
-    var store = new UnreadItemsStore();
+    var store = this.store;
     store._unreadItemAdded("1", true);
 
     assert.strictEqual(store.length, 1);
     assert.deepEqual(store.getItems(), ["1"]);
     assert.deepEqual(store.getMentions(), ["1"]);
 
+    assert.strictEqual(1, this.onAdd.callCount);
+    assert(this.onAdd.calledWith("1", true));
 
     store._unreadItemAdded("1", false);
 
     assert.strictEqual(store.length, 1);
     assert.deepEqual(store.getItems(), ["1"]);
     assert.deepEqual(store.getMentions(), []);
+
+    assert.strictEqual(1, this.onAdd.callCount);
+    assert.strictEqual(1, this.onChangeStatus.callCount);
+    assert(this.onChangeStatus.calledWith("1", false));
   });
 
   it('should remove', function() {
-    var store = new UnreadItemsStore();
-    store._unreadItemAdded("1");
+    var store = this.store;
+    store._unreadItemAdded("1", false);
+
+    assert.strictEqual(1, this.onAdd.callCount);
+    assert(this.onAdd.calledWith("1", false));
+
     store._unreadItemRemoved("1");
+
+    assert.strictEqual(1, this.onUnreadItemRemoved.callCount);
+    assert(this.onUnreadItemRemoved.calledWith("1"));
 
     assert.strictEqual(store.length, 0);
     assert.deepEqual(store.getItems(), []);
 
-    assert(store.isMarkedAsRead("1"));
+    assert.strictEqual(false, store.isMarkedAsRead("1"));
   });
 
   it('should mark an item as read', function() {
-    var store = new UnreadItemsStore();
-    store._unreadItemAdded("1");
-    store._markItemRead("1");
+    var store = this.store;
+    store._unreadItemAdded("1", false);
+    store.markItemRead("1");
+
+    assert.strictEqual(1, this.onAdd.callCount);
+    assert(this.onAdd.calledWith("1", false));
+
+    assert.strictEqual(1, this.onItemMarkedRead.callCount);
+    assert(this.onItemMarkedRead.calledWith("1", false, false));
 
     assert.strictEqual(store.length, 0);
     assert.deepEqual(store.getItems(), []);
@@ -83,9 +135,12 @@ describe('unread-items-client-store', function() {
   });
 
   it('should handle bulk unreadItemsAdded', function() {
-    var store = new UnreadItemsStore();
+    var store = this.store;
 
-    store._unreadItemsAdded({ chat: ["1" ] });
+    store.add({ chat: ["1" ] });
+
+    assert.strictEqual(1, this.onAdd.callCount);
+    assert(this.onAdd.calledWith("1", false));
 
     assert.strictEqual(store.length, 1);
     assert.deepEqual(store.getItems(), ["1"]);
@@ -93,9 +148,12 @@ describe('unread-items-client-store', function() {
   });
 
   it('should handle bulk unreadItemsAdded with mentions', function() {
-    var store = new UnreadItemsStore();
+    var store = this.store;
 
-    store._unreadItemsAdded({ chat: ["1"], mention: ["1"] });
+    store.add({ chat: ["1"], mention: ["1"] });
+
+    assert.strictEqual(1, this.onAdd.callCount);
+    assert(this.onAdd.calledWith("1", true));
 
     assert.strictEqual(store.length, 1);
     assert.deepEqual(store.getItems(), ["1"]);
@@ -103,36 +161,49 @@ describe('unread-items-client-store', function() {
   });
 
   it('should handle bulk unreadItemsRemoved', function() {
-    var store = new UnreadItemsStore();
+    var store = this.store;
 
     store._unreadItemAdded("1");
     store._unreadItemAdded("2");
 
-    store._unreadItemsRemoved({ chat: ["1"] });
+    store.remove({ chat: ["1"] });
+
+    assert.strictEqual(1, this.onUnreadItemRemoved.callCount);
+    assert(this.onUnreadItemRemoved.calledWith("1"));
 
     assert.strictEqual(store.length, 1);
     assert.deepEqual(store.getItems(), ["2"]);
     assert.deepEqual(store.getMentions(), []);
+
+    assert(!store.isMarkedAsRead("1"));
   });
 
   it('should handle bulk unreadItemsRemoved with mentions', function() {
-    var store = new UnreadItemsStore();
+    var store = this.store;
 
     store._unreadItemAdded("1", true);
     store._unreadItemAdded("2", true);
 
-    store._unreadItemsRemoved({ chat: ["1"], mention: ["1"] });
+    store.remove({ chat: ["1"], mention: ["1"] });
+
+    assert.strictEqual(1, this.onUnreadItemRemoved.callCount);
+    assert(this.onUnreadItemRemoved.calledWith("1"));
 
     assert.strictEqual(store.length, 1);
     assert.deepEqual(store.getItems(), ["2"]);
     assert.deepEqual(store.getMentions(), ["2"]);
+
+    assert(!store.isMarkedAsRead("1"));
   });
 
   it('should handle bulk unreadItemsAdded with mention upgrade', function() {
-    var store = new UnreadItemsStore();
+    var store = this.store;
     store._unreadItemAdded("1", false);
 
-    store._unreadItemsAdded({ mention: ["1"] });
+    store.add({ mention: ["1"] });
+
+    assert.strictEqual(1, this.onChangeStatus.callCount);
+    assert(this.onChangeStatus.calledWith("1", true));
 
     assert.strictEqual(store.length, 1);
     assert.deepEqual(store.getItems(), ["1"]);
@@ -140,16 +211,76 @@ describe('unread-items-client-store', function() {
   });
 
   it('should handle bulk unreadItemsRemoved with mention downgrade', function() {
-    var store = new UnreadItemsStore();
+    var store = this.store;
     store._unreadItemAdded("1", true);
 
-    store._unreadItemsRemoved({ mention: ["1"] });
+    store.remove({ mention: ["1"] });
+
+    assert.strictEqual(1, this.onChangeStatus.callCount);
+    assert(this.onChangeStatus.calledWith("1", false));
 
     assert.strictEqual(store.length, 1);
     assert.deepEqual(store.getItems(), ["1"]);
     assert.deepEqual(store.getMentions(), []);
 
     assert(!store.isMarkedAsRead("1"));
+  });
+
+  it('should handle an item being added after its been marked as read', function() {
+    var store = this.store;
+    store.markItemRead("1");
+
+    assert.strictEqual(1, this.onItemMarkedRead.callCount);
+    assert.strictEqual(0, this.onAdd.callCount);
+    assert(this.onItemMarkedRead.calledWith("1", false, false));
+
+    store.add({ chat: ["1"] });
+
+    assert.strictEqual(2, this.onItemMarkedRead.callCount);
+    assert.strictEqual(0, this.onAdd.callCount);
+
+    assert.strictEqual(store.length, 0);
+    assert.deepEqual(store.getItems(), []);
+    assert.deepEqual(store.getMentions(), []);
+
+    assert(store.isMarkedAsRead("1"));
+  });
+
+  it('should handle markAllAsRead', function() {
+    var store = this.store;
+    store.add({ chat: ["1", "2"], mention: ["1"] });
+
+    store.markAllRead();
+
+    assert.strictEqual(2, this.onItemMarkedRead.callCount);
+    assert(this.onItemMarkedRead.calledWith("1", true, false));
+    assert(this.onItemMarkedRead.calledWith("2", false, false));
+
+    assert.strictEqual(store.length, 0);
+    assert.deepEqual(store.getItems(), []);
+    assert.deepEqual(store.getMentions(), []);
+
+    assert(store.isMarkedAsRead("1"));
+    assert(store.isMarkedAsRead("2"));
+  });
+
+  it('should handle markAllReadNotification', function() {
+    var store = this.store;
+    store.add({ chat: ["1", "2"], mention: ["1"] });
+
+    store.markAllReadNotification();
+
+    assert.strictEqual(0, this.onItemMarkedRead.callCount);
+    assert.strictEqual(2, this.onUnreadItemRemoved.callCount);
+    assert(this.onUnreadItemRemoved.calledWith("1"));
+    assert(this.onUnreadItemRemoved.calledWith("2"));
+
+    assert.strictEqual(store.length, 0);
+    assert.deepEqual(store.getItems(), []);
+    assert.deepEqual(store.getMentions(), []);
+
+    assert(!store.isMarkedAsRead("1"));
+    assert(!store.isMarkedAsRead("2"));
   });
 
 
