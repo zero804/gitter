a1367ebdc799c797d6d35b8640fd514e51015b71
Jon Parsons <jon@ctrlxctrlv.co.uk>
2015-12-15 15:47:56 +0000

    added hover effect to pinned menu

--- public/js/views/menu/room/footer/footer-view.js
+++ public/js/views/menu/room/footer/footer-view.js
@@ -21,7 +21,6 @@ module.exports = Marionette.ItemView.extend({
     e.preventDefault();
     var newVal = !this.model.get('roomMenuIsPinned');
     this.model.set('roomMenuIsPinned', newVal);
-    console.log('setting panelOpenState to', newVal);
     this.model.set('panelOpenState', newVal);
     this.bus.trigger('room-menu:pin', newVal);
   },
