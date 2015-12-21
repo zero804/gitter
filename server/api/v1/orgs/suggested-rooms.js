b5907ff902928150389a4f31eb63ec2b402633bc
Jon Parsons <jon@ctrlxctrlv.co.uk>
2015-12-21 16:42:28 +0000

    push lcOwner data into neo4j

---
+++ server/api/v1/orgs/suggested-rooms.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  index: function(req) {
+    console.log('-----------------------');
+    console.log('holy what now', req.params);
+    console.log('-----------------------');
+  },
+};
