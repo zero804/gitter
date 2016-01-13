fc4cfec8ea94d9a044a0586f6b0cfde6913d8a83
Le Roux Bodenstein <leroux@gitter.im>
2016-01-13 14:54:24 +0200

    Replace the login link in the chat frame with a modal.

---
+++ public/js/views/modals/login-view.js
@@ -0,0 +1,46 @@
+"use strict";
+
+var $             = require('jquery');
+var Marionette    = require('backbone.marionette');
+var ModalView     = require('./modal');
+var template      = require('./tmpl/login-view.hbs');
+
+require('gitter-styleguide/css/components/buttons.css');
+
+
+var View = Marionette.ItemView.extend({
+  template: template,
+  className: 'login-view',
+
+  initialize: function() {
+    this.listenTo(this, 'menuItemClicked', this.menuItemClicked);
+  },
+
+  events: {
+  },
+
+  menuItemClicked: function(button) {
+    switch (button) {
+      case 'cancel':
+        this.dialog.hide();
+        break;
+    }
+  },
+
+  serializeData: function() {
+    return { };
+  },
+
+});
+
+var Modal = ModalView.extend({
+  initialize: function(options) {
+    options = options || {};
+    options.title = options.title || "Login";
+
+    ModalView.prototype.initialize.call(this, options);
+    this.view = new View(options);
+  }
+});
+
+module.exports = Modal;
