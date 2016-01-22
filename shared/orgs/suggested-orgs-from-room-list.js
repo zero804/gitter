579efc011888851a942bee465dafa7afa0bd59a6
Jon Parsons <Pastey@ip-192-168-1-142.ec2.internal>
2016-01-22 15:13:37 +0000

    removed org check when generating suggested rooms

--- shared/orgs/suggested-orgs-from-room-list.js
+++ shared/orgs/suggested-orgs-from-room-list.js
@@ -4,7 +4,6 @@ var getRoomAvatar = require('../../public/js/utils/get-room-avatar');
 
 module.exports = function suggestedOrgsFromRoomList(roomList){
   return roomList.reduce(function(memo, room){
-    if(room.githubType !== 'ORG') return memo;
     var orgName = room.name.split('/')[0];
     if(!!_.where(memo, { name: orgName }).length) return memo;
     memo.push({ name: orgName, imgUrl: getRoomAvatar(orgName) });
