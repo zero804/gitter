3588cd9369c6a40eea7926654568dadb51ba1741
Le Roux Bodenstein <leroux@gitter.im>
2016-01-25 16:28:07 +0200

    Take room language into account for suggestions.

--- scripts/utils/list-suggested-rooms.js
+++ scripts/utils/list-suggested-rooms.js
@@ -19,6 +19,7 @@ var opts = require("nomnom")
 
 
 function getRooms() {
+  var lang = opts.lang || 'en';
   if (opts.uri) {
     return troupeService.findByUri(opts.uri)
       .then(function(room) {
@@ -33,13 +34,41 @@ function getRooms() {
         // cap it just in case
         // roomIds = roomIds.slice(0, 10);
 
-        // NOTE: this isn't needed in a script to just find suggestions, but
-        // I'm debugging the input rooms
-        return troupeService.findByIdsLean(roomIds, { uri: 1, url: 1, name: 1, userCount: 1});
+        // NOTE: we'll only need id and lang in normal operation
+        return troupeService.findByIdsLean(roomIds, {
+          uri: 1,
+          lang: 1,
+          name: 1,
+          userCount: 1
+        });
       });
   } else {
     throw new Error('uri or username required');
   }
+}
+
+function mostPopularItem(strings) {
+  if (strings.length == 0) return undefined;
+  return _(strings).countBy().toPairs().max(_.last).head().value();
+}
+
+function pickLanguageFromRooms(rooms) {
+  // languages that aren't null or undefined
+  var languages = _(rooms).pluck('lang').filter().value();
+
+  // try the most popular non-english one
+  // (or is it better to go with just the most popular one?)
+  var nonEnglish = _.filter(languages, function(lang) { return lang != 'en'; });
+  var mostPopular;
+  if (nonEnglish) {
+    mostPopular = mostPopularItem(nonEnglish);
+    if (mostPopular) {
+      return mostPopular;
+    }
+  }
+
+  // then fall back to english
+  return 'en';
 }
 
 getRooms()
@@ -47,16 +76,17 @@ getRooms()
     rooms.sort(function(a, b) {
       return a.userCount - b.userCount;
     });
-    console.log("input", rooms);    
-    return suggestions.getSuggestionsForRooms(rooms);
+    console.log("input", rooms);
+    var lang = pickLanguageFromRooms(rooms);
+    console.log("lang", lang)
+    return suggestions.getSuggestionsForRooms(rooms, lang);
   })
   .then(function(suggestions) {
-    console.log("suggestions", suggestions);
     var roomIds = _.pluck(suggestions, 'roomId');
-    return troupeService.findByIdsLean(roomIds, {uri: 1, url: 1, name: 1});
+    return troupeService.findByIdsLean(roomIds, {uri: 1, name: 1});
   })
   .then(function(suggestedRooms) {
-    console.log("suggestedRooms", suggestedRooms);
+    console.log("suggestedRooms", _.pluck(suggestedRooms, 'uri'));
   })
   .catch(function(err) {
     console.error(err.stack);
