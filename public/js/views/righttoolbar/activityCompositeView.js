79ed2c90274ed87a2b6bf3fbc7d3e2265fe3dfca
Andrew Newdigate <andrew@troupe.co>
2016-01-26 14:47:09 +0000

    Fixed activity tooltip

--- public/js/views/righttoolbar/activityCompositeView.js
+++ public/js/views/righttoolbar/activityCompositeView.js
@@ -86,7 +86,7 @@ module.exports = (function() {
       Tooltip: {
         '#time': {
           titleFn: 'getTimeTooltip',
-          position: 'left'
+          placement: 'left'
         },
       }
     },
