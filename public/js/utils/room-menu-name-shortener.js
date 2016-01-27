3ae60e9d8668eebf3942355035a19101319a9ede
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-01-27 16:35:26 +0000

    amend roomNameShortener to work properly

--- public/js/utils/room-menu-name-shortener.js
+++ public/js/utils/room-menu-name-shortener.js
@@ -2,5 +2,6 @@
 
 module.exports = function roomNameShortener(name){
   if(!name) return '';
+  if(name[0] === '/') { name = name.substring(1) }
   return (name.length > 19) ? name.split('/')[1] : name;
 };
