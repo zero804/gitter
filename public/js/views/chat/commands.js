1a1c492e878c0d5e7f1d4de9272af7e624b6d9f9
Andrew Newdigate <andrew@troupe.co>
2016-01-27 12:26:09 +0000

    Removed lurk command and added new notify-* commands

--- public/js/views/chat/commands.js
+++ public/js/views/chat/commands.js
@@ -4,272 +4,288 @@ var context = require('utils/context');
 var appEvents = require('utils/appevents');
 var apiClient = require('components/apiClient');
 
-module.exports = (function() {
 
+function updateNotifications(mode) {
+  apiClient.userRoom.put('/settings/notifications', { mode: mode })
+    .then(function() {
+      appEvents.triggerParent('user_notification', {
+        title: "Notifications",
+        text: "Notification settings have been updated for this room"
+      });
+    });
+}
 
-  var commandsList = [
-    {
-      command: 'ban @username',
-      description: 'Ban somebody from the room.',
-      criteria: function() {
-        var isOrgRoom = false;
-        if (context.troupe().get("githubType") == "ORG") isOrgRoom = true;
-        return !context.inOneToOneTroupeContext() && context.isTroupeAdmin() && !isOrgRoom;
-      },
-      completion: 'ban @',
-      regexp: /^\/ban/,
-      action: function(text) {
-        var userMatch = text.match(/\/ban @([\w\-]+)(\s+(removemsgs))?/);
-        if (!userMatch) return;
-        var user = userMatch[1];
-        var removeMessages = !!userMatch[3];
-
-        apiClient.room.post('/bans', { username: user, removeMessages: removeMessages })
-          .catch(function(e) {
-            var errorMessage;
-            switch(e.status) {
-              case 403:
-                errorMessage = 'You do not have permission to ban people.';
-                break;
-
-              case 400:
-                errorMessage = e.friendlyMessage || "Ban failed";
-                break;
-
-              case 404:
-                errorMessage = 'That person does not exist (on Gitter that is)';
-                break;
-              default:
-                errorMessage = 'Ban failed';
-            }
-
-            appEvents.triggerParent('user_notification', {
-              title: 'Could not ban user',
-              text: errorMessage,
-              className: 'notification-error'
-            });
-          });
-      }
+var commandsList = [
+  {
+    command: 'ban @username',
+    description: 'Ban somebody from the room.',
+    criteria: function() {
+      var isOrgRoom = false;
+      if (context.troupe().get("githubType") == "ORG") isOrgRoom = true;
+      return !context.inOneToOneTroupeContext() && context.isTroupeAdmin() && !isOrgRoom;
     },
-    {
-      command: 'channel',
-      description: 'Create/join a channel',
-      completion: 'channel ',
-      regexp: /^\/channel/,
-      action: function(text) {
-        var channelMatch = text.match(/^\s*\/channel(?:\s+(\w+))?/);
-        var channel = channelMatch[1];
+    completion: 'ban @',
+    regexp: /^\/ban/,
+    action: function(text) {
+      var userMatch = text.match(/\/ban @([\w\-]+)(\s+(removemsgs))?/);
+      if (!userMatch) return;
+      var user = userMatch[1];
+      var removeMessages = !!userMatch[3];
 
-        if(channel) {
-          appEvents.trigger('route', 'createcustomroom/' + channel);
-        } else {
-          appEvents.trigger('route', 'createcustomroom');
-        }
-      }
-    },
-    {
-      command: 'fav',
-      description: 'Toggle the room as a favourite',
-      completion: 'fav ',
-      regexp: /^\/fav\s*$/,
-      action: function() {
-        var isFavourite = !context.troupe().get('favourite');
+      apiClient.room.post('/bans', { username: user, removeMessages: removeMessages })
+        .catch(function(e) {
+          var errorMessage;
+          switch(e.status) {
+            case 403:
+              errorMessage = 'You do not have permission to ban people.';
+              break;
 
-        apiClient.userRoom.put('', { favourite: isFavourite });
-      }
-    },
-    {
-      command: 'leave',
-      description: 'Leave the room',
-      completion: 'leave ',
-      regexp: /^\/(leave|part)\s*$/,
-      criteria: function() {
-        return !context.inOneToOneTroupeContext();
-      },
-      action: function() {
-        context.troupe().set('aboutToLeave', true);
-        apiClient.room.delete('/users/' + context.getUserId(), { })
-          .then(function() {
-            appEvents.trigger('navigation', '/home', 'home', '');
-            context.troupe().set('roomMember', false);
-          });
-      }
-    },
-    {
-      command: 'lurk',
-      description: 'Lurk in the room',
-      completion: 'lurk ',
-      regexp: /^\/lurk\s*$/,
-      criteria: function() {
-        return !context.inOneToOneTroupeContext();
-      },
-      action: function() {
-        var c = 0;
-        function done() {
-          if(++c == 2) {
-            appEvents.triggerParent('user_notification', {
-              title: "Lurking",
-              text: "Lurk mode has been enabled for this room"
-            });
+            case 400:
+              errorMessage = e.friendlyMessage || "Ban failed";
+              break;
+
+            case 404:
+              errorMessage = 'That person does not exist (on Gitter that is)';
+              break;
+            default:
+              errorMessage = 'Ban failed';
           }
-        }
 
-        apiClient.userRoom.put('/settings/notifications', { push: "mention" })
-          .then(done);
-
-        apiClient.userRoom.put('', { lurk: true })
-          .then(done);
-      }
-    },
-    {
-      command: 'me',
-      description: 'Let people know what\'s happening',
-      completion: 'me ',
-      regexp: /^\/me/
-    },
-    {
-      command: 'query @username',
-      description: 'Have a private conversation with @username',
-      completion: 'query @',
-      regexp: /^\/query/,
-      action: function(text) {
-        var userMatch = text.match(/\/query @([\w\-]+)/);
-        if (!userMatch) return;
-        var user = userMatch[1];
-
-        var url = '/' + user;
-        var type = user === context.user().get('username') ? 'home' : 'chat';
-        var title = user;
-
-        appEvents.trigger('navigation', url, type, title);
-      }
-    },
-    {
-      command: 'remove @username',
-      description: 'Remove somebody from the room',
-      criteria: function() {
-        return !context.inOneToOneTroupeContext() && context.isTroupeAdmin();
-      },
-      completion: 'remove @',
-      regexp: /^\/remove/,
-      action: function(text) {
-        var userMatch = text.match(/\/remove @([\w\-]+)/);
-        if (!userMatch) return;
-        var user = userMatch[1];
-        appEvents.trigger('command.room.remove', user);
-      }
-    },
-    {
-      command: 'subst',
-      regexp: /^s\/([^\/]+)\/([^\/]*)\/i?(g?)\s*$/,
-      action: function(text) {
-        var re = this.regexp.exec(text);
-        var search = re[1];
-        var replace = re[2];
-        var global = !!re[3];
-        appEvents.trigger('chatCollectionView:substLastChat', context.getUserId(), search, replace, global);
-      }
-    },
-    {
-      command: 'collapse',
-      description: 'Collapse chat messages with embedded media',
-      completion: 'collapse ',
-      regexp: /^\/collapse\s*$/,
-      action: function() {
-        appEvents.trigger('command.collapse.chat');
-      }
-    },
-    {
-      command: 'expand',
-      description: 'Expand chat messages with embedded media',
-      completion: 'expand ',
-      regexp: /^\/expand\s*$/,
-      action: function() {
-        appEvents.trigger('command.expand.chat');
-      }
-    },
-    {
-      command: 'topic foo',
-      description: 'Set room topic to foo',
-      criteria: function() {
-        return !context.inOneToOneTroupeContext() && context.isTroupeAdmin();
-      },
-      completion: 'topic ',
-      regexp: /^\/topic/,
-      action: function(text) {
-        var topicMatch = text.match(/^\/topic (.+)/);
-        if (topicMatch) {
-          var topic = topicMatch[1];
-
-          context.troupe().set('topic', topic);
-
-          apiClient.room.put('', { topic: topic });
-        }
-      }
-    },
-    {
-      command: 'unban @username',
-      description: 'Unban somebody from the room',
-      criteria: function() {
-        var isOrgRoom = false;
-        if (context.troupe().get("githubType") == "ORG") isOrgRoom = true;
-        return !context.inOneToOneTroupeContext() && context.isTroupeAdmin() && !isOrgRoom;
-      },
-      completion: 'unban @',
-      regexp: /^\/unban/,
-      action: function(text) {
-        var userMatch = text.match(/\/unban @([\w\-]+)/);
-        if (!userMatch) return;
-        var user = userMatch[1];
-
-        apiClient.room.delete('/bans/' + user, { })
-          .catch(function(e) {
-            var errorMessage;
-            switch(e.status) {
-              case 403:
-                errorMessage = 'You do not have permission to unban people.';
-                break;
-
-              case 400:
-                errorMessage = e.friendlyMessage || "Unban failed";
-                break;
-
-              case 404:
-                errorMessage = 'That person is not on the banned list.';
-                break;
-              default:
-                errorMessage = 'Unban failed';
-            }
-
-            appEvents.triggerParent('user_notification', {
-              title: 'Could not unban user',
-              text: errorMessage,
-              className: 'notification-error'
-            });
+          appEvents.triggerParent('user_notification', {
+            title: 'Could not ban user',
+            text: errorMessage,
+            className: 'notification-error'
           });
+        });
+    }
+  },
+  {
+    command: 'channel',
+    description: 'Create/join a channel',
+    completion: 'channel ',
+    regexp: /^\/channel/,
+    action: function(text) {
+      var channelMatch = text.match(/^\s*\/channel(?:\s+(\w+))?/);
+      var channel = channelMatch[1];
 
+      if(channel) {
+        appEvents.trigger('route', 'createcustomroom/' + channel);
+      } else {
+        appEvents.trigger('route', 'createcustomroom');
       }
     }
-  ];
+  },
+  {
+    command: 'fav',
+    description: 'Toggle the room as a favourite',
+    completion: 'fav ',
+    regexp: /^\/fav\s*$/,
+    action: function() {
+      var isFavourite = !context.troupe().get('favourite');
 
-  return {
-
-    size: commandsList.length,
-
-    getSuggestions: function(term) {
-      return commandsList.filter(function(cmd) {
-        var elligible = (!cmd.criteria || cmd.criteria()) && cmd.completion;
-        return elligible && cmd.command.indexOf(term) === 0;
-      });
-    },
-
-    findMatch: function(text) {
-      return _.find(commandsList, function(cmd) {
-        return text.match(cmd.regexp);
-      });
+      apiClient.userRoom.put('', { favourite: isFavourite });
     }
+  },
+  {
+    command: 'leave',
+    description: 'Leave the room',
+    completion: 'leave ',
+    regexp: /^\/(leave|part)\s*$/,
+    criteria: function() {
+      return !context.inOneToOneTroupeContext();
+    },
+    action: function() {
+      context.troupe().set('aboutToLeave', true);
+      apiClient.room.delete('/users/' + context.getUserId(), { })
+        .then(function() {
+          appEvents.trigger('navigation', '/home', 'home', '');
+          context.troupe().set('roomMember', false);
+        });
+    }
+  },
+  {
+    command: 'me',
+    description: 'Let people know what\'s happening',
+    completion: 'me ',
+    regexp: /^\/me/
+  },
+  {
+    command: 'query @username',
+    description: 'Have a private conversation with @username',
+    completion: 'query @',
+    regexp: /^\/query/,
+    action: function(text) {
+      var userMatch = text.match(/\/query @([\w\-]+)/);
+      if (!userMatch) return;
+      var user = userMatch[1];
 
-  };
+      var url = '/' + user;
+      var type = user === context.user().get('username') ? 'home' : 'chat';
+      var title = user;
 
+      appEvents.trigger('navigation', url, type, title);
+    }
+  },
+  {
+    command: 'remove @username',
+    description: 'Remove somebody from the room',
+    criteria: function() {
+      return !context.inOneToOneTroupeContext() && context.isTroupeAdmin();
+    },
+    completion: 'remove @',
+    regexp: /^\/remove/,
+    action: function(text) {
+      var userMatch = text.match(/\/remove @([\w\-]+)/);
+      if (!userMatch) return;
+      var user = userMatch[1];
+      appEvents.trigger('command.room.remove', user);
+    }
+  },
+  {
+    command: 'subst',
+    regexp: /^s\/([^\/]+)\/([^\/]*)\/i?(g?)\s*$/,
+    action: function(text) {
+      var re = this.regexp.exec(text);
+      var search = re[1];
+      var replace = re[2];
+      var global = !!re[3];
+      appEvents.trigger('chatCollectionView:substLastChat', context.getUserId(), search, replace, global);
+    }
+  },
+  {
+    command: 'collapse',
+    description: 'Collapse chat messages with embedded media',
+    completion: 'collapse ',
+    regexp: /^\/collapse\s*$/,
+    action: function() {
+      appEvents.trigger('command.collapse.chat');
+    }
+  },
+  {
+    command: 'expand',
+    description: 'Expand chat messages with embedded media',
+    completion: 'expand ',
+    regexp: /^\/expand\s*$/,
+    action: function() {
+      appEvents.trigger('command.expand.chat');
+    }
+  },
+  {
+    command: 'topic foo',
+    description: 'Set room topic to foo',
+    criteria: function() {
+      return !context.inOneToOneTroupeContext() && context.isTroupeAdmin();
+    },
+    completion: 'topic ',
+    regexp: /^\/topic/,
+    action: function(text) {
+      var topicMatch = text.match(/^\/topic (.+)/);
+      if (topicMatch) {
+        var topic = topicMatch[1];
 
-})();
+        context.troupe().set('topic', topic);
+
+        apiClient.room.put('', { topic: topic });
+      }
+    }
+  },
+  {
+    command: 'unban @username',
+    description: 'Unban somebody from the room',
+    criteria: function() {
+      var isOrgRoom = false;
+      if (context.troupe().get("githubType") == "ORG") isOrgRoom = true;
+      return !context.inOneToOneTroupeContext() && context.isTroupeAdmin() && !isOrgRoom;
+    },
+    completion: 'unban @',
+    regexp: /^\/unban/,
+    action: function(text) {
+      var userMatch = text.match(/\/unban @([\w\-]+)/);
+      if (!userMatch) return;
+      var user = userMatch[1];
+
+      apiClient.room.delete('/bans/' + user, { })
+        .catch(function(e) {
+          var errorMessage;
+          switch(e.status) {
+            case 403:
+              errorMessage = 'You do not have permission to unban people.';
+              break;
+
+            case 400:
+              errorMessage = e.friendlyMessage || "Unban failed";
+              break;
+
+            case 404:
+              errorMessage = 'That person is not on the banned list.';
+              break;
+            default:
+              errorMessage = 'Unban failed';
+          }
+
+          appEvents.triggerParent('user_notification', {
+            title: 'Could not unban user',
+            text: errorMessage,
+            className: 'notification-error'
+          });
+        });
+
+    }
+  },
+  {
+    command: 'notify-all',
+    description: 'Get notified on all messages',
+    completion: 'notify-all',
+    regexp: /^\/notify-all\s*$/,
+    criteria: function() {
+      return !context.inOneToOneTroupeContext();
+    },
+    action: function() {
+      updateNotifications('all');
+    }
+  },
+  {
+    command: 'notify-announcements',
+    description: 'Get notified on mentions and group messages',
+    completion: 'notify-announcements',
+    regexp: /^\/notify-announcements\s*$/,
+    criteria: function() {
+      return !context.inOneToOneTroupeContext();
+    },
+    action: function() {
+      updateNotifications('mention');
+    }
+  },
+  {
+    command: 'notify-none',
+    description: 'Mute all notifications for this room',
+    completion: 'notify-none',
+    regexp: /^\/notify-none\s*$/,
+    criteria: function() {
+      return !context.inOneToOneTroupeContext();
+    },
+    action: function() {
+      updateNotifications('mute');
+    }
+  },
+
+];
+
+module.exports = {
+
+  size: commandsList.length,
+
+  getSuggestions: function(term) {
+    return commandsList.filter(function(cmd) {
+      var elligible = (!cmd.criteria || cmd.criteria()) && cmd.completion;
+      return elligible && cmd.command.indexOf(term) === 0;
+    });
+  },
+
+  findMatch: function(text) {
+    return _.find(commandsList, function(cmd) {
+      return text.match(cmd.regexp);
+    });
+  }
+
+};
