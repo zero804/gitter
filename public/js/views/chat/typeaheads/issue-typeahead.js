zero804/gitter

commit 7e40c929dd94c41d179bf89a9438bb1435a65ec3

Remove $.Deferred from the client

public/js/views/chat/typeaheads/issue-typeahead.js
@@ -18,12 +18,10 @@ module.exports = function() {
       if(issueNumber) query.issueNumber = issueNumber;
 
       apiClient.room.get('/issues', query)
-        .then(function(resp) {
-          callback(resp);
+        .catch(function() {
+          return [];
         })
-        .fail(function() {
-          callback([]);
-        });
+        .asCallback(callback);
     },
     template: function(issue) {
       return template({

public/js/views/modals/create-room-view.js
@@ -155,20 +155,15 @@ var View = Marionette.LayoutView.extend({
         self.dialog.hide();
         appEvents.trigger('navigation', data.url , 'chat#add', data.uri);
       })
-      .fail(function(xhr) {
-        var response;
-        try {
-          response = JSON.parse(xhr.responseText);
-        } catch (e) {
-          // Ignore
-        }
+      .catch(function(err) {
 
-        var status = xhr.status;
+
+        var status = err.status;
         var message = 'Unable to create channel.';
 
         switch (status) {
           case 400:
-            if (response && response.illegalName) {
+            if (err.response && err.response.illegalName) {
               message = 'Please choose a channel name consisting of letter and number characters.';
               self.ui.roomNameInput.focus();
             } else {
@@ -295,11 +290,12 @@ var View = Marionette.LayoutView.extend({
     function checkForRepoExistence(repo, cb) {
       apiClient.priv.get('/gh/repos/' + repo)
         .then(function () {
-          cb(true);
+          return true;
         })
-        .fail(function () {
-          cb(false);
-        });
+        .catch(function () {
+          return false;
+        })
+        .asCallback(cb);
     }
 
     function applyShowHides() {

public/js/collections/sync-mixin.js
@@ -30,7 +30,7 @@ module.exports = {
     }
 
     if(options.error) {
-      promise = promise.fail(options.error); // Backbone will trigger the 'error' event
+      promise = promise.catch(options.error); // Backbone will trigger the 'error' event
     }
 
     model.trigger('request', model, null, options);

public/js/components/apiClient.js
@@ -10,9 +10,6 @@
  * The basic form of requests is:
  * apiClient.operation(url, data, options) -> $.Deferrer
  *
- * Note that altough the client returns a deferred, it will
- * in future return a promise.
- *
  * apiClient.get('/v1/eyeballs', data, options)
  * apiClient.post('/v1/eyeballs', data, options)
  * apiClient.put('/v1/eyeballs', data, options)
@@ -43,14 +40,15 @@
  *   .then(function(response) {
  *     window.alert('I did a post.');
  *   })
- *   .fail(function(xhr) {
- *     window.alert('I am a failure: ' + xhr.status);
+ *   .catch(function(err) {
+ *     window.alert('I am a failure: ' + err.status);
  *   })
  */
 var $ = require('jquery');
 var context = require('utils/context');
 var appEvents = require('utils/appevents');
 var debug = require('debug-proxy')('app:api-client');
+var Promise = require('bluebird');
 
 module.exports = (function() {
 
@@ -114,17 +112,6 @@ module.exports = (function() {
     return baseUrlFunction() + url;
   }
 
-  function accessTokenDeferred() {
-    var deferred = $.Deferred();
-
-    context.getAccessToken(function(accessToken) {
-      deferred.resolve(accessToken);
-    });
-
-    return deferred;
-  }
-
-  // TODO return a proper promise instead of a $.Deferred
   function operation(fullUrlFunction, baseUrlFunction, method, defaultOptions, url, data, options) {
     options = defaults(options, defaultOptions);
 
@@ -141,34 +128,74 @@ module.exports = (function() {
       dataSerialized = data;
     }
 
-    return accessTokenDeferred()
+    return context.getAccessToken()
       .then(function(accessToken) {
-        var fullUrl = fullUrlFunction(baseUrlFunction, url);
-        debug('%s: %s', method, fullUrl);
-        var deferred = $.ajax({
-          url: fullUrl,
-          contentType: options.contentType,
-          dataType: options.dataType,
-          type: method,
-          global: options.global,
-          data: dataSerialized,
-          context: options.context, // NB: deprecated: cant use with real promises
-          timeout: options.timeout,
-          async: options.async,
-          headers: {
-            'x-access-token': accessToken
+
+        var promise = new Promise(function(resolve, reject) {
+          var headers = {};
+
+          if (accessToken) {
+            headers['x-access-token'] = accessToken;
           }
+
+          // TODO: drop jquery `ajax`
+          var fullUrl = fullUrlFunction(baseUrlFunction, url);
+
+          function makeError(jqXhr, textStatus, errorThrown) {
+            var json = jqXhr.responseJSON;
+            var friendlyMessage = json && (json.message || json.error);
+
+            var e = new Error(friendlyMessage || errorThrown || textStatus || "AJAX error");
+            e.url = fullUrl;
+            e.friendlyMessage = friendlyMessage;
+            e.response = json;
+            e.method = method;
+            e.status = jqXhr.status;
+            e.statusText = jqXhr.statusText;
+            return e;
+          }
+
+          debug('%s: %s', method, fullUrl);
+          $.ajax({
+            url: fullUrl,
+            contentType: options.contentType,
+            dataType: options.dataType,
+            type: method,
+            global: options.global,
+            data: dataSerialized,
+            timeout: options.timeout,
+            async: options.async,
+            headers: headers,
+            success: function(data, textStatus, xhr) {
+              var status = xhr.status;
+              if (status >= 400 && status !== 1223) {
+
+                var e = new Error(textStatus);
+                e.status = status;
+                return reject(makeError(xhr, textStatus, "HTTP Status " + status));
+              }
+
+              resolve(data);
+            },
+            error: function(xhr, textStatus, errorThrown) {
+              return reject(makeError(xhr, textStatus, errorThrown));
+            }
+          });
         });
 
-        /* Don't add this to the 'promise' chain */
-        deferred.fail(function(xhr) {
-          if(options.global) {
+        if(options.global) {
+          promise.catch(function(err) {
             /* Asyncronously notify */
-            handleApiError(xhr, method, fullUrl);
-          }
-        });
+            handleApiError(err.status, err.statusText, err.method, err.url);
+          });
+        }
 
-        return deferred;
+        if (options.context) {
+          // TODO: remove this option
+          promise.bind(context);
+        }
+
+        return promise;
       });
 
   }
@@ -190,12 +217,10 @@ module.exports = (function() {
     }
   }
 
-  function handleApiError(xhr, method, url) {
-    var status = xhr.status;
-
+  function handleApiError(status, statusText, method, url) {
     var route = findRouteForUrl(url);
 
-    if(xhr.statusText == "error" && status === 404) {
+    if(statusText == "error" && status === 404) {
       /* Unreachable server */
       appEvents.trigger('bugreport', 'ajaxError: unreachable: '+ method + ' ' + (route || url), {
         tags: {

public/js/components/cordova-navigate.js
@@ -13,13 +13,11 @@ module.exports = (function() {
   }
 
   function getIdForUri(uri, cb) {
-    apiClient.post('/v1/rooms', { uri: uri })
+    return apiClient.post('/v1/rooms', { uri: uri })
       .then(function(room) {
-        cb(null, room.id);
+        return room.id;
       })
-      .fail(function() {
-        return cb(new Error('API call failed'));
-      });
+      .asCallback(cb);
   }
 
   /**
@@ -91,4 +89,3 @@ module.exports = (function() {
 
 
 })();
-

public/js/components/eyeballs.js
@@ -33,8 +33,8 @@ module.exports = (function() {
         async: !synchronous,
         global: false
       })
-      .fail(function(xhr) {
-        if(xhr.status !== 400) {
+      .catch(function(err) {
+        if(err.status !== 400) {
           debug('An error occurred while communicating eyeballs');
         } else {
           // The connection is gone...

public/js/components/ping.js
@@ -10,7 +10,7 @@ window.setInterval(function() {
   apiClient.web.get('/api_web/private/ping', undefined, {
       global: false
     })
-    .fail(function(err) {
+    .catch(function(err) {
       debug('An error occurred pinging the server: %j', err);
     });
 }, PING_POLL);

public/js/components/realtime.js
@@ -22,19 +22,20 @@ appEvents.on('eyeballStateChange', function (state) {
 });
 
 function authProvider(callback) {
-  context.getAccessToken(function (accessToken) {
-    var mobile = isMobile();
+  context.getAccessToken()
+    .then(function(accessToken) {
+      var mobile = isMobile();
 
-    return callback({
-      token: accessToken,
-      version: context.env('version'),
-      troupeId: context.getTroupeId(),
-      connType: mobile ? 'mobile' : 'online',
-      client: mobile ? 'mobweb' : 'web',
-      eyeballs: eyeballState ? 1 : 0
+      return callback({
+        token: accessToken,
+        version: context.env('version'),
+        troupeId: context.getTroupeId(),
+        connType: mobile ? 'mobile' : 'online',
+        client: mobile ? 'mobweb' : 'web',
+        eyeballs: eyeballState ? 1 : 0
+      });
+
     });
-
-  });
 }
 
 var updateTimers;

commit 1289aaec4f1761009351ae8805381bb473c2e817

Fix bluebird `Promise.asCallback` usage (issue typeahead, channel creation with repo name)
Introduced in: https://github.com/troupe/gitter-webapp/commit/7e40c929dd94c41d179bf89a9438bb1435a65ec3
was alread fixed in user typeahead: https://github.com/troupe/gitter-webapp/commit/fa0222969023b6c13d3860086ff8f67f77237d92

public/js/views/chat/typeaheads/issue-typeahead.js
@@ -21,7 +21,9 @@ module.exports = function() {
         .catch(function() {
           return [];
         })
-        .asCallback(callback);
+        .then(function(data) {
+          callback(data);
+        });
     },
     template: function(issue) {
       return template({

public/js/views/modals/create-room-view.js
@@ -295,7 +295,9 @@ var View = Marionette.LayoutView.extend({
         .catch(function () {
           return false;
         })
-        .asCallback(cb);
+        .then(function(result) {
+          cb(result);
+        });
     }
 
     function applyShowHides() {