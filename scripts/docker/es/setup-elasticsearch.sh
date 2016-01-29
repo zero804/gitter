7ce1079a668f20694c3eddd5c32ed133a90a1eec
Andrew Newdigate <andrew@troupe.co>
2016-01-29 16:54:43 +0000

    Docker sanity

--- scripts/docker/es/setup-elasticsearch.sh
+++ scripts/docker/es/setup-elasticsearch.sh
@@ -7,25 +7,19 @@ sleep 30
 set -e
 set -x
 
-
-
-MONGODB1=`ping -c 1 mongo1 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
-MONGODB2=`ping -c 1 mongo2 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
-ELASTICSEARCH=`ping -c 1 elasticsearch | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
-
 export INDEX_VERSION=01
 export INDEX_NAME=gitter${INDEX_VERSION}
 export USER_RIVER=gitterUserRiver${INDEX_VERSION}
 export CHAT_RIVER=gitterChatRiver${INDEX_VERSION}
 export ROOM_RIVER=gitterRoomRiver${INDEX_VERSION}
-export ES_URL=http://$ELASTICSEARCH:9200
+export ES_URL=http://elasticsearch:9200
 
-while ! curl --fail "${ES_URL}"; do sleep 1; done
+while ! curl -q --fail "${ES_URL}"; do sleep 1; done
 
-export MONGO_HOST_1=$MONGODB1
+export MONGO_HOST_1=mongo1
 export MONGO_PORT_1=27017
 
-export MONGO_HOST_2=$MONGODB2
+export MONGO_HOST_2=mongo2
 export MONGO_PORT_2=27018
 
 $SCRIPT_DIR/01-create-index-with-mapping
