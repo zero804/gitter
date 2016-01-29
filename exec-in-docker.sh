4344b2720d2d8b51cb8d4bf0761656378febecc0
Andrew Newdigate <andrew@troupe.co>
2016-01-29 16:54:43 +0000

    Docker sanity

---
+++ exec-in-docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -eux
+
+function join { local IFS="$1"; shift; echo "$*"; }
+
+WORKSPACE=${WORKSPACE-$(pwd)}
+DEBUG=${DEBUG-}
+export WORKSPACE
+export DEBUG
+
+echo $WORKSPACE
+
+ISOLATED_UNIQ_ID=${BUILD_NUMBER:-$(date +"%Y-%m-%dT%H:%M:%S")}
+
+function finish {
+  docker-compose -p "webapp-${ISOLATED_UNIQ_ID}" -f docker-compose.internal.yml stop
+  docker-compose -p "webapp-${ISOLATED_UNIQ_ID}" -f docker-compose.internal.yml rm -f
+}
+trap finish EXIT
+
+docker-compose -p "webapp-${ISOLATED_UNIQ_ID}" -f docker-compose.internal.yml run -d mongosetup
+docker-compose -p "webapp-${ISOLATED_UNIQ_ID}" -f docker-compose.internal.yml run --rm --entrypoint "$(join ' ' "$@")" internal-min
