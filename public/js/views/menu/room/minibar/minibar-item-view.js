5a65e003bfd6942cdb9b38265dfae2a2d80bf756
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-02-03 16:13:51 +0000

    break out minibar views and add people notifications

--- public/js/views/menu/room/minibar/minibar-item-view.js
+++ public/js/views/menu/room/minibar/minibar-item-view.js
@@ -1,141 +1,58 @@
 'use strict';
 
-var _          = require('underscore');
-var Backbone   = require('backbone');
-var Marionette = require('backbone.marionette');
-var RAF        = require('utils/raf');
+var _             = require('underscore');
+var Marionette    = require('backbone.marionette');
+var itemTemplate  = require('./minibar-item-view.hbs');
+var getRoomAvatar = require('utils/get-room-avatar');
 
-var MENU_REACTION_DEBOUNCE = 200;
-
-module.exports = Marionette.ItemView.extend({
-
+module.exports =  Marionette.ItemView.extend({
+  tagName:      'li',
+  template:     itemTemplate,
+  modelEvents: {
+    'change:unreadItems change:mentions': 'render',
+    'change:active': 'onActiveStateUpdate',
+  },
   events: {
     'click': 'onItemClicked',
   },
+  attributes: function() {
+    var type = this.model.get('type');
 
-  modelEvents: {
-    'change:active': 'onActiveStateChange',
+    //account for initial render
+    var className = 'room-menu-options__item--' + type;
+    if (this.model.get('active')) { className = className += ' active'; }
+
+    return {
+      'class':             className,
+      'data-state-change': type,
+    };
   },
 
-  initialize: function(attrs) {
-
-    this.debounceTime = MENU_REACTION_DEBOUNCE;
-
-    this.model = (this.model || new Backbone.Model());
-    this.model.set({
-      type:          this.$el.data('stateChange'),
-      orgName:       this.$el.data('org-name'),
-      isCloseButton: !!this.$el.find('#menu-close-button').length,
+  serializeData: function() {
+    var data = this.model.toJSON();
+    var activity = (data.mentions || data.unreadItems) ? false : data.activity;
+    return _.extend({}, data, {
+      isHome:      (data.type === 'all'),
+      isSearch:    (data.type === 'search'),
+      isFavourite: (data.type === 'favourite'),
+      isPeople:    (data.type === 'people'),
+      isOrg:       (data.type === 'org'),
+      avatarUrl:   getRoomAvatar(data.name),
+      activity:    activity,
     });
-
-    //Events
-    if (!attrs || !attrs.bus) {
-      throw new Error('A valid event bus must be passed to a new instance of MiniBarItemView');
-    }
-
-    this.bus = attrs.bus;
-    this.listenTo(this.bus, 'ui:swiperight', this.onSwipeRight, this);
-
-    //Drag & Drop Controller
-    if (!attrs || !attrs.dndCtrl) {
-      throw new Error('A valid drag & drop controller must be passed to a new instance of MiniBarItemView');
-    }
-
-    this.dndCtrl   = attrs.dndCtrl;
-
-    //Menu Model
-    if (!attrs || !attrs.menuModel) {
-      throw new Error('A valid roomMenuModel must be passed to a new instance of MiniBarItemView');
-    }
-
-    this.menuModel = attrs.menuModel;
-    if (this.model.get('isCloseButton')) {
-      this.listenTo(this.menuModel, 'change:roomMenuIsPinned change:panelOpenState', this.onPanelOpen, this);
-    }
-
-    //TODO This component should be extended here instead of the check JP 12/1/16
-    if (this.model.get('type') === 'favourite') {
-      this.dndCtrl.pushContainer(this.el);
-      this.listenTo(this.dndCtrl, 'dnd:start-drag', this.onDragStart, this);
-      this.listenTo(this.dndCtrl, 'dnd:end-drag', this.onDragStop, this);
-      this.listenTo(this.dndCtrl, 'room-menu:add-favourite', this.onFavourite, this);
-    }
   },
 
-  //TODO TEST THIS
-  onPanelOpen: _.debounce(function(model) {
-    var pinState  = model.get('roomMenuIsPinned');
-    var openState = model.get('panelOpenState');
-
-    //if the menu is open && pinned
-    if (!!openState && !!pinState) {
-      this.$el.addClass('left');
-      this.$el.removeClass('right');
-    }
-
-    if (!!openState && !pinState) {
-      this.$el.addClass('right');
-      this.$el.removeClass('left');
-    }
-
-    if (!openState) {
-      this.$el.removeClass('left');
-      this.$el.removeClass('right');
-    }
-  }, 200),
-
-  onDragStart: function() {
-    this.$el.addClass('drag-start');
+  onItemClicked: function() {
+    this.trigger('minibar-item:clicked', this.model);
   },
 
-  onDragStop: function() {
-    this.$el.removeClass('drag-start');
+  onActiveStateUpdate: function(model, val) { //jshint unused: true
+    this.$el.toggleClass('active', !!val);
   },
 
-  //Test this
-  onFavourite: function() {
-    this.$el.addClass('dropped');
-    setTimeout(function() {
-      this.$el.removeClass('dropped');
-
-      //Dragula places dropped items into the drop container
-      //This needs to be fixed upstream
-      //util that date just remove the dropped container manually
-      //https://github.com/bevacqua/dragula/issues/188
-      this.$el.find('.room-item').remove();
-    }.bind(this), 200);
+  onRender: function() {
+    this.$el.toggleClass('active', !!this.model.get('active'));
   },
 
-  onSwipeRight: function(e) {
-    if (e.target === this.el) {
-      this._triggerRoomChange();
-    }
-  },
-
-  onItemClicked: function(e) {
-    e.preventDefault();
-    this._triggerRoomChange();
-  },
-
-  _triggerRoomChange: function() {
-    this.trigger('room-item-view:clicked',
-                 this.model.get('type'),
-                 this.model.get('orgName'),
-                 this.model.get('isCloseButton')
-                );
-  },
-
-  onActiveStateChange: function(model, val) {/*jshint unused:true */
-    if (this.model.get('isCloseButton')) return;
-    RAF(function() {
-      this.$el.toggleClass('active', val);
-    }.bind(this));
-  },
-
-
-  onDestroy: function (){
-    this.stopListening(this.dndCtrl);
-    this.stopListening(this.menuModel);
-    this.stopListening(this.bus);
-  },
 });
+
--- public/js/views/menu/room/minibar/minibar-view.js
+++ public/js/views/menu/room/minibar/minibar-view.js
@@ -2,135 +2,12 @@
 
 var _             = require('underscore');
 var Marionette    = require('backbone.marionette');
-var itemTemplate  = require('./minibar-item-view.hbs');
-var closeTemplate = require('./minibar-close-item-view.hbs');
+var ItemView      = require('./minibar-item-view');
+var CloseItemView = require('./minibar-close-item-view');
+var FavouriteView = require('./minibar-favourite-item-view');
+var PeopleView    = require('./minibar-people-item-view.js');
 var RAF           = require('utils/raf');
-var getRoomAvatar = require('utils/get-room-avatar');
 
-var ItemView = Marionette.ItemView.extend({
-  tagName:      'li',
-  template:     itemTemplate,
-  modelEvents: {
-    'change:unreadItems change:mentions': 'render',
-    'change:active': 'onActiveStateUpdate',
-  },
-  events: {
-    'click': 'onItemClicked',
-  },
-  attributes: function() {
-    var type = this.model.get('type');
-
-    //account for initial render
-    var className = 'room-menu-options__item--' + type;
-    if (this.model.get('active')) { className = className += ' active'; }
-
-    return {
-      'class':             className,
-      'data-state-change': type,
-    };
-  },
-
-  serializeData: function() {
-    var data = this.model.toJSON();
-    var activity = (data.mentions || data.unreadItems) ? false : data.activity;
-    return _.extend({}, data, {
-      isHome:      (data.type === 'all'),
-      isSearch:    (data.type === 'search'),
-      isFavourite: (data.type === 'favourite'),
-      isPeople:    (data.type === 'people'),
-      isOrg:       (data.type === 'org'),
-      avatarUrl:   getRoomAvatar(data.name),
-      activity:    activity,
-    });
-  },
-
-  onItemClicked: function() {
-    this.trigger('minibar-item:clicked', this.model);
-  },
-
-  onActiveStateUpdate: function(model, val) { //jshint unused: true
-    this.$el.toggleClass('active', !!val);
-  },
-
-  onRender: function() {
-    this.$el.toggleClass('active', !!this.model.get('active'));
-  },
-
-});
-
-var CloseItemView = ItemView.extend({
-  template: closeTemplate,
-
-  initialize: function(attrs) {
-    this.roomModel = attrs.roomModel;
-    this.listenTo(this.roomModel, 'change:panelOpenState change:roomMenuIsPinned', this.onPanelOpenChange, this);
-  },
-
-  onItemClicked: function() {
-    this.trigger('minibar-item:close');
-  },
-
-  onPanelOpenChange: _.debounce(function() { //jshint unused: true
-    var pinState  = this.roomModel.get('roomMenuIsPinned');
-    var openState = this.roomModel.get('panelOpenState');
-
-    //if the menu is open && pinned
-    if (!!openState && !!pinState) {
-      this.$el.addClass('left');
-      this.$el.removeClass('right');
-    }
-
-    if (!!openState && !pinState) {
-      this.$el.addClass('right');
-      this.$el.removeClass('left');
-    }
-
-    if (!openState) {
-      this.$el.removeClass('left');
-      this.$el.removeClass('right');
-    }
-  }, 200),
-
-  onDestroy: function() {
-    this.stopListening(this.roomModel);
-  },
-
-});
-
-var FavouriteView = ItemView.extend({
-  initialize: function(attrs) {
-    this.dndCtrl = attrs.dndCtrl;
-    this.dndCtrl.pushContainer(this.el);
-    this.listenTo(this.dndCtrl, 'dnd:start-drag', this.onDragStart, this);
-    this.listenTo(this.dndCtrl, 'dnd:end-drag', this.onDragStop, this);
-    this.listenTo(this.dndCtrl, 'room-menu:add-favourite', this.onFavourite, this);
-  },
-
-  onDragStart: function() {
-    this.$el.addClass('active');
-  },
-
-  onDragStop: function() {
-    this.$el.removeClass('active');
-  },
-
-  onFavourite: function() {
-    this.$el.addClass('dropped');
-    setTimeout(function() {
-      this.$el.removeClass('dropped');
-
-      //Dragula places dropped items into the drop container
-      //This needs to be fixed upstream
-      //util that date just remove the dropped container manually
-      //https://github.com/bevacqua/dragula/issues/188
-      this.$el.find('.room-item').remove();
-    }.bind(this), 200);
-  },
-
-  onDestroy: function() {
-    this.stopListening(this.dndCtrl);
-  },
-});
 
 //TODO TEST ALL THE THINGS JP 2/2/16
 module.exports = Marionette.CollectionView.extend({
@@ -167,6 +44,9 @@ module.exports = Marionette.CollectionView.extend({
       case 'favourite':
         viewOptions = _.extend(viewOptions, { dndCtrl: this.dndCtrl });
         return new FavouriteView(viewOptions);
+      case 'people':
+        viewOptions = _.extend(viewOptions, { roomCollection: this.roomCollection });
+        return new PeopleView(viewOptions);
       default:
         return new ViewClass(viewOptions);
     }
@@ -174,10 +54,13 @@ module.exports = Marionette.CollectionView.extend({
   },
 
   initialize: function(attrs) {
-    this.shouldRender = false;
-    this.bus          = attrs.bus;
-    this.dndCtrl      = attrs.dndCtrl;
-    this.model        = attrs.model;
+
+    this.shouldRender   = false;
+    this.bus            = attrs.bus;
+    this.dndCtrl        = attrs.dndCtrl;
+    this.model          = attrs.model;
+    this.roomCollection = attrs.roomCollection;
+
     this.listenTo(this.collection, 'snapshot', this.onCollectionSnapshot, this);
     this.listenTo(this.model, 'change:state change:selectedOrgName', this.onMenuStateUpdate, this);
   },
@@ -248,172 +131,3 @@ module.exports = Marionette.CollectionView.extend({
   },
 
 });
-
-/*
-var Backbone            = require('backbone');
-var RoomMenuItemView    = require('./minibar-item-view');
-var cocktail            = require('cocktail');
-var KeyboardEventsMixin = require('views/keyboard-events-mixin');
-
-require('nanoscroller');
-
-
-var MiniBarView = Marionette.ItemView.extend({
-
-  //TODO JP 28/1/16
-  //Move keyboard events out into keyboard ctrl singleton
-  keyboardEvents: {
-     'room.1 room.2 room.3 room.4 room.5 room.6 room.7 room.8 room.9': 'onKeypressFocus',
-  },
-
-  initialize: function(attrs) {
-
-    if (!attrs || !attrs.bus) {
-      throw new Error('A valid event bus must be passed to a new instance of the MiniBarView');
-    }
-
-    this.bus = attrs.bus;
-
-    if (!attrs || !attrs.dndCtrl)  {
-      throw new Error('A valid drag & drop controller must be passed to a new instance of the MiniBarView');
-    }
-
-    this.dndCtrl = attrs.dndCtrl;
-
-    this.roomMenuItems = [];
-    this.roomMenuItemModels = new Backbone.Collection();
-
-    //Feels icky to pull this out of the dom
-    //but we have to because it's pre rendered
-    //and not stored in the context
-    //JP 28/1/16
-    var _roomMenuItems = Array.prototype.slice.apply(this.$el.find('[data-state-change]'));
-    _roomMenuItems.forEach(function(el, index) {
-
-      var type = el.dataset.stateChange;
-
-      var model = new Backbone.Model({ active: (index === 0), type: type });
-      this.roomMenuItemModels.add(model);
-
-      var view = new RoomMenuItemView({
-        model:   model,
-        el:      el,
-        bus:     attrs.bus,
-        dndCtrl: this.dndCtrl,
-        menuModel: this.model,
-      });
-      this.roomMenuItems.push(view);
-
-      this.listenTo(view, 'room-item-view:clicked', this.onItemClicked, this);
-
-    }.bind(this));
-
-    this.listenTo(this.model, 'change:panelOpenState', this.onPanelStateChange, this);
-    this.listenTo(this.model, 'change', this.onMenuChange, this);
-
-    this.$el.find('.nano').nanoScroller({
-      iOSNativeScrolling: true,
-      sliderMaxHeight:    200,
-    });
-
-  },
-
-  //TODO this sequencing is not 100% decided upon
-  //when it is the login needs to be tests
-  //JP 12/1/16
-  onItemClicked: function(type, orgName, isCloseButton) {
-    if (isCloseButton) {
-      var newVal = !this.model.get('roomMenuIsPinned');
-      var ANIMATION_TIME = 300;
-
-      //if we are opening the panel
-      if (newVal === true) {
-        if (this.model.get('panelOpenState') === true) {
-          this.model.set({ roomMenuIsPinned: newVal });
-          this.bus.trigger('room-menu:pin', newVal);
-        } else {
-          setTimeout(function() {
-            this.model.set({ roomMenuIsPinned: newVal });
-            this.bus.trigger('room-menu:pin', newVal);
-          }.bind(this), ANIMATION_TIME);
-        }
-
-        this.model.set({ panelOpenState: newVal });
-      } else {
-        this.model.set({ roomMenuIsPinned: newVal });
-        this.bus.trigger('room-menu:pin', newVal);
-        setTimeout(function() {
-          this.model.set({ panelOpenState: newVal });
-        }.bind(this), ANIMATION_TIME);
-      }
-    }
-
-    //If the pin button is clicked retain the current state
-    if (type === 'pin') { type = this.model.get('state') }
-
-    if (!orgName) { orgName = this.model.get('selectedOrgName') }
-
-    this.model.set({
-      panelOpenState:       true,
-      state:                type,
-      profileMenuOpenState: false,
-      selectedOrgName:      orgName,
-    });
-  },
-
-  onPanelStateChange: function(model, state) { //jshint unused: true
-    this.$el.find('#menu-close-button').toggleClass('active', state);
-    if (!state) {
-      var currentActiveModel = this._getCurrentlyActiveChildModel();
-      if (currentActiveModel) currentActiveModel.set('active', false);
-    }
-  },
-
-  onMenuChange: function() {
-
-    var orgName = this.model.get('selectedOrgName');
-    var type    = this.model.get('state');
-
-    if (!this.model.get('panelOpenState')) { return }
-
-    //de-activate the old active item
-    var currentActiveModel = this._getCurrentlyActiveChildModel();
-    if (!!currentActiveModel) currentActiveModel.set('active', false);
-
-    //activate the next item
-    var query = (type === 'org') ? { orgName: orgName } : { type: type };
-    var nextActiveModel = this.roomMenuItemModels.where(query)[0];
-    if (!!nextActiveModel) nextActiveModel.set('active', true);
-
-  },
-
-  //TODO this feals too much like the above function, start extracting the functionality
-  onKeypressFocus: function(e, handler) {//jshint unused: true
-    var index = handler.key.split('+').slice(-1)[0];
-    index     = parseInt(index, 10) - 1;
-    var nextActiveModel = this.roomMenuItemModels.at(index);
-    if (!nextActiveModel) { return }
-
-    this.onItemClicked(nextActiveModel.get('type'), nextActiveModel.get('orgName'), false);
-    this.bus.trigger('room-menu:keyboard:focus');
-    this.bus.trigger('focus.request.out', e);
-  },
-
-  _getCurrentlyActiveChildModel: function() {
-    return this.roomMenuItemModels.where({ active: true })[0];
-  },
-
-  onDestroy: function() {
-    //unbind all child views
-    this.roomMenuItems.forEach(function(v) {
-      this.stopListening(v);
-    }.bind(this));
-    this.stopListening(this.model);
-  },
-
-});
-
-cocktail.mixin(MiniBarView, KeyboardEventsMixin);
-
-module.exports = MiniBarView;
-*/
