12600a9650ff4dd1b3673bb2893631620e91732b
Andrew Newdigate <andrew@troupe.co>
2016-02-04 19:54:35 +0000

    Test cases moved across to bluebird

--- test/integration/services/email-notification-service-test.js
+++ test/integration/services/email-notification-service-test.js
@@ -1,9 +1,9 @@
 "use strict";
 
-var testRequire   = require('../test-require');
-var Q             = require('bluebird-q');
-var assert        = require('assert');
-var i18nFactory   = testRequire('./utils/i18n-factory');
+var testRequire = require('../test-require');
+var Promise     = require('bluebird');
+var assert      = require('assert');
+var i18nFactory = testRequire('./utils/i18n-factory');
 
 var EMAIL_DATA = [
   {
@@ -113,13 +113,13 @@ describe('email-notification-service', function() {
   beforeEach(function() {
     emailNotificationService = testRequire.withProxies("./services/email-notification-service", {
       './email-address-service': function() {
-        return Q.resolve('mike.bartlett@gmail.com');
+        return Promise.resolve('mike.bartlett@gmail.com');
       },
       './user-settings-service': {
         getUserSettings: function(userId, key) {
           assert(userId);
           assert.strictEqual(key, 'lang');
-          return Q.resolve(lang);
+          return Promise.resolve(lang);
         }
       }
     });
