55fee9c7da3b81e09f618cb872141e9d6b6876f8
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-02-09 15:12:11 +0000

    add suggestions to favourite state

--- public/js/models/room-menu-model.js
+++ public/js/models/room-menu-model.js
@@ -138,6 +138,7 @@ module.exports = Backbone.Model.extend({
       case 'favourite':
         this.primaryCollection.switchCollection(this.activeRoomCollection);
         this.secondaryCollection.switchCollection(this._suggestedRoomCollection);
+        this.tertiaryCollection.switchCollection(this._suggestedRoomCollection);
         break;
 
       case 'org':
