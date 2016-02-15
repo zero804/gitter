b8fade511c449d567a98d8850c12ac28a2d06abd
Andrew Newdigate <andrew@troupe.co>
2016-02-15 16:19:59 +0000

    Response to https://github.com/troupe/gitter-webapp/pull/952#discussion_r52915420

--- server/utils/mongo-utils.js
+++ server/utils/mongo-utils.js
@@ -127,6 +127,34 @@ function setIds(array) {
   return array;
 }
 
+/**
+ * Given a set of conjunctions, attempts to return a mongo-efficient form of
+ * the query.
+ *
+ * So imagine you want to query for a set of user rooms, like
+ * userId: X1, roomId: Y1
+ * userId: X2, roomId: Y2
+ * userId: X3, roomId: Y3
+ *
+ * The default query would be
+ * `{ $or: [{ userId: X1, roomId: Y1 }, { userId: X2, roomId: Y2 }, etc ]}`
+ *
+ * Unfortunately, mongo will attempt to handle this conjunction by issuing
+ * `n` parallel queries, which is very inefficient, especially if the
+ * one of the terms in all the queries is common
+ *
+ * This method will attempt to group the terms either by the first term
+ * or the second, if it's possible to do so.
+ *
+ * For example, if all the roomId terms are equal, it is much faster to ask
+ * mongo for `{ roomId: Y1, userId: { $in: { X1, X2, X3} } }`
+ *
+ * A similar transform is possible if all the userIds are equal.
+ *
+ * While it's possible to break the query into more sets, there is a cpu cost
+ * associated with doing this, so only the trivial case is attempted.
+ *
+ */
 function conjunctionIds(terms, termIdentifiers) {
   if (!terms.length) return { $or: terms };
   if (terms.length < 3) return { $or: terms };
@@ -136,27 +164,28 @@ function conjunctionIds(terms, termIdentifiers) {
   var t1Identifier = termIdentifiers[0];
   var t2Identifier = termIdentifiers[1];
 
-  var t1UniqueValue = terms[0][t1Identifier];
-  var t1Unique = true;
-  var t2UniqueValue = terms[0][t2Identifier];
-  var t2Unique = true;
+  var t1CommonValue = terms[0][t1Identifier];
+  var t1Common = true;
+  var t2CommonValue = terms[0][t2Identifier];
+  var t2Common = true;
 
   for (var i = 1; i < terms.length; i++) {
     var t1 = terms[i][t1Identifier];
     var t2 = terms[i][t2Identifier];
 
-    if (t1UniqueValue != t1) t1Unique = false;
-    if (t2UniqueValue != t2) t2Unique = false;
+    if (t1CommonValue != t1) t1Common = false;
+    if (t2CommonValue != t2) t2Common = false;
 
-    if (!t1Unique && !t2Unique) break;
+    if (!t1Common && !t2Common) break;
   }
 
-  // Everything is the same. Duh
-  if (t1Unique && t2Unique) return terms[0];
+  // Everything is the same. Just return the first term
+  if (t1Common && t2Common) return terms[0];
 
-  if (t1Unique) {
-    var t1UniqueQuery = {};
-    t1UniqueQuery[t1Identifier] = terms[0][t1Identifier];
+  // If the first term is common for all conjunction terms...
+  if (t1Common) {
+    var t1CommonQuery = {};
+    t1CommonQuery[t1Identifier] = t1CommonValue;
 
     var t2MultiQuery = {};
     t2MultiQuery[t2Identifier] = {
@@ -165,12 +194,13 @@ function conjunctionIds(terms, termIdentifiers) {
             })
     };
 
-    return { $and: [ t1UniqueQuery, t2MultiQuery] };
+    return { $and: [ t1CommonQuery, t2MultiQuery] };
   }
 
-  if (t2Unique) {
-    var t2UniqueQuery = {};
-    t2UniqueQuery[t2Identifier] = terms[0][t2Identifier];
+  // If the second term is common for all conjunction terms...
+  if (t2Common) {
+    var t2CommonQuery = {};
+    t2CommonQuery[t2Identifier] = t2CommonValue;
 
     var t1MultiQuery = {};
     t1MultiQuery[t1Identifier] = {
@@ -179,7 +209,7 @@ function conjunctionIds(terms, termIdentifiers) {
             })
     };
 
-    return { $and: [ t2UniqueQuery, t1MultiQuery] };
+    return { $and: [ t2CommonQuery, t1MultiQuery] };
   }
 
   return { $or: terms };
