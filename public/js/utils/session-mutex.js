df1681a0dd1f3cd28d4444e975e27737c9bc1c55
Andrew Newdigate <andrew@troupe.co>
2016-02-15 15:06:14 +0000

    Session mutex: a mutex for multiple between multiple tabs in a browser

---
+++ public/js/utils/session-mutex.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var Promise = require('bluebird');
+var storage = window.localStorage;
+
+var TIMEOUT = 60000;
+var uniq = Math.floor(Math.random() * 10000000);
+
+function garbageCollection() {
+  var now = Date.now();
+
+  for (var i = storage.length; i >= 0; i--) {
+    var key = storage.key(i);
+    if (key && key.indexOf('lock:') === 0) {
+      var value = key.getItem(key);
+      if (value) {
+        var pair = value.split('-');
+        var lockDate =  parseInt(pair[0], 10);
+        if (now - lockDate > TIMEOUT) {
+          storage.removeItem(key);
+        }
+      }
+    }
+  }
+}
+
+setInterval(garbageCollection, 60000);
+
+var sessionMutex = Promise.method(function (key) {
+  var storageKey = 'lock:' + key;
+
+  var k = storage.getItem(storageKey);
+  if (k) {
+    return false;
+  }
+
+  var value = Date.now() + ':' + uniq;
+
+  storage.setItem(storageKey, value);
+  return Promise.delay(16)
+    .then(function() {
+      if (storage.getItem(storageKey) === value) {
+        Promise.delay(10000).then(function() {
+          storage.removeItem('lock:' + key);
+        });
+
+        return true;
+      }
+    });
+});
+
+module.exports = sessionMutex;
+window.sessionMutex = sessionMutex;
