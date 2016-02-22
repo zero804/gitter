e30c582ac735ffc3ce91339be1feab3a1531c752
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-02-22 17:57:02 +0000

    WIP pef improvements

--- public/js/views/menu/room/primary-collection/primary-collection-view.js
+++ public/js/views/menu/room/primary-collection/primary-collection-view.js
@@ -13,7 +13,7 @@ var PrimaryCollectionView = BaseCollectionView.extend({
   childView: ItemView,
   className: 'primary-collection',
   ui: {
-    collection: '.js-collection-list',
+    collection: '#collection-list',
   },
 
   emptyView: EmptySearchView,
