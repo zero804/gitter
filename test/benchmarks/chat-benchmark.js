575d3afb50e79d50b3ffb836c57de077a33bfc8c
Andrew Newdigate <andrew@troupe.co>
2016-03-01 14:38:32 +0000

    Allow fixtures to specify membership modes for users in room...

--- test/benchmarks/chat-benchmark.js
+++ test/benchmarks/chat-benchmark.js
@@ -4,6 +4,7 @@
 var makeBenchmark = require('../make-benchmark');
 var fixtureLoader = require('../integration/test-fixtures');
 var chatService = require('../../server/services/chat-service');
+var roomMembershipFlags = require("../../server/services/room-membership-flags");
 
 var fixture = {};
 
@@ -12,7 +13,30 @@ makeBenchmark({
   before: function(done) {
     var fixtureDescription = {
       troupeSmall: { users: ['user1', 'user2', 'user3'] },
-      troupeBig: { users: [] },
+      troupeBig: {
+        users: [],
+        membershipStrategy: function(userId, index) {
+          switch (index % 3) {
+            case 0:
+              return {
+                mode: roomMembershipFlags.MODES.all,
+                lurk: false
+              };
+
+            case 1:
+              return {
+                mode: roomMembershipFlags.MODES.announcement,
+                lurk: true
+              };
+
+            case 2:
+              return {
+                mode: roomMembershipFlags.MODES.mute,
+                lurk: true
+              };
+          }
+        }
+      },
     };
 
     for(var i = 0; i < 10000; i++) {
