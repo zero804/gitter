c23769ebbd34de7d6db1ee74ca3106bfcb628d0e
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-03-02 15:43:36 +0000

    replace room sorting with old sort

--- shared/sorting/left-menu-primary-default.js
+++ shared/sorting/left-menu-primary-default.js
@@ -1,23 +1,5 @@
 'use strict';
 
-module.exports = function leftMenuDefaultSort(a, b) {
-    var aMentions   = a.mentions;
-    var bMentions   = b.mentions;
-    if (aMentions) { return -1; }
-    if (bMentions) { return 1; }
+var sort = require('gitter-realtime-client/lib/sorts-filters').pojo.recents.sort;
 
-    var aUnread = !!a.unreadItems;
-    var bUnread = !!b.unreadItems;
-
-    if (aUnread) { return -1; }
-    if (bUnread) { return 1;}
-
-    var aLastAccess = a.lastAccessTime;
-    var bLastAccess = b.lastAccessTime;
-
-    if(!aLastAccess || !aLastAccess.valueOf) { return 1 }
-    if(!bLastAccess || !bLastAccess.valueOf) { return -1 }
-
-    return aLastAccess < bLastAccess ? 1 : -1;
-
-};
+module.exports = sort;
