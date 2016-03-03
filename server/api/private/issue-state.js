618d5ea5c3bca0fcacca2f65ea76fc8fe7135a7f
Andrew Newdigate <andrew@troupe.co>
2016-03-03 16:19:31 +0000

    Don't timeout on issue state errors

--- server/api/private/issue-state.js
+++ server/api/private/issue-state.js
@@ -1,38 +1,42 @@
 "use strict";
 
-var Promise = require('bluebird');
-var winston = require('../../utils/winston');
+
+var env                     = require('gitter-web-env');
+var logger                  = env.logger;
+var stats                   = env.stats;
+var Promise                 = require('bluebird');
 var GithubIssueStateService = require('gitter-web-github').GitHubIssueStateService;
+var StatusError             = require('statuserror');
 
 var EXPIRES_SECONDS = 180;
 var EXPIRES_MILLISECONDS = 180 * 1000;
 
 module.exports =  function(req, res, next) {
-  var issues = req.query.q;
+  var issue = req.query.q;
 
-  if(!Array.isArray(issues)) issues = [issues];
+  return Promise.try(function() {
+      if (!issue) throw new StatusError(400);
 
-  var service = new GithubIssueStateService(req.user);
+      var parts = issue.split('/');
+      if(parts.length !== 3) throw new StatusError(400);
 
-  Promise.all(issues.map(function(issue) {
-    var parts = issue.split('/');
-    if(parts.length !== 3) return '';
+      var service = new GithubIssueStateService(req.user);
+      return service.getIssueState(parts[0] + '/' + parts[1], parts[2]);
+    })
+    .then(function(results) {
+      res.setHeader('Cache-Control', 'public, max-age=' + EXPIRES_SECONDS);
+      res.setHeader('Expires', new Date(Date.now() + EXPIRES_MILLISECONDS).toUTCString());
 
-    return service.getIssueState(parts[0] + '/' + parts[1], parts[2])
-      .timeout(2500)
-      .catch(function(err) {
+      res.send([results]);
+    })
+    .catch(function(e) {
+      stats.eventHF('issue.state.query.fail', 1, 1);
 
-        winston.warn('Unable to obtain issue state for ' +
-            parts[0] + '/' + parts[1] + '#' +parts[2] + ': ' + err,
-            { exception: err });
+      logger.warn('Unable to obtain issue state for ' + issue + ': ' + e,
+          { exception: e });
 
-        return '';
-      });
-  })).then(function(results) {
-    res.setHeader('Cache-Control', 'public, max-age=' + EXPIRES_SECONDS);
-    res.setHeader('Expires', new Date(Date.now() + EXPIRES_MILLISECONDS).toUTCString());
-
-    res.send(results);
-  }).catch(next);
+      next(e);
+      return null;
+    });
 
 };
