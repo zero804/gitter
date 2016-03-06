3a1713ff9456cac95923e4c1d4cf2c85c436385c
Andrew Newdigate <andrew@troupe.co>
2016-03-06 00:04:42 +0000

    Chat benchmark

--- server/serializers/rest/chat-strategy.js
+++ server/serializers/rest/chat-strategy.js
@@ -143,7 +143,7 @@ function ChatStrategy(options)  {
       readBy: item.readBy ? item.readBy.length : undefined,
       urls: castArray(item.urls),
       initial: options.initialId && item._id == options.initialId || undefined,
-      mentions: castArray(item.mentions.map(function(m) {
+      mentions: castArray(item.mentions && _.map(item.mentions, function(m) {
           return {
             screenName: m.screenName,
             userId: m.userId,
