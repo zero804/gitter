d7940c5d0ba009c499daa16ffcc7271304b7896f
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-03-07 09:36:53 +0000

    update room-name-shortener

--- shared/room-name-shortener.js
+++ shared/room-name-shortener.js
@@ -1,18 +1,22 @@
 'use strict';
 
-module.exports = function roomNameShortener(name){
-  if(!name) return '';
-  var formattedName = name;
-  if(formattedName[0] === '/') { formattedName = formattedName.substring(1) }
-  while(formattedName.length > 19) {
-    formattedName = spliceFirstPart(name);
-    //Using a RegExp here is actually faster than splitting and testing length
-    //JP 10/2/16 http://jsperf.com/testing-for-slashes/2
-    if(!/\//.test(formattedName)){ break }
-  }
-  return formattedName || name;
-};
+module.exports = function roomNameShortener(name) {
+  var resultantName = '';
 
-function spliceFirstPart(name) {
-  return name.split('/').slice(1).join('/');
-}
+  name
+    .split('/')
+    .reverse()
+    .some(function(piece, index) {
+      var newResult = piece + (resultantName.length ? '/' : '') + resultantName;
+
+      if(newResult.length <= 19 || index === 0) {
+        resultantName = newResult;
+      }
+      else {
+        // break, we went over with this piece
+        return true;
+      }
+    }, '');
+
+  return resultantName || name;
+};
