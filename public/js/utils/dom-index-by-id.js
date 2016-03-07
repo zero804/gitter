db4bd340a927d07830f3ba87f1577ab67ac6baa6
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-03-07 16:48:09 +0000

    merge develop && address PR issues

--- public/js/utils/dom-index-by-id.js
+++ public/js/utils/dom-index-by-id.js
@@ -1,6 +1,6 @@
 'use strict'
 
-module.exports = function DomIndexerById(el){
+module.exports = function mapChildrenById(el){
   var map = {};
   for(var i = 0; i < el.children.length; i++){
     var child = el.children[i];
