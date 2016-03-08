763595066ba94df5a5d1774e677711fb94544bfb
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-03-08 13:28:15 +0000

    added detecting and selecting tempory orgs in the minibar and left menu

--- shared/get-org-name-from-troupe-name.js
+++ shared/get-org-name-from-troupe-name.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = function getRoomNameFromTroupeName(name){
+  if(name[0] === '/') { name = name.substring(1)}
   return /\//.test(name) ? name.split('/')[0] : name;
 };
