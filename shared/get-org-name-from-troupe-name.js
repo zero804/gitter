8525c8277f501787aa9934567b753b19696d83c0
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-03-08 14:01:28 +0000

    added default to name var

--- shared/get-org-name-from-troupe-name.js
+++ shared/get-org-name-from-troupe-name.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = function getRoomNameFromTroupeName(name){
+  name = (name || '');
   if(name[0] === '/') { name = name.substring(1)}
   return /\//.test(name) ? name.split('/')[0] : name;
 };
