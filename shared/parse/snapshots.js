225058622234ace44e2104b5006f377e3d046cd0
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-03-08 13:34:11 +0000

    make the comments actually make sense this time

--- shared/parse/snapshots.js
+++ shared/parse/snapshots.js
@@ -12,7 +12,7 @@ module.exports = function getSnapshotsForPageContext(req, troupeContext, orgs, r
     rooms:    rooms,
     orgs:     minibarOrgList,
 
-    //In the case a user is viewing a room, owned by an org the member is not yet a part of
+    //In the case a user is viewing a room, owned by an org that they are not a member of
     //we need to change the menu's state so it shows that org as selected both in the minibar
     //and the menu, as such the left menu context need to know about the org list
     leftMenu: parseLeftMenuTroupeContext(req, troupeContext, orgs, minibarOrgList),
