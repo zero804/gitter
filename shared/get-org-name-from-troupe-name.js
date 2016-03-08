85b7755457bb88d86734082574ed3e7cec917a72
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-03-08 15:24:49 +0000

    fixed getting org names from the url and force suggested org room collection fetch on init

--- shared/get-org-name-from-troupe-name.js
+++ shared/get-org-name-from-troupe-name.js
@@ -1,7 +1,11 @@
 'use strict';
 
-module.exports = function getRoomNameFromTroupeName(name){
+module.exports = function getRoomNameFromTroupeName(name) {
   name = (name || '');
-  if(name[0] === '/') { name = name.substring(1)}
+  if (name[0] === '/') { name = name.substring(1);}
+
+  //We do have the url /orgs/:orgName/rooms which we have to account for
+  if (/^orgs/.test(name)) { return name.split('/')[1]; }
+
   return /\//.test(name) ? name.split('/')[0] : name;
 };
