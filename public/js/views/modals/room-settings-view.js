9a0d3ffe5a4f30820db68abcae44acc80e3deff4
Andrew Newdigate <andrew@troupe.co>
2016-03-08 16:30:29 +0000

    Rework of legacy

--- public/js/views/modals/room-settings-view.js
+++ public/js/views/modals/room-settings-view.js
@@ -1,12 +1,16 @@
 "use strict";
 
 var Marionette             = require('backbone.marionette');
-var _                      = require('underscore');
-var context                = require('utils/context');
 var apiClient              = require('components/apiClient');
 var ModalView              = require('./modal');
 var troupeSettingsTemplate = require('./tmpl/room-settings-view.hbs');
 var notifications          = require('components/notifications');
+
+var OPTIONS = [
+  { val: 'all', text: 'All: Notify me for all messages' },
+  { val: 'mention', text: 'Announcements: Notify me for mentions and announcements' },
+  { val: 'mute', text: 'Mute: Notify me only when I\m directly mentioned' }
+];
 
 var View = Marionette.ItemView.extend({
   template: troupeSettingsTemplate,
@@ -20,7 +24,6 @@ var View = Marionette.ItemView.extend({
   ui: {
     options: '#notification-options',
     nonstandard: '#nonstandard',
-    lurkmode: '#lurkmode'
   },
 
   initialize: function() {
@@ -56,17 +59,50 @@ var View = Marionette.ItemView.extend({
 
   update: function() {
     var val = this.getNotificationOption();
+    var nonStandard = false;
+
     if (val) {
-      this.ui.options.val(val.selectValue);
-      this.ui.lurkmode.text(val.lurk ? 'Lurk on' : 'Lurk off');
       if (val.nonStandard) {
-        this.ui.nonstandard.show();
+        nonStandard = true;
+        this.setOption('', 'Legacy setting: ' + val.selectValue + ' mode, with ' + (val.lurk ? 'lurk on' : 'lurk off'));
       } else {
-        this.ui.nonstandard.hide();
+        this.setOption(val.selectValue);
       }
     } else {
-      this.ui.options.val('');
+      this.setOption('', 'Please wait...');
+    }
+
+    if (nonStandard) {
+      this.ui.nonstandard.show();
+    } else {
       this.ui.nonstandard.hide();
+    }
+  },
+
+  setOption: function(val, text) {
+    var selectInput = this.ui.options;
+    selectInput.empty();
+
+    var found = false;
+    var items = OPTIONS.map(function(o) {
+      var option = document.createElement("option");
+      option.value = o.val;
+      option.textContent = o.text;
+      var selected = o.val === val;
+      option.selected = selected;
+      if (selected) {
+        found = true;
+      }
+      return option;
+    });
+    selectInput.append(items);
+
+    if (!found) {
+      var option = document.createElement("option");
+      option.value = val;
+      option.textContent = text;
+      option.selected = true;
+      selectInput.append(option);
     }
   },
 
@@ -91,12 +127,9 @@ var View = Marionette.ItemView.extend({
   },
 
   serializeData: function() {
-    return _.extend({},
-      context.getTroupe(), {
-        notificationsBlocked: notifications.hasBeenDenied(),
-        isNativeDesktopApp: context().isNativeDesktopApp,
-        troupeUrl: '//' + window.location.host + window.location.pathname
-      });
+    return {
+      notificationsBlocked: notifications.hasBeenDenied(),
+    };
   }
 
 });
