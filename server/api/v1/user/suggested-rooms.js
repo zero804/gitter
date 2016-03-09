09f6ef181e21326c9061c51b7c3a665791e18ccc
Le Roux Bodenstein <leroux@gitter.im>
2016-03-09 15:16:16 +0200

    reuse user suggestions endpoint for the new code

--- server/api/v1/user/suggested-rooms.js
+++ server/api/v1/user/suggested-rooms.js
@@ -1,19 +1,38 @@
 "use strict";
 
-var restSerializer        = require("../../../serializers/rest-serializer");
-var legacyRecommendations = require('../../../services/recommendations/legacy-recommendations');
-var StatusError           = require('statuserror');
+var Promise = require('bluebird');
+var restSerializer = require("../../../serializers/rest-serializer");
+var roomMembershipService = require('../../../services/room-membership-service');
+var suggestionsService = require('../../../services/suggestions-service');
+var userSettingsService = require('../../../services/user-settings-service');
+var troupeService = require('../../../services/troupe-service');
+var StatusError = require('statuserror');
+var _ = require('lodash');
 
 module.exports = {
   id: 'resourceUserSuggestedRoom',
 
   index: function(req) {
-    if(!req.user) throw new StatusError(401);
+    // TODO: cache wrap this
+    if (!req.user) throw new StatusError(401);
 
-    return legacyRecommendations.getSuggestionsForUser(req.user, req.i18n.getLocale())
-      .then(function(suggestions) {
-        return restSerializer.serialize(suggestions, new restSerializer.SuggestedRoomStrategy({ }));
-      });
+    return Promise.all([
+      roomMembershipService.findRoomIdsForUser(req.user.id)
+        .then(function(roomIds) {
+          return troupeService.findByIdsLean(roomIds, {
+            uri: 1,
+            lcOwner: 1,
+            lang: 1,
+            oneToOne: 1
+          });
+        }),
+      userSettingsService.getUserSettings(req.user.id, 'lang')
+    ])
+    .spread(function(user, existingRooms, language) {
+      return suggestionsService.findSuggestionsForRooms(user, existingRooms, language);
+    })
+    .then(function(suggestedRooms) {
+      return restSerializer.serialize(suggestedRooms, new restSerializer.SuggestedRoomStrategy());
+    });
   }
-
 };
