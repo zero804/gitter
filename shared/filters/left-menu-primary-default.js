bf61c615c78c9055359b57be4d3bcaac3d821b74
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-03-10 13:36:11 +0000

    reset sort/filter functions in filtered room collection

--- shared/filters/left-menu-primary-default.js
+++ shared/filters/left-menu-primary-default.js
@@ -1,8 +1,8 @@
 'use strict';
 
-module.exports = function leftMenuDefaultFilter(room) {
-  var lastAccess = (room.lastAccessTime && !!room.lastAccessTime.valueOf) ?
-    room.lastAccessTime.valueOf() :
-    room.lastAccessTime;
-  return !!lastAccess;
+var rawFilter   = require('gitter-realtime-client/lib/sorts-filters').pojo.recents.filter;
+var modelFilter = require('gitter-realtime-client/lib/sorts-filters').model.recents.filter;
+
+module.exports = function defaultCollectionFilter(room){
+  return !!room.get ? modelFilter(room) : rawFilter(room);
 };
--- shared/filters/left-menu-primary-one2one.js
+++ shared/filters/left-menu-primary-one2one.js
@@ -3,5 +3,6 @@
 var defaultFilter = require('./left-menu-primary-default.js');
 
 module.exports = function leftMenuFavouriteOneToOne(room) {
+  console.log(defaultFilter(room), room.githubType, room.name, room.lastAccessTime, room.unreadItems, room.mentions);
   return defaultFilter(room) && room.githubType === 'ONETOONE';
 };
