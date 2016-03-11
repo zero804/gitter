a7291b72facd704cb4ed6e67ec994a3c9e2b966a
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-03-11 15:19:11 +0000

    remove log

--- shared/filters/left-menu-primary-one2one.js
+++ shared/filters/left-menu-primary-one2one.js
@@ -3,6 +3,5 @@
 var defaultFilter = require('./left-menu-primary-default.js');
 
 module.exports = function leftMenuFavouriteOneToOne(room) {
-  console.log(defaultFilter(room), room.githubType, room.name, room.lastAccessTime, room.unreadItems, room.mentions);
   return defaultFilter(room) && room.githubType === 'ONETOONE';
 };
