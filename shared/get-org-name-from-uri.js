546d52765e2d5b01d78a94198deb009494e6243c
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-03-14 09:56:18 +0000

    rename var that causes crash (silly cutandpastey)

--- shared/get-org-name-from-uri.js
+++ shared/get-org-name-from-uri.js
@@ -5,6 +5,6 @@ var getOrgNameFromTroupeName = require('./get-org-name-from-troupe-name.js');
 //TODO We should account for more cases than just the org urls
 module.exports = function getOrgNameFromUri(uri) {
   //We do have the url /orgs/:orgName/rooms which we have to account for
-  if (/^orgs/.test(name)) { return name.split('/')[1]; }
+  if (/^orgs/.test(uri)) { return uri.split('/')[1]; }
   return getOrgNameFromTroupeName(uri);
 };
