b2bfa955e26b8a89c1ed61bc39b121036d6bbcd3
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-03-14 14:11:59 +0000

    add more checks around temporary orgs

--- shared/orgs/suggested-orgs-from-room-list.js
+++ shared/orgs/suggested-orgs-from-room-list.js
@@ -38,7 +38,9 @@ module.exports = function suggestedOrgsFromRoomList(roomList, uri) {
   // If a user is viewing `/gitterHQ/gitter` or `/gitterHQ` and have never joined a gitterHQ room
   // we want to show the `gitterHQ` org in the minibar
   // But when someone visits `/home` or `/explore`, we don't want it to pick it up as an org
-  if (currentOrg === 'home' || currentOrg === 'explore') {
+  var existingConversationByOrg = _.where(roomList, { url: '/' + currentOrg })[0];
+  var isExistingOneToOne        = (existingConversationByOrg.githubType === 'ONETOONE');
+  if (currentOrg === 'home' || currentOrg === 'explore' || isExistingOneToOne) {
     return orgList;
   }
 
