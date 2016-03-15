57fe3773a4b9d533d2272f75137f9f1e20fe816c
Le Roux Bodenstein <leroux@gitter.im>
2016-03-15 19:36:23 +0200

    Don't accidentally intercept links in the modal.

--- public/js/views/layouts/chat-toolbar.js
+++ public/js/views/layouts/chat-toolbar.js
@@ -10,7 +10,7 @@ require('views/behaviors/isomorphic');
 
 module.exports = ChatLayout.extend({
   events: {
-    'click a[href^="/login"]': 'clickLogin'
+    'click .chat-input-nli a[href^="/login"]': 'clickLogin'
   },
 
   clickLogin: function(e) {
