9ea4f281d86be405e413a0e77898e3211a80c184
Eric Eastwood <contact@ericeastwood.com>
2016-03-14 22:59:10 -0500

    Fix logic error in `toggleClass` implementation with undefined state
    `toggleClass(el, 'foo')` should toggle the class. Previsouly, it would always disable

--- public/js/utils/toggle-class.js
+++ public/js/utils/toggle-class.js
@@ -1,11 +1,21 @@
 'use strict';
 
-function toggleClass(el, className, state) {
-  if(state) {
-    el.classList.add(className);
-  } else {
-    el.classList.remove(className);
+// Can't use `classList.toggle` with the second parameter (force)
+// Because IE11 does not support it
+var toggleClass = function(element, class1, force) {
+  if(arguments.length === 3) {
+    if(force) {
+      element.classList.add(class1);
+    }
+    else {
+      element.classList.remove(class1);
+    }
   }
-}
+  else {
+    element.classList.toggle(class1);
+  }
+
+  return force;
+};
 
 module.exports = toggleClass;
