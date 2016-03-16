e0a9790360c40b6c0daab7cad11e424fcffd653b
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-03-16 10:10:25 +0000

    stop empty view from being draggable

--- public/js/views/menu/room/primary-collection/primary-collection-item-favourite-empty-view.js
+++ public/js/views/menu/room/primary-collection/primary-collection-item-favourite-empty-view.js
@@ -4,6 +4,7 @@ var Marionette = require('backbone.marionette');
 var template   = require('./primary-collection-item-favourite-empty-view.hbs');
 
 module.exports = Marionette.ItemView.extend({
+  id:        'empty-view',
   className: 'empty-favourite loaded',
   template:  template,
 });
