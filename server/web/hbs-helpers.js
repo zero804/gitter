d1d3bdb00c3be0b139d313f71c1a0b0dfcb35db2
Eric Eastwood <contact@ericeastwood.com>
2016-03-21 23:18:28 -0500

    Update explore to be close to shippable

--- server/web/hbs-helpers.js
+++ server/web/hbs-helpers.js
@@ -1,51 +1,10 @@
 "use strict";
 
-var nconf           = require('../utils/config');
-var cdn             = require("./cdn");
-var _               = require('underscore');
-var appVersion      = require('./appVersion');
-var safeJson        = require('../utils/safe-json');
-var env             = process.env.NODE_ENV;
-var util            = require('util');
-
-var cdns;
-if(nconf.get("cdn:use")) {
-  cdns = nconf.get("cdn:hosts");
-}
-
-// This stuff never changes
-var troupeEnv = {
-  domain: nconf.get('web:domain'),
-  baseServer: nconf.get('web:baseserver'),
-  basePath: nconf.get('web:basepath'),
-  apiBasePath: nconf.get('web:apiBasePath'),
-  homeUrl: nconf.get('web:homeurl'),
-  badgeBaseUrl: nconf.get('web:badgeBaseUrl'),
-  embedBaseUrl: nconf.get('web:embedBaseUrl'),
-  mixpanelToken: nconf.get("stats:mixpanel:enabled") && nconf.get("stats:mixpanel:token"),
-  googleTrackingId: nconf.get("stats:ga:key"),
-  googleTrackingDomain: nconf.get("stats:ga:domain"),
-  goSquaredTrackingId: nconf.get("web:goSquaredId"),
-  env: env,
-  cdns: cdns,
-  version: appVersion.getVersion(),
-  assetTag: appVersion.getAssetTag(),
-  logging: nconf.get("web:consoleLogging"),
-  ravenUrl: nconf.get("errorReporting:clientRavenUrl"),
-  websockets: {
-    fayeUrl: nconf.get('ws:fayeUrl'),
-    options: {
-      timeout: nconf.get('ws:fayeTimeout'),
-      retry: nconf.get('ws:fayeRetry')
-    }
-  },
-  embed: {
-    basepath: nconf.get('embed:basepath'),
-    cacheBuster: nconf.get('embed:cacheBuster')
-  },
-  billingUrl: nconf.get('web:billingBaseUrl'),
-  maxFreeOrgRoomMembers: nconf.get('maxFreeOrgRoomMembers')
-};
+var _         = require('underscore');
+var safeJson  = require('../utils/safe-json');
+var util      = require('util');
+var troupeEnv = require('./troupe-env');
+var cdn = require('./cdn');
 
 exports.cdn = function(url, parameters) {
   return cdn(url, parameters ? parameters.hash:null);
