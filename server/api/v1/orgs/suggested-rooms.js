4a44296f97a3af08bcb959d7a0f0f5af1d8519cd
Le Roux Bodenstein <leroux@gitter.im>
2016-03-29 16:21:40 +0200

    Filter out existing org rooms.

--- server/api/v1/orgs/suggested-rooms.js
+++ server/api/v1/orgs/suggested-rooms.js
@@ -1,17 +1,29 @@
 'use strict';
 
+var Promise = require('bluebird');
+var _ = require('lodash');
 var suggestions    = require('gitter-web-suggestions');
 var restSerializer = require('../../../serializers/rest-serializer');
 var troupeService = require('../../../services/troupe-service');
+var roomMembershipService = require('../../../services/room-membership-service');
+var collections = require('../../../utils/collections');
 
 module.exports = {
   index: function(req) {
     var userId  = req.user._id;
     var orgName = req.params.orgName.toLowerCase();
-    //return suggestions.getSuggestionsForOrg(orgName, userId)
-    return troupeService.findChildRoomsForOrg(orgName, {security: 'PUBLIC'})
-      .then(function(suggestions) {
-        return restSerializer.serialize(suggestions, new restSerializer.SuggestedRoomStrategy({ }));
-      });
+    return Promise.join(
+        troupeService.findChildRoomsForOrg(orgName, {security: 'PUBLIC'}),
+        roomMembershipService.findRoomIdsForUser(userId),
+        function(rooms, existingRoomIds) {
+          var idMap = collections.hashArray(existingRoomIds);
+
+          var suggestions = _.reject(rooms, function(room) {
+            return idMap[room.id];
+          });
+
+          return restSerializer.serialize(suggestions, new restSerializer.SuggestedRoomStrategy({ }));
+        }
+      );
   },
 };
