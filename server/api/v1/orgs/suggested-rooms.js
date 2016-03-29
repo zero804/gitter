aee84177e57252ce173ec5731c10038a9cd5ca8f
Le Roux Bodenstein <leroux@gitter.im>
2016-03-29 15:47:49 +0200

    Don't use neo4j for org suggestions.

--- server/api/v1/orgs/suggested-rooms.js
+++ server/api/v1/orgs/suggested-rooms.js
@@ -2,12 +2,14 @@
 
 var suggestions    = require('gitter-web-suggestions');
 var restSerializer = require('../../../serializers/rest-serializer');
+var troupeService = require('../../../services/troupe-service');
 
 module.exports = {
   index: function(req) {
     var userId  = req.user._id;
     var orgName = req.params.orgName.toLowerCase();
-    return suggestions.getSuggestionsForOrg(orgName, userId)
+    //return suggestions.getSuggestionsForOrg(orgName, userId)
+    return troupeService.findChildRoomsForOrg(orgName, {security: 'PUBLIC'})
       .then(function(suggestions) {
         return restSerializer.serialize(suggestions, new restSerializer.SuggestedRoomStrategy({ }));
       });
