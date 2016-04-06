62c97cf26d4ee6fefdb86bb7d8f6e2ec2e59341e
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-04-06 12:13:26 +0100

    use getOrgNameFromTroupeName

--- shared/filters/left-menu-primary-org.js
+++ shared/filters/left-menu-primary-org.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var defaultFilter            = require('./left-menu-primary-default.js');
-var getTroupeNameFromORgName = require('../get-org-name-from-troupe-name');
+var getTroupeNameFromOrgName = require('../get-org-name-from-troupe-name');
 
 module.exports = function leftMenuFavouriteOrg(room, orgName) {
-  return defaultFilter(room) && (getTroupeNameFromORgName(room.name) === orgName) && room.roomMember;
+  return defaultFilter(room) && (getTroupeNameFromOrgName(room.name) === orgName) && room.roomMember;
 };
