493c1f1cd028f96417b2d83eadcffd487ffee1fd
Andrew Newdigate <andrew@troupe.co>
2016-04-07 13:09:39 +0200

    Making chat message realtime notifications work once again.

--- server/services/live-collections/live-collection-rooms.js
+++ server/services/live-collections/live-collection-rooms.js
@@ -26,11 +26,11 @@ function serializeRoomToUsers(userIds, operation, troupe) {
 
   if(troupe.oneToOne) {
     // Because the troupe needs customized per-user....
-    serializeOneToOneRoomToUsers(userIds, operation, troupe);
-    return;
+    return serializeOneToOneRoomToUsers(userIds, operation, troupe);
   }
 
-  return restSerializer.serializeModel(troupe)
+  var strategy = new restSerializer.TroupeStrategy();
+  return restSerializer.serializeObject(troupe, strategy)
     .then(function(serializedTroupe) {
       appEvents.dataChange2('/rooms/' + troupe._id, operation, serializedTroupe, 'room');
 
