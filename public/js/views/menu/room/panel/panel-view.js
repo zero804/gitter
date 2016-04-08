049f510ce5fc737e3ecede6f86908b8af9977b7f
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-04-08 11:07:00 +0100

    remove favourite collection from window object

--- public/js/views/menu/room/panel/panel-view.js
+++ public/js/views/menu/room/panel/panel-view.js
@@ -54,7 +54,7 @@ module.exports = Marionette.LayoutView.extend({
   initFavouriteCollection: function (optionsForRegion) {
     //Sadly the favourite collection needs to be generated here rather than the room-menu-model
     //because it has a dependency on the dnd-controller JP 1/4/16
-    var favCollection = window.fav = new FilteredFavouriteRoomCollection({
+    var favCollection =  new FilteredFavouriteRoomCollection({
       roomModel:  this.model,
       collection: this.model._roomCollection,
       dndCtrl:    this.dndCtrl,
