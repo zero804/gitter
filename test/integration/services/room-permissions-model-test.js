2e9c2167f37f1b31b3342684336ee249b9aed135
Le Roux Bodenstein <leroux@gitter.im>
2016-04-12 17:33:35 +0200

    Also test one-to-one rooms.

--- test/integration/services/room-permissions-model-test.js
+++ test/integration/services/room-permissions-model-test.js
@@ -7,8 +7,9 @@ var testRequire = require('../test-require');
 describe('roomPermissionsModel', function() {
   var githubUser = { providers: ['github'] };
   var twitterUser = { providers: ['twitter'] };
-  var legacyRoom = { uri: 'foo/bar', githubType: 'REPO', security: 'PUBLIC'};
+  var legacyRoom = { uri: 'foo/bar', githubType: 'REPO', security: 'PUBLIC' };
   var githubRoom = { providers: ['github'], uri: 'foo/bar', githubType: 'REPO', security: 'PUBLIC' };
+  var oneToOneRoom = { oneToOne: true, githubType: 'ONETOONE', security: 'PRIVATE' };
 
   var roomPermissionsModel = testRequire.withProxies('./services/room-permissions-model', {
     './permissions-model': function(user, right, uri, githubType, security) {
@@ -44,6 +45,13 @@ describe('roomPermissionsModel', function() {
 
   it('should allow a user to join a github-only room if the user has a github identity', function() {
     return roomPermissionsModel(githubUser, 'join', githubRoom)
+      .then(function(result) {
+        assert.strictEqual(result, 'ok');
+      });
+  });
+
+  it('should pass through one-to-one rooms too', function() {
+    return roomPermissionsModel(twitterUser, 'join', oneToOneRoom)
       .then(function(result) {
         assert.strictEqual(result, 'ok');
       });
