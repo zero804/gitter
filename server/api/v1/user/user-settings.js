228154a5b33ff8b5bc6ea1348e8141ba823142f6
Andrew Newdigate <andrew@troupe.co>
2016-04-14 19:07:00 +0100

    Made code testable

--- server/api/v1/user/user-settings.js
+++ server/api/v1/user/user-settings.js
@@ -1,120 +1,6 @@
 'use strict';
 
-var Promise                       = require('bluebird');
-var StatusError                   = require('statuserror');
-var userSettingsService           = require('../../../services/user-settings-service');
-var userDefaultFlagsService       = require('../../../services/user-default-flags-service');
-var userDefaultFlagsUpdateService = require('../../../services/user-default-flags-update-service');
-
-function standardHandler(settingsKey) {
-  return {
-    get: function(user) {
-      return userSettingsService.getUserSettings(user._id, settingsKey);
-    },
-    set: function(user, value) {
-      // Does anyone know why the hell we do this?
-      if (value.hasOwnProperty('value')) {
-        value = value.value;
-      }
-
-      return userSettingsService.setUserSettings(user._id, settingsKey, value)
-        .return(value);
-    }
-  };
-}
-
-/**
- * Each setting key can have it's own handler.
- * This maps the setting key to a read and write handler.
- * If the user attempts to write to an unknown key, we throw a
- * 404 error
- */
-var HANDLERS = {
-  leftRoomMenu: standardHandler('leftRoomMenu'),
-  lang: standardHandler('lang'),
-  unread_notifications_optout: {
-    get: function(user) {
-      return userSettingsService.getUserSettings(user._id, 'unread_notifications_optout')
-        .then(function(value) {
-          return !!value;
-        });
-    },
-    set: function(user, value) {
-      return userSettingsService.setUserSettings(user._id, 'unread_notifications_optout', value ? 1 : 0)
-        .then(function() {
-          return !!value;
-        });
-    }
-  },
-  defaultRoomMode: {
-    get: function(user) {
-      return userDefaultFlagsService.getDefaultFlagDetailsForUserId(user._id);
-    },
-    set: function(user, value) {
-      var mode = value.mode;
-      var override = value.override;
-
-      return userDefaultFlagsUpdateService.updateDefaultModeForUser(user, mode, override)
-        .then(function() {
-          return userDefaultFlagsService.getDefaultFlagDetailsForUserId(user._id);
-        });
-    }
-  }
-};
-
-function mapHandlers(keys) {
-  return keys.map(function(key) {
-    var handler = HANDLERS[key];
-    if (!handler) throw new StatusError(404);
-    return handler;
-  });
-}
-
-function getSetting(user, key) {
-  var handler = HANDLERS[key];
-  if (!handler) throw new StatusError(404);
-  return handler.get(user)
-    .then(function(f) {
-      return f || {};
-    });
-}
-
-function getSettings(user, keys) {
-  var handlers = mapHandlers(keys);
-  return Promise.map(handlers, function(handler) {
-    return handler.get(user);
-  })
-  .then(function(results) {
-    return keys.reduce(function(memo, key, index) {
-      memo[key] = results[index];
-      return memo;
-    }, {});
-  });
-}
-
-function updateSetting(user, key, value) {
-  var handler = HANDLERS[key];
-  if (!handler) throw new StatusError(404);
-
-  return handler.set(user, value);
-}
-
-function updateSettings(user, valuesHash) {
-  var keys = Object.keys(valuesHash);
-  var handlers = mapHandlers(keys);
-
-  return Promise.map(handlers, function(handler, index) {
-    var key = keys[index];
-    var value = valuesHash[key];
-    return handler.set(user, value);
-  })
-  .then(function(results) {
-    return keys.reduce(function(memo, key, index) {
-      memo[key] = results[index];
-      return memo;
-    }, {});
-  });
-}
+var userSettingsMuxer = require('../../../services/user-settings-muxer');
 
 module.exports = {
   id: 'userSetting',
@@ -126,16 +12,16 @@ module.exports = {
     var settings = settingsKey.split(/,/);
 
     if (settings.length === 1) {
-      return getSetting(user, settingsKey);
+      return userSettingsMuxer.getSetting(user, settingsKey);
     } else {
-      return getSettings(user, settingsKey.split(','));
+      return userSettingsMuxer.getSettings(user, settingsKey.split(','));
     }
   },
 
   create: function(req) {
     var valuesHash = req.body;
     var user = req.resourceUser;
-    return updateSettings(user, valuesHash);
+    return userSettingsMuxer.updateSettings(user, valuesHash);
   },
 
   update: function(req) {
@@ -143,7 +29,7 @@ module.exports = {
     var user = req.resourceUser;
     var settingsKey = req.params.userSetting;
 
-    return updateSetting(user, settingsKey, value);
+    return userSettingsMuxer.updateSetting(user, settingsKey, value);
   },
 
   respond: function(req, res, responseBody) {
