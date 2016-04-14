31161f8b7f8d3f38515dfefc7b6f5edbea9e7378
Andrew Newdigate <andrew@troupe.co>
2016-04-14 16:00:25 +0100

    Allow multiple items to update simulatenously in userSettings resource Also fixes the exception for left-room menu not wanting the JSON back cc @cutandpastey @lerouxb

--- server/api/v1/user/user-settings.js
+++ server/api/v1/user/user-settings.js
@@ -1,86 +1,156 @@
 'use strict';
 
 var Promise                       = require('bluebird');
+var StatusError                   = require('statuserror');
 var userSettingsService           = require('../../../services/user-settings-service');
 var userDefaultFlagsService       = require('../../../services/user-default-flags-service');
 var userDefaultFlagsUpdateService = require('../../../services/user-default-flags-update-service');
 
-var DEFAULT_ROOM_MEMBERSHIP_MODE_KEY = 'defaultRoomMode';
-var UNREAD_EMAIL_OPT_OUT_KEY = 'unread_notifications_optout';
+function standardHandler(settingsKey) {
+  return {
+    get: function(user) {
+      return userSettingsService.getUserSettings(user._id, settingsKey);
+    },
+    set: function(user, value) {
+      // Does anyone know why the hell we do this?
+      if (value.hasOwnProperty('value')) {
+        value = value.value;
+      }
+
+      return userSettingsService.setUserSettings(user._id, settingsKey, value)
+        .return(value);
+    }
+  };
+}
+
+var SETTINGS = {
+  leftRoomMenu: standardHandler('leftRoomMenu'),
+  lang: standardHandler('leftRoomMenu'),
+  unread_notifications_optout: {
+    get: function(user) {
+      return userSettingsService.getUserSettings(user._id, 'unread_notifications_optout')
+        .then(function(value) {
+          return !!value;
+        });
+    },
+    set: function(user, value) {
+      return userSettingsService.setUserSettings(user._id, 'unread_notifications_optout', value ? 1 : 0)
+        .then(function() {
+          return !!value;
+        });
+    }
+  },
+  defaultRoomMode: {
+    get: function(user) {
+      return userDefaultFlagsService.getDefaultFlagDetailsForUserId(user._id);
+    },
+    set: function(user, value) {
+      var mode = value.mode;
+      var override = value.override;
+
+      return userDefaultFlagsUpdateService.updateDefaultModeForUser(user, mode, override)
+        .then(function() {
+          return userDefaultFlagsService.getDefaultFlagDetailsForUserId(user._id);
+        });
+    }
+  }
+};
+
+function mapHandlers(keys) {
+  return keys.map(function(key) {
+    var handler = SETTINGS[key];
+    if (!handler) throw new StatusError(404);
+    return handler;
+  });
+}
+
+function getSetting(user, key) {
+  var handler = SETTINGS[key];
+  if (!handler) throw new StatusError(404);
+  return handler.get(user)
+    .then(function(f) {
+      return f || {};
+    });
+}
+
+function getSettings(user, keys) {
+  var handlers = mapHandlers(keys);
+  return Promise.map(handlers, function(handler) {
+    return handler.get(user);
+  })
+  .then(function(results) {
+    return keys.reduce(function(memo, key, index) {
+      memo[key] = results[index];
+      return memo;
+    }, {});
+  });
+}
+
+function updateSetting(user, key, value) {
+  var handler = SETTINGS[key];
+  if (!handler) throw new StatusError(404);
+
+  return handler.set(user, value);
+}
+
+function updateSettings(user, valuesHash) {
+  var keys = Object.keys(valuesHash);
+  var handlers = mapHandlers(keys);
+
+  return Promise.map(handlers, function(handler, index) {
+    var key = keys[index];
+    var value = valuesHash[key];
+    return handler.set(user, value);
+  })
+  .then(function(results) {
+    return keys.reduce(function(memo, key, index) {
+      memo[key] = results[index];
+      return memo;
+    }, {});
+  });
+}
 
 module.exports = {
   id: 'userSetting',
 
-  index: function(req) {
-    var userId = req.resourceUser._id;
-
-    if(req.query.category === 'notifications') {
-      // Allow a single get for the notification default settings dialog
-      return Promise.join(
-        userSettingsService.getUserSettings(userId, UNREAD_EMAIL_OPT_OUT_KEY),
-        userDefaultFlagsService.getDefaultFlagDetailsForUserId(userId),
-        function(emailOptOut, defaultFlags) {
-          var result = {};
-          result[UNREAD_EMAIL_OPT_OUT_KEY] = !!emailOptOut;
-          result[DEFAULT_ROOM_MEMBERSHIP_MODE_KEY] = defaultFlags;
-          return result;
-        });
-    }
-
-    return userSettingsService.getAllUserSettings(req.resourceUser.id)
-      .then(function (settings) {
-        return settings || {};
-      });
-  },
-
   show: function(req) {
     var settingsKey = req.params.userSetting;
-    var userId = req.resourceUser._id;
+    var user = req.resourceUser;
 
-    if (settingsKey === DEFAULT_ROOM_MEMBERSHIP_MODE_KEY) {
-      return userDefaultFlagsService.getDefaultFlagDetailsForUserId(userId);
+    var settings = settingsKey.split(/,/);
+
+    if (settings.length === 1) {
+      return getSetting(user, settingsKey);
+    } else {
+      return getSettings(user, settingsKey.split(','));
     }
+  },
 
-    return userSettingsService.getUserSettings(userId, settingsKey)
-      .then(function(f) {
-        return f || {};
-      });
+  create: function(req) {
+    var valuesHash = req.body;
+    var user = req.resourceUser;
+    return updateSettings(user, valuesHash);
   },
 
   update: function(req) {
-    var settings = req.body;
-    var userId = req.resourceUser._id;
+    var value = req.body;
+    var user = req.resourceUser;
     var settingsKey = req.params.userSetting;
 
-    if (settingsKey === DEFAULT_ROOM_MEMBERSHIP_MODE_KEY) {
-      var mode = settings.mode;
-      var override = settings.override;
-      return userDefaultFlagsUpdateService.updateDefaultModeForUser(req.resourceUser, mode, override)
-        .then(function() {
-          return userDefaultFlagsService.getDefaultFlagDetailsForUserId(userId);
-        });
+    return updateSetting(user, settingsKey, value);
+  },
+
+  respond: function(req, res, responseBody) {
+    switch(req.accepts(['json', 'text'])) {
+      case 'json':
+        res.send(responseBody);
+        break;
+
+      default:
+        res.sendStatus(200);
+        break;
     }
 
-    if (settings.hasOwnProperty('value')) {
-      settings = settings.value;
-    }
-
-    // NOTE: this didn't used to return and when the return was added it
-    // started causing the left menu to respond to the payload whereas before
-    // it was "fire and forget" and this API just returned 200 OK without a
-    // json response. So as a quick temporary fix we're checking for the exact
-    // key and emulating the old behavior for that one case for now.
-    // cc @suprememoocow
-    return userSettingsService.setUserSettings(userId, settingsKey, settings)
-      .then(function() {
-        if (settingsKey == 'leftRoomMenu') {
-          // blank object should be close enough to a 200 OK and hopefully will
-          // stop backbone from picking it up.
-          return {};
-        } else {
-          return settings;
-        }
-      });
   }
 
 };
