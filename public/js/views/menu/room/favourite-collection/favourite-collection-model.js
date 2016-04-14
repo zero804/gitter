5f0b1ace87a703503f787803786949445fd27b56
Eric Eastwood <contact@ericeastwood.com>
2016-04-14 00:20:35 -0500

    Update left-menu keyboard interaction

--- public/js/views/menu/room/favourite-collection/favourite-collection-model.js
+++ public/js/views/menu/room/favourite-collection/favourite-collection-model.js
@@ -4,6 +4,27 @@ var PrimaryCollectionModel = require('../primary-collection/primary-collection-m
 
 module.exports = PrimaryCollectionModel.extend({
 
+
+  constructor: function(attrs, options) {
+    PrimaryCollectionModel.prototype.constructor.apply(this, arguments);
+    this.collection = options.collection;
+  },
+
+  onModelChangePreState: function() {
+    var active = false;
+
+    switch (this.roomMenuModel.get('state')) {
+      default:
+        // We use `collection.models.length` vs `collection.length`
+        active = !!this.collection.models.length;
+        break;
+    }
+
+    // TODO, see why this isn't showing the correct number of filtered favourites in the views
+    console.log('onModelChangePreState', active, this.collection.length, this.collection.models.length, this.collection.models, this);
+    this.set('active', active);
+  },
+
   onSearch: function(){
     this.set({
       active: false,
