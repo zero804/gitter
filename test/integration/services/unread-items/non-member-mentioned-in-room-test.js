025453163885f9072bc6b6727fb7c2676b05f21d
Andrew Newdigate <andrew@troupe.co>
2016-04-20 17:59:46 +0100

    Added another integration test. Fixes https://github.com/troupe/gitter-webapp/issues/1361

--- test/integration/services/unread-items/non-member-mentioned-in-room-test.js
+++ test/integration/services/unread-items/non-member-mentioned-in-room-test.js
@@ -3,26 +3,26 @@
 var testRequire       = require('./../../test-require');
 var fixtureLoader     = require('../../test-fixtures');
 var assert            = testRequire("assert");
-var Promise           = require('bluebird');
-var userService       = testRequire("./services/user-service");
 var chatService       = testRequire("./services/chat-service");
 var unreadItemService = testRequire("./services/unread-items");
 var appEvents         = testRequire("gitter-web-appevents");
 
-describe("Ensure that non-members receive the correct notifications for mentions #slow", function() {
+describe("unread item end-to-end integration tests #slow", function() {
   var fixture = {};
 
   before(fixtureLoader(fixture, {
     user1: { username: true },
     user2: { username: true },
-    troupe1: { users: ['user1'], githubType: 'REPO', security: 'PUBLIC' }
+    user3: { username: true },
+    troupe1: { users: ['user1'], githubType: 'REPO', security: 'PUBLIC' },
+    troupe2: { users: ['user1'], githubType: 'ORG_CHANNEL', security: 'PRIVATE' },
   }));
 
   after(function() {
     return fixture.cleanup();
   });
 
-  it('should notify', function() {
+  it('should notify when the user has access', function() {
     var troupe = fixture.troupe1;
     var troupeId = troupe.id;
     var user2 = fixture.user2;
@@ -32,7 +32,7 @@ describe("Ensure that non-members receive the correct notifications for mentions
       if (String(data.userId) === String(user2.id)) {
         onUserMentionedInNonMemberRoom++;
       }
-    })
+    });
 
     return chatService.newChatMessageToTroupe(troupe, fixture.user1, {
         text: 'Hey there @' + user2.username
@@ -51,6 +51,38 @@ describe("Ensure that non-members receive the correct notifications for mentions
       })
       .then(function(roomIds) {
         assert.deepEqual(roomIds, [troupeId]);
+      });
+  });
+
+  it('should not notify when the user does not have access', function() {
+    var troupe = fixture.troupe2;
+    var troupeId = troupe.id;
+    var user3 = fixture.user3;
+
+    var onUserMentionedInNonMemberRoom = 0;
+    appEvents.onUserMentionedInNonMemberRoom(function(data) {
+      if (String(data.userId) === String(user3.id)) {
+        onUserMentionedInNonMemberRoom++;
+      }
+    });
+
+    return chatService.newChatMessageToTroupe(troupe, fixture.user1, {
+        text: 'Hey there @' + user3.username
+      })
+      .delay(200)  // NB: magic number :(
+      .bind({})
+      .then(function(chat) {
+        assert.strictEqual(onUserMentionedInNonMemberRoom, 0);
+        this.chatId = chat.id;
+        return unreadItemService.getUnreadItems(user3.id, troupeId);
+      })
+      .then(function(x) {
+        assert.deepEqual(x, []);
+
+        return unreadItemService.getRoomIdsMentioningUser(user3._id);
+      })
+      .then(function(roomIds) {
+        assert.deepEqual(roomIds, []);
       });
   });
 
