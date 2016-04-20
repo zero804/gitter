c2db365cb2d531c31ec22364f42e1b876a8bf8af
Eric Eastwood <contact@ericeastwood.com>
2016-04-20 10:17:27 -0500

    Slim down the logic to bare minimum 5000 timeout threshold

--- shared/parse/left-menu-troupe-context.js
+++ shared/parse/left-menu-troupe-context.js
@@ -10,32 +10,15 @@ var resolvePageLoadedLeftMenuState = function(currentLeftRoomMenuData, req) {
 
   // If actually on a room page that needs some auto-resolving
   if(req.troupe) {
-    var referrer = req.get('Referrer') || '';
     var timeNow = new Date().getTime();
-    var previousTroupeUri = req.cookies.previousTroupeUri;
     var previousLocationUnloadTime = req.cookies.previousLocationUnloadTime;
-    var currentTroupeUri = req.troupe.uri;
-
     // 5000 is an arbitrary good-enough threshold to aproximate page-refresh
     var isWithinRefreshTimeThreshold = previousLocationUnloadTime && (timeNow - previousLocationUnloadTime) < 5000;
 
-    // If most-likely was not refresh because timing
-    // and they came through a link(because referrer).
-    // note: `document.referrer` is sticky through refreshes
-    var didComeThroughLinkOutsideApp = !isWithinRefreshTimeThreshold && referrer.length > 0;
-    // If they navigated to a completely separate URL than what we have saved
-    // And they they don't have a referrer meaning they navigated directly most likely
-    var didNavigateDirectly =  (!isWithinRefreshTimeThreshold || currentTroupeUri !== previousTroupeUri) && referrer.length === 0;
-
-    if(didComeThroughLinkOutsideApp || didNavigateDirectly) {
+    if(!isWithinRefreshTimeThreshold) {
       currentState = 'org';
       currentlySelectedOrg = getOrgNameFromTroupeName(req.troupe.uri);
     }
-
-
-    console.log('asdf', currentTroupeUri, previousTroupeUri, referrer, isWithinRefreshTimeThreshold, (timeNow - previousLocationUnloadTime), previousLocationUnloadTime);
-    console.log('didComeThroughLinkOutsideApp', didComeThroughLinkOutsideApp);
-    console.log('didNavigateDirectly', didNavigateDirectly);
   }
 
   return {
