e731414eb877d07c2d17a8d7002008727a2fd1e0
Andrew Newdigate <andrew@troupe.co>
2016-04-25 12:20:43 +0100

    Fixed search-and-replace error

--- server/event-listeners/mongoose-events.js
+++ server/event-listeners/mongoose-events.js
@@ -5,7 +5,7 @@ exports.install = function() {
   if (installed) return;
   installed = true;
 
-  var events = require('gitter-web-persistence-events');
+  var events = require('../services/persistence-service-events');
   var persistence = require('gitter-web-persistence');
   events.install(persistence);
 };
