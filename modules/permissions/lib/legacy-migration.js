e84932c3292f798034fb04140f3dbad17ab3af6e
Andrew Newdigate <andrew@troupe.co>
2016-04-25 22:03:04 +0100

    Made legacy-migration synchronous

--- modules/permissions/lib/legacy-migration.js
+++ modules/permissions/lib/legacy-migration.js
@@ -1,15 +1,13 @@
 'use strict';
 
 var StatusError = require('statuserror');
-var Promise = require('bluebird');
-var Troupe = require('gitter-web-persistence').Troupe;
-var User = require('gitter-web-persistence').User;
 
 /**
  * This module helps migrating from the old world of permissions
  * to the new world.
  *
- * At the end of the migration process (see https://github.com/troupe/gitter-webapp/milestones/New%20Permissions%20API%20and%20GitHub%20URI%20break)
+ * At the end of the migration process
+ * (see https://github.com/troupe/gitter-webapp/milestones/New%20Permissions%20API%20and%20GitHub%20URI%20break)
  * this module should be removed
  */
 
@@ -59,47 +57,42 @@ function generateRepoPermissionsForRoom(room) {
     default:
       throw new StatusError(500, 'Unknown security type: ' + security);
   }
-
 }
 
-function getParentRoom(id) {
-  return Troupe.findById(id)
-    .lean()
-    .exec()
-    .then(function(parentRoom) {
-      if (!parentRoom) {
-        throw new StatusError(500, 'Parent room not found: ' + id);
-      }
-      return parentRoom;
-    });
-}
 
-function getOwnerUser(id) {
-  return User.findById(id)
-    .lean()
-    .exec()
-    .then(function(ownerUser) {
-      if (!ownerUser) {
-        throw new StatusError(500, 'Owner not found: ' + id);
-      }
-      return ownerUser;
-    });
-}
-
-function generateRepoChannelPermissionsForRoom(room) {
+function generateRepoChannelPermissionsForRoom(room, parentRoom) {
   var uri = room.uri;
   var ownerUri = uri.split(/\//).slice(0, 2).join('/');
+  var security = room.security;
+  var githubId = parentRoom.githubId;
 
-  return getParentRoom(room.parentId)
-    .then(function(parentRoom) {
-      var security = room.security;
-      var githubId = parentRoom.githubId;
+  if (ownerUri !== parentRoom.uri) {
+    throw new StatusError(500, 'Owner URI vs Parent URI mismatch: ' + ownerUri + ' vs. ' + parentRoom.uri);
+  }
 
-      if (ownerUri !== parentRoom.uri) {
-        throw new StatusError(500, 'Owner URI vs Parent URI mismatch: ' + ownerUri + ' vs. ' + parentRoom.uri);
-      }
+  switch(security) {
+    case 'PUBLIC':
+      return {
+        type: 'GH_REPO',
+        members: 'PUBLIC',
+        admins: 'GH_REPO_PUSH',
+        public: true,
+        linkPath: ownerUri,
+        externalId: githubId
+      };
 
-      switch(security) {
+    case 'PRIVATE':
+      return {
+        type: 'GH_REPO',
+        members: 'INVITE',
+        admins: 'GH_REPO_PUSH',
+        public: false,
+        linkPath: ownerUri,
+        externalId: githubId
+      };
+
+    case 'INHERITED':
+      switch(parentRoom.security) {
         case 'PUBLIC':
           return {
             type: 'GH_REPO',
@@ -113,140 +106,108 @@ function generateRepoChannelPermissionsForRoom(room) {
         case 'PRIVATE':
           return {
             type: 'GH_REPO',
-            members: 'INVITE',
+            members: 'GH_REPO_ACCESS',
             admins: 'GH_REPO_PUSH',
             public: false,
             linkPath: ownerUri,
             externalId: githubId
           };
 
-        case 'INHERITED':
-          switch(parentRoom.security) {
-            case 'PUBLIC':
-              return {
-                type: 'GH_REPO',
-                members: 'PUBLIC',
-                admins: 'GH_REPO_PUSH',
-                public: true,
-                linkPath: ownerUri,
-                externalId: githubId
-              };
-
-            case 'PRIVATE':
-              return {
-                type: 'GH_REPO',
-                members: 'GH_REPO_ACCESS',
-                admins: 'GH_REPO_PUSH',
-                public: false,
-                linkPath: ownerUri,
-                externalId: githubId
-              };
-
-            default:
-              throw new StatusError(500, 'Unknown parent room security type: ' + security);
-          }
-          break;
-
         default:
-          throw new StatusError(500, 'Unknown security type: ' + security);
+          throw new StatusError(500, 'Unknown parent room security type: ' + security);
       }
+      break;
 
-    });
+    default:
+      throw new StatusError(500, 'Unknown security type: ' + security);
+  }
 }
 
-function generateOrgChannelPermissionsForRoom(room) {
+function generateOrgChannelPermissionsForRoom(room, parentRoom) {
   var uri = room.uri;
-  var ownerUri = uri.split(/\//).slice(0, 2).join('/');
+  var ownerUri = uri.split(/\//)[0]
+  var githubId = parentRoom.githubId;
+  var security = room.security;
 
-  return getParentRoom(room.parentId)
-    .then(function(parentRoom) {
-      var githubId = parentRoom.githubId;
-      var security = room.security;
+  if (ownerUri !== parentRoom.uri) {
+    throw new StatusError(500, 'Owner URI vs Parent URI mismatch: ' + ownerUri + ' vs. ' + parentRoom.uri);
+  }
 
-      if (ownerUri !== parentRoom.uri) {
-        throw new StatusError(500, 'Owner URI vs Parent URI mismatch: ' + ownerUri + ' vs. ' + parentRoom.uri);
-      }
+  switch(security) {
+    case 'PUBLIC':
+      return {
+        type: 'GH_ORG',
+        members: 'PUBLIC',
+        admins: 'GH_ORG_MEMBER',
+        public: true,
+        linkPath: ownerUri,
+        externalId: githubId
+      };
 
-      switch(security) {
-        case 'PUBLIC':
-          return {
-            type: 'GH_ORG',
-            members: 'PUBLIC',
-            admins: 'GH_ORG_MEMBER',
-            public: true,
-            linkPath: ownerUri,
-            externalId: githubId
-          };
+    case 'PRIVATE':
+      return {
+        type: 'GH_ORG',
+        members: 'INVITE',
+        admins: 'GH_ORG_MEMBER',
+        public: false,
+        linkPath: ownerUri,
+        externalId: githubId
+      };
 
-        case 'PRIVATE':
-          return {
-            type: 'GH_ORG',
-            members: 'INVITE',
-            admins: 'GH_ORG_MEMBER',
-            public: false,
-            linkPath: ownerUri,
-            externalId: githubId
-          };
+    case 'INHERITED':
+      return {
+        type: 'GH_ORG',
+        members: 'GH_ORG_MEMBER',
+        admins: 'GH_ORG_MEMBER',
+        public: false,
+        linkPath: ownerUri,
+        externalId: githubId
+      };
 
-        case 'INHERITED':
-          return {
-            type: 'GH_ORG',
-            members: 'GH_ORG_MEMBER',
-            admins: 'GH_ORG_MEMBER',
-            public: false,
-            linkPath: ownerUri,
-            externalId: githubId
-          };
+    default:
+      throw new StatusError(500, 'Unknown security type: ' + security);
+  }
 
-        default:
-          throw new StatusError(500, 'Unknown security type: ' + security);
-      }
-
-    });
 }
 
-function generateUserChannelPermissionsForRoom(room) {
+function generateUserChannelPermissionsForRoom(room, ownerUser) {
   var uri = room.uri;
-  var ownerUri = uri.split(/\//).slice(0, 2).join('/');
+  var ownerUri = uri.split(/\//)[0];
+  var security = room.security;
 
-  return getOwnerUser(room.ownerUserId)
-    .then(function(ownerUser) {
-      var security = room.security;
+  if (ownerUri !== ownerUser.username) {
+    throw new StatusError(500, 'Owner URI vs owner username mismatch: ' + ownerUri + ' vs. ' + ownerUser.username);
+  }
 
-      if (ownerUri !== ownerUser.username) {
-        throw new StatusError(500, 'Owner URI vs owner username mismatch: ' + ownerUri + ' vs. ' + ownerUser.username);
-      }
+  switch(security) {
+    case 'PUBLIC':
+      return {
+        type: 'NONE',
+        members: 'PUBLIC',
+        admins: 'MANUAL',
+        public: true,
+        extraAdmins: [ownerUser.id]
+      };
 
-      switch(security) {
-        case 'PUBLIC':
-          return {
-            type: 'NONE',
-            members: 'PUBLIC',
-            admins: 'MANUAL',
-            public: true,
-            extraAdmins: [ownerUser.id]
-          };
+    case 'PRIVATE':
+      return {
+        type: 'NONE',
+        members: 'INVITE',
+        admins: 'MANUAL',
+        public: false,
+        extraAdmins: [ownerUser.id]
+      };
 
-        case 'PRIVATE':
-          return {
-            type: 'NONE',
-            members: 'INVITE',
-            admins: 'MANUAL',
-            public: false,
-            extraAdmins: [ownerUser.id]
-          };
-
-        default:
-          throw new StatusError(500, 'Unknown security type: ' + security);
-      }
-    });
+    default:
+      throw new StatusError(500, 'Unknown security type: ' + security);
+  }
 }
 
 
 /**
  * Given a room, returns a `permissions` object
  */
-function generatePermissionsForRoom(room) {
+function generatePermissionsForRoom(room, parentRoom, ownerUser) {
   var oneToOne = room.oneToOne;
   var githubType = room.githubType;
 
@@ -264,20 +225,35 @@ function generatePermissionsForRoom(room) {
   switch(githubType) {
     case 'ONETOONE':
       return generateOneToOnePermissionsForRoom(room);
+
     case 'REPO':
       return generateRepoPermissionsForRoom(room);
+
     case 'ORG':
       return generateOrgPermissionsForRoom(room);
+
     case 'REPO_CHANNEL':
-      return generateRepoChannelPermissionsForRoom(room);
+      if (!parentRoom) {
+        throw new StatusError(500, 'Parent room not found: ' + room.uri);
+      }
+
+      return generateRepoChannelPermissionsForRoom(room, parentRoom);
     case 'ORG_CHANNEL':
-      return generateOrgChannelPermissionsForRoom(room);
+      if (!parentRoom) {
+        throw new StatusError(500, 'Parent room not found: ' + room.uri);
+      }
+      return generateOrgChannelPermissionsForRoom(room, parentRoom);
+
     case 'USER_CHANNEL':
-      return generateUserChannelPermissionsForRoom(room);
+      if (!ownerUser) {
+        throw new StatusError(500, 'Owner not found: ' + room.uri);
+      }
+      return generateUserChannelPermissionsForRoom(room, ownerUser);
+
     default:
       throw new Error(500, 'Unknown room type ' + githubType);
   }
 }
 
 
-exports.generatePermissionsForRoom = Promise.method(generatePermissionsForRoom);
+exports.generatePermissionsForRoom = generatePermissionsForRoom;
