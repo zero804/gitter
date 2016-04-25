6bf0477837c591b0b2b4d5f7db38dac2d0b83578
Andrew Newdigate <andrew@troupe.co>
2016-04-25 12:36:29 +0100

    Comment about using underscore `_.intersection`

--- server/services/room-permissions-model.js
+++ server/services/room-permissions-model.js
@@ -1,6 +1,5 @@
 "use strict";
 
-var _ = require('lodash');
 var Promise = require('bluebird');
 var permissionsModel = require('./permissions-model');
 var identityService = require("./identity-service");
