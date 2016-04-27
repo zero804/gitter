14d53d1fa9c5971cd720a16d540cefa5efa9c6ad
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-04-27 11:26:53 +0100

    fix server rendering to show all rooms but with hidden classes

--- shared/rooms/left-menu-room-list.js
+++ shared/rooms/left-menu-room-list.js
@@ -6,34 +6,35 @@ var one2oneFilter   = require('../filters/left-menu-primary-one2one');
 var orgFilter       = require('../filters/left-menu-primary-org');
 
 var defaultSort   = require('../sorting/left-menu-primary-default');
-var favouriteSort = require('../sorting/left-menu-primary-favourite');
 
 var parseToTemplateItem = require('../parse/left-menu-primary-item');
 
 module.exports = function generateLeftMenuRoomsList(state, rooms, selectedOrgName){
+
+  var filter;
   switch(state) {
     case 'search':
-      return [];
-    case 'favourite':
-      return rooms
-        .filter(favouriteFilter).sort(favouriteSort)
-        .map(function(room) {
-          return parseToTemplateItem(room, state);
-        });
+      filter = function(){ return false; };
+      break;
     case 'people':
-      return rooms
-        .filter(one2oneFilter).sort(defaultSort)
-        .map(function(room) {
-          return parseToTemplateItem(room, state);
-        });
+      filter = one2oneFilter;
+      break;
     case 'org':
-      return rooms
-        .filter(function(model){ return orgFilter(model, selectedOrgName); })
-        .sort(defaultSort)
-        .map(function(room) {
-          return parseToTemplateItem(room, state);
-        });
+      filter = function(model) { return orgFilter(model, selectedOrgName) };
+      break;
     default:
-      return rooms.filter(defaultFilter).sort(defaultSort).map(parseToTemplateItem);
+      filter = favouriteFilter;
   }
+
+  return rooms
+  .filter(defaultFilter)
+  .map(function(model){
+    if(!filter(model)) {
+      if(model.get) { model.set('isHidden', true); }
+      model.isHidden = true;
+    }
+    return parseToTemplateItem(model, state);
+  })
+  .sort(defaultSort);
+
 };
