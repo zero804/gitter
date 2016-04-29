69967b41304f1598e38fc94159259480275232fb
Eric Eastwood <contact@ericeastwood.com>
2016-04-29 07:01:18 -0500

    Add source tracking suggestion room-item to js click navigation

--- public/js/views/menu/room/base-collection/base-collection-view.js
+++ public/js/views/menu/room/base-collection/base-collection-view.js
@@ -5,6 +5,7 @@ var fastdom     = require('fastdom');
 var template    = require('./base-collection-view.hbs');
 var context     = require('utils/context');
 var toggleClass = require('utils/toggle-class');
+var parseItemForTemplate = require('gitter-web-shared/parse/left-menu-primary-item');
 
 module.exports = Marionette.CompositeView.extend({
 
@@ -57,13 +58,12 @@ module.exports = Marionette.CompositeView.extend({
     Marionette.CompositeView.prototype.constructor.apply(this, arguments);
   },
 
+
+
   onItemClicked: function(view) {
     var model = view.model;
-    var name = (model.get('uri') ||
-                model.get('url') ||
-                model.get('name') ||
-                (model.get('fromUser') && model.get('fromUser').username));
-    var url  = (name[0] !== '/') ?  '/' + name : name;
+    var url = view.getRoomUrl();
+    var name = view.getRoomName();
 
     //We have to explicitly check for false because search
     //results come through with `exists: false` for rooms yet to be created
