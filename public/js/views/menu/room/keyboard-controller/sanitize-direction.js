8e67c28729d6d13ed1fc62ef78459f33694411f4
Eric Eastwood <contact@ericeastwood.com>
2016-04-28 21:55:38 -0500

    Start splitting off keyboard controller logic into testable chunks

---
+++ public/js/views/menu/room/keyboard-controller/sanitize-direction.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Transforms 1/-1 or true/false
+// into 1 or -1
+var sanitizeDir = function(dir) {
+  return (dir === false) ? -1 : Math.sign(dir || 1);
+};
+
+module.exports = sanitizeDir;
