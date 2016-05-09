48c64fd6c1b74174012e8baad45463148bbee778
Le Roux Bodenstein <leroux@gitter.im>
2016-05-09 17:27:46 +0200

    YES, NO, MAYBE for group migration.

--- scripts/migration/upsert-missing-groups.js
+++ scripts/migration/upsert-missing-groups.js
@@ -6,6 +6,7 @@ var persistence = require('gitter-web-persistence');
 var mongooseUtils = require('gitter-web-persistence-utils/lib/mongoose-utils');
 var onMongoConnect = require('../../server/utils/on-mongo-connect');
 var through2Concurrent = require('through2-concurrent');
+var orgMap = require('./org-map.json');
 
 
 function getGroupableRooms() {
@@ -30,7 +31,26 @@ function getGroupableRooms() {
           _id: '$lcOwner',
           rooms: { $push: '$$CURRENT' }
         }
+      }, {
+        $lookup: {
+          // NOTE: I'm assuming that troupe.lcUri is maintained and correct
+          from: "troupes",
+          localField: "_id",
+          foreignField: "lcUri",
+          as: "orgRoom"
+        }
+      }, {
+        // NOTE: comparing lcOwner (lowercased) with username (mixed case). Not
+        // sure of how to find owner (mixed case) at this stage because there
+        // is no troupe.owner unless we calculate it somehow.
+        $lookup: {
+          from: "users",
+          localField: "_id",
+          foreignField: "username",
+          as: "user"
+        }
       }
+      // TODO: project orgRoom & user for efficiency
     ])
     .read('secondaryPreferred')
     .cursor({ batchSize: 1000 })
@@ -46,14 +66,44 @@ function log(batch, enc, callback) {
   callback();
 }
 
+var lookups = [];
 function migrate(batch, enc, callback) {
   var lcOwner = batch._id;
 
   // Fish the owner out of the first room's uri. This is the case-sensitive
   // version used for name and uri.
   var owner = batch.rooms[0].uri.split('/')[0];
-  console.log(owner, batch.rooms.length);
 
+  var hasOrgRoom = !!batch.orgRoom.length;
+  var hasUser = !!batch.user.length;
+  var isDefinitelyOrg = !!orgMap[lcOwner];
+  var result;
+  if (hasOrgRoom || isDefinitelyOrg) {
+    result = "YES";
+  } else if (hasUser) {
+    result = "NO";
+  } else {
+    // TODO: maybe here we can somehow lookup owner in users again so we can do
+    // a case-sensitive match?
+    result = "MAYBE";
+    lookups.push(owner);
+  }
+
+  console.log(
+    owner,
+    batch.rooms.length+' rooms',
+    'hasOrgRoom:'+hasOrgRoom,
+    'hasUser:'+hasUser,
+    'isDefinitelyOrg:'+isDefinitelyOrg,
+    result
+  );
+
+  callback();
+
+  // NOTE: disabling the actual upserting for now while I figure out how to
+  // calculate if we need a group
+
+  /*
   // upsert the lcOwner into group
   var query = { lcUri: lcOwner };
   return mongooseUtils.upsert(persistence.Group, query, {
@@ -83,32 +133,46 @@ function migrate(batch, enc, callback) {
         .exec();
     })
     .nodeify(callback);
+  */
 }
 
-function run(f) {
+function run(f, callback) {
   getGroupableRooms()
     .pipe(through2Concurrent.obj({maxConcurrency: 10}, f))
     .on('data', function(batch) {
     })
     .on('end', function() {
       console.log('done!');
-      return shutdown.shutdownGracefully();
+      callback();
     })
     .on('error', function(error) {
-      console.error(error);
-      console.error(error.stack);
-      process.exit(1);
-    });
+      callback(err);
+    })
+}
+
+function done(err) {
+  if (err) {
+    console.error(error);
+    console.error(error.stack);
+    process.exit(1);
+  } else {
+    shutdown.shutdownGracefully();
+  }
 }
 
 onMongoConnect()
   .then(function() {
     require('yargs')
       .command('dry-run', 'Dry run', { }, function() {
-        run(log);
+        run(log, done);
       })
       .command('execute', 'Execute', { }, function() {
-        run(migrate);
+        run(migrate, function(err) {
+          console.log("numLookups: "+lookups.length);
+          console.log('==========')
+          console.log(lookups);
+          done(err);
+        });
       })
       .demand(1)
       .strict()
