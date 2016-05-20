308c75480b738d7b9ae327086b693b2fc79533cf
Le Roux Bodenstein <leroux@gitter.im>
2016-05-20 13:30:10 +0200

    make it all use yargs

--- scripts/migration/report-to-userupdate-csv.js
+++ scripts/migration/report-to-userupdate-csv.js
@@ -6,8 +6,22 @@ var JSONStream = require('JSONStream');
 var es = require('event-stream');
 var fs = require('fs');
 var csv = require('fast-csv')
+
+var opts = require('yargs')
+  .option('input', {
+    required: true,
+    description: 'where to find the json report file'
+  })
+  .option('output', {
+    required: true,
+    description: 'where to write the csv file'
+  })
+  .help('help')
+  .alias('help', 'h')
+  .argv;
+
 var csvStream = csv.createWriteStream({headers: true})
-var writableStream = fs.createWriteStream("user-updates.csv");
+var writableStream = fs.createWriteStream(opts.output);
 
 var t = es.through(function write(data) {
   data.forEach(function(update) {
@@ -17,7 +31,7 @@ var t = es.through(function write(data) {
   }, this)
 });
 
-fs.createReadStream('/Users/leroux/owner-report.json')
+fs.createReadStream(opts.input)
   .pipe(JSONStream.parse('updates'))
   .pipe(t)
   .pipe(csvStream)
