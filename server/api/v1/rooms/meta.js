1475b6089d752335a9df96a1cf9d22e0fad47e09
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-05-25 11:50:06 +0100

    remove uneeded funciton

--- server/api/v1/rooms/meta.js
+++ server/api/v1/rooms/meta.js
@@ -3,10 +3,6 @@ var metaService = require('../../../services/room-meta-service');
 var RoomWithPolicyService = require('../../../services/room-with-policy-service');
 var troupeService = require('../../../services/troupe-service');
 
-function allowAdmin() {
-  return this.policy.canAdmin();
-}
-
 module.exports = {
   id: 'roomMetaKey',
 
