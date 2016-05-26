db3c946cdeac92fb26acc02438544479d216ed68
Andrew Newdigate <andrew@troupe.co>
2016-05-26 10:41:34 +0100

    Added `/v1/user/:userId/groups` endpoint

--- test/api-tests/user-group-api-tests.js
+++ test/api-tests/user-group-api-tests.js
@@ -6,7 +6,7 @@ var Promise = require('bluebird');
 var fixtureLoader = require('../integration/test-fixtures');
 var assert = require('assert');
 
-describe('group-api', function() {
+describe('user-group-api', function() {
   this.timeout(10000);
 
   var app, request;
@@ -28,57 +28,44 @@ describe('group-api', function() {
     },
     group1: {
     },
+    group2: {
+    },
     troupe1: {
       security: 'PUBLIC',
-      group: 'group1'
+      group: 'group1',
+      users: ['user1']
     },
     troupe2: {
       security: 'PRIVATE',
-      group: 'group1'
+      group: 'group1',
+      users: ['user1']
     },
     troupe3: {
       /* Security is undefined */
-      group: 'group1'
+      group: 'group2',
+      users: ['user1']
     }
   });
 
-  it('GET /v1/groups', function() {
+  it('GET /v1/user/:userId/groups', function() {
     return request(app)
       .get('/v1/groups')
       .set('x-access-token', fixture.user1.accessToken)
       .expect(200)
-  });
-
-  it('POST /v1/groups', function() {
-    return request(app)
-      .post('/v1/groups')
-      .send({ uri: fixtureLoader.GITTER_INTEGRATION_ORG, name: 'Test' })
-      .set('x-access-token', fixture.user1.accessToken)
-      .expect(200)
-  });
-
-  it('GET /v1/groups/:groupId/rooms', function() {
-    return request(app)
-      .get('/v1/groups/' + fixture.group1.id + '/rooms')
-      .set('x-access-token', fixture.user1.accessToken)
-      .expect(200)
       .then(function(result) {
-        var rooms = result.body;
+        var groups = result.body;
 
-        assert(rooms.some(function(r) {
-          return r.id === fixture.troupe1.id;
+        assert(groups.some(function(r) {
+          return r.id === fixture.group1.id;
         }));
 
-        assert(rooms.every(function(r) {
-          return r.id !== fixture.troupe2.id;
+        assert(groups.some(function(r) {
+          return r.id === fixture.group2.id;
         }));
 
-        assert(rooms.every(function(r) {
-          return r.id !== fixture.troupe3.id;
-        }));
 
-        assert.strictEqual(result.body.length, 1);
-      })
+        assert.strictEqual(result.body.length, 2);
+      });
   });
 
 
