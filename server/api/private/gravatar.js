25900aadb67709cffbc8f8a5a06742446ec712e8
Eric Eastwood <contact@ericeastwood.com>
2016-05-26 17:57:01 -0500

    Add image accept type

--- server/api/private/gravatar.js
+++ server/api/private/gravatar.js
@@ -4,24 +4,31 @@ var gravatar = require('../../utils/gravatar');
 
 var DEFAULT_SIZE = 64;
 
+var addSizeToUrl = function(url, size) {
+  return url + (size ? '?s=' + size : '');
+};
+
 var resolveGravatarForEmail = function(req, res) {
   var email = req.params.email;
   var size = req.query.s || DEFAULT_SIZE;
   var gravatarUrl = gravatar(email);
 
-  switch(req.accepts(['json', 'text'])) {
+  switch(req.accepts(['json', 'text', 'image/*'])) {
+    case 'image/*':
+      res.redirect(addSizeToUrl(gravatarUrl, size));
+      break;
     case 'json':
       res.send({
         avatarSrcSet: {
-          src: gravatarUrl + (size ? '?s=' + size : ''),
+          src: addSizeToUrl(gravatarUrl, size),
           size: size,
-          srcset: gravatarUrl + (size ? '?s=' + (size * 2) : '') + ' 2x',
+          srcset: addSizeToUrl(gravatarUrl, size * 2)
         }
       });
       break;
 
     default:
-      res.send(gravatarUrl + (size ? '?s=' + size : ''));
+      res.send(addSizeToUrl(gravatarUrl, size));
       break;
   }
 
