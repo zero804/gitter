fc2d3442ee8d2a2a5dbdc82778cd05baa539fb38
Andrew Newdigate <andrew@troupe.co>
2016-05-27 12:02:40 +0100

    Move app tag stuff out of render

---
+++ server/web/app-tag.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var env = require('gitter-web-env');
+var nconf = env.config;
+var appVersion = require('gitter-app-version');
+
+var stagingText, stagingLink;
+
+/* App tag */
+var staging = nconf.get('STAGING');
+switch(nconf.get('NODE_ENV')) {
+  case 'prod':
+    if(staging) {
+      stagingText = 'NEXT';
+      stagingLink = 'http://next.gitter.im';
+    }
+    break;
+  case 'beta':
+    var branch = appVersion.getBranch();
+    stagingText = branch ? branch.split('/').pop() : 'BETA';
+    stagingLink = appVersion.getGithubLink();
+    break;
+  case 'dev':
+    stagingText = 'DEV';
+    stagingLink = 'https://github.com/troupe/gitter-webapp/tree/develop';
+    break;
+}
+
+module.exports = {
+  text: stagingText,
+  stagingLink: stagingLink
+}
