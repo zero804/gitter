8b94ee2494d7cf20bd826a2b458f66ed71e6d649
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-05-27 11:15:50 +0100

    add test single run

--- test/in-browser/fixtures/runner.js
+++ test/in-browser/fixtures/runner.js
@@ -1,16 +1,13 @@
-'use strict';
+var Mocha = require('mocha');
+var path = require('path');
 
-var __karmaWebpackManifest__ = [];
-var testsContext = require.context('../specs', true, /\.js$/);
 
-function inManifest(path) {
-  return __karmaWebpackManifest__.indexOf(path) >= 0;
-}
+var mocha = new Mocha();
+mocha.addFile(path.resolve(__dirname, './build/test.js'));
 
-var runnable = testsContext.keys().filter(inManifest);
-
-if (!runnable.length) {
-  runnable = testsContext.keys();
-}
-
-runnable.forEach(testsContext);
+// Run the tests.
+mocha.run(function(failures){
+  process.on('exit', function () {
+    process.exit(failures);  // exit with non-zero status if there were failures
+  });
+});
