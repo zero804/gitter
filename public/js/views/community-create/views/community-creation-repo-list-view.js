d276c5c4ffdcc60fe06e54680ee82a621f6a2a4d
Eric Eastwood <contact@ericeastwood.com>
2016-05-30 22:53:36 -0500

    Defer repo collection fetch until you click on the tab

--- public/js/views/community-create/views/community-creation-repo-list-view.js
+++ public/js/views/community-create/views/community-creation-repo-list-view.js
@@ -1,17 +1,50 @@
 'use strict';
 
 var Marionette = require('backbone.marionette');
+var toggleClass = require('utils/toggle-class');
 
 var CommunityCreationRepoListTemplate = require('./community-creation-repo-list-view.hbs');
+var CommunityCreationRepoListEmptyTemplate = require('./community-creation-repo-list-empty-view.hbs');
 var CommunityCreationRepoListItemView = require('./community-creation-repo-list-item-view');
 
+
+var CommunityCreationRepoListEmptyView = Marionette.ItemView.extend({
+  template: CommunityCreationRepoListEmptyTemplate,
+  ui: {
+    emptyNote: '.community-create-repo-list-empty-note__text-empty',
+    loadingNote: '.community-create-repo-list-empty-note__text-loading'
+  },
+  initialize: function() {
+    this.listenTo(this.collection, 'request', this.onCollectionFetch.bind(this), this);
+    this.listenTo(this.collection, 'reset sync snapshot', this.onCollectionDoneLoading.bind(this), this);
+  },
+
+  onCollectionFetch: function() {
+    this.ui.emptyNote[0].classList.add('hidden');
+    this.ui.loadingNote[0].classList.remove('hidden');
+  },
+
+  onCollectionDoneLoading: function() {
+    this.ui.emptyNote[0].classList.remove('hidden');
+    this.ui.loadingNote[0].classList.add('hidden');
+  }
+});
 
 var CommunityCreationRepoListView = Marionette.CompositeView.extend({
   template: CommunityCreationRepoListTemplate,
   childView: CommunityCreationRepoListItemView,
+  emptyView: CommunityCreationRepoListEmptyView,
+  emptyViewOptions: function() {
+    return {
+      collection: this.collection
+    };
+  },
   childViewContainer: '.community-create-repo-list',
   childEvents: {
     'item:activated': 'onItemActivated'
+  },
+
+  initialize: function() {
   },
 
   onItemActivated: function(view) {
