373a09159e7953de3fda2f88dbe28195c71798de
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-06-01 23:00:14 +0100

    add js based font loading

--- public/js/utils/initial-setup.js
+++ public/js/utils/initial-setup.js
@@ -2,9 +2,18 @@
 
 var Backbone = require('backbone');
 var Marionette = require('backbone.marionette');
+var FontFaceObserver = require('fontfaceobserver');
 
 module.exports = (function () {
   if (window.__agent) {
     window.__agent.start(Backbone, Marionette);
   }
+
+  var font = new FontFaceObserver('source-sans-pro');
+
+  font.load().then(function () {
+    document.body.classList.add('fonts-loaded');
+  });
+
 })();
+
