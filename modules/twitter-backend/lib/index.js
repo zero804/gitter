095b7af71112d1e4792caa82464e177d1b8d72c4
Eric Eastwood <contact@ericeastwood.com>
2016-06-01 21:39:12 -0500

    Add Twitter suggestions from followers

--- modules/twitter-backend/lib/index.js
+++ modules/twitter-backend/lib/index.js
@@ -1,6 +1,10 @@
 'use strict';
 
 var Promise = require('bluebird');
+var request = Promise.promisify(require('request'));
+
+var env = require('gitter-web-env');
+var config = env.config;
 
 function TwitterBackend(user, identity) {
   this.user = user;
@@ -17,6 +21,28 @@ TwitterBackend.prototype.findOrgs = Promise.method(function() {
 
 TwitterBackend.prototype.getProfile = Promise.method(function() {
   return { provider: 'twitter' };
+});
+
+TwitterBackend.prototype.getFollowers = Promise.method(function() {
+  var followerApiUrl = 'https://api.twitter.com/1.1/followers/list.json';
+  var username = this.user.username.replace(/_twitter$/, '');
+  var twitterRequest = request({
+    url: followerApiUrl,
+    json: true,
+    oauth: {
+      consumer_key: config.get('twitteroauth:consumer_key'),
+      consumer_secret: config.get('twitteroauth:consumer_secret'),
+      token: this.identity.accessToken,
+      token_secret: this.identity.accessTokenSecret
+    },
+    qs: {
+      screen_name: username
+    }
+  });
+
+  return twitterRequest.then(function(result) {
+    return result.body && result.body.users;
+  });
 });
 
 module.exports = TwitterBackend;
