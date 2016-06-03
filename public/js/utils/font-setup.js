7d2ebaa27893ffdf12ae9f6aab1f9f466a095b4d
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-06-03 11:22:26 +0100

    add comment

--- public/js/utils/font-setup.js
+++ public/js/utils/font-setup.js
@@ -13,6 +13,7 @@ font.load().then(function () {
     document.documentElement.className += " fonts-loaded";
     //Store a cookie to say the fonts have been loaded and should be cached
     var now = new Date();
+    //TODO this is set 11months from now but should follow our caching rules
     now.setMonth(now.getMonth() + 11);
     cookie.set('webfontsLoaded', true, { expires: now.toGMTString() });
   });
