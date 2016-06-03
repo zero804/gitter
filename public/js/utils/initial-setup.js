0ca584c595d0a33a37365544400e99174b053550
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-06-03 11:04:50 +0100

    add fonts to chat frame & try to limit reflows on font load

--- public/js/utils/initial-setup.js
+++ public/js/utils/initial-setup.js
@@ -3,17 +3,22 @@
 var Backbone = require('backbone');
 var Marionette = require('backbone.marionette');
 var FontFaceObserver = require('fontfaceobserver');
+var RAF = require('utils/raf');
 
 module.exports = (function () {
   if (window.__agent) {
     window.__agent.start(Backbone, Marionette);
   }
 
-  var font = new FontFaceObserver('source-sans-pro');
+  //We only want to observer events for the default font
+  var font = new FontFaceObserver('source-sans-pro', {
+    weight: 'normal',
+  });
 
   font.load().then(function () {
-    document.body.classList.add('fonts-loaded');
+    RAF(function(){
+      document.documentElement.className += " fonts-loaded";
+    });
   });
 
 })();
-
