c21c9d165ad15bf361e540c24e35c9182f8f2a5d
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-06-03 11:21:28 +0100

    abstract font setup andd add cookie checks to avoid flashed

--- public/js/utils/initial-setup.js
+++ public/js/utils/initial-setup.js
@@ -2,23 +2,9 @@
 
 var Backbone = require('backbone');
 var Marionette = require('backbone.marionette');
-var FontFaceObserver = require('fontfaceobserver');
-var RAF = require('utils/raf');
 
 module.exports = (function () {
   if (window.__agent) {
     window.__agent.start(Backbone, Marionette);
   }
-
-  //We only want to observer events for the default font
-  var font = new FontFaceObserver('source-sans-pro', {
-    weight: 'normal',
-  });
-
-  font.load().then(function () {
-    RAF(function(){
-      document.documentElement.className += " fonts-loaded";
-    });
-  });
-
 })();
