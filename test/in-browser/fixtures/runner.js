5afabb9e02edd0d62a6bb0f2391d929e679cc2a0
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-06-03 13:06:16 +0100

    added error handle so you can view errors when watching tests

--- test/in-browser/fixtures/runner.js
+++ test/in-browser/fixtures/runner.js
@@ -2,8 +2,14 @@ var Mocha = require('mocha');
 var path = require('path');
 
 
-var mocha = new Mocha();
+var mocha = new Mocha({ useColors: true });
 mocha.addFile(path.resolve(__dirname, './build/test.js'));
+
+window.onerror = function(message){
+  console.log('-----------------------');
+  console.log(message);
+  console.log('-----------------------');
+};
 
 // Run the tests.
 mocha.run(function(failures){
