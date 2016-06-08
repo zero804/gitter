0315880ad2964369bf7d072d891f3b0492eabdb1
Andrew Newdigate <andrew@troupe.co>
2016-06-08 16:36:49 -0700

    🚚 Split github policy factory from legacy policy factory

--- modules/permissions/lib/legacy-policy-factory.js
+++ modules/permissions/lib/legacy-policy-factory.js
@@ -3,7 +3,6 @@
 var Promise = require('bluebird');
 var LegacyPolicyEvaluator = require('./policies/legacy-policy-evaluator');
 var LegacyGitHubPolicyEvaluator = require('./policies/legacy-github-policy-evaluator');
-var LegacyGroupPolicyEvaluator = require('./policies/legacy-group-policy-evaluator');
 
 function createPolicyForRoom(user, room) {
   var userId = user && user._id;
@@ -23,34 +22,14 @@ function createPolicyForUserIdInRoom(userId, room) {
   return new LegacyPolicyEvaluator(userId, null, room._id, room);
 }
 
-function createPolicyForGithubObject(user, uri, ghType, security) {
-  return new LegacyGitHubPolicyEvaluator(user, uri, ghType, security);
-}
-
 function createPolicyForOneToOne(user, toUser) {
   return new LegacyGitHubPolicyEvaluator(user, toUser.username, 'ONETOONE', null);
 }
-
-function createGroupPolicyForGithubObject(user, type, uri, githubId, obtainAccessFromGitHubRepo) {
-  var userId = user && user._id;
-  return new LegacyGroupPolicyEvaluator(userId, user, type, uri, githubId, obtainAccessFromGitHubRepo)
-}
-/**
- * NB: obtainAccessFromGitHubRepo is a stop-gap until we have proper
- * user-group permissions
- */
-// function createPolicyForGroup(user, group, obtainAccessFromGitHubRepo) {
-//   var userId = user && user._id;
-//   return new LegacyGroupPolicyEvaluator(userId, user, group._id, group, obtainAccessFromGitHubRepo);
-// }
 
 module.exports = {
   createPolicyForRoom: Promise.method(createPolicyForRoom),
   createPolicyForRoomId: Promise.method(createPolicyForRoomId),
   createPolicyForUserIdInRoomId: Promise.method(createPolicyForUserIdInRoomId),
   createPolicyForUserIdInRoom: Promise.method(createPolicyForUserIdInRoom),
-  createPolicyForGithubObject: Promise.method(createPolicyForGithubObject),
   createPolicyForOneToOne: Promise.method(createPolicyForOneToOne),
-  createGroupPolicyForGithubObject: Promise.method(createGroupPolicyForGithubObject),
-  // createPolicyForGroup: Promise.method(createPolicyForGroup)
 };
