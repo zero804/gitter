574c330f96189a074d4fa37acc5a321adc95dd24
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-06-15 13:53:51 +0100

    remove keyboard stuff from minibar

--- public/js/views/menu/room/minibar/minibar-view.js
+++ public/js/views/menu/room/minibar/minibar-view.js
@@ -16,7 +16,6 @@ var MinibarView = Marionette.CollectionView.extend({
   id:        'minibar-list',
   childView: ItemView,
   childEvents: {
-    'minibar-item:keyboard-activated': 'onItemKeyboardActivated',
     'minibar-item:activated': 'onItemActivated',
     'minibar-item:close':   'onCloseClicked',
   },
@@ -80,16 +79,6 @@ var MinibarView = Marionette.CollectionView.extend({
     this.listenTo(this.model, 'change:state change:selectedOrgName', this.onMenuStateUpdate, this);
     this.onMenuStateUpdate();
 
-    this.keyboardControllerView.inject(this.keyboardControllerView.constants.MINIBAR_KEY, [
-      { collection: this.collection }
-    ]);
-
-    // For the normal arrowing through
-    // when we have too many rooms to render instantly
-    this.debouncedOnItemActivated = _.debounce(this.onItemActivated, 300);
-    // Just for the quick flicks
-    this.shortDebouncedOnItemActivated = _.debounce(this.onItemActivated, 100);
-
     //Guard against not getting a snapshot
     this.timeout = setTimeout(function() {
       this.onCollectionSnapshot();
@@ -140,19 +129,6 @@ var MinibarView = Marionette.CollectionView.extend({
         selectedOrgName:      modelName,
         activationSourceType: activationSourceType
       });
-    }
-  },
-
-  onItemKeyboardActivated: function(view, model) {
-    // Arbitrary threshold based on when we can't render "instantly".
-    // We don't want to delay too much because majority of users won't run into render shortcomings
-    // and the delay could cause confusion for screen-reader users not recnogizing when
-    // the switch happens
-    if(this.roomCollection.length > 50) {
-      this.debouncedOnItemActivated(view, model, 'keyboard');
-    }
-    else {
-      this.shortDebouncedOnItemActivated(view, model, 'keyboard');
     }
   },
 
