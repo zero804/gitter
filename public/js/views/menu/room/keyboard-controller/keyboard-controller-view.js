432ff8011765c43dfac71dda3444694334ae6b4d
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-06-16 12:52:33 +0100

    setup initial event handle

--- public/js/views/menu/room/keyboard-controller/keyboard-controller-view.js
+++ public/js/views/menu/room/keyboard-controller/keyboard-controller-view.js
@@ -1,8 +1,19 @@
 'use strict';
 
 var Marionette = require('backbone.marionette');
+var cocktail = require('cocktail');
+var KeyboardEventMixin = require('views/keyboard-events-mixin');
 
-module.exports = Marionette.ItemView.extend({
-  initialize: function() {
+var KeyboardController = Marionette.ItemView.extend({
+
+  keyboardEvents: {
+    'room.1': 'onMinibarItemSelected'
   },
+
+  onMinibarItemSelected: function (){
+  },
+
 });
+
+cocktail.mixin(KeyboardController, KeyboardEventMixin);
+module.exports = KeyboardController;
--- test/in-browser/specs/views/menu/room/keyboard-controller/kayboard-controller-test.js
+++ test/in-browser/specs/views/menu/room/keyboard-controller/kayboard-controller-test.js
@@ -3,6 +3,7 @@
 var assert = require('assert');
 var Backbone = require('backbone');
 var KeyboardControllerView = require('public/js/views/menu/room/keyboard-controller/keyboard-controller-view');
+var appEvents = require('utils/appevents');
 
 describe('KeyboardControllerView', function(){
 
@@ -11,11 +12,12 @@ describe('KeyboardControllerView', function(){
   beforeEach(function(){
     model = new Backbone.Model();
     view = new KeyboardControllerView({
-      model: model,
+      model: model
     });
   });
 
   it('should pass a test', function(){
+    appEvents.trigger('keyboard.room.1');
     assert(true);
   });
 
