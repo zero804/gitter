9f52542093f0d3238e00dabfda2c05658c661dca
Andrew Newdigate <andrew@troupe.co>
2016-06-20 20:05:40 +0100

    ✨ Feature: Invites with accept

--- modules/invites/test/invites-service-test.js
+++ modules/invites/test/invites-service-test.js
@@ -4,6 +4,7 @@ var invitesService = require('../lib/invites-service');
 var ObjectID = require('mongodb').ObjectID;
 var assert = require('assert');
 var StatusError = require('statuserror');
+var TroupeInvite = require('gitter-web-persistence').TroupeInvite;
 
 describe('invite-service', function() {
   describe('integration tests #slow', function() {
@@ -54,11 +55,58 @@ describe('invite-service', function() {
           })
           .catch(StatusError, function(err) {
             assert(err.status, 419);
-          })
+          });
       });
     });
 
-    describe('associateSecretWithUser', function() {});
+    describe('create-accept-complete flow', function() {
+      it('should accept an invite', function() {
+        var roomId = new ObjectID();
+        var invitedBy = new ObjectID();
+        var userId = new ObjectID();
+        var userId2 = new ObjectID();
+
+        return invitesService.createInvite(roomId, {
+            type: 'GITHUB',
+            externalId: 'gitterawesome',
+            invitedByUserId: invitedBy,
+            emailAddress: 'test@gitter.im'
+          })
+          .bind({
+            invite: null
+          })
+          .then(function(invite) {
+            this.invite = invite;
+            return invitesService.accept(userId, invite.secret);
+          })
+          .then(function(invite) {
+            assert.strictEqual(String(invite._id), String(this.invite._id));
+            return invitesService.markInviteAccepted(invite._id, userId);
+          })
+          .then(function() {
+            // Attempt to reuse the invite, same user
+            return invitesService.accept(userId, this.invite.secret);
+          })
+          .then(function(invite) {
+            assert.strictEqual(String(invite._id), String(this.invite._id));
+            return invitesService.accept(userId2, this.invite.secret)
+              .then(function() {
+                assert.ok(false, 'Expected exception');
+              })
+              .catch(StatusError, function(err) {
+                assert.strictEqual(err.status, 404);
+              })
+          })
+          .then(function() {
+            return TroupeInvite.findById(this.invite._id);
+          })
+          .then(function(invite) {
+            assert.strictEqual(invite.state, 'ACCEPTED');
+            assert.strictEqual(String(invite.userId), String(userId));
+          })
+      });
+
+    });
 
   });
 });
