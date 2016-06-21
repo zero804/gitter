bae2dc8c8dbe4633cefa5491a1fcdc60f498fc20
Andrew Newdigate <andrew@troupe.co>
2016-06-21 08:34:56 +0100

    💬 PR Review: removed IFFE as per @MadLittleMods review

--- modules/cdn/browser/cdn.js
+++ modules/cdn/browser/cdn.js
@@ -2,61 +2,55 @@
 
 var clientEnv = require('gitter-client-env');
 
-module.exports = (function() {
+var hosts = clientEnv['cdns'];
+var hostLength = hosts && hosts.length;
+var assetTag = clientEnv['assetTag'];
+var cdnPrefix = assetTag ? "/_s/" + assetTag : '';
 
+function cdnNativeApp(url) {
+  // nicest way of supporting embedded mobile chat
+  return '../' + url;
+}
 
-  var hosts = clientEnv['cdns'];
-  var hostLength = hosts && hosts.length;
-  var assetTag = clientEnv['assetTag'];
-  var cdnPrefix = assetTag ? "/_s/" + assetTag : '';
+function cdnPassthrough(url) {
+  return '/_s/l/' + url;
+}
 
-  function cdnNativeApp(url) {
-    // nicest way of supporting embedded mobile chat
-    return '../' + url;
+function cdnSingle(url, options) {
+  var nonrelative = options && options.nonrelative;
+
+  var prefix = nonrelative ? "https://" : "//";
+  if(options && options.notStatic === true) {
+    return prefix + hosts[0] + "/" + url;
   }
 
-  function cdnPassthrough(url) {
-    return '/_s/l/' + url;
+  return prefix + hosts[0] + cdnPrefix + "/" + url;
+}
+
+function cdnMulti(url, options) {
+  var x = 0;
+  for(var i = 0; i < url.length; i = i + 3) {
+    x = x + url.charCodeAt(i);
   }
 
-  function cdnSingle(url, options) {
-    var nonrelative = options && options.nonrelative;
+  var host = hosts[x % hostLength];
 
-    var prefix = nonrelative ? "https://" : "//";
-    if(options && options.notStatic === true) {
-      return prefix + hosts[0] + "/" + url;
-    }
+  var nonrelative = options && options.nonrelative;
+  var prefix = nonrelative ? "https://" : "//";
 
-    return prefix + hosts[0] + cdnPrefix + "/" + url;
+  if(options && options.notStatic === true) {
+    return prefix + host + "/" + url;
   }
 
-  function cdnMulti(url, options) {
-    var x = 0;
-    for(var i = 0; i < url.length; i = i + 3) {
-      x = x + url.charCodeAt(i);
-    }
+  return prefix + host + cdnPrefix + "/" + url;
+}
 
-    var host = hosts[x % hostLength];
-
-    var nonrelative = options && options.nonrelative;
-    var prefix = nonrelative ? "https://" : "//";
-
-    if(options && options.notStatic === true) {
-      return prefix + host + "/" + url;
-    }
-
-    return prefix + host + cdnPrefix + "/" + url;
-  }
-
-  if(window.location.protocol === 'file:') {
-    return cdnNativeApp;
-  } else if(!hostLength) {
-    return cdnPassthrough;
-  } else if(hostLength === 1) {
-    return cdnSingle;
-  } else {
-    return cdnMulti;
-  }
-
-
-})();
+if(window.location.protocol === 'file:') {
+  module.exports = cdnNativeApp;
+} else if(!hostLength) {
+  module.exports = cdnPassthrough;
+} else if(hostLength === 1) {
+  module.exports = cdnSingle;
+} else {
+  module.exports = cdnMulti;
+}
