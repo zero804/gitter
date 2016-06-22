8e4d3876d88daed468b09dffa400863c6e1ddcb1
Le Roux Bodenstein <leroux@gitter.im>
2016-06-22 16:45:42 +0200

    Allow users to create groups for GH org URIs if they have admin access.

--- server/api/private/check-group-uri.js
+++ server/api/private/check-group-uri.js
@@ -4,8 +4,15 @@ var groupUriChecker = require('../../services/group-uri-checker');
 
 function checkGroupUri(req, res, next) {
   return groupUriChecker(req.user, req.query.uri)
-    .then(function(exists) {
-      res.sendStatus(exists ? 409 : 200);
+    .then(function(info) {
+      if (info.allowCreate) {
+        res.send({
+          type: info.type,
+          allowCreate: info.allowCreate
+        });
+      } else {
+        res.sendStatus(409);
+      }
     })
     .catch(next);
 }
--- server/services/group-uri-checker.js
+++ server/services/group-uri-checker.js
@@ -5,6 +5,7 @@ var StatusError = require('statuserror');
 
 var userService = require('./user-service');
 var troupeService = require('./troupe-service');
+var legacyPolicyFactory = require('gitter-web-permissions/lib/legacy-policy-factory');
 var groupService = require('gitter-web-groups/lib/group-service');
 var validateGitHubUri = require('gitter-web-github').GitHubUriValidator;
 var validateGroupUri = require('gitter-web-validators/lib/validate-group-uri');
@@ -18,16 +19,34 @@ function checkLocalUri(uri) {
       groupService.findByUri(uri),
       troupeService.findByUri(uri),
       function(user, group, troupe) {
-        return user || group || troupe;
+        return !!(user || group || troupe);
       }
     );
 }
 
 function checkGitHubUri(user, uri) {
-  //gh orgs or users
+  // gh orgs or users
   return validateGitHubUri(user, uri)
     .then(function(githubInfo) {
-      return !!githubInfo;
+      if (githubInfo && githubInfo.type === 'ORG') {
+        // also check if you can actually admin the org.
+        return legacyPolicyFactory.createGroupPolicyForGithubObject(user, 'ORG', uri, githubInfo.githubId)
+          .then(function(policy) {
+            return policy.canAdmin();
+          })
+          .then(function(access) {
+            return {
+              githubInfo: githubInfo,
+              canAdmin: access
+            }
+          });
+      } else {
+        // either not found or not an org, so no reason to check permission
+        return {
+          githubInfo: githubInfo,
+          canAdmin: false // more like N/A
+        }
+      }
     });
 }
 
@@ -39,10 +58,33 @@ function checkIfGroupUriExists(user, uri) {
       return Promise.join(
         checkLocalUri(uri),
         checkGitHubUri(user, uri),
-        function(localUriExists, githubUriExists) {
-          return !!(localUriExists || githubUriExists);
+        function(localUriExists, info) {
+          var githubInfo = info.githubInfo;
+          var canAdminGitHubOrg = info.canAdmin;
+          var githubUriExists = !!githubInfo;
+
+          var allowCreate;
+          if (localUriExists) {
+            allowCreate = false;
+          } else if (githubUriExists) {
+            // If it is a github uri it must be an org and you have to have
+            // admin rights for you to be able to create a community for it.
+            allowCreate = (githubInfo.type === 'ORG') && canAdminGitHubOrg;
+          } else {
+            // if it doesn't exist locally AND it doesn't exist on github, then
+            // the user is definitely allowed to create it.
+            allowCreate = true;
+          }
+
+          return {
+            // The future group will either be org-based or of type null which
+            // is just the new types that aren't backed by GitHub.
+            type: (githubInfo && githubInfo.type === 'ORG') ? 'GH_ORG' : null,
+            allowCreate: allowCreate
+          };
         });
     });
+
 }
 
 module.exports = checkIfGroupUriExists;
