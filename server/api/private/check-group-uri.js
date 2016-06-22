febdac6747eb2acda450ab598defc39ebc16168d
Le Roux Bodenstein <leroux@gitter.im>
2016-06-22 11:34:06 +0200

    409, not 419

--- server/api/private/check-group-uri.js
+++ server/api/private/check-group-uri.js
@@ -5,7 +5,7 @@ var groupUriChecker = require('../../services/group-uri-checker');
 function checkGroupUri(req, res, next) {
   return groupUriChecker(req.user, req.query.uri)
     .then(function(exists) {
-      res.sendStatus(exists ? 419 : 200);
+      res.sendStatus(exists ? 409 : 200);
     })
     .catch(next);
 }
