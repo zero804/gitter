db97f411590032e12c96c66d4990915ca137d80d
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-06-24 11:19:50 +0100

    fix broken reference to group collection

--- public/js/router-app.js
+++ public/js/router-app.js
@@ -172,7 +172,7 @@ onready(function() {
     //TODO ADD THIS TO MOBILE JP 25/1/16
     orgCollection: troupeCollections.orgs,
     repoCollection: repoCollection,
-    groupsCollection: troupeCollections.groupsCollection
+    groupsCollection: troupeCollections.groups
   });
   appLayout.render();
 
