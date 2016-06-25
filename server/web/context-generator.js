zero804/gitter

commit 322ef786017d023108496710d6d0beb11908297e

🐎 Less mongodb queries before page load and more parallel

server/web/context-generator.js
@@ -1,42 +1,47 @@
 "use strict";
 
+var assert = require("assert");
+var Promise = require('bluebird');
+var _ = require('lodash');
 var restSerializer = require("../serializers/rest-serializer");
-var presenceService = require("gitter-web-presence");
 var userService = require('../services/user-service');
 var userSettingsService = require('../services/user-settings-service');
 var roomMetaService = require('../services/room-meta-service');
-var isNative = require('./is-native');
-
-var assert = require("assert");
-var Promise = require('bluebird');
-var _ = require('underscore');
+var contextGeneratorRequest = require('./context-generator-request');
 
 /**
  * Returns the promise of a mini-context
  */
-exports.generateNonChatContext = function(req) {
+function generateNonChatContext(req) {
   var user = req.user;
-  var troupe = (req.uriContext || {}).troupe;
+  var uriContext = req.uriContext;
+  var troupe = uriContext && uriContext.troupe;
+  var roomMember = uriContext && uriContext.roomMember;
 
   return Promise.all([
+      contextGeneratorRequest(req),
       user ? serializeUser(user) : null,
       troupe ? serializeTroupe(troupe, user) : undefined,
-      user ? determineDesktopNotifications(user, req) : false,
-      user ? userSettingsService.getUserSettings(user.id, 'suggestedRoomsHidden') : false,
-      user ? userSettingsService.getUserSettings(user.id, 'leftRoomMenu') : false,
+      user ? userSettingsService.getMultiUserSettingsForUserId(user._id, ['suggestedRoomsHidden', 'leftRoomMenu']) : null,
     ])
-    .spread(function (serializedUser, serializedTroupe, desktopNotifications, suggestedRoomsHidden, leftRoomMenuState) {
-      return createTroupeContext(req, {
-        user:                 serializedUser,
-        troupe:               serializedTroupe,
+    .spread(function (reqContextHash, serializedUser, serializedTroupe, settings) {
+      var suggestedRoomsHidden = settings && settings.suggestedRoomsHidden;
+      var leftRoomMenuState = settings && settings.leftRoomMenuState;
+
+      return _.extend({}, reqContextHash, {
+        roomMember: roomMember,
+        user: serializedUser,
+        troupe: serializedTroupe,
         suggestedRoomsHidden: suggestedRoomsHidden,
-        desktopNotifications: desktopNotifications,
-        leftRoomMenuState:    leftRoomMenuState,
+        leftRoomMenuState: leftRoomMenuState,
       });
     });
-};
+}
 
-exports.generateSocketContext = function(userId, troupeId) {
+/**
+ * Generates a context for sending over a bayeux connection
+ */
+function generateSocketContext(userId, troupeId) {
   function getUser() {
     if (!userId) return Promise.resolve(null);
     return userService.findById(userId);
@@ -55,63 +60,34 @@ exports.generateSocketContext = function(userId, troupeId) {
         troupe: serializedTroupe || undefined
       };
     });
-};
+}
 
-exports.generateTroupeContext = function(req, extras) {
+/**
+ * Generates the context to send for a main-frame
+ */
+function generateTroupeContext(req, extras) {
   var user = req.user;
   var uriContext = req.uriContext;
   assert(uriContext);
-
-  var troupe = req.uriContext.troupe;
+  var troupe = uriContext.troupe;
+  var roomMember = uriContext && uriContext.roomMember;
 
   return Promise.all([
+    contextGeneratorRequest(req),
     user ? serializeUser(user) : null,
     troupe ? serializeTroupe(troupe, user) : undefined,
-    determineDesktopNotifications(user, req),
     troupe && troupe._id ? roomMetaService.findMetaByTroupeId(troupe._id, 'welcomeMessage') : false,
   ])
-  .spread(function(serializedUser, serializedTroupe, desktopNotifications, welcomeMessage) {
+  .spread(function(reqContextHash, serializedUser, serializedTroupe, welcomeMessage) {
+    var roomHasWelcomeMessage = !!(welcomeMessage && welcomeMessage.text && welcomeMessage.text.length);
 
-    welcomeMessage = (welcomeMessage || { text: '' });
-
-    return createTroupeContext(req, {
+    return _.extend({}, reqContextHash, {
+      roomMember: roomMember,
       user: serializedUser,
       troupe: serializedTroupe,
-      desktopNotifications: desktopNotifications,
-      extras: extras,
-      roomHasWelcomeMessage: !!welcomeMessage.text && !!welcomeMessage.text.length,
-    });
+      roomHasWelcomeMessage: roomHasWelcomeMessage
+    }, extras);
   });
-};
-
-/**
- * Figures out whether to use desktop notifications for this user
- */
-
-function determineDesktopNotifications(user, req) {
-  if(!user) return true;
-
-  var agent = req.getParsedUserAgent();
-  var os = agent.os.family;
-  var clientType;
-
-  if(os === 'Mac OS X') {
-    clientType = 'osx';
-  } else if(os.indexOf('Windows') === 0) {
-    clientType = 'win';
-  } else if (os.indexOf('Linux') === 0) {
-    clientType= 'linux';
-  }
-
-  if(clientType) {
-    return presenceService.isUserConnectedWithClientType(user.id, clientType)
-      .then(function(result) {
-        return !result;
-      });
-  }
-
-  return true;
-
 }
 
 function serializeUser(user) {
@@ -138,7 +114,6 @@ function serializeTroupeId(troupeId, user) {
   return restSerializer.serializeObject(troupeId, strategy);
 }
 
-
 function serializeTroupe(troupe, user) {
   var strategy = new restSerializer.TroupeStrategy({
     currentUserId: user ? user.id : null,
@@ -151,36 +126,8 @@ function serializeTroupe(troupe, user) {
   return restSerializer.serializeObject(troupe, strategy);
 }
 
-function createTroupeContext(req, options) {
-  var events = req.session && req.session.events;
-  var extras = options.extras || {};
-
-  // Pass the feature toggles through to the client
-  var features;
-  if (req.fflip && req.fflip.features) {
-    features = Object.keys(req.fflip.features).filter(function(featureKey) {
-      return req.fflip.features[featureKey];
-    });
-  }
-
-  if (events) { req.session.events = []; }
-
-  return _.extend({
-    roomMember: req.uriContext && req.uriContext.roomMember,
-    user: options.user,
-    troupe: options.troupe,
-    homeUser: options.homeUser,
-    accessToken: req.accessToken,
-    suggestedRoomsHidden: options.suggestedRoomsHidden,
-    desktopNotifications: options.desktopNotifications,
-    events: events,
-    troupeUri: options.troupe ? options.troupe.uri : undefined,
-    troupeHash: options.troupeHash,
-    isNativeDesktopApp: isNative(req),
-    permissions: options.permissions,
-    locale: req.i18n.locales[req.i18n.locale],
-    features: features,
-    leftRoomMenuState: options.leftRoomMenuState,
-    roomHasWelcomeMessage: options.roomHasWelcomeMessage
-  }, extras);
+module.exports = {
+  generateNonChatContext: generateNonChatContext,
+  generateSocketContext: generateSocketContext,
+  generateTroupeContext: generateTroupeContext
 }

server/services/user-settings-service.js
@@ -20,6 +20,27 @@ exports.getUserSettings = function(userId, settingsKey) {
     });
 };
 
+exports.getMultiUserSettingsForUserId = function(userId, settingsKeys) {
+  /* Not sure why mongoose isn't converting these */
+  assert(mongoUtils.isLikeObjectId(userId));
+  userId = mongoUtils.asObjectID(userId);
+
+  var select = settingsKeys.reduce(function(memo, settingsKey) {
+    memo['settings.' + settingsKey] = 1;
+    return memo;
+  }, {
+    _id: 0
+  });
+
+  return persistence.UserSettings.findOne({ userId: userId }, select, { lean: true })
+    .exec()
+    .then(function(us) {
+      if(!us) return {};
+      if(!us.settings) return {};
+
+      return us.settings;
+    });
+};
 
 exports.getMultiUserSettings = function(userIds, settingsKey) {
   userIds = userIds.map(function(id) {

server/web/context-generator-request.js
@@ -1,0 +1,68 @@
+"use strict";
+
+var Promise = require('bluebird');
+var presenceService = require("gitter-web-presence");
+var isNative = require('./is-native');
+
+/**
+ * Figures out whether to use desktop notifications for this user
+ */
+function determineDesktopNotifications(user, req) {
+  if(!user) return true;
+
+  var agent = req.getParsedUserAgent();
+  var os = agent.os.family;
+  var clientType;
+
+  if(os === 'Mac OS X') {
+    clientType = 'osx';
+  } else if(os.indexOf('Windows') === 0) {
+    clientType = 'win';
+  } else if (os.indexOf('Linux') === 0) {
+    clientType= 'linux';
+  }
+
+  if(clientType) {
+    return presenceService.isUserConnectedWithClientType(user.id, clientType)
+      .then(function(result) {
+        return !result;
+      });
+  }
+
+  return true;
+}
+
+function contextFromRequest(req) {
+  var user = req.user;
+  var events = req.session && req.session.events;
+
+  // Pass the feature toggles through to the client
+  var features;
+  if (req.fflip && req.fflip.features) {
+    features = Object.keys(req.fflip.features).filter(function(featureKey) {
+      return req.fflip.features[featureKey];
+    });
+  }
+
+  if (events) { req.session.events = []; }
+
+  var contextHash = {
+    events: events,
+    accessToken: req.accessToken,
+    isNativeDesktopApp: isNative(req),
+    locale: req.i18n.locales[req.i18n.locale],
+    features: features
+  };
+
+  if (!user) {
+    return contextHash;
+  }
+
+  return determineDesktopNotifications(user, req)
+    .then(function(desktopNotifications) {
+      contextHash.desktopNotifications = desktopNotifications;
+      return contextHash;
+    });
+}
+
+module.exports = Promise.method(contextFromRequest);

test/integration/services/user-settings-service-test.js
@@ -1,11 +1,10 @@
-/*global describe:true, it:true, before:true, after:true */
 "use strict";
 
 var testRequire = require('./../test-require');
 var fixtureLoader = require('gitter-web-test-utils/lib/test-fixtures');
 var userSettingsService = testRequire('./services/user-settings-service');
 var assert = testRequire("assert");
-
+var ObjectID = require('mongodb').ObjectID;
 var fixture = {};
 
 describe("User Settings Service", function() {
@@ -66,6 +65,34 @@ describe("User Settings Service", function() {
         assert(settings.test2.human);
       })
       .nodeify(done);
+  });
+
+  it('should be able to get multiple keys', function() {
+    var userId = fixture.user1.id;
+    var V1 = { value1: 1, value2: true, value3: 'string' };
+    var V2 = { human: 1, monkey: 0 };
+
+    return userSettingsService.setUserSettings(userId, 'test', V1)
+      .then(function() {
+        return userSettingsService.setUserSettings(userId, 'test2', V2);
+      })
+      .then(function() {
+        return userSettingsService.getMultiUserSettingsForUserId(userId, ['test', 'test2', 'notset']);
+      })
+      .then(function(settings) {
+        assert.deepEqual(settings, {
+          test: V1,
+          test2: V2
+        });
+      });
+  });
+
+  it('should be able to get multiple keys when the user has no settings', function() {
+    return userSettingsService.getMultiUserSettingsForUserId(new ObjectID(), ['test', 'test2', 'notset'])
+      .then(function(settings) {
+        assert.deepEqual(settings, {
+        });
+      });
   });
 
   it('should be able to fetch keys for multiple usertroupes', function(done) {

commit 5e92cca9abd82d7a27bc3b0213bf2f7089824967

Fix `leftRoomMenu` context cc @suprememoocow
Bug introduced in 322ef786017d023108496710d6d0beb11908297e

server/web/context-generator.js
@@ -26,7 +26,7 @@ function generateNonChatContext(req) {
     ])
     .spread(function (reqContextHash, serializedUser, serializedTroupe, settings) {
       var suggestedRoomsHidden = settings && settings.suggestedRoomsHidden;
-      var leftRoomMenuState = settings && settings.leftRoomMenuState;
+      var leftRoomMenuState = settings && settings.leftRoomMenu;
 
       return _.extend({}, reqContextHash, {
         roomMember: roomMember,