zero804/gitter

commit fc2d3442ee8d2a2a5dbdc82778cd05baa539fb38

Move app tag stuff out of render

server/web/app-tag.js
@@ -1,0 +1,32 @@
+"use strict";
+
+var env = require('gitter-web-env');
+var nconf = env.config;
+var appVersion = require('gitter-app-version');
+
+var stagingText, stagingLink;
+
+/* App tag */
+var staging = nconf.get('STAGING');
+switch(nconf.get('NODE_ENV')) {
+  case 'prod':
+    if(staging) {
+      stagingText = 'NEXT';
+      stagingLink = 'http://next.gitter.im';
+    }
+    break;
+  case 'beta':
+    var branch = appVersion.getBranch();
+    stagingText = branch ? branch.split('/').pop() : 'BETA';
+    stagingLink = appVersion.getGithubLink();
+    break;
+  case 'dev':
+    stagingText = 'DEV';
+    stagingLink = 'https://github.com/troupe/gitter-webapp/tree/develop';
+    break;
+}
+
+module.exports = {
+  text: stagingText,
+  stagingLink: stagingLink
+}

server/handlers/app/render.js
@@ -9,7 +9,6 @@ var contextGenerator = require('../../web/context-generator');
 var restful = require('../../services/restful');
 var userService = require('../../services/user-service');
 var chatService = require('../../services/chat-service');
-var appVersion = require('gitter-app-version');
 var social = require('../social-metadata');
 var restSerializer = require("../../serializers/rest-serializer");
 var burstCalculator = require('../../utils/burst-calculator');
@@ -98,32 +97,6 @@ function getSubResources(entryPoint, jsRoot) {
   }
 
   return cdnSubResources(SUBRESOURCE_MAPPINGS[entryPoint], jsRoot);
-}
-
-
-var stagingText, stagingLink;
-var dnsPrefetch = (nconf.get('cdn:hosts') || []).concat([
-  nconf.get('ws:hostname')
-]);
-
-/* App tag */
-var staging = nconf.get('STAGING');
-switch(nconf.get('NODE_ENV')) {
-  case 'prod':
-    if(staging) {
-      stagingText = 'NEXT';
-      stagingLink = 'http://next.gitter.im';
-    }
-    break;
-  case 'beta':
-    var branch = appVersion.getBranch();
-    stagingText = branch ? branch.split('/').pop() : 'BETA';
-    stagingLink = appVersion.getGithubLink();
-    break;
-  case 'dev':
-    stagingText = 'DEV';
-    stagingLink = 'https://github.com/troupe/gitter-webapp/tree/develop';
-    break;
 }
 
 function renderHomePage(req, res, next) {
@@ -272,9 +245,6 @@ function renderMainFrame(req, res, next, frame) {
         roomMenuIsPinned:       snapshots.leftMenu.roomMenuIsPinned,
         chatAppLocation:        chatAppLocation,
         agent:                  req.headers['user-agent'],
-        stagingText:            stagingText,
-        stagingLink:            stagingLink,
-        dnsPrefetch:            dnsPrefetch,
         subresources:           getSubResources(bootScriptName),
         showFooterButtons:      true,
         showUnreadTab:          true,
@@ -387,7 +357,6 @@ function renderChat(req, res, options, next) {
             classNames: classNames.join(' '),
             agent: req.headers['user-agent'],
             subresources: getSubResources(script),
-            dnsPrefetch: dnsPrefetch,
             isPrivate: isPrivate,
             activityEvents: activityEvents,
             users: users && users.sort(userSort),
@@ -433,8 +402,7 @@ function renderMobileUserHome(req, res, next) {
       troupeName: req.uriContext.uri,
       troupeContext: troupeContext,
       agent: req.headers['user-agent'],
-      user: req.user,
-      dnsPrefetch: dnsPrefetch
+      user: req.user
     });
   })
   .catch(next);
@@ -650,7 +618,7 @@ function renderChatCard(req, res, next) {
     filterChats: function(chats) {
       // Only show the burst
       // TODO: move this somewhere useful
-      var permalinkedChat = _.find(chats, function(chat) { return chat.id == aroundId; });
+      var permalinkedChat = _.find(chats, function(chat) { return chat.id === aroundId; });
       if (!permalinkedChat) return [];
 
       var burstChats = isolateBurst(chats, permalinkedChat);
@@ -698,9 +666,7 @@ function renderUserNotSignedUpMainFrame(req, res, next, frame) {
         troupeName: req.params.roomPart1,
         troupeContext: troupeContext,
         chatAppLocation: chatAppLocation,
-        agent: req.headers['user-agent'],
-        stagingText: stagingText,
-        stagingLink: stagingLink,
+        agent: req.headers['user-agent']
       });
     })
     .catch(next);

server/web/express.js
@@ -12,9 +12,33 @@ var cookieParser = require('cookie-parser');
 var methodOverride = require('method-override');
 var session = require('express-session');
 var devMode = config.get('dev-mode');
+var appTag = require('./app-tag');
 
 // Naughty naughty naught, install some extra methods on the express prototype
 require('./http');
+
+function getSessionStore() {
+  var RedisStore = require('connect-redis')(session);
+  return new RedisStore({
+    client: env.redis.createClient(process.env.REDIS_NOPERSIST_CONNECTION_STRING || config.get("redis_nopersist")),
+    ttl: config.get('web:sessionTTL')
+  });
+
+}
+
+function configureLocals(app) {
+  var locals = app.locals;
+
+  locals.googleTrackingId = config.get("stats:ga:key");
+  locals.googleTrackingDomain = config.get("stats:ga:domain");
+  locals.liveReload = config.get('web:liveReload');
+  locals.stagingText = appTag.text;
+  locals.stagingLink = appTag.link;
+
+  locals.dnsPrefetch = (config.get('cdn:hosts') || []).concat([
+    config.get('ws:hostname')
+  ]);
+}
 
 module.exports = {
   /**
@@ -23,9 +47,7 @@ module.exports = {
   installFull: function(app) {
     require('./register-helpers')(expressHbs);
 
-    app.locals.googleTrackingId = config.get("stats:ga:key");
-    app.locals.googleTrackingDomain = config.get("stats:ga:domain");
-    app.locals.liveReload = config.get('web:liveReload');
+    configureLocals(app);
 
     app.engine('hbs', expressHbs.express3({
       partialsDir: resolveStatic('/templates/partials'),
@@ -60,16 +82,10 @@ module.exports = {
     app.use(require('./middlewares/ie6-post-caching'));
     app.use(require('./middlewares/i18n'));
 
-    var RedisStore = require('connect-redis')(session);
-    var sessionStore = new RedisStore({
-      client: env.redis.createClient(process.env.REDIS_NOPERSIST_CONNECTION_STRING || config.get("redis_nopersist")),
-      ttl: config.get('web:sessionTTL')
-    });
-
     app.use(session({
       secret: config.get('web:sessionSecret'),
       key: config.get('web:cookiePrefix') + 'session',
-      store: sessionStore,
+      store: getSessionStore(),
       cookie: {
         path: '/',
         httpOnly: true,

commit 522627c4036834b2d4ee0674ce7bd02363998bae

Restore next indicator link cc @suprememoocow
Regression introduced in fc2d3442ee8d2a2a5dbdc82778cd05baa539fb38

server/web/app-tag.js
@@ -28,5 +28,5 @@ switch(nconf.get('NODE_ENV')) {
 
 module.exports = {
   text: stagingText,
-  stagingLink: stagingLink
+  link: stagingLink
 }