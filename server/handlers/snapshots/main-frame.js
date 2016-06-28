1bc90a519e19badf49c142ffe8e97344f4a1ae97
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-06-28 13:51:18 +0100

    added room formatting for renderer

--- server/handlers/snapshots/main-frame.js
+++ server/handlers/snapshots/main-frame.js
@@ -1,13 +1,19 @@
 "use strict";
 
 var _ = require('lodash');
+var parseRoomsIntoLeftMenuRoomList = require('gitter-web-shared/rooms/left-menu-room-list.js');
+var parseRoomsIntoLeftMenuFavouriteRoomList = require('gitter-web-shared/rooms/left-menu-room-favourite-list');
+var getOrgNameFromTroupeName = require('gitter-web-shared/get-org-name-from-troupe-name');
+var resolveRoomAvatarSrcSet = require('gitter-web-shared/avatars/resolve-room-avatar-srcset');
 
-module.exports = function getMainFrameSnapshots(req, troupeContext) {
+module.exports = function getMainFrameSnapshots(req, troupeContext, rooms) {
   var hasNewLeftMenu = !req.isPhone && req.fflip && req.fflip.has('left-menu');
   var hasGroups = req.fflip && req.fflip.has('groups');
   var currentRoom = (req.troupe || {});
   var lastLeftMenuSnapshot = troupeContext.leftRoomMenuState;
   var uri = req.uriContext.uri;
+  var selectedOrgName = lastLeftMenuSnapshot.selectedOrgName;
+  var tempOrg = false;
 
   //Left menu state
   //------------------------------------------------------
@@ -18,8 +24,22 @@ module.exports = function getMainFrameSnapshots(req, troupeContext) {
 
   //Rooms
   //------------------------------------------------------
+  //If the room is not in the room list we need to switch the state to show the parent group
+  if(rooms.indexOf(currentRoom) === -1) {
+    menuState = 'org';
+    selectedOrgName = getOrgNameFromTroupeName(currentRoom.uri);
+    tempOrg = {
+      name: selectedOrgName,
+      avatarSrcset: resolveRoomAvatarSrcSet({ uri: selectedOrgName}, 22),
+    };
+  }
+
+  //Orgs
+  //------------------------------------------------------
 
   return {
-    leftMenu: _.extend({}, lastLeftMenuSnapshot, { state: menuState }),
+    leftMenu: _.extend({}, lastLeftMenuSnapshot, { state: menuState, tempOrg: tempOrg }),
+    rooms: parseRoomsIntoLeftMenuRoomList(menuState, rooms, selectedOrgName),
+    favourites: parseRoomsIntoLeftMenuFavouriteRoomList(menuState, rooms, selectedOrgName),
   };
 };
