32e3d95b3c2fc2ae3fb906e254f96639854a6b99
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-06-30 13:23:58 +0100

    account for the existance of groups when showing tempOrgItem

--- public/js/views/menu/room/minibar/temp-org-view/temp-org-view.js
+++ public/js/views/menu/room/minibar/temp-org-view/temp-org-view.js
@@ -5,12 +5,15 @@ var _ = require('underscore');
 var toggleClass = require('utils/toggle-class');
 var resolveRoomAvatarSrcSet = require('gitter-web-shared/avatars/resolve-room-avatar-srcset');
 var context = require('utils/context');
+var getOrgnameFromUri = require('gitter-web-shared/get-org-name-from-uri');
+
 var _super = MinibarItemView.prototype;
 
 module.exports = MinibarItemView.extend({
 initialize: function(attrs, options) {
     _super.initialize.apply(this, arguments);
     this.roomCollection = attrs.roomCollection;
+    this.groupCollection = attrs.groupCollection;
     this.listenTo(this.model.troupe, 'change:id', this.onRoomChange, this);
     this.listenTo(this.model, 'change:hidden', this.render, this);
   },
@@ -20,15 +23,23 @@ initialize: function(attrs, options) {
     //as we are sure we should be showing a temp org
     //return context.hasFeature('groups') && this.model.set('hidden', !!this.roomCollection.get(val));
 
-    this.model.set('hidden', !!this.roomCollection.get(val));
+    if(!context.hasFeature('groups')) { return; }
+    var orgName = getOrgnameFromUri(this.getRoomUri());
+    var roomId = val;
+    var hasJoinedOrg = !this.roomCollection.get(roomId) && !this.groupCollection.findWhere({ name: orgName });
+    this.model.set('hidden', !hasJoinedOrg);
   },
 
   serializeData: function (){
     var data = this.model.toJSON();
-    var name = (data.name || document.location.pathname);
+    var uri = this.getRoomUri();
     return  _.extend({}, _super.serializeData.apply(this, arguments), {
-      avatarSrcset: resolveRoomAvatarSrcSet({ uri: name }, 22),
+      avatarSrcset: resolveRoomAvatarSrcSet({ uri: uri }, 22),
     });
+  },
+
+  getRoomUri: function (model){
+    return (context.troupe().get('uri') || document.location.pathname);
   },
 
   onRender: function (){
