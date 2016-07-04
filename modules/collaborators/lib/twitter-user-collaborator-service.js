8d0e10b9cf3e7f666e229684c6bc7787656fdc51
Andrew Newdigate <andrew@troupe.co>
2016-07-04 15:01:58 +0100

    ✨ Feature: Collaborators service handles service descriptors

---
+++ modules/collaborators/lib/twitter-user-collaborator-service.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var _ = require('lodash');
+var TwitterService = require('gitter-web-twitter');
+
+function TwitterUserCollaboratorService(user, identity) {
+  this.user = user;
+  this.identity = identity;
+}
+
+TwitterUserCollaboratorService.prototype.findCollaborators = function() {
+  var username = this.user.username.replace(/_twitter$/, ''); // This is awful
+  var twitterService = new TwitterService(this.identity);
+
+  return twitterService.findFollowers(username)
+    .then(function(followers) {
+      followers.sort(function(a, b) {
+        return b.followers_count - a.followers_count;
+      });
+
+      followers = followers.slice(0, 20);
+
+      return _.map(followers, function(follower) {
+        return {
+          displayName: follower.name,
+          twitterUsername: follower.screen_name,
+          avatar: follower.profile_image_url_https // TODO: use avatar service?
+        };
+      });
+    });
+}
+
+module.exports = TwitterUserCollaboratorService;
