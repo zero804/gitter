308de5665b7f93ed92fb98f9b0877da4515b4038
Andrew Newdigate <andrew@troupe.co>
2016-07-04 10:51:44 +0100

    ✨ Feature: allow sort-of-bans from private rooms

--- server/api/v1/rooms/bans.js
+++ server/api/v1/rooms/bans.js
@@ -16,7 +16,7 @@ module.exports = {
       });
   },
 
-  create: function(req) {
+  create: function(req, res) {
     return loadTroupeFromParam(req)
       .then(function(troupe) {
         var roomWithPolicyService = new RoomWithPolicyService(troupe, req.user, req.userRoomPolicy);
@@ -26,6 +26,11 @@ module.exports = {
         return roomWithPolicyService.banUserFromRoom(username, { removeMessages: removeMessages });
       })
       .then(function(ban) {
+        if (!ban) {
+          res.status(202).send({ removed: true });
+          return;
+        }
+
         var strategy = new restSerializer.TroupeBanStrategy({ });
         return restSerializer.serializeObject(ban, strategy);
       });
