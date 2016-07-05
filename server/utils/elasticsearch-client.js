10c64b3fa2e677f723933839bba6450e7f8de85a
Andy Trevorah <andy@troupe.co>
2016-07-05 12:05:15 +0100

    switched elasticsearch to use bluebird promises directly

--- server/utils/elasticsearch-client.js
+++ server/utils/elasticsearch-client.js
@@ -4,6 +4,7 @@ var env = require('gitter-web-env');
 var logger = env.logger;
 var config = env.config;
 var elasticsearch = require('elasticsearch');
+var Promise = require('bluebird');
 var debug = require('debug')('gitter:infra:elasticsearch');
 
 function ElasticSearchLoggingAdapter(/*config*/) {
@@ -30,12 +31,26 @@ ElasticSearchLoggingAdapter.prototype.trace = function (method, requestUrl, body
 };
 ElasticSearchLoggingAdapter.prototype.close = function () { };
 
+function defer() {
+  var resolve, reject;
+  var promise = new Promise(function() {
+      resolve = arguments[0];
+      reject = arguments[1];
+  });
+  return {
+      resolve: resolve,
+      reject: reject,
+      promise: promise
+  };
+}
+
 var v1 = new elasticsearch.Client({
   hosts: config.get('elasticsearch:hosts'),
   // Warning: possible memory leak: https://github.com/elasticsearch/elasticsearch-js/issues/71
   sniffOnStart: config.get('elasticsearch:sniffOnStart'),
   sniffInterval: 300000,
   apiVersion: '1.4',
+  defer: defer,
   log: ElasticSearchLoggingAdapter
 });
 
@@ -45,6 +60,7 @@ var typeahead = new elasticsearch.Client({
   sniffOnStart: config.get('elasticsearch:sniffOnStart'),
   sniffInterval: 300000,
   apiVersion: '2.3',
+  defer: defer,
   log: ElasticSearchLoggingAdapter
 });
 
