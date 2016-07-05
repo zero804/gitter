5cbfb45c3b19cc402c2196783a3a261ace4c6b24
Andy Trevorah <andy@troupe.co>
2016-07-05 18:08:49 +0100

    moved reusable elastic bulk tools into own module

--- server/services/typeaheads/elastic-bulk-update-stream.js
+++ server/services/typeaheads/elastic-bulk-update-stream.js
@@ -2,6 +2,7 @@
 
 var Writable = require('stream').Writable;
 var util = require('util');
+var bulkTools = require('./elastic-bulk-tools');
 var debug = require('debug')('gitter:app:elastic-bulk-update-stream');
 
 function ElasticBulkUpdateStream(elasticClient) {
@@ -14,52 +15,10 @@ util.inherits(ElasticBulkUpdateStream, Writable);
 // expects an array of updates where each element is a valid req for elasticClient.update
 ElasticBulkUpdateStream.prototype._write = function(updates, encoding, callback) {
   debug('uploading %d updates', updates.length);
-
-  var req = {
-    body: updates.reduce(function(body, update) {
-      body.push({
-        update: {
-          _index: update.index,
-          _type: update.type,
-          _id: update.id,
-          _retry_on_conflict: update._retry_on_conflict
-        }
-      });
-      body.push(update.body)
-      return body;
-    }, [])
-  };
-
-  this._elasticClient.bulk(req, function(err, resp) {
-    if (err) {
-      return callback(err);
-    }
-
-    var respErrors = getErrors(req, resp)
-    if (respErrors) {
-      return callback(new Error('elastic bulk upload failed for some. failures: ' + JSON.stringify(respErrors, null, 2)));
-    }
-
-    return callback();
+  var req = bulkTools.createBulkUpdate(updates);
+  this._elasticClient.bulk(req, function(err, res) {
+    return callback(err || bulkTools.findErrors(req, res));
   });
 };
-
-function getErrors(req, resp) {
-  if (!resp.errors) return;
-
-  var errors = [];
-
-  resp.items.forEach(function(item, index) {
-    if (item.update.error) {
-      errors.push({
-        path: req.body[index * 2],
-        body: req.body[(index * 2) + 1],
-        resp: item
-      });
-    }
-  });
-
-  return errors;
-}
 
 module.exports = ElasticBulkUpdateStream;
