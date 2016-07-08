b253a7252a699d893cbbee0ed56e4c8effd529db
Le Roux Bodenstein <leroux@gitter.im>
2016-07-08 12:33:34 +0200

    Revert "Remove INHERITED weirdness."
    This reverts commit 0f7dc4ba160ced4671b11a86baa3c969d87515a5.

--- test/api-tests/group-api-tests.js
+++ test/api-tests/group-api-tests.js
@@ -128,7 +128,7 @@ describe('group-api', function() {
         name: fixtureLoader.GITTER_INTEGRATION_REPO,
         topic: 'all about testing',
         security: {
-          security: 'PUBLIC',
+          security: 'INHERITED',
           type: 'GH_REPO',
           linkPath: fixtureLoader.GITTER_INTEGRATION_USERNAME + '/' + fixtureLoader.GITTER_INTEGRATION_REPO
         }
