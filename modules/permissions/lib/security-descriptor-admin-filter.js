a9ff5a2b516987882a7d417514e542519c12b1dd
Andrew Newdigate <andrew@troupe.co>
2016-07-14 11:53:36 +0100

    🚨 Tests: added tests for list-group-admin functionality

--- modules/permissions/lib/security-descriptor-admin-filter.js
+++ modules/permissions/lib/security-descriptor-admin-filter.js
@@ -4,24 +4,35 @@ var Promise = require('bluebird');
 var policyDelegateFactory = require('./policy-delegate-factory');
 var PolicyEvaluator = require('./policies/policy-evaluator');
 
+/**
+ * When testing n > MAX_FILTER_CONCURRENCY items, this is the
+ * maximum concurrency we'll use
+ */
+var MAX_FILTER_CONCURRENCY = 32;
+
 function securityDescriptorAdminFilter(user, objectsWithSecurityDescriptors) {
+  if (!user || !objectsWithSecurityDescriptors.length) return [];
 
   return Promise.map(objectsWithSecurityDescriptors, function(objectsWithSecurityDescriptor) {
-    var securityDescriptor = objectsWithSecurityDescriptor.sd;
+      var securityDescriptor = objectsWithSecurityDescriptor.sd;
 
-    var policyDelegate = policyDelegateFactory(user._id, user, securityDescriptor);
-    var contextDelegate = null; // No context delegate needed for admin
+      var policyDelegate = policyDelegateFactory(user._id, user, securityDescriptor);
+      var contextDelegate = null; // No context delegate needed for admin
 
-    var policyEvaluator = new PolicyEvaluator(user._id, securityDescriptor, policyDelegate, contextDelegate);
+      var policyEvaluator = new PolicyEvaluator(user._id, securityDescriptor, policyDelegate, contextDelegate);
 
-    return policyEvaluator.canAdmin()
-      .then(function(admin) {
-        if (admin) return objectsWithSecurityDescriptor;
-      });
-  }, { concurrency: 32 })
-  .then(function(adminSecurityDescriptors) {
-    return adminSecurityDescriptors.filter(Boolean);
-  });
+      return policyEvaluator.canAdmin()
+        .bind(objectsWithSecurityDescriptor)
+        .then(function(admin) {
+          if (admin) return this;
+        });
+
+    }, {
+      concurrency: MAX_FILTER_CONCURRENCY
+    })
+    .then(function(adminSecurityDescriptors) {
+      return adminSecurityDescriptors.filter(Boolean);
+    });
 }
 
-module.exports = securityDescriptorAdminFilter;
+module.exports = Promise.method(securityDescriptorAdminFilter);
