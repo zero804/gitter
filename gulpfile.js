06e2479dcf9faa63343ca9199dbaa1c9482a835e
Andrew Newdigate <andrew@troupe.co>
2016-07-14 12:02:52 +0100

    🐞 Bugfix: disabled brotli compression by default

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,6 @@ var webpack = require('gulp-webpack');
 var eslint = require('gulp-eslint');
 
 var gzip = require('gulp-gzip');
-var brotli = require('gulp-brotli');
 var mocha = require('gulp-spawn-mocha');
 var using = require('gulp-using');
 var tar = require('gulp-tar');
@@ -632,6 +631,9 @@ gulp.task('compress-assets-gzip', ['build-assets'], function() {
 
 // Brotli compression for text files
 gulp.task('compress-assets-brotli-text', ['build-assets'], function() {
+  if (!process.env.ENABLE_BROTLI_COMPRESSION) return;
+
+  var brotli = require('gulp-brotli');
   return gulp.src(['output/assets/**/*.{css,svg,js}', '!**/*.map'], { stat: true, base: 'output/assets/' })
     .pipe(brotli.compress({
       mode: 1, // 1 = TEXT
@@ -644,6 +646,9 @@ gulp.task('compress-assets-brotli-text', ['build-assets'], function() {
 
 // Brotli compression for non-text files
 gulp.task('compress-assets-brotli-generic', ['build-assets'], function() {
+  if (!process.env.ENABLE_BROTLI_COMPRESSION) return;
+
+  var brotli = require('gulp-brotli');
   return gulp.src(['output/assets/**/*.{ttf,eot}', '!**/*.map'], { stat: true, base: 'output/assets/' })
     .pipe(brotli.compress({
       mode: 0, // 0 = GENERIC
