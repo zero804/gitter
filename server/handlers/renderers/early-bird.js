5fbbf720ce4c6d8fe2678c24328fffb886468cd7
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-07-15 15:49:11 +0100

    move exports ...

--- server/handlers/renderers/early-bird.js
+++ server/handlers/renderers/early-bird.js
@@ -2,13 +2,13 @@
 
 var fonts = require('../../web/fonts');
 
-module.exports = {
-  renderEarlyBirdPage: renderEarlyBirdPage,
-};
-
 function renderEarlyBirdPage(req, res) {
   return res.render('early-bird', {
     hasCachedFonts: fonts.hasCachedFonts(req.cookies),
     fonts: fonts.getFonts(),
   });
 }
+
+module.exports = {
+  renderEarlyBirdPage: renderEarlyBirdPage,
+};
