3ac276a6b34a06c6427823bfe0fe239b25e45848
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-07-19 14:56:03 +0100

    comment webpack build config

--- modules/topics-ui/webpack.config.js
+++ modules/topics-ui/webpack.config.js
@@ -1,11 +1,22 @@
 "use strict";
 
+/*
+ This config is likely to chage and be broken into
+ - server
+ - client
+managed by a gulp build
+its good enough for now however to have everything in one file
+*/
+
 var path = require('path');
 var packageInfo = require('./package.json');
 var fs = require('fs');
 
 var webpack = require('webpack');
 
+//We need to stop webpack building and .bin files
+//as this will absolutely destroy the prod servers
+//http://jlongster.com/Backend-Apps-with-Webpack--Part-I
 var nodeModules = {};
 fs.readdirSync('node_modules')
   .filter(function(x) {
@@ -16,13 +27,16 @@ fs.readdirSync('node_modules')
   });
 
 var config = {
+  //Leave node deps alone
   target: 'node',
+  //Ignore any .bin files
   externals: nodeModules,
   entry: {
     ForumContainer: path.resolve(__dirname, './containers/ForumContainer'),
   },
   output: {
     path:     path.resolve(__dirname, './output'),
+    //module.exports = myExport
     libraryTarget: 'commonjs2',
     filename: '[name].js'
   },
@@ -40,6 +54,7 @@ var config = {
     extensions: [ '', '.js', '.jsx' ]
   },
   plugins: [
+    //For all those GLOBAL jsx assumptions
     new webpack.ProvidePlugin({
       React: 'react',
     })
