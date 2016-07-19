7d205960e98c5aa39abb735c6b579a27282f42f7
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-07-19 15:01:25 +0100

    OMG gitter-reviewer

--- modules/topics-ui/renderer.js
+++ modules/topics-ui/renderer.js
@@ -4,12 +4,13 @@ var path = require('path');
 var reactDomServer = require('react-dom/server');
 
 module.exports = function(componentName, context){
+
   //Require a component built with webpack
-  var component   = require(__dirname, './output', componentName);
+  var component = require(path.resolve(__dirname, './output', componentName));
 
   //Do some stupid stuff because we don't use jsx on the server like sane people
   //https://facebook.github.io/react/blog/2014/10/14/introducing-react-elements.html#react-without-jsx
-  var Component   = React.createFactory(component);
+  var Component = React.createFactory(component);
 
   //Return the rendered component with a given context
   return reactDomServer.renderToString(Component(context));
