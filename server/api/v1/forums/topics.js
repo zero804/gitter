4d8b310bd0d5bd1920e1588499ba76442f770ee6
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-07-20 14:24:33 +0100

    move fake data into module

--- server/api/v1/forums/topics.js
+++ server/api/v1/forums/topics.js
@@ -1,10 +1,14 @@
 "use strict";
 
 var Promise = require('bluebird');
+var getTopics = require('gitter-web-fake-data').getTopics;
 
 module.exports = {
   id: 'topicId',
   index: function(req, res){
+    return Promise.resolve(getTopics());
+  },
+  show: function (){
     return Promise.resolve(200);
-  }
+  },
 };
