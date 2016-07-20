e2c64bd01cb56cf9fd58646022e1b960d7b6d56e
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-07-20 13:51:31 +0100

    WIP /forums/:forumId/topics

--- server/api/v1/forums/index.js
+++ server/api/v1/forums/index.js
@@ -6,9 +6,16 @@ var StatusError = require('statuserror');
 var getForum = require('gitter-web-fake-data').getForum;
 
 module.exports = {
+
   id: 'forumId',
+
   show: function(req, res){
     if(!req.user) { throw new StatusError(404); }
     return Promise.resolve(getForum());
-  }
+  },
+
+  subresources: {
+    'topics': require('./topics'),
+  },
+
 };
