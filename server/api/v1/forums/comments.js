0b77d6b655e582e3d5096bf78156a9874bb6f4c6
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-07-20 15:50:59 +0100

    start returning some comments

--- server/api/v1/forums/comments.js
+++ server/api/v1/forums/comments.js
@@ -1,11 +1,14 @@
 "use strict";
 
 var Promise = require('bluebird');
+var fakeData = require('gitter-web-fake-data');
 
 module.exports = {
 
   id: 'commentId',
+
   index: function(){
-    return Promise.resolve(200);
+    return Promise.resolve(fakeData.getComments());
   }
+
 };
