a9729ea3ce9211f4be97f9b9748f866d42ed2ede
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-07-20 15:50:59 +0100

    start returning some comments

--- modules/fake-data/index.js
+++ modules/fake-data/index.js
@@ -1,9 +1,10 @@
 "use strict";
 
 module.exports = {
-  getForum:   require('./lib/forum'),
-  getTopics:  require('./lib/topics'),
-  getTopic:   require('./lib/topics/get-topic'),
-  getReplies: require('./lib/replies'),
-  getReply:   require('./lib/replies/get-reply'),
+  getForum:    require('./lib/forum'),
+  getTopics:   require('./lib/topics'),
+  getTopic:    require('./lib/topics/get-topic'),
+  getReplies:  require('./lib/replies'),
+  getReply:    require('./lib/replies/get-reply'),
+  getComments: require('./lib/comments'),
 };
