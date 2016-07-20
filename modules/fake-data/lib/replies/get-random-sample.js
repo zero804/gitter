ecf1f3239616116fdf21aa1075bb9d6fb3e6bc82
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-07-20 15:22:39 +0100

    added nested replies to topics

---
+++ modules/fake-data/lib/replies/get-random-sample.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var _ = require('lodash');
+var getReplies   = require('./index');
+var getRandomInt = require('../utils/get-random-int');
+
+module.exports = function getRandomReplySample(parentId){
+
+  var replies   = getReplies();
+  var lastIndex = replies.length - 1;
+
+  var start = lastIndex;
+  while(start === lastIndex) { start = getRandomInt(0, lastIndex);}
+
+  var end = start;
+  while(end === start) { end = getRandomInt(start, lastIndex); }
+
+  return replies.slice(start, end).map(function(reply){
+    return _.extend({}, reply, { parentId: parentId });
+  });
+};
