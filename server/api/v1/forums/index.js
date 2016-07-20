11fbfe80bbc8d7cdac24a17ca87db6e167906b2e
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-07-20 12:10:11 +0100

    add forum building rigging

--- server/api/v1/forums/index.js
+++ server/api/v1/forums/index.js
@@ -2,9 +2,18 @@
 
 var Promise = require('bluebird');
 
+var forum;
+function buildForum(){
+  return { key: 'THIS IS THE NEWS' };
+}
+function getForum(){
+  if(!forum) { forum = buildForum(); }
+  return forum;
+}
+
 module.exports = {
   id: 'forumId',
   show: function(req, res){
-    return Promise.resolve({ key: 'THIS IS THE NEWS' });
+    return Promise.resolve(getForum());
   }
 };
