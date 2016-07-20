2cbee8fa1a211ce4b57c28a780042aa655e3f4ed
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-07-20 15:22:39 +0100

    added nested replies to topics

--- modules/fake-data/lib/topics/index.js
+++ modules/fake-data/lib/topics/index.js
@@ -4,13 +4,14 @@ var _ = require('lodash'); // eslint-disable-line node/no-unpublished-require
 var faker = require('faker');
 var getRandomInt = require('../utils/get-random-int');
 var getRandomBool = require('../utils/get-random-bool');
+var getRandomReplySample = require('../replies/get-random-sample');
 
 var topics;
 
 module.exports = function getFakeTopicsList(){
 
   if(!topics) {
-    topics = _.range(getRandomInt(3, 10)).map(function(){
+    topics = _.range(getRandomInt(3, 10)).map(function(val){
 
       var title = faker.commerce.productName();
       var titleHTML = '<h1>' + title + '</h1>';
@@ -18,14 +19,18 @@ module.exports = function getFakeTopicsList(){
       var body = faker.hacker.phrase();
       var bodyHTML = '<p>' + body + '</p>';
 
+      var replies = getRandomReplySample(val);
+
       var topic = {
+        id: val,
         title: { text: title, html: titleHTML },
-        body:  { text: body, html: bodyHTML},
-        repliesTotal:       getRandomInt(1, 8),
+        body: { text: body, html: bodyHTML},
+        repliesTotal: replies.length,
+        replies: replies,
         participatingTotal: getRandomInt(8, 25),
-        isFaved:            getRandomBool(),
-        isParticipating:    getRandomBool(),
-        isWatching:         getRandomBool(),
+        isFaved: getRandomBool(),
+        isParticipating: getRandomBool(),
+        isWatching: getRandomBool(),
       };
 
       return topic;
