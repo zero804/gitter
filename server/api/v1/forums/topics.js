33f7044aa6ca42930a8906276a30e5293803edc2
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-07-20 15:08:33 +0100

    added /replies endpoint

--- server/api/v1/forums/topics.js
+++ server/api/v1/forums/topics.js
@@ -4,11 +4,19 @@ var Promise = require('bluebird');
 var fakeData = require('gitter-web-fake-data');
 
 module.exports = {
+
   id: 'topicId',
+
   index: function(req, res){
     return Promise.resolve(fakeData.getTopics());
   },
+
   show: function (){
     return Promise.resolve(fakeData.getTopic());
   },
+
+  subresources: {
+    'replies': require('./replies'),
+  },
+
 };
