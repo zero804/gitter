ae0429fd5b902bbbcb80145e9d1510b1f9f59d8f
Andrew Newdigate <andrew@troupe.co>
2016-07-21 09:26:23 +0100

    ✨ Feature: topics get uglified

--- modules/topics-ui/dev/gulp-tasks-webpack.js
+++ modules/topics-ui/dev/gulp-tasks-webpack.js
@@ -2,10 +2,15 @@
 
 var gulp = require('gulp');
 var webpack = require('gulp-webpack');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
 
 function webpackPipeline(rootDir) {
   return gulp.src(rootDir + '/webpack.config.js')
     .pipe(webpack(require('../webpack.config')))
+    .pipe(sourcemaps.init({ /* loadMaps: true */ debug:true }))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('../maps'));
 }
 
 module.exports = webpackPipeline;
