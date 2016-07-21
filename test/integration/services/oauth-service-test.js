7d37e2f2e02b8e9962ebf6f16c5d00d2a8ab7f61
Andrew Newdigate <andrew@troupe.co>
2016-07-21 13:15:18 +0100

    💬 PR Review: addressed @trevorah's comments

--- test/integration/services/oauth-service-test.js
+++ test/integration/services/oauth-service-test.js
@@ -198,16 +198,30 @@ describe('oauth-service', function() {
   });
 
   describe('isInternalClient', function() {
-    var FIXTURES = [
-      { client: null, result: false },
-      { client: { }, result: false },
-      { client: { clientKey: 'bob', canSkipAuthorization: false }, result: false },
-      { client: { clientKey: 'bob', canSkipAuthorization: true }, result: true },
-      { client: { clientKey: 'web-internal' }, result: true },
-    ];
+    var FIXTURES = [{
+      name: 'null is not an internal client',
+      client: null,
+      result: false
+    }, {
+      name: 'invalid client is not an internal client',
+      client: { },
+      result: false
+    }, {
+      name: 'an arbitrary client is not internal client',
+      client: { clientKey: 'bob', canSkipAuthorization: false },
+      result: false
+    }, {
+      name: 'clients who canSkipAuthorization are internal',
+      client: { clientKey: 'bob', canSkipAuthorization: true },
+      result: true
+    }, {
+      name: 'web-internal is internal',
+      client: { clientKey: 'web-internal' },
+      result: true
+    }];
 
-    FIXTURES.forEach(function(meta, index) {
-      it('check #' + (index + 1), function() {
+    FIXTURES.forEach(function(meta) {
+      it(meta.name, function() {
         var result = oauthService.isInternalClient(meta.client);
         assert.strictEqual(result, meta.result);
       });
