0d90b8aa0197697eb8cd53d837e47947f5c92a5e
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-07-21 11:54:40 +0100

    simplfy the get-random-smaple

--- modules/fake-data/lib/replies/get-random-sample.js
+++ modules/fake-data/lib/replies/get-random-sample.js
@@ -7,15 +7,8 @@ var getRandomInt = require('../utils/get-random-int');
 module.exports = function getRandomReplySample(parentId){
 
   var replies = getReplies();
-  var lastIndex = replies.length - 1;
-
-  var start = lastIndex;
-  while(start === lastIndex) { start = getRandomInt(0, lastIndex);}
-
-  var end = start;
-  while(end === start) { end = getRandomInt(start, lastIndex); }
-
-  return replies.slice(start, end).map(function(reply){
+  return replies.slice(0, replies.length - 1).map(function(reply){
     return _.extend({}, reply, { parentId: parentId });
   });
+
 };
