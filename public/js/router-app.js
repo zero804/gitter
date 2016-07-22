18d952e95276f826115b7e5aaf902ac2baf16b05
Eric Eastwood <contact@ericeastwood.com>
2016-07-22 03:25:35 -0500

    Move create-room-view-model to its own folder

--- public/js/router-app.js
+++ public/js/router-app.js
@@ -27,7 +27,7 @@ var repoModels = require('collections/repos');
 var ReposCollection = repoModels.ReposCollection;
 var groupModels = require('collections/groups');
 var CommunityCreateModel = require('views/community-create/community-create-model');
-var CreateRoomModel = require('views/modals/create-room-view-model');
+var CreateRoomModel = require('models/create-room-view-model');
 
 var AppLayout = require('views/layouts/app-layout');
 var LoadingView = require('views/app/loading-view');
