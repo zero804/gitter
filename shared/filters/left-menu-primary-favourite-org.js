3524c723796497f9f68f19dbb4f0048794abd533
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-07-22 15:41:53 +0100

    ficx favourites not filtering

--- shared/filters/left-menu-primary-favourite-org.js
+++ shared/filters/left-menu-primary-favourite-org.js
@@ -3,5 +3,6 @@
 var favouriteFilter = require('./left-menu-primary-favourite');
 
 module.exports = function leftMenuFavouriteOrg(room, groupId) {
+  console.log('filter');
   return favouriteFilter(room) && room.groupId === groupId && room.roomMember;
 };
