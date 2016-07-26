968d70bfeef8e13ad978059ec7bddee1287f192d
Eric Eastwood <contact@ericeastwood.com>
2016-07-26 15:06:00 -0500

    Add avatarUrl to user?q endpoint

--- test/integration/serializers/rest/troupe-strategy-test.js
+++ test/integration/serializers/rest/troupe-strategy-test.js
@@ -3,6 +3,8 @@
 var assert = require('assert');
 var testRequire = require('../../test-require');
 var assertUtils = require('../../assert-utils')
+var env = require('gitter-web-env');
+var nconf = env.config;
 var fixtureLoader = require('gitter-web-test-utils/lib/test-fixtures');
 var serialize = testRequire('./serializers/serialize');
 var TroupeStrategy = testRequire('./serializers/rest/troupe-strategy');
@@ -128,6 +130,7 @@ describe('TroupeStrategy', function() {
             username: u2.username,
             displayName: u2.displayName,
             url: '/' + u2.username,
+            avatarUrl:  nconf.get('avatar:officialHost') + '/g/u/' + u2.username,
             avatarUrlSmall: '/api/private/user-avatar/'+u2.username+'?s=60',
             avatarUrlMedium: '/api/private/user-avatar/'+u2.username+'?s=128',
             staff: false,
