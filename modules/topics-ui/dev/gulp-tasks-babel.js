b8076d024793b5e547d598f812ac63605ee72dac
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-07-26 16:00:41 +0100

    apply babel config in gulp task

--- modules/topics-ui/dev/gulp-tasks-babel.js
+++ modules/topics-ui/dev/gulp-tasks-babel.js
@@ -2,12 +2,11 @@
 
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var babelConfig = require('./babel-config');
 
 function babelPipeline(rootDir) {
   return gulp.src(rootDir + '/containers/**.{js,jsx}')
-      .pipe(babel({
-        // Babel options
-      }));
+      .pipe(babel(babelConfig));
 }
 
 module.exports = babelPipeline;
