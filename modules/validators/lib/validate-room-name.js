140ba6845ac0d4c1b4ec0f8043fc9f87896ef169
Le Roux Bodenstein <leroux@gitter.im>
2016-07-27 13:57:33 +0200

    reserve a bunch of things at group and room level

--- modules/validators/lib/validate-room-name.js
+++ modules/validators/lib/validate-room-name.js
@@ -1,8 +1,14 @@
 'use strict';
 
 var xregexp = require('xregexp').XRegExp;
+var reservedSubNamespaceHash = require('./reserved-sub-namespaces').hashMap;
 
 function validateRoomName(name) {
+  // prevent rooms called "topics" and stuff like that.
+  if (reservedSubNamespaceHash[name.toLowerCase()]) {
+    return false;
+  }
+
   var matcher = xregexp('^[\\p{L}\\d\\_][\\p{L}\\d\\-\\_]{1,80}$');
   return !!matcher.test(name);
 }
