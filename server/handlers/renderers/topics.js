043bca15ebc204b64c0ade3dc1792b74f6906128
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-07-28 16:04:41 +0100

    move method to find by name

--- server/handlers/renderers/topics.js
+++ server/handlers/renderers/topics.js
@@ -2,6 +2,7 @@
 
 var StatusError = require('statuserror');
 var fonts = require('../../web/fonts');
+var forumService = require('gitter-web-forums').forumService;
 
 function renderForum(req, res, next) {
 
@@ -9,14 +10,18 @@ function renderForum(req, res, next) {
     return next(new StatusError(404));
   }
 
-  res.render('topics/forum', {
-    layout: 'topics-layout',
-    hasCachedFonts: fonts.hasCachedFonts(req.cookies),
-    fonts: fonts.getFonts(),
-    componentData: {
-      groupName: req.params.roomPart1,
-    }
-  });
+  forumService.findByName(req.params.roomPart1)
+    .then(function(forum){
+      res.render('topics/forum', {
+        layout: 'topics-layout',
+        hasCachedFonts: fonts.hasCachedFonts(req.cookies),
+        fonts: fonts.getFonts(),
+        componentData: {
+          groupName: req.params.roomPart1,
+          forum: forum,
+        }
+      });
+    });
 }
 
 module.exports = {
