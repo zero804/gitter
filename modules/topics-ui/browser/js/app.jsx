b4f7860fe9688c7d130e37409702e49c6e54216b
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-07-29 16:45:19 +0100

    migrate router outside of app

--- modules/topics-ui/browser/js/app.jsx
+++ modules/topics-ui/browser/js/app.jsx
@@ -4,30 +4,35 @@ const React = require('react');
 const Backbone = require('backbone');
 const ForumContainer = require('../../containers/ForumContainer.jsx');
 const CategoryStore = require('./stores/forum-category-store');
-const router = require('./routers/index');
-
-Backbone.history.start({ pushState: true });
 
 module.exports = React.createClass({
 
   getInitialState(){
+    const { router } = this.props;
     switch(router.get('route')) {
       case 'forum': return this.getForumState();
     }
   },
 
   render(){
-    switch(router.get('route')) {
+    const { route } = this.state;
+    switch(route) {
       case 'forum': return <ForumContainer {...this.state} />
     }
   },
 
+  getDefaultState(){
+    const { router } = this.props;
+    return { route: router.get('route') };
+  },
+
   getForumState(){
     const categoryStore = (window.context.categoryStore || {});
-    return {
+    const { router } = this.props;
+    return Object.assign(this.getDefaultState(), {
       groupName: router.get('groupName'),
       categoryStore: new CategoryStore(categoryStore.models),
-    };
+    });
   }
 
 });
