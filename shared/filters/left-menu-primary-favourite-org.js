0d68f75bf8608f95b834182b5aeb7c5cd019f741
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-07-29 10:59:00 +0100

    move groupId casting to toString

--- shared/filters/left-menu-primary-favourite-org.js
+++ shared/filters/left-menu-primary-favourite-org.js
@@ -3,6 +3,7 @@
 var favouriteFilter = require('./left-menu-primary-favourite');
 
 module.exports = function leftMenuFavouriteOrg(room, groupId) {
-  var id = room.groupId + '';
+  var id = '';
+  if(room.groupId && room.groupId.toString) { id = room.groupId.toString(); }
   return favouriteFilter(room) && id === groupId && room.roomMember;
 };
--- shared/filters/left-menu-primary-org.js
+++ shared/filters/left-menu-primary-org.js
@@ -3,6 +3,7 @@
 var defaultFilter = require('./left-menu-primary-default');
 
 module.exports = function leftMenuFavouriteOrg(room, groupId) {
-  var id = room.groupId + '';
+  var id = '';
+  if(room.groupId && room.groupId.toString) { id = room.groupId.toString(); }
   return defaultFilter(room) && id === groupId && room.roomMember;
 };
