5c454a4164737da4e355c5052fb677e192d7a1c5
Le Roux Bodenstein <leroux@gitter.im>
2016-08-01 16:02:38 +0200

    Remove the room from everyone's groups when deleting the room.

--- server/services/room-service.js
+++ server/services/room-service.js
@@ -733,7 +733,7 @@ function deleteRoom(troupe) {
         }))
         .then(function() {
           // Remove all the folk from the room
-          return roomMembershipService.removeRoomMembers(troupe._id, userIds/*, groupId */);
+          return roomMembershipService.removeRoomMembers(troupe._id, userIds, troupe.groupId);
         });
     })
     .then(function() {
