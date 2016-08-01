934da6e8c3f3b8e7401c02ee449e3470bd3ecbb0
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-08-01 11:56:47 +0100

    added category list container

--- modules/topics-ui/shared/components/forum/category-list.jsx
+++ modules/topics-ui/shared/components/forum/category-list.jsx
@@ -2,13 +2,32 @@
 
 var React = require('react');
 var Container = require('../container.jsx');
+var Panel = require('../panel.jsx');
 
 module.exports = React.createClass({
+
+  displayName: 'CategoryList',
+  propTypes: {
+    categories: React.PropTypes.array.isRequired
+  },
+
   render(){
+
+    const { categories } = this.props;
+
     return (
       <Container>
-        { this.props.children }
+        <Panel>
+          <ul className="category-list">{ categories.map(this.getChildCategory) }</ul>
+        </Panel>
       </Container>
     );
+  },
+
+  getChildCategory(category, index){
+    return (
+      <li key={`forum-category-list-item-${index}`}>{ category }</li>
+    );
   }
+
 });
