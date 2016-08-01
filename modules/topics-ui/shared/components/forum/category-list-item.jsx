aaa19db3b8353d6c0e321cb3b246fe564d04dd4a
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-08-01 13:02:49 +0100

    added category list item

---
+++ modules/topics-ui/shared/components/forum/category-list-item.jsx
@@ -0,0 +1,22 @@
+"use strict";
+
+var React = require('react');
+
+module.exports = React.createClass({
+
+  displayName: 'CategoryListItem',
+  propTypes: {
+    category: React.PropTypes.string.isRequired,
+    active: React.PropTypes.bool.isRequired,
+    onClick: React.PropTypes.func.isRequired,
+  },
+
+  render(){
+    const { category, active, onClick } = this.props;
+    var className = 'subdued-button-clouds--xsmall';
+    if(active) { className = 'button-clouds--xsmall'; }
+    return (
+      <button className={ className } onClick={() => onClick(category)}>{ category }</button>
+    );
+  }
+});
