b82200310a3600b89582dbd6961a623b44e7d2b9
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-08-02 17:26:38 +0100

    added route dispatching to ForumContainer

---
+++ modules/topics-ui/test/specs/containers/forum-container-test.jsx
@@ -0,0 +1,35 @@
+"use strict";
+
+var assert = require('assert');
+var Backbone = require('backbone');
+var React = require('react');
+var { shallow } = require('enzyme');
+var ForumContainer = require('../../../containers/ForumContainer.jsx');
+var dispatcher = require('../../../browser/js/dispatcher');
+
+var Collection = Backbone.Collection.extend({
+  getCategories(){ return this.models.map((m) => m.toJSON() ); }
+});
+
+describe.only('<ForumContainer />', function(){
+
+  let wrapper;
+  let collection;
+
+  beforeEach(function(){
+    collection = new Collection([ { category: 'all', active: true } ]);
+    wrapper = shallow(<ForumContainer categoryStore={collection} groupName='gitterHQ' />);
+  });
+
+  it('should dispatch the right action when a category is clicked', function(done){
+
+    dispatcher.on('navigate-to', function(data){
+      assert(true);
+      done();
+    });
+
+    wrapper.find('CategoryList').prop('onCategoryClicked')('all');
+
+  });
+
+});
