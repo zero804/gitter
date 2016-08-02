7f336c0fddaaee391df50171bac92ef709bd8dc1
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-08-02 17:59:34 +0100

    update client side category store to update active states

--- modules/topics-ui/browser/js/stores/forum-category-store.js
+++ modules/topics-ui/browser/js/stores/forum-category-store.js
@@ -8,7 +8,19 @@ var Model = Backbone.Model.extend({
 
 module.exports = Backbone.Collection.extend({
   model: Model,
+
+  initialize: function(models, attrs) {
+    this.router = attrs.router;
+    this.listenTo(this.router, 'change:categoryName', this.onCategoryUpdate, this);
+  },
+
   getCategories: function() {
     return this.models.map(model => model.toJSON());
+  },
+
+  onCategoryUpdate(model, val){
+    this.where({ active: true }).forEach((m) => m.set('active', false));
+    this.findWhere({ category: val }).set('active', true);
   }
+
 });
