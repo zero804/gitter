1fcad1c394797b8ede61e533f6bf700acfd5c3dd
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-08-02 17:59:34 +0100

    update client side category store to update active states

--- modules/topics-ui/test/specs/browser/stores/forum-category-store-test.js
+++ modules/topics-ui/test/specs/browser/stores/forum-category-store-test.js
@@ -5,4 +5,20 @@ var Backbone = require('backbone');
 var CategoryStore = require('../../../../browser/js/stores/forum-category-store');
 var serverSideStore = require('../../../../server/stores/forum-category-store.js');
 
-describe('ForumCategoryStore', function(){});
+describe('ForumCategoryStore', function(){
+  var router;
+  var categories;
+  var categoryStore;
+  beforeEach(function(){
+    categories = [ { category: 'all', active: true }, { category: 'test-1', active: false } ];
+    router = new Backbone.Model({ route: 'forum', categoryName: 'all' });
+    categoryStore = new CategoryStore(categories, { router: router });
+  });
+
+  it.only('should update the active element when the route changes', function(){
+    router.set('categoryName', 'test-1');
+    assert.equal(categoryStore.at(0).get('active'), false);
+    assert(categoryStore.at(1).get('active'));
+  });
+
+});
