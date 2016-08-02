4f4df1f56e70970d859964b0ac9a1bdba9ca9424
Le Roux Bodenstein <leroux@gitter.im>
2016-08-02 22:29:49 +0200

    WIP scripts for setting up fixtures & injecting new livecollection events

---
+++ scripts/fixtures/create-comment.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+'use strict';
+
+/*
+INPUT:
+
+* username
+* replyUri (TODO: or topic uri or id? Depends on if replies & comments have uris or not)
+* (optional) text - defaults to fake data
+
+Adds a reply to the specified reply as the specified user.
+*/
---
+++ scripts/fixtures/create-reply.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+'use strict';
+
+/*
+INPUT:
+
+* username
+* topicUri
+* (optional) text - defaults to fake data
+
+Adds a reply to the specified topic as the specified user.
+*/
