217e0e9d44cf9699a51c51f8f6e04df050e3b2d5
Andrew Newdigate <andrew@troupe.co>
2016-08-04 10:35:45 +0100

    Merge conflict hell

---
+++ server/services/group-creation-service.js
@@ -0,0 +1,119 @@
+"use strict";
+
+var env = require('gitter-web-env');
+var errorReporter = env.errorReporter;
+var Promise = require('bluebird');
+var groupService = require('gitter-web-groups/lib/group-service');
+var policyFactory = require('gitter-web-permissions/lib/policy-factory');
+var GroupWithPolicyService = require('./group-with-policy-service');
+var RoomWithPolicyService = require('./room-with-policy-service');
+var clientEnv = require('gitter-client-env');
+var TwitterBadger = require('gitter-web-twitter/lib/twitter-badger');
+var identityService = require('gitter-web-identity');
+
+function inviteTroubleTwitterUsers(user, room, invitesReport) {
+  return identityService.getIdentityForUser(user, 'twitter')
+    .then(function(identity) {
+      user.twitterUsername = identity.username;
+
+      var usersToTweet = [];
+      invitesReport.forEach(function(report) {
+        if(report.status === 'error' && report.inviteInfo.type === 'twitter') {
+          usersToTweet.push({
+            twitterUsername: report.inviteInfo.externalId
+          });
+        }
+      });
+
+      var roomUrl = room.lcUri ? (clientEnv['basePath'] + '/' + room.lcUri) : undefined;
+
+      return TwitterBadger.sendUserInviteTweets(user, usersToTweet, roomUrl);
+    });
+}
+
+/**
+ * Create a group with a default room and invite some people
+ *
+ * Returns
+ *  {
+ *    group: ...,
+ *    defaultRoom: ...,
+ *    invitesReport: ...
+ *  }
+ */
+function groupCreationService(user, options) {
+  return groupService.createGroup(user, options)
+    .bind({
+      group: null,
+      defaultRoom: null,
+      invitesReport: null,
+      hookCreationFailedDueToMissingScope: null
+    })
+    .then(function(group) {
+      this.group = group;
+      return policyFactory.createPolicyForGroupId(user, group._id);
+    })
+    .then(function(userGroupPolicy) {
+      var group = this.group;
+      var groupWithPolicyService = new GroupWithPolicyService(this.group, user, userGroupPolicy);
+
+      var defaultRoomOptions = options.defaultRoom;
+      var defaultRoomName = defaultRoomOptions.defaultRoomName || 'Lobby';
+
+      return groupWithPolicyService.createRoom({
+        name: defaultRoomName,
+
+        // default rooms are always public
+        security: 'PUBLIC',
+
+        // New way:
+        // type: 'GROUP',
+
+        // Old way
+        // use the same backing object for the default room
+        type: group.sd.type,
+        linkPath: group.sd.linkPath,
+
+        // only github repo based rooms have the default room automatically
+        // integrated with github
+        // This is going to have to change in the new GROUP world
+        runPostGitHubRoomCreationTasks: group.sd.type === 'GH_REPO',
+
+        addBadge: defaultRoomOptions.addBadge,
+        providers: defaultRoomOptions.providers
+      });
+    })
+    .then(function(createRoomResult) {
+      this.hookCreationFailedDueToMissingScope = createRoomResult.hookCreationFailedDueToMissingScope;
+      var defaultRoom = this.defaultRoom = createRoomResult.troupe;
+
+      if (!options.invites || !options.invites.length) {
+        // No invites to send out....
+        return [];
+      }
+
+      // Invite all the users
+      return policyFactory.createPolicyForRoomId(user, defaultRoom._id)
+        .then(function(userRoomPolicy) {
+          var roomWithPolicyService = new RoomWithPolicyService(defaultRoom, user, userRoomPolicy);
+
+          // Some of these can fail, but the errors will be caught and added to
+          // the report that the promise resolves to.
+          return roomWithPolicyService.createRoomInvitations(options.invites);
+        });
+    })
+    .then(function(invitesReport) {
+      this.invitesReport = invitesReport;
+
+      if(options.allowTweeting) {
+        inviteTroubleTwitterUsers(user, this.defaultRoom, invitesReport)
+          .catch(function(err) {
+            errorReporter(err, { user: user.username }, { module: 'group-creation-service' });
+          });
+      }
+
+      return this;
+    });
+}
+
+module.exports = Promise.method(groupCreationService);
