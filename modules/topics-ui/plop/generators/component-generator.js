18e7b341c1203567a80c19dbf016e96d05184ea9
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-08-08 17:41:34 +0100

    start building out component generator

---
+++ modules/topics-ui/plop/generators/component-generator.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var path = require('path');
+var glob = require('glob');
+
+module.exports = function(plop) {
+
+  var baseComponentDir = path.resolve(__dirname, '../../shared/components');
+  var baseComponentDirs = glob.sync(baseComponentDir + '/*/');
+  var componentDirs = ['./'].concat(baseComponentDirs.map(function(folderPath){
+    return '.' + folderPath.replace(baseComponentDir, '');
+  }));
+
+  plop.setGenerator('component', {
+    description: 'Generate a react component',
+    prompts: [{
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your component?',
+      validate: function (value) {
+        if ((/.+/).test(value)) { return true; }
+        return 'name is required';
+      }
+    },
+    {
+      type: 'list',
+      name: 'subFolder',
+      message: 'Pick a folder for your component',
+      choices: componentDirs,
+    }],
+    actions: function(data){
+      return [{
+
+      }]
+    }
+  });
+
+};
