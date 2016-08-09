abcab336fc0f15dc7a497057ebe08d0aed04f1db
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-08-09 09:58:40 +0100

    basic ss collection template

---
+++ modules/topics-ui/plop/generators/store-generator.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var path = require('path');
+
+module.exports = function(plop) {
+
+  plop.setGenerator('store', {
+    description: 'Generate a store',
+    prompts: [{
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your resource',
+      validate: function (value) {
+        if ((/.+/).test(value)) { return true; }
+        return 'name is required';
+      }
+    },
+    {
+      type: 'list',
+      name: 'resourceType',
+      message: 'What kind of resource are you modeling',
+      choices: [ 'Model', 'Collection' ]
+    }],
+    actions: function(data){
+
+      var serverSideTemplate = data.resourceType === 'Collection' ?
+        path.resolve(__dirname, '../templates/store-server-collection.txt') :
+        '';
+
+      return [{
+        type: 'add',
+        path: path.resolve(__dirname, '../../server/stores/{{dashCase name}}'),
+        templateFile: serverSideTemplate
+      }];
+
+    }
+  });
+};
