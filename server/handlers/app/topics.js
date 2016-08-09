05cede13a24081d8b84bb80e509dd1b616951bff
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-08-09 15:40:12 +0100

    setup create-topic route on server

---
+++ server/handlers/app/topics.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var express = require('express');
+var topicsRenderers = require('../renderers/topics');
+var identifyRoute = require('gitter-web-env').middlewares.identifyRoute;
+var featureToggles = require('../../web/middlewares/feature-toggles');
+
+var router = express.Router({ caseSensitive: true, mergeParams: true });
+
+router.get('/',
+  identifyRoute('forum'),
+  featureToggles,
+  function(req, res, next){
+    return topicsRenderers.renderForum(req, res, next);
+  }
+);
+
+router.get('/categories/:categoryName',
+  identifyRoute('forum'),
+  featureToggles,
+  function(req, res, next){
+    return topicsRenderers.renderForum(req, res, next);
+  }
+);
+
+router.get('/create-topic',
+  identifyRoute('create-topic'),
+  featureToggles,
+  function(req, res, next){
+    return topicsRenderers.renderForum(req, res, next, {
+      createTopic: true
+    });
+  }
+);
+
+module.exports = router;
