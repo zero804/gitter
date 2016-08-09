eac57aaf15d49c5e2b32744fe87da188793b2206
Le Roux Bodenstein <leroux@gitter.im>
2016-08-09 21:47:46 +0200

    scripts/fixtures/create-topic.js

--- scripts/fixtures/create-topic.js
+++ scripts/fixtures/create-topic.js
@@ -1,15 +1,58 @@
 #!/usr/bin/env node
 'use strict';
 
-/*
-INPUT:
+var yargs = require('yargs');
+var faker = require('faker');
+var StatusError = require('statuserror');
+var utils = require('./fixture-script-utils');
+var categoryService = require('gitter-web-forum-categories/lib/forum-category-service');
 
-* username
-* (optional) forumUri - only required if categoryUri is not set
-* (optional) categoryUri - defaults to the defaults to the default category if
-                           forumUri is set
-* (optional) title - defaults to fake data
-* (optional) text - defaults to fake data
 
-Adds a topic to the specified category as the specified user.
-*/
+var argv = yargs.argv;
+var opts = yargs
+  .option('username', {
+    required: true,
+    description: 'username of the user that should perform the action'
+  })
+  .option('group', {
+    required: true,
+    description: 'group uri of the group containing the forum'
+  })
+  .option('category', {
+    required: true,
+    description: 'category slug of the category you want to post to'
+  })
+  .option('title', {
+    required: false,
+    description: 'topic title (optional)'
+  })
+  .option('text', {
+    required: false,
+    description: 'topic text (optional)'
+  })
+  .help('help')
+  .alias('help', 'h')
+  .argv;
+
+utils.runScript(function() {
+  return utils.getForumWithPolicyService(opts.username, opts.group)
+    .then(function(forumWithPolicyService) {
+      this.forumWithPolicyService = forumWithPolicyService;
+      return categoryService.findBySlugForForum(this.forum._id, opts.category);
+    })
+    .then(function(category) {
+      if (!category) throw new StatusError(404, 'Category not found.');
+
+      var forumWithPolicyService = this.forumWithPolicyService;
+      return forumWithPolicyService.createTopic(category, {
+        title: opts.title || faker.commerce.productName(),
+        text: opts.text || faker.hacker.phrase(),
+        // TODO: slug
+        // TODO: tags
+        // TODO: sticky
+      })
+    })
+    .then(function(topic) {
+      console.log("CREATED " + topic.id);
+    });
+});
