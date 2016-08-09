fc27cc4f9a91e8b4f63460c59e10a3af917b368d
Le Roux Bodenstein <leroux@gitter.im>
2016-08-09 18:10:35 +0200

    scripts/fixtures/create-forum.js

--- scripts/fixtures/create-forum.js
+++ scripts/fixtures/create-forum.js
@@ -1,16 +1,29 @@
 #!/usr/bin/env node
 'use strict';
 
-/*
-INPUT:
+var yargs = require('yargs');
+var utils = require('./fixture-script-utils');
 
-* username
-* groupUri
-* (optional) name
-* (optional) defaultCategoryName
-* (optional) defaultTopicTitle
+var argv = yargs.argv;
+var opts = yargs
+  .option('username', {
+    required: true,
+    description: 'username of the user that should perform the action'
+  })
+  .option('group', {
+    required: true,
+    description: 'group uri of the group that you want to add a forum to'
+  })
+  .help('help')
+  .alias('help', 'h')
+  .argv;
 
-Adds a forum called "Topics" to the group as the specified user.
-Adds a "General" category and sets that as the default category.
-Adds a "Welcome to {group.name}" topic and sets it as the default topic.
-*/
+utils.runScript(function() {
+  return utils.getGroupWithPolicyService(opts.username, opts.group)
+    .then(function(groupWithPolicyService) {
+      return groupWithPolicyService.createForum()
+    })
+    .then(function(forum) {
+      console.log("CREATED " + forum.id);
+    });
+});
