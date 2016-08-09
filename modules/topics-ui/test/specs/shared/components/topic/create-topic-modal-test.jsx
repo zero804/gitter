5dab1c3556d11dee80c2eb9731d6abe742e75da2
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-08-09 16:48:29 +0100

    update generators and make test pass

---
+++ modules/topics-ui/test/specs/shared/components/topic/create-topic-modal-test.jsx
@@ -0,0 +1,16 @@
+"use strict";
+
+import assert from 'assert';
+import React from 'react';
+import { shallow } from 'enzyme';
+import CreateTopicModal from '../../../../../shared/components/topic/create-topic-modal.jsx';
+
+describe('<CreateTopicModal/>', () => {
+
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<CreateTopicModal/>);
+  });
+
+});
