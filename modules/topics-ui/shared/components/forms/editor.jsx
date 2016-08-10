b7bf2b8fdee744c435b99206c335d0d9459916d1
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-08-10 14:15:33 +0100

    added submit row

--- modules/topics-ui/shared/components/forms/editor.jsx
+++ modules/topics-ui/shared/components/forms/editor.jsx
@@ -7,15 +7,16 @@ export default React.createClass({
   displayName: 'Editor',
   propTypes: {
     className: PropTypes.string,
+    name: PropTypes.string
   },
 
   render(){
 
-    const { className } = this.props;
+    const { className, name } = this.props;
     const compiledClass = !!className ? `editor ${className}` : 'editor';
 
     return (
-      <textarea className={compiledClass}>
+      <textarea className={compiledClass} name={name}>
         { this.props.children }
       </textarea>
     );
