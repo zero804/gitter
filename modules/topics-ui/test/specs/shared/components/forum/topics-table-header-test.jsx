c825c296108897064a078b40e9deb11e7bc26bd0
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-08-10 15:53:11 +0100

    added topics table

---
+++ modules/topics-ui/test/specs/shared/components/forum/topics-table-header-test.jsx
@@ -0,0 +1,41 @@
+"use strict";
+
+import {equal} from 'assert';
+import React from 'react';
+import { mount } from 'enzyme';
+import TopicsTableHeader from '../../../../../shared/components/forum/topics-table-header.jsx';
+
+describe('<TopicsTableHeader/>', () => {
+
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(<TopicsTableHeader/>);
+  });
+
+  it('render a thead', () => {
+    equal(wrapper.find('thead').length, 1);
+  });
+
+  it('should render 4 th elements', () => {
+    console.log(wrapper.debug());
+    equal(wrapper.find('th').length, 4);
+  });
+
+  it('should render with the right class', () => {
+    equal(wrapper.find('.topics-table-header').length, 1);
+  });
+
+  it('should render the th elements with the right class', () => {
+    equal(wrapper.find('.topics-table-header__cell').length, 2);
+  });
+
+  it('it should render one first element', () => {
+    equal(wrapper.find('.topics-table-header__cell--first').length, 1);
+  });
+
+  it('it should render one last element', () => {
+    equal(wrapper.find('.topics-table-header__cell--last').length, 1);
+  });
+
+});
--- modules/topics-ui/test/specs/shared/components/forum/topics-table-test.jsx
+++ modules/topics-ui/test/specs/shared/components/forum/topics-table-test.jsx
@@ -8,13 +8,26 @@ import TopicsTable from '../../../../../shared/components/forum/topics-table.jsx
 describe('<TopicsTable/>', () => {
 
   let wrapper;
+  let topics = [];
 
   beforeEach(() => {
-    wrapper = shallow(<TopicsTable/>);
+    wrapper = shallow(<TopicsTable topics={topics}/>);
   });
 
   it('should render a container', () => {
-      equal(wrapper.find('Container').length, 1);
+    equal(wrapper.find('Container').length, 1);
+  });
+
+  it('should render a table', () => {
+    equal(wrapper.find('table').length, 1);
+  });
+
+  it('should render a TopicsTableHeader', () => {
+    equal(wrapper.find('TopicsTableHeader').length, 1);
+  });
+
+  it('should render the table with the right class', () => {
+    equal(wrapper.find('.topics-table').length, 1);
   });
 
 });
