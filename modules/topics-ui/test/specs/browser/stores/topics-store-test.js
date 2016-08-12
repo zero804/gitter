f9a81fad1c66237ce5d5604aa5025e39cc8942ed
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-08-12 11:22:11 +0100

    plumb in getTopic

--- modules/topics-ui/test/specs/browser/stores/topics-store-test.js
+++ modules/topics-ui/test/specs/browser/stores/topics-store-test.js
@@ -6,12 +6,21 @@ var Store = require('../../../../browser/js/stores/topics-store.js');
 describe('TopicsStore', () => {
 
   let store;
+  const models = [
+    { id: 1 },
+    { id: 2 },
+  ];
+
   beforeEach(() => {
-    store = new Store();
+    store = new Store(models);
   });
 
   it('should provide a getTopics()', () => {
     assert(store.getTopics);
+  });
+
+  it('should get a topic but id', () => {
+    assert.deepEqual(store.getById(1), models[0]);
   });
 
 });
