a39c54bf9c576e8aaa45024228553039b8aad51e
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-08-17 13:48:01 +0100

    move strings into constants

--- modules/topics-ui/browser/js/constants/navigation.js
+++ modules/topics-ui/browser/js/constants/navigation.js
@@ -3,6 +3,7 @@
 module.exports = {
   NAVIGATE_TO: 'navigate-to',
   FORUM_ROUTE: 'forum',
+  CREATE_TOPIC_ROUTE: 'create-topic',
 
   DEFAULT_CATEGORY_NAME: 'all',
   DEFAULT_FILTER_NAME: 'activity',
