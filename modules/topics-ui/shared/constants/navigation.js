5d194ff2576dbeba146723aa70be8459727d5e86
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-08-18 12:59:55 +0100

    fixup some merge erros

--- modules/topics-ui/shared/constants/navigation.js
+++ modules/topics-ui/shared/constants/navigation.js
@@ -4,3 +4,4 @@ export const DEFAULT_CATEGORY_NAME = 'all';
 export const DEFAULT_FILTER_NAME = 'activity';
 export const DEFAULT_TAG_NAME = 'all-tags';
 export const DEFAULT_SORT_NAME = 'most-recent';
+export const CREATE_TOPIC_ROUTE = 'create-topic';
