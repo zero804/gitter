035cee71385a76dd6c6ead0f65e79ca9ee3dddb2
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-08-18 14:05:46 +0100

    fix typo

--- modules/topics-ui/test/specs/browser/app-test.jsx
+++ modules/topics-ui/test/specs/browser/app-test.jsx
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import Backbone from 'backbone';
 import React from 'react';
 import App from 'gitter-web-topics-ui/browser/js/app.jsx';
-1
+
 describe.skip('App', function() {
 
   it('should set the right state when rendered with the forum route', function(){
