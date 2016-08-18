a1103f65c4129140135d5b13f6b9d16d090d7078
Andrew Newdigate <andrew@troupe.co>
2016-08-18 10:24:48 +0100

    🛠 Build: added an assembly phase between compile and package

--- build-scripts/gulpfile-embed-topics.js
+++ build-scripts/gulpfile-embed-topics.js
@@ -13,17 +13,17 @@ var TOPIC_RELOCATIONS = {
 }
 
 var TOPIC_RELOCATION_TASKS = Object.keys(TOPIC_RELOCATIONS).map(function(key) {
-  return 'embedtopics:post-compile:' + key;
+  return 'embedtopics:post-assemble:' + key;
 });
 
 Object.keys(TOPIC_RELOCATIONS).forEach(function(key) {
   var dest = TOPIC_RELOCATIONS[key];
 
-  gulp.task('embedtopics:post-compile:' + key, function() {
+  gulp.task('embedtopics:post-assemble:' + key, function() {
     return gulp.src(['modules/topics-ui/output/' + key + '/**', '!**/*.map'], { stat: true, base: 'modules/topics-ui/output/' + key })
       .pipe(gulp.dest(dest))
       .pipe(restoreTimestamps());
   });
 });
 
-gulp.task('embedtopics:post-compile', TOPIC_RELOCATION_TASKS);
+gulp.task('embedtopics:post-assemble', TOPIC_RELOCATION_TASKS);
