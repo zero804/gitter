05b0db0b438161e555e7604d24157c9049e850f3
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-08-19 10:49:48 +0100

    preparing to post data to the server

--- modules/topics-ui/browser/js/stores/topics-store.js
+++ modules/topics-ui/browser/js/stores/topics-store.js
@@ -1,12 +1,27 @@
 import { Model, Collection } from 'backbone';
+import {subscribe} from '../../../shared/dispatcher';
+import {SUBMIT_NEW_TOPIC} from '../../../shared/constants/create-topic';
 
 var TopicModel = Model.extend({
-  defaults: {}
+  defaults: {},
+  url(){
+    return !this.get('id') ? `/api/v1/forums/:forumId/topics` : '';
+  },
+
+  sync(){
+    //Need to abstract and pull in the apiClient here so this is a bodge
+    console.log('working');
+  }
+
 });
 
 export default Collection.extend({
 
   model: TopicModel,
+
+  initialize(){
+    subscribe(SUBMIT_NEW_TOPIC, this.creatNewTopic, this);
+  },
 
   getTopics() {
     return this.models.map(model => model.toJSON());
@@ -14,6 +29,13 @@ export default Collection.extend({
 
   getById(id){
     return this.get(id).toJSON();
+  },
+
+  creatNewTopic(title, body){
+    console.log('-----------------------');
+    console.log(title, body);
+    console.log('-----------------------');
+    this.create({ title: title, body: body });
   }
 
 });
