ed3b2f1a4fa62ad30ab462e7d42604854c1a00bb
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-08-19 08:57:47 +0100

    added create new topic store events

--- modules/topics-ui/shared/constants/create-topic.js
+++ modules/topics-ui/shared/constants/create-topic.js
@@ -1,3 +1,5 @@
 export const TITLE_UPDATE = 'title-update';
 export const BODY_UPDATE = 'body-update';
 export const SUBMIT = 'submit';
+export const STORE_UPDATE = 'store-update';
+export const STORE_CREATE_NEW = 'create-new-topic';
