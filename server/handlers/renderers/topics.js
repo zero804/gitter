aa3f04ab29a63ca97783707d13f7e85fdc85df37
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-08-19 10:54:28 +0100

    added app context

--- server/handlers/renderers/topics.js
+++ server/handlers/renderers/topics.js
@@ -18,54 +18,68 @@ var forumStore = require('gitter-web-topics-ui/server/stores/forum-store');
 var navConstants = require('gitter-web-topics-ui/shared/constants/navigation');
 
 var restSerializer = require('../../serializers/rest-serializer');
+var contextGenerator = require('../../web/context-generator.js');
 
 
 function renderForum(req, res, next, options) {
 
+  //No switch, no business
   if (!req.fflip || !req.fflip.has('topics')) {
+    return next(new StatusError(404));
+  }
+
+  //Have to be logged in to get here
+  if(!req.user) {
     return next(new StatusError(404));
   }
 
   options = (options || {});
   var groupUri = req.params.groupName;
 
-  groupService.findByUri(groupUri)
-    .then(function(group){
-      return forumService.findById(group.forumId);
+  return contextGenerator.generateNonChatContext(req)
+    .then(function(context){
+      //context.accessToken
+      return groupService.findByUri(groupUri)
+        .then(function(group){
+          return forumService.findById(group.forumId);
+        })
+        .then(function(forum){
+          var strategy = new restSerializer.ForumStrategy();
+          return restSerializer.serializeObject(forum, strategy);
+        })
+        .then(function(forum){
+          var categoryName = (req.params.categoryName || navConstants.DEFAULT_CATEGORY_NAME);
+          var filterName = (req.query.filter || navConstants.DEFAULT_FILTER_NAME);
+          var tagName = (req.query.tag || navConstants.DEFAULT_TAG_NAME);
+          var sortName = (req.query.sort || navConstants.DEFAULT_SORT_NAME);
+
+          return res.render('topics/forum', {
+            layout: 'topics-layout',
+            hasCachedFonts: fonts.hasCachedFonts(req.cookies),
+            fonts: fonts.getFonts(),
+            componentData: {
+              forum: forum,
+
+              groupName: req.params.groupName,
+              categoryName: categoryName,
+              filterName: filterName,
+              tagName: tagName,
+              sortName: sortName,
+
+              createTopic: options.createTopic,
+
+              categoryStore: forumCategoryStore(forum.categories, categoryName),
+              tagStore: forumTagStore(forum.tags, tagName),
+              topicsStore: forumTopicsStore(forum.topics),
+              newTopicStore: newTopicStore(),
+              forumStore: forumStore(forum),
+            }
+          });
+        })
+
+
     })
-    .then(function(forum){
-      var strategy = new restSerializer.ForumStrategy();
-      return restSerializer.serializeObject(forum, strategy);
-    })
-    .then(function(forum){
-      var categoryName = (req.params.categoryName || navConstants.DEFAULT_CATEGORY_NAME);
-      var filterName = (req.query.filter || navConstants.DEFAULT_FILTER_NAME);
-      var tagName = (req.query.tag || navConstants.DEFAULT_TAG_NAME);
-      var sortName = (req.query.sort || navConstants.DEFAULT_SORT_NAME);
 
-      res.render('topics/forum', {
-        layout: 'topics-layout',
-        hasCachedFonts: fonts.hasCachedFonts(req.cookies),
-        fonts: fonts.getFonts(),
-        componentData: {
-          forum: forum,
-
-          groupName: req.params.groupName,
-          categoryName: categoryName,
-          filterName: filterName,
-          tagName: tagName,
-          sortName: sortName,
-
-          createTopic: options.createTopic,
-
-          categoryStore: forumCategoryStore(forum.categories, categoryName),
-          tagStore: forumTagStore(forum.tags, tagName),
-          topicsStore: forumTopicsStore(forum.topics),
-          newTopicStore: newTopicStore(),
-          forumStore: forumStore(forum),
-        }
-      });
-    })
 }
 
 
