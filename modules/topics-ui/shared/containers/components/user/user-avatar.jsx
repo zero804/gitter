fec610e43434a8233718fd868138ad87bf5f2502
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-08-23 11:06:08 +0100

    PR Feedback use custom class

--- modules/topics-ui/shared/containers/components/user/user-avatar.jsx
+++ modules/topics-ui/shared/containers/components/user/user-avatar.jsx
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import Avatar from '../avatar.jsx';
+import classNames from 'classnames';
 
 export default React.createClass({
 
@@ -7,6 +8,7 @@ export default React.createClass({
   propTypes: {
     width: PropTypes.number,
     height: PropTypes.number,
+    className: PropTypes.string,
     user: PropTypes.shape({
       avatarUrl: PropTypes.string.isRequired,
       displayName: PropTypes.string.isRequired,
@@ -14,10 +16,11 @@ export default React.createClass({
   },
 
   render(){
-    const { width, height } = this.props;
+    const { width, height, className } = this.props;
     const { avatarUrl, displayName } = this.props.user;
+    const compiledClass = classNames("avatar--user", className);
     return (
-      <Avatar title={displayName} src={avatarUrl} className="avatar--user" width={width} height={height}/>
+      <Avatar title={displayName} src={avatarUrl} className={compiledClass} width={width} height={height}/>
     );
   }
 
