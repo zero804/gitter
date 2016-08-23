476cdc8e32338173fadaad4a6f101457568d9b3b
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-08-23 11:22:24 +0100

    fixup some eslint

--- modules/topics-ui/test/specs/browser/stores/forum-store-test.js
+++ modules/topics-ui/test/specs/browser/stores/forum-store-test.js
@@ -1,3 +1,4 @@
+import {ok} from 'assert';
 import Store from '../../../../browser/js/stores/forum-store';
 
 describe('ForumStore', () => {
@@ -5,6 +6,10 @@ describe('ForumStore', () => {
   let store;
   beforeEach(() => {
     store = new Store();
+  });
+
+  it('should have at least one test', () => {
+    ok(store);
   });
 
 });
