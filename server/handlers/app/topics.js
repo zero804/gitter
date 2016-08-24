08dae912811f265f6d7bd9e163154e7f2d0849f0
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-08-24 11:36:29 +0100

    add /{groupName}/topics main frame renderer

--- server/handlers/app/topics.js
+++ server/handlers/app/topics.js
@@ -1,16 +1,42 @@
 "use strict";
 
 var express = require('express');
+var StatusError = require('statuserror');
 var topicsRenderers = require('../renderers/topics');
+var mainFrameRenderers = require('../renderers/main-frame');
 var identifyRoute = require('gitter-web-env').middlewares.identifyRoute;
 var featureToggles = require('../../web/middlewares/feature-toggles');
 
 var router = express.Router({ caseSensitive: true, mergeParams: true });
 
+//Main frame renderer for topic
 router.get('/',
   identifyRoute('forum'),
   featureToggles,
   function(req, res, next){
+
+    //No switch, no business
+    if (!req.fflip || !req.fflip.has('topics')) {
+      return next(new StatusError(404));
+    }
+
+    return mainFrameRenderers.renderMainFrame(req, res, next, {
+      subFrameLocation: '/' + req.params.groupName + '/topics/~topics'
+    });
+  }
+);
+
+//Render the topics view
+router.get('/~topics',
+  identifyRoute('forum'),
+  featureToggles,
+  function(req, res, next){
+
+    //No switch, no business
+    if (!req.fflip || !req.fflip.has('topics')) {
+      return next(new StatusError(404));
+    }
+
     return topicsRenderers.renderForum(req, res, next);
   }
 );
@@ -19,6 +45,12 @@ router.get('/categories/:categoryName',
   identifyRoute('forum'),
   featureToggles,
   function(req, res, next){
+
+    //No switch, no business
+    if (!req.fflip || !req.fflip.has('topics')) {
+      return next(new StatusError(404));
+    }
+
     return topicsRenderers.renderForum(req, res, next);
   }
 );
@@ -27,6 +59,12 @@ router.get('/create-topic',
   identifyRoute('create-topic'),
   featureToggles,
   function(req, res, next){
+
+    //No switch, no business
+    if (!req.fflip || !req.fflip.has('topics')) {
+      return next(new StatusError(404));
+    }
+
     return topicsRenderers.renderForum(req, res, next, {
       createTopic: true
     });
