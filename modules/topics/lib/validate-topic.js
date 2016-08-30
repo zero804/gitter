f7b4ac6578cabcd2939a996caf60c980d6783bb7
Le Roux Bodenstein <leroux@gitter.im>
2016-08-30 14:45:49 +0200

    validate topic tags against forum tags

--- modules/topics/lib/validate-topic.js
+++ modules/topics/lib/validate-topic.js
@@ -5,21 +5,25 @@ var validators = require('gitter-web-validators');
 
 function validateTopic(data, options) {
   options = options || {};
+
+  // allowedTags should come from forum.tags
   options.allowedTags = options.allowedTags || [];
 
   if (!validators.validateDisplayName(data.title)) {
-    throw new StatusError(400, 'Title is invalid.')
+    throw new StatusError(400, 'Title is invalid.');
   }
 
   if (!validators.validateSlug(data.slug)) {
-    throw new StatusError(400, 'Slug is invalid.')
+    throw new StatusError(400, 'Slug is invalid.');
   }
 
   if (!validators.validateMarkdown(data.text)) {
-    throw new StatusError(400, 'Text is invalid.')
+    throw new StatusError(400, 'Text is invalid.');
   }
 
-  // TODO: validate data.tags against options.allowedTags
+  if (!validators.validateTags(data.tags, options.allowedTags)) {
+    throw new StatusError(400, 'Tags are invalid.');
+  }
 
   return data;
 }
