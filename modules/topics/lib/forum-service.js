4fe4a06f1d47d145ad76cda60fc988d138e7443e
Le Roux Bodenstein <leroux@gitter.im>
2016-08-30 14:41:24 +0200

    validate forums before inserting them

--- modules/topics/lib/forum-service.js
+++ modules/topics/lib/forum-service.js
@@ -2,7 +2,9 @@
 
 var env = require('gitter-web-env');
 var stats = env.stats;
+var Promise = require('bluebird');
 var Forum = require('gitter-web-persistence').Forum;
+var validateForum = require('./validate-forum');
 
 
 function findById(forumId) {
@@ -13,19 +15,26 @@ function findById(forumId) {
 
 function createForum(user, forumInfo, securityDescriptor) {
   // we can't upsert because there's nothing unique on a Forum to check against
-  var insertData = {
+  var data = {
     tags: forumInfo.tags || [],
     sd: securityDescriptor
   };
-  return Forum.create(insertData)
-    .then(function(forum) {
-      stats.event('new_forum', {
-        // TODO: groupId would probably have been handy here? But leaky.
-        forumId: forum._id,
-        userId: user._id
-      });
 
-      return forum;
+  return Promise.try(function() {
+      return validateForum(data);
+    })
+    .bind({})
+    .then(function(insertData) {
+      return Forum.create(insertData)
+        .then(function(forum) {
+          stats.event('new_forum', {
+            // TODO: groupId would probably have been handy here? But leaky.
+            forumId: forum._id,
+            userId: user._id
+          });
+
+          return forum;
+        });
     });
 }
 
