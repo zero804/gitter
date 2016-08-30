842aec7670cb6861cd4db015a5bec3fca096f374
Eric Eastwood <contact@ericeastwood.com>
2016-08-30 02:26:12 -0500

    Add more logic and error stuff

---
+++ public/js/views/modals/permissions/submit-security-descriptor-status-constants.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var submitSecurityDescriptorStatusConstants = {
+  PENDING: 'pending',
+  COMPLETE: 'complete',
+  ERROR: 'error',
+  ERROR_NEED_TO_REQUEST_SD_BEFORE_UPDATE: 'error-need-to-request-sd-before-update'
+};
+
+
+module.exports = submitSecurityDescriptorStatusConstants;
