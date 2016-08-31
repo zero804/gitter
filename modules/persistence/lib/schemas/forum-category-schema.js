d168ef52169086d59e0950a6c0ccbc5cae51daca
Le Roux Bodenstein <leroux@gitter.im>
2016-08-31 16:48:06 +0200

    big-ass comment about category sort ordering.

--- modules/persistence/lib/schemas/forum-category-schema.js
+++ modules/persistence/lib/schemas/forum-category-schema.js
@@ -8,7 +8,19 @@ var ForumCategorySchema = new Schema({
   name: { type: String, required: true },
   slug: { type: String, required: true },
   forumId: { type: ObjectId, required: true },
-  order: { type: Number, "default": 10000000000, required: false }
+  /*
+  Go with a large number by default so that new ones get added to the end.
+  Otherwise mongo returns undefined/null at the start when you ask to order by
+  a number, ascending. Using Math.pow(2, 31)-1 just in case there are any max
+  int bugs around 32/64 bits or signed/unsigned, etc.
+
+  Other alternatives would be to ALWAYS fill in order (ie. make it required) or
+  to go withe order number, descending and make the default -1. But then if you
+  want to fill in a known order number and add a new one you have to adjust
+  them all. With ascending numbers you can just go with max order number +1.
+  (Bringing you back into contact with potential max integer size bugs..)
+  */
+  order: { type: Number, "default": 2147483647, required: false }
 }, { strict: "throw" });
 
 ForumCategorySchema.schemaTypeName = 'ForumCategorySchema';
