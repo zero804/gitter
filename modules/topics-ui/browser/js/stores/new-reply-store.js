4b218f8b923431705b619c2bab389a5efd69e4cf
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-08-31 17:39:06 +0100

    Update newReplyModel text attribute when editor changes

--- modules/topics-ui/browser/js/stores/new-reply-store.js
+++ modules/topics-ui/browser/js/stores/new-reply-store.js
@@ -1,5 +1,14 @@
 import {ReplyStore} from './replies-store';
+import {subscribe} from '../../../shared/dispatcher';
+import {BODY_UPDATE} from '../../../shared/constants/create-reply';
 
 export default ReplyStore.extend({
-  defaults: {}
+  defaults: {},
+  initialize() {
+    subscribe(BODY_UPDATE, this.onReplyBodyUpdate, this);
+  },
+
+  onReplyBodyUpdate(data){
+    this.set('text', data.value);
+  }
 });
--- modules/topics-ui/test/specs/shared/action-creators/create-reply/body-update-test.js
+++ modules/topics-ui/test/specs/shared/action-creators/create-reply/body-update-test.js
@@ -3,10 +3,20 @@ import {equal} from 'assert';
 import createAction from '../../../../../shared/action-creators//create-reply//body-update';
 import { BODY_UPDATE } from '../../../../../shared/constants/create-topic.js';
 
-describe('bodyUpdate', () => {
+describe('replyBodyUpdate', () => {
 
   it('should return the right type', () => {
-    equal(createAction().type, BODY_UPDATE);
+    equal(createAction('test').type, BODY_UPDATE);
+  });
+
+  it('should throw an error if no value is passed', () => {
+   try { createAction(); }
+   catch(e) { equal(e.message, 'updateReplyBody called with no value'); }
+  });
+
+  it('should correctly pass the value', () => {
+    const val = 'test';
+    equal(createAction(val).value, val, 'failed to pass the correct update value');
   });
 
 });
