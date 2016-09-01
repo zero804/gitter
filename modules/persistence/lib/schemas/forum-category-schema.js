d7ecf1146108bfaa894c34a1ba036990e6673665
Le Roux Bodenstein <leroux@gitter.im>
2016-09-01 11:45:13 +0200

    Move magic number a few lines up.

--- modules/persistence/lib/schemas/forum-category-schema.js
+++ modules/persistence/lib/schemas/forum-category-schema.js
@@ -4,23 +4,27 @@ var mongoose = require('gitter-web-mongoose-bluebird');
 var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
 
+
+
+/*
+Go with a large number by default so that new ones get added to the end.
+Otherwise mongo returns undefined/null at the start when you ask to order by
+a number, ascending. Using Math.pow(2, 31)-1 just in case there are any max
+int bugs around 32/64 bits or signed/unsigned, etc.
+
+Other alternatives would be to ALWAYS fill in order (ie. make it required) or
+to go withe order number, descending and make the default -1. But then if you
+want to fill in a known order number and add a new one you have to adjust
+them all. With ascending numbers you can just go with max order number +1.
+(Bringing you back into contact with potential max integer size bugs..)
+*/
+var DEFAULT_CATEGORY_ORDER = 2147483647;
+
 var ForumCategorySchema = new Schema({
   name: { type: String, required: true },
   slug: { type: String, required: true },
   forumId: { type: ObjectId, required: true },
-  /*
-  Go with a large number by default so that new ones get added to the end.
-  Otherwise mongo returns undefined/null at the start when you ask to order by
-  a number, ascending. Using Math.pow(2, 31)-1 just in case there are any max
-  int bugs around 32/64 bits or signed/unsigned, etc.
-
-  Other alternatives would be to ALWAYS fill in order (ie. make it required) or
-  to go withe order number, descending and make the default -1. But then if you
-  want to fill in a known order number and add a new one you have to adjust
-  them all. With ascending numbers you can just go with max order number +1.
-  (Bringing you back into contact with potential max integer size bugs..)
-  */
-  order: { type: Number, "default": 2147483647, required: false }
+  order: { type: Number, "default": DEFAULT_CATEGORY_ORDER, required: false }
 }, { strict: "throw" });
 
 ForumCategorySchema.schemaTypeName = 'ForumCategorySchema';
