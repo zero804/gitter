e6e0b89df42eea6b8fcf0a1d54d13b16220a87e1
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-09-02 11:12:07 +0100

    fixed tests

--- modules/topics-ui/test/specs/browser/stores/replies-store-test.js
+++ modules/topics-ui/test/specs/browser/stores/replies-store-test.js
@@ -1,11 +1,17 @@
 import assert from 'assert';
 import {RepliesStore} from '../../../../browser/js/stores/replies-store';
+import forumStore from '../../../mocks/forum-store';
+import replies from '../../../mocks/mock-data/replies';
+import router from '../../../mocks/router';
 
 describe('RepliesStore', () => {
 
   let store;
   beforeEach(() => {
-    store = new RepliesStore();
+    store = new RepliesStore(replies, {
+      forumStore: forumStore,
+      router: router
+    });
   });
 
   it('should provide a getReplies()', () => {
