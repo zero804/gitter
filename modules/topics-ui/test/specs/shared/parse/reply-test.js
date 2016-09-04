93376d57698410bd4da43d4abed31b9b2acd0a30
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-09-04 12:38:51 +0100

    Fixup broken tests

--- modules/topics-ui/test/specs/shared/parse/reply-test.js
+++ modules/topics-ui/test/specs/shared/parse/reply-test.js
@@ -4,7 +4,11 @@ import parseReply from '../../../../shared/parse/reply';
 describe('parseReply', () => {
 
   it('should provide a sent attribute in the right format', () => {
-    const expected = { sent: '2016-08-24T15:17:22.439Z', formattedSentDate: 'Aug 24th' };
+    const expected = {
+      sent: '2016-08-24T15:17:22.439Z',
+      formattedSentDate: 'Aug 24th',
+      displayText: undefined
+    };
     const result = parseReply({ sent: '2016-08-24T15:17:22.439Z' });
     deepEqual(result, expected);
   });
