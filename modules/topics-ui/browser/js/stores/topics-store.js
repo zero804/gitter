2ac14a4477a3114f069664076cb23d5bf1966c71
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-09-04 11:33:51 +0100

    Fix create topic modal

--- modules/topics-ui/browser/js/stores/topics-store.js
+++ modules/topics-ui/browser/js/stores/topics-store.js
@@ -1,55 +1,26 @@
 import Backbone from 'backbone';
 import {subscribe} from '../../../shared/dispatcher';
 import {SUBMIT_NEW_TOPIC, TOPIC_CREATED} from '../../../shared/constants/create-topic';
-import $ from 'jquery';
 import parseTag from '../../../shared/parse/tag';
 import {getRealtimeClient} from './realtime-client';
 import LiveCollection from './live-collection';
 import dispatchOnChangeMixin from './mixins/dispatch-on-change';
-import {getAccessToken} from './access-token-store';
 import {getForumId, getForumStore} from './forum-store';
+import {BaseModel} from './base-model';
 
-export const TopicModel = Backbone.Model.extend({
-  defaults: {},
+export const TopicModel = BaseModel.extend({
   url(){
     return this.get('id') ? null : `/api/v1/forums/${getForumId()}/topics`;
   },
-
-  sync(){
-    //Need to abstract and pull in the apiClient here so this is a bodge
-    const headers = { "x-access-token": getAccessToken() }
-    const catId = this.collection.getCategoryId();
-    const data = JSON.stringify(Object.assign(this.toJSON(), {
-      categoryId: catId,
-    }));
-
-    $.ajax({
-      url: this.url(),
-      contentType: 'application/json',
-      type: 'POST',
-      headers: headers,
-      data: data,
-      success: this.onSuccess.bind(this),
-      error: this.onError.bind(this),
-    })
-  },
-
-  onSuccess(attrs) {
-    this.set(attrs);
-    this.trigger(TOPIC_CREATED, this);
-  },
-
-  onError(){},
 
   toJSON() {
     var data = this.attributes;
     data.tags = (data.tags || []);
     return Object.assign({}, data, {
-      tags: data.tags.map(parseTag)
+      tags: data.tags.map(parseTag),
+      categoryId: this.collection.getCategoryId(),
     });
   }
-
-
 });
 
 export const TopicsStore = LiveCollection.extend({
@@ -79,11 +50,13 @@ export const TopicsStore = LiveCollection.extend({
   },
 
   creatNewTopic(data){
-    const model = this.create({ title: data.title, text: data.body });
-    model.once(TOPIC_CREATED, () => this.trigger(TOPIC_CREATED, {
-      topicId: model.get('id'),
-      slug: model.get('slug')
-    }));
+    const model = this.create({ title: data.title, text: data.body }, { wait: true });
+    model.once('add', () => {
+      this.trigger(TOPIC_CREATED, {
+        topicId: model.get('id'),
+        slug: model.get('slug')
+      });
+    });
   },
 
   //TODO REMOVE
@@ -98,7 +71,7 @@ export const TopicsStore = LiveCollection.extend({
 dispatchOnChangeMixin(TopicsStore);
 
 const serverStore = (window.context.topicsStore || {});
-const serverData = (serverStore.data || {});
+const serverData = (serverStore.data || []);
 let store;
 export function getTopicsStore(data){
   if(!store) { store = new TopicsStore(serverData); }
