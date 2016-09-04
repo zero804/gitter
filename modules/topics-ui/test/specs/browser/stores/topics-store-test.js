86adfccfc20d8fe0c1cb836ec48a43089e53cd0f
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-09-04 14:12:46 +0100

    Abstract out ajax into base model

--- modules/topics-ui/test/specs/browser/stores/topics-store-test.js
+++ modules/topics-ui/test/specs/browser/stores/topics-store-test.js
@@ -1,7 +1,4 @@
 import assert from 'assert';
-import {stub} from 'sinon';
-import {dispatch} from '../../../../shared/dispatcher';
-import submitNewTopic from '../../../../shared/action-creators/create-topic/submit-new-topic';
 
 //Mocks
 import topics from '../../../mocks/mock-data/topics';
@@ -25,14 +22,6 @@ describe('TopicsStore', () => {
 
   it('should get a topic but id', () => {
     assert.deepEqual(store.getById(1).id, topics[0].id);
-  });
-
-  it.skip('should call create after the submit new topic event', () => {
-    //Why is this called four times??
-    stub(Store.prototype, 'create');
-    store = new Store(topics);
-    dispatch(submitNewTopic('title', 'body'));
-    Store.prototype.create.restore();
   });
 
 });
