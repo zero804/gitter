86c63cb784b15ac68b66d57f4b661187f5561ea0
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-09-07 18:16:01 +0100

    added serverside topic filtering

--- modules/topics-ui/server/stores/topics-store.js
+++ modules/topics-ui/server/stores/topics-store.js
@@ -1,16 +1,41 @@
 "use strict";
 
 var _ = require('lodash');
+var navConstants = require('../../shared/constants/navigation');
+var forumFilterConstants = require('../../shared/constants/forum-filters');
 
-module.exports = function topicsStore(models) {
+module.exports = function topicsStore(models, category, tag, filter, user) {
 
   //Defaults
   models = (models || []);
+  category = (category || navConstants.DEFAULT_CATEGORY_NAME);
+  tag = (tag || navConstants.DEFAULT_TAG_NAME);
+  filter = (filter || navConstants.DEFAULT_FILTER_NAME)
+  user = (user || {});
 
   //Transform the server side models
   models = models.map((model) => {
     return model;
   });
+
+  //Filter based on the currently selected category
+  if(category !== navConstants.DEFAULT_CATEGORY_NAME) {
+    models = models.filter((m) => m.category.slug === category);
+  }
+
+  if(tag !== navConstants.DEFAULT_TAG_NAME) {
+    //Could this be more efficient? /cc @supreememoocow
+    models = models.filter((m) => m.tags.some((t) => t === tag));
+  }
+
+  if(filter !== navConstants.DEFAULT_FILTER_NAME) {
+    //Filter by my topics
+    if(filter === forumFilterConstants.FILTER_BY_TOPIC) {
+      models = models.filter((m) => m.user.username === user.username);
+    }
+    //TODO ...
+  }
+
 
   //Get resource
   const getTopics = () => models;
--- modules/topics-ui/test/mocks/mock-data/topics.js
+++ modules/topics-ui/test/mocks/mock-data/topics.js
@@ -1,7 +1,51 @@
 export default [
-  { title: '1', id: '1', slug: '1', name: '1', body: { html: 'test'}, category: "test", tags: [ '1', '2', '3' ], user: { displayName: 'test-user', avatarUrl: 'test-src'}},
-  { title: '2', id: '2', slug: '2', name: '2', body: { html: 'test'}, category: "test", tags: [ '1', '2', '3' ], user: { displayName: 'test-user', avatarUrl: 'test-src'}},
-  { title: '3', id: '3', slug: '3', name: '3', body: { html: 'test'}, category: "test", tags: [ '1', '2', '3' ], user: { displayName: 'test-user', avatarUrl: 'test-src'}},
-  { title: '4', id: '4', slug: '4', name: '4', body: { html: 'test'}, category: "test", tags: [ '1', '2', '3' ], user: { displayName: 'test-user', avatarUrl: 'test-src'}},
-  { title: '5', id: '5', slug: '5', name: '5', body: { html: 'test'}, category: "test", tags: [ '1', '2', '3' ], user: { displayName: 'test-user', avatarUrl: 'test-src'}},
+  {
+    title: '1',
+    id: '1',
+    slug: '1',
+    name: '1',
+    body: { html: 'test'},
+    category: { name: 'Test 1', slug: 'test-1'},
+    tags: [ '1', '2', '3' ],
+    user: { username: 'cutandpastey', avatarUrl: 'test-src'}
+  },
+  {
+    title: '2',
+    id: '2',
+    slug: '2',
+    name: '2',
+    body: { html: 'test'},
+    category: { name: 'Test 2', slug: 'test-2'},
+    tags: [ '2', '3', '4' ],
+    user: { username: 'test-user', avatarUrl: 'test-src'}
+  },
+  {
+    title: '3',
+    id: '3',
+    slug: '3',
+    name: '3',
+    body: { html: 'test'},
+    category: { name: 'Test 3', slug: 'test-3'},
+    tags: [ '3', '4', '5' ],
+    user: { username: 'cutandpastey', avatarUrl: 'test-src'}
+  },
+  {
+    title: '4',
+    id: '4', slug: '4',
+    name: '4',
+    body: { html: 'test'},
+    category: { name: 'Test 4', slug: 'test-4'},
+    tags: [ '4', '5', '6' ],
+    user: { username: 'test-user', avatarUrl: 'test-src'}
+  },
+  {
+    title: '5',
+    id: '5',
+    slug: '5',
+    name: '5',
+    body: { html: 'test'},
+    category: { name: 'Test 5', slug: 'test-5'},
+    tags: [ '5', '6', '7' ],
+    user: { username: 'test-user', avatarUrl: 'test-src'}
+  },
 ];
