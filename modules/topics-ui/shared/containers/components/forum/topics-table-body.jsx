f56d86579fb2c2c1ab553571efe091d0f469dcff
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-09-08 12:59:38 +0100

    added replying users to topic table

--- modules/topics-ui/shared/containers/components/forum/topics-table-body.jsx
+++ modules/topics-ui/shared/containers/components/forum/topics-table-body.jsx
@@ -16,14 +16,14 @@ export default React.createClass({
     const { topics } = this.props;
     return (
       <tbody className="topics-table-body">
-        {topics.map((topic, i) => this.renderChildRow(topic, i))}
+      {topics.map((topic, i) => this.renderChildRow(topic, i))}
       </tbody>
     );
   },
 
   renderChildRow(topic, i) {
     const {groupName} = this.props;
-    const {user} = topic;
+    const {user, replyingUsers} = topic;
     return (
       <tr className="topics-table-body__row" key={`topics-table-row-${i}`}>
         <td className="topics-table-body__cell--details">
@@ -39,10 +39,31 @@ export default React.createClass({
             {topic.title}
           </TopicLink>
         </td>
-        <td className="topics-table-body__cell">0</td>
+        <td className="topics-table-body__cell">
+          {this.getUserList(replyingUsers)}
+        </td>
         <td className="topics-table-body__cell">0</td>
         <td className="topics-table-body__cell">0</td>
       </tr>
+    );
+  },
+
+  getUserList(users){
+    return (
+      <ul className="topics-table-body__user-list">
+        {users.map(this.getUserListAvatar)}
+      </ul>
+    );
+  },
+
+  getUserListAvatar(user, i){
+    return (
+      <UserAvatar
+        width={10}
+        height={10}
+        user={user}
+        className="topics-table-body__user-list__item"
+        key={`user-list-item${i}`} />
     );
   }
 
