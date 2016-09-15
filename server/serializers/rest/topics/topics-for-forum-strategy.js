ed2c07d638f19aede94c927d9cf4fa1231c42bbb
Le Roux Bodenstein <leroux@gitter.im>
2016-09-15 20:53:06 +0200

    standardise ForumStrategy like TopicStrategy while at it

---
+++ server/serializers/rest/topics/topics-for-forum-strategy.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var Lazy = require('lazy.js');
+var _ = require('lodash');
+var topicService = require('gitter-web-topics/lib/topic-service');
+var TopicStrategy = require('../topic-strategy');
+
+function TopicsForForumStrategy(/*options*/) {
+  this.topicsByForum = null;
+  this.topicStrategy = null;
+}
+
+TopicsForForumStrategy.prototype = {
+  preload: function(forumIds) {
+    return topicService.findByForumIds(forumIds.toArray())
+      .bind(this)
+      .then(function(topics) {
+        this.topicsByForum = _.groupBy(topics, 'forumId');
+
+        var topicStrategy = this.topicStrategy = TopicStrategy.standard();
+        return topicStrategy.preload(Lazy(topics));
+      });
+  },
+
+  map: function(forumId) {
+    var topics = this.topicsByForum[forumId];
+    if (!topics || !topics.length) return [];
+
+    var topicStrategy = this.topicStrategy;
+    return _.map(topics, function(topic) {
+      return topicStrategy.map(topic);
+    })
+  },
+
+  name: 'TopicsForForumStrategy'
+};
+
+
+module.exports = TopicsForForumStrategy;
