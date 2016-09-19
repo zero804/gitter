78d3f324bddded3008ccbb8adbb103a9303edb6f
Le Roux Bodenstein <leroux@gitter.im>
2016-09-19 15:46:46 +0200

    Parse URI parameters in GET topics REST API.

---
+++ modules/topics/test/get-topics-filter-sort-options-test.js
@@ -0,0 +1,97 @@
+'use strict';
+
+var getTopicsFilterSortOptions = require('../lib/get-topics-filter-sort-options');
+var assert = require('assert');
+var Promise = require('bluebird');
+var StatusError = require('statuserror');
+
+describe('get-topics-filter-sort-options', function() {
+  var FIXTURES = [
+    {
+      input: {},
+      output: {}
+    }, {
+      input: {
+        tags: 'foo,bar'
+      },
+      output: {
+        filter: {
+          tags: ['foo', 'bar']
+        }
+      }
+    }, {
+      input: {
+        category: 'general'
+      },
+      output: {
+        filter: {
+          category: 'general'
+        }
+      }
+    }, {
+      input: {
+        username: 'lerouxb'
+      },
+      output: {
+        filter: {
+          username: 'lerouxb'
+        }
+      }
+    }, {
+      input: {
+        since: '2016-09-19T12:27:15.917Z'
+      },
+      output: {
+        filter: {
+          since: new Date('2016-09-19T12:27:15.917Z')
+        }
+      }
+    }, {
+      input: {
+        sort: '-lastChanged,-_id'
+      },
+      output: {
+        sort: {
+          lastChanged: -1,
+          _id: -1
+        }
+      }
+    }, {
+      input: {
+        since: '2016-09-19T12:27:15.917Z',
+        sort: '-lastChanged'
+      },
+      output: {
+        filter: {
+          since: new Date('2016-09-19T12:27:15.917Z')
+        },
+        sort: {
+          lastChanged: -1
+        }
+      }
+    }
+  ];
+
+  FIXTURES.forEach(function(fixture) {
+    var input = fixture.input;
+    var output = fixture.output;
+
+    it('Should parse ' + JSON.stringify(input) + ' as ' + JSON.stringify(output), function() {
+      var options = getTopicsFilterSortOptions(input);
+      assert.deepStrictEqual(options, output);
+    });
+
+    it('should throw a 400 StatusError if you pass it an incorrect date.', function() {
+      return Promise.try(function() {
+        return getTopicsFilterSortOptions({ since: 'foo' });
+      })
+      .then(function() {
+        assert.ok(false, "Expected error.");
+      })
+      .catch(StatusError, function(err) {
+        assert.strictEqual(err.status, 400);
+      });
+    });
+  });
+});
+
