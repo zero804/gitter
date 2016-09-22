d7f3430427d2d94bffd34431be171cc54fdfdc67
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-09-22 14:51:31 +0100

    plumb in events into new comments store

--- modules/topics-ui/browser/js/stores/new-comment-store-store.js
+++ modules/topics-ui/browser/js/stores/new-comment-store-store.js
@@ -1,6 +1,24 @@
 import Backbone from 'backbone';
+import {subscribe} from '../../../shared/dispatcher';
+import {COMMENT_BODY_UPDATE} from '../../../shared/constants/create-comment';
+import {SHOW_REPLY_COMMENTS} from '../../../shared/constants/topic';
 
 export const NewCommentStore = Backbone.Model.extend({
 
+  defaults: {
+    text: '',
+  },
 
+  initialize(){
+    subscribe(COMMENT_BODY_UPDATE, this.onCommentBodyUpdate, this);
+    subscribe(SHOW_REPLY_COMMENTS, this.onCommentFocusReset, this);
+  },
+
+  onCommentBodyUpdate({replyId, val}){
+    this.set({ replyId: replyId, text: val});
+  },
+
+  onCommentFocusReset(){
+    this.clear();
+  }
 });
