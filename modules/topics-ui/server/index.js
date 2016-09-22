00b8f312c0f739424133e36bfbb9daa77072a193
Eric Eastwood <contact@ericeastwood.com>
2016-09-22 01:12:11 -0500

    Sqaush some eslint

--- modules/topics-ui/server/index.js
+++ modules/topics-ui/server/index.js
@@ -5,7 +5,6 @@ var components = require('./components');
 
 module.exports = function(componentName, context) {
   var Component = components(componentName);
-  console.log('context', context);
 
   //Return the rendered component with a given context
   return reactDomServer.renderToString(Component(context));
