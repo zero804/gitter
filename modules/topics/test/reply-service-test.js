14097b74c9232bdba2f73f7999e779a772c791fd
Le Roux Bodenstein <leroux@gitter.im>
2016-09-23 13:42:07 +0200

    split out the topics services' tests.

--- modules/topics/test/reply-service-test.js
+++ modules/topics/test/reply-service-test.js
@@ -3,11 +3,54 @@
 var assert = require('assert');
 var fixtureLoader = require('gitter-web-test-utils/lib/test-fixtures');
 var replyService = require('../lib/reply-service');
+var mongoUtils = require('gitter-web-persistence-utils/lib/mongo-utils');
 
-describe('reply-service', function() {
+describe('reply-service #slow', function() {
+  describe('service tests', function() {
+    var fixture = fixtureLoader.setup({
+      user1: {},
+      forum1: {},
+      category1: {
+        forum: 'forum1'
+      },
+      topic1: {
+        user: 'user1',
+        forum: 'forum1',
+        category: 'category1'
+      },
+      reply1: {
+        user: 'user1',
+        forum: 'forum1',
+        topic: 'topic1'
+      }
+    });
 
-  describe('integration tests #slow', function() {
+    it('should add a reply', function() {
+      return replyService.createReply(fixture.user1, fixture.topic1, {
+          text: 'hello **there**'
+        })
+        .then(function(reply) {
+          assert(mongoUtils.objectIDsEqual(reply.forumId, fixture.forum1._id));
+          assert(mongoUtils.objectIDsEqual(reply.topicId, fixture.topic1._id));
+          assert(mongoUtils.objectIDsEqual(reply.userId, fixture.user1._id));
+          assert.strictEqual(reply.text, 'hello **there**');
+          assert.strictEqual(reply.html, 'hello <strong>there</strong>');
+        });
+    });
 
+    it("should update a reply's text", function() {
+      return replyService.updateReply(fixture.user1, fixture.reply1, {
+          text: 'hello **there**'
+        })
+        .then(function(reply) {
+          assert.strictEqual(reply.text, 'hello **there**');
+          assert.strictEqual(reply.html, 'hello <strong>there</strong>');
+        });
+    });
+  });
+
+
+  describe('integration tests', function() {
     var fixture = fixtureLoader.setup({
       user1: {},
       user2: {},
@@ -34,11 +77,13 @@ describe('reply-service', function() {
       it('should return users', function() {
         return replyService.findSampleReplyingUserIdsForTopics([fixture.topic1._id])
           .then(function(result) {
+            console.log(result);
             assert.deepEqual(Object.keys(result), [fixture.topic1.id]);
             assert.deepEqual(result[fixture.topic1._id].map(String), [fixture.user2.id]);
           })
       })
     });
+
 
   });
 
