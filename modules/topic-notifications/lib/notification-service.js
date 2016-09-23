661ae1e89ec6b217acf664db38427c80b7655fdb
Andrew Newdigate <andrew@troupe.co>
2016-09-23 13:29:53 +0100

    ✨ Feature: send email notifications atomically

--- modules/topic-notifications/lib/notification-service.js
+++ modules/topic-notifications/lib/notification-service.js
@@ -21,6 +21,39 @@ function createNotifications(forumObject, userIds) {
   return ForumNotification.insertMany(docs);
 }
 
+
+/**
+ * Only really useful for testing?
+ */
+function findNotification(forumObject, userId) {
+  return ForumNotification.findOne({
+      userId: userId,
+      forumId: forumObject.forumId,
+      topicId: forumObject.topicId,
+      replyId: forumObject.replyId,
+      commentId: forumObject.commentId
+    })
+    .lean()
+    .exec();
+}
+
+function markNotificationAsEmailSent(notificationId) {
+  return ForumNotification.update({
+      _id: notificationId,
+      emailSent: { $eq: null }
+    }, {
+      $set: {
+        emailSent: new Date()
+      }
+    })
+    .exec()
+    .then(function(result) {
+      return result.nModified > 0;
+    })
+}
+
 module.exports = {
   createNotifications: Promise.method(createNotifications),
+  findNotification: findNotification,
+  markNotificationAsEmailSent: Promise.method(markNotificationAsEmailSent)
 }
