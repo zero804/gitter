6ef85dcd84949935b53e46b968d997b03a9f125a
Andrew Newdigate <andrew@troupe.co>
2016-09-23 09:45:55 +0100

    🚚 Refactor: missed some files

--- shared/handlebars/helpers/pluralize.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// credit to @lazd (https://github.com/lazd) - https://github.com/wycats/handlebars.js/issues/249
-module.exports = function(number, singular, plural) {
-  // TODO: use i18n as this doesn't work except in English
-  if (number === 1) return singular;
-  return (typeof plural === 'string') ? plural : singular + 's';
-};
--- shared/handlebars/helpers/when.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-function whenHelper(condition, truthValue, falseValue) {
-  return condition ? truthValue : falseValue;
-}
-
-module.exports = whenHelper;
