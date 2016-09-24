2d89def1d4989a1aef5991df1fcc6ab9d0ca2ec1
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-09-24 11:14:17 +0100

    add edit comment events to collection

--- modules/topics-ui/shared/action-creators/topic/update-save-comment.js
+++ modules/topics-ui/shared/action-creators/topic/update-save-comment.js
@@ -1,7 +1,8 @@
 import { UPDATE_SAVE_COMMENT } from '../../constants/topic.js';
 
-export default function updateSaveComment(){
+export default function updateSaveComment(commentId){
   return {
-    type: UPDATE_SAVE_COMMENT
+    type: UPDATE_SAVE_COMMENT,
+    commentId
   };
 }
