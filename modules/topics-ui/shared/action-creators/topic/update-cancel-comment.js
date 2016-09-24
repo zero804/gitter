e7aa25f4e296ab633e9f2d568f49fa33f48df332
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-09-24 10:35:45 +0100

    start adding edit comment

---
+++ modules/topics-ui/shared/action-creators/topic/update-cancel-comment.js
@@ -0,0 +1,7 @@
+import { UPDATE_CANCEL_COMMENT } from '../../constants/topic.js';
+
+export default function updateCancelComment(){
+  return {
+    type: UPDATE_CANCEL_COMMENT
+  };
+}
---
+++ modules/topics-ui/shared/action-creators/topic/update-comment.js
@@ -0,0 +1,7 @@
+import { UPDATE_COMMENT } from '../../constants/topic.js';
+
+export default function updateComment(){
+  return {
+    type: UPDATE_COMMENT
+  };
+};
