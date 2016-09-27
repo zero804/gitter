367ed037a6177a1c11c60ccd8a8f7c8a7c42e19f
Andrew Newdigate <andrew@troupe.co>
2016-09-27 12:30:45 +0100

    🚚 Refactor: use multiple instances of handlebars instead of a single global So that we can use partials with emails, for example

--- modules/mailer/lib/mailer-service.js
+++ modules/mailer/lib/mailer-service.js
@@ -2,27 +2,9 @@
 
 var env = require('gitter-web-env');
 var mailer = env.mailer;
-
-var troupeTemplate = require('gitter-web-templates/lib/troupe-template');
 var Promise = require('bluebird');
 var cdn = require('gitter-web-cdn');
-var path = require('path');
-
-var CACHED = { };
-function getCachedTemplate(templateName) {
-  if(CACHED[templateName]) return CACHED[templateName];
-
-  var templateFile = path.join(__dirname, '../templates', templateName + '.hbs');
-  CACHED[templateName] = troupeTemplate.compile(templateFile);
-  return CACHED[templateName];
-}
-
-function applyTemplate(templateName, data) {
-  return getCachedTemplate(templateName)
-    .then(function(template) {
-      return template(data);
-    });
-}
+var mailerTemplate = require('./mailer-template');
 
 var VALID_TEMPLATES = {
   'added-to-room': addedToRoomMapping,
@@ -79,9 +61,9 @@ function unreadNoticationMapping(data) {
     ROOMURI:    data.roomUri,
     ROOMURL:    data.roomUrl,
     UNSUB:      data.unsubscribeUrl,
-    HTML:       applyTemplate("unread_notification_html", data),
-    MICRODATA:  applyTemplate("unread_notification_microdata", data),
-    PLAINTEXT:  applyTemplate("unread_notification", data),
+    HTML:       mailerTemplate("unread_notification_html", data),
+    MICRODATA:  mailerTemplate("unread_notification_microdata", data),
+    PLAINTEXT:  mailerTemplate("unread_notification", data),
     LOGOURL:    cdn('images/logo/gitter-logo-email-64.png', {email: true})
   };
 
@@ -106,28 +88,25 @@ function createdRoomMapping(data) {
 
 function newTopicMapping(data) {
   return {
-    // HTML:       applyTemplate("unread_notification_html", data),
-    // MICRODATA:  applyTemplate("unread_notification_microdata", data),
-    PLAINTEXT:  applyTemplate("topic_new_topic_notification", data),
-    LOGOURL:    cdn('images/logo/gitter-logo-email-64.png', {email: true})
+    HTML: mailerTemplate("topic_new_topic_notification_html", data),
+    PLAINTEXT: mailerTemplate("topic_new_topic_notification", data),
+    LOGOURL: cdn('images/logo/gitter-logo-email-64.png', {email: true})
   };
 }
 
 function newTopicReplyMapping(data) {
   return {
-    // HTML:       applyTemplate("unread_notification_html", data),
-    // MICRODATA:  applyTemplate("unread_notification_microdata", data),
-    PLAINTEXT:  applyTemplate("topic_new_reply_notification", data),
-    LOGOURL:    cdn('images/logo/gitter-logo-email-64.png', {email: true})
+    HTML: mailerTemplate("topic_new_reply_notification_html", data),
+    PLAINTEXT: mailerTemplate("topic_new_reply_notification", data),
+    LOGOURL: cdn('images/logo/gitter-logo-email-64.png', {email: true})
   };
 }
 
 function newTopicCommentMapping(data) {
   return {
-    // HTML:       applyTemplate("unread_notification_html", data),
-    // MICRODATA:  applyTemplate("unread_notification_microdata", data),
-    PLAINTEXT:  applyTemplate("topic_new_comment_notification", data),
-    LOGOURL:    cdn('images/logo/gitter-logo-email-64.png', {email: true})
+    HTML: mailerTemplate("topic_new_comment_notification_html", data),
+    PLAINTEXT: mailerTemplate("topic_new_comment_notification", data),
+    LOGOURL: cdn('images/logo/gitter-logo-email-64.png', {email: true})
   };
 }
 
---
+++ modules/mailer/lib/mailer-template.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var fs = require('fs');
+var HandlebarsWrapper = require('gitter-web-templates/lib/handlebars-wrapper');
+var path = require('path');
+
+var handlebarsWrapper = new HandlebarsWrapper();
+
+function registerPartial(name) {
+  var partialFileName = path.join(__dirname, '../templates/partials', name + '.hbs');
+  var partialContent = fs.readFileSync(partialFileName, 'utf8');
+  handlebarsWrapper.handlebars.registerPartial(name, partialContent);
+}
+
+registerPartial('topic_html_email_header')
+registerPartial('topic_html_email_footer')
+
+var CACHED = { };
+function getCachedTemplate(templateName) {
+  if(CACHED[templateName]) return CACHED[templateName];
+
+  var templateFile = path.join(__dirname, '/../templates', templateName + '.hbs');
+  CACHED[templateName] = handlebarsWrapper.compile(templateFile);
+  return CACHED[templateName];
+}
+
+function mailerTemplate(templateName, data) {
+  return getCachedTemplate(templateName)
+    .then(function(template) {
+      return template(data);
+    });
+}
+
+module.exports = mailerTemplate;
