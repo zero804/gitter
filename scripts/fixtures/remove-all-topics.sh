55ee83d9377cf3a6a6789ef59eab5b219d763b5d
Andrew Newdigate <andrew@troupe.co>
2016-09-27 17:40:31 +0100

    🐞 Bugfix: typo

--- scripts/fixtures/remove-all-topics.sh
+++ scripts/fixtures/remove-all-topics.sh
@@ -6,5 +6,5 @@ set -x
 
 MONGO_CONNECTION=${MONGO_CONNECTION:-gitter-mongo-dev/gitter}
 
-mongo gitter-mongo-dev/gitter --eval 'db.forums.remove({})'
-mongo gitter-mongo-dev/gitter --eval 'db.groups.update({ forumId: { $exists: true }}, { $unset: { forumId: true } }, { multi: true })'
+mongo ${MONGO_CONNECTION} --eval 'db.forums.remove({})'
+mongo ${MONGO_CONNECTION} --eval 'db.groups.update({ forumId: { $exists: true }}, { $unset: { forumId: true } }, { multi: true })'
