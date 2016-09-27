6c89640535816ee8502f4ba8685ac7b997c0f54a
Andrew Newdigate <andrew@troupe.co>
2016-09-27 16:48:28 +0100

    ✨ Feature: forums have names and uris

--- modules/persistence/lib/schemas/forum-schema.js
+++ modules/persistence/lib/schemas/forum-schema.js
@@ -5,11 +5,16 @@ var Schema = mongoose.Schema;
 var securityDescriptor = require('./security-descriptor-subdocument');
 
 var ForumSchema = new Schema({
+  name: { type: String, required: true },
+  uri: { type: String, required: true },
+  lcUri: { type: String, required: true },
   tags: [String],
   sd: { type: securityDescriptor.Schema, required: true },
 }, { strict: 'throw' });
 
 ForumSchema.schemaTypeName = 'ForumSchema';
+
+ForumSchema.index({ lcUri: 1 }, { unique: true });
 
 module.exports = {
   install: function(mongooseConnection) {
