d6c3bfa063660fb63d6f0ec956aacbebbf624aed
Eric Eastwood <contact@ericeastwood.com>
2016-09-27 03:15:33 -0500

    Use BaseResolverCollection for forum categories

--- public/js/collections/forum-category-collection.js
+++ public/js/collections/forum-category-collection.js
@@ -1,40 +1,9 @@
 'use strict';
 
-var Backbone = require('backbone');
-var urlJoin = require('url-join');
-var apiClient = require('../components/api-client');
+var BaseResolverCollection = require('./base-resolver-collection');
 
-var ForumCategoryCollection = Backbone.Collection.extend({
-  initialize: function(models, options) {
-    if (!options || !options.roomMenuModel) {
-      throw new Error('A valid ForumCategoryCollection must be passed to a new instance of RoomMenuModel');
-    }
-
-    this.roomMenuModel = options.roomMenuModel;
-    this.groupsCollection = options.groupsCollection;
-    this.listenTo(this.roomMenuModel, 'change:state change:groupId', this.onModelChangeState);
-  },
-
-  onModelChangeState: function() {
-    var groupId = this.roomMenuModel.get('groupId');
-    var menuState = this.roomMenuModel.get('state');
-
-    var group = this.groupsCollection && this.groupsCollection.get(groupId);
-    var forumId = group && group.get('forumId');
-
-    this.reset();
-    if(menuState === 'org' && forumId) {
-      apiClient.get(urlJoin('/v1/forums/', forumId))
-        .bind(this)
-        .then(function(forum) {
-          this.reset(forum.categories.map(function(category) {
-            category.groupUri = group.get('uri');
-            return category;
-          }));
-        });
-    }
-  },
-
+var ForumCategoryCollection = BaseResolverCollection.extend({
+  template: '/v1/forums/:forumId/categories'
 });
 
 module.exports = ForumCategoryCollection;
