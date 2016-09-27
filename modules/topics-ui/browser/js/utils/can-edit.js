b76004abb4f40a44a36d87b66e1c3427ba1841aa
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-09-27 13:17:14 +0100

    add canEdit util

---
+++ modules/topics-ui/browser/js/utils/can-edit.js
@@ -0,0 +1,30 @@
+const defaultForum = {
+  permissions: {
+    admin: false
+  }
+}
+
+const defaultUser = {
+  id: false
+}
+
+const defaultResource = {
+  user: {}
+}
+
+
+export default function canEdit(forum, user, resource = defaultResource){
+
+  forum = (forum || defaultForum);
+  user = (user || defaultUser);
+
+  //User is admin
+  const {permissions} = forum;
+  if(permissions.admin) { return true; }
+
+  //User owns resource
+  const {id} = user;
+  if(id === resource.user.id) { return true;}
+
+  return false;
+}
