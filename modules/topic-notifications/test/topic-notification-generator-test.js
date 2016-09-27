fc781dbc1abb8f73623328cf498fc443104dc166
Andrew Newdigate <andrew@troupe.co>
2016-09-27 23:32:20 +0100

    🚧 WIP: work on the templates

--- modules/topic-notifications/test/topic-notification-generator-test.js
+++ modules/topic-notifications/test/topic-notification-generator-test.js
@@ -9,13 +9,13 @@ var RxNode = require('rx-node');
 var ObjectID = require('mongodb').ObjectID;
 var mongoUtils = require('gitter-web-persistence-utils/lib/mongo-utils');
 var fixtureLoader = require('gitter-web-test-utils/lib/test-fixtures');
+var mailerTemplate = require('gitter-web-mailer/lib/mailer-template');
 
 describe('forum-notification-events', function() {
 
   describe('integration tests #slow', function() {
 
-    describe('notificationObserver', function() {
-
+    describe('notificationObserver multi', function() {
       var fixture = fixtureLoader.setup({
         user1: {},
         user2: {},
@@ -89,11 +89,153 @@ describe('forum-notification-events', function() {
             });
 
           });
-
       });
     });
 
-    describe('streamNotificationsForEmail', function() {
+    describe('notificationObserver new topic', function() {
+      var fixture = fixtureLoader.setup({
+        user1: {},
+        forum1: {},
+        category1: {
+          forum: 'forum1',
+        },
+        topic1: {
+          user: 'user1',
+          forum: 'forum1',
+          category: 'category1',
+          sent: new Date('2014-01-01T00:00:00.000Z'),
+          title: 'Hello',
+          html: '<strong>Hello</strong>',
+          text: '**Hello**'
+        },
+      });
+
+      it('should raise notifications', function() {
+
+        var userId = fixture.user1._id;
+        var forumId = fixture.forum1._id;
+        var topicId1 = fixture.topic1._id;
+        var topicRef1 = ForumObject.createForTopic(forumId, topicId1);
+
+        // Create three notifications in order
+        return notificationService.createNotifications(topicRef1, [userId])
+          .then(function() {
+            return topicNotificationGenerator.notificationObserver({ userId: userId })
+              .toPromise();
+          })
+          .then(function(notification) {
+            return mailerTemplate('topic_new_topic_notification_html', {
+              notification: notification.data
+            });
+          });
+      });
+    });
+
+    describe('notificationObserver new reply', function() {
+      var fixture = fixtureLoader.setup({
+        user1: {},
+        forum1: {},
+        category1: {
+          forum: 'forum1',
+        },
+        topic1: {
+          user: 'user1',
+          forum: 'forum1',
+          category: 'category1',
+          title: 'Hello',
+          html: '<strong>Hello</strong>',
+          text: '**Hello**'
+        },
+        reply1: {
+          forum: 'forum1',
+          category: 'category1',
+          user: 'user1',
+          topic: 'topic1',
+          text: '*Goodbye*',
+          html: '<strong>Goodbye</strong>',
+        },
+      });
+
+      it('should raise notifications', function() {
+
+        var userId = fixture.user1._id;
+        var forumId = fixture.forum1._id;
+        var topicId1 = fixture.topic1._id;
+        var replyId1 = fixture.reply1._id;
+        var replyRef1 = ForumObject.createForReply(forumId, topicId1, replyId1);
+
+        // Create three notifications in order
+        return notificationService.createNotifications(replyRef1, [userId])
+          .then(function() {
+            return topicNotificationGenerator.notificationObserver({ userId: userId })
+              .toPromise();
+          })
+          .then(function(notification) {
+            return mailerTemplate('topic_new_reply_notification_html', {
+              notification: notification.data
+            });
+          });
+      });
+    });
+
+    describe('notificationObserver new comment', function() {
+      var fixture = fixtureLoader.setup({
+        user1: {},
+        forum1: {},
+        category1: {
+          forum: 'forum1',
+        },
+        topic1: {
+          user: 'user1',
+          forum: 'forum1',
+          category: 'category1',
+          title: 'Hello',
+          html: '<strong>Hello</strong>',
+          text: '**Hello**'
+        },
+        reply1: {
+          forum: 'forum1',
+          category: 'category1',
+          user: 'user1',
+          topic: 'topic1',
+          text: '*Goodbye*',
+          html: '<strong>Goodbye</strong>',
+        },
+        comment1: {
+          user: 'user1',
+          forum: 'forum1',
+          topic: 'topic1',
+          reply: 'reply1',
+          text: '*xxx*',
+          html: '<strong>xxx</strong>',
+        }
+      });
+
+      it('should raise notifications', function() {
+
+        var userId = fixture.user1._id;
+        var forumId = fixture.forum1._id;
+        var topicId1 = fixture.topic1._id;
+        var replyId1 = fixture.reply1._id;
+        var commentId1 = fixture.comment1._id;
+        var commentRef1 = ForumObject.createForComment(forumId, topicId1, replyId1, commentId1);
+
+        // Create three notifications in order
+        return notificationService.createNotifications(commentRef1, [userId])
+          .then(function() {
+            return topicNotificationGenerator.notificationObserver({ userId: userId })
+              .toPromise();
+          })
+          .then(function(notification) {
+            return mailerTemplate('topic_new_comment_notification_html', {
+              notification: notification.data
+            });
+          });
+      });
+    });
+
+
+    describe.skip('streamNotificationsForEmail', function() {
       var userId = new ObjectID();
       var forumId = new ObjectID();
       var topicId1 = new ObjectID();
