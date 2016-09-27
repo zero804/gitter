60bbb78c3f14596130abab098634b7b79b56d0c3
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-09-27 13:17:14 +0100

    add canEdit util

---
+++ modules/topics-ui/test/specs/browser/utils/can-edit-test.js
@@ -0,0 +1,22 @@
+import {ok} from 'assert';
+import canEdit from '../../../../browser/js/utils/can-edit.js';
+
+describe('canEdit', () => {
+
+  const adminForum = { permissions: { admin: true } };
+  const nonAdminForum = { permissions: { admin: false } };
+  const user = { id: 1 };
+
+  it('should return false by default', () => {
+    ok(!canEdit());
+  });
+
+  it('should return true if the user is a forum admin', () => {
+    ok(canEdit(adminForum));
+  });
+
+  it('should return true id the resource belongs to the user', () => {
+    ok(canEdit(nonAdminForum, user, { user: user }));
+  });
+
+});
