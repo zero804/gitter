7b3dcf2e6563c6fcf11571e2bb631dad68413e6c
Andrew Newdigate <andrew@troupe.co>
2016-09-28 17:20:26 +0100

    ✨ Feature: reactions return the count totals

--- modules/topic-reactions/lib/reaction-service.js
+++ modules/topic-reactions/lib/reaction-service.js
@@ -4,6 +4,8 @@ var ForumReaction = require('gitter-web-persistence').ForumReaction;
 var ForumObject = require('gitter-web-topic-models/lib/forum-object');
 var assert = require('assert');
 var mongooseUtils = require('gitter-web-persistence-utils/lib/mongoose-utils');
+var validReactions = require('./valid-reactions');
+var StatusError = require('statuserror');
 
 function updateReactionTotals(forumObject, reaction, inc) {
   var Model = forumObject.type.model;
@@ -11,13 +13,31 @@ function updateReactionTotals(forumObject, reaction, inc) {
   var incOp = {};
   incOp['reactionCounts.' + reaction] = inc;
 
-  return Model.update(query, {
+  return Model.findOneAndUpdate(query, {
       $inc: incOp
+    }, {
+      new: true
     })
-    .exec();
+    .select({ _id: 0, reactionCounts: 1 })
+    .exec()
+    .then(function(doc) {
+      var reactionCounts = doc && doc.reactionCounts;
+
+      if (reactionCounts) {
+        forumObject.liveCollectionPatch({
+          reactions: reactionCounts
+        });
+      }
+
+      return reactionCounts;
+    });
 }
 
 function addReaction(forumObject, userId, reaction) {
+  if (!validReactions.isValid(reaction)) {
+    throw new StatusError(400);
+  }
+
   assert(forumObject.type !== ForumObject.TYPE.Forum);
   assert(reaction);
 
@@ -42,14 +62,15 @@ function addReaction(forumObject, userId, reaction) {
   })
   .then(function(result) {
     var modified = !!(result.nModified || result.upserted && result.upserted.length);
-    if (!modified) return false;
+    if (!modified) return null;
 
-    return updateReactionTotals(forumObject, reaction, +1)
-      .return(true);
+    return updateReactionTotals(forumObject, reaction, +1);
   });
 }
 
 function removeReaction(forumObject, userId, reaction) {
+  // Allow invalid reactions to be removed
+  // in case they were valid in another version of the app
   assert(forumObject.type !== ForumObject.TYPE.Forum);
   assert(reaction);
 
@@ -66,10 +87,9 @@ function removeReaction(forumObject, userId, reaction) {
     .exec()
     .then(function(result) {
       var modified = result.result.n > 0;
-      if (!modified) return false;
+      if (!modified) return null;
 
-      return updateReactionTotals(forumObject, reaction, -1)
-        .return(true);
+      return updateReactionTotals(forumObject, reaction, -1);
     })
 }
 
