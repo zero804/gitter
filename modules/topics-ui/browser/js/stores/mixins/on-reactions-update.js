a1753ad31fd8068d3b7088ee90acd2faa78670a4
Eric Eastwood <contact@ericeastwood.com>
2016-09-29 07:02:42 -0500

    Fix indent

--- modules/topics-ui/browser/js/stores/mixins/on-reactions-update.js
+++ modules/topics-ui/browser/js/stores/mixins/on-reactions-update.js
@@ -1,30 +1,30 @@
 import _ from 'lodash';
 
-const onReactionsUpdate = function({ topicId, replyId, commentId, reactionKey, isReacting }) {
-  const id = topicId || replyId || commentId;
-  const topic = this.collection.get(id);
-  if(!topic) { return; }
+const onReactionsUpdate = function({ entityId, replyId, commentId, reactionKey, isReacting }) {
+  const id = entityId || replyId || commentId;
+  const entity = this.collection.get(id);
+  if(!entity) { return; }
 
   if(isReacting) {
-    const reactionCounts = topic.get('reactionCounts') || {};
-    const ownReactions = topic.get('ownReactions') || {};
+    const reactionCounts = entity.get('reactionCounts') || {};
+    const ownReactions = entity.get('ownReactions') || {};
     const existingCount = reactionCounts[reactionKey] || 0;
 
     let newCount = existingCount;
     if(isReacting === true && ownReactions[reactionKey] === false) {
-    newCount += 1;
+      newCount += 1;
     }
     else if(isReacting === false && ownReactions[reactionKey] === true) {
-    newCount -= 1;
+      newCount -= 1;
     }
 
-    topic.set({
-    reactionCounts: _.extend({}, reactionCounts, {
-      [reactionKey]: newCount
-    }),
-    ownReactions: _.extend({}, ownReactions, {
-      [reactionKey]: isReacting
-    })
+    entity.set({
+      reactionCounts: _.extend({}, reactionCounts, {
+        [reactionKey]: newCount
+      }),
+      ownReactions: _.extend({}, ownReactions, {
+        [reactionKey]: isReacting
+      })
     });
   }
 }
