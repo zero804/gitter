152ce77487a4c8b6df62e455aee42318f8bc4c38
Eric Eastwood <contact@ericeastwood.com>
2016-09-29 03:23:56 -0500

    Add tests for ReactionButton

--- modules/topics-ui/test/specs/shared/containers/components/forum/reaction-button-test.jsx
+++ modules/topics-ui/test/specs/shared/containers/components/forum/reaction-button-test.jsx
@@ -1,18 +1,31 @@
-import assert from 'assert';
+import { equal, deepEqual } from 'assert';
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
+import { spy } from 'sinon';
+
 import ReactionButton from '../../../../../../shared/containers/components/forum/reaction-button.jsx';
+import * as reactionConstants from '../../../../../../shared/constants/reactions';
 
 describe('<ReactionButton/>', () => {
 
   let wrapper;
+  let onReactionPickHandle;
 
   beforeEach(() => {
-    wrapper = shallow(<ReactionButton/>);
+    onReactionPickHandle = spy();
+    wrapper = mount(<ReactionButton
+      onReactionPick={onReactionPickHandle} />);
   });
 
-  it('should fail a test because you should write some', () => {
-    assert(false, 'Srsly, write some tests');
+  it('should render a RawReactionButton', () => {
+    equal(wrapper.find('RawReactionButton').length, 1);
   });
 
+  it('calls `onReactionPick` callback attribute', () => {
+    wrapper.simulate('click');
+    equal(onReactionPickHandle.callCount, 1);
+    var args = onReactionPickHandle.getCall(0).args;
+    equal(args.length, 2);
+    deepEqual(args, [reactionConstants.LIKE, true]);
+  });
 });
