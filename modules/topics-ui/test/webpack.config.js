61d2e3eee4a7d037b6fd8fb56207e9d576b0ab41
Eric Eastwood <contact@ericeastwood.com>
2016-09-30 09:13:35 -0500

    Add npm run test-watch-with-sourcemaps

--- modules/topics-ui/test/webpack.config.js
+++ modules/topics-ui/test/webpack.config.js
@@ -1,8 +1,19 @@
 "use strict";
 
+var webpack = require('webpack');
 var webpackConfig = require('../webpack.config.js');
 var _ = require('lodash');
 var path = require('path');
+
+
+var opts = require('yargs')
+  .option('use-sourcemaps', {
+    description: 'Whether to use sourcemaps',
+    default: false
+  })
+  .help('help')
+  .alias('help', 'h')
+  .argv;
 
 var moduleConfig = _.extend({}, (webpackConfig.module || {}), {
   loaders: (webpackConfig.module.loaders || []).concat([
@@ -16,12 +27,12 @@ var moduleConfig = _.extend({}, (webpackConfig.module || {}), {
 
 var config = _.extend({}, webpackConfig, {
   // Use `'source-map'` when you want working source-maps in the browser tests
-  devtool: 'source-map',
+  devtool: opts.useSourcemaps ? 'source-map' : 'eval',
   entry: {
     runner: path.resolve(__dirname, './fixtures/runner-browser.js')
   },
   output: {
-    filename: "[name].js",
+    filename: '[name].js',
     path: path.resolve(__dirname, './fixtures/build'),
   },
   module: moduleConfig,
@@ -35,7 +46,14 @@ var config = _.extend({}, webpackConfig, {
       'gitter-realtime-client/lib/simple-filtered-collection': require.resolve('gitter-realtime-client/lib/simple-filtered-collection'),
       'gitter-realtime-client': path.resolve(__dirname, './mocks/realtime.js'),
     }
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'useSourcemaps': opts.useSourcemaps
+      }
+    })
+  ]
 });
 
 //Load in the mocha CSS so everything looks pretty
