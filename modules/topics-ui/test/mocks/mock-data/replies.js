3edf95d3cd37a1e6a9565b01189b1e1da7d16d73
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-10-03 13:23:49 +0100

    add sorting tests to browser replues store

--- modules/topics-ui/test/mocks/mock-data/replies.js
+++ modules/topics-ui/test/mocks/mock-data/replies.js
@@ -1,6 +1,34 @@
 export default [
-  { id: '1', sent: "2016-08-24T15:17:22.439Z", body: { text: 'test', html: '<p>test</p>'}, user: { avatarUrl: '', displayName: ''} },
-  { id: '2', sent: "2016-08-24T15:17:22.439Z", body: { text: 'test', html: '<p>test</p>'}, user: { avatarUrl: '', displayName: ''} },
-  { id: '3', sent: "2016-08-24T15:17:22.439Z", body: { text: 'test', html: '<p>test</p>'}, user: { avatarUrl: '', displayName: ''} },
-  { id: '4', sent: "2016-08-24T15:17:22.439Z", body: { text: 'test', html: '<p>test</p>'}, user: { avatarUrl: '', displayName: ''} },
+  {
+    id: '1',
+    sent: "2016-08-24T15:17:22.439Z",
+    body: { text: 'test', html: '<p>test</p>'},
+    user: { avatarUrl: '', displayName: ''},
+    reactions: { like: 0 },
+    commentsTotal: 4,
+  },
+  {
+    id: '2',
+    sent: "2016-08-24T15:17:23.439Z",
+    body: { text: 'test', html: '<p>test</p>'},
+    user: { avatarUrl: '', displayName: ''},
+    reactions: { like: 1 },
+    commentsTotal: 3,
+  },
+  {
+    id: '3',
+    sent: "2016-08-24T15:17:24.439Z",
+    body: { text: 'test', html: '<p>test</p>'},
+    user: { avatarUrl: '', displayName: ''},
+    reactions: { like: 2 },
+    commentsTotal: 2,
+  },
+  {
+    id: '4',
+    sent: "2016-08-24T15:17:25.439Z",
+    body: { text: 'test', html: '<p>test</p>'},
+    user: { avatarUrl: '', displayName: ''},
+    reactions: { like: 4 },
+    commentsTotal: 1,
+  },
 ]
