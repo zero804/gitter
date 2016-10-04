aad7b009ed0fb0fe919026ce3f3933eebbff2da9
Le Roux Bodenstein <leroux@gitter.im>
2016-10-04 18:37:30 +0200

    Change fetchByTags to use the new public boolean field.

---
+++ test/integration/services/explore-service-test.js
@@ -0,0 +1,37 @@
+'use strict';
+
+
+var assert = require('assert');
+var fixtureLoader = require('gitter-web-test-utils/lib/test-fixtures');
+var testRequire = require('../test-require');
+var exploreService = testRequire('./services/explore-service');
+
+
+describe('explore-service #slow', function() {
+  //fixtureLoader.disableMongoTableScans();
+
+  var fixture = fixtureLoader.setup({
+    deleteDocuments: {
+      Troupe: [{tags: ['explore-test']}]
+    },
+    troupe1: {
+      tags: ['explore-test']
+    },
+    troupe2: {},
+      tags: ['explore-test'],
+      securityDescriptor: {
+        private: true
+      }
+  });
+
+  it('should only find public troupes', function() {
+    return exploreService.fetchByTags(['explore-test'])
+      .then(function(rooms) {
+        // it should only find the public troupe
+        assert.strictEqual(rooms.length, 1);
+        assert(rooms.some(function(room) {
+          return room.id === fixture.troupe1.id
+        }));
+      });
+  });
+});
