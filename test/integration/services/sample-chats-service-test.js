fa45c960bd520eab9014598da9aff8f118098cb2
Andrew Newdigate <andrew@troupe.co>
2016-10-11 11:52:09 +0100

    🚨 Tests: ensure that sample chats service does not use table scans

--- test/integration/services/sample-chats-service-test.js
+++ test/integration/services/sample-chats-service-test.js
@@ -3,15 +3,22 @@
 var testRequire = require('../test-require');
 var assert = require('assert');
 var sampleChatsService = testRequire("./services/sample-chats-service");
+var fixtureLoader = require('gitter-web-test-utils/lib/test-fixtures');
 
 describe('sample-chats-service', function() {
 
-  it('should return sample chats #slow', function(done) {
-    sampleChatsService.getSamples()
-      .then(function(chats) {
-        assert(Array.isArray(chats));
-      })
-      .nodeify(done);
+  describe('integration tests #slow', function() {
+    fixtureLoader.disableMongoTableScans();
+
+    it('should return sample chats #slow', function(done) {
+      sampleChatsService.getSamples()
+        .then(function(chats) {
+          assert(Array.isArray(chats));
+        })
+        .nodeify(done);
+    });
+
   });
+
 
 });
