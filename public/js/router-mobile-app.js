2dd9601190a414e9cfbe9ec94c7275202900ac3e
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-10-11 17:48:12 +0100

    add login modal to mobile view with router

--- public/js/router-mobile-app.js
+++ public/js/router-mobile-app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var $ = require('jquery');
+var Backbone = require('backbone');
 var onready = require('./utils/onready');
 var appEvents = require('./utils/appevents');
 var MobileAppLayout = require('./views/layouts/mobile-app');
@@ -17,8 +18,58 @@ require('./utils/tracking');
 require('./views/widgets/avatar');
 require('./template/helpers/all');
 
+//Super simple router used only to dismiss any modals
+//that are shown
+var Router = Backbone.Router.extend({
+  routes: {
+    '': 'index',
+    login: 'login'
+  },
+
+  initialize: function(attrs) {
+    this.modalRegion = attrs.modalRegion;
+  },
+
+  index: function(){
+    //Clear any existing modals
+    this.modalRegion.destroy();
+  },
+
+  login: function(){
+    //Async load our login modal
+    var self = this;
+    require.ensure(['./views/modals/login-view'], function(require){
+      var LoginView = require('./views/modals/login-view');
+      self.modalRegion.show(new LoginView());
+    });
+  }
+});
+
+//Simple helper to get data from
+//any postMessage event handles
+function parsePostMessagePayload(data){
+  var message;
+  try { message = JSON.parse(data); }
+  catch (err) { return; }
+  return message;
+}
+
+//Simple wrapper for changing pushState
+function pushState(state, title, url) {
+  if (state === window.history.state) { return; }
+  window.history.pushState(state, title, url);
+}
+
+
 onready(function() {
   $('html').removeClass('loading');
+
+  var mobileView = new MobileAppLayout({
+    template: false,
+    el: 'body'
+  });
+
+  var router = new Router({ modalRegion: mobileView.dialogRegion });
 
   //Remove for Left Menu
   FastClick.attach(document.body);
@@ -30,33 +81,20 @@ onready(function() {
 
   window.addEventListener('message', function(e){
     if (e.origin !== clientEnv.basePath) { return; }
-
-    var message;
-    try {
-      message = JSON.parse(e.data);
-    }
-    catch (err) { return; }
+    var message = parsePostMessagePayload(e.data);
 
     switch (message.type) {
-      case 'navigation':
-        var url = message.url;
-        pushState(url, message.title, url)
-        break;
+      case 'navigation': return pushState(message.url, message.title, message.url);
+        //I literally have no idea why this in not "navigation"
+        //seems foolish not to be .... JP 11/10/16
+      case 'route': return router.navigate('login', { trigger: true });
     }
 
   });
 
-  new MobileAppLayout({
-    template: false,
-    el: 'body'
-  }).render();
+  //Render our view so its good and ready for updates
+  mobileView.render();
+
+  //Start the routing
+  Backbone.history.start();
 });
-
-function pushState(state, title, url) {
-  if (state === window.history.state) {
-    // Don't repush the same state...
-    return;
-  }
-
-  window.history.pushState(state, title, url);
-}
