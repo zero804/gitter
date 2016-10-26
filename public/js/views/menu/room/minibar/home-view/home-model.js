10a32ac436f8100a55bf7c44f3ee85d48f8b54fa
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-10-26 11:17:29 +0100

    fixup topic styles for new menu styles

--- public/js/views/menu/room/minibar/home-view/home-model.js
+++ public/js/views/menu/room/minibar/home-view/home-model.js
@@ -12,7 +12,6 @@ module.exports = ItemModel.extend({
   },
 
   initialize: function(attrs, options) { //jshint unused: true
-    console.log('yo yhis is working');
     this.roomCollection = options.roomCollection;
     this.listenTo(this.roomCollection, 'snapshot sync change remove reset', this.onRoomsUpdate, this);
   },
@@ -22,7 +21,6 @@ module.exports = ItemModel.extend({
   },
 
   reduceRooms: function (){
-    console.log('reducing');
     return this.roomCollection.reduce(function(memo, room){
       return {
         mentions:    (memo.mentions + room.get('mentions')),
