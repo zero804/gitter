9ee10fcfef4f505530dae85a428c4648e2a487fc
Andrew Newdigate <andrew@troupe.co>
2016-11-04 17:34:06 +0000

    💄 Cosmetic: switch from `var` to `const` on requires, as a test

--- modules/app-version/index.js
+++ modules/app-version/index.js
@@ -1,9 +1,9 @@
 "use strict";
 
 /* #### See version-files in the Makefile #### */
-var fs = require('fs');
-var path = require('path');
-var winston = require('gitter-web-env').logger;
+const fs = require('fs');
+const path = require('path');
+const winston = require('gitter-web-env').logger;
 
 function readFileSync(fileName) {
   var file = path.join(__dirname, '../..', fileName);
@@ -17,35 +17,43 @@ function readFileSync(fileName) {
   return '';
 }
 
-var assetTag = readFileSync('ASSET_TAG') || 'l';
-var commit = readFileSync('GIT_COMMIT') || '';
-var branch = readFileSync('VERSION') || 'HEAD';
-var version = commit ? commit.substring(0, 6) : 'HEAD-' + Math.floor(Date.now() / 10000);
+const assetTag = readFileSync('ASSET_TAG') || 'l';
+const commit = readFileSync('GIT_COMMIT') || '';
+const branch = readFileSync('VERSION') || 'HEAD';
+const version = commit ? commit.substring(0, 6) : 'HEAD-' + Math.floor(Date.now() / 10000);
 
 /* THE NEW */
 /* Returns a unique identifier for the current code */
-exports.getVersion = function() {
+function getVersion() {
   return version;
-};
+}
 
 /* Returns the current asset hash or '' */
-exports.getAssetTag = function() {
+function getAssetTag() {
   return assetTag;
-};
+}
 
 /* Returns the current commit hash */
-exports.getCommit = function() {
+function getCommit() {
   return commit;
-};
+}
 
 /* Returns the current branch or HEAD */
-exports.getBranch = function() {
+function getBranch() {
   return branch;
-};
+}
 
-exports.getGithubLink = function() {
+function getGithubLink() {
   if(commit)
     return 'https://github.com/troupe/gitter-webapp/commit/' + commit;
 
   return '';
+}
+
+module.exports = {
+  getVersion: getVersion,
+  getAssetTag: getAssetTag,
+  getCommit: getCommit,
+  getBranch: getBranch,
+  getGithubLink: getGithubLink
 };
