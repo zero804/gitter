b19e1fa058017a61e73d77d753b968c6bb2066ff
Eric Eastwood <contact@ericeastwood.com>
2016-11-08 02:25:20 -0600

    Add favouriting star button to org directory page

--- public/js/router-org-page.js
+++ public/js/router-org-page.js
@@ -2,12 +2,18 @@
 
 require('./utils/font-setup');
 
-var onReady = require('./utils/onready');
-var Backbone = require('backbone');
-var appEvents = require('./utils/appevents');
-var modalRegion = require('./components/modal-region');
 var _ = require('underscore');
+var Backbone = require('backbone');
+var urlJoin = require('url-join');
 var clientEnv = require('gitter-client-env');
+var onReady = require('./utils/onready');
+var context = require('./utils/context');
+var appEvents = require('./utils/appevents');
+var realtime = require('./components/realtime');
+var SyncMixin = require('./collections/sync-mixin');
+var modalRegion = require('./components/modal-region');
+
+var OrgDirectoryLayout = require('./views/layouts/org-directory-layout');
 
 require('gitter-styleguide/css/components/buttons.css');
 require('gitter-styleguide/css/components/headings.css');
@@ -62,4 +68,28 @@ onReady(function(){
 
   new Router();
   Backbone.history.start({ silent: true });
+
+
+
+  var group = context.group();
+  var user = context.user();
+
+  var GroupModel = Backbone.Model.extend({
+    url: function() {
+      return urlJoin('/v1/user/', user.get('id'), '/groups/', this.get('id'));
+    },
+    client: function() {
+      return realtime.getClient();
+    },
+    sync: SyncMixin.sync
+  });
+
+  var orgDirectoryLayout = new OrgDirectoryLayout({
+    template: false,
+    el: 'body',
+    group: new GroupModel(group.attributes)
+  });
+  orgDirectoryLayout.render();
+
+
 });
---
+++ public/js/views/org-directory/org-directory-header-view.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var _ = require('underscore');
+var Backbone = require('backbone');
+var Marionette = require('backbone.marionette');
+var toggleClass = require('../../utils/toggle-class');
+
+var headerViewTemplate = require('./org-directory-header-view.hbs');
+
+require('../behaviors/tooltip');
+
+
+var HeaderView = Marionette.ItemView.extend({
+  el: 'header',
+  template: headerViewTemplate,
+
+  ui: {
+    favourite: '.js-group-favourite-button',
+    favouriteIcon: '.js-group-favourite-button-icon'
+  },
+
+  events: {
+    'click @ui.favourite': 'onFavouriteButtonClicked',
+  },
+
+  modelEvents: {
+    'change:favourite': 'onFavouriteChange'
+  },
+
+  behaviors: {
+    Tooltip: {
+      '.js-group-favourite-button': { placement: 'left' }
+    },
+  },
+
+
+  serializeData: function() {
+    var data = this.model.toJSON();
+
+    _.extend(data, {
+      // ...
+    });
+
+    return data;
+  },
+
+  onFavouriteChange: function() {
+    var isFavourited = !!this.model.get('favourite');
+    toggleClass(this.ui.favouriteIcon[0], 'favourite', isFavourited);
+  },
+
+  onFavouriteButtonClicked: function() {
+    var isFavourited = !!this.model.get('favourite');
+    this.model.save({
+      favourite: !isFavourited
+    }, { wait: true });
+  }
+
+});
+
+module.exports = HeaderView;
