d152af3a05d378d00f3eb4b3bdc2c24cded147e8
Andrew Newdigate <andrew@troupe.co>
2016-11-10 18:56:37 +0000

    🐞 Bugfix: Fix for https://github.com/troupe/gitter-webapp/issues/2211

--- public/js/ensured/present-permissions-dialog.js
+++ public/js/ensured/present-permissions-dialog.js
@@ -3,24 +3,26 @@
 var context = require('../utils/context');
 
 function presentPermissionsDialog(options) {
-  var roomCollection = options.roomCollection;
-  var groupCollection = options.groupCollection;
   var dialogRegion = options.dialogRegion;
 
   require.ensure([
     '../models/permissions-view-model',
-    '../views/modals/permissions-view'
+    '../views/modals/permissions-view',
+    '../collections/groups'
   ], function(require) {
     var PermissionsModel = require('../models/permissions-view-model');
     var permissionsView = require('../views/modals/permissions-view');
+    var groupModels = require('../collections/groups');
+
+    var groupCollection = new groupModels.Collection([], { listen: false });
+    // This is total overkill, since we only need a single group....
+    groupCollection.fetch();
 
     var slimCurrentRoom = context.troupe();
-    // Try to get something better otherwise work with the slim version
-    var currentRoom = roomCollection.get(slimCurrentRoom.get('id')) || slimCurrentRoom;
 
     var modal = new permissionsView.Modal({
       model: new PermissionsModel({
-        entity: currentRoom
+        entity: slimCurrentRoom//currentRoom
       }, {
         groupCollection: groupCollection
       }),
