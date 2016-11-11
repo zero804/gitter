060a655a2eb6d426d23a8304fec3f43e963b4d9d
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-11-11 16:49:25 +0000

    correct silly addition to partial resolution

--- server/web/register-partials.js
+++ server/web/register-partials.js
@@ -11,7 +11,7 @@ module.exports = function(hbs) {
 
   var files = glob.sync(partialsGlob);
   return files.map(function(file) {
-    var partialName = path.relative(partialsDir, file).replace(/\.hbs$/, '').replace(path.sep, '/');
+    var partialName = path.relative(partialsDir, file).replace(/\.hbs$/, '');
     var partialTemplate = fs.readFileSync(file, 'utf8');
     hbs.registerPartial(partialName, partialTemplate);
 
