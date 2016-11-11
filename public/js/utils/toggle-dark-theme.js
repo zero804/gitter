477ccf1d84ac0080a4e931ffc26790d824dd471d
Jon Parsons <jon@ctrlxctrlv.co.uk>
2016-11-11 16:11:30 +0000

    refactor due to code review

--- public/js/utils/toggle-dark-theme.js
+++ public/js/utils/toggle-dark-theme.js
@@ -3,9 +3,9 @@
 var cdn = require('gitter-web-cdn');
 var frameUtils = require('gitter-web-frame-utils');
 
-module.exports = function toggleDarkTheme(isFrame){
+module.exports = function toggleDarkTheme(isChildFrame){
 
-  isFrame = (isFrame || false);
+  isChildFrame = (isChildFrame || false);
   var darkThemeLink = document.getElementById('dark-theme-styles');
 
   //If we have a reference to the dark-theme script we remove it
@@ -14,8 +14,9 @@ module.exports = function toggleDarkTheme(isFrame){
     //actually remove the script from the document
     darkThemeLink.remove();
 
-    //We only want to past to the parent frame if we are actuallyin the child iFrame
-    if(!isFrame) { return; }
+    //We only want to post the message up into the parent framw if we
+    //are inside a child iframe
+    if(!isChildFrame) { return; }
 
     //Tell the parent frame to remove the dark-theme script in the css
     return frameUtils.postMessage({ type: 'toggle-dark-theme' });
@@ -33,7 +34,7 @@ module.exports = function toggleDarkTheme(isFrame){
   document.head.appendChild(darkThemeLink);
 
   //We only want to past to the parent frame if we are actuallyin the child iFrame
-  if(!isFrame) { return; }
+  if(!isChildFrame) { return; }
 
   //Tell the parent frame to add the script
   frameUtils.postMessage({ type: 'toggle-dark-theme' });
