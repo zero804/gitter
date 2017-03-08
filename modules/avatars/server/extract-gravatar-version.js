0923e19c6bd3a8aa421c4d8c63ed732523ee1085
Andrew Newdigate <andrew@troupe.co>
2017-03-08 15:56:35 +0000

    🐞 Bugfix: support GitHub CDN avatars

--- modules/avatars/server/extract-gravatar-version.js
+++ modules/avatars/server/extract-gravatar-version.js
@@ -2,11 +2,14 @@
 
 var url = require('url');
 
+var GITHUB_AVATARS_HOST_RE = /^avatars\d*\.githubusercontent\.com$/;
+
 /* Given a avatar url, get the cache buster */
 module.exports = function extractGravatarVersion(avatarUrl) {
   try {
     var parsed = url.parse(avatarUrl, true, true);
-    if (parsed.hostname === 'avatars.githubusercontent.com') {
+
+    if (GITHUB_AVATARS_HOST_RE.test(parsed.hostname)) {
       return parseInt(parsed.query.v, 10) || undefined;
     }
   } catch(e) {
