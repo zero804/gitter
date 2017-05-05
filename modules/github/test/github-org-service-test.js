286e201aae428a2fde58fee17419c63db87e1c5a
Andrew Newdigate <andrew@troupe.co>
2017-05-05 20:36:36 +0100

    Another secret removed from integration tests

--- modules/github/test/github-org-service-test.js
+++ modules/github/test/github-org-service-test.js
@@ -3,16 +3,27 @@
 
 var assert = require("assert");
 var GithubOrgService = require('..').GitHubOrgService;
-var GITTER_TEST_BOT = { username: 'gittertestbot', githubToken: '***REMOVED***'};
+var fixtureLoader = require('gitter-web-test-utils/lib/test-fixtures');
 
 describe('github-org-service #slow #github', function() {
+
+  fixtureLoader.ensureIntegrationEnvironment(
+      'GITTER_INTEGRATION_USERNAME',
+      'GITTER_INTEGRATION_USER_SCOPE_TOKEN',
+      'GITTER_INTEGRATION_ORG',
+      'GITTER_INTEGRATION_COLLAB_USERNAME');
+
+  var GITTER_TEST_BOT = {
+    username: fixtureLoader.GITTER_INTEGRATION_USERNAME,
+    githubToken: fixtureLoader.GITTER_INTEGRATION_USER_SCOPE_TOKEN
+  };
 
   describe('members', function() {
 
     it('should fetch members', function(done) {
       var gh = new GithubOrgService(GITTER_TEST_BOT);
 
-      gh.members('gitterHQ')
+      gh.members(fixtureLoader.GITTER_INTEGRATION_ORG)
         .then(function(members) {
           assert(members.length >= 1);
         })
@@ -22,7 +33,7 @@ describe('github-org-service #slow #github', function() {
     it('should return true if a user checks that it is in an org', function(done) {
       var gh = new GithubOrgService(GITTER_TEST_BOT);
 
-      gh.member('gitterHQ', 'gittertestbot')
+      gh.member(fixtureLoader.GITTER_INTEGRATION_ORG, fixtureLoader.GITTER_INTEGRATION_USERNAME)
         .then(function(isMember) {
           assert(isMember);
         })
@@ -32,7 +43,7 @@ describe('github-org-service #slow #github', function() {
     it('should return true if a user checks that another member is in an org', function(done) {
       var gh = new GithubOrgService(GITTER_TEST_BOT);
 
-      gh.member('gitterHQ', 'mydigitalself')
+      gh.member(fixtureLoader.GITTER_INTEGRATION_ORG, fixtureLoader.GITTER_INTEGRATION_COLLAB_USERNAME)
         .then(function(isMember) {
           assert(isMember);
         })
@@ -42,7 +53,7 @@ describe('github-org-service #slow #github', function() {
     it('should return false if a user checks that it is in an org that it is not a member of', function(done) {
       var gh = new GithubOrgService(GITTER_TEST_BOT);
 
-      gh.member('adobe', 'gittertestbot')
+      gh.member('adobe', fixtureLoader.GITTER_INTEGRATION_USERNAME)
         .then(function(isMember) {
           assert(!isMember);
         })
@@ -52,7 +63,7 @@ describe('github-org-service #slow #github', function() {
     it('should return false if a user checks that a stranger is in the users org', function(done) {
       var gh = new GithubOrgService(GITTER_TEST_BOT);
 
-      gh.member('gitterHQ', 'indexzero')
+      gh.member(fixtureLoader.GITTER_INTEGRATION_ORG, 'indexzero')
         .then(function(isMember) {
           assert(!isMember);
         })
@@ -62,10 +73,10 @@ describe('github-org-service #slow #github', function() {
     it('should return membership information for an org', function(done) {
       var gh = new GithubOrgService(GITTER_TEST_BOT);
 
-      gh.getMembership('gitterTest', 'gittertestbot')
+      gh.getMembership(fixtureLoader.GITTER_INTEGRATION_ORG, fixtureLoader.GITTER_INTEGRATION_USERNAME)
         .then(function(membership) {
-          assert('gitterTest', membership.organization.login);
-          assert('gittertestbot', membership.user.login);
+          assert.strictEqual(membership.organization.login, fixtureLoader.GITTER_INTEGRATION_ORG);
+          assert.strictEqual(membership.user.login,fixtureLoader.GITTER_INTEGRATION_USERNAME);
         })
         .nodeify(done);
     });
