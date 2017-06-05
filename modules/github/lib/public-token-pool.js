de6c9a33cda776ba5846c4a277b8b98484493ccb
Andrew Newdigate <andrew@troupe.co>
2017-06-05 11:18:49 +0100

    Investigating...

--- modules/github/lib/public-token-pool.js
+++ modules/github/lib/public-token-pool.js
@@ -8,6 +8,8 @@ var stats = env.stats;
 var anonymousClientId = nconf.get('github:anonymous_app:client_id');
 var anonymousClientSecret = nconf.get('github:anonymous_app:client_secret');
 
+console.log('ANONYMOUSE', anonymousClientId); // eslint-disable-line no-console
+
 module.exports = function(options, callback, request) {
   var accessToken;
   if (options.headers) {
