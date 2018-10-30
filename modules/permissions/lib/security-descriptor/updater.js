0a31579f4af377d0427c360918c90e30e19adc3e
Eric Eastwood <contact@ericeastwood.com>
2018-10-30 16:58:34 -0500

    Remove dead security descriptor updater code
    This method was introduced in https://gitlab.com/gitlab-org/gitter/webapp/commit/2d3b775037d19bf12d1b403376e4fe412181171c#1f22745dcec1a68e40ae2cf8884185fbe232427a_71_79
    but I didn't see any usage ever :/

--- modules/permissions/lib/security-descriptor/updater.js
+++ modules/permissions/lib/security-descriptor/updater.js
@@ -6,41 +6,6 @@ var assert = require('assert');
 var StatusError = require('statuserror');
 var Promise = require('bluebird');
 
-function updateLinksForRepo(linkPath, newLinkPath, externalId) {
-  assert(linkPath, 'linkPath expected');
-  assert(newLinkPath, 'newLinkPath expected');
-
-  var parts = newLinkPath.split('/');
-  if (parts.length !== 2) {
-    throw new StatusError(400, 'Invalid linkPath attribute');
-  }
-
-  if (!parts[0].length || !parts[1].length) {
-    throw new StatusError(400, 'Invalid linkPath attribute: ' + linkPath);
-  }
-
-  var query = {
-    'sd.type': 'GH_REPO',
-    'sd.linkPath': linkPath
-  };
-
-  var update = {
-    $set: {
-      'sd.linkPath': newLinkPath
-    }
-  };
-
-  if (externalId) {
-    update.$set['sd.externalId'] = externalId;
-  }
-
-  return Promise.join(
-    Troupe.update(query, update, { multi: true }).exec(),
-    Group.update(query, update, { multi: true }).exec());
-
-  // TODO: consider sending live-collection updates
-}
-
 function updatePublicFlagForRepo(linkPath, isPublic) {
   assert(linkPath, 'linkPath expected');
   assert(isPublic === true || isPublic === false, 'isPublic must be a boolean');
@@ -64,6 +29,5 @@ function updatePublicFlagForRepo(linkPath, isPublic) {
 }
 
 module.exports = {
-  updateLinksForRepo: updateLinksForRepo,
   updatePublicFlagForRepo: updatePublicFlagForRepo,
 };
