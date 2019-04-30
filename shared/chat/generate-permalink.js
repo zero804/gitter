c53040752086578f4db96dc0c9433bc5e32021ef
Tomas Vik <vicek22@gmail.com>
2019-04-30 10:43:04 +0200

    Rename troupeName + default parameters in generatePermalink

--- shared/chat/generate-permalink.js
+++ shared/chat/generate-permalink.js
@@ -7,17 +7,17 @@ const clientEnv = require('gitter-client-env');
 /*
  * Generates absolute URL representing permanent link to a chat item
  *
- * troupeName - URL segment identifying room (usually community-name/room-name)
+ * troupeUri - URL segment identifying room (usually community-name/room-name)
  * id - message id
  * sent - date when the message was sent, accepted formats: JavaScript Date, moment
  * isArchive - if true, generated link will point to room archive,
  *             otherwise link points to normal chat
  */
-module.exports = (troupeName, id, sent, isArchive) => {
+module.exports = (troupeUri, id, sent = null, isArchive = false) => {
   const basePath = clientEnv['basePath'];
   if (isArchive) {
     const urlDate = moment(sent).format('YYYY/MM/DD');
-    return urlJoin(basePath, troupeName, 'archives', urlDate, `?at=${id}`);
+    return urlJoin(basePath, troupeUri, 'archives', urlDate, `?at=${id}`);
   }
-  return urlJoin(basePath, troupeName, `?at=${id}`);
+  return urlJoin(basePath, troupeUri, `?at=${id}`);
 };
