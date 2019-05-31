e395c36c71d94f9f0bbe3f821b92b04c3644158b
Eric Eastwood <contact@ericeastwood.com>
2019-05-31 02:03:11 -0500

    Update all public-js tests to use Jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  testMatch: ['<rootDir>/test/public-js/vue/**/*-test.js'],
+  testMatch: ['<rootDir>/test/public-js/**/*-test.js'],
   moduleFileExtensions: ['js', 'json', 'vue'],
   transform: {
     '^.+\\.js$': 'babel-jest',
