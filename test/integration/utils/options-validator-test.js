06720169cbeabf0c57341a1338139d9738c527c2
Tomas Vik <tvik@gitlab.com>
2019-07-19 08:27:54 +0200

    Stub logger.warn so we don't see warnings in the test ouptut

--- test/integration/utils/options-validator-test.js
+++ test/integration/utils/options-validator-test.js
@@ -11,6 +11,11 @@ const { createOptionsValidator } = testRequire.withProxies('./utils/options-vali
 describe('option-validator', function() {
   it('finds unexpected options and logs warning', () => {
     const validator = createOptionsValidator('validator-name', ['a', 'b', 'd', 'x']);
+    // stub the real warn so we don't see it in test output
+    mockito
+      .when(loggerMock)
+      .warn()
+      .then(() => undefined);
     validator({ a: 1, b: 2, c: 3, d: 4, e: 5 });
     mockito.verify(loggerMock).warn('unexpected options - validator-name - {"c":3,"e":5}');
   });
