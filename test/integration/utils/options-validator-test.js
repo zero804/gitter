7439bccce64b577f97b3a7e6b1ee5bc93f0cc093
Tomas Vik <tvik@gitlab.com>
2019-07-22 10:23:53 +0200

    validate that only the correct warning is logged

--- test/integration/utils/options-validator-test.js
+++ test/integration/utils/options-validator-test.js
@@ -4,6 +4,7 @@ const testRequire = require('../test-require');
 const mockito = require('jsmockito').JsMockito;
 const underlyingEnv = require('gitter-web-env');
 const loggerMock = mockito.spy(underlyingEnv.logger);
+const assert = require('assert');
 const { createOptionsValidator } = testRequire.withProxies('./utils/options-validator', {
   'gitter-web-env': { logger: loggerMock }
 });
@@ -20,7 +21,7 @@ describe('option-validator', function() {
     mockito
       .when(loggerMock)
       .warn()
-      .then(() => undefined);
+      .then(message => assert(message.match(/unexpected options/)));
     validator({ a: 1, b: 2, c: 3, d: 4, e: 5 });
     mockito.verify(loggerMock).warn('unexpected options - validator-name - {"c":3,"e":5}');
   });
