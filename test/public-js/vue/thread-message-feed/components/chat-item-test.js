dc2803d9408119077f3ee888fa2b42472c5e8d2f
Tomas Vik <tvik@gitlab.com>
2019-07-25 17:20:39 +0200

    Add tests for compact styles and showActionItems

--- test/public-js/vue/thread-message-feed/components/chat-item-test.js
+++ test/public-js/vue/thread-message-feed/components/chat-item-test.js
@@ -31,8 +31,18 @@ describe('thread-message-feed chat-item', () => {
     useCompactStyles: true
   };
 
-  it('matches snapshot', () => {
-    const { wrapper } = mount(ChatItem, defaultProps);
-    expect(wrapper.element).toMatchSnapshot();
+  describe('snapshot', () => {
+    it('with default props', () => {
+      const { wrapper } = mount(ChatItem, defaultProps);
+      expect(wrapper.element).toMatchSnapshot();
+    });
+    it('showing item actions', () => {
+      const { wrapper } = mount(ChatItem, { ...defaultProps, showItemActions: true });
+      expect(wrapper.element).toMatchSnapshot();
+    });
+    it('compact styles', () => {
+      const { wrapper } = mount(ChatItem, { ...defaultProps, useCompactStyles: true });
+      expect(wrapper.element).toMatchSnapshot();
+    });
   });
 });
