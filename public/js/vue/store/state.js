c99592de21ef922a021ee437f1838dcffc9041f1
Tomas Vik <tvik@gitlab.com>
2019-08-06 15:10:44 +0200

    use the vuex request intial state

--- public/js/vue/store/state.js
+++ public/js/vue/store/state.js
@@ -1,36 +1,45 @@
 import * as leftMenuConstants from '../left-menu/constants';
+import * as _ from 'lodash';
+import {
+  roomSearchRepoRequest,
+  roomSearchRoomRequest,
+  roomSearchPeopleRequest,
+  messageSearchRequest
+} from './requests';
 
 function state() {
-  return {
-    test: 'initialTestValue',
+  return _.defaultsDeep(
+    roomSearchRepoRequest.initialState,
+    roomSearchRoomRequest.initialState,
+    roomSearchPeopleRequest.initialState,
+    messageSearchRequest.initialState,
+    {
+      test: 'initialTestValue',
 
-    isMobile: false,
-    // `true` just for the sake of easy tests
-    isLoggedIn: true,
-    darkTheme: false,
+      isMobile: false,
+      // `true` just for the sake of easy tests
+      isLoggedIn: true,
+      darkTheme: false,
 
-    groupMap: {},
-    roomMap: {},
-    messageMap: {},
+      groupMap: {},
+      roomMap: {},
+      messageMap: {},
 
-    displayedRoomId: null,
-    hightLightedMessageId: null,
+      displayedRoomId: null,
+      hightLightedMessageId: null,
 
-    leftMenuState: leftMenuConstants.LEFT_MENU_ALL_STATE,
-    leftMenuPinnedState: true,
-    leftMenuExpandedState: false,
-    favouriteDraggingInProgress: false,
+      leftMenuState: leftMenuConstants.LEFT_MENU_ALL_STATE,
+      leftMenuPinnedState: true,
+      leftMenuExpandedState: false,
+      favouriteDraggingInProgress: false,
 
-    search: {
-      searchInputValue: '',
+      search: {
+        searchInputValue: '',
 
-      current: { results: [] },
-      repo: { loading: false, error: false, results: [] },
-      room: { loading: false, error: false, results: [] },
-      people: { loading: false, error: false, results: [] },
-      message: { loading: false, error: false, results: [] }
+        current: { results: [] }
+      }
     }
-  };
+  );
 }
 
 export default state;
