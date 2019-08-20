c3d2550050711ce76b4cab90f27b0b48d0b80871
Tomas Vik <tvik@gitlab.com>
2019-08-20 12:36:50 +0200

    Remove unnecessary chatCollection

--- public/js/vue/thread-message-feed/components/chat-input-test.js
+++ public/js/vue/thread-message-feed/components/chat-input-test.js
@@ -35,7 +35,6 @@ describe('thread-message-feed chat-input', () => {
 
     it('should trigger send action when enter key is pressed', () => {
       const { wrapper, stubbedActions } = mount(ChatInput, defaultProps, store => {
-        store.state.threadMessageFeed.chatCollection = { create: jest.fn() };
         store.state.user = { _id: 'userId' };
       });
       wrapper.find({ ref: 'chat-input' }).trigger('keydown.enter');
