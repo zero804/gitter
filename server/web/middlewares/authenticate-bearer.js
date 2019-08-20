146cd0c586cf88c5117406d2ea24c7adcd50328f
Eric Eastwood <contact@ericeastwood.com>
2019-08-20 10:13:30 -0500

    Remove access token after logging out
    Fix https://gitlab.com/gitlab-org/gitter/webapp/issues/2056

--- server/web/middlewares/authenticate-bearer.js
+++ server/web/middlewares/authenticate-bearer.js
@@ -1,50 +1,10 @@
 'use strict';
 
-var env = require('gitter-web-env');
-var logger = env.logger;
 var StatusError = require('statuserror');
 var oauthService = require('../../services/oauth-service');
 var validateUserAgentFromReq = require('../validate-user-agent-from-req');
 var checkAlreadyOnUnauthorizedUrl = require('../../utils/check-already-on-unauthorized-url');
-
-// eslint-disable-next-line complexity
-function getAccessToken(req) {
-  if (req.headers && req.headers['authorization']) {
-    var authHeader = req.headers['authorization'];
-
-    /* Temporary fix - remove 15 May 2014 */
-    /* A bug in the OSX client adds this header each time a refresh is done */
-    if (authHeader.indexOf('Bearer ') === 0 && authHeader.indexOf(',') >= 0) {
-      logger.warn('auth: compensating for incorrect auth header');
-      authHeader = authHeader.split(/,/)[0];
-    }
-
-    var parts = authHeader.split(' ');
-
-    if (parts.length === 2) {
-      var scheme = parts[0];
-
-      if (/Bearer/i.test(scheme)) {
-        return parts[1];
-      }
-    }
-  }
-
-  if (req.body && req.body['access_token']) {
-    return req.body['access_token'];
-  }
-
-  if (req.query && req.query['access_token']) {
-    return req.query['access_token'];
-  }
-
-  // FIXME Hack for the node-webkit app, we *have* to send the token in the user-agent header.
-  // If in the future node-webkit adds support for custom headers we can remove this.
-  if (req.headers && req.headers['user-agent']) {
-    var ua_token = req.headers['user-agent'].match(/Token\/(\w+)/);
-    if (ua_token) return ua_token[1];
-  }
-}
+const getAccessToken = require('../get-access-token-from-req');
 
 /**
  *
