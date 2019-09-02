3f9beb38a63b22b5ce2ace90f900e3ec4d12b084
Tomas Vik <tvik@gitlab.com>
2019-09-02 09:13:56 +0200

    Show "Reply in thread" for parent messages

--- public/js/views/chat/chatItemView.js
+++ public/js/views/chat/chatItemView.js
@@ -558,7 +558,7 @@ module.exports = (function() {
         chatItemView: this,
         targetElement: e.target,
         placement: 'horizontal',
-        width: useThreadedConversations ? '115px' : '100px'
+        width: useThreadedConversations ? '125px' : '100px'
       });
 
       this.listenTo(actions, 'render', function() {
@@ -834,9 +834,10 @@ module.exports = (function() {
       const actions = [];
 
       if (useThreadedConversations) {
+        const threadMessageCount = this.model.get('threadMessageCount');
         actions.push({
           name: 'threadReply',
-          description: 'Start a thread',
+          description: threadMessageCount ? 'Reply in thread' : 'Start a thread',
           disabled: !isPersisted
         });
       } else {
