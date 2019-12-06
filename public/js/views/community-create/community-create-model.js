0d86c454d001bd470543dd329b5d3fbd90e6b0c5
Eric Eastwood <contact@ericeastwood.com>
2019-12-05 22:14:53 -0600

    Remove community creation invite step
    Fix https://gitlab.com/gitlab-org/gitter/webapp/issues/2103
    
    Invites can be done after the fact from the room. Nothing special about adding people there
    
    Sometimes the invites can fail and the community creates sucessfully but
    one of the invite fails so the UI shows an error which just confuses people.
    
    We could make the invite errors more clear in the creation process but it's
    simpler just to remove the invite step.
    
    It also removes a barrier to entry to just create a community (less to think about).

--- public/js/views/community-create/community-create-model.js
+++ public/js/views/community-create/community-create-model.js
@@ -3,17 +3,7 @@
 var Backbone = require('backbone');
 var urlJoin = require('url-join');
 var stepConstants = require('./step-constants');
-var apiClient = require('../../components/api-client');
 var Promise = require('bluebird');
-var peopleToInviteStatusConstants = require('./people-to-invite-status-constants');
-
-var INVITE_STATUS_PENDING = peopleToInviteStatusConstants.PENDING;
-var INVITE_STATUS_READY = peopleToInviteStatusConstants.READY;
-var INVITE_STATUS_NEEDS_EMAIL = peopleToInviteStatusConstants.NEEDS_EMAIL;
-
-function inviteNeedsEmailPredicate(invite) {
-  return invite.get('inviteStatus') !== INVITE_STATUS_READY;
-}
 
 var CommunityCreateModel = Backbone.Model.extend({
   defaults: {
@@ -35,8 +25,6 @@ var CommunityCreateModel = Backbone.Model.extend({
   initialize: function(attrs, options) {
     this.orgs = options.orgs;
     this.repos = options.repos;
-
-    this.invites = new Backbone.Collection([]);
   },
 
   getGithubProjectInfo: function() {
@@ -76,56 +64,6 @@ var CommunityCreateModel = Backbone.Model.extend({
     };
   },
 
-  addInvitation: function(type, externalId, displayName, avatarUrl) {
-    var id = type + ':' + externalId;
-    if (this.invites.get(id)) {
-      // Ignore
-      return;
-    }
-
-    var inviteStatus = INVITE_STATUS_PENDING;
-    var emailAddress = null;
-
-    if (type === 'email') {
-      inviteStatus = INVITE_STATUS_READY;
-      emailAddress = externalId;
-    }
-
-    var invite = this.invites.add({
-      id: id,
-      type: type,
-      externalId: externalId,
-      displayName: displayName,
-      avatarUrl: avatarUrl,
-      emailAddress: emailAddress,
-      inviteStatus: inviteStatus
-    });
-
-    if (type !== 'email') {
-      // Check if we need to capture the email address
-      apiClient.priv
-        .get('/check-invite', { type: type, externalId: externalId })
-        .then(function() {
-          invite.set({
-            inviteStatus: INVITE_STATUS_READY
-          });
-        })
-        .catch(function() {
-          invite.set('inviteStatus', INVITE_STATUS_NEEDS_EMAIL);
-        });
-    }
-
-    return invite;
-  },
-
-  /**
-   * Returns true if manual entry is required for some email addresses
-   */
-  hasInvitesRequiringEmailEntry: function() {
-    // Are all invites set to 'ready?'
-    return this.invites.some(inviteNeedsEmailPredicate);
-  },
-
   getSecurityData: function() {
     var githubInfo = this.getGithubProjectInfo();
     if (githubInfo && githubInfo.type) {
@@ -138,26 +76,13 @@ var CommunityCreateModel = Backbone.Model.extend({
     return undefined; // No security info
   },
 
-  getInviteData: function() {
-    return this.invites.map(function(item) {
-      var attributes = item.attributes;
-      return {
-        type: attributes.type,
-        externalId: attributes.externalId,
-        emailAddress: attributes.emailAddress
-      };
-    });
-  },
-
   getSerializedCreateData: function() {
     var security = this.getSecurityData();
-    var invites = this.getInviteData();
 
     return {
       name: this.get('communityName'),
       uri: this.get('communitySlug'),
       security: security,
-      invites: invites,
       addBadge: this.get('allowBadger'),
       allowTweeting: true
     };
@@ -233,8 +158,5 @@ var CommunityCreateModel = Backbone.Model.extend({
     this.set(githubInfo);
   }
 });
-
-// Static Methods
-CommunityCreateModel.inviteNeedsEmailPredicate = inviteNeedsEmailPredicate;
 
 module.exports = CommunityCreateModel;
