3efb148c2fb980e63a16484ee240bd5c41861016
Eric Eastwood <contact@ericeastwood.com>
2020-01-23 02:26:39 -0600

    Refactor group-with-policy-service tests to async/await
    Part of https://gitlab.com/gitlab-org/gitter/webapp/issues/2393

--- test/integration/services/group-with-policy-service-test.js
+++ test/integration/services/group-with-policy-service-test.js
@@ -1,14 +1,12 @@
 'use strict';
 
-var testRequire = require('../test-require');
-var assert = require('assert');
-var fixtureLoader = require('gitter-web-test-utils/lib/test-fixtures');
-var Promise = require('bluebird');
-var StatusError = require('statuserror');
-var GroupWithPolicyService = testRequire('./services/group-with-policy-service');
-var securityDescriptorService = require('gitter-web-permissions/lib/security-descriptor');
+const testRequire = require('../test-require');
+const assert = require('assert');
+const fixtureLoader = require('gitter-web-test-utils/lib/test-fixtures');
+const GroupWithPolicyService = testRequire('./services/group-with-policy-service');
+const securityDescriptorService = require('gitter-web-permissions/lib/security-descriptor');
 
-var isAdminPolicy = {
+const isAdminPolicy = {
   canAdmin: function() {
     return Promise.resolve(true);
   },
@@ -18,16 +16,16 @@ var isAdminPolicy = {
 };
 
 describe('group-with-policy-service #slow', function() {
-  var group1WithPolicyService;
-  var group2WithPolicyService;
-  var group3WithPolicyService;
+  let group1WithPolicyService;
+  let group2WithPolicyService;
+  let group3WithPolicyService;
 
-  var linkPath =
+  const linkPath =
     fixtureLoader.GITTER_INTEGRATION_USERNAME + '/' + fixtureLoader.GITTER_INTEGRATION_REPO;
 
   fixtureLoader.ensureIntegrationEnvironment('#integrationUser1', 'GITTER_INTEGRATION_ORG');
 
-  var fixture = fixtureLoader.setupEach({
+  const fixture = fixtureLoader.setupEach({
     deleteDocuments: {
       Group: [
         { lcUri: fixtureLoader.GITTER_INTEGRATION_USERNAME.toLowerCase() },
@@ -108,272 +106,241 @@ describe('group-with-policy-service #slow', function() {
     );
   });
 
-  // repo rooms
-
-  it('should create a repo room (inherited)', function() {
-    return group1WithPolicyService
-      .createRoom({
-        type: 'GH_REPO',
-        name: fixtureLoader.GITTER_INTEGRATION_REPO,
-        security: 'PUBLIC',
-        linkPath: linkPath
-      })
-      .then(function(createRoomResult) {
-        var room = createRoomResult.troupe;
-        return securityDescriptorService.room.findById(room._id, null);
-      })
-      .then(function(securityDescriptor) {
-        assert.deepEqual(securityDescriptor, {
-          type: 'GH_REPO',
-          members: 'PUBLIC',
-          admins: 'GH_REPO_PUSH',
-          public: true,
-          linkPath: linkPath,
-          externalId: fixtureLoader.GITTER_INTEGRATION_REPO_ID
-        });
-      });
-  });
-
-  it('should create a repo room (public)', function() {
-    return group1WithPolicyService
-      .createRoom({
-        type: 'GH_REPO',
-        name: fixtureLoader.GITTER_INTEGRATION_REPO,
-        security: 'PUBLIC',
-        linkPath: linkPath
-      })
-      .then(function(createRoomResult) {
-        var room = createRoomResult.troupe;
-        return securityDescriptorService.room.findById(room._id, null);
-      })
-      .then(function(securityDescriptor) {
-        assert.deepEqual(securityDescriptor, {
-          type: 'GH_REPO',
-          members: 'PUBLIC',
-          admins: 'GH_REPO_PUSH',
-          public: true,
-          linkPath: linkPath,
-          externalId: fixtureLoader.GITTER_INTEGRATION_REPO_ID
-        });
-      });
-  });
-
-  it('should create a repo room (private)', function() {
-    return group1WithPolicyService
-      .createRoom({
-        type: 'GH_REPO',
-        name: fixtureLoader.GITTER_INTEGRATION_REPO,
-        security: 'PRIVATE',
-        linkPath: linkPath
-      })
-      .then(function(createRoomResult) {
-        var room = createRoomResult.troupe;
-        return securityDescriptorService.room.findById(room._id, null);
-      })
-      .then(function(securityDescriptor) {
-        assert.deepEqual(securityDescriptor, {
-          type: 'GH_REPO',
-          members: 'GH_REPO_ACCESS',
-          admins: 'GH_REPO_PUSH',
-          public: false,
-          linkPath: linkPath,
-          externalId: fixtureLoader.GITTER_INTEGRATION_REPO_ID
-        });
-      });
-  });
-
-  // normal rooms
-
-  it('should create a normal room (public)', function() {
-    var topic = 'litter us with puns';
-    return group2WithPolicyService
-      .createRoom({
+  describe('normal rooms', () => {
+    it('should create a normal room (public)', async () => {
+      const topic = 'litter us with puns';
+      const createRoomResult = await group2WithPolicyService.createRoom({
         type: null,
         name: fixtureLoader.GITTER_INTEGRATION_ROOM,
         topic: topic,
         security: 'PUBLIC'
-      })
-      .then(function(createRoomResult) {
-        var room = createRoomResult.troupe;
-        assert.strictEqual(
-          room.uri,
-          fixture.group2.uri + '/' + fixtureLoader.GITTER_INTEGRATION_ROOM
-        );
-        assert.strictEqual(
-          room.lcUri,
-          fixture.group2.lcUri + '/' + fixtureLoader.GITTER_INTEGRATION_ROOM.toLowerCase()
-        );
-        assert.equal(room.topic, topic);
-        assert.equal(room.groupId, fixture.group2.id);
-
-        return securityDescriptorService.room.findById(room._id, null);
-      })
-      .then(function(securityDescriptor) {
-        assert.deepEqual(securityDescriptor, {
-          type: null,
-          public: true,
-          admins: 'MANUAL',
-          members: 'PUBLIC'
-        });
       });
-  });
 
-  it('should create a normal room (private)', function() {
-    var topic = 'should we change the topic?';
-    return group2WithPolicyService
-      .createRoom({
+      const room = createRoomResult.troupe;
+      assert.strictEqual(
+        room.uri,
+        fixture.group2.uri + '/' + fixtureLoader.GITTER_INTEGRATION_ROOM
+      );
+      assert.strictEqual(
+        room.lcUri,
+        fixture.group2.lcUri + '/' + fixtureLoader.GITTER_INTEGRATION_ROOM.toLowerCase()
+      );
+      assert.equal(room.topic, topic);
+      assert.equal(room.groupId, fixture.group2.id);
+
+      const securityDescriptor = await securityDescriptorService.room.findById(room._id, null);
+
+      assert.deepEqual(securityDescriptor, {
+        type: null,
+        public: true,
+        admins: 'MANUAL',
+        members: 'PUBLIC'
+      });
+    });
+
+    it('should create a normal room (private)', async () => {
+      const topic = 'should we change the topic?';
+      const createRoomResult = await group2WithPolicyService.createRoom({
         type: null,
         name: fixtureLoader.GITTER_INTEGRATION_ROOM,
         topic: topic,
         security: 'PRIVATE'
-      })
-      .then(function(createRoomResult) {
-        var room = createRoomResult.troupe;
-        return securityDescriptorService.room.findById(room._id, null);
-      })
-      .then(function(securityDescriptor) {
-        assert.deepEqual(securityDescriptor, {
-          type: null,
-          public: false,
-          admins: 'MANUAL',
-          members: 'INVITE'
-        });
       });
-  });
 
-  it('should throw an error when validateRoomName fails', function() {
-    return group2WithPolicyService
-      .createRoom({
+      const room = createRoomResult.troupe;
+      const securityDescriptor = await securityDescriptorService.room.findById(room._id, null);
+
+      assert.deepEqual(securityDescriptor, {
         type: null,
-        name: '',
-        security: 'PUBLIC'
-      })
-      .then(function() {
+        public: false,
+        admins: 'MANUAL',
+        members: 'INVITE'
+      });
+    });
+
+    it('should throw an error when validateRoomName fails', async () => {
+      try {
+        await group2WithPolicyService.createRoom({
+          type: null,
+          name: '',
+          security: 'PUBLIC'
+        });
         assert.ok(false, 'expected error');
-      })
-      .catch(StatusError, function(error) {
-        assert.strictEqual(error.status, 400);
-      });
-  });
+      } catch (err) {
+        assert.strictEqual(err.status, 400);
+      }
+    });
 
-  it('should throw an error when the room uri is already taken', function() {
-    var roomOpts = {
-      type: null,
-      name: fixtureLoader.GITTER_INTEGRATION_ROOM,
-      security: 'PUBLIC'
-    };
-    // create once
-    return group2WithPolicyService
-      .createRoom(roomOpts)
-      .then(function() {
+    it('should throw an error when the room uri is already taken', async () => {
+      const roomOpts = {
+        type: null,
+        name: fixtureLoader.GITTER_INTEGRATION_ROOM,
+        security: 'PUBLIC'
+      };
+
+      try {
+        // create once
+        await group2WithPolicyService.createRoom(roomOpts);
         // create twice
-        return group2WithPolicyService.createRoom(roomOpts);
-      })
-      .then(function() {
+        await group2WithPolicyService.createRoom(roomOpts);
+
         assert.ok(false, 'error expected');
-      })
-      .catch(StatusError, function(err) {
+      } catch (err) {
         assert.deepEqual(err.status, 409);
-      });
+      }
+    });
   });
 
-  // org rooms
+  describe('GitHub repo rooms', () => {
+    it('should create a repo room (inherited)', async () => {
+      const createRoomResult = await group1WithPolicyService.createRoom({
+        type: 'GH_REPO',
+        name: fixtureLoader.GITTER_INTEGRATION_REPO,
+        security: 'PUBLIC',
+        linkPath: linkPath
+      });
 
-  it('should create an org room (public)', function() {
-    return group3WithPolicyService
-      .createRoom({
+      const room = createRoomResult.troupe;
+      const securityDescriptor = await securityDescriptorService.room.findById(room._id, null);
+
+      assert.deepEqual(securityDescriptor, {
+        type: 'GH_REPO',
+        members: 'PUBLIC',
+        admins: 'GH_REPO_PUSH',
+        public: true,
+        linkPath: linkPath,
+        externalId: fixtureLoader.GITTER_INTEGRATION_REPO_ID
+      });
+    });
+
+    it('should create a repo room (public)', async () => {
+      const createRoomResult = await group1WithPolicyService.createRoom({
+        type: 'GH_REPO',
+        name: fixtureLoader.GITTER_INTEGRATION_REPO,
+        security: 'PUBLIC',
+        linkPath: linkPath
+      });
+
+      const room = createRoomResult.troupe;
+      const securityDescriptor = await securityDescriptorService.room.findById(room._id, null);
+
+      assert.deepEqual(securityDescriptor, {
+        type: 'GH_REPO',
+        members: 'PUBLIC',
+        admins: 'GH_REPO_PUSH',
+        public: true,
+        linkPath: linkPath,
+        externalId: fixtureLoader.GITTER_INTEGRATION_REPO_ID
+      });
+    });
+
+    it('should create a repo room (private)', async () => {
+      const createRoomResult = await group1WithPolicyService.createRoom({
+        type: 'GH_REPO',
+        name: fixtureLoader.GITTER_INTEGRATION_REPO,
+        security: 'PRIVATE',
+        linkPath: linkPath
+      });
+
+      const room = createRoomResult.troupe;
+      const securityDescriptor = await securityDescriptorService.room.findById(room._id, null);
+
+      assert.deepEqual(securityDescriptor, {
+        type: 'GH_REPO',
+        members: 'GH_REPO_ACCESS',
+        admins: 'GH_REPO_PUSH',
+        public: false,
+        linkPath: linkPath,
+        externalId: fixtureLoader.GITTER_INTEGRATION_REPO_ID
+      });
+    });
+  });
+
+  describe('GitHub org rooms', () => {
+    it('should create an org room (public)', async () => {
+      const createRoomResult = await group3WithPolicyService.createRoom({
         type: 'GH_ORG',
         name: fixtureLoader.GITTER_INTEGRATION_ROOM,
         security: 'PUBLIC',
         linkPath: fixtureLoader.GITTER_INTEGRATION_ORG
-      })
-      .then(function(createRoomResult) {
-        var room = createRoomResult.troupe;
-        return securityDescriptorService.room.findById(room._id, null);
-      })
-      .then(function(securityDescriptor) {
-        assert.deepEqual(securityDescriptor, {
-          type: 'GH_ORG',
-          members: 'PUBLIC',
-          admins: 'GH_ORG_MEMBER',
-          public: true,
-          linkPath: fixtureLoader.GITTER_INTEGRATION_ORG,
-          externalId: fixtureLoader.GITTER_INTEGRATION_ORG_ID
-        });
       });
-  });
 
-  it('should create an org room (private)', function() {
-    return group3WithPolicyService
-      .createRoom({
+      const room = createRoomResult.troupe;
+      const securityDescriptor = await securityDescriptorService.room.findById(room._id, null);
+
+      assert.deepEqual(securityDescriptor, {
+        type: 'GH_ORG',
+        members: 'PUBLIC',
+        admins: 'GH_ORG_MEMBER',
+        public: true,
+        linkPath: fixtureLoader.GITTER_INTEGRATION_ORG,
+        externalId: fixtureLoader.GITTER_INTEGRATION_ORG_ID
+      });
+    });
+
+    it('should create an org room (private)', async () => {
+      const createRoomResult = await group3WithPolicyService.createRoom({
         type: 'GH_ORG',
         name: fixtureLoader.GITTER_INTEGRATION_ROOM,
         security: 'PRIVATE',
         linkPath: fixtureLoader.GITTER_INTEGRATION_ORG
-      })
-      .then(function(createRoomResult) {
-        var room = createRoomResult.troupe;
-        return securityDescriptorService.room.findById(room._id, null);
-      })
-      .then(function(securityDescriptor) {
-        assert.deepEqual(securityDescriptor, {
-          type: 'GH_ORG',
-          members: 'GH_ORG_MEMBER',
-          admins: 'GH_ORG_MEMBER',
-          public: false,
-          linkPath: fixtureLoader.GITTER_INTEGRATION_ORG,
-          externalId: fixtureLoader.GITTER_INTEGRATION_ORG_ID
-        });
       });
+
+      const room = createRoomResult.troupe;
+      const securityDescriptor = await securityDescriptorService.room.findById(room._id, null);
+
+      assert.deepEqual(securityDescriptor, {
+        type: 'GH_ORG',
+        members: 'GH_ORG_MEMBER',
+        admins: 'GH_ORG_MEMBER',
+        public: false,
+        linkPath: fixtureLoader.GITTER_INTEGRATION_ORG,
+        externalId: fixtureLoader.GITTER_INTEGRATION_ORG_ID
+      });
+    });
   });
 
-  // user rooms
-
-  it('should create a user room - public', function() {
-    return group1WithPolicyService
-      .createRoom({
+  describe('user rooms', () => {
+    it('should create a user room - public', async () => {
+      const createRoomResult = await group1WithPolicyService.createRoom({
         type: 'GH_USER',
         name: fixtureLoader.GITTER_INTEGRATION_ROOM,
         security: 'PUBLIC',
         linkPath: fixtureLoader.GITTER_INTEGRATION_USERNAME
-      })
-      .then(function(createRoomResult) {
-        var room = createRoomResult.troupe;
-        return securityDescriptorService.room.findById(room._id, null);
-      })
-      .then(function(securityDescriptor) {
-        assert.deepEqual(securityDescriptor, {
-          type: 'GH_USER',
-          members: 'PUBLIC',
-          admins: 'GH_USER_SAME',
-          public: true,
-          linkPath: fixtureLoader.GITTER_INTEGRATION_USERNAME,
-          externalId: fixtureLoader.GITTER_INTEGRATION_USER_ID
-        });
       });
-  });
 
-  it('should create a user room (private)', function() {
-    return group1WithPolicyService
-      .createRoom({
+      const room = createRoomResult.troupe;
+      const securityDescriptor = await securityDescriptorService.room.findById(room._id, null);
+
+      assert.deepEqual(securityDescriptor, {
+        type: 'GH_USER',
+        members: 'PUBLIC',
+        admins: 'GH_USER_SAME',
+        public: true,
+        linkPath: fixtureLoader.GITTER_INTEGRATION_USERNAME,
+        externalId: fixtureLoader.GITTER_INTEGRATION_USER_ID
+      });
+    });
+
+    it('should create a user room (private)', async () => {
+      const createRoomResult = await group1WithPolicyService.createRoom({
         type: 'GH_USER',
         name: fixtureLoader.GITTER_INTEGRATION_ROOM,
         security: 'PRIVATE',
         linkPath: fixtureLoader.GITTER_INTEGRATION_USERNAME
-      })
-      .then(function(createRoomResult) {
-        var room = createRoomResult.troupe;
-        return securityDescriptorService.room.findById(room._id, null);
-      })
-      .then(function(securityDescriptor) {
-        assert.deepEqual(securityDescriptor, {
-          type: 'GH_USER',
-          members: 'INVITE',
-          admins: 'GH_USER_SAME',
-          public: false,
-          linkPath: fixtureLoader.GITTER_INTEGRATION_USERNAME,
-          externalId: fixtureLoader.GITTER_INTEGRATION_USER_ID
-        });
       });
+
+      const room = createRoomResult.troupe;
+      const securityDescriptor = await securityDescriptorService.room.findById(room._id, null);
+
+      assert.deepEqual(securityDescriptor, {
+        type: 'GH_USER',
+        members: 'INVITE',
+        admins: 'GH_USER_SAME',
+        public: false,
+        linkPath: fixtureLoader.GITTER_INTEGRATION_USERNAME,
+        externalId: fixtureLoader.GITTER_INTEGRATION_USER_ID
+      });
+    });
   });
 });
