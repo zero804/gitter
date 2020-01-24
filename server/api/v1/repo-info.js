e226db99a4b3f15d0d204ef1fd358e06dfa407a9
Eric Eastwood <contact@ericeastwood.com>
2020-01-24 04:04:57 -0600

    Fix GROUP inherited room sd with GL_REPO backed group not showing repo info
    Fix https://gitlab.com/gitlab-org/gitter/webapp/issues/1955
    
    Also getting ready to make the `/api/v1/repo-info` endpoint more generic to support GitLab `GL_PROJECT` in the future

--- server/api/v1/repo-info.js
+++ server/api/v1/repo-info.js
@@ -4,12 +4,14 @@ var RepoService = require('gitter-web-github').GitHubRepoService;
 var StatusError = require('statuserror');
 
 module.exports = function(req, res, next) {
-  var repoName = req.query.repo ? String(req.query.repo) : null;
+  const type = req.query.type ? String(req.query.type) : null;
+  const linkPath = req.query.linkPath ? String(req.query.linkPath) : null;
 
-  if (!repoName) return next(new StatusError(400, 'repo parameter required'));
-  var repoService = new RepoService(req.user);
+  if (type !== 'GH_REPO') return next(new StatusError(400, 'only GH_REPO type supported'));
+  if (!linkPath) return next(new StatusError(400, 'repo parameter required'));
+  const repoService = new RepoService(req.user);
 
-  return repoService.getRepo(repoName).then(function(repo) {
+  return repoService.getRepo(linkPath).then(function(repo) {
     if (!repo) return next(new StatusError(204, 'repo not found'));
     res.send(repo);
   }, next);
