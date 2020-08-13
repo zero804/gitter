f0b171bf9b761d480c15680fcf8afc48454ee706
Eric Eastwood <contact@ericeastwood.com>
2020-08-13 17:41:33 -0500

    Update our cookie utility to use SameSite

--- public/js/utils/font-setup.js
+++ public/js/utils/font-setup.js
@@ -2,7 +2,7 @@
 
 var FontFaceObserver = require('fontfaceobserver');
 var RAF = require('./raf');
-var cookie = require('tiny-cookie');
+const cookies = require('./cookies');
 
 //We only want to observer events for the default font
 var font = new FontFaceObserver('source-sans-pro', {
@@ -13,9 +13,6 @@ font.load().then(function() {
   RAF(function() {
     document.documentElement.className += ' fonts-loaded';
     //Store a cookie to say the fonts have been loaded and should be cached
-    var now = new Date();
-    //TODO this is set 11months from now but should follow our caching rules
-    now.setMonth(now.getMonth() + 11);
-    cookie.set('webfontsLoaded', true, { expires: now.toGMTString() });
+    cookies.set('webfontsLoaded', true);
   });
 });
