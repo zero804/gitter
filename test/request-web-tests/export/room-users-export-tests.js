7fcb0a8765377827573595459425ea5eb966c049
Eric Eastwood <contact@ericeastwood.com>
2020-08-21 09:33:42 -0500

    Add export for room members
    Part of https://gitlab.com/gitlab-org/gitter/webapp/-/issues/2557

--- test/request-web-tests/export/room-users-export-tests.js
+++ test/request-web-tests/export/room-users-export-tests.js
@@ -10,7 +10,7 @@ const request = require('supertest');
 
 const app = require('../../../server/web');
 
-describe('room-messages-export-api', function() {
+describe('room-users-export-api', function() {
   fixtureLoader.ensureIntegrationEnvironment('#oauthTokens');
 
   before(function() {
@@ -21,43 +21,27 @@ describe('room-messages-export-api', function() {
     user1: {
       accessToken: 'web-internal'
     },
+    user2: {
+      accessToken: 'web-internal'
+    },
     userNoExport1: {
       accessToken: 'web-internal'
     },
     troupe1: {
+      users: ['user1', 'user2'],
       securityDescriptor: {
         admins: 'MANUAL',
         extraAdmins: ['user1']
       }
     },
     troupeNoExport1: {
-      securityDescriptor: {
-        admins: 'MANUAL',
-        extraAdmins: ['user1']
-      }
-    },
-
-    message1: {
-      user: 'user1',
-      troupe: 'troupe1',
-      text: 'message1'
-    },
-    message2: {
-      user: 'user1',
-      troupe: 'troupe1',
-      text: 'message2'
-    },
-
-    messageNoExport1: {
-      user: 'userNoExport1',
-      troupe: 'troupeNoExport1',
-      text: 'messageNoExport1'
+      users: ['userNoExport1']
     }
   });
 
-  it('GET /api_web/export/rooms/:room_id/messages.ndjson as admin works', function() {
+  it('GET /api_web/export/rooms/:room_id/users.ndjson as admin works', function() {
     return request(app)
-      .get(`/api_web/export/rooms/${fixture.troupe1.id}/messages.ndjson`)
+      .get(`/api_web/export/rooms/${fixture.troupe1.id}/users.ndjson`)
       .set('Accept', 'application/x-ndjson,application/json')
       .set('Authorization', `Bearer ${fixture.user1.accessToken}`)
       .expect(200)
@@ -65,18 +49,15 @@ describe('room-messages-export-api', function() {
         assert.strictEqual(
           result.text.split('\n').length,
           3,
-          'includes 2 messages (extra newline at the end)'
+          'includes 2 users (extra newline at the end)'
         );
-        assert(result.text.includes(fixture.message1.id), 'includes message1');
-        assert(result.text.includes(fixture.message2.id), 'includes message2');
+        assert(result.text.includes(fixture.user1.id), 'includes user1');
+        assert(result.text.includes(fixture.user2.id), 'includes user2');
         assert(
-          result.text.includes(`"fromUser":"${fixture.user1.id}"`),
-          'serializes userId instead of user object'
+          result.text.includes(`"username":"${fixture.user1.username}"`),
+          'make sure it serializes the user itself and not just raw troupe-user'
         );
-        assert(
-          !result.text.includes(fixture.messageNoExport1.id),
-          'does not include messageNoExport1'
-        );
+        assert(!result.text.includes(fixture.userNoExport1.id), 'does not include userNoExport1');
       });
   });
 });
