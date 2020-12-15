1a415978eeee5a3edd97cb6e87faca407a1f6425
Eric Eastwood <contact@ericeastwood.com>
2020-12-15 15:54:52 -0600

    Add utility script to ensure existing Matrix bridge rooms updated
    Fix https://gitlab.com/gitterHQ/webapp/-/issues/2701

---
+++ scripts/utils/ensure-existing-bridged-matrix-rooms-up-to-date.js
@@ -0,0 +1,89 @@
+#!/usr/bin/env node
+'use strict';
+
+const shutdown = require('shutdown');
+const persistence = require('gitter-web-persistence');
+const { iterableFromMongooseCursor } = require('gitter-web-persistence-utils/lib/mongoose-utils');
+const mongoReadPrefs = require('gitter-web-persistence-utils/lib/mongo-read-prefs');
+
+const installBridge = require('gitter-web-matrix-bridge');
+const matrixBridge = require('gitter-web-matrix-bridge/lib/matrix-bridge');
+const MatrixUtils = require('gitter-web-matrix-bridge/lib/matrix-utils');
+
+const matrixUtils = new MatrixUtils(matrixBridge);
+
+const opts = require('yargs')
+  .option('delay', {
+    alias: 'd',
+    type: 'number',
+    required: true,
+    default: 2000,
+    description:
+      'Delay timeout(in milliseconds) between rooms to update to not overwhelm the homeserver'
+  })
+  .help('help')
+  .alias('help', 'h').argv;
+
+let numberOfRoomsUpdated = 0;
+const failedRoomUpdates = [];
+
+async function updateAllRooms() {
+  const cursor = persistence.MatrixBridgedRoom.find()
+    .lean()
+    .read(mongoReadPrefs.secondaryPreferred)
+    .batchSize(100)
+    .cursor();
+
+  const iterable = iterableFromMongooseCursor(cursor);
+
+  for await (let bridgedRoomEntry of iterable) {
+    try {
+      console.log(
+        `Updating matrixRoomId=${bridgedRoomEntry.matrixRoomId}, gitterRoomId=${bridgedRoomEntry.troupeId}`
+      );
+      await matrixUtils.ensureCorrectRoomState(
+        bridgedRoomEntry.matrixRoomId,
+        bridgedRoomEntry.troupeId
+      );
+      numberOfRoomsUpdated += 1;
+    } catch (err) {
+      console.error(
+        `Failed to update matrixRoomId=${bridgedRoomEntry.matrixRoomId}, gitterRoomId=${bridgedRoomEntry.troupeId}`,
+        err,
+        err.stack
+      );
+      failedRoomUpdates.push(bridgedRoomEntry);
+    }
+
+    // Put a delay between each time we process and update a bridged room
+    // to avoid overwhelming and hitting the rate-limits on the Matrix homeserver
+    if (opts.delay > 0) {
+      await new Promise(resolve => {
+        setTimeout(resolve, opts.delay);
+      });
+    }
+  }
+}
+
+async function run() {
+  try {
+    console.log('Setting up Matrix bridge');
+    await installBridge();
+
+    console.log('Starting to update all bridged rooms');
+    await updateAllRooms();
+    console.log(`All bridged matrix rooms(${numberOfRoomsUpdated}) updated!`);
+
+    if (failedRoomUpdates.length) {
+      console.warn(
+        `But some rooms failed to update (${failedRoomUpdates.length})`,
+        failedRoomUpdates
+      );
+    }
+  } catch (err) {
+    console.error(err, err.stack);
+  }
+  shutdown.shutdownGracefully();
+}
+
+run();
